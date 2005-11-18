601401ef7d56e4a00544b824d43704203b40dbc6
Christian Bauer <christian@hibernate.org>
2005-11-18 22:37:23 +0000

    Added webapp to tutorial
    git-svn-id: https://svn.jboss.org/repos/hibernate/trunk/Hibernate3/doc@8618 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- reference/tutorial/lib/readme.txt
+++ reference/tutorial/lib/readme.txt
@@ -1,3 +1,7 @@
 Put all required Hibernate libraries here to run the tutorial.
 See lib/README.txt in the Hibernate distribution and the tutorial
 chapter in the reference documentation.
+
+You might need other third-party libraries to compile the full
+tutorial source code. For example, you will need jsdk.jar if you
+want to compile the servlets of the example web application.
---
+++ reference/tutorial/src/events/EventManagerServlet.java
@@ -0,0 +1,102 @@
+package events;
+
+import util.HibernateUtil;
+
+import javax.servlet.http.*;
+import javax.servlet.ServletException;
+import java.io.*;
+import java.util.*;
+import java.text.SimpleDateFormat;
+
+public class EventManagerServlet extends HttpServlet {
+
+    private final SimpleDateFormat dateFormatter =
+                            new SimpleDateFormat("dd.MM.yyyy");
+
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
+
+        try {
+            // Begin unit of work
+            HibernateUtil.getSessionFactory()
+                    .getCurrentSession().beginTransaction();
+
+            // Write HTML header
+            PrintWriter out = response.getWriter();
+            out.println("<html><head><title>Event Manager</title></head><body>");
+
+            // Handle actions
+            if ( "store".equals(request.getParameter("action")) ) {
+
+                String eventTitle = request.getParameter("eventTitle");
+                String eventDate = request.getParameter("eventDate");
+
+                if ( "".equals(eventTitle) || "".equals(eventDate) ) {
+                    out.println("<b><i>Please enter event title and date.</i></b>");
+                } else {
+                    createAndStoreEvent(eventTitle, dateFormatter.parse(eventDate));
+                    out.println("<b><i>Added event.</i></b>");
+                }
+            }
+
+            // Print page
+            printEventForm(out);
+            listEvents(out);
+
+            // Write HTML footer
+            out.println("</body></html>");
+            out.flush();
+            out.close();
+
+            // End unit of work
+            HibernateUtil.getSessionFactory()
+                    .getCurrentSession().getTransaction().commit();
+
+        } catch (Exception ex) {
+            HibernateUtil.getSessionFactory()
+                    .getCurrentSession().getTransaction().rollback();
+            throw new ServletException(ex);
+        }
+    }
+
+    private void printEventForm(PrintWriter out) {
+        out.println("<h2>Add new event:</h2>");
+        out.println("<form>");
+        out.println("Title: <input name='eventTitle' length='50'/><br/>");
+        out.println("Date (e.g. 24.12.2009): <input name='eventDate' length='10'/><br/>");
+        out.println("<input type='submit' name='action' value='store'/>");
+        out.println("</form>");
+    }
+
+    private void listEvents(PrintWriter out) {
+        List result = HibernateUtil.getSessionFactory()
+                        .getCurrentSession().createCriteria(Event.class).list();
+        if (result.size() > 0) {
+            out.println("<h2>Events in database:</h2>");
+            out.println("<table border='1'>");
+            out.println("<tr>");
+            out.println("<th>Event title</th>");
+            out.println("<th>Event date</th>");
+            out.println("</tr>");
+            for (Iterator it = result.iterator(); it.hasNext();) {
+                Event event = (Event) it.next();
+                out.println("<tr>");
+                out.println("<td>" + event.getTitle() + "</td>");
+                out.println("<td>" + dateFormatter.format(event.getDate()) + "</td>");
+                out.println("</tr>");
+            }
+            out.println("</table>");
+        }
+    }
+
+    protected void createAndStoreEvent(String title, Date theDate) {
+        Event theEvent = new Event();
+        theEvent.setTitle(title);
+        theEvent.setDate(theDate);
+
+        HibernateUtil.getSessionFactory()
+                        .getCurrentSession().save(theEvent);
+    }
+
+}

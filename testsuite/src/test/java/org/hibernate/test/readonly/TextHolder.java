e959b5787db38fa55b9c2e42517b99f04c8f0c3a
Steve Ebersole <steve@hibernate.org>
2007-08-13 22:16:21 +0000

    added additional test
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/trunk@12929 1b8cb986-b30d-0410-93ca-fae66ebed9b2

---
+++ testsuite/src/test/java/org/hibernate/test/readonly/TextHolder.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2007, Red Hat Middleware, LLC. All rights reserved.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, v. 2.1. This program is distributed in the
+ * hope that it will be useful, but WITHOUT A WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. You should have received a
+ * copy of the GNU Lesser General Public License, v.2.1 along with this
+ * distribution; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * Red Hat Author(s): Steve Ebersole
+ */
+package org.hibernate.test.readonly;
+
+/**
+ * TextHolder implementation
+ *
+ * @author Steve Ebersole
+ */
+public class TextHolder {
+	private Long id;
+	private String theText;
+
+	public TextHolder() {
+	}
+
+	public TextHolder(String theText) {
+		this.theText = theText;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTheText() {
+		return theText;
+	}
+
+	public void setTheText(String theText) {
+		this.theText = theText;
+	}
+}

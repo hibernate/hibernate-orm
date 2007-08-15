68dded3aea860b19a0465638a33dbf84d043a4c7
Diego Plentz <diego@plentz.org>
2007-08-15 14:39:43 +0000

    [HHH-1396] Table.validateColumns fails on valid column
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/trunk@12944 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- core/src/main/java/org/hibernate/mapping/Table.java
+++ core/src/main/java/org/hibernate/mapping/Table.java
@@ -254,7 +254,7 @@ public class Table implements RelationalModel, Serializable {
 				throw new HibernateException( "Missing column: " + col.getName() + " in " + Table.qualify( tableInfo.getCatalog(), tableInfo.getSchema(), tableInfo.getName()));
 			}
 			else {
-				final boolean typesMatch = col.getSqlType( dialect, mapping )
+				final boolean typesMatch = col.getSqlType( dialect, mapping ).toLowerCase()
 						.startsWith( columnInfo.getTypeName().toLowerCase() )
 						|| columnInfo.getTypeCode() == col.getSqlTypeCode( mapping );
 				if ( !typesMatch ) {

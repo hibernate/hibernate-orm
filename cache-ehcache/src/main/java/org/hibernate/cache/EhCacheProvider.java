545296be3f99b614dad85d720578edd253bb672c
Steve Ebersole <steve@hibernate.org>
2007-12-07 03:10:46 +0000

    source headers
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/trunk@14217 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- cache-ehcache/src/main/java/org/hibernate/cache/EhCacheProvider.java
+++ cache-ehcache/src/main/java/org/hibernate/cache/EhCacheProvider.java
@@ -1,18 +1,25 @@
-//$Id: EhCacheProvider.java 9964 2006-05-30 15:40:54Z epbernard $
-/**
- *  Copyright 2003-2006 Greg Luck, Jboss Inc
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Copyright (c) 2007, Red Hat Middleware LLC or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Middleware LLC.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
  */
 package org.hibernate.cache;
 

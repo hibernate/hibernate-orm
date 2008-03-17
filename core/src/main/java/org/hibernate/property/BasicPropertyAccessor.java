e01a7bfe54785bb3dc97055e89936cfb2e7558e1
Steve Ebersole <steve@hibernate.org>
2008-03-17 22:17:35 +0000

    HHH-2268 : handle bridged return types for getters
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/trunk@14448 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- core/src/main/java/org/hibernate/property/BasicPropertyAccessor.java
+++ core/src/main/java/org/hibernate/property/BasicPropertyAccessor.java
@@ -316,8 +316,8 @@ public class BasicPropertyAccessor implements PropertyAccessor {
 
 		Method[] methods = theClass.getDeclaredMethods();
 		for (int i=0; i<methods.length; i++) {
-			// only carry on if the method has no parameters
-			if ( methods[i].getParameterTypes().length==0 ) {
+			// only carry on if the method has no parameters and is not a bridge
+			if ( methods[i].getParameterTypes().length == 0 && !methods[i].isBridge() ) {
 				String methodName = methods[i].getName();
 
 				// try "get"
@@ -330,9 +330,7 @@ public class BasicPropertyAccessor implements PropertyAccessor {
 
 				}
 
-				// if not "get" then try "is"
-				/*boolean isBoolean = methods[i].getReturnType().equals(Boolean.class) ||
-					methods[i].getReturnType().equals(boolean.class);*/
+				// if not "get", then try "is"
 				if ( methodName.startsWith("is") ) {
 					String testStdMethod = Introspector.decapitalize( methodName.substring(2) );
 					String testOldMethod = methodName.substring(2);

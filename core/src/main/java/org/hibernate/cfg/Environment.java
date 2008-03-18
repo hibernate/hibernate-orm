bd2e1a6a4b8ac8ae75c4fead17fa6d485af52703
Steve Ebersole <steve@hibernate.org>
2008-03-18 07:26:18 +0000

    prep 3.3.0.CR1 release
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/trunk@14456 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- core/src/main/java/org/hibernate/cfg/Environment.java
+++ core/src/main/java/org/hibernate/cfg/Environment.java
@@ -153,7 +153,7 @@ import org.hibernate.util.PropertiesHelper;
  */
 public final class Environment {
 
-	public static final String VERSION = "3.2 cr3";
+	public static final String VERSION = "3.3.0.CR1";
 
 	/**
 	 * <tt>ConnectionProvider</tt> implementor to use when obtaining connections

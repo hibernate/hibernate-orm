73625c07f82e22edff1796689c039ed66d1ce5ae
Steve Ebersole <steve@hibernate.org>
2008-10-03 03:02:25 +0000

    HHH-3481 : minor bug in previous JTATransactionFactory patch
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/branches/Branch_3_2@15250 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- src/org/hibernate/transaction/JTATransactionFactory.java
+++ src/org/hibernate/transaction/JTATransactionFactory.java
@@ -155,12 +155,13 @@ public class JTATransactionFactory implements TransactionFactory {
 	 * @return The appropriate {@link UserTransaction} reference.
 	 */
 	protected UserTransaction getUserTransaction() {
-		log.trace( "Attempting to locate UserTransaction via JNDI [" + getUserTransactionName() + "]" );
+		final String utName = getUserTransactionName();
+		log.trace( "Attempting to locate UserTransaction via JNDI [" + utName + "]" );
 
 		try {
-			UserTransaction ut = ( UserTransaction ) getInitialContext().lookup( getUserTransactionName() );
+			UserTransaction ut = ( UserTransaction ) getInitialContext().lookup( utName );
 			if ( ut == null ) {
-				throw new TransactionException( "Naming service lookup for UserTransaction returned null [" + getUserTransactionName() +"]" );
+				throw new TransactionException( "Naming service lookup for UserTransaction returned null [" + utName +"]" );
 			}
 
 			log.trace( "Obtained UserTransaction" );
@@ -168,7 +169,7 @@ public class JTATransactionFactory implements TransactionFactory {
 			return ut;
 		}
 		catch ( NamingException ne ) {
-			throw new TransactionException( "Could not find UserTransaction in JNDI [" + getUserTransaction() + "]", ne );
+			throw new TransactionException( "Could not find UserTransaction in JNDI [" + utName + "]", ne );
 		}
 	}
 

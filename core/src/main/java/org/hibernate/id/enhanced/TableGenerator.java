4d23b828036eadae5627065a961e5fddf8144757
Juraci Krohling <jcosta@redhat.com>
2008-12-12 15:37:44 +0000

    HHH-3650 - Added fixed 'not null' to the create-table statement
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/trunk@15690 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- core/src/main/java/org/hibernate/id/enhanced/TableGenerator.java
+++ core/src/main/java/org/hibernate/id/enhanced/TableGenerator.java
@@ -518,6 +518,7 @@ public class TableGenerator extends TransactionHelper implements PersistentIdent
 						.append( segmentColumnName )
 						.append( ' ' )
 						.append( dialect.getTypeName( Types.VARCHAR, segmentValueLength, 0, 0 ) )
+						.append( " not null " )
 						.append( ",  " )
 						.append( valueColumnName )
 						.append( ' ' )

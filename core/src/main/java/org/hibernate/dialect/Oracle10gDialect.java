4167921ea37f7fba4f86b9c6a01143b4a991df16
Chris Bredesen <cbredesen@redhat.com>
2009-01-26 20:30:09 +0000

    HHH-3159 replace 'oracle.jdbc.driver' with 'oracle.jdbc'
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/trunk@15820 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- core/src/main/java/org/hibernate/dialect/Oracle10gDialect.java
+++ core/src/main/java/org/hibernate/dialect/Oracle10gDialect.java
@@ -32,7 +32,9 @@ import org.hibernate.sql.ANSIJoinFragment;
  * A dialect specifically for use with Oracle 10g.
  * <p/>
  * The main difference between this dialect and {@link Oracle9iDialect}
- * is the use of "ANSI join syntax" here...
+ * is the use of "ANSI join syntax".  This dialect also retires the use
+ * of the <tt>oracle.jdbc.driver</tt> package in favor of 
+ * <tt>oracle.jdbc</tt>.
  *
  * @author Steve Ebersole
  */
@@ -46,4 +48,12 @@ public class Oracle10gDialect extends Oracle9iDialect {
 		return new ANSIJoinFragment();
 	}
 
+	/*
+	 * The package "oracle.jdbc.driver" was retired in 9.0.1 but works fine up
+	 * through 10g. So as not to mess with 9i, we're changing it in 10g -- we
+	 * may not need an 11g Dialect at all.
+	 */
+	String getOracleTypesClassName() {
+		return "oracle.jdbc.OracleTypes";
+	}
 }

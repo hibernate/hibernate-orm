73e0c3dad23b3e3d5998fb95f9e3009fc5f83986
Gail Badner <gbadner@redhat.com>
2009-04-10 22:37:51 +0000

    JBPAPP-1124 ANN-779 : Oracle/DB2 - Identifier is too long
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/trunk@16298 1b8cb986-b30d-0410-93ca-fae66ebed9b2

---
+++ annotations/src/test/java/org/hibernate/test/annotations/generics/Classes.java
@@ -0,0 +1,47 @@
+package org.hibernate.test.annotations.generics;
+
+/**
+ * A test case for ANN-494.
+ *
+ * @author Edward Costello
+ * @author Paolo Perrotta
+ */
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Embeddable;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CollectionOfElements;
+
+public class Classes {
+
+	@Embeddable
+	public static class Edition<T> {
+		T name;
+	}
+	
+	@Entity
+	public static class Book {
+		@Id
+		@GeneratedValue(strategy=GenerationType.AUTO)
+		Long id;
+		
+		@Embedded
+		Edition<String> edition;
+	}
+	
+	@Entity
+	public static class PopularBook {
+		@Id
+		@GeneratedValue(strategy=GenerationType.AUTO)
+		Long id;
+		
+		@CollectionOfElements
+		Set<Edition<String>> editions = new HashSet<Edition<String>>();
+	}
+}

21aaac2ea4372053f3ad5fa7386979000a4a0636
Emmanuel Bernard <emmanuel@hibernate.org>
2009-04-29 14:45:14 +0000

    ANN-827 add initial support for Bean Validation
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/trunk@16475 1b8cb986-b30d-0410-93ca-fae66ebed9b2

---
+++ annotations/src/test/java/org/hibernate/test/annotations/beanvalidation/BeanValidationProvidedFactoryTest.java
@@ -0,0 +1,61 @@
+package org.hibernate.test.annotations.beanvalidation;
+
+import java.math.BigDecimal;
+import java.util.Locale;
+import javax.validation.ConstraintViolationException;
+import javax.validation.MessageInterpolator;
+import javax.validation.Validation;
+import javax.validation.ValidatorFactory;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.test.annotations.TestCase;
+
+/**
+ * @author Emmanuel Bernard
+ */
+public class BeanValidationProvidedFactoryTest extends TestCase {
+	public void testListeners() {
+		CupHolder ch = new CupHolder();
+		ch.setRadius( new BigDecimal( "12" ) );
+		Session s = openSession(  );
+		Transaction tx = s.beginTransaction();
+		try {
+			s.persist( ch );
+			s.flush();
+			fail("invalid object should not be persisted");
+		}
+		catch ( ConstraintViolationException e ) {
+			assertEquals( 1, e.getConstraintViolations().size() );
+			assertEquals( "Oops", e.getConstraintViolations().iterator().next().getMessage() );
+		}
+		tx.rollback();
+		s.close();
+	}
+
+	protected Class<?>[] getMappings() {
+		return new Class<?>[] {
+				CupHolder.class
+		};
+	}
+
+	@Override
+	protected void configure(Configuration cfg) {
+		super.configure( cfg );
+		final MessageInterpolator messageInterpolator = new MessageInterpolator() {
+
+			public String interpolate(String s, Context context) {
+				return "Oops";
+			}
+
+			public String interpolate(String s, Context context, Locale locale) {
+				return interpolate( s, context );
+			}
+		};
+		final javax.validation.Configuration<?> configuration = Validation.byDefaultProvider().configure();
+		configuration.messageInterpolator( messageInterpolator );
+		ValidatorFactory vf = configuration.buildValidatorFactory();
+		cfg.getProperties().put( "javax.persistence.validation.factory", vf);
+	}
+}

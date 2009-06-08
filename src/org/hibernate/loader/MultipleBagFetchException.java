5085ee0ccb8e9b2231fcf6f84866c5e2f591150f
Steve Ebersole <steve@hibernate.org>
2009-06-08 16:05:08 +0000

    HHH-2980 - Error "org.hibernate.HibernateException: cannot simultaneously fetch multiple bags" not specific enough
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/branches/Branch_3_2@16718 1b8cb986-b30d-0410-93ca-fae66ebed9b2

---
+++ src/org/hibernate/loader/MultipleBagFetchException.java
@@ -0,0 +1,52 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2009, Red Hat Middleware LLC or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Middleware LLC.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.loader;
+
+import java.util.Set;
+
+import org.hibernate.HibernateException;
+
+/**
+ * Exception used to indicate that a query is attempting to simultaneously fetch multiple
+ * {@link org.hibernate.type.BagType bags}
+*
+* @author Steve Ebersole
+*/
+public class MultipleBagFetchException extends HibernateException {
+	private final Set bagRoles;
+
+	public MultipleBagFetchException(Set bagRoles) {
+		super( "cannot simultaneously fetch multiple bags" );
+		this.bagRoles = bagRoles;
+	}
+
+	/**
+	 * Retrieves the set of collection roles for the bags encountered.
+	 *
+	 * @return The bag collection roles.
+	 */
+	public Set getBagRoles() {
+		return bagRoles;
+	}
+}

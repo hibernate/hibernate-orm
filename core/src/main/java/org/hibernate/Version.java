0c15a4d6209f486bfa4efc178ed8b0abf701a302
Steve Ebersole <steve@hibernate.org>
2009-06-22 21:58:23 +0000

    HHH-3781 - Echo version information to the console in main()
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/trunk@16871 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- core/src/main/java/org/hibernate/Version.java
+++ core/src/main/java/org/hibernate/Version.java
@@ -32,4 +32,8 @@ public class Version {
 	public static String getVersionString() {
 		return "3.5.0-SNAPSHOT";
 	}
+
+	public static void main(String[] args) {
+		System.out.println( "Hibernate version " + getVersionString() );
+	}
 }

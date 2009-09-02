4bd7cb84d7a0fecae60cda091f4dfcf7e41c5078
Strong Liu <stliu@redhat.com>
2009-09-02 02:30:42 +0000

    JBPAPP-2716 HHH-4115 : Core - FooBarTest - 'operator does not exist: character varying = integer'
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/trunk@17463 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- testsuite/src/test/java/org/hibernate/test/legacy/FooBarTest.java
+++ testsuite/src/test/java/org/hibernate/test/legacy/FooBarTest.java
@@ -2222,7 +2222,7 @@ public class FooBarTest extends LegacyTestCase {
 			s.createQuery( "select count(*) from Baz as baz where 1 in indices(baz.fooArray)" ).list();
 			s.createQuery( "select count(*) from Bar as bar where 'abc' in elements(bar.baz.fooArray)" ).list();
 			s.createQuery( "select count(*) from Bar as bar where 1 in indices(bar.baz.fooArray)" ).list();
-			if ( !(getDialect() instanceof DB2Dialect) &&  !(getDialect() instanceof Oracle8iDialect ) && !( getDialect() instanceof SybaseDialect ) && !( getDialect() instanceof Sybase11Dialect ) && !( getDialect() instanceof SybaseASE15Dialect )) {
+			if ( !(getDialect() instanceof DB2Dialect) &&  !(getDialect() instanceof Oracle8iDialect ) && !( getDialect() instanceof SybaseDialect ) && !( getDialect() instanceof Sybase11Dialect ) && !( getDialect() instanceof SybaseASE15Dialect ) && !( getDialect() instanceof PostgreSQLDialect )) {
 				// SybaseAnywhereDialect supports implicit conversions from strings to ints
 				s.createQuery(
 						"select count(*) from Bar as bar, bar.component.glarch.proxyArray as g where g.id in indices(bar.baz.fooArray)"

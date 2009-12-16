384d5859c2083bf075051b44f455e6da09b2dc26
Steve Ebersole <steve@hibernate.org>
2009-12-16 22:12:45 +0000

    git-svn-id: https://svn.jboss.org/repos/hibernate/core/trunk@18250 1b8cb986-b30d-0410-93ca-fae66ebed9b2

---
+++ entitymanager/src/test/java/org/hibernate/ejb/metamodel/EmbeddedTypeTest.java
@@ -0,0 +1,58 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2009, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.ejb.metamodel;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.SingularAttribute;
+
+import org.hibernate.ejb.test.TestCase;
+
+/**
+ * TODO : javadoc
+ *
+ * @author Steve Ebersole
+ */
+public class EmbeddedTypeTest extends TestCase {
+	@Override
+	public Class[] getAnnotatedClasses() {
+		return new Class[] {
+				Product.class, ShelfLife.class
+		};
+	}
+
+	public void testSingularAttributeAccessByNameFailureExpected() {
+		// HHH-4702
+		EntityManager em = getOrCreateEntityManager();
+		em.getTransaction().begin();
+
+		SingularAttribute soldDate_ = em.getMetamodel().embeddable( ShelfLife.class )
+				.getSingularAttribute( "soldDate" );
+		assertEquals( java.sql.Date.class, soldDate_.getBindableJavaType());
+		assertEquals( java.sql.Date.class, soldDate_.getType().getJavaType() );
+		assertEquals( java.sql.Date.class, soldDate_.getJavaType() );
+
+		em.getTransaction().commit();
+		em.close();
+	}
+}

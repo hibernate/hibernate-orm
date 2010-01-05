39b098aac34937e7dc35b9ecc5b185ca972e8fca
Scott Marlow <smarlow@redhat.com>
2010-01-05 03:16:05 +0000

    HHH-4753 Default table name for @CollectionTable is not inferred correctly according to spec requirement
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/trunk@18405 1b8cb986-b30d-0410-93ca-fae66ebed9b2

---
+++ annotations/src/test/java/org/hibernate/test/annotations/namingstrategy/AddressEntry.java
@@ -0,0 +1,45 @@
+package org.hibernate.test.annotations.namingstrategy;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class AddressEntry implements java.io.Serializable {
+	protected String street;
+	protected String city;
+	protected String state;
+	protected String zip;
+
+	public AddressEntry() {
+	}
+
+ 	public AddressEntry( String street, String city, String state, String zip) {
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String c) {
+		city = c;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+}
--- annotations/src/test/java/org/hibernate/test/annotations/namingstrategy/NamingStrategyTest.java
+++ annotations/src/test/java/org/hibernate/test/annotations/namingstrategy/NamingStrategyTest.java
@@ -1,12 +1,16 @@
-// $Id:$
+// $Id$
 package org.hibernate.test.annotations.namingstrategy;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Iterator;
 
 import junit.framework.TestCase;
 
 import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.EJB3NamingStrategy;
+import org.hibernate.cfg.Mappings;
+import org.hibernate.mapping.Table;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,7 +38,36 @@ public class NamingStrategyTest extends TestCase {
 			fail(e.getMessage());
 		}
 	}
-	
+
+	public void testWithEJB3NamingStrategy() throws Exception {
+		try {
+			AnnotationConfiguration config = new AnnotationConfiguration();
+			config.setNamingStrategy(EJB3NamingStrategy.INSTANCE);
+			config.addAnnotatedClass(A.class);
+			config.addAnnotatedClass(AddressEntry.class);
+			config.buildSessionFactory();
+			Mappings mappings = config.createMappings();
+			boolean foundIt = false;
+
+			for ( Iterator iter = mappings.iterateTables(); iter.hasNext();  ) {
+				Table table = (Table) iter.next();
+				log.info("testWithEJB3NamingStrategy table = " + table.getName());
+				if ( table.getName().equalsIgnoreCase("A_ADDRESS")) {
+					foundIt = true;
+				}
+				// make sure we use A_ADDRESS instead of AEC_address
+				assertFalse("got table name mapped to: AEC_address which violates JPA-2 spec section 11.1.8 ([OWNING_ENTITY_NAME]_[COLLECTION_ATTRIBUTE_NAME])",table.getName().equalsIgnoreCase("AEC_address"));
+			}
+			assertTrue("table not mapped to A_ADDRESS which violates JPA-2 spec section 11.1.8",foundIt);
+		}
+		catch( Exception e ) {
+			StringWriter writer = new StringWriter();
+			e.printStackTrace(new PrintWriter(writer));
+			log.debug(writer.toString());
+			fail(e.getMessage());
+		}
+	}
+
 	public void testWithoutCustomNamingStrategy() throws Exception {
 		try {
 			AnnotationConfiguration config = new AnnotationConfiguration();

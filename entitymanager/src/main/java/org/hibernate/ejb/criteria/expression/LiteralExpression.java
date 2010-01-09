0f96757d1a2bd6e7da076c72050f9f7376400267
Steve Ebersole <steve@hibernate.org>
2010-01-09 18:01:20 +0000

    HHH-4774 - Do not handle literals using parameters in JPA criteria select
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/trunk@18486 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- entitymanager/src/main/java/org/hibernate/ejb/criteria/expression/LiteralExpression.java
+++ entitymanager/src/main/java/org/hibernate/ejb/criteria/expression/LiteralExpression.java
@@ -93,7 +93,7 @@ public class LiteralExpression<T> extends ExpressionImpl<T> implements Serializa
 		// some drivers/servers do not like parameters in the select clause
 		final ValueHandlerFactory.ValueHandler handler =
 				ValueHandlerFactory.determineAppropriateHandler( literal.getClass() );
-		if ( ! ValueHandlerFactory.isCharacter( literal ) ) {
+		if ( ValueHandlerFactory.isCharacter( literal ) ) {
 			return '\'' + handler.render( literal ) + '\'';
 		}
 		else {

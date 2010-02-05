ccad83c774f68120a06d30c24a9ddb8c10fd18b2
Steve Ebersole <steve@hibernate.org>
2010-02-05 22:02:36 +0000

    HHH-4884 - Fix binding of @TableGenerator#initialValue into org.hibernate.id.enhanced.TableGenerator
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/trunk@18707 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- annotations/src/test/java/org/hibernate/test/annotations/id/generationmappings/NewGeneratorMappingsTest.java
+++ annotations/src/test/java/org/hibernate/test/annotations/id/generationmappings/NewGeneratorMappingsTest.java
@@ -113,8 +113,8 @@ public class NewGeneratorMappingsTest extends TestCase {
 		assertEquals( TableGenerator.DEF_SEGMENT_COLUMN, tabGenerator.getSegmentColumnName() );
 		assertEquals( "MINIMAL_TBL", tabGenerator.getSegmentValue() );
 		assertEquals( TableGenerator.DEF_VALUE_COLUMN, tabGenerator.getValueColumnName() );
-		// 0 is the annotation default
-		assertEquals( 0, tabGenerator.getInitialValue() );
+		// 0 is the annotation default, but its expected to be treated as 1
+		assertEquals( 1, tabGenerator.getInitialValue() );
 		// 50 is the annotation default
 		assertEquals( 50, tabGenerator.getIncrementSize() );
 		assertTrue( OptimizerFactory.PooledOptimizer.class.isInstance( tabGenerator.getOptimizer() ) );

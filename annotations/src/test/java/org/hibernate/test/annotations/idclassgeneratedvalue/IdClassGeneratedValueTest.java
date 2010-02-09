c833f338125fa511a9e31e24356fb1dfc8d3fd66
Steve Ebersole <steve@hibernate.org>
2010-02-09 19:55:48 +0000

    removed commented-out code
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/trunk@18749 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- annotations/src/test/java/org/hibernate/test/annotations/idclassgeneratedvalue/IdClassGeneratedValueTest.java
+++ annotations/src/test/java/org/hibernate/test/annotations/idclassgeneratedvalue/IdClassGeneratedValueTest.java
@@ -109,49 +109,8 @@ public class IdClassGeneratedValueTest extends TestCase {
 		s.close();
 	}
 
-//	public void testComplexIdClass() {
-//		Session s = openSession();
-//		Transaction tx = s.beginTransaction();
-//
-//		Customer c1 = new Customer(
-//				"foo", "bar", "contact1", "100", new BigDecimal( 1000 ), new BigDecimal( 1000 ), new BigDecimal( 1000 )
-//		);
-//
-//		s.persist( c1 );
-//		Item boat = new Item();
-//		boat.setId( "1" );
-//		boat.setName( "cruiser" );
-//		boat.setPrice( new BigDecimal( 500 ) );
-//		boat.setDescription( "a boat" );
-//		boat.setCategory( 42 );
-//
-//		s.persist( boat );
-//		s.flush();
-//		s.clear();
-//
-//		c1.addInventory( boat, 10, new BigDecimal( 5000 ) );
-//		s.merge( c1 );
-//		s.flush();
-//		s.clear();
-//
-//		Customer c2 = (Customer) s.createQuery( "select c from Customer c" ).uniqueResult();
-//
-//		List<CustomerInventory> inventory = c2.getInventories();
-//
-//		assertEquals( 1, inventory.size() );
-//		assertEquals( 10, inventory.get( 0 ).getQuantity() );
-//
-//		tx.rollback();
-//		s.close();
-//
-//		assertTrue( true );
-//	}
-
 	protected Class[] getAnnotatedClasses() {
 		return new Class[] {
-//				Customer.class,
-//				CustomerInventory.class,
-//				Item.class,
 				Simple.class,
 				Simple2.class,
 				Multiple.class

aa82fd710cf65ff6e9aa564f358ca4384ac08fd2
Steve Ebersole <steve@hibernate.org>
2010-02-09 21:50:09 +0000

    HHH-4894 - Process composite-id sub-generators PersistentIdentifierGenerator contract(s)
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/trunk@18755 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- annotations/src/test/java/org/hibernate/test/annotations/derivedidentities/e1/b2/IdClassGeneratedValueManyToOneTest.java
+++ annotations/src/test/java/org/hibernate/test/annotations/derivedidentities/e1/b2/IdClassGeneratedValueManyToOneTest.java
@@ -1,3 +1,26 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2010, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
 package org.hibernate.test.annotations.derivedidentities.e1.b2;
 
 import java.math.BigDecimal;
@@ -5,18 +28,15 @@ import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import org.hibernate.junit.FailureExpected;
 import org.hibernate.test.annotations.TestCase;
 
 /**
  * A test.
  *
  * @author <a href="mailto:stale.pedersen@jboss.org">Stale W. Pedersen</a>
- * @version $Revision: 1.1 $
  */
 public class IdClassGeneratedValueManyToOneTest extends TestCase {
 
-	@FailureExpected(jiraKey="HHH-4890")
 	public void testComplexIdClass() {
 		Session s = openSession();
 		Transaction tx = s.beginTransaction();

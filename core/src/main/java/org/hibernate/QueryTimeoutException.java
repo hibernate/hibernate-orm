32e3b9c2c9dcf9f925e8c3d9f39a424eb119944b
Scott Marlow <smarlow@redhat.com>
2010-02-23 01:06:16 +0000

    HHH-4662 Implement javax.persistence.query.timeout
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/trunk@18853 1b8cb986-b30d-0410-93ca-fae66ebed9b2

---
+++ core/src/main/java/org/hibernate/QueryTimeoutException.java
@@ -0,0 +1,60 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2009, Red Hat Middleware LLC or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Middleware LLC.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ *
+ */
+package org.hibernate;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * Thrown when a database query timeout occurs.
+ *
+ * @author Scott Marlow
+ */
+
+public class QueryTimeoutException extends JDBCException {
+
+	Object entity;
+
+
+	public QueryTimeoutException( String s, JDBCException je, Object entity ) {
+			super(s, je.getSQLException());
+			this.entity = entity;
+		}
+
+   public QueryTimeoutException( String s, SQLException se, Object entity ) {
+         super(s, se);
+         this.entity = entity;
+      }
+
+	public QueryTimeoutException( String s, SQLException se, String sql ) {
+			super(s, se, sql);
+			this.entity = null;
+		}
+
+	public Object getEntity() {
+		return entity;
+	}
+
+}

64799c07cbee162a4e00d1a569f2faa96f940ee3
Strong Liu <stliu@redhat.com>
2010-05-11 01:44:32 +0000

    HHH-5210 Query Cache effective only after closing the session that created the cache
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/branches/Branch_3_5@19463 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- testsuite/src/test/java/org/hibernate/test/querycache/QueryCacheTest.java
+++ testsuite/src/test/java/org/hibernate/test/querycache/QueryCacheTest.java
@@ -1,6 +1,7 @@
 //$Id: QueryCacheTest.java 10977 2006-12-12 23:28:04Z steve.ebersole@jboss.com $
 package org.hibernate.test.querycache;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -40,6 +41,48 @@ public class QueryCacheTest extends FunctionalTestCase {
 
 	public static Test suite() {
 		return new FunctionalTestClassTestSuite( QueryCacheTest.class );
+	}
+	//https://jira.jboss.org/jira/browse/JBPAPP-4224
+	public void testHitCacheInSameSession() {
+		getSessions().evictQueries();
+		getSessions().getStatistics().clear();
+		Session s = openSession();
+		List list = new ArrayList();
+		s.beginTransaction();
+		for ( int i = 0; i < 3; i++ ) {
+			Item a = new Item();
+			a.setName( "a" + i );
+			a.setDescription( "a" + i );
+			list.add( a );
+			s.persist( a );
+		}
+		s.getTransaction().commit();
+		
+//		s.close();
+//		s=openSession();
+		
+		s.beginTransaction();
+		String queryString = "from Item";
+		// this query will hit the database and create the cache
+		s.createQuery( queryString ).setCacheable( true ).list();
+		s.getTransaction().commit();
+		
+		s.beginTransaction();
+		//and this one SHOULD served by the cache
+		s.createQuery( queryString ).setCacheable( true ).list();
+		s.getTransaction().commit();
+		QueryStatistics qs = s.getSessionFactory().getStatistics().getQueryStatistics( queryString );
+		assertEquals( 1, qs.getCacheHitCount() );
+		assertEquals( 1, qs.getCachePutCount() );
+		s.close();
+		s = openSession();
+		s.beginTransaction();
+		for(Object obj:list){
+			s.delete( obj );
+		}
+		s.getTransaction().commit();
+		s.close();
+		
 	}
 	
 	public void testQueryCacheInvalidation() throws Exception {

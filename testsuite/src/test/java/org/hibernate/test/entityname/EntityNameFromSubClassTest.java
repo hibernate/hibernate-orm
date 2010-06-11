ebb7d0d3756c67da3dd25fb0590c790b35ea5bfb
Strong Liu <stliu@redhat.com>
2010-06-11 16:35:32 +0000

    HHH-4036 EntityMetamodel entityNameByInheritenceClassNameMap field used inconsistently
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/branches/Branch_3_5@19715 1b8cb986-b30d-0410-93ca-fae66ebed9b2

---
+++ testsuite/src/test/java/org/hibernate/test/entityname/EntityNameFromSubClassTest.java
@@ -0,0 +1,41 @@
+package org.hibernate.test.entityname;
+
+import org.hibernate.Session;
+import org.hibernate.junit.functional.FunctionalTestCase;
+/**
+ * 
+ * @author stliu
+ *
+ */
+public class EntityNameFromSubClassTest extends FunctionalTestCase {
+
+	public EntityNameFromSubClassTest(String string) {
+		super(string);
+	}
+
+	public void testEntityName() {
+		Session s = openSession();
+		s.beginTransaction();
+		Person stliu = new Person();
+		stliu.setName("stliu");
+		Car golf = new Car();
+		golf.setOwner("stliu");
+		stliu.getCars().add(golf);
+		s.save(stliu);
+		s.getTransaction().commit();
+		s.close();
+		
+		s=openSession();
+		s.beginTransaction();
+		Person p = (Person)s.get(Person.class, stliu.getId());
+		assertEquals(1, p.getCars().size());
+		assertEquals(Car.class, p.getCars().iterator().next().getClass());
+		s.getTransaction().commit();
+		s.close();
+	}
+
+	public String[] getMappings() {
+		return new String[] { "entityname/Vehicle.hbm.xml" };
+	}
+
+}
---
+++ testsuite/src/test/java/org/hibernate/test/entityname/Person.java
@@ -0,0 +1,39 @@
+package org.hibernate.test.entityname;
+
+import java.util.HashSet;
+import java.util.Set;
+/**
+ * 
+ * @author stliu
+ *
+ */
+public class Person {
+	private Long id;
+	private String Name;
+	private Set cars = new HashSet();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public Set getCars() {
+		return cars;
+	}
+
+	public void setCars(Set cars) {
+		this.cars = cars;
+	}
+
+}

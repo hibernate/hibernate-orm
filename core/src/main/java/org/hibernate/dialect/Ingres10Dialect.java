2f6321cb5bc77ff5e07445b24ba3e4c73a52863f
Strong Liu <stliu@redhat.com>
2010-06-29 07:25:17 +0000

    HHH-5213 Add native SQL Boolean type to Ingres10Dialect
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/branches/Branch_3_5@19853 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- core/src/main/java/org/hibernate/dialect/Ingres10Dialect.java
+++ core/src/main/java/org/hibernate/dialect/Ingres10Dialect.java
@@ -1,6 +1,7 @@
 package org.hibernate.dialect;
 
 import java.sql.Types;
+import java.util.Properties;
 
 import org.hibernate.Hibernate;
 import org.hibernate.cfg.Environment;
@@ -8,9 +9,10 @@ import org.hibernate.dialect.function.NoArgSQLFunction;
 
 /**
  * A SQL dialect for Ingres 10 and later versions.
- *
+ * <p/>
  * Changes:
  * <ul>
+ * <li>Add native BOOLEAN type support</li>
  * </ul>
  *
  * @author Raymond Fan
@@ -18,5 +20,39 @@ import org.hibernate.dialect.function.NoArgSQLFunction;
 public class Ingres10Dialect extends Ingres9Dialect {
     public Ingres10Dialect() {
         super();
+        registerBooleanSupport();
+    }
+
+    // Boolean column type support
+
+    /**
+     * The SQL literal value to which this database maps boolean values.
+     *
+     * @param bool The boolean value
+     * @return The appropriate SQL literal.
+     */
+    public String toBooleanValueString(boolean bool) {
+        return bool ? "true" : "false";
+    }
+
+    protected void registerBooleanSupport() {
+        // Column type
+
+        // Boolean type (mapping/BooleanType) mapping maps SQL BIT to Java
+        // Boolean. In order to create a boolean column, BIT needs to be mapped
+        // to boolean as well, similar to H2Dialect.
+        registerColumnType( Types.BIT, "boolean" );
+        registerColumnType( Types.BOOLEAN, "boolean" );
+
+        // Functions
+
+        // true, false and unknown are now valid values
+        // Remove the query substitutions previously added in IngresDialect.
+        Properties properties = getDefaultProperties();
+        String querySubst = properties.getProperty(Environment.QUERY_SUBSTITUTIONS);
+        if (querySubst != null) {
+            String newQuerySubst = querySubst.replace("true=1,false=0","");
+            properties.setProperty(Environment.QUERY_SUBSTITUTIONS, newQuerySubst);
+        }
     }
 }

bd3e4f175482127b66ba0ef51ee6cd233f4ac49d
Sharath Reddy <sharathreddy@yahoo.com>
2010-07-01 11:09:45 +0000

    HHH-5109 @OneToOne - too many joins
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/branches/Branch_3_5@19881 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- annotations/src/test/java/org/hibernate/test/annotations/onetoone/Party.java
+++ annotations/src/test/java/org/hibernate/test/annotations/onetoone/Party.java
@@ -1,6 +1,7 @@
 //$Id$
 package org.hibernate.test.annotations.onetoone;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
@@ -14,7 +15,7 @@ public class Party {
 	@Id
 	String partyId;
 
-	@OneToOne
+	@OneToOne(cascade=CascadeType.ALL)
 	@PrimaryKeyJoinColumn
 	PartyAffiliate partyAffiliate;
 }

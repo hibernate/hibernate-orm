41ca853f367ffce9c7f738fe6c4f709c0ef93073
Sharath Reddy <sharathreddy@yahoo.com>
2010-07-04 19:47:28 +0000

    HHH-4250 : @ManyToOne - @OneToMany doesn't work with @Inheritance(strategy= InheritanceType.JOINED)
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/trunk@19895 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- annotations/src/test/java/org/hibernate/test/annotations/inheritance/joined/JoinedSubclassTest.java
+++ annotations/src/test/java/org/hibernate/test/annotations/inheritance/joined/JoinedSubclassTest.java
@@ -1,7 +1,9 @@
 //$Id$
 package org.hibernate.test.annotations.inheritance.joined;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -122,6 +124,48 @@ public class JoinedSubclassTest extends TestCase {
 		transaction.commit();
 		session.close();
 	}
+	
+	//HHH-4250 : @ManyToOne - @OneToMany doesn't work with @Inheritance(strategy= InheritanceType.JOINED)
+	public void testManyToOneWithJoinTable() {
+
+		Session s = openSession();
+		Transaction tx = s.beginTransaction();
+				
+		Client c1 = new Client();
+		c1.setFirstname("Firstname1");
+		c1.setName("Name1");
+		c1.setCode("1234");
+		c1.setStreet("Street1");
+		c1.setCity("City1");
+		
+		Account a1 = new Account();
+		a1.setNumber("1000");
+		a1.setBalance(5000.0);
+		
+		a1.addClient(c1);
+		
+		s.persist(c1);
+		s.persist(a1);
+	    
+		s.flush();
+		s.clear();
+		
+		c1 = (Client) s.load(Client.class, c1.getId());
+		assertEquals(5000.0, c1.getAccount().getBalance());
+		
+		s.flush();
+		s.clear();
+		
+		a1 = (Account) s.load(Account.class,a1.getId());
+		Set<Client> clients = a1.getClients();
+		assertEquals(1, clients.size());
+		Iterator<Client> it = clients.iterator();
+		c1 = it.next();
+		assertEquals("Name1", c1.getName());
+				
+		tx.rollback();
+		s.close();
+	}
 
 //	public void testManyToOneAndJoin() throws Exception {
 //		Session session = openSession();
@@ -168,6 +212,8 @@ public class JoinedSubclassTest extends TestCase {
 				Sweater.class,
 				EventInformation.class,
 				Alarm.class,
+				Client.class,
+				Account.class
 				//Asset.class,
 				//Parent.class,
 				//PropertyAsset.class,

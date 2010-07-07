7697640089a7ae805ce7f4cc6c9ae258d7c70be0
Sharath Reddy <sharathreddy@yahoo.com>
2010-07-07 14:05:01 +0000

    HHH-4240 - SecondaryTables not recognized when using JOINED inheritance
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/trunk@19908 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- annotations/src/test/java/org/hibernate/test/annotations/inheritance/joined/JoinedSubclassTest.java
+++ annotations/src/test/java/org/hibernate/test/annotations/inheritance/joined/JoinedSubclassTest.java
@@ -167,6 +167,38 @@ public class JoinedSubclassTest extends TestCase {
 		s.close();
 	}
 
+	/**
+     *   HHH-4240 - SecondaryTables not recognized when using JOINED inheritance
+	 */	
+	public void testSecondaryTables() {
+		
+		Session s = openSession();
+		s.getTransaction().begin();
+		
+		Company company = new Company();
+		company.setCustomerName("Mama");
+		company.setCustomerCode("123");
+		company.setCompanyName("Mama Mia Pizza");
+		company.setCompanyAddress("Rome");
+		
+		s.persist( company );
+		s.getTransaction().commit();
+		s.clear();
+		
+		s = openSession();
+		s.getTransaction().begin();
+		company = (Company) s.get( Company.class, company.getId());
+		assertEquals("Mama", company.getCustomerName());
+		assertEquals("123", company.getCustomerCode());
+		assertEquals("Mama Mia Pizza", company.getCompanyName());
+		assertEquals("Rome", company.getCompanyAddress());
+				
+		s.delete( company );
+		s.getTransaction().commit();
+		s.close();
+	}
+	
+	
 //	public void testManyToOneAndJoin() throws Exception {
 //		Session session = openSession();
 //		Transaction transaction = session.beginTransaction();

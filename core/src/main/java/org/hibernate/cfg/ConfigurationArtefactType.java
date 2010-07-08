df184979ed2f19887bbea46603148dd59d3bfd1f
Steve Ebersole <steve@hibernate.org>
2010-07-08 23:56:18 +0000

    HHH-5367 - Move annotations module sources into core module
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/trunk@19922 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- core/src/main/java/org/hibernate/cfg/ConfigurationArtefactType.java
+++ core/src/main/java/org/hibernate/cfg/ConfigurationArtefactType.java
@@ -1,11 +1,10 @@
-// $Id$
 /*
  * Hibernate, Relational Persistence for Idiomatic Java
  *
- * Copyright (c) 2010, Red Hat Middleware LLC or third-party contributors as
+ * Copyright (c) 2010, Red Hat Inc. or third-party contributors as
  * indicated by the @author tags or express copyright attribution
  * statements applied by the authors.  All third-party contributions are
- * distributed under license by Red Hat Middleware LLC.
+ * distributed under license by Red Hat Inc.
  *
  * This copyrighted material is made available to anyone wishing to use, modify,
  * copy, or redistribute it subject to the terms and conditions of the GNU
@@ -22,6 +21,9 @@
  * 51 Franklin Street, Fifth Floor
  * Boston, MA  02110-1301  USA
  */
+
+// $Id$
+
 package org.hibernate.cfg;
 
 import org.hibernate.HibernateException;

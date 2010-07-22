17049da4f6c7c591bc1342a0232f5e2a21f2c371
Gail Badner <gbadner@redhat.com>
2010-07-22 19:03:09 +0000

    HHH-2350 : second-level cache broken for non-inverse bidirectional one-to-many relation
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/branches/Branch_3_5@20012 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- testsuite/src/test/java/org/hibernate/test/onetomany/AbstractRecursiveBidirectionalOneToManyTest.java
+++ testsuite/src/test/java/org/hibernate/test/onetomany/AbstractRecursiveBidirectionalOneToManyTest.java
@@ -29,6 +29,7 @@ import java.util.ArrayList;
 import junit.framework.Test;
 
 import org.hibernate.CacheMode;
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.junit.functional.FunctionalTestCase;
@@ -68,36 +69,43 @@ public abstract class AbstractRecursiveBidirectionalOneToManyTest extends Functi
 		return new String[] { "onetomany/Node.hbm.xml" };
 	}	
 
-    public void testOneToManyMoveElement() {
-        init();
-        transformMove();
-        check();
+	public void testOneToManyMoveElement() {
+		init();
+		transformMove();
+		check( false );
 		delete();
-    }
+	}
+
+	public void testOneToManyMoveElementWithDirtySimpleProperty() {
+		init();
+		transformMoveWithDirtySimpleProperty();
+		check( true );
+		delete();
+	}
 
 	public void testOneToManyReplaceList() {
 		init();
 		transformReplace();
-		check();
+		check( false );
 		delete();
 	}
 
-    void init() {
+	void init() {
 
 		Session s = openSession();
 		Transaction tx = s.beginTransaction();
 
-	    Node node1 = new Node( 1 );
-	    Node node2 = new Node( 2 );
-		Node node3 = new Node( 3 );
+		Node node1 = new Node( 1, "node1" );
+		Node node2 = new Node( 2, "node2" );
+		Node node3 = new Node( 3, "node3" );
 
-	    node1.addSubNode( node2 );
-	    node2.addSubNode( node3 );
+		node1.addSubNode( node2 );
+		node2.addSubNode( node3 );
 
-	    s.save(node1);
+		s.save(node1);
 
 		tx.commit();
-        s.close();
+		s.close();
 	}
 
 	void transformMove() {
@@ -111,6 +119,23 @@ public abstract class AbstractRecursiveBidirectionalOneToManyTest extends Functi
 
 		node2.removeSubNode( node3 );
 		node1.addSubNode( node3 );
+
+		tx.commit();
+		s.close();
+	}
+
+	void transformMoveWithDirtySimpleProperty() {
+
+		Session s = openSession();
+		Transaction tx = s.beginTransaction();
+
+		Node node3 = (Node) s.load(Node.class, new Integer(3));
+		Node node2 = node3.getParentNode();
+		Node node1 = node2.getParentNode();
+
+		node2.removeSubNode( node3 );
+		node1.addSubNode( node3 );
+		node3.setDescription( "node3-updated" );
 
 		tx.commit();
 		s.close();
@@ -134,21 +159,32 @@ public abstract class AbstractRecursiveBidirectionalOneToManyTest extends Functi
 		s.close();
 	}
 
-	void check() {
-
+	void check(boolean simplePropertyUpdated) {
 		Session s = openSession();
 		Transaction tx = s.beginTransaction();
-		Node node3 = (Node) s.get(Node.class, new Integer(3));
+		Node node3 = (Node) s.get( Node.class, new Integer(3) );
 
 		// fails with 2nd level cache enabled
-		assertEquals(1, node3.getParentNode().getId().intValue());
+		assertEquals( 1, node3.getParentNode().getId().intValue() );
+		assertEquals( ( simplePropertyUpdated ? "node3-updated" : "node3" ), node3.getDescription() );
+		assertTrue( node3.getSubNodes().isEmpty() );
+
+		Node node1 = node3.getParentNode();
+		assertNull( node1.getParentNode() );
+		assertEquals( 2, node1.getSubNodes().size() );
+		assertEquals( 2, ( ( Node ) node1.getSubNodes().get( 0 ) ).getId().intValue() );
+		assertEquals( "node1", node1.getDescription() );
+
+		Node node2 = ( Node ) node1.getSubNodes().get( 0 );
+		assertSame( node1, node2.getParentNode() );
+		assertTrue( node2.getSubNodes().isEmpty() );
+		assertEquals( "node2", node2.getDescription() );
 
 		tx.commit();
 		s.close();
 	}
 
 	void delete() {
-
 		Session s = openSession();
 		Transaction tx = s.beginTransaction();
 		Node node1 = ( Node ) s.get(  Node.class, new Integer( 1 ) );

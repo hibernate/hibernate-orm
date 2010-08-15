c4e630364a7594832ae2486307b7f021c19ed433
misty <misty@redhat.com>
2010-08-15 23:30:30 +0000

    Line edits regarding HHH-5441
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/trunk@20148 1b8cb986-b30d-0410-93ca-fae66ebed9b2

---
+++ documentation/quickstart/src/main/docbook/en-US/content/extras/example-entity.java
@@ -0,0 +1,36 @@
+package org.hibernate.tutorial.native;
+
+import java.util.Date;
+
+public class Event {
+    private Long id;
+
+    private String title;
+    private Date date;
+
+    public Event() {}
+
+    public Long getId() {
+	return id;
+    }
+
+    private void setId(Long id) {
+	this.id = id;
+    }
+
+    public Date getDate() {
+	return date;
+    }
+
+    public void setDate(Date date) {
+	this.date = date;
+    }
+
+    public String getTitle() {
+	return title;
+    }
+
+    public void setTitle(String title) {
+	this.title = title;
+    }
+}

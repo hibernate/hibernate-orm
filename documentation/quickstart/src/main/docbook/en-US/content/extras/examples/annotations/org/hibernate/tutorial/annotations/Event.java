c2e3712ddc1586ce6ea721d02a17f1e69d99ac8e
Steve Ebersole <steve@hibernate.org>
2010-08-16 21:10:23 +0000

    HHH-5441 - Create "Getting Started Guide"
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/trunk@20152 1b8cb986-b30d-0410-93ca-fae66ebed9b2

---
+++ documentation/quickstart/src/main/docbook/en-US/content/extras/examples/annotations/org/hibernate/tutorial/annotations/Event.java
@@ -0,0 +1,47 @@
+package org.hibernate.tutorial.annotations;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table( name = "EVENTS" )
+public class Event {
+    private Long id;
+
+    private String title;
+    private Date date;
+
+    public Event() {}
+
+	@Id
+    public Long getId() {
+		return id;
+    }
+
+    private void setId(Long id) {
+		this.id = id;
+    }
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "EVENT_DATE")
+    public Date getDate() {
+		return date;
+    }
+
+    public void setDate(Date date) {
+		this.date = date;
+    }
+
+    public String getTitle() {
+		return title;
+    }
+
+    public void setTitle(String title) {
+		this.title = title;
+    }
+}

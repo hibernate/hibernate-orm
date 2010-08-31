efe2ad807ac2a215dc1ae734b76a9e454b2b0f47
Galder Zamarreno <galder.zamarreno@jboss.com>
2010-08-31 16:05:27 +0000

    [HHH-5519] (VersionedItem should not extend Item, otherwise query cache results are confusing) Fixed.
    git-svn-id: https://svn.jboss.org/repos/hibernate/core/trunk@20291 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- cache-infinispan/src/test/java/org/hibernate/test/cache/infinispan/functional/VersionedItem.java
+++ cache-infinispan/src/test/java/org/hibernate/test/cache/infinispan/functional/VersionedItem.java
@@ -1,9 +1,8 @@
-/*
- * Hibernate, Relational Persistence for Idiomatic Java
+/* * Hibernate, Relational Persistence for Idiomatic Java
  *
  * Copyright (c) 2007, Red Hat, Inc. and/or it's affiliates or third-party contributors as
  * indicated by the @author tags or express copyright attribution
- * statements applied by the authors.  All third-party contributions are
+ * statements applied by the authors.  All third-party contributions are
  * distributed under license by Red Hat, Inc. and/or it's affiliates.
  *
  * This copyrighted material is made available to anyone wishing to use, modify,
@@ -26,14 +25,42 @@ package org.hibernate.test.cache.infinispan.functional;
 /**
  * @author Steve Ebersole
  */
-public class VersionedItem extends Item {
-    private Long version;
+public class VersionedItem {
+   private Long id;
+   private Long version;
+   private String name;
+   private String description;
 
-    public Long getVersion() {
-        return version;
-    }
+   public Long getId() {
+      return id;
+   }
 
-    public void setVersion(Long version) {
-        this.version = version;
-    }
+   public void setId(Long id) {
+      this.id = id;
+   }
+
+   public Long getVersion() {
+      return version;
+   }
+
+   public void setVersion(Long version) {
+      this.version = version;
+   }
+
+   public String getDescription() {
+      return description;
+   }
+
+   public void setDescription(String description) {
+      this.description = description;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
 }

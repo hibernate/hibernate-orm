2355fc7d3e2a7a8a31010783a3007a8e51507a0b
Hardy Ferentschik <hibernate@ferentschik.de>
2010-10-12 13:43:11 +0200

    HHH-5646 updated script to use git

--- tagRelease.sh
+++ tagRelease.sh
@@ -1,5 +1,15 @@
 #! /bin/sh
 
+#************************************************#
+#                   tagRelease.sh                #
+#                     written by                 #
+#                  Steve Ebersole                #
+#                Hardy Ferentschik               #
+#                                                #
+#  Replacement for the maven-release-pluign      #
+#************************************************#
+
+
 usage="Usage: tagRelease [-u] [-e exportDirectory] [-r releaseVersion] [-d devVersion]"
 projectDir=`pwd`
 
@@ -14,20 +24,16 @@ exportDirectory=''
 #
 # $1: project directory (base directory of recursve find)
 # $2: new value for the pom version
-# $3: new scm url
+################################################################################
 updatePomVersionsAndCommit() {
     for i in `find $1 -name "pom.xml"`; do 
         xmlstarlet ed -P -N x="http://maven.apache.org/POM/4.0.0" \
         		-u "/x:project/x:parent/x:version" -v $2 \
         		-u "/x:project/x:version" -v $2 \
-        		-u "/x:project/x:scm/x:connection" -v scm:svn:$3 \
-        		-u "/x:project/x:scm/x:developerConnection" -v scm:svn:$3 \
-        		-u "/x:project/x:scm/x:url" -v $3 \
         		$i > tmp
         mv tmp $i
     done
-
-    svn commit $projectDirectory -m "Updating pom versions to $1 for release tagging"
+    git commit -m "Updating pom versions to $1 for release tagging"
 }
 
 ################################################################################
@@ -67,9 +73,6 @@ if [ -z "$projectName" ]; then
     echo "Could not determine propject name (misasing/incomplete pom?)."
     exit;
 fi
-projectUrl=`svn info $projectDir | grep URL | awk '{print $2}'`
-tagUrl=`echo $projectUrl | sed 's|\(.*\)/.*$|\1/tags/|'`$projectName-$releaseVersion
-
 
 ################################################################################
 # Confirm data
@@ -77,8 +80,6 @@ tagUrl=`echo $projectUrl | sed 's|\(.*\)/.*$|\1/tags/|'`$projectName-$releaseVer
 echo "About to tag release with following information:"
 echo "  tag version : $releaseVersion"
 echo "  dev version : $devVersion"
-echo "  dev url : $projectUrl" 
-echo "  tag url : $tagUrl"
 while true; do
     read -p "Continue? " yn
     case $yn in
@@ -92,16 +93,16 @@ done
 # Go, go , go
 
 if [ -n "$performUpdate" ]; then
-    echo "Performing requested svn update..."
-    svn update $projectDir
+    echo "Performing requested git pull..."
+    git pull
 fi
 
-updatePomVersionsAndCommit $projectDir $releaseVersion $tagUrl
+updatePomVersionsAndCommit $projectDir $releaseVersion 
 
-svn copy $projectUrl/ $tagUrl -m "Tagging $releaseVersion release"
+git tag -m "Tagging $releaseVersion release" $releaseVersion
 
-updatePomVersionsAndCommit $projectDir $devVersion $projectUrl
+updatePomVersionsAndCommit $projectDir $devVersion
 
 if [ $exportDirectory ]; then
-	svn export $tagUrl $exportDirectory
+	git archive master | tar -x -C $exportDirectory
 fi

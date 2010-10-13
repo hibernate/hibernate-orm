d4bf11d8ba495ce308e7914fc1c2620937b68612
Gail Badner <gbadner@redhat.com>
2010-10-13 13:16:25 -0700

    HHH-5641 : Import JtaPlatform services

---
+++ hibernate-core/src/main/java/org/hibernate/service/jta/platform/internal/JBossAppServerPlatform.java
@@ -0,0 +1,94 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2010, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.service.jta.platform.internal;
+
+import java.util.Map;
+import javax.transaction.Synchronization;
+import javax.transaction.TransactionManager;
+import javax.transaction.TransactionSynchronizationRegistry;
+import javax.transaction.UserTransaction;
+
+import org.hibernate.internal.util.jta.JtaStatusHelper;
+import org.hibernate.service.jta.platform.spi.JtaPlatform;
+import org.hibernate.service.spi.Configurable;
+import org.hibernate.service.spi.InjectService;
+import org.hibernate.internal.util.config.ConfigurationHelper;
+import org.hibernate.service.jndi.spi.JndiService;
+
+
+/**
+ * TODO : javadoc
+ *
+ * @author Steve Ebersole
+ */
+public class JBossAppServerPlatform implements JtaPlatform, Configurable {
+	public static final String TM_NAME = "java:/TransactionManager";
+	public static final String UT_NAME = "UserTransaction";
+	public static final String REG_NAME = "java:comp/TransactionSynchronizationRegistry";
+
+	private JndiService jndiService;
+
+	@InjectService
+	public void setJndiService(JndiService jndiService) {
+		this.jndiService = jndiService;
+	}
+
+	private boolean cacheTransactionManager;
+
+	public void configure(Map configValues) {
+		cacheTransactionManager = ConfigurationHelper.getBoolean( CACHE_TM, configValues, true );
+	}
+
+	private TransactionManager transactionManager;
+
+	public TransactionManager resolveTransactionManager() {
+		if ( cacheTransactionManager ) {
+			if ( transactionManager == null ) {
+				transactionManager = (TransactionManager) jndiService.locate( TM_NAME );
+			}
+			return transactionManager;
+		}
+		else {
+			return (TransactionManager) jndiService.locate( TM_NAME );
+		}
+	}
+
+	public UserTransaction resolveUserTransaction() {
+		return (UserTransaction) jndiService.locate( UT_NAME );
+	}
+
+	public void registerSynchronization(Synchronization synchronization) {
+		getTransactionSynchronizationRegistry().registerInterposedSynchronization( synchronization );
+	}
+
+	private TransactionSynchronizationRegistry getTransactionSynchronizationRegistry() {
+		return (TransactionSynchronizationRegistry) jndiService.locate( REG_NAME );
+	}
+
+	public boolean canRegisterSynchronization() {
+		TransactionSynchronizationRegistry registry = getTransactionSynchronizationRegistry();
+		int status = registry.getTransactionStatus();
+		return JtaStatusHelper.isActive( status ) && ! registry.getRollbackOnly();
+	}
+}
---
+++ hibernate-core/src/main/java/org/hibernate/service/jta/platform/spi/JtaPlatform.java
@@ -0,0 +1,74 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2010, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.service.jta.platform.spi;
+
+import javax.transaction.Synchronization;
+import javax.transaction.TransactionManager;
+import javax.transaction.UserTransaction;
+
+import org.hibernate.service.spi.Service;
+
+/**
+ * Defines how we interact with various JTA services on the given platform/environment.
+ *
+ * @author Steve Ebersole
+ */
+public interface JtaPlatform extends Service {
+	/**
+	 * A configuration value key used to indicate that it is safe to cache
+	 * {@link TransactionManager} references.
+	 *
+	 * @todo add to Environment.
+	 * @todo same for UserTransaction too?  On many platforms UserTransaction and TransactionManager are the same
+	 */
+	public static final String CACHE_TM = "hibernate.jta.cacheTransactionManager";
+
+	/**
+	 * Locate the {@link TransactionManager}
+	 *
+	 * @return The {@link TransactionManager}
+	 */
+	public TransactionManager resolveTransactionManager();
+
+	/**
+	 * Locate the {@link UserTransaction}
+	 *
+	 * @return The {@link UserTransaction}
+	 */
+	public UserTransaction resolveUserTransaction();
+
+	/**
+	 * Register a JTA {@link Synchronization} in the means defined by the platform.
+	 *
+	 * @param synchronization The synchronization to register
+	 */
+	public void registerSynchronization(Synchronization synchronization);
+
+	/**
+	 * Can we currently regsiter a {@link Synchronization}?
+	 *
+	 * @return True if regsitering a {@link Synchronization} is currently allowed; false otherwise.
+	 */
+	public boolean canRegisterSynchronization();
+}

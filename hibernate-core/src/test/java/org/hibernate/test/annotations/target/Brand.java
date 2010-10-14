aec8d1a742410314b0e65ee63aaff8f8b2be9c9d
Steve Ebersole <steve@hibernate.org>
2010-10-14 09:33:20 -0500

    HHH-5658 - Better .gitignore

---
+++ hibernate-core/src/test/java/org/hibernate/test/annotations/target/Brand.java
@@ -0,0 +1,79 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2010 by Red Hat Inc and/or its affiliates or by
+ * third-party contributors as indicated by either @author tags or express
+ * copyright attribution statements applied by the authors.  All
+ * third-party contributions are distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.test.annotations.target;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.MapKeyClass;
+
+import org.hibernate.annotations.MapKeyManyToMany;
+
+/**
+ * @author Emmanuel Bernard
+ */
+@Entity
+public class Brand {
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@ManyToMany(targetEntity = LuggageImpl.class)
+	@MapKeyClass(SizeImpl.class)
+	private Map<Size, Luggage> luggagesBySize = new HashMap<Size, Luggage>();
+
+	@ElementCollection(targetClass = SizeImpl.class)
+	@MapKeyClass(LuggageImpl.class)
+	@MapKeyManyToMany //legacy column name: was never officially supported BTW
+	private Map<Luggage, Size> sizePerLuggage = new HashMap<Luggage, Size>();
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Map<Size, Luggage> getLuggagesBySize() {
+		return luggagesBySize;
+	}
+
+	public void setLuggagesBySize(Map<Size, Luggage> luggagesBySize) {
+		this.luggagesBySize = luggagesBySize;
+	}
+
+	public Map<Luggage, Size> getSizePerLuggage() {
+		return sizePerLuggage;
+	}
+
+	public void setSizePerLuggage(Map<Luggage, Size> sizePerLuggage) {
+		this.sizePerLuggage = sizePerLuggage;
+	}
+}
---
+++ hibernate-core/src/test/java/org/hibernate/test/annotations/target/TargetTest.java
@@ -0,0 +1,104 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2010 by Red Hat Inc and/or its affiliates or by
+ * third-party contributors as indicated by either @author tags or express
+ * copyright attribution statements applied by the authors.  All
+ * third-party contributions are distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.test.annotations.target;
+
+import org.hibernate.test.annotations.TestCase;
+import org.hibernate.Session;
+
+/**
+ * @author Emmanuel Bernard
+ */
+public class TargetTest extends TestCase {
+
+	public void testTargetOnEmbedded() throws Exception {
+		Session s = openSession();
+		s.getTransaction().begin();
+		Luggage l = new LuggageImpl();
+		l.setHeight( 12 );
+		l.setWidth( 12 );
+		Owner o = new OwnerImpl();
+		o.setName( "Emmanuel" );
+		l.setOwner( o );
+		s.persist( l );
+		s.flush();
+		s.clear();
+		l = (Luggage) s.get(LuggageImpl.class, ( (LuggageImpl) l).getId() );
+		assertEquals( "Emmanuel", l.getOwner().getName() );
+		s.getTransaction().rollback();
+		s.close();
+	}
+
+	public void testTargetOnMapKey() throws Exception {
+		Session s = openSession();
+		s.getTransaction().begin();
+		Luggage l = new LuggageImpl();
+		l.setHeight( 12 );
+		l.setWidth( 12 );
+		Size size = new SizeImpl();
+		size.setName( "S" );
+		Owner o = new OwnerImpl();
+		o.setName( "Emmanuel" );
+		l.setOwner( o );
+		s.persist( l );
+		Brand b = new Brand();
+		s.persist( b );
+		b.getLuggagesBySize().put( size, l );
+		s.flush();
+		s.clear();
+		b = (Brand) s.get(Brand.class, b.getId() );
+		assertEquals( "S", b.getLuggagesBySize().keySet().iterator().next().getName() );
+		s.getTransaction().rollback();
+		s.close();
+	}
+
+	public void testTargetOnMapKeyManyToMany() throws Exception {
+		Session s = openSession();
+		s.getTransaction().begin();
+		Luggage l = new LuggageImpl();
+		l.setHeight( 12 );
+		l.setWidth( 12 );
+		Size size = new SizeImpl();
+		size.setName( "S" );
+		Owner o = new OwnerImpl();
+		o.setName( "Emmanuel" );
+		l.setOwner( o );
+		s.persist( l );
+		Brand b = new Brand();
+		s.persist( b );
+		b.getSizePerLuggage().put( l, size );
+		s.flush();
+		s.clear();
+		b = (Brand) s.get(Brand.class, b.getId() );
+		assertEquals( 12d, b.getSizePerLuggage().keySet().iterator().next().getWidth() );
+		s.getTransaction().rollback();
+		s.close();
+	}
+
+	protected Class[] getAnnotatedClasses() {
+		return new Class[] {
+				LuggageImpl.class,
+				Brand.class
+		};
+	}
+}

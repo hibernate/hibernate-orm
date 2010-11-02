ba040a3378bd3a6606219ad529111fc936c8896f
Strong Liu <stliu@hibernate.org>
2010-11-02 20:47:37 +0800

    HHH-5700 incorrect test logic of org.hibernate.test.readonly.ReadOnlyCriteriaQueryTest

--- hibernate-core/src/test/java/org/hibernate/test/readonly/ReadOnlyCriteriaQueryTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/readonly/ReadOnlyCriteriaQueryTest.java
@@ -1143,7 +1143,7 @@ public class ReadOnlyCriteriaQueryTest extends AbstractReadOnlyTest {
 			assertEquals( list.size(), 2 );
 
 			Enrolment e = ( Enrolment ) list.get( 0 );
-			if ( e.getStudent().getStudentNumber() == xam.getStudentNumber() ) {
+			if ( e.getStudent().getStudentNumber() == gavin.getStudentNumber() ) {
 				enrolment1 = e;
 				enrolment2 = ( Enrolment ) list.get( 1 );
 			}
@@ -1181,7 +1181,7 @@ public class ReadOnlyCriteriaQueryTest extends AbstractReadOnlyTest {
 			assertEquals( list.size(), 2 );
 
 			e = ( Enrolment ) list.get( 0 );
-			if ( e.getStudent().getStudentNumber() == xam.getStudentNumber() ) {
+			if ( e.getStudent().getStudentNumber() == gavin.getStudentNumber() ) {
 				enrolment1 = e;
 				enrolment2 = ( Enrolment ) list.get( 1 );
 			}
@@ -1218,7 +1218,7 @@ public class ReadOnlyCriteriaQueryTest extends AbstractReadOnlyTest {
 			assertEquals( list.size(), 2 );
 
 			e = ( Enrolment ) list.get( 0 );
-			if ( e.getStudent().getStudentNumber() == xam.getStudentNumber() ) {
+			if ( e.getStudent().getStudentNumber() == gavin.getStudentNumber() ) {
 				enrolment1 = e;
 				enrolment2 = ( Enrolment ) list.get( 1 );
 			}

2451d3f2860f4be3ed40c9dea422459fe258e2bd
Anton Marsden <anton@paradise.net.nz>
2010-11-10 10:42:00 +1300

    Revert "Implement Serializable"
    This reverts commit d9e146efedd1cdb054d15cb2c849aa1af1db392c.

--- core/src/main/java/org/hibernate/action/ExecutableList.java
+++ core/src/main/java/org/hibernate/action/ExecutableList.java
@@ -10,7 +10,7 @@ import java.util.*;
  * the property spaces for these actions.
  * </p>
  */
-public class ExecutableList<E extends Executable> extends AbstractList<E> implements Cloneable, Serializable {
+public class ExecutableList<E extends Executable> extends AbstractList<E> implements Cloneable {
 
     private Set propertySpaces;
 

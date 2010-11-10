ec34519070dee0280f393ce1fa32bad97e4c5355
Strong Liu <stliu@hibernate.org>
2010-11-10 12:02:26 +0800

    HHH-5715 Using row_number with the SQLServerDialect makes select distinct useless

---
+++ core/src/test/java/org/hibernate/dialect/SQLServerDialectTestCase.java
@@ -0,0 +1,42 @@
+package org.hibernate.dialect;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test of the behavior of the SQLServerDialect utility methods
+ * 
+ * @author Valotasion Yoryos
+ * 
+ */
+public class SQLServerDialectTestCase extends TestCase {
+
+	public void testStripAliases() {
+		String input = "some_field1 as f1, some_fild2 as f2, _field3 as f3 ";
+
+		assertEquals( "some_field1, some_fild2, _field3", SQLServerDialect.stripAliases(input) );
+	}
+	
+	public void testGetSelectFieldsWithoutAliases() {
+		StringBuilder input = new StringBuilder( "select some_field1 as f12, some_fild2 as f879, _field3 as _f24674_3 from...." );
+		String output = SQLServerDialect.getSelectFieldsWithoutAliases( input ).toString();
+
+		assertEquals( " some_field1, some_fild2, _field3", output );
+	}
+	
+	
+	public void testReplaceDistinctWithGroupBy() {
+		StringBuilder input = new StringBuilder( "select distinct f1, f2 as ff, f3 from table where f1 = 5" );
+		SQLServerDialect.replaceDistinctWithGroupBy( input );
+		
+		assertEquals( "select f1, f2 as ff, f3 from table where f1 = 5 group by f1, f2, f3 ", input.toString() );
+	}
+	
+	
+	public void testGetLimitString() {
+		String input = "select distinct f1 as f53245 from table849752 order by f234, f67 desc"; 
+		
+		SQLServerDialect sqlDialect = new SQLServerDialect();
+		
+		assertEquals( "with query as (select row_number() over (order by f234, f67 desc) as __hibernate_row_nr__, f1 as f53245 from table849752  group by f1) select * from query where __hibernate_row_nr__ between 11 and 15", sqlDialect.getLimitString(input, 10, 15).toLowerCase() );
+	}
+}
---
+++ testsuite/src/test/java/org/hibernate/test/pagination/Tag.java
@@ -0,0 +1,31 @@
+package org.hibernate.test.pagination;
+
+public class Tag {
+	private int id;
+	private String surrogate;
+	
+	public Tag() {
+	
+	}
+
+	public Tag(String surrogate) {
+		this.surrogate = surrogate;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSurrogate() {
+		return surrogate;
+	}
+
+	public void setSurrogate(String surrogate) {
+		this.surrogate = surrogate;
+	}
+
+}

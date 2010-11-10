d24a06ee8d4bec8cd33a037af56c3ad0a3d6c6a8
Hardy Ferentschik <hardy@ferentschik.de>
2010-11-10 17:41:20 +0100

    HV-361 Formatting and adding/updating copyrights

--- hibernate-core/src/test/java/org/hibernate/test/annotations/beanvalidation/BeanValidationDisabledTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/annotations/beanvalidation/BeanValidationDisabledTest.java
@@ -1,13 +1,37 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2010 by Red Hat Inc and/or its affiliates or by
+ * third-party contributors as indicated by either @author tags or express
+ * copyright attribution statements applied by the authors.  All
+ * third-party contributions are distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+
 package org.hibernate.test.annotations.beanvalidation;
 
 import java.math.BigDecimal;
 import javax.validation.ConstraintViolationException;
 
-import org.hibernate.mapping.PersistentClass;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import org.hibernate.mapping.Column;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.mapping.Column;
+import org.hibernate.mapping.PersistentClass;
 import org.hibernate.test.annotations.TestCase;
 
 /**
@@ -17,23 +41,23 @@ public class BeanValidationDisabledTest extends TestCase {
 	public void testListeners() {
 		CupHolder ch = new CupHolder();
 		ch.setRadius( new BigDecimal( "12" ) );
-		Session s = openSession(  );
+		Session s = openSession();
 		Transaction tx = s.beginTransaction();
 		try {
 			s.persist( ch );
 			s.flush();
 		}
 		catch ( ConstraintViolationException e ) {
-			fail("invalid object should not be validated");
+			fail( "invalid object should not be validated" );
 		}
 		tx.rollback();
 		s.close();
 	}
-	
+
 	public void testDDLDisabled() {
-		PersistentClass classMapping = getCfg().getClassMapping( Address.class.getName() ); 		
-		Column countryColumn = (Column) classMapping.getProperty( "country" ).getColumnIterator().next(); 	
-		assertTrue("DDL constraints are applied", countryColumn.isNullable() ); 	
+		PersistentClass classMapping = getCfg().getClassMapping( Address.class.getName() );
+		Column countryColumn = (Column) classMapping.getProperty( "country" ).getColumnIterator().next();
+		assertTrue( "DDL constraints are applied", countryColumn.isNullable() );
 	}
 
 	@Override
--- hibernate-core/src/test/java/org/hibernate/test/annotations/beanvalidation/DDLTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/annotations/beanvalidation/DDLTest.java
@@ -1,7 +1,31 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2010 by Red Hat Inc and/or its affiliates or by
+ * third-party contributors as indicated by either @author tags or express
+ * copyright attribution statements applied by the authors.  All
+ * third-party contributions are distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+
 package org.hibernate.test.annotations.beanvalidation;
 
-import org.hibernate.mapping.PersistentClass;
 import org.hibernate.mapping.Column;
+import org.hibernate.mapping.PersistentClass;
 import org.hibernate.mapping.Property;
 import org.hibernate.test.annotations.TestCase;
 
@@ -37,7 +61,7 @@ public class DDLTest extends TestCase {
 		PersistentClass classMapping = getCfg().getClassMapping( Tv.class.getName() );
 		Column modelColumn = (Column) classMapping.getProperty( "model" ).getColumnIterator().next();
 		assertEquals( modelColumn.getLength(), 5 );
-	}	
+	}
 
 	public void testApplyOnManyToOne() throws Exception {
 		PersistentClass classMapping = getCfg().getClassMapping( TvOwner.class.getName() );
@@ -48,18 +72,22 @@ public class DDLTest extends TestCase {
 	public void testSingleTableAvoidNotNull() throws Exception {
 		PersistentClass classMapping = getCfg().getClassMapping( Rock.class.getName() );
 		Column serialColumn = (Column) classMapping.getProperty( "bit" ).getColumnIterator().next();
-		assertTrue( "Notnull should not be applised on single tables", serialColumn.isNullable() );
+		assertTrue( "Notnull should not be applied on single tables", serialColumn.isNullable() );
 	}
 
 	public void testNotNullOnlyAppliedIfEmbeddedIsNotNullItself() throws Exception {
 		PersistentClass classMapping = getCfg().getClassMapping( Tv.class.getName() );
 		Property property = classMapping.getProperty( "tuner.frequency" );
 		Column serialColumn = (Column) property.getColumnIterator().next();
-		assertEquals( "Validator annotations are applied on tunner as it is @NotNull", false, serialColumn.isNullable() );
+		assertEquals(
+				"Validator annotations are applied on tuner as it is @NotNull", false, serialColumn.isNullable()
+		);
 
 		property = classMapping.getProperty( "recorder.time" );
 		serialColumn = (Column) property.getColumnIterator().next();
-		assertEquals( "Validator annotations are applied on tunner as it is @NotNull", true, serialColumn.isNullable() );
+		assertEquals(
+				"Validator annotations are applied on tuner as it is @NotNull", true, serialColumn.isNullable()
+		);
 	}
 
 	protected Class<?>[] getAnnotatedClasses() {

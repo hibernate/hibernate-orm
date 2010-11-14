2964b975ef05173949f2700a33c3297923832ccd
Yoryos Valotasios <valotas@gmail.com>
2010-11-14 23:55:06 +0200

    HHH-3965: registered some more columntypes as varchar(max) and varbinary(max)

--- hibernate-core/src/main/java/org/hibernate/dialect/SQLServer2005Dialect.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/SQLServer2005Dialect.java
@@ -43,12 +43,13 @@ public class SQLServer2005Dialect extends SQLServerDialect {
 		// HHH-3965 fix
 		// As per http://www.sql-server-helper.com/faq/sql-server-2005-varchar-max-p01.aspx
 		// use varchar(max) and varbinary(max) instead of TEXT and IMAGE types
+		registerColumnType( Types.BLOB, "varbinary(MAX)" );
 		registerColumnType( Types.VARBINARY, "varbinary(MAX)" );
 		registerColumnType( Types.VARBINARY, MAX_LENGTH, "varbinary($l)" );
-		
 		registerColumnType( Types.LONGVARBINARY, "varbinary(MAX)" );
-		registerColumnType( Types.LONGVARCHAR, "varchar(MAX)" );
 		
+		registerColumnType( Types.CLOB, "varchar(MAX)" );
+		registerColumnType( Types.LONGVARCHAR, "varchar(MAX)" );
 		registerColumnType( Types.VARCHAR, "varchar(MAX)" );
 		registerColumnType( Types.VARCHAR, MAX_LENGTH, "varchar($l)" );
 		

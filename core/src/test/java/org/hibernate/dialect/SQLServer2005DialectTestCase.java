722a1b385166845a6594697de9dbab6983fc9575
Strong Liu <stliu@hibernate.org>
2010-11-29 18:11:44 +0800

    HHH-5726 update test case

--- core/src/test/java/org/hibernate/dialect/SQLServer2005DialectTestCase.java
+++ core/src/test/java/org/hibernate/dialect/SQLServer2005DialectTestCase.java
@@ -36,7 +36,6 @@ public class SQLServer2005DialectTestCase extends TestCase {
 		String input = "select distinct f1 as f53245 from table849752 order by f234, f67 desc"; 
 		
 		Dialect sqlDialect = new SQLServer2005Dialect();
-		
-		assertEquals( "with query as (select row_number() over (order by f234, f67 desc) as __hibernate_row_nr__, f1 as f53245 from table849752  group by f1) select * from query where __hibernate_row_nr__ between 11 and 15", sqlDialect.getLimitString(input, 10, 15).toLowerCase() );
+		assertEquals( "with query as (select row_number() over (order by f234, f67 desc) as __hibernate_row_nr__, f1 as f53245 from table849752  group by f1) select * from query where __hibernate_row_nr__ between ? and ?", sqlDialect.getLimitString(input, 10, 15).toLowerCase() );
 	}
 }

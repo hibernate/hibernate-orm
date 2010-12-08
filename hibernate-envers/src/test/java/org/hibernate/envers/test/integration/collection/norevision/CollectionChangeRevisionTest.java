4e102c68c1d7b377b06936d0221f0f331f63a515
adamw <adam@warski.org>
2010-12-08 19:56:04 +0100

    Refactoring the norevision tests: checking explicitly the revision numbers, introducing an abstract common class

--- hibernate-envers/src/test/java/org/hibernate/envers/test/integration/collection/norevision/CollectionChangeRevisionTest.java
+++ hibernate-envers/src/test/java/org/hibernate/envers/test/integration/collection/norevision/CollectionChangeRevisionTest.java
@@ -1,27 +1,16 @@
 package org.hibernate.envers.test.integration.collection.norevision;
 
-import org.hibernate.MappingException;
-import org.testng.annotations.BeforeMethod;
+import java.util.Arrays;
+import java.util.List;
 
-import java.net.URISyntaxException;
-
-public class CollectionChangeRevisionTest extends CollectionChangeNoRevisionTest {
-
-    protected static final int PERSON_COUNT_NEW_REVISION_ON_COLLECTION = 2;
-    protected static final String NEW_REVISION_ON_COLLECTION = "true";
-
-    @Override
-    protected void initMappings() throws MappingException, URISyntaxException {
-        super.initMappings();
-    }
-
-    @Override
-    protected int getExpectedPersonRevisionCount() {
-        return PERSON_COUNT_NEW_REVISION_ON_COLLECTION;
-    }
-
+public class CollectionChangeRevisionTest extends AbstractCollectionChangeTest {       
     @Override
     protected String getCollectionChangeValue() {
-        return NEW_REVISION_ON_COLLECTION;
+        return "true";
+    }
+
+    @Override
+    protected List<Integer> getExpectedPersonRevisions() {
+        return Arrays.asList(1, 3);
     }
 }

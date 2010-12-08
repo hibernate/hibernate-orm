e4315140d17f8c96ad9511223848dbeef19ab007
adamw <adam@warski.org>
2010-12-08 19:56:04 +0100

    Refactoring the norevision tests: checking explicitly the revision numbers, introducing an abstract common class

--- envers/src/test/java/org/hibernate/envers/test/integration/collection/norevision/CollectionChangeNoRevisionTest.java
+++ envers/src/test/java/org/hibernate/envers/test/integration/collection/norevision/CollectionChangeNoRevisionTest.java
@@ -1,69 +1,15 @@
 package org.hibernate.envers.test.integration.collection.norevision;
 
-import org.hibernate.MappingException;
-import org.hibernate.Transaction;
-import org.hibernate.criterion.Restrictions;
-import org.hibernate.envers.test.AbstractSessionTest;
-import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Test;
-
-import java.io.File;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class CollectionChangeNoRevisionTest extends AbstractSessionTest {
-
-    protected static final int EXPECTED_PERSON_REVISION_COUNT = 1;
-    protected static final String CREATE_REVISION_ON_COLLECTION_CHANGE = "false";
-    protected Integer personId;
+public class CollectionChangeNoRevisionTest extends AbstractCollectionChangeTest {
+    protected String getCollectionChangeValue() {
+        return "false";
+    }
 
     @Override
-    protected void initMappings() throws MappingException, URISyntaxException {
-        config.addAnnotatedClass(Person.class);
-        config.addAnnotatedClass(Name.class);
-        config.setProperty("org.hibernate.envers.revision_on_collection_change", getCollectionChangeValue());
+    protected List<Integer> getExpectedPersonRevisions() {
+        return Arrays.asList(1);
     }
-
-    protected String getCollectionChangeValue() {
-        return CREATE_REVISION_ON_COLLECTION_CHANGE;
-    }
-
-    @BeforeClass(dependsOnMethods = "init")
-    public void initData() {
-    	newSessionFactory();
-        Person p = new Person();
-        Name n = new Name();
-        n.setName("name1");
-        p.getNames().add(n);
-        getSession().getTransaction().begin();
-        getSession().saveOrUpdate(p);
-        getSession().getTransaction().commit();
-        personId = p.getId();
-        getSession().getTransaction().begin();
-        n.setName("Changed name");
-        getSession().saveOrUpdate(p);
-        getSession().getTransaction().commit();
-        getSession().getTransaction().begin();
-        Name n2 = new Name();
-        n2.setName("name2");
-        p.getNames().add(n2);
-        getSession().getTransaction().commit();
-    }
-
-    @Test
-    public void testPersonRevisionCount() {
-        int sizePerson = getAuditReader().getRevisions(Person.class, personId).size();
-        assert sizePerson == getExpectedPersonRevisionCount();
-    }
-
-    protected int getExpectedPersonRevisionCount() {
-        return EXPECTED_PERSON_REVISION_COUNT;
-    }
-
-
 }

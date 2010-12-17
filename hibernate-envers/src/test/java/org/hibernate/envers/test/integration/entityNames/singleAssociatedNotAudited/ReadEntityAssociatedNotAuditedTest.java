0f228d5b2ed2659d85f9cb1e51d263c67cf09db2
Hernan <Hernan@colossus>
2010-12-17 20:18:48 -0200

    - Creating abstract test with one session-per-test
    - Fixing typo error.

--- hibernate-envers/src/test/java/org/hibernate/envers/test/integration/entityNames/singleAssociatedNotAudited/ReadEntityAssociatedNotAuditedTest.java
+++ hibernate-envers/src/test/java/org/hibernate/envers/test/integration/entityNames/singleAssociatedNotAudited/ReadEntityAssociatedNotAuditedTest.java
@@ -5,7 +5,7 @@ import java.net.URISyntaxException;
 import java.net.URL;
 
 import org.hibernate.MappingException;
-import org.hibernate.envers.test.AbstractSessionTest;
+import org.hibernate.envers.test.AbstractOneSessionTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -15,7 +15,7 @@ import org.testng.annotations.Test;
  */
 
 @Test(sequential=true)
-public class ReadEntityAssociatedNotAuditedTest extends AbstractSessionTest{
+public class ReadEntityAssociatedNotAuditedTest extends AbstractOneSessionTest {
 
 	private long id_car1;
 	private long id_car2;
@@ -24,7 +24,9 @@ public class ReadEntityAssociatedNotAuditedTest extends AbstractSessionTest{
 	private long id_pers2; 
 	
 	private Car car1;
+	private Car car2;
 	private Person person1_1;
+	private Person person2;
 	private Person currentPerson1;
 	private Car currentCar1;
 	
@@ -33,21 +35,11 @@ public class ReadEntityAssociatedNotAuditedTest extends AbstractSessionTest{
 		URL url = Thread.currentThread().getContextClassLoader().getResource("mappings/entityNames/singleAssociatedNotAudited/mappings.hbm.xml");
         config.addFile(new File(url.toURI()));
 	}
-	
-	/**
-	 * The test needs to run with the same session and auditReader.
-	 */
-	@Override
-	public void newSessionFactory() {
-		if (getSession() == null) {
-			super.newSessionFactory();
-		}
-	}
-	
+
     @BeforeClass(dependsOnMethods = "init")
     public void initData() {
     	
-    	newSessionFactory();
+    	initializeSession();
 
         Person pers1 = new Person("Hernan", 15);
         Person pers2 = new Person("Leandro", 19);
@@ -75,18 +67,29 @@ public class ReadEntityAssociatedNotAuditedTest extends AbstractSessionTest{
 
     }
     
-    @Test
-    public void testGetAssociationWithEntityNameAndNotAuditedMode() {
-    	// persons from "actual" model 
+    private void loadDataOnSessionAndAuditReader() {
     	currentPerson1 = (Person)getSession().get("Personaje", id_pers1);
-    	Person person2 = (Person)getSession().get("Personaje", id_pers2);
+    	person2 = (Person)getSession().get("Personaje", id_pers2);
     	
     	currentCar1 = (Car)getSession().get(Car.class, id_car1);
     	
     	car1 = getAuditReader().find(Car.class, id_car1, 1);
-    	Car car2 = getAuditReader().find(Car.class, id_car2, 2);
+    	car2 = getAuditReader().find(Car.class, id_car2, 2);
     	
-    	// persons from "historic" model 
+    }
+    
+    private void checkEntityNames() {
+    	
+    	String currentCar1EN = getSession().getEntityName(currentCar1);
+    	
+    	String car1EN = getAuditReader().getEntityName(id_car1, 1, car1);
+    	
+    	assert (currentCar1EN.equals(car1EN));
+    	
+    }
+    
+    private void checkEntities() {
+
     	person1_1 = car1.getOwner();
     	Person person2_1 = car2.getOwner();
     	
@@ -94,29 +97,27 @@ public class ReadEntityAssociatedNotAuditedTest extends AbstractSessionTest{
     	assert(person2.getAge() == person2_1.getAge());
     }
     
-    @Test(dependsOnMethods="testGetAssociationWithEntityNameAndNotAuditedMode")
+    @Test
     public void testObtainEntityNameAssociationWithEntityNameAndNotAuditedMode() {
+    	loadDataOnSessionAndAuditReader();
     	
-    	String currentCar1EN = getSession().getEntityName(currentCar1);
+    	checkEntities();
     	
-    	String car1EN = getAuditReader().getEntityName(id_car1, 1, car1);
-    	assert (currentCar1EN.equals(car1EN));
-    	
+    	checkEntityNames();
     }
     
+
     @Test(dependsOnMethods="testObtainEntityNameAssociationWithEntityNameAndNotAuditedMode")
-    public void testFindHistoricAndCurrentGetAssociationWithEntityNameAndNotAuditedMode() {
+    public void testObtainEntityNameAssociationWithEntityNameAndNotAuditedModeInNewSession() {
     	//force a new session and AR
-    	super.newSessionFactory();
+    	forceNewSession();
     	
-    	Car car1 = getAuditReader().find(Car.class, id_car1, 1);
-    	car1.getOwner().getName();
-    	Car car2 = getAuditReader().find(Car.class, id_car2, 2);
-    	car2.getOwner().getName();
+    	loadDataOnSessionAndAuditReader();
     	
-    	// entityName personaje is marked as NOT_AUDITED
-   		Person person = (Person)getSession().get("Personaje", id_pers1); 
-       	person.getName();
+    	checkEntities();
+    	
+    	checkEntityNames();
+
     }    
 
 }

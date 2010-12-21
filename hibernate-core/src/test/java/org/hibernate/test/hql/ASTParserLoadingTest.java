bf6bd51c7e8ca4bd354afa4579d21c5383a2846a
Gail Badner <gbadner@redhat.com>
2010-12-20 17:27:10 -0800

    HHH-5325 : Improvements to test suite (fixes HQLDB 2.0 failures)

--- hibernate-core/src/test/java/org/hibernate/test/hql/ASTParserLoadingTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/hql/ASTParserLoadingTest.java
@@ -307,7 +307,7 @@ public class ASTParserLoadingTest extends FunctionalTestCase {
 		results = s.createQuery( "from Human where name is not null" ).list();
 		assertEquals( 3, results.size() );
 		String query =
-				getDialect() instanceof DB2Dialect ?
+				( getDialect() instanceof DB2Dialect || getDialect() instanceof HSQLDialect ) ?
 						"from Human where cast(? as string) is null" :
 						"from Human where ? is null"
 				;
@@ -2600,14 +2600,14 @@ public class ASTParserLoadingTest extends FunctionalTestCase {
 		 * PostgreSQL >= 8.3.7 typecasts are no longer automatically allowed 
 		 * <link>http://www.postgresql.org/docs/current/static/release-8-3.html</link>
 		 */
-		if(getDialect() instanceof PostgreSQLDialect){
+		if(getDialect() instanceof PostgreSQLDialect || getDialect() instanceof HSQLDialect){
 			hql = "from Animal a where bit_length(str(a.bodyWeight)) = 24";
 		}else{
 			hql = "from Animal a where bit_length(a.bodyWeight) = 24";
 		}
 		
 		session.createQuery(hql).list();
-		if(getDialect() instanceof PostgreSQLDialect){
+		if(getDialect() instanceof PostgreSQLDialect || getDialect() instanceof HSQLDialect){
 			hql = "select bit_length(str(a.bodyWeight)) from Animal a";
 		}else{
 			hql = "select bit_length(a.bodyWeight) from Animal a";

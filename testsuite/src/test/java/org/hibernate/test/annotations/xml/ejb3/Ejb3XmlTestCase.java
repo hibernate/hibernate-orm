57f5dce48c21277a36864c66af955e918338e233
davidmc24 <david@carrclan.us>
2010-12-21 20:51:10 -0500

    HHH-5800 Add tests and fixes for many-to-one orm XML support

---
+++ testsuite/src/test/java/org/hibernate/test/annotations/xml/ejb3/Ejb3XmlTestCase.java
@@ -0,0 +1,76 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2010 by Red Hat Inc and/or its affiliates or by
+ * third-party contributors as indicated by either @author tags or express
+ * copyright attribution statements applied by the authors.  All
+ * third-party contributions are distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+
+package org.hibernate.test.annotations.xml.ejb3;
+
+import java.io.InputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedElement;
+
+import org.dom4j.Document;
+import org.dom4j.io.SAXReader;
+import org.hibernate.cfg.annotations.reflection.JPAOverridenAnnotationReader;
+import org.hibernate.cfg.annotations.reflection.XMLContext;
+import org.hibernate.test.annotations.TestCase;
+
+abstract class Ejb3XmlTestCase extends TestCase {
+	protected JPAOverridenAnnotationReader reader;
+	
+	protected void assertAnnotationPresent(Class<? extends Annotation> annotationType) {
+		assertTrue(reader.isAnnotationPresent(annotationType));
+	}
+	
+	protected void assertAnnotationNotPresent(Class<? extends Annotation> annotationType) {
+		assertFalse(reader.isAnnotationPresent(annotationType));
+	}
+	
+	protected JPAOverridenAnnotationReader getReader(Class<?> entityClass, String fieldName, String ormResourceName) throws Exception {
+		AnnotatedElement el = getAnnotatedElement(entityClass, fieldName);
+		XMLContext xmlContext = getContext(ormResourceName);
+		JPAOverridenAnnotationReader reader = new JPAOverridenAnnotationReader(el, xmlContext);
+		return reader;
+	}
+	
+	protected AnnotatedElement getAnnotatedElement(Class<?> entityClass, String fieldName) throws Exception {
+		return entityClass.getDeclaredField(fieldName);
+	}
+	
+	protected XMLContext getContext(String resourceName) throws Exception {
+		InputStream is = getClass().getResourceAsStream(resourceName);
+		assertNotNull("Could not load resource " + resourceName, is);
+		return getContext(is);
+	}
+	
+	protected XMLContext getContext(InputStream is) throws Exception {
+		XMLContext xmlContext = new XMLContext();
+		Document doc = new SAXReader().read(is);
+		xmlContext.addDocument(doc);
+		return xmlContext;
+	}
+	
+	@Override
+	protected Class<?>[] getAnnotatedClasses() {
+		return new Class<?>[0];
+	}
+}

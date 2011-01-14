2e9bb1a4b47044d0c21d5a4f23e73e482ef344a4
Emmanuel Bernard <emmanuel@hibernate.org>
2011-01-14 14:42:16 +0100

    HHH-5280 Add test failing on multiple owners

---
+++ testsuite/src/test/java/org/hibernate/test/annotations/engine/collection/UnidirCollectionWithMultipleOwnerTest.java
@@ -0,0 +1,48 @@
+package org.hibernate.test.annotations.engine.collection;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.test.annotations.TestCase;
+
+/**
+ * @author Emmanuel Bernard
+ */
+public class UnidirCollectionWithMultipleOwnerTest extends TestCase {
+
+	public void testUnidirCollectionWithMultipleOwner() throws Exception {
+		Session s = openSession();
+		Transaction tx;
+		tx = s.beginTransaction();
+		Father father = new Father();
+		Mother mother = new Mother();
+		Son son = new Son();
+		father.getOrderedSons().add( son );
+		son.setFather( father );
+		mother.getSons().add( son );
+		son.setMother( mother );
+		s.save( mother );
+		s.save( father );
+		tx.commit();
+
+		s.clear();
+
+		tx = s.beginTransaction();
+		son = (Son) s.get( Son.class, son.getId() );
+		father = (Father) s.get( Father.class, father.getId() );
+		mother = (Mother) s.get( Mother.class, mother.getId() );
+		s.delete( father );
+		s.delete( mother );
+		s.delete( son );
+		tx.commit();
+		s.close();
+	}
+
+	@Override
+	protected Class<?>[] getAnnotatedClasses() {
+		return new Class<?>[] {
+				Father.class,
+				Mother.class,
+				Son.class
+		};
+	}
+}

126592a55d61195d55c73f36f8427badb79a713f
Gail Badner <gbadner@redhat.com>
2011-01-17 13:50:39 -0800

    HHH-5695 : Test case for derived ID with bidirectional one-to-one

---
+++ hibernate-core/src/test/java/org/hibernate/test/annotations/derivedidentities/bidirectional/Bar.java
@@ -0,0 +1,58 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2011, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.test.annotations.derivedidentities.bidirectional;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Bar implements Serializable {
+
+	@Id
+	@OneToOne
+	@JoinColumn(name = "BAR_ID")
+	private Foo foo;
+
+	private String details;
+
+	public Foo getFoo() {
+		return foo;
+	}
+
+	public void setFoo(Foo foo) {
+		this.foo = foo;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+
+	public void setDetails(String details) {
+		this.details = details;
+	}
+
+}

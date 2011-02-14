cb900355a72740b74dce3c51b25e687f98ea0200
Hardy Ferentschik <hibernate@ferentschik.de>
2011-02-14 18:31:29 +0100

    METAGEN-54 Added license headers

--- tooling/metamodel-generator/src/test/java/org/hibernate/jpamodelgen/test/xmlonly/Car.java
+++ tooling/metamodel-generator/src/test/java/org/hibernate/jpamodelgen/test/xmlonly/Car.java
@@ -1,19 +1,35 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.hibernate.jpamodelgen.test.xmlonly;
 
 import java.util.Set;
 
 public class Car {
-    public Long getId() {
-        return 1L;
-    }
+	public Long getId() {
+		return 1L;
+	}
 
-    public void setId(Long id) {
-    }
+	public void setId(Long id) {
+	}
 
-    public Set<Tire> getTires() {
-        return null;
-    }
+	public Set<Tire> getTires() {
+		return null;
+	}
 
-    public void setTires(Set<Tire> tires) {
-    }
+	public void setTires(Set<Tire> tires) {
+	}
 }

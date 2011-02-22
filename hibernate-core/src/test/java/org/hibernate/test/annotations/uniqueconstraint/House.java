7a1b8cf4a5e26e9ace87fd783df69d571c09dbe1
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-02-22 15:49:49 +0100

    HHH-5947: Test case

---
+++ hibernate-core/src/test/java/org/hibernate/test/annotations/uniqueconstraint/House.java
@@ -0,0 +1,35 @@
+package org.hibernate.test.annotations.uniqueconstraint;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+/**
+ * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
+ */
+@Entity
+@Table(uniqueConstraints = {@UniqueConstraint(name = "uniqueWithInherited", columnNames = {"room", "cost"} )})
+public class House extends Building {
+
+    public Long id;
+
+    public Integer cost;
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+}

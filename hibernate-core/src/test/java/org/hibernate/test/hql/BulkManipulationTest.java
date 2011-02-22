50853035315cb4ae2f9fc59334a5b38796817832
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-02-22 12:57:22 +0100

    HHH-2692 Handling subqueries in INSERT...SELECT, disallowing use of :parameters in SELECT if not in INSERT...SELECT, additional test cases

--- hibernate-core/src/test/java/org/hibernate/test/hql/BulkManipulationTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/hql/BulkManipulationTest.java
@@ -176,6 +176,18 @@ public class BulkManipulationTest extends FunctionalTestCase {
 		data.cleanup();
 	}
 
+    public void testSelectWithNamedParamProjection() {
+        Session s = openSession();
+        try {
+            s.createQuery("select :someParameter, id from Car");
+            fail("Should throw an unsupported exception");
+        } catch(QueryException q) {
+            // allright
+        } finally {
+            s.close();
+        }
+    }
+
     public void testSimpleInsertWithNamedParam() {
 		TestData data = new TestData();
 		data.prepare();
@@ -190,6 +202,65 @@ public class BulkManipulationTest extends FunctionalTestCase {
 
 		t.commit();
 		t = s.beginTransaction();
+
+		s.createQuery( "delete Vehicle" ).executeUpdate();
+
+		t.commit();
+		s.close();
+
+		data.cleanup();
+	}
+
+    public void testInsertWithMultipleNamedParams() {
+		TestData data = new TestData();
+		data.prepare();
+
+		Session s = openSession();
+		Transaction t = s.beginTransaction();
+
+		org.hibernate.Query q = s.createQuery( "insert into Pickup (id, owner, vin) select :id, owner, :vin from Car" );
+		q.setParameter("id", 5l);
+        q.setParameter("vin", "some");
+
+		q.executeUpdate();
+
+		t.commit();
+		t = s.beginTransaction();
+
+		s.createQuery( "delete Vehicle" ).executeUpdate();
+
+		t.commit();
+		s.close();
+
+		data.cleanup();
+	}
+
+    public void testInsertWithSubqueriesAndNamedParams() {
+		TestData data = new TestData();
+		data.prepare();
+
+		Session s = openSession();
+		Transaction t = s.beginTransaction();
+
+		org.hibernate.Query q = s.createQuery( "insert into Pickup (id, owner, vin) select :id, (select a.description from Animal a where a.description = :description), :vin from Car" );
+		q.setParameter("id", 5l);
+        q.setParameter("description", "Frog");
+        q.setParameter("vin", "some");
+
+		q.executeUpdate();
+
+		t.commit();
+		t = s.beginTransaction();
+
+        try {
+            org.hibernate.Query q1 = s.createQuery( "insert into Pickup (id, owner, vin) select :id, (select :description from Animal a where a.description = :description), :vin from Car" );
+            fail("Unsupported exception should have been thrown");
+        } catch(QueryException e) {
+            assertTrue(e.getMessage().indexOf("Use of parameters in subqueries of INSERT INTO DML statements is not supported.") > -1);
+        }
+
+        t = s.beginTransaction();
+
 
 		s.createQuery( "delete Vehicle" ).executeUpdate();
 

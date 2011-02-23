c71e3c4bafad4f1f979945c7e94ab8d73c4c2f5a
JPAV <jverhaeg@redhat.com>
2011-02-23 16:46:58 -0600

    Changed copyright notice from JBoss to Hibernate for new logging classes

--- hibernate-ehcache/src/main/java/org/hibernate/cache/EhCacheLogger.java
+++ hibernate-ehcache/src/main/java/org/hibernate/cache/EhCacheLogger.java
@@ -1,11 +1,28 @@
 /*
- * JBoss, Home of Professional Open Source.
+ * Hibernate, Relational Persistence for Idiomatic Java
  *
- * See the LEGAL.txt file distributed with this work for information regarding copyright ownership and licensing.
+ * Copyright (c) 2007-2011, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
  *
- * See the AUTHORS.txt file distributed with this work for a full listing of individual contributors.
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
  */
 package org.hibernate.cache;
+
 import static org.jboss.logging.Logger.Level.WARN;
 import org.hibernate.HibernateLogger;
 import org.jboss.logging.LogMessage;

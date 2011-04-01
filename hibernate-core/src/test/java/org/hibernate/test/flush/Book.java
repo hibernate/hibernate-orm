38e0484b4e2cdb3fcb1d2acc7256ae292c9f7bbb
Steve Ebersole <steve@hibernate.org>
2011-04-01 14:50:19 -0500

    HHH-2763 - Allow collection initialization during flush

---
+++ hibernate-core/src/test/java/org/hibernate/test/flush/Book.java
@@ -0,0 +1,80 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2011, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.test.flush;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import java.util.Set;
+
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ * @author Steve Ebersole
+ */
+@Entity
+public class Book {
+	private Long id;
+	private String title;
+	private Author author;
+
+	public Book() {
+	}
+
+	public Book(String title, Author author) {
+		this.title = title;
+		this.author = author;
+	}
+
+	@Id
+	@GeneratedValue( generator = "increment" )
+	@GenericGenerator( name = "increment", strategy = "increment" )
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@ManyToOne( cascade = CascadeType.ALL )
+	public Author getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+}
---
+++ hibernate-core/src/test/java/org/hibernate/test/flush/TestCollectionInitializingDuringFlush.java
@@ -0,0 +1,98 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2011, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.test.flush;
+
+import java.util.Map;
+
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.event.EventListenerRegistration;
+import org.hibernate.event.EventType;
+import org.hibernate.service.StandardServiceInitiators;
+import org.hibernate.service.event.spi.EventListenerRegistry;
+import org.hibernate.service.internal.BasicServiceRegistryImpl;
+import org.hibernate.service.spi.ServiceRegistryImplementor;
+
+import org.junit.Test;
+
+import org.hibernate.testing.FailureExpected;
+import org.hibernate.testing.TestForIssue;
+import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
+
+/**
+ * @author Steve Ebersole
+ */
+@TestForIssue( jiraKey = "HHH-2763" )
+public class TestCollectionInitializingDuringFlush extends BaseCoreFunctionalTestCase {
+	@Override
+	protected Class<?>[] getAnnotatedClasses() {
+		return new Class<?>[] { Author.class, Book.class, Publisher.class };
+	}
+
+	@Override
+	protected void applyServices(BasicServiceRegistryImpl serviceRegistry) {
+		super.applyServices( serviceRegistry );
+		serviceRegistry.getService( StandardServiceInitiators.EventListenerRegistrationService.class ).attachEventListenerRegistration(
+				new EventListenerRegistration() {
+					@Override
+					public void apply(
+							EventListenerRegistry eventListenerRegistry,
+							Configuration configuration,
+							Map<?, ?> configValues,
+							ServiceRegistryImplementor serviceRegistry) {
+						eventListenerRegistry.getEventListenerGroup( EventType.PRE_UPDATE ).appendListener( new InitializingPreUpdateEventListener() );
+					}
+				}
+		);
+	}
+
+	@Test
+	@FailureExpected( jiraKey = "HHH-2763" )
+	public void testInitializationDuringFlush() {
+		Session s = openSession();
+		s.beginTransaction();
+		Publisher publisher = new Publisher( "acme" );
+		Author author = new Author( "john" );
+		author.setPublisher( publisher );
+		publisher.getAuthors().add( author );
+		author.getBooks().add( new Book( "Reflections on a Wimpy Kid", author ) );
+		s.save( author );
+		s.getTransaction().commit();
+		s.clear();
+
+		s = openSession();
+		s.beginTransaction();
+		publisher = (Publisher) s.get( Publisher.class, publisher.getId() );
+		publisher.setName( "random nally" );
+		s.flush();
+		s.getTransaction().commit();
+		s.clear();
+
+		s = openSession();
+		s.beginTransaction();
+		s.delete( author );
+		s.getTransaction().commit();
+		s.clear();
+	}
+}

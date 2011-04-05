8bd56a097ad6b379bb70bc20c8ea098f5cbf162f
Alex Snaps <alex.snaps@gmail.com>
2011-04-05 17:46:57 +0200

    Added tests for newly introduced RegionFactories and upgraded to 2.4.1 (2.3 is busted with Hibernate 4.0)

---
+++ hibernate-ehcache/src/test/java/org/hibernate/test/cache/ehcache/EhCacheRegionFactoryImpl.java
@@ -0,0 +1,39 @@
+package org.hibernate.test.cache.ehcache;
+
+import org.hibernate.cache.EhCacheRegionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+/**
+ * @author Alex Snaps
+ */
+public class EhCacheRegionFactoryImpl extends EhCacheTest {
+
+	@Override
+	protected void configCache(final Configuration cfg) {
+		cfg.setProperty(Environment.CACHE_REGION_FACTORY, EhCacheRegionFactory.class.getName());
+		cfg.setProperty( Environment.CACHE_PROVIDER_CONFIG, "ehcache.xml" );
+	}
+
+	@Override
+	protected Map getMapFromCacheEntry(final Object entry) {
+		final Map map;
+		if ("net.sf.ehcache.hibernate.strategy.AbstractReadWriteEhcacheAccessStrategy$Item".equals(entry.getClass().getName())) {
+			try {
+				Field field = entry.getClass().getDeclaredField("value");
+				field.setAccessible(true);
+				map = (Map)field.get(entry);
+			} catch (NoSuchFieldException e) {
+				throw new RuntimeException(e);
+			} catch (IllegalAccessException e) {
+				throw new RuntimeException(e);
+			}
+		} else {
+			map = (Map)entry;
+		}
+		return map;
+	}
+}

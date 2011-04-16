54ff546a0830feb996d15d33146865586246f22d
Lukasz Antoniak <lukasz.antoniak@gmail.com>
2011-04-16 18:37:01 +0200

    HHH-5276 - REVINFO in different database schema creation

---
+++ hibernate-envers/src/test/java/org/hibernate/envers/test/integration/reventity/DifferentDBSchemaTest.java
@@ -0,0 +1,68 @@
+package org.hibernate.envers.test.integration.reventity;
+
+import org.hibernate.cfg.Environment;
+import org.hibernate.ejb.Ejb3Configuration;
+import org.hibernate.envers.test.AbstractEntityTest;
+import org.hibernate.envers.test.Priority;
+import org.hibernate.envers.test.entities.StrTestEntity;
+import org.hibernate.mapping.Table;
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import java.util.Arrays;
+
+/**
+ * Tests simple auditing process (read and write operations) when <i>REVINFO</i> and audit tables
+ * exist in a different database schema.
+ * @author Lukasz Antoniak (lukasz dot antoniak at gmail dot com)
+ */
+public class DifferentDBSchemaTest extends AbstractEntityTest {
+    private static final String SCHEMA_NAME = "ENVERS_AUDIT";
+    private Integer steId = null;
+
+    @Override
+    public void configure(Ejb3Configuration cfg) {
+        // Creates new schema after establishing connection
+        cfg.setProperty(Environment.URL, cfg.getProperties().getProperty(Environment.URL) + ";INIT=CREATE SCHEMA IF NOT EXISTS " + SCHEMA_NAME);
+        cfg.setProperty("org.hibernate.envers.default_schema", SCHEMA_NAME);
+        cfg.addAnnotatedClass(StrTestEntity.class);
+    }
+
+    @Test
+    @Priority(10)
+    public void initData() throws InterruptedException {
+        // Revision 1
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        StrTestEntity ste = new StrTestEntity("x");
+        em.persist(ste);
+        steId = ste.getId();
+        em.getTransaction().commit();
+
+        // Revision 2
+        em.getTransaction().begin();
+        ste = em.find(StrTestEntity.class, steId);
+        ste.setStr("y");
+        em.getTransaction().commit();
+    }
+
+    @Test
+    public void testRevinfoSchemaName() {
+        Table revisionTable = getCfg().getClassMapping("org.hibernate.envers.DefaultRevisionEntity").getTable();
+        assert SCHEMA_NAME.equals(revisionTable.getSchema());
+    }
+
+    @Test
+    public void testRevisionsCounts() {
+        assert Arrays.asList(1, 2).equals(getAuditReader().getRevisions(StrTestEntity.class, steId));
+    }
+
+    @Test
+    public void testHistoryOfId1() {
+        StrTestEntity ver1 = new StrTestEntity("x", steId);
+        StrTestEntity ver2 = new StrTestEntity("y", steId);
+
+        assert getAuditReader().find(StrTestEntity.class, steId, 1).equals(ver1);
+        assert getAuditReader().find(StrTestEntity.class, steId, 2).equals(ver2);
+    }
+}

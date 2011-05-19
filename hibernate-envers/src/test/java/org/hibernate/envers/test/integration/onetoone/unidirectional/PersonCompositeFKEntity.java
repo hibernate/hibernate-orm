d22b9571b7818881ac7063c74146a2daa92e0aa7
Lukasz Antoniak <lukasz.antoniak@gmail.com>
2011-05-19 21:39:31 +0200

    HHH-5808 - Code formatting

--- hibernate-envers/src/test/java/org/hibernate/envers/test/integration/onetoone/unidirectional/PersonCompositeFKEntity.java
+++ hibernate-envers/src/test/java/org/hibernate/envers/test/integration/onetoone/unidirectional/PersonCompositeFKEntity.java
@@ -16,64 +16,64 @@ import org.hibernate.envers.Audited;
 @Entity
 @Audited
 public class PersonCompositeFKEntity {
-	private Integer id;
-	private String name;
-	private AddressCompositePKEntity address;
+    private Integer id;
+    private String name;
+    private AddressCompositePKEntity address;
 
-	public PersonCompositeFKEntity() {
-	}
+    public PersonCompositeFKEntity() {
+    }
 
-	public PersonCompositeFKEntity(Integer id, String name, AddressCompositePKEntity address) {
-		this.setId(id);
-		this.setName(name);
-		this.setAddress(address);
-	}
+    public PersonCompositeFKEntity(Integer id, String name, AddressCompositePKEntity address) {
+        this.setId(id);
+        this.setName(name);
+        this.setAddress(address);
+    }
 
-	@Id
-	@Column(name = "ID", nullable = false, updatable = false)
-	public Integer getId() {
-		return id;
-	}
+    @Id
+    @Column(name = "ID", nullable = false, updatable = false)
+    public Integer getId() {
+        return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	@Column(name = "NAME", nullable = false)
-	public String getName() {
-		return name;
-	}
+    @Column(name = "NAME", nullable = false)
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	@OneToOne(cascade = { CascadeType.ALL }, orphanRemoval = true)
-	@JoinColumns({@JoinColumn(name = "ID_ADDRESS", referencedColumnName = "ID"),
+    @OneToOne(cascade = {CascadeType.ALL}, orphanRemoval = true)
+    @JoinColumns({@JoinColumn(name = "ID_ADDRESS", referencedColumnName = "ID"),
                   @JoinColumn(name = "VER_ADDRESS", referencedColumnName = "VER")})
-	public AddressCompositePKEntity getAddress() {
-		return address;
-	}
+    public AddressCompositePKEntity getAddress() {
+        return address;
+    }
 
-	public void setAddress(AddressCompositePKEntity address) {
-		this.address = address;
-	}
+    public void setAddress(AddressCompositePKEntity address) {
+        this.address = address;
+    }
 
-	public String toString() {
-		return "PersonCompositeFKEntity[id = " + getId() + ", name = " + getName() + "]";
-	}
+    public String toString() {
+        return "PersonCompositeFKEntity[id = " + getId() + ", name = " + getName() + "]";
+    }
 
-	@Override
-	public int hashCode() {
-		int result = 1;
-		result = 31 * result + ((id == null) ? 0 : id.hashCode());
-		result = 31 * result + ((name == null) ? 0 : name.hashCode());
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = 31 * result + ((id == null) ? 0 : id.hashCode());
+        result = 31 * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
         if (!(o instanceof PersonCompositeFKEntity)) return false;
 
         PersonCompositeFKEntity that = (PersonCompositeFKEntity) o;
@@ -82,5 +82,5 @@ public class PersonCompositeFKEntity {
         if (name != null ? !name.equals(that.name) : that.name != null) return false;
 
         return true;
-	}
+    }
 }

0df2d4a7807257eb49d2a93124ef55392a018fa9
Lukasz Antoniak <lukasz.antoniak@gmail.com>
2011-05-19 21:39:31 +0200

    HHH-5808 - Code formatting

--- hibernate-envers/src/test/java/org/hibernate/envers/test/integration/onetoone/unidirectional/AddressPK.java
+++ hibernate-envers/src/test/java/org/hibernate/envers/test/integration/onetoone/unidirectional/AddressPK.java
@@ -6,47 +6,47 @@ import java.io.Serializable;
  * @author Andrei Zagorneanu
  */
 public class AddressPK implements Serializable {
-	private Integer id;
-	private Integer ver;
+    private Integer id;
+    private Integer ver;
 
-	public AddressPK() {
-	}
+    public AddressPK() {
+    }
 
-	public AddressPK(Integer id, Integer ver) {
-		this.setId(id);
-		this.setVer(ver);
-	}
+    public AddressPK(Integer id, Integer ver) {
+        this.setId(id);
+        this.setVer(ver);
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public Integer getVer() {
-		return ver;
-	}
+    public Integer getVer() {
+        return ver;
+    }
 
-	public void setVer(Integer ver) {
-		this.ver = ver;
-	}
+    public void setVer(Integer ver) {
+        this.ver = ver;
+    }
 
-	public String toString() {
-		return "AddressPK[id = " + getId() + ", ver = " + getVer() + "]";
-	}
+    public String toString() {
+        return "AddressPK[id = " + getId() + ", ver = " + getVer() + "]";
+    }
 
-	@Override
-	public int hashCode() {
-		int result = 1;
-		result = 31 * result + ((id == null) ? 0 : id.hashCode());
-		result = 31 * result + ((ver == null) ? 0 : ver.hashCode());
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = 31 * result + ((id == null) ? 0 : id.hashCode());
+        result = 31 * result + ((ver == null) ? 0 : ver.hashCode());
+        return result;
+    }
 
-	@Override
-	public boolean equals(Object o) {
+    @Override
+    public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof AddressPK)) return false;
 
@@ -56,5 +56,5 @@ public class AddressPK implements Serializable {
         if (id != null ? !id.equals(that.id) : that.id != null) return false;
 
         return true;
-	}
+    }
 }

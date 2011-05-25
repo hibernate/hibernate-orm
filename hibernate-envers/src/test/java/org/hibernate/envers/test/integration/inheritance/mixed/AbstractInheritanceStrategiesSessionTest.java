3acf1f23bd273da18f8f768f63099fef489ec3f4
Michal Skowronek <michal.skowronek@uj.edu.pl>
2011-05-25 02:57:55 +0200

    Extended test cases and provided a fix for the issue The idea for the fix is to simply stop using properties tag for relations in favor of plain (possibly multiple) propeties

--- hibernate-envers/src/test/java/org/hibernate/envers/test/integration/inheritance/mixed/AbstractInheritanceStrategiesSessionTest.java
+++ hibernate-envers/src/test/java/org/hibernate/envers/test/integration/inheritance/mixed/AbstractInheritanceStrategiesSessionTest.java
@@ -3,6 +3,7 @@ package org.hibernate.envers.test.integration.inheritance.mixed;
 import org.hibernate.envers.test.AbstractSessionTest;
 import org.hibernate.envers.test.Priority;
 import org.hibernate.envers.test.integration.inheritance.mixed.entities.Activity;
+import org.hibernate.envers.test.integration.inheritance.mixed.entities.ActivityId;
 import org.hibernate.envers.test.integration.inheritance.mixed.entities.CheckInActivity;
 import org.hibernate.envers.test.integration.inheritance.mixed.entities.NormalActivity;
 import org.junit.Test;
@@ -15,11 +16,17 @@ import static org.junit.Assert.assertEquals;
  * @author Michal Skowronek (mskowr at o2 pl)
  */
 public abstract class AbstractInheritanceStrategiesSessionTest extends AbstractSessionTest {
-    @Test
+
+	private ActivityId id1;
+	private ActivityId id2;
+	private ActivityId id3;
+
+	@Test
     @Priority(10)
     public void initData() {
         NormalActivity normalActivity = new NormalActivity();
-        normalActivity.setId(1);
+		id1 = new ActivityId(1, 2);
+		normalActivity.setId(id1);
         normalActivity.setSequenceNumber(1);
 
         // Revision 1
@@ -31,9 +38,10 @@ public abstract class AbstractInheritanceStrategiesSessionTest extends AbstractS
         // Revision 2
         getSession().getTransaction().begin();
 
-        normalActivity = (NormalActivity) getSession().get(NormalActivity.class, 1);
+        normalActivity = (NormalActivity) getSession().get(NormalActivity.class, id1);
         CheckInActivity checkInActivity = new CheckInActivity();
-        checkInActivity.setId(2);
+		id2 = new ActivityId(2, 3);
+		checkInActivity.setId(id2);
         checkInActivity.setSequenceNumber(0);
         checkInActivity.setDurationInMinutes(30);
         checkInActivity.setRelatedActivity(normalActivity);
@@ -44,7 +52,8 @@ public abstract class AbstractInheritanceStrategiesSessionTest extends AbstractS
 
         // Revision 3
         normalActivity = new NormalActivity();
-        normalActivity.setId(3);
+		id3 = new ActivityId(3, 4);
+		normalActivity.setId(id3);
         normalActivity.setSequenceNumber(2);
 
         getSession().getTransaction().begin();
@@ -56,8 +65,8 @@ public abstract class AbstractInheritanceStrategiesSessionTest extends AbstractS
         // Revision 4
         getSession().getTransaction().begin();
 
-        normalActivity = (NormalActivity) getSession().get(NormalActivity.class, 3);
-        checkInActivity = (CheckInActivity) getSession().get(CheckInActivity.class, 2);
+        normalActivity = (NormalActivity) getSession().get(NormalActivity.class, id3);
+        checkInActivity = (CheckInActivity) getSession().get(CheckInActivity.class, id2);
         checkInActivity.setRelatedActivity(normalActivity);
 
         getSession().merge(checkInActivity);
@@ -69,18 +78,18 @@ public abstract class AbstractInheritanceStrategiesSessionTest extends AbstractS
 
     @Test
     public void testRevisionsCounts() {
-        assertEquals(Arrays.asList(1), getAuditReader().getRevisions(NormalActivity.class, 1));
-        assertEquals(Arrays.asList(3), getAuditReader().getRevisions(NormalActivity.class, 3));
-        assertEquals(Arrays.asList(2, 4), getAuditReader().getRevisions(CheckInActivity.class, 2));
+        assertEquals(Arrays.asList(1), getAuditReader().getRevisions(NormalActivity.class, id1));
+        assertEquals(Arrays.asList(3), getAuditReader().getRevisions(NormalActivity.class, id3));
+        assertEquals(Arrays.asList(2, 4), getAuditReader().getRevisions(CheckInActivity.class, id2));
     }
 
     @Test
     public void testCurrentStateOfCheckInActivity() {
 
-        final CheckInActivity checkInActivity = (CheckInActivity) getSession().get(CheckInActivity.class, 2);
-        final NormalActivity normalActivity = (NormalActivity) getSession().get(NormalActivity.class, 3);
+        final CheckInActivity checkInActivity = (CheckInActivity) getSession().get(CheckInActivity.class, id2);
+        final NormalActivity normalActivity = (NormalActivity) getSession().get(NormalActivity.class, id3);
 
-        assertEquals(2, checkInActivity.getId().intValue());
+        assertEquals(id2, checkInActivity.getId());
         assertEquals(0, checkInActivity.getSequenceNumber().intValue());
         assertEquals(30, checkInActivity.getDurationInMinutes().intValue());
         final Activity relatedActivity = checkInActivity.getRelatedActivity();
@@ -90,20 +99,20 @@ public abstract class AbstractInheritanceStrategiesSessionTest extends AbstractS
 
     @Test
     public void testCheckCurrentStateOfNormalActivities() throws Exception {
-        final NormalActivity normalActivity1 = (NormalActivity) getSession().get(NormalActivity.class, 1);
-        final NormalActivity normalActivity2 = (NormalActivity) getSession().get(NormalActivity.class, 3);
+        final NormalActivity normalActivity1 = (NormalActivity) getSession().get(NormalActivity.class, id1);
+        final NormalActivity normalActivity2 = (NormalActivity) getSession().get(NormalActivity.class, id3);
 
-        assertEquals(1, normalActivity1.getId().intValue());
+        assertEquals(id1, normalActivity1.getId());
         assertEquals(1, normalActivity1.getSequenceNumber().intValue());
-        assertEquals(3, normalActivity2.getId().intValue());
+        assertEquals(id3, normalActivity2.getId());
         assertEquals(2, normalActivity2.getSequenceNumber().intValue());
     }
 
     public void doTestFirstRevisionOfCheckInActivity() throws Exception {
-        CheckInActivity checkInActivity = getAuditReader().find(CheckInActivity.class, 2, 2);
-        NormalActivity normalActivity = getAuditReader().find(NormalActivity.class, 1, 2);
+        CheckInActivity checkInActivity = getAuditReader().find(CheckInActivity.class, id2, 2);
+        NormalActivity normalActivity = getAuditReader().find(NormalActivity.class, id1, 2);
 
-        assertEquals(2, checkInActivity.getId().intValue());
+        assertEquals(id2, checkInActivity.getId());
         assertEquals(0, checkInActivity.getSequenceNumber().intValue());
         assertEquals(30, checkInActivity.getDurationInMinutes().intValue());
         Activity relatedActivity = checkInActivity.getRelatedActivity();
@@ -112,10 +121,10 @@ public abstract class AbstractInheritanceStrategiesSessionTest extends AbstractS
     }
 
     public void doTestSecondRevisionOfCheckInActivity() throws Exception {
-        CheckInActivity checkInActivity = getAuditReader().find(CheckInActivity.class, 2, 4);
-        NormalActivity normalActivity = getAuditReader().find(NormalActivity.class, 3, 4);
+        CheckInActivity checkInActivity = getAuditReader().find(CheckInActivity.class, id2, 4);
+        NormalActivity normalActivity = getAuditReader().find(NormalActivity.class, id3, 4);
 
-        assertEquals(2, checkInActivity.getId().intValue());
+        assertEquals(id2, checkInActivity.getId());
         assertEquals(0, checkInActivity.getSequenceNumber().intValue());
         assertEquals(30, checkInActivity.getDurationInMinutes().intValue());
         Activity relatedActivity = checkInActivity.getRelatedActivity();
--- hibernate-envers/src/test/java/org/hibernate/envers/test/integration/inheritance/mixed/entities/AbstractActivity.java
+++ hibernate-envers/src/test/java/org/hibernate/envers/test/integration/inheritance/mixed/entities/AbstractActivity.java
@@ -2,8 +2,8 @@ package org.hibernate.envers.test.integration.inheritance.mixed.entities;
 
 import org.hibernate.envers.Audited;
 
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 
@@ -11,19 +11,20 @@ import javax.persistence.InheritanceType;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public abstract class AbstractActivity implements Activity {
-    @Id
-    private Integer id;
+	@EmbeddedId
+    private ActivityId id;
+
     private Integer sequenceNumber;
 
-    public Integer getId() {
+    public ActivityId getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(ActivityId id) {
         this.id = id;
     }
 
-    public Integer getSequenceNumber() {
+	public Integer getSequenceNumber() {
         return sequenceNumber;
     }
 

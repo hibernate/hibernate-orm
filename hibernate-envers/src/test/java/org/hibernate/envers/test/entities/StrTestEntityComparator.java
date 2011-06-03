e5ad9885fff3fed078d00fb994bab2a4b3312d7f
adamw <adam@warski.org>
2011-06-03 09:40:29 +0200

    Fixing JDK5 compatibility problem & compilation error

--- hibernate-envers/src/test/java/org/hibernate/envers/test/entities/StrTestEntityComparator.java
+++ hibernate-envers/src/test/java/org/hibernate/envers/test/entities/StrTestEntityComparator.java
@@ -5,7 +5,6 @@ import java.util.Comparator;
 public class StrTestEntityComparator implements Comparator<StrTestEntity> {
     public static final StrTestEntityComparator INSTANCE = new StrTestEntityComparator();
 
-    @Override
     public int compare(StrTestEntity o1, StrTestEntity o2) {
         return o1.getStr().compareTo(o2.getStr());
     }

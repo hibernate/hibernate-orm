f12aad349321a97dfb8ea082fc9ed311ae358049
Gail Badner <gbadner@redhat.com>
2011-06-14 13:54:24 -0700

    HHH-6318 : Change EntityIdentifier.attributeBinding to be type SimpleAttributeBinding

--- hibernate-core/src/main/java/org/hibernate/metamodel/binding/EntityIdentifier.java
+++ hibernate-core/src/main/java/org/hibernate/metamodel/binding/EntityIdentifier.java
@@ -45,7 +45,7 @@ public class EntityIdentifier {
 	);
 
 	private final EntityBinding entityBinding;
-	private KeyValueBinding attributeBinding;
+	private SimpleAttributeBinding attributeBinding;
 	private IdentifierGenerator identifierGenerator;
 	private IdGenerator idGenerator;
 	// todo : mappers, etc
@@ -59,11 +59,11 @@ public class EntityIdentifier {
 		this.entityBinding = entityBinding;
 	}
 
-	public KeyValueBinding getValueBinding() {
+	public SimpleAttributeBinding getValueBinding() {
 		return attributeBinding;
 	}
 
-	public void setValueBinding(KeyValueBinding attributeBinding) {
+	public void setValueBinding(SimpleAttributeBinding attributeBinding) {
 		if ( this.attributeBinding != null ) {
 			// todo : error?  or just log?  For now throw exception and see what happens. Easier to see whether this
 			// method gets called multiple times

efc9377b3177d0f29701c96d591be362ea9b1f69
Lukasz Antoniak <lukasz.antoniak@gmail.com>
2011-06-18 10:04:52 +0200

    HHH-5917 - Fix and test

---
+++ hibernate-envers/src/test/java/org/hibernate/envers/test/integration/superclass/auditparents/BabyCompleteEntity.java
@@ -0,0 +1,53 @@
+package org.hibernate.envers.test.integration.superclass.auditparents;
+
+import org.hibernate.envers.Audited;
+import org.hibernate.envers.test.entities.StrIntTestEntity;
+
+import javax.persistence.Entity;
+
+/**
+ * @author Lukasz Antoniak (lukasz dot antoniak at gmail dot com)
+ */
+@Entity
+@Audited(auditParents = {MappedParentEntity.class})
+public class BabyCompleteEntity extends ChildCompleteEntity {
+    private String baby;
+
+    public BabyCompleteEntity() {
+    }
+
+    public BabyCompleteEntity(Long id, String grandparent, String notAudited, String parent, String child, StrIntTestEntity relation, String baby) {
+        super(id, grandparent, notAudited, parent, child, relation);
+        this.baby = baby;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BabyCompleteEntity)) return false;
+        if (!super.equals(o)) return false;
+
+        BabyCompleteEntity that = (BabyCompleteEntity) o;
+
+        if (baby != null ? !baby.equals(that.baby) : that.baby != null) return false;
+
+        return true;
+    }
+
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (baby != null ? baby.hashCode() : 0);
+        return result;
+    }
+
+    public String toString() {
+        return "BabyCompleteEntity(" + super.toString() + ", baby = " + baby + ")";
+    }
+
+    public String getBaby() {
+        return baby;
+    }
+
+    public void setBaby(String baby) {
+        this.baby = baby;
+    }
+}
---
+++ hibernate-envers/src/test/java/org/hibernate/envers/test/integration/superclass/auditparents/MappedParentEntity.java
@@ -0,0 +1,61 @@
+package org.hibernate.envers.test.integration.superclass.auditparents;
+
+import org.hibernate.envers.test.entities.StrIntTestEntity;
+
+import javax.persistence.ManyToOne;
+import javax.persistence.MappedSuperclass;
+
+/**
+ * @author Lukasz Antoniak (lukasz dot antoniak at gmail dot com)
+ */
+@MappedSuperclass
+public class MappedParentEntity extends MappedGrandparentEntity {
+    private String parent;
+
+    @ManyToOne
+    private StrIntTestEntity relation;
+
+    public MappedParentEntity(Long id, String grandparent, String notAudited, String parent, StrIntTestEntity relation) {
+        super(id, grandparent, notAudited);
+        this.parent = parent;
+        this.relation = relation;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MappedParentEntity)) return false;
+        if (!super.equals(o)) return false;
+
+        MappedParentEntity that = (MappedParentEntity) o;
+
+        if (parent != null ? !parent.equals(that.parent) : that.parent != null) return false;
+
+        return true;
+    }
+
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (parent != null ? parent.hashCode() : 0);
+        return result;
+    }
+
+    public String toString() {
+        return "MappedParentEntity(" + super.toString() + ", parent = " + parent + ", relation = " + relation + ")";
+    }
+
+    public String getParent() {
+        return parent;
+    }
+
+    public void setParent(String parent) {
+        this.parent = parent;
+    }
+
+    public StrIntTestEntity getRelation() {
+        return relation;
+    }
+
+    public void setRelation(StrIntTestEntity relation) {
+        this.relation = relation;
+    }
+}

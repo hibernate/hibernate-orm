6ef6b4097da4806040dac79e66ad8992e085f48f
Erik-Berndt Scheper <erik.berndt.scheper@gmail.com>
2011-07-14 16:01:49 +0200

    HHH-6361: Testcase demonstrating that collection events may contain wrong stored snapshot after merging a detached entity into the persistencecontext

---
+++ hibernate-core/src/test/java/org/hibernate/test/event/collection/detached/MultipleCollectionRefEntity1.java
@@ -0,0 +1,127 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2011, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.test.event.collection.detached;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class MultipleCollectionRefEntity1 implements org.hibernate.test.event.collection.Entity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID", length = 10)
+	private Long id;
+
+	@Column(name = "TEXT", length = 50, nullable = false)
+	private String text;
+
+	@ManyToOne
+	@JoinColumn(name = "MCE_ID", nullable = false, insertable = false, updatable = false)
+	@org.hibernate.annotations.ForeignKey(name = "FK_RE1_MCE")
+	private MultipleCollectionEntity multipleCollectionEntity;
+
+	@Column(name = "MCE_ID", insertable = false, updatable = false)
+	private Long multipleCollectionEntityId;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public MultipleCollectionEntity getMultipleCollectionEntity() {
+		return multipleCollectionEntity;
+	}
+
+	public void setMultipleCollectionEntity(
+			MultipleCollectionEntity multipleCollectionEntity) {
+		this.multipleCollectionEntity = multipleCollectionEntity;
+	}
+
+	public Long getMultipleCollectionEntityId() {
+		return multipleCollectionEntityId;
+	}
+
+	public void setMultipleCollectionEntityId(Long multipleCollectionEntityId) {
+		this.multipleCollectionEntityId = multipleCollectionEntityId;
+	}
+
+	@Override
+	public String toString() {
+		return "MultipleCollectionRefEntity1 [id=" + id + ", text=" + text
+				+ ", multipleCollectionEntityId=" + multipleCollectionEntityId
+				+ "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((text == null) ? 0 : text.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MultipleCollectionRefEntity1 other = (MultipleCollectionRefEntity1) obj;
+		if (text == null) {
+			if (other.text != null)
+				return false;
+		} else if (!text.equals(other.text))
+			return false;
+		return true;
+	}
+	
+	public MultipleCollectionRefEntity1 deepCopy(MultipleCollectionEntity newRef) {
+		MultipleCollectionRefEntity1 clone = new MultipleCollectionRefEntity1();
+		clone.setText(this.text);
+		clone.setId(this.id);
+		clone.setMultipleCollectionEntity(newRef);
+		clone.setMultipleCollectionEntityId(newRef.getId());
+		return clone;
+	}
+
+}

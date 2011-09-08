e8b6c1e45f8c685890269985ee20c13208da2bd8
Strong Liu <stliu@hibernate.org>
2011-09-08 12:33:08 +0800

    hibernate-core 3.6 branch is using jdk 1.5

--- hibernate-envers/src/test/java/org/hibernate/envers/test/entities/reventity/trackmodifiedentities/CustomTrackingRevisionListener.java
+++ hibernate-envers/src/test/java/org/hibernate/envers/test/entities/reventity/trackmodifiedentities/CustomTrackingRevisionListener.java
@@ -9,13 +9,11 @@ import java.io.Serializable;
  * @author Lukasz Antoniak (lukasz dot antoniak at gmail dot com)
  */
 public class CustomTrackingRevisionListener implements EntityTrackingRevisionListener {
-    @Override
     public void entityChanged(Class entityClass, String entityName, Serializable entityId, RevisionType revisionType,
                               Object revisionEntity) {
         ((CustomTrackingRevisionEntity)revisionEntity).addModifiedEntityType(entityClass.getName());
     }
 
-    @Override
     public void newRevision(Object revisionEntity) {
     }
 }

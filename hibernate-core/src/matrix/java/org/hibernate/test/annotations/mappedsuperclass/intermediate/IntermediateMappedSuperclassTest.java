7c2d88df8bcc4c1023b56aeaaad9622fe06cd665
Gail Badner <gbadner@redhat.com>
2011-10-20 13:01:16 -0700

    HHH-6751 : Test failure due to inconsistent scale returned for BigDecimal properties

--- hibernate-core/src/matrix/java/org/hibernate/test/annotations/mappedsuperclass/intermediate/IntermediateMappedSuperclassTest.java
+++ hibernate-core/src/matrix/java/org/hibernate/test/annotations/mappedsuperclass/intermediate/IntermediateMappedSuperclassTest.java
@@ -55,7 +55,12 @@ public class IntermediateMappedSuperclassTest extends BaseCoreFunctionalTestCase
 		session = openSession();
 		session.beginTransaction();
 		Account account = (Account) session.get( Account.class, savingsAccount.getId() );
-		assertEquals( withdrawalLimit, ( (SavingsAccount) account ).getWithdrawalLimit() );
+		// Oracle returns the BigDecimal with scale=0, which is equal to 1000 (not 1000.00);
+		// compare using BigDecimal.doubleValue;
+		assertEquals(
+				withdrawalLimit.doubleValue(),
+				( (SavingsAccount) account ).getWithdrawalLimit().doubleValue(),
+				0.001);
 		session.delete( account );
 		session.getTransaction().commit();
 		session.close();

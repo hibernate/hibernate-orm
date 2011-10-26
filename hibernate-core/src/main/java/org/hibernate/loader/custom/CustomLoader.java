c26030a27e301d4f99c2e577978f02f7d25edde3
Strong Liu <stliu@hibernate.org>
2011-10-26 23:12:22 +0800

    HHH-2304 Wrong type detection for sql type char(x) columns

--- hibernate-core/src/main/java/org/hibernate/loader/custom/CustomLoader.java
+++ hibernate-core/src/main/java/org/hibernate/loader/custom/CustomLoader.java
@@ -633,10 +633,14 @@ public class CustomLoader extends Loader {
 			int columnType = resultSetMetaData.getColumnType( columnPos );
 			int scale = resultSetMetaData.getScale( columnPos );
 			int precision = resultSetMetaData.getPrecision( columnPos );
+            int length = precision;
+            if ( columnType == 1 && precision == 0 ) {
+                length = resultSetMetaData.getColumnDisplaySize( columnPos );
+            }
 			return factory.getTypeResolver().heuristicType(
 					factory.getDialect().getHibernateTypeName(
 							columnType,
-							precision,
+							length,
 							precision,
 							scale
 					)

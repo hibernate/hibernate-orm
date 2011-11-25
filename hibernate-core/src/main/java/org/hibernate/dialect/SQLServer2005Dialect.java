fafcded4f2bc4c2b994c6fa8ebeae216d26ca141
Hardy Ferentschik <hardy@hibernate.org>
2011-11-25 12:05:51 +0100

    HHH-6849 Formatting and adding actual assertions to testOffset(). This is not addressing the SQLServer issue yet.

--- hibernate-core/src/main/java/org/hibernate/dialect/SQLServer2005Dialect.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/SQLServer2005Dialect.java
@@ -22,6 +22,7 @@
  * Boston, MA  02110-1301  USA
  */
 package org.hibernate.dialect;
+
 import java.sql.Types;
 
 import org.hibernate.dialect.function.NoArgSQLFunction;
@@ -84,7 +85,9 @@ public class SQLServer2005Dialect extends SQLServerDialect {
 	@Override
 	public String getLimitString(String query, int offset, int limit) {
 		// We transform the query to one with an offset and limit if we have an offset and limit to bind
-		if (offset > 1 || limit > 1) return getLimitString(query, true);
+		if ( offset > 1 || limit > 1 ) {
+			return getLimitString( query, true );
+		}
 		return query;
 	}
 
@@ -101,37 +104,32 @@ public class SQLServer2005Dialect extends SQLServerDialect {
 	 * SELECT * FROM query WHERE __hibernate_row_nr__ BEETWIN offset AND offset + last
 	 * </pre>
 	 *
-	 *
-	 * @param querySqlString
-	 *            The SQL statement to base the limit query off of.
-	 * @param offset
-	 *            Offset of the first row to be returned by the query (zero-based)
-	 * @param limit
-	 *            Maximum number of rows to be returned by the query
+	 * @param querySqlString The SQL statement to base the limit query off of.
+	 * @param hasOffset Is the query requesting an offset?
 	 *
 	 * @return A new SQL statement with the LIMIT clause applied.
 	 */
 	@Override
 	public String getLimitString(String querySqlString, boolean hasOffset) {
-		StringBuilder sb = new StringBuilder(querySqlString.trim().toLowerCase());
+		StringBuilder sb = new StringBuilder( querySqlString.trim().toLowerCase() );
 
-		int orderByIndex = sb.indexOf("order by");
-		CharSequence orderby = orderByIndex > 0 ? sb.subSequence(orderByIndex, sb.length())
+		int orderByIndex = sb.indexOf( "order by" );
+		CharSequence orderby = orderByIndex > 0 ? sb.subSequence( orderByIndex, sb.length() )
 				: "ORDER BY CURRENT_TIMESTAMP";
 
 		// Delete the order by clause at the end of the query
-		if (orderByIndex > 0) {
-			sb.delete(orderByIndex, orderByIndex + orderby.length());
+		if ( orderByIndex > 0 ) {
+			sb.delete( orderByIndex, orderByIndex + orderby.length() );
 		}
 
 		// HHH-5715 bug fix
-		replaceDistinctWithGroupBy(sb);
+		replaceDistinctWithGroupBy( sb );
 
-		insertRowNumberFunction(sb, orderby);
+		insertRowNumberFunction( sb, orderby );
 
 		// Wrap the query within a with statement:
-		sb.insert(0, "WITH query AS (").append(") SELECT * FROM query ");
-		sb.append("WHERE __hibernate_row_nr__ >= ? AND __hibernate_row_nr__ < ?");
+		sb.insert( 0, "WITH query AS (" ).append( ") SELECT * FROM query " );
+		sb.append( "WHERE __hibernate_row_nr__ >= ? AND __hibernate_row_nr__ < ?" );
 
 		return sb.toString();
 	}
@@ -144,10 +142,10 @@ public class SQLServer2005Dialect extends SQLServerDialect {
 	 * @param sql an sql query
 	 */
 	protected static void replaceDistinctWithGroupBy(StringBuilder sql) {
-		int distinctIndex = sql.indexOf(DISTINCT);
-		if (distinctIndex > 0) {
-			sql.delete(distinctIndex, distinctIndex + DISTINCT.length() + 1);
-			sql.append(" group by").append(getSelectFieldsWithoutAliases(sql));
+		int distinctIndex = sql.indexOf( DISTINCT );
+		if ( distinctIndex > 0 ) {
+			sql.delete( distinctIndex, distinctIndex + DISTINCT.length() + 1 );
+			sql.append( " group by" ).append( getSelectFieldsWithoutAliases( sql ) );
 		}
 	}
 
@@ -155,41 +153,39 @@ public class SQLServer2005Dialect extends SQLServerDialect {
 	 * This utility method searches the given sql query for the fields of the select statement and returns them without
 	 * the aliases. See {@link SQLServer2005DialectTestCase#testGetSelectFieldsWithoutAliases()}
 	 *
-	 * @param an
-	 *            sql query
+	 * @param sql sql query
+	 *
 	 * @return the fields of the select statement without their alias
 	 */
 	protected static CharSequence getSelectFieldsWithoutAliases(StringBuilder sql) {
-		String select = sql.substring(sql.indexOf(SELECT) + SELECT.length(), sql.indexOf(FROM));
+		String select = sql.substring( sql.indexOf( SELECT ) + SELECT.length(), sql.indexOf( FROM ) );
 
 		// Strip the as clauses
-		return stripAliases(select);
+		return stripAliases( select );
 	}
 
 	/**
 	 * Utility method that strips the aliases. See {@link SQLServer2005DialectTestCase#testStripAliases()}
 	 *
-	 * @param a
-	 *            string to replace the as statements
+	 * @param str string to replace the as statements
+	 *
 	 * @return a string without the as statements
 	 */
 	protected static String stripAliases(String str) {
-		return str.replaceAll("\\sas[^,]+(,?)", "$1");
+		return str.replaceAll( "\\sas[^,]+(,?)", "$1" );
 	}
 
 	/**
 	 * Right after the select statement of a given query we must place the row_number function
 	 *
-	 * @param sql
-	 *            the initial sql query without the order by clause
-	 * @param orderby
-	 *            the order by clause of the query
+	 * @param sql the initial sql query without the order by clause
+	 * @param orderby the order by clause of the query
 	 */
 	protected static void insertRowNumberFunction(StringBuilder sql, CharSequence orderby) {
 		// Find the end of the select statement
-		int selectEndIndex = sql.indexOf(SELECT) + SELECT.length();
+		int selectEndIndex = sql.indexOf( SELECT ) + SELECT.length();
 
 		// Insert after the select statement the row_number() function:
-		sql.insert(selectEndIndex, " ROW_NUMBER() OVER (" + orderby + ") as __hibernate_row_nr__,");
+		sql.insert( selectEndIndex, " ROW_NUMBER() OVER (" + orderby + ") as __hibernate_row_nr__," );
 	}
 }

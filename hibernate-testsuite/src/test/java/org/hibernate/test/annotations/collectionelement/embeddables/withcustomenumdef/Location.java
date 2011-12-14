d321c8e280cab591000db60d1a658847f56819fb
Gail Badner <gbadner@redhat.com>
2011-12-13 23:24:00 -0800

    HHH-5937 - @Enumerated(EnumType.STRING) not taken into account in an @Embeddable included in another @Embeddable

---
+++ hibernate-testsuite/src/test/java/org/hibernate/test/annotations/collectionelement/embeddables/withcustomenumdef/Location.java
@@ -0,0 +1,72 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2011, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.test.annotations.collectionelement.embeddables.withcustomenumdef;
+
+import javax.persistence.Embeddable;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+/**
+ * @author Steve Ebersole
+ */
+@Embeddable
+public class Location {
+	public static enum Type {
+		POSTAL_CODE,
+		COMMUNE,
+		REGION,
+		PROVINCE,
+		COUNTY
+	}
+
+	private String name;
+
+	@Enumerated(EnumType.STRING)
+//	@Column(columnDefinition = "VARCHAR(32)")
+	private Type type;
+
+	public Location() {
+	}
+
+	public Location(String name, Type type) {
+		this.name = name;
+		this.type = type;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}

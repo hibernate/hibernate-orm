ea7afb26833f38e4e5f43a56abc9f3c07a85bffa
Gail Badner <gbadner@redhat.com>
2011-12-22 02:24:00 -0800

    HHH-5472 : Added tests for delaying entity save due to  non-nullable transient entities

---
+++ hibernate-core/src/matrix/java/org/hibernate/test/annotations/cascade/multicircle/nonjpa/identity/B.java
@@ -0,0 +1,83 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2011, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.test.annotations.cascade.multicircle.nonjpa.identity;
+
+@javax.persistence.Entity
+public class B extends AbstractEntity {
+    private static final long serialVersionUID = 325417243L;
+
+    @javax.persistence.OneToMany(mappedBy = "b")
+	@org.hibernate.annotations.Cascade({
+			org.hibernate.annotations.CascadeType.PERSIST,
+			org.hibernate.annotations.CascadeType.SAVE_UPDATE,
+			org.hibernate.annotations.CascadeType.MERGE,
+			org.hibernate.annotations.CascadeType.REFRESH
+	})
+    private java.util.Set<G> gCollection = new java.util.HashSet<G>();
+
+
+	@javax.persistence.ManyToOne(optional = false)
+	@org.hibernate.annotations.Cascade({
+			org.hibernate.annotations.CascadeType.PERSIST,
+			org.hibernate.annotations.CascadeType.SAVE_UPDATE,
+			org.hibernate.annotations.CascadeType.MERGE,
+			org.hibernate.annotations.CascadeType.REFRESH
+	})
+	private C c;
+
+	@javax.persistence.ManyToOne(optional = false)
+	@org.hibernate.annotations.Cascade({
+			org.hibernate.annotations.CascadeType.PERSIST,
+			org.hibernate.annotations.CascadeType.SAVE_UPDATE,
+			org.hibernate.annotations.CascadeType.MERGE,
+			org.hibernate.annotations.CascadeType.REFRESH
+	})
+    private D d;
+
+    public java.util.Set<G> getGCollection() {
+        return gCollection;
+    }
+
+    public void setGCollection(
+        java.util.Set<G> parameter) {
+        this.gCollection = parameter;
+    }
+
+    public C getC() {
+        return c;
+    }
+
+    public void setC(C parameter) {
+        this.c = parameter;
+    }
+
+	public D getD() {
+		return d;
+	}
+
+	public void setD(D parameter) {
+		this.d = parameter;
+	}
+
+}
---
+++ hibernate-core/src/matrix/java/org/hibernate/test/annotations/cascade/multicircle/nonjpa/sequence/D.java
@@ -0,0 +1,81 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2011, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.test.annotations.cascade.multicircle.nonjpa.sequence;
+
+/**
+ * No Documentation
+ */
+@javax.persistence.Entity
+public class D extends AbstractEntity {
+    private static final long serialVersionUID = 2417176961L;
+
+	@javax.persistence.OneToMany(mappedBy = "d")
+	private java.util.Set<B> bCollection = new java.util.HashSet<B>();
+
+	@javax.persistence.ManyToOne(optional = false)
+	private C c;
+
+	@javax.persistence.ManyToOne(optional = false)
+	private E e;
+
+    @javax.persistence.OneToMany(mappedBy = "d")
+	@org.hibernate.annotations.Cascade({
+			org.hibernate.annotations.CascadeType.PERSIST,
+			org.hibernate.annotations.CascadeType.SAVE_UPDATE,
+			org.hibernate.annotations.CascadeType.MERGE,
+			org.hibernate.annotations.CascadeType.REFRESH
+	})
+    private java.util.Set<F> fCollection = new java.util.HashSet<F>();
+
+	public java.util.Set<B> getBCollection() {
+		return bCollection;
+	}
+	public void setBCollection(
+			java.util.Set<B> parameter) {
+		this.bCollection = parameter;
+	}
+
+	public C getC() {
+		return c;
+	}
+	public void setC(C c) {
+		this.c = c;
+	}
+
+	public E getE() {
+		return e;
+	}
+	public void setE(E e) {
+		this.e = e;
+	}
+
+    public java.util.Set<F> getFCollection() {
+        return fCollection;
+    }
+    public void setFCollection(
+			java.util.Set<F> parameter) {
+        this.fCollection = parameter;
+    }
+
+}

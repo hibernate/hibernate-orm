6332bab3bb43e83a5afbdfe5253687cde8ec37b9
Gail Badner <gbadner@redhat.com>
2011-12-22 02:24:00 -0800

    HHH-5472 : Added tests for delaying entity save due to  non-nullable transient entities

---
+++ hibernate-core/src/matrix/java/org/hibernate/test/annotations/cascade/multicircle/nonjpa/identity/G.java
@@ -0,0 +1,53 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2011, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.test.annotations.cascade.multicircle.nonjpa.identity;
+
+/**
+ * No Documentation
+ */
+@javax.persistence.Entity
+public class G extends AbstractEntity {
+    private static final long serialVersionUID = 325417437L;
+
+	@javax.persistence.ManyToOne(optional = false)
+	private B b;
+
+    @javax.persistence.OneToMany(mappedBy = "g")
+    private java.util.Set<F> fCollection = new java.util.HashSet<F>();
+
+	public B getB() {
+		return b;
+	}
+	public void setB(B parameter){
+		this.b = parameter;
+	}
+
+    public java.util.Set<F> getFCollection() {
+        return fCollection;
+    }
+    public void setFCollection(
+        java.util.Set<F> parameter) {
+        this.fCollection = parameter;
+    }
+}
---
+++ hibernate-core/src/matrix/java/org/hibernate/test/annotations/cascade/multicircle/nonjpa/sequence/C.java
@@ -0,0 +1,64 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2011, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.test.annotations.cascade.multicircle.nonjpa.sequence;
+
+import java.util.Set;
+
+/**
+ * No Documentation
+ */
+@javax.persistence.Entity
+public class C extends AbstractEntity {
+    private static final long serialVersionUID = 1226955752L;
+
+	@javax.persistence.OneToMany(mappedBy = "c")
+	private Set<B> bCollection = new java.util.HashSet<B>();
+
+	@javax.persistence.OneToMany(mappedBy = "c")
+	@org.hibernate.annotations.Cascade({
+			org.hibernate.annotations.CascadeType.PERSIST,
+			org.hibernate.annotations.CascadeType.SAVE_UPDATE,
+			org.hibernate.annotations.CascadeType.MERGE,
+			org.hibernate.annotations.CascadeType.REFRESH
+	})
+
+	private Set<D> dCollection = new java.util.HashSet<D>();
+
+	public Set<B> getBCollection() {
+		return bCollection;
+	}
+
+	public void setBCollection(Set<B> bCollection) {
+		this.bCollection = bCollection;
+	}
+
+	public Set<D> getDCollection() {
+		return dCollection;
+	}
+
+	public void setDCollection(Set<D> dCollection) {
+		this.dCollection = dCollection;
+	}
+
+}

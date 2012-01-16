600da664035d87dd2cc121d317354dff09753ab3
Hardy Ferentschik <hibernate@ferentschik.de>
2012-01-16 13:09:19 +0100

    METAGEN-65 Updating the configuration of the maven annotation processor plugin

--- tooling/metamodel-generator/src/test/java/org/hibernate/jpamodelgen/test/accesstype/Inhabitant.java
+++ tooling/metamodel-generator/src/test/java/org/hibernate/jpamodelgen/test/accesstype/Inhabitant.java
@@ -14,8 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
 package org.hibernate.jpamodelgen.test.accesstype;
 
 import java.util.Set;

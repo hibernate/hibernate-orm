214e357d133221755ae7b1f6982d22c3cd7c002a
Hardy Ferentschik <hibernate@ferentschik.de>
2012-01-17 14:03:12 +0100

    METAGEN-67 Renaming tests to reflect actual problem

--- tooling/metamodel-generator/src/test/java/org/hibernate/jpamodelgen/test/mappedsuperclasswithoutid/Shop.java
+++ tooling/metamodel-generator/src/test/java/org/hibernate/jpamodelgen/test/mappedsuperclasswithoutid/Shop.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.hibernate.jpamodelgen.test.manytoone;
+package org.hibernate.jpamodelgen.test.mappedsuperclasswithoutid;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;

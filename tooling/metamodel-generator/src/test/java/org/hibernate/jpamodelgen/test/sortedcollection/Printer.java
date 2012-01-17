eb4521b1e9efb7335ce1812da379d5fd944e7717
Hardy Ferentschik <hibernate@ferentschik.de>
2012-01-17 11:43:17 +0100

    METAGEN-62 Package renaming

--- tooling/metamodel-generator/src/test/java/org/hibernate/jpamodelgen/test/sortedcollection/Printer.java
+++ tooling/metamodel-generator/src/test/java/org/hibernate/jpamodelgen/test/sortedcollection/Printer.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.hibernate.jpamodelgen.test.onetomanysorted;
+package org.hibernate.jpamodelgen.test.sortedcollection;
 
 import java.util.SortedMap;
 import java.util.SortedSet;

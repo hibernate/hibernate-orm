c29711f11baf25928febc1eb99814bb243a62686
Lukasz Antoniak <lukasz.antoniak@gmail.com>
2012-02-04 12:27:40 +0100

    HHH-6738 - Alias change

--- hibernate-envers/src/main/java/org/hibernate/envers/entities/mapper/relation/query/QueryConstants.java
+++ hibernate-envers/src/main/java/org/hibernate/envers/entities/mapper/relation/query/QueryConstants.java
@@ -6,14 +6,14 @@ package org.hibernate.envers.entities.mapper.relation.query;
  * @author Lukasz Antoniak (lukasz dot antoniak at gmail dot com)
  */
 public class QueryConstants {
-    public static final String REFERENCED_ENTITY_ALIAS = "e_h";
-    public static final String REFERENCED_ENTITY_ALIAS_DEF_AUD_STR = "e2_h";
+    public static final String REFERENCED_ENTITY_ALIAS = "e__";
+    public static final String REFERENCED_ENTITY_ALIAS_DEF_AUD_STR = "e2__";
 
-    public static final String INDEX_ENTITY_ALIAS = "f_h";
-    public static final String INDEX_ENTITY_ALIAS_DEF_AUD_STR = "f2_h";
+    public static final String INDEX_ENTITY_ALIAS = "f__";
+    public static final String INDEX_ENTITY_ALIAS_DEF_AUD_STR = "f2__";
 
-    public static final String MIDDLE_ENTITY_ALIAS = "ee_h";
-    public static final String MIDDLE_ENTITY_ALIAS_DEF_AUD_STR = "ee2_h";
+    public static final String MIDDLE_ENTITY_ALIAS = "ee__";
+    public static final String MIDDLE_ENTITY_ALIAS_DEF_AUD_STR = "ee2__";
 
     public static final String REVISION_PARAMETER = "revision";
     public static final String DEL_REVISION_TYPE_PARAMETER = "delrevisiontype";

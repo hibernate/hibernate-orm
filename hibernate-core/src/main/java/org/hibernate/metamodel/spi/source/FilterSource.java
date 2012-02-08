b847b0598aba30964ce9860022aa4d20546174c6
Steve Ebersole <steve@hibernate.org>
2012-02-08 13:11:20 -0600

    HHH-7038 - Define sources for filters, filter-defs, type-defs, fetch-profiles

---
+++ hibernate-core/src/main/java/org/hibernate/metamodel/spi/source/FilterSource.java
@@ -0,0 +1,60 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2012, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.metamodel.spi.source;
+
+/**
+ * Defines the source of filter information.  May have an associated {@link FilterDefSource}.
+ * Relates to both {@code <filter/>} and {@link org.hibernate.annotations.Filter @Filter}
+ *
+ * @author Steve Ebersole
+ */
+public interface FilterSource {
+	/**
+	 * Get the name of the filter being described.
+	 *
+	 * @return The name.
+	 */
+	public String getName();
+
+	/**
+	 * Get the condition associated with the filter.  Can be {@code null} in the case of a filter described
+	 * further by a "filter def" which contains the condition text.
+	 *
+	 * @return The condition defined on the filter.
+	 *
+	 * @see {@link org.hibernate.metamodel.spi.source.FilterDefSource#getCondition()}
+	 */
+	public String getCondition();
+
+	/**
+	 * Retrieves the defined sources of parameter information pertaining to this filer.
+	 * <p/>
+	 * Will be <b>merged</b> with parameter sources associated with matching "filter def"
+	 *
+	 * @return The parameter sources defined on the filter.
+	 *
+	 * @see {@link org.hibernate.metamodel.spi.source.FilterDefSource#getParameterSources()}
+	 */
+	public Iterable<FilterParameterSource> getParameterSources();
+}

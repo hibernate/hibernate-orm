209abeafa8a47ae6f30b98b0f4102a7ba01778b2
Steve Ebersole <steve@hibernate.org>
2012-02-09 09:44:19 -0600

    HHH-7047 - Fix problems created by HHH-7044

--- hibernate-core/src/main/java/org/hibernate/dialect/SQLServer2005Dialect.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/SQLServer2005Dialect.java
@@ -147,7 +147,7 @@ public class SQLServer2005Dialect extends SQLServerDialect {
 	 *
 	 * @param sql an sql query
 	 */
-	protected void replaceDistinctWithGroupBy(StringBuilder sql) {
+	protected static void replaceDistinctWithGroupBy(StringBuilder sql) {
 		int distinctIndex = sql.indexOf( DISTINCT );
 		int selectEndIndex = sql.indexOf( FROM );
 		if (distinctIndex > 0 && distinctIndex < selectEndIndex) {
@@ -164,7 +164,7 @@ public class SQLServer2005Dialect extends SQLServerDialect {
 	 *
 	 * @return the fields of the select statement without their alias
 	 */
-	protected CharSequence getSelectFieldsWithoutAliases(StringBuilder sql) {
+	protected static CharSequence getSelectFieldsWithoutAliases(StringBuilder sql) {
 		String select = sql.substring( sql.indexOf( SELECT ) + SELECT.length(), sql.indexOf( FROM ) );
 
 		// Strip the as clauses
@@ -178,7 +178,7 @@ public class SQLServer2005Dialect extends SQLServerDialect {
 	 *
 	 * @return a string without the as statements
 	 */
-	protected String stripAliases(String str) {
+	protected static String stripAliases(String str) {
 		Matcher matcher = ALIAS_PATTERN.matcher( str );
 		return matcher.replaceAll( "$1" );
 	}

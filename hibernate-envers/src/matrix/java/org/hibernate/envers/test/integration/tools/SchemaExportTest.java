7c60f01f2d5e5dd40d4f2edb0ef3339c803a7560
Lukasz Antoniak <lukasz.antoniak@gmail.com>
2012-03-03 10:27:35 +0100

    HHH-7106 - Fix, test and documentation

---
+++ hibernate-envers/src/matrix/java/org/hibernate/envers/test/integration/tools/SchemaExportTest.java
@@ -0,0 +1,52 @@
+package org.hibernate.envers.test.integration.tools;
+
+import org.hibernate.MappingException;
+import org.hibernate.Session;
+import org.hibernate.cfg.Environment;
+import org.hibernate.envers.test.AbstractSessionTest;
+import org.hibernate.envers.test.Priority;
+import org.hibernate.envers.test.entities.StrTestEntity;
+import org.hibernate.testing.TestForIssue;
+import org.hibernate.tool.EnversSchemaGenerator;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.net.URISyntaxException;
+import java.util.Arrays;
+
+/**
+ * @author Lukasz Antoniak (lukasz dot antoniak at gmail dot com)
+ */
+@TestForIssue(jiraKey = "HHH-7106")
+public class SchemaExportTest extends AbstractSessionTest {
+    private Integer id = null;
+
+    @Override
+    protected void initMappings() throws MappingException, URISyntaxException {
+        config.addAnnotatedClass(StrTestEntity.class);
+        // Disable schema auto generation.
+        config.setProperty(Environment.HBM2DDL_AUTO, "");
+    }
+
+    @Test
+    @Priority(10)
+    public void testSchemaCreation() {
+        // Generate complete schema.
+        new EnversSchemaGenerator(config).export().create(true, true);
+
+        // Populate database with test data.
+        Session session = getSession();
+        session.getTransaction().begin();
+        StrTestEntity entity = new StrTestEntity("data");
+        session.save(entity);
+        session.getTransaction().commit();
+
+        id = entity.getId();
+    }
+
+    @Test
+    public void testAuditDataRetrieval() {
+        Assert.assertEquals(Arrays.asList(1), getAuditReader().getRevisions(StrTestEntity.class, id));
+        Assert.assertEquals(new StrTestEntity("data", id), getAuditReader().find(StrTestEntity.class, id, 1));
+    }
+}

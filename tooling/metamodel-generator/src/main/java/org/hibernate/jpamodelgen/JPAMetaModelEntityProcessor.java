bf27eb0180663666a9de8fbf727fbef6ea0d9e43
Guillaume Smet <guillaume.smet@gmail.com>
2012-03-11 02:29:19 +0100

    METAGEN-82 Fixed a few inconsistencies in the way @MappedSuperClass are considered

--- tooling/metamodel-generator/src/main/java/org/hibernate/jpamodelgen/JPAMetaModelEntityProcessor.java
+++ tooling/metamodel-generator/src/main/java/org/hibernate/jpamodelgen/JPAMetaModelEntityProcessor.java
@@ -259,11 +259,11 @@ public class JPAMetaModelEntityProcessor extends AbstractProcessor {
 
 	private MetaEntity tryGettingExistingEntityFromContext(AnnotationMirror mirror, String fqn) {
 		MetaEntity alreadyExistingMetaEntity = null;
-		if ( TypeUtils.isAnnotationMirrorOfType( mirror, Constants.ENTITY ) ) {
+		if ( TypeUtils.isAnnotationMirrorOfType( mirror, Constants.ENTITY )
+				|| TypeUtils.isAnnotationMirrorOfType( mirror, Constants.MAPPED_SUPERCLASS )) {
 			alreadyExistingMetaEntity = context.getMetaEntity( fqn );
 		}
-		else if ( TypeUtils.isAnnotationMirrorOfType( mirror, Constants.MAPPED_SUPERCLASS )
-				|| TypeUtils.isAnnotationMirrorOfType( mirror, Constants.EMBEDDABLE ) ) {
+		else if ( TypeUtils.isAnnotationMirrorOfType( mirror, Constants.EMBEDDABLE ) ) {
 			alreadyExistingMetaEntity = context.getMetaEmbeddable( fqn );
 		}
 		return alreadyExistingMetaEntity;

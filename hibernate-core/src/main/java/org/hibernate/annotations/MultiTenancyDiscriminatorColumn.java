556ca03e210b5d2c14b9414a8deb5e96526c0e53
root <root@apollo.(none)>
2012-03-12 11:24:34 -0500

    HHH-6054 - Support for discriminator-based multi-tenancy

---
+++ hibernate-core/src/main/java/org/hibernate/annotations/MultiTenancyDiscriminatorColumn.java
@@ -0,0 +1,86 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2012, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.annotations;
+
+import java.lang.annotation.Retention;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Describes the column to use as the multi-tenancy discriminator value for the entity.
+ *
+ * NOTE : For sure this will go away and get replaced with the JPA standard one..
+ *
+ * @todo better (shorter) name?  TenantDiscriminatorColumn?  TenantColumn?  Similar for formula too
+ *
+ * @author Steve Ebersole
+ */
+@java.lang.annotation.Target( TYPE )
+@Retention( RUNTIME )
+public @interface MultiTenancyDiscriminatorColumn {
+	/**
+	 * Names of the column to use.
+	 */
+	public String name();
+
+	/**
+	 * (Optional) The name of the table that contains the column. If absent the column is assumed to be in the 
+	 * primary table.
+	 */
+	public String table() default "";
+
+	/**
+	 * The discriminator values can be either be handled as literals or handled through JDBC parameter binding.
+	 * {@code true} here (the default) indicates that the parameter binding approach should be used; {@code false}
+	 * indicates the value should be handled as a literal.
+	 * <p/>
+	 * Care should be used specifying to use literals here.  PreparedStatements will not be able to be reused
+	 * nearly as often by the database/driver which can potentially cause a significant performance impact to your
+	 * application.
+	 */
+	public boolean useParameterBinding() default true;
+
+	/**
+	 * Names the Hibernate mapping type to use for mapping values to/from the specified column.  Defaults to
+	 * {@code "string"} which is a {@link String}/{@link java.sql.Types#VARCHAR VARCHAR} mapping.
+	 */
+	public String type() default "string";
+
+	/**
+	 * (Optional) The column length. (Applies only if a string-valued column is used.)
+	 */
+	int length() default 255;
+
+	/**
+	 * (Optional) The precision for a decimal (exact numeric) column. (Applies only if a decimal column is used.)
+	 * Value must be set by developer if used when generating the DDL for the column.
+	 */
+	int precision() default 0;
+
+	/**
+	 * (Optional) The scale for a decimal (exact numeric) column. (Applies only if a decimal column is used.)
+	 */
+	int scale() default 0;
+}

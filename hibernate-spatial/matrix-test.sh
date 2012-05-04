fd3311b3ab89f974b1c0ffdb3283144802d48c0d
Karel Maesen <karel@geovise.com>
2012-05-04 21:24:12 +0200

    Updated Oracle JDBC driver.

--- hibernate-spatial/matrix-test.sh
+++ hibernate-spatial/matrix-test.sh
@@ -5,4 +5,4 @@ if [[ -n $@ ]]; then
   TASK="$@"
 fi
 echo "TASK : $TASK"
-../gradlew -Dhibernate-matrix-databases="/home/maesenka/workspaces/github/hibernate-core/hibernate-spatial/databases" -Dhibernate-matrix-ignore='mysql50,mysql51,postgresql82,postgresql83,postgresql84' $TASK
+../gradlew -Dhibernate-matrix-databases="/home/maesenka/workspaces/github/hibernate-core-metamodel/hibernate-spatial/databases" -Dhibernate-matrix-ignore='mysql50,mysql51,postgresql82,postgresql83,postgresql84' $TASK

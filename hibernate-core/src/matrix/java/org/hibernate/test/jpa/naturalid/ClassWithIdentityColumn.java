48cd9d3f7d45ca07ff7412212d303082f874c133
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2012-05-09 18:07:27 +0200

    HHH-7304 new testcase entity class

---
+++ hibernate-core/src/matrix/java/org/hibernate/test/jpa/naturalid/ClassWithIdentityColumn.java
@@ -0,0 +1,27 @@
+package org.hibernate.test.jpa.naturalid;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.NaturalId;
+
+@Entity
+public class ClassWithIdentityColumn {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NaturalId(mutable = true)
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}

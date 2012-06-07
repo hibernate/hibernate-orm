b0bdda114141c799bd071a4eae6ec25f423571ec
Steve Ebersole <steve@hibernate.org>
2012-06-07 14:12:03 -0500

    consolidate src/test and src/matrix

---
+++ hibernate-envers/src/test/java/org/hibernate/envers/test/integration/entityNames/oneToManyNotAudited/Car.java
@@ -0,0 +1,57 @@
+package org.hibernate.envers.test.integration.entityNames.oneToManyNotAudited;
+import java.util.List;
+
+import org.hibernate.envers.Audited;
+import org.hibernate.envers.RelationTargetAuditMode;
+
+/**
+ * @author Hern&aacute;n Chanfreau
+ * 
+ */
+
+public class Car {
+	
+	private long id;
+	
+	private int number;
+	
+	private List<Person> owners;
+
+	
+	public Car() { }
+
+	public Car(int number, List<Person> owners) {
+		this.number = number;
+		this.owners = owners;
+	}
+
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	@Audited(targetAuditMode=RelationTargetAuditMode.NOT_AUDITED)
+	public List<Person> getOwners() {
+		return owners;
+	}
+
+	public void setOwners(List<Person> owners) {
+		this.owners = owners;
+	}	
+
+	@Audited
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+
+
+}
---
+++ hibernate-envers/src/test/java/org/hibernate/envers/test/integration/entityNames/singleAssociatedNotAudited/Person.java
@@ -0,0 +1,49 @@
+package org.hibernate.envers.test.integration.entityNames.singleAssociatedNotAudited;
+
+
+/**
+ * @author Hern&aacute;n Chanfreau
+ * 
+ */
+
+public class Person {
+	
+	private long id;
+	
+	private String name;
+	
+	private int age;
+	
+	public Person(){ }
+	
+	public Person(String name, int age){
+		this.name = name;
+		this.age = age;
+	}
+	
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}

0bd42556e336f1a4b9c4cde760bb9ab46ee6a672
Rob Worsnop <rworsnop@gmail.com>
2012-06-28 10:16:29 -0400

    HHH-2394 Allowed usage of {alias} marker.

--- hibernate-core/src/main/java/org/hibernate/internal/FilterHelper.java
+++ hibernate-core/src/main/java/org/hibernate/internal/FilterHelper.java
@@ -123,10 +123,12 @@ public class FilterHelper {
 	
 	private String render(FilterAliasGenerator aliasGenerator, int filterIndex){
 		Map<String,String> aliasTableMap = filterAliasTableMaps[filterIndex];
+		String condition = filterConditions[filterIndex];
 		if (filterAutoAliasFlags[filterIndex]){
-			return StringHelper.replace(filterConditions[filterIndex], FilterImpl.MARKER, aliasGenerator.getAlias(aliasTableMap.get(null)));
-		} else{
-			String condition = filterConditions[filterIndex];
+			return StringHelper.replace(condition, FilterImpl.MARKER, aliasGenerator.getAlias(aliasTableMap.get(null)));
+		} else if (isTableFromPersistentClass(aliasTableMap)){
+			return condition.replace("{alias}", aliasGenerator.getAlias(aliasTableMap.get(null)));
+		} else {
 			for (Map.Entry<String, String> entry : aliasTableMap.entrySet()){
 				condition = condition.replace("{"+entry.getKey()+"}", aliasGenerator.getAlias(entry.getValue()));
 			}

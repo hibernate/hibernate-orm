65d1724433cd30510908b24dd960cc3af7ce7070
Strong Liu <stliu@hibernate.org>
2012-07-11 01:21:46 +0800

    HHH-7310 Resolution of types registered in type registry does not work properly for Properties of @Embeddable types

---
+++ hibernate-core/src/test/java/org/hibernate/test/annotations/embeddables/InvestorIntegrator.java
@@ -0,0 +1,57 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2012, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.test.annotations.embeddables;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.integrator.spi.Integrator;
+import org.hibernate.metamodel.source.MetadataImplementor;
+import org.hibernate.service.spi.SessionFactoryServiceRegistry;
+
+/**
+ * @author Chris Pheby
+ */
+public class InvestorIntegrator implements Integrator {
+
+	@Override
+	public void integrate(Configuration configuration,
+			SessionFactoryImplementor sessionFactory,
+			SessionFactoryServiceRegistry serviceRegistry) {
+		configuration.registerTypeOverride(new DollarValueUserType(), new String[] {DollarValue.class.getName()});
+		configuration.registerTypeOverride(new MyDateUserType(), new String[] {MyDate.class.getName()});
+	}
+
+	@Override
+	public void integrate(MetadataImplementor metadata,
+			SessionFactoryImplementor sessionFactory,
+			SessionFactoryServiceRegistry serviceRegistry) {
+		// No-op
+	}
+
+	@Override
+	public void disintegrate(SessionFactoryImplementor sessionFactory,
+			SessionFactoryServiceRegistry serviceRegistry) {		
+	}
+
+}

b76e7c49870cf34eaecbd63079b17bb5c6dbd537
Scott Marlow <smarlow@redhat.com>
2012-07-18 10:59:42 -0400

    HHH-7454 added DEBUG log message 'PersistenceUnitUtil#isLoaded is not always accurate; consider using EntityManager#contains instead'

--- hibernate-entitymanager/src/main/java/org/hibernate/ejb/EntityManagerFactoryImpl.java
+++ hibernate-entitymanager/src/main/java/org/hibernate/ejb/EntityManagerFactoryImpl.java
@@ -293,6 +293,8 @@ public class EntityManagerFactoryImpl implements HibernateEntityManagerFactory {
 		}
 
 		public boolean isLoaded(Object entity, String attributeName) {
+			// added log message to help with HHH-7454, if state == LoadState,NOT_LOADED, returning true or false is not accurate.
+			log.debug("PersistenceUnitUtil#isLoaded is not always accurate; consider using EntityManager#contains instead");
 			LoadState state = PersistenceUtilHelper.isLoadedWithoutReference( entity, attributeName, cache );
 			if (state == LoadState.LOADED) {
 				return true;
@@ -306,6 +308,8 @@ public class EntityManagerFactoryImpl implements HibernateEntityManagerFactory {
 		}
 
 		public boolean isLoaded(Object entity) {
+			// added log message to help with HHH-7454, if state == LoadState,NOT_LOADED, returning true or false is not accurate.
+			log.debug("PersistenceUnitUtil#isLoaded is not always accurate; consider using EntityManager#contains instead");
 			return PersistenceUtilHelper.isLoaded( entity ) != LoadState.NOT_LOADED;
 		}
 

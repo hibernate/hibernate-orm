1ec1254babb900798cb118ca28185b58ec93b055
Tair Sabirgaliev <tair.sabirgaliev@gmail.com>
2012-07-22 02:13:54 +0600

    Test: order of <mapping .../> affects caching configuration

---
+++ hibernate-core/src/test/java/org/hibernate/test/cfg/cache/BaseClass.java
@@ -0,0 +1,23 @@
+package org.hibernate.test.cfg.cache;
+
+public class BaseClass {
+	Long id;
+	String value;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+}

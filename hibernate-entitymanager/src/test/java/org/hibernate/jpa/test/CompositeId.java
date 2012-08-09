7975dead95308c582be70c34d8b46077a17f9fc4
Eskil Bylund <eskil@vidispine.com>
2012-08-09 12:53:38 +0200

    Fix missing parenthesis around composite properties in WHERE clauses
    The currentClauseType field would not be correct if the query contained a
    subquery.
    
    Signed-off-by: Andrea Boriero <andrea@hibernate.org>

--- hibernate-entitymanager/src/test/java/org/hibernate/jpa/test/CompositeId.java
+++ hibernate-entitymanager/src/test/java/org/hibernate/jpa/test/CompositeId.java
@@ -18,6 +18,14 @@ public class CompositeId implements Serializable {
     private int id1;
     private int id2;
 
+    public CompositeId() {
+    }
+
+    public CompositeId(int id1, int id2) {
+        this.id1 = id1;
+        this.id2 = id2;
+    }
+
     public int getId1() {
         return id1;
     }

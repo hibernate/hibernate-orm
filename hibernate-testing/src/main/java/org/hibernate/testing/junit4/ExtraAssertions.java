ca3cc50a2fcc050ad67186afa57d68f76a854165
Steve Ebersole <steve@hibernate.org>
2012-09-03 22:06:23 -0500

    HHH-5188 - Use provided enum type ordinal/string when reading (cherry picked from commit 076eccb7a21d8cdbc820a5d089645fabe9d6b933)
    Conflicts:
    
    	hibernate-core/src/main/java/org/hibernate/type/EnumType.java
    	hibernate-testing/src/main/java/org/hibernate/testing/junit4/ExtraAssertions.java

--- hibernate-testing/src/main/java/org/hibernate/testing/junit4/ExtraAssertions.java
+++ hibernate-testing/src/main/java/org/hibernate/testing/junit4/ExtraAssertions.java
@@ -23,7 +23,15 @@
  */
 package org.hibernate.testing.junit4;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.sql.Types;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Assert;
+
+import static org.junit.Assert.fail;
 
 /**
  * @author Steve Ebersole
@@ -36,5 +44,59 @@ public class ExtraAssertions {
 							actual.getName() + "]"
 			);
 		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <T> T assertTyping(Class<T> expectedType, Object value) {
+		if ( ! expectedType.isInstance( value ) ) {
+			Assert.fail(
+					String.format(
+							"Expecting value of type [%s], but found [%s]",
+							expectedType.getName(),
+							value == null ? "<null>" : value
+					)
+			);
+		}
+		return (T) value;
+	}
+
+	public static void assertJdbcTypeCode(int expected, int actual) {
+		if ( expected != actual ) {
+			final String message = String.format(
+					"JDBC type codes did not match...\n" +
+							"Expected: %s (%s)\n" +
+							"Actual  : %s (%s)",
+					jdbcTypeCodeMap().get( expected ),
+					expected,
+					jdbcTypeCodeMap().get( actual ),
+					actual
+			);
+			fail( message );
+		}
+	}
+
+	private static Map<Integer,String> jdbcTypeCodeMap;
+
+	private static synchronized Map<Integer,String> jdbcTypeCodeMap() {
+		if ( jdbcTypeCodeMap == null ) {
+			jdbcTypeCodeMap = generateJdbcTypeCache();
+		}
+		return jdbcTypeCodeMap;
+	}
+
+	private static Map generateJdbcTypeCache() {
+		final Field[] fields = Types.class.getFields();
+		Map cache = new HashMap( (int)( fields.length * .75 ) + 1 );
+		for ( int i = 0; i < fields.length; i++ ) {
+			final Field field = fields[i];
+			if ( Modifier.isStatic( field.getModifiers() ) ) {
+				try {
+					cache.put( field.get( null ), field.getName() );
+				}
+				catch ( Throwable ignore ) {
+				}
+			}
+		}
+		return cache;
 	}
 }

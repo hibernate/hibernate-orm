8dbaefced41dfa17aa7681dcd0ae002ec039e1a1
brmeyer <brmeyer@redhat.com>
2012-09-26 17:33:54 -0400

    HHH-6972 Support PostgreSQL and Oracle LOBs

--- hibernate-core/src/main/java/org/hibernate/dialect/PostgreSQL81Dialect.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/PostgreSQL81Dialect.java
@@ -155,6 +155,33 @@ public class PostgreSQL81Dialect extends Dialect {
 		getDefaultProperties().setProperty( Environment.NON_CONTEXTUAL_LOB_CREATION, "true" );
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public SqlTypeDescriptor getSqlTypeDescriptorOverride(int sqlCode) {
+		SqlTypeDescriptor descriptor;
+		switch ( sqlCode ) {
+			case Types.BLOB: {
+				// Force BLOB binding.  Otherwise, byte[] fields annotated
+				// with @Lob will attempt to use
+				// BlobTypeDescriptor.PRIMITIVE_ARRAY_BINDING.  Since the
+				// dialect uses oid for Blobs, byte arrays cannot be used.
+				descriptor = BlobTypeDescriptor.BLOB_BINDING;
+				break;
+			}
+			case Types.CLOB: {
+				descriptor = ClobTypeDescriptor.CLOB_BINDING;
+				break;
+			}
+			default: {
+				descriptor = super.getSqlTypeDescriptorOverride( sqlCode );
+				break;
+			}
+		}
+		return descriptor;
+	}
+
 	public String getAddColumnString() {
 		return "add column";
 	}

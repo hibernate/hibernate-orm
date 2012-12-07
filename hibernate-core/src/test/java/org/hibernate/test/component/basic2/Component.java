02825e9fd66be189fd94a9fa1f40c9d852cfc6ae
mgrenonville <mgrenonville@excilys.com>
2012-12-07 17:40:51 +0100

    HHH-7849 Unable to join on an embedded field

---
+++ hibernate-core/src/test/java/org/hibernate/test/component/basic2/Component.java
@@ -0,0 +1,39 @@
+package org.hibernate.test.component.basic2;
+
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * {@inheritDoc}
+ *
+ * @author Mathieu Grenonville
+ */
+@Entity
+public class Component {
+
+    @Id
+	private Long id;
+    @Embedded
+	private Component.Emb emb;
+
+	public Component() {
+	}
+
+    @Access(AccessType.FIELD)
+    @Embeddable
+    public static class Emb {
+
+        @OneToMany(targetEntity = Stuff.class)
+        Set<Stuff> stuffs = new HashSet<Stuff>();
+
+        @Entity
+        public static class Stuff {
+            @Id
+            private Long id;
+        }
+    }
+
+
+}

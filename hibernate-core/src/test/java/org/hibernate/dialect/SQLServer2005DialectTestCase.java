03520b928f1c69dbf128d083f31b13dc03f7d314
Lukasz Antoniak <lukasz.antoniak@gmail.com>
2012-12-31 12:24:39 +0100

    HHH-7781 - Fix and test

--- hibernate-core/src/test/java/org/hibernate/dialect/SQLServer2005DialectTestCase.java
+++ hibernate-core/src/test/java/org/hibernate/dialect/SQLServer2005DialectTestCase.java
@@ -153,6 +153,21 @@ public class SQLServer2005DialectTestCase extends BaseUnitTestCase {
 		);
 	}
 
+	@Test
+	@TestForIssue(jiraKey = "HHH-7781")
+	public void testGetLimitStringWithCastOperator() {
+		final String query = "select cast(lc302_doku6_.redniBrojStavke as varchar(255)) as col_0_0_, lc302_doku6_.dokumentiID as col_1_0_ " +
+				"from LC302_Dokumenti lc302_doku6_ order by lc302_doku6_.dokumentiID DESC";
+
+		assertEquals(
+				"WITH query AS (SELECT inner_query.*, ROW_NUMBER() OVER (ORDER BY CURRENT_TIMESTAMP) as __hibernate_row_nr__ FROM ( " +
+					"select TOP(?) cast(lc302_doku6_.redniBrojStavke as varchar(255)) as col_0_0_, lc302_doku6_.dokumentiID as col_1_0_ " +
+					"from LC302_Dokumenti lc302_doku6_ order by lc302_doku6_.dokumentiID DESC ) inner_query ) " +
+					"SELECT col_0_0_, col_1_0_ FROM query WHERE __hibernate_row_nr__ >= ? AND __hibernate_row_nr__ < ?",
+				dialect.buildLimitHandler( query, toRowSelection( 1, 3 ) ).getProcessedSql()
+		);
+	}
+
 	private RowSelection toRowSelection(int firstRow, int maxRows) {
 		RowSelection selection = new RowSelection();
 		selection.setFirstRow( firstRow );

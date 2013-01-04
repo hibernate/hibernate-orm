7b9b9b39c0aa2deec0c80c018bc1967b4a511a0b
Brett Meyer <brmeyer@redhat.com>
2013-01-04 14:50:50 -0500

    HHH-2448 Generate identical column aliases among cluster

--- hibernate-core/src/main/java/org/hibernate/mapping/Table.java
+++ hibernate-core/src/main/java/org/hibernate/mapping/Table.java
@@ -57,11 +57,10 @@ public class Table implements RelationalModel, Serializable {
 	private Map indexes = new LinkedHashMap();
 	private Map foreignKeys = new LinkedHashMap();
 	private Map<String,UniqueKey> uniqueKeys = new LinkedHashMap<String,UniqueKey>();
-	private final int uniqueInteger;
+	private int uniqueInteger;
 	private boolean quoted;
 	private boolean schemaQuoted;
 	private boolean catalogQuoted;
-	private static int tableCounter = 0;
 	private List checkConstraints = new ArrayList();
 	private String rowId;
 	private String subselect;
@@ -99,9 +98,7 @@ public class Table implements RelationalModel, Serializable {
 		}
 	}
 
-	public Table() {
-		uniqueInteger = tableCounter++;
-	}
+	public Table() { }
 
 	public Table(String name) {
 		this();
@@ -734,6 +731,12 @@ public class Table implements RelationalModel, Serializable {
 		else {
 			this.catalog = catalog;
 		}
+	}
+
+	// This must be done outside of Table, rather than statically, to ensure
+	// deterministic alias names.  See HHH-2448.
+	public void setUniqueInteger( int uniqueInteger ) {
+		this.uniqueInteger = uniqueInteger;
 	}
 
 	public int getUniqueInteger() {

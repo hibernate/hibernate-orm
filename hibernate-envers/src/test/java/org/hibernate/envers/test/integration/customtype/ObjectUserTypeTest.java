2fc81e9d98872f2e051a09a4df036ae6a311ab3f
Lukasz Antoniak <lukasz.antoniak@gmail.com>
2013-01-10 14:38:11 +0100

    HHH-7870 - Fix and test (cherry picked from commit 0713cea18098e6fdec50dfffb09b324bbc900571)

---
+++ hibernate-envers/src/test/java/org/hibernate/envers/test/integration/customtype/ObjectUserTypeTest.java
@@ -0,0 +1,84 @@
+package org.hibernate.envers.test.integration.customtype;
+
+import java.util.Arrays;
+import java.util.Map;
+import javax.persistence.EntityManager;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.hibernate.dialect.Oracle8iDialect;
+import org.hibernate.envers.test.BaseEnversJPAFunctionalTestCase;
+import org.hibernate.envers.test.Priority;
+import org.hibernate.testing.RequiresDialect;
+import org.hibernate.testing.TestForIssue;
+
+/**
+ * @author Lukasz Antoniak (lukasz dot antoniak at gmail dot com)
+ */
+@TestForIssue(jiraKey = "HHH-7870")
+@RequiresDialect(Oracle8iDialect.class)
+public class ObjectUserTypeTest extends BaseEnversJPAFunctionalTestCase {
+	private int id;
+
+	@Override
+	protected Class<?>[] getAnnotatedClasses() {
+		return new Class<?>[] { ObjectUserTypeEntity.class };
+	}
+
+	@Override
+	protected void addConfigOptions(Map options) {
+		super.addConfigOptions( options );
+		options.put( "org.hibernate.envers.store_data_at_delete", "true" );
+	}
+
+	@Test
+	@Priority(10)
+	public void initData() {
+		EntityManager em = getEntityManager();
+
+		// Revision 1 - add
+		em.getTransaction().begin();
+		ObjectUserTypeEntity entity = new ObjectUserTypeEntity( "builtInType1", "stringUserType1" );
+		em.persist( entity );
+		em.getTransaction().commit();
+
+		id = entity.getId();
+
+		// Revision 2 - modify
+		em.getTransaction().begin();
+		entity = em.find( ObjectUserTypeEntity.class, entity.getId() );
+		entity.setUserType( 2 );
+		entity = em.merge( entity );
+		em.getTransaction().commit();
+
+		// Revision 3 - remove
+		em.getTransaction().begin();
+		entity = em.find( ObjectUserTypeEntity.class, entity.getId() );
+		em.remove( entity );
+		em.getTransaction().commit();
+
+		em.close();
+	}
+
+	@Test
+	public void testRevisionCount() {
+		Assert.assertEquals(
+				Arrays.asList( 1, 2, 3 ),
+				getAuditReader().getRevisions( ObjectUserTypeEntity.class, id )
+		);
+	}
+
+	@Test
+	public void testHistory() {
+		ObjectUserTypeEntity ver1 = new ObjectUserTypeEntity( id, "builtInType1", "stringUserType1" );
+		ObjectUserTypeEntity ver2 = new ObjectUserTypeEntity( id, "builtInType1", 2 );
+
+		Assert.assertEquals( ver1, getAuditReader().find( ObjectUserTypeEntity.class, id, 1 ) );
+		Assert.assertEquals( ver2, getAuditReader().find( ObjectUserTypeEntity.class, id, 2 ) );
+		Assert.assertEquals(
+				ver2,
+				getAuditReader().createQuery().forRevisionsOfEntity( ObjectUserTypeEntity.class, true, true ).getResultList().get( 2 )
+		); // Checking delete state.
+	}
+}

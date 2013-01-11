ecf0114a63ef3f74165195fdc8a2b217f5e993e9
Strong Liu <stliu@hibernate.org>
2013-01-12 04:29:17 +0800

    HHH-7916 @NotFound.IGNORE

--- hibernate-core/src/test/java/org/hibernate/test/unconstrained/UnconstrainedTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/unconstrained/UnconstrainedTest.java
@@ -30,7 +30,6 @@ import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
-import org.hibernate.testing.FailureExpectedWithNewMetamodel;
 import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
 
 import static org.junit.Assert.assertNotNull;
@@ -40,7 +39,6 @@ import static org.junit.Assert.assertTrue;
 /**
  * @author Gavin King
  */
-@FailureExpectedWithNewMetamodel
 public class UnconstrainedTest extends BaseCoreFunctionalTestCase {
 	@Override
 	public String[] getMappings() {

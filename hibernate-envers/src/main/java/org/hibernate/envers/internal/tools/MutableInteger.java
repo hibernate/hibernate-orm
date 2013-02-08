6be8b4aad03e42f8758df217546010371ec3ee23
Lukasz Antoniak <lukasz.antoniak@gmail.com>
2013-02-08 11:15:43 +0100

    HHH-7478 - Fix and test

--- hibernate-envers/src/main/java/org/hibernate/envers/internal/tools/MutableInteger.java
+++ hibernate-envers/src/main/java/org/hibernate/envers/internal/tools/MutableInteger.java
@@ -23,17 +23,32 @@
  */
 package org.hibernate.envers.internal.tools;
 
-
 /**
  * @author Adam Warski (adam at warski dot org)
  */
 public class MutableInteger {
-    private int value;
+    private int value = 0;
 
     public MutableInteger() {
     }
 
+    public MutableInteger(int value) {
+        this.value = value;
+    }
+
     public int getAndIncrease() {
         return value++;
+    }
+
+    public int get() {
+        return value;
+    }
+
+    public void set(int value) {
+        this.value = value;
+    }
+
+    public void increase() {
+        ++value;
     }
 }

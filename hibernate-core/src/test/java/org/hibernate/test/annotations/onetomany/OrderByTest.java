f41434296b1a1d34e971cffa1f08591d72e9f1ea
Lukasz Antoniak <lukasz.antoniak@gmail.com>
2013-02-19 17:37:54 -0500

    HHH-7608 - Fix and test
    Conflicts:
    	hibernate-core/src/test/java/org/hibernate/test/annotations/onetomany/OrderByTest.java

--- hibernate-core/src/test/java/org/hibernate/test/annotations/onetomany/OrderByTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/annotations/onetomany/OrderByTest.java
@@ -23,17 +23,29 @@
  */
 package org.hibernate.test.annotations.onetomany;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
 
 import org.hibernate.Session;
+import org.hibernate.dialect.H2Dialect;
+import org.hibernate.dialect.Oracle8iDialect;
+import org.hibernate.testing.RequiresDialect;
+import org.hibernate.testing.TestForIssue;
 import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
-
-import static org.junit.Assert.assertEquals;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * @author Emmanuel Bernard
  */
 public class OrderByTest extends BaseCoreFunctionalTestCase {
+
+	@Override
+	protected Class[] getAnnotatedClasses() {
+		return new Class[] { Order.class, OrderItem.class, Box.class, Item.class };
+	}
+		
 	@Test
 	public void testOrderByOnIdClassProperties() throws Exception {
 		Session s = openSession( );
@@ -70,8 +82,45 @@ public class OrderByTest extends BaseCoreFunctionalTestCase {
 		s.close();
 	}
 
-	@Override
-	protected Class[] getAnnotatedClasses() {
-		return new Class[] { Order.class, OrderItem.class };
+	@Test
+	@TestForIssue( jiraKey = "HHH-7608" )
+	@RequiresDialect({ H2Dialect.class, Oracle8iDialect.class })
+	public void testOrderByReferencingFormulaColumn() {
+		Session session = openSession();
+
+		// Populating database with test data.
+		session.getTransaction().begin();
+		Box box1 = new Box( 1 );
+		Item item1 = new Item( 1, "1", box1 );
+		Item item2 = new Item( 2, "22", box1 );
+		Item item3 = new Item( 3, "2", box1 );
+		session.persist( box1 );
+		session.persist( item1 );
+		session.persist( item2 );
+		session.persist( item3 );
+		session.flush();
+		session.refresh( item1 );
+		session.refresh( item2 );
+		session.refresh( item3 );
+		session.getTransaction().commit();
+
+		session.clear();
+
+		session.getTransaction().begin();
+		box1 = (Box) session.get( Box.class, box1.getId() );
+		Assert.assertEquals( Arrays.asList( item2, item1, item3 ), box1.getItems() );
+		session.getTransaction().commit();
+
+		session.clear();
+
+		// Cleanup data.
+		session.getTransaction().begin();
+		session.delete( item1 );
+		session.delete( item2 );
+		session.delete( item3 );
+		session.delete( box1 );
+		session.getTransaction().commit();
+
+		session.close();
 	}
 }

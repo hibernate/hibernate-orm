09620e3cae72cdf2b80dc9a7ed9d52ee9bba8e22
Steve Ebersole <steve@hibernate.org>
2013-02-22 12:51:37 -0600

    HHH-7990 - Bootstrapping Hibernate fails if javax.validation API is on classpath but no provider

--- hibernate-core/src/main/java/org/hibernate/internal/CoreMessageLogger.java
+++ hibernate-core/src/main/java/org/hibernate/internal/CoreMessageLogger.java
@@ -1609,6 +1609,8 @@ public interface CoreMessageLogger extends BasicLogger {
 	)
 	void embedXmlAttributesNoLongerSupported();
 
+	// id=447 is used in 4.3 code
+
 	@LogMessage(level = INFO)
 	@Message( value = "'javax.persistence.validation.mode' named multiple values : %s", id = 447 )
 	void multipleValidationModes(String modes);

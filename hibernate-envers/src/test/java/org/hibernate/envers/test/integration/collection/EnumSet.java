75681bcee2574f7214349358a86ce2da00863bd4
Lukasz Antoniak <lukasz.antoniak@gmail.com>
2013-03-13 20:43:14 +0100

    HHH-8061 - Fixing EnumSet test on Oracle (cherry picked from commit 11287c2e02414ea415779c6445927aa244af8f7d)

--- hibernate-envers/src/test/java/org/hibernate/envers/test/integration/collection/EnumSet.java
+++ hibernate-envers/src/test/java/org/hibernate/envers/test/integration/collection/EnumSet.java
@@ -115,11 +115,15 @@ public class EnumSet extends BaseEnversJPAFunctionalTestCase  {
 	@TestForIssue( jiraKey = "HHH-7780" )
 	public void testEnumRepresentation() {
 		EntityManager entityManager = getEntityManager();
-		List<Object[]> enums1 = entityManager.createNativeQuery( "SELECT enums1 FROM EnumSetEntity_enums1_AUD ORDER BY rev ASC" ).getResultList();
-		List<Object[]> enums2 = entityManager.createNativeQuery( "SELECT enums2 FROM EnumSetEntity_enums2_AUD ORDER BY rev ASC" ).getResultList();
+		List<Object> enums1 = entityManager.createNativeQuery( "SELECT enums1 FROM EnumSetEntity_enums1_AUD ORDER BY rev ASC" ).getResultList();
+		List<Object> enums2 = entityManager.createNativeQuery( "SELECT enums2 FROM EnumSetEntity_enums2_AUD ORDER BY rev ASC" ).getResultList();
 		entityManager.close();
 
 		Assert.assertEquals( Arrays.asList( "X", "Y", "X" ), enums1 );
-		Assert.assertEquals( Arrays.asList( 0 ), enums2 );
+
+		Assert.assertEquals( 1, enums2.size() );
+		Object enum2 = enums2.get( 0 );
+		// Compare the Strings to account for, as an example, Oracle returning a BigDecimal instead of an int.
+		Assert.assertEquals( "0", enum2.toString() );
 	}
 }

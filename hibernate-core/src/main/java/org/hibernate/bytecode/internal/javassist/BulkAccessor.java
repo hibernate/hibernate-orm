3343e6d6c36a96c2c9003f7ee016f59228d27ab9
Steve Ebersole <steve@hibernate.org>
2013-04-19 15:49:53 -0500

    HHH-8159 - Apply fixups indicated by analysis tools

--- hibernate-core/src/main/java/org/hibernate/bytecode/internal/javassist/BulkAccessor.java
+++ hibernate-core/src/main/java/org/hibernate/bytecode/internal/javassist/BulkAccessor.java
@@ -29,6 +29,10 @@ import java.io.Serializable;
  * A JavaBean bulk accessor, which provides methods capable of getting/setting multiple properties
  * of a JavaBean at once.
  *
+ * IMPORTANT NOTE!!! Apparently the order of the methods here is important as I think BulkAccessorFactory
+ * makes use of that information in terms of accessing the constructor.  Anyway, when I tried to re-arrange them
+ * the BulkAccessor creation failed and tests started to fail.
+ *
  * @author Muga Nishizawa
  * @author Shigeru Chiba
  */
@@ -38,24 +42,9 @@ public abstract class BulkAccessor implements Serializable {
 	protected Class[] types;
 
 	/**
-	 * Creates a new instance of BulkAccessor.  The created instance provides methods for setting/getting
-	 * specified properties at once.
-	 *
-	 * @param beanClass the class of the JavaBeans accessed
-	 *                  through the created object.
-	 * @param getters   the names of setter methods for specified properties.
-	 * @param setters   the names of getter methods for specified properties.
-	 * @param types     the types of specified properties.
-	 *
-	 * @return The created bulk accessor
+	 * Protected access constructor so the generated class has access to it.
 	 */
-	public static BulkAccessor create(
-			Class beanClass,
-			String[] getters,
-			String[] setters,
-			Class[] types) {
-		final BulkAccessorFactory factory = new BulkAccessorFactory( beanClass, getters, setters, types );
-		return factory.create();
+	protected BulkAccessor() {
 	}
 
 	/**
@@ -114,6 +103,24 @@ public abstract class BulkAccessor implements Serializable {
 		return setters.clone();
 	}
 
-	private BulkAccessor() {
+	/**
+	 * Creates a new instance of <code>BulkAccessor</code>.
+	 * The created instance provides methods for setting/getting
+	 * specified properties at once.
+	 *
+	 * @param beanClass the class of the JavaBeans accessed
+	 *                  through the created object.
+	 * @param getters   the names of setter methods for specified properties.
+	 * @param setters   the names of getter methods for specified properties.
+	 * @param types     the types of specified properties.
+	 *
+	 * @return The created BulkAccessor
+	 */
+	public static BulkAccessor create(
+			Class beanClass,
+			String[] getters,
+			String[] setters,
+			Class[] types) {
+		return new BulkAccessorFactory( beanClass, getters, setters, types ).create();
 	}
 }

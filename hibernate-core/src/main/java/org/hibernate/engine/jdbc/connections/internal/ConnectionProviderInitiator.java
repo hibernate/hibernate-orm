1df4b2ea3c98c74f3b6bbd42e266ee5c7ad60d27
Brett Meyer <brett@3riverdev.com>
2013-05-07 16:02:37 -0400

    HHH-8226 table synonyms cannot find columns on Oracle

--- hibernate-core/src/main/java/org/hibernate/engine/jdbc/connections/internal/ConnectionProviderInitiator.java
+++ hibernate-core/src/main/java/org/hibernate/engine/jdbc/connections/internal/ConnectionProviderInitiator.java
@@ -268,6 +268,9 @@ public class ConnectionProviderInitiator implements StandardServiceInitiator<Con
 					);
 				}
 			}
+			else if ( CONDITIONAL_PROPERTIES.containsKey( key ) ) {
+				result.setProperty( CONDITIONAL_PROPERTIES.get( key ), value );
+			}
 		}
 		return result;
 	}
@@ -283,6 +286,16 @@ public class ConnectionProviderInitiator implements StandardServiceInitiator<Con
 		SPECIAL_PROPERTIES.add( Environment.ISOLATION );
 		SPECIAL_PROPERTIES.add( Environment.DRIVER );
 		SPECIAL_PROPERTIES.add( Environment.USER );
+	}
 
+	// Connection properties (map value) that automatically need set if the
+	// Hibernate property (map key) is available. Makes the assumption that
+	// both settings use the same value type.
+	private static final Map<String, String> CONDITIONAL_PROPERTIES;
+
+	static {
+		CONDITIONAL_PROPERTIES = new HashMap<String, String>();
+		// Oracle requires that includeSynonyms=true in order for getColumns to work using a table synonym name.
+		CONDITIONAL_PROPERTIES.put( Environment.ENABLE_SYNONYMS, "includeSynonyms" );
 	}
 }

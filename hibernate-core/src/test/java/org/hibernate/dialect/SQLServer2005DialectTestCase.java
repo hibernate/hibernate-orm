6a71cbb991104176c32bbd5a697e724ac8429f3a
Lukasz Antoniak <lukasz.antoniak@gmail.com>
2013-06-11 10:14:45 -0700

    HHH-8301 - SQLServer2005LimitHandler skips column alias generation

--- hibernate-core/src/test/java/org/hibernate/dialect/SQLServer2005DialectTestCase.java
+++ hibernate-core/src/test/java/org/hibernate/dialect/SQLServer2005DialectTestCase.java
@@ -82,6 +82,19 @@ public class SQLServer2005DialectTestCase extends BaseUnitTestCase {
 	}
 
 	@Test
+	@TestForIssue(jiraKey = "HHH-8301")
+	public void testGetLimitStringAliasGeneration() {
+		final String notAliasedSQL = "select column1, column2, column3, column4 from table1";
+
+		assertEquals(
+				"WITH query AS (SELECT inner_query.*, ROW_NUMBER() OVER (ORDER BY CURRENT_TIMESTAMP) as __hibernate_row_nr__ FROM ( " +
+						"select column1 as page0_, column2 as page1_, column3 as page2_, column4 as page3_ from table1 ) inner_query ) " +
+						"SELECT page0_, page1_, page2_, page3_ FROM query WHERE __hibernate_row_nr__ >= ? AND __hibernate_row_nr__ < ?",
+				dialect.buildLimitHandler( notAliasedSQL, toRowSelection( 3, 5 ) ).getProcessedSql()
+		);
+	}
+
+	@Test
 	@TestForIssue(jiraKey = "HHH-7019")
 	public void testGetLimitStringWithSubselect() {
 		final String subselectInSelectClauseSQL = "select persistent0_.id as col_0_0_, " +

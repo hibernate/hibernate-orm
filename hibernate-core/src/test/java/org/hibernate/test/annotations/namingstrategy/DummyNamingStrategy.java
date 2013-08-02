1ff1129faa77ca2d3cc8c90294020e4a87e8853e
Brett Meyer <brett@3riverdev.com>
2013-08-02 15:52:50 -0400

    HHH-8224 test case

--- hibernate-core/src/test/java/org/hibernate/test/annotations/namingstrategy/DummyNamingStrategy.java
+++ hibernate-core/src/test/java/org/hibernate/test/annotations/namingstrategy/DummyNamingStrategy.java
@@ -1,12 +1,41 @@
 // $Id$
 package org.hibernate.test.annotations.namingstrategy;
-import org.hibernate.cfg.EJB3NamingStrategy;
+import java.util.List;
+
+import org.hibernate.cfg.DefaultNamingStrategy;
 
 @SuppressWarnings("serial")
-public class DummyNamingStrategy extends EJB3NamingStrategy {
+public class DummyNamingStrategy extends DefaultNamingStrategy {
 	
+	private int counter = 0;
+	
+	@Override
 	public String tableName(String tableName) {
 		return "T" + tableName;
+	}
+	
+	@Override
+	public String foreignKeyName(String sourceTableName, List<String> sourceColumnNames,
+			String targetTableName, List<String> targetColumnNames) {
+		return "F" + counter++;
+	}
+	
+	@Override
+	public String uniqueKeyName(String tableName, List<String> columnNames) {
+		StringBuilder sb = new StringBuilder();
+		for ( String columnName : columnNames ) {
+			sb.append( columnName );
+		}
+		return "U" + sb.toString();
+	}
+	
+	@Override
+	public String indexName(String tableName, List<String> columnNames) {
+		StringBuilder sb = new StringBuilder();
+		for ( String columnName : columnNames ) {
+			sb.append( columnName );
+		}
+		return "I" + sb.toString();
 	}
 
 }

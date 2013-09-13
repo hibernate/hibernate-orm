5211fd853cc8c09e16fc41126774dedf2648878f
Karel Maesen <karel@geovise.com>
2013-09-13 18:42:07 +0200

    Adds MySQL 5 InnoDB spatial dialect.
    Also fixes regression bug on MySQL 5 integration tests.

--- hibernate-spatial/src/test/java/org/hibernate/spatial/testing/TestSupportFactories.java
+++ hibernate-spatial/src/test/java/org/hibernate/spatial/testing/TestSupportFactories.java
@@ -77,7 +77,8 @@ public class TestSupportFactories {
 			return SQLServerTestSupport.class;
 		}
 		if ("org.hibernate.spatial.dialect.mysql.MySQLSpatialDialect".equals(canonicalName) ||
-				"org.hibernate.spatial.dialect.mysql.MySQLSpatialInnoDBDialect".equals(canonicalName)) {
+				"org.hibernate.spatial.dialect.mysql.MySQLSpatialInnoDBDialect".equals(canonicalName) ||
+				"org.hibernate.spatial.dialect.mysql.MySQLSpatial5InnoDBDialect".equals(canonicalName) ) {
 			return MySQLTestSupport.class;
 		}
 		if ("org.hibernate.spatial.dialect.oracle.OracleSpatial10gDialect".equals(canonicalName)) {

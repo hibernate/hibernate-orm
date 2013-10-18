07764a53b53ac718ce9c54ac9eea4eab146b6179
Deven Phillips <deven.phillips@gmail.com>
2013-10-18 12:44:38 -0400

    Update SQLServer2012Dialect.java

--- hibernate-core/src/main/java/org/hibernate/dialect/SQLServer2012Dialect.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/SQLServer2012Dialect.java
@@ -21,7 +21,7 @@
  * 51 Franklin Street, Fifth Floor
  * Boston, MA  02110-1301  USA
  */
-package com.hps.hibernate.dialect;
+package org.hibernate.dialect;
 
 import org.hibernate.dialect.SQLServer2008Dialect;
 

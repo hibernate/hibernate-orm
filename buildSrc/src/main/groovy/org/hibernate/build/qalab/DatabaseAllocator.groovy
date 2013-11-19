77064022132b2f35afda26bc73a8c71abd3a7fd4
Martin Simka <msimka@redhat.com>
2013-11-19 10:01:23 +0100

    [HHH-8707] Add support for oracle12c, postgresql92, postgresplus92, db2-10, mssql2012 to DatabaseAllocator

--- buildSrc/src/main/groovy/org/hibernate/build/qalab/DatabaseAllocator.groovy
+++ buildSrc/src/main/groovy/org/hibernate/build/qalab/DatabaseAllocator.groovy
@@ -50,11 +50,12 @@ class DatabaseAllocator {
 	public static final String DB_ALLOCATOR_KEY = "dbAllocator";
 
     public static def SUPPORTED_DB_NAMES = [
-            "oracle9i", "oracle10g", "oracle11gR1", "oracle11gR2", "oracle11gR2RAC", "oracle11gR1RAC",
-            "postgresql82", "postgresql83", "postgresql84", "postgresql91",
+            "oracle9i", "oracle10g", "oracle11gR1", "oracle11gR2", "oracle11gR2RAC", "oracle11gR1RAC", "oracle12c",
+            "postgresql82", "postgresql83", "postgresql84", "postgresql91", "postgresql92", 
+            "postgresplus92",
             "mysql50", "mysql51","mysql55",
-            "db2-91", "db2-97",
-            "mssql2005", "mssql2008R1", "mssql2008R2",
+            "db2-91", "db2-97", "db2-10",
+            "mssql2005", "mssql2008R1", "mssql2008R2", "mssql2012", 
             "sybase155", "sybase157"
     ];
 

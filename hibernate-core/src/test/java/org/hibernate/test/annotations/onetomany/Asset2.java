b498824ce6f449ac57308bd2cf9b6eaf95abfda5
Matus Zamborsky <zamborsky@inqool.cz>
2013-12-12 12:34:39 +0100

    HHH-8794 Support for @OrderBy without specifying attribute

---
+++ hibernate-core/src/test/java/org/hibernate/test/annotations/onetomany/Asset2.java
@@ -0,0 +1,80 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2013, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.test.annotations.onetomany;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ *
+ */
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Asset2 implements Serializable {
+	/** */
+	@Id
+	@Column(name = "id_asset")
+	private final Integer idAsset;
+	@Id
+	@Column(name = "id_test")
+	private final Integer test;
+	/** */
+	@ManyToOne(cascade = {CascadeType.ALL})
+	@JoinColumn(nullable = false)
+	private Employee2 employee;
+
+	public Asset2() {
+		this.idAsset = 0;
+		this.test = 1;
+	}
+
+	/**
+	 * @param idAsset
+	 */
+	public Asset2(Integer idAsset) {
+		this.idAsset = idAsset;
+		this.test = 1;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getIdAsset() {
+		return idAsset;
+	}
+
+	/**
+	 * @return the employee
+	 */
+	public Employee2 getEmployee() {
+		return employee;
+	}
+
+	/**
+	 * @param employee the employee to set
+	 */
+	public void setEmployee(Employee2 employee) {
+		this.employee = employee;
+	}
+}

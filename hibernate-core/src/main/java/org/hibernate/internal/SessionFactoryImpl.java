b47dd338d2b85271a25df85f121003d840275f63
mukhanov <mukhanov@gmail.com>
2014-01-12 15:06:08 +0400

    HHH-3482 Fixed StatelessSession inserting bug + test

--- hibernate-core/src/main/java/org/hibernate/internal/SessionFactoryImpl.java
+++ hibernate-core/src/main/java/org/hibernate/internal/SessionFactoryImpl.java
@@ -1690,7 +1690,7 @@ public final class SessionFactoryImpl
 
 		@Override
 		public StatelessSession openStatelessSession() {
-			return new StatelessSessionImpl( connection, tenantIdentifier, sessionFactory );
+			return new StatelessSessionImpl( connection, tenantIdentifier, sessionFactory, sessionFactory.settings.getRegionFactory().nextTimestamp() );
 		}
 
 		@Override

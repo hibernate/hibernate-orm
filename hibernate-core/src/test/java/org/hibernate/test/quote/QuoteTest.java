f19c8e995a763fe7c31fbb7f5777ac6ff608c215
Gail Badner <gbadner@redhat.com>
2014-01-27 06:56:14 -0800

    HHH-8897 : Integrate org.hibernate.metamodel from metamodel branch into master

--- hibernate-core/src/test/java/org/hibernate/test/quote/QuoteTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/quote/QuoteTest.java
@@ -46,7 +46,6 @@ import org.hibernate.Transaction;
 import org.hibernate.metamodel.spi.binding.AttributeBinding;
 import org.hibernate.metamodel.spi.binding.PluralAttributeBinding;
 import org.hibernate.metamodel.spi.binding.PluralAttributeKeyBinding;
-import org.hibernate.metamodel.spi.relational.Table;
 import org.hibernate.testing.TestForIssue;
 import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
 import org.junit.Test;
@@ -75,7 +74,11 @@ public class QuoteTest extends BaseCoreFunctionalTestCase {
 		if ( isMetadataUsed() ) {
 			AttributeBinding attributeBinding = metadata().getEntityBinding( User.class.getName() ).locateAttributeBinding( "roles" );
 			PluralAttributeKeyBinding keyBinding = ( (PluralAttributeBinding) attributeBinding ).getPluralAttributeKeyBinding();
-			assertEquals( "User_Role", ( (Table) keyBinding.getCollectionTable() ).getPhysicalName().getText() );
+			assertEquals(
+					"User_Role",
+					( (org.hibernate.metamodel.spi.relational.Table) keyBinding.getCollectionTable() )
+							.getPhysicalName().getText()
+			);
 		}
 		else {
 			assertEquals( "User_Role", configuration().getCollectionMapping( role ).getCollectionTable().getName() );

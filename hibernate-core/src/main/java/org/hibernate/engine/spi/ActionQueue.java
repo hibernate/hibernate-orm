cd14a5a473533a02cdf0c9b2e620fefee1a1c7fb
sndyuk <sanada@sndyuk.com>
2014-02-28 20:06:01 +0900

    HHH-8374: Treat an entity wrapped by hibernate proxy equals a raw entity

--- hibernate-core/src/main/java/org/hibernate/engine/spi/ActionQueue.java
+++ hibernate-core/src/main/java/org/hibernate/engine/spi/ActionQueue.java
@@ -59,6 +59,8 @@ import org.hibernate.action.spi.Executable;
 import org.hibernate.cache.CacheException;
 import org.hibernate.engine.internal.NonNullableTransientDependencies;
 import org.hibernate.internal.CoreMessageLogger;
+import org.hibernate.proxy.HibernateProxy;
+import org.hibernate.proxy.LazyInitializer;
 import org.hibernate.type.Type;
 import org.jboss.logging.Logger;
 
@@ -535,6 +537,12 @@ public class ActionQueue {
 	}
 
 	public void unScheduleDeletion(EntityEntry entry, Object rescuedEntity) {
+		if ( rescuedEntity instanceof HibernateProxy ) {
+			LazyInitializer initializer = ( ( HibernateProxy ) rescuedEntity ).getHibernateLazyInitializer();
+			if ( !initializer.isUninitialized() ) {
+				rescuedEntity = initializer.getImplementation( session );
+			}
+		}
 		for ( int i = 0; i < deletions.size(); i++ ) {
 			EntityDeleteAction action = deletions.get( i );
 			if ( action.getInstance() == rescuedEntity ) {

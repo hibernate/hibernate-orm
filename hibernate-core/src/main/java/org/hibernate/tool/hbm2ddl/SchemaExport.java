9eaca501d98b0e56d47298bcdce0c94f205f6547
Libor Ondrusek <libor.ondrusek@artin.cz>
2014-03-12 17:04:06 -0400

    HHH-9040 Added halt on error support for scripts importing
    Conflicts:
    	hibernate-core/src/main/java/org/hibernate/tool/hbm2ddl/SchemaExport.java

--- hibernate-core/src/main/java/org/hibernate/tool/hbm2ddl/SchemaExport.java
+++ hibernate-core/src/main/java/org/hibernate/tool/hbm2ddl/SchemaExport.java
@@ -448,7 +448,13 @@ public class SchemaExport {
 							}
 						}
 						catch ( Exception e ) {
-							throw new ImportScriptException( "Error during statement execution (file: '" + namedReader.getName() + "'): " + trimmedSql, e );
+						  	if (haltOnError) {
+						  		throw new ImportScriptException( "Error during statement execution (file: '"
+						  				+ namedReader.getName() + "'): " + trimmedSql, e );
+							}
+						  	exceptions.add(e);
+						  	LOG.unsuccessful(trimmedSql);
+						  	LOG.error(e.getMessage());
 						}
 					}
 				}

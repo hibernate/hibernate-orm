b693a24faeccbbe62ba7af17c6b73969233eb97d
Eric Haas <eric@s3a.org>
2014-03-13 12:05:00 -0500

    HHH-9403 Added the ability to customize the modified field name for audited fields.

--- hibernate-envers/src/main/java/org/hibernate/envers/configuration/internal/metadata/reader/AuditedPropertiesReader.java
+++ hibernate-envers/src/main/java/org/hibernate/envers/configuration/internal/metadata/reader/AuditedPropertiesReader.java
@@ -518,19 +518,12 @@ public class AuditedPropertiesReader {
 			}
 		}
 
-
-		if ( !this.checkAudited( property, propertyData, allClassAudited ) ) {
+        final String propertyName = propertyNamePrefix + property.getName();
+		if ( !this.checkAudited( property, propertyData, propertyName, allClassAudited, globalCfg.getModifiedFlagSuffix() ) ) {
 			return false;
 		}
 
-		final String propertyName = propertyNamePrefix + property.getName();
-		propertyData.setName( propertyName );
-		propertyData.setModifiedFlagName(
-				MetadataTools.getModifiedFlagPropertyName(
-						propertyName,
-						globalCfg.getModifiedFlagSuffix()
-				)
-		);
+		propertyData.setName(propertyName);
 		propertyData.setBeanName( property.getName() );
 		propertyData.setAccessType( accessType );
 
@@ -550,7 +543,7 @@ public class AuditedPropertiesReader {
 
 	protected boolean checkAudited(
 			XProperty property,
-			PropertyAuditingData propertyData, Audited allClassAudited) {
+			PropertyAuditingData propertyData, String propertyName, Audited allClassAudited, String modifiedFlagSuffix) {
 		// Checking if this property is explicitly audited or if all properties are.
 		Audited aud = ( property.isAnnotationPresent( Audited.class ) )
 				? property.getAnnotation( Audited.class )
@@ -566,6 +559,13 @@ public class AuditedPropertiesReader {
 			propertyData.setStore( aud.modStore() );
 			propertyData.setRelationTargetAuditMode( aud.targetAuditMode() );
 			propertyData.setUsingModifiedFlag( checkUsingModifiedFlag( aud ) );
+            if(aud.modifiedColumnName() != null && !"".equals(aud.modifiedColumnName())) {
+                propertyData.setModifiedFlagName(aud.modifiedColumnName());
+            } else {
+                propertyData.setModifiedFlagName(
+                        MetadataTools.getModifiedFlagPropertyName(propertyName, modifiedFlagSuffix)
+                );
+            }
 			return true;
 		}
 		else {
@@ -682,7 +682,12 @@ public class AuditedPropertiesReader {
 			return false;
 		}
 
-		@Override
+        @Override
+        public String modifiedColumnName() {
+            return "";
+        }
+
+        @Override
 		public Class<? extends Annotation> annotationType() {
 			return this.getClass();
 		}

e8a52e290437c510927106d33efe9e6350bc66e8
Brett Meyer <brett@3riverdev.com>
2014-03-17 16:33:44 -0400

    HHH-8869 created hibernate-hikaricp connection provider
    Conflicts:
    	libraries.gradle

---
+++ hibernate-hikaricp/src/main/java/org/hibernate/hikaricp/internal/StrategyRegistrationProviderImpl.java
@@ -0,0 +1,58 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2012, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.hikaricp.internal;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.hibernate.boot.registry.selector.SimpleStrategyRegistrationImpl;
+import org.hibernate.boot.registry.selector.StrategyRegistration;
+import org.hibernate.boot.registry.selector.StrategyRegistrationProvider;
+import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
+
+/**
+ * Provides the {@link HikariCPConnectionProvider} to the
+ * {@link org.hibernate.boot.registry.selector.spi.StrategySelector} service.
+ * 
+ * @author Brett Meyer
+ */
+public class StrategyRegistrationProviderImpl implements StrategyRegistrationProvider {
+	private static final List<StrategyRegistration> REGISTRATIONS = Collections.singletonList(
+			(StrategyRegistration) new SimpleStrategyRegistrationImpl<ConnectionProvider>(
+					ConnectionProvider.class,
+					HikariCPConnectionProvider.class,
+					"hikari",
+					"hikaricp",
+					HikariCPConnectionProvider.class.getSimpleName(),
+					// for consistency's sake
+					"org.hibernate.connection.HikariCPConnectionProvider"
+			)
+	);
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public Iterable<StrategyRegistration> getStrategyRegistrations() {
+		return REGISTRATIONS;
+	}
+}

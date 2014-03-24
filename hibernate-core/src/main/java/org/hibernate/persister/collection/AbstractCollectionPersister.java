87d04ef3ed4c1fc3ce1adce0b5544b2ff2d11fdb
Brett Meyer <brett@3riverdev.com>
2014-03-24 13:24:35 -0400

    HHH-9078 correct OrderColumn indexes for inverse, extra lazy collections

--- hibernate-core/src/main/java/org/hibernate/persister/collection/AbstractCollectionPersister.java
+++ hibernate-core/src/main/java/org/hibernate/persister/collection/AbstractCollectionPersister.java
@@ -1759,11 +1759,13 @@ public abstract class AbstractCollectionPersister
 	public void processQueuedOps(PersistentCollection collection, Serializable key, SessionImplementor session)
 			throws HibernateException {
 		if ( collection.hasQueuedOperations() ) {
-			doProcessQueuedOps( collection, key, session );
+			int nextIndex = getSize( key, session );
+			doProcessQueuedOps( collection, key, nextIndex, session );
 		}
 	}
 
-	protected abstract void doProcessQueuedOps(PersistentCollection collection, Serializable key, SessionImplementor session)
+	protected abstract void doProcessQueuedOps(PersistentCollection collection, Serializable key,
+			int nextIndex, SessionImplementor session)
 			throws HibernateException;
 
 	@Override

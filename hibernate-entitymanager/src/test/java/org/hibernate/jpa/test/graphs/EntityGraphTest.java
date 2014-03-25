e541ab02df7b5f91d088b1e34ceaf8707d7f342a
Jocki Hendry <jocki.hendry@gmail.com>
2014-03-25 15:22:34 +0700

    HHH-9080 - Can't add attribute node declared in superclass (inheritance) in EntityGraph

--- hibernate-entitymanager/src/test/java/org/hibernate/jpa/test/graphs/EntityGraphTest.java
+++ hibernate-entitymanager/src/test/java/org/hibernate/jpa/test/graphs/EntityGraphTest.java
@@ -225,7 +225,45 @@ public class EntityGraphTest extends BaseEntityManagerFunctionalTestCase {
 		em.close();
 	}
 
-	@Entity
+    @Test
+    @TestForIssue(jiraKey = "HHH-9080")
+    public void attributeNodeInheritanceTest() {
+        EntityManager em = getOrCreateEntityManager();
+        em.getTransaction().begin();
+
+        Manager manager = new Manager();
+        em.persist( manager );
+        Employee employee = new Employee();
+        manager.friends.add( employee);
+        em.persist( employee );
+        Manager anotherManager = new Manager();
+        manager.managers.add(anotherManager);
+        em.persist( anotherManager );
+        em.getTransaction().commit();
+        em.clear();
+
+        em.getTransaction().begin();
+
+        EntityGraph<Manager> entityGraph = em.createEntityGraph( Manager.class );
+        entityGraph.addAttributeNodes( "friends" );
+        entityGraph.addAttributeNodes( "managers" );
+
+        Map<String, Object> properties = new HashMap<String, Object>();
+        properties.put( "javax.persistence.loadgraph", entityGraph );
+
+        Manager result = em.find( Manager.class, manager.id, properties );
+
+        assertTrue( Hibernate.isInitialized( result ) );
+        assertTrue( Hibernate.isInitialized( result.friends ) );
+        assertEquals( result.friends.size(), 1 );
+        assertTrue( Hibernate.isInitialized( result.managers) );
+        assertEquals( result.managers.size(), 1 );
+
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    @Entity
     public static class Foo {
 
 		@Id

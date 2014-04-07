03586f3b7b4dac80568eddd62c1590dfbb6f7038
Brett Meyer <brett@3riverdev.com>
2014-04-07 11:09:40 -0400

    HHH-9103 Dialect resolver should use a newer Oracle dialect for Oracle12c

--- hibernate-core/src/main/java/org/hibernate/engine/jdbc/dialect/internal/StandardDialectResolver.java
+++ hibernate-core/src/main/java/org/hibernate/engine/jdbc/dialect/internal/StandardDialectResolver.java
@@ -195,8 +195,10 @@ public class StandardDialectResolver implements DialectResolver {
 			final int majorVersion = info.getDatabaseMajorVersion();
 
 			switch ( majorVersion ) {
+				case 12:
+					// fall through
 				case 11:
-					return new Oracle10gDialect();
+					// fall through
 				case 10:
 					return new Oracle10gDialect();
 				case 9:
@@ -206,6 +208,7 @@ public class StandardDialectResolver implements DialectResolver {
 				default:
 					LOG.unknownOracleVersion( majorVersion );
 			}
+			return new Oracle8iDialect();
 		}
 
 		if ( "HDB".equals( databaseName ) ) {

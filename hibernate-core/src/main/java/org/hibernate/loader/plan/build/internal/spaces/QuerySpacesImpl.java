025d2e2f44a6d3082273db3714462fc4ecc0b33a
Steve Ebersole <steve@hibernate.org>
2014-04-08 16:35:17 -0500

    HHH-8980 - Instance of super-class not found because of joined subclass reference is an inner join (cherry picked from commit 651b23b1739a1409dea7e4521f26185d48fbba71)

--- hibernate-core/src/main/java/org/hibernate/loader/plan/build/internal/spaces/QuerySpacesImpl.java
+++ hibernate-core/src/main/java/org/hibernate/loader/plan/build/internal/spaces/QuerySpacesImpl.java
@@ -38,6 +38,7 @@ import org.hibernate.loader.plan.spi.QuerySpace;
 import org.hibernate.loader.plan.spi.QuerySpaceUidNotRegisteredException;
 import org.hibernate.persister.collection.CollectionPersister;
 import org.hibernate.persister.entity.EntityPersister;
+import org.hibernate.persister.entity.JoinedSubclassEntityPersister;
 
 import org.jboss.logging.Logger;
 
@@ -101,11 +102,25 @@ public class QuerySpacesImpl implements ExpandingQuerySpaces {
 
 		checkQuerySpaceDoesNotExist( uid );
 
+		// as a temporary fix for HHH-8980 and HHH-8830 we circumvent allowing
+		// inner joins (canJoinsBeRequired) when the persister is part of an
+		// entity inheritance.
+		//
+		// hasSubclasses() is the closest we can come to even knowing if the
+		// entity is part of a hierarchy.  But its enough, since if there are no
+		// subclasses we cannot have the case where the attribute to join comes from
+		// a subclass :)
+		//
+		// a better long term fix is to expose whether a joined association attribute
+		// is defined on the class/superClass(es) or on the subClass(es).  Attributes
+		// from the subClass(es) should not be inner joined; it is ok to inner join
+		// attributes from the class/superClass(es).
+
 		final EntityQuerySpaceImpl space = new EntityQuerySpaceImpl(
 				entityPersister,
 				uid,
 				this,
-				canJoinsBeRequired
+				canJoinsBeRequired && !entityPersister.getEntityMetamodel().hasSubclasses()
 		);
 		registerQuerySpace( space );
 

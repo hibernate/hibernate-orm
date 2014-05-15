bd9ddf10d529ec6e581da75070983ee7b22c5778
Mark Robinson <mark@mrobinson.ca>
2014-05-14 18:27:02 -0700

    reformat code

--- hibernate-core/src/test/java/org/hibernate/dialect/PostgreSQL81DialectTestCase.java
+++ hibernate-core/src/test/java/org/hibernate/dialect/PostgreSQL81DialectTestCase.java
@@ -23,22 +23,19 @@
  */
 package org.hibernate.dialect;
 
-import org.junit.Test;
-
-import java.sql.BatchUpdateException;
-import java.sql.SQLException;
 import org.hibernate.JDBCException;
 import org.hibernate.PessimisticLockException;
 import org.hibernate.exception.LockAcquisitionException;
 import org.hibernate.exception.spi.SQLExceptionConversionDelegate;
-
 import org.hibernate.testing.TestForIssue;
 import org.hibernate.testing.junit4.BaseUnitTestCase;
+import org.junit.Test;
+
+import java.sql.BatchUpdateException;
+import java.sql.SQLException;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 
 /**
@@ -48,39 +45,39 @@ import static org.junit.Assert.assertNotNull;
  */
 
 public class PostgreSQL81DialectTestCase extends BaseUnitTestCase {
-	
+
 	@Test
-    @TestForIssue( jiraKey = "HHH-7251" )
+	@TestForIssue(jiraKey = "HHH-7251")
 	public void testDeadlockException() {
 		PostgreSQL81Dialect dialect = new PostgreSQL81Dialect();
 		SQLExceptionConversionDelegate delegate = dialect.buildSQLExceptionConversionDelegate();
 		assertNotNull(delegate);
-		
+
 		JDBCException exception = delegate.convert(new SQLException("Deadlock Detected", "40P01"), "", "");
 		assertTrue(exception instanceof LockAcquisitionException);
 	}
-	
+
 	@Test
-    @TestForIssue( jiraKey = "HHH-7251" )
+	@TestForIssue(jiraKey = "HHH-7251")
 	public void testTimeoutException() {
 		PostgreSQL81Dialect dialect = new PostgreSQL81Dialect();
 		SQLExceptionConversionDelegate delegate = dialect.buildSQLExceptionConversionDelegate();
 		assertNotNull(delegate);
-		
+
 		JDBCException exception = delegate.convert(new SQLException("Lock Not Available", "55P03"), "", "");
 		assertTrue(exception instanceof PessimisticLockException);
 	}
 
-    @Test
-    public void testExtractConstraintName() {
-        PostgreSQL81Dialect dialect = new PostgreSQL81Dialect();
+	@Test
+	public void testExtractConstraintName() {
+		PostgreSQL81Dialect dialect = new PostgreSQL81Dialect();
 
 
-        SQLException psqlException = new java.sql.SQLException("ERROR: duplicate key value violates unique constraint \"uk_4bm1x2ultdmq63y3h5r3eg0ej\"  Detail: Key (username, server_config)=(user, 1) already exists.", "23505");
-        BatchUpdateException batchUpdateException = new BatchUpdateException("Concurrent Error", "23505", null);
-        batchUpdateException.setNextException(psqlException);
+		SQLException psqlException = new java.sql.SQLException("ERROR: duplicate key value violates unique constraint \"uk_4bm1x2ultdmq63y3h5r3eg0ej\"  Detail: Key (username, server_config)=(user, 1) already exists.", "23505");
+		BatchUpdateException batchUpdateException = new BatchUpdateException("Concurrent Error", "23505", null);
+		batchUpdateException.setNextException(psqlException);
 
-        String constraintName = dialect.getViolatedConstraintNameExtracter().extractConstraintName(batchUpdateException);
-        assertThat(constraintName, is("uk_4bm1x2ultdmq63y3h5r3eg0ej"));
-    }
+		String constraintName = dialect.getViolatedConstraintNameExtracter().extractConstraintName(batchUpdateException);
+		assertThat(constraintName, is("uk_4bm1x2ultdmq63y3h5r3eg0ej"));
+	}
 }

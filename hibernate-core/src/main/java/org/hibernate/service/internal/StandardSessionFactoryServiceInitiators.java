088c610b78572b3dfaed054701a9dcc5de7635ce
Gunnar Morling <gunnar.morling@googlemail.com>
2014-05-19 14:33:15 +0200

    HHH-9190 Introducing ParameterMetadataRecognizer contract in order to make retrieval fo parameter meta-data for native queries customizable

--- hibernate-core/src/main/java/org/hibernate/service/internal/StandardSessionFactoryServiceInitiators.java
+++ hibernate-core/src/main/java/org/hibernate/service/internal/StandardSessionFactoryServiceInitiators.java
@@ -27,6 +27,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.hibernate.engine.query.spi.ParameterMetadataRecognizerInitiator;
 import org.hibernate.engine.spi.CacheInitiator;
 import org.hibernate.event.service.internal.EventListenerServiceInitiator;
 import org.hibernate.service.spi.SessionFactoryServiceInitiator;
@@ -47,6 +48,7 @@ public class StandardSessionFactoryServiceInitiators {
 		serviceInitiators.add( EventListenerServiceInitiator.INSTANCE );
 		serviceInitiators.add( StatisticsInitiator.INSTANCE );
 		serviceInitiators.add( CacheInitiator.INSTANCE );
+		serviceInitiators.add( ParameterMetadataRecognizerInitiator.INSTANCE );
 
 		return Collections.unmodifiableList( serviceInitiators );
 	}

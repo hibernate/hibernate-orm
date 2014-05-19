efaa916a8e2b2c122f70f8f17f65c71fa28afe98
Brett Meyer <brett@3riverdev.com>
2014-05-19 15:17:10 -0400

    HHH-9167 - Support Postgres9.2 JSON data type

--- hibernate-core/src/main/java/org/hibernate/dialect/PostgreSQL94Dialect.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/PostgreSQL94Dialect.java
@@ -23,20 +23,23 @@
  */
 package org.hibernate.dialect;
 
-import java.sql.Types;
+import org.hibernate.dialect.function.StandardSQLFunction;
+import org.hibernate.type.StandardBasicTypes;
 
 /**
- * An SQL dialect for Postgres 9.2 and later, adds support for JSON data type
- * 
- * @author Mark Robinson
+ * An SQL dialect for Postgres 9.4 and later. Adds support for various date and time functions
  */
-public class PostgreSQL92Dialect extends PostgreSQL9Dialect {
+public class PostgreSQL94Dialect extends PostgreSQL92Dialect {
 
 	/**
-	 * Constructs a PostgreSQL92Dialect
+	 * Constructs a PostgreSQL94Dialect
 	 */
-	public PostgreSQL92Dialect() {
+	public PostgreSQL94Dialect() {
 		super();
-		this.registerColumnType( Types.JAVA_OBJECT, "json" );
+		registerFunction( "make_interval", new StandardSQLFunction("make_interval", StandardBasicTypes.TIMESTAMP) );
+		registerFunction( "make_timestamp", new StandardSQLFunction("make_timestamp", StandardBasicTypes.TIMESTAMP) );
+		registerFunction( "make_timestamptz", new StandardSQLFunction("make_timestamptz", StandardBasicTypes.TIMESTAMP) );
+		registerFunction( "make_date", new StandardSQLFunction("make_date", StandardBasicTypes.DATE) );
+		registerFunction( "make_time", new StandardSQLFunction("make_time", StandardBasicTypes.TIME) );
 	}
 }

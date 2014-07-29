a0663f0d6c4185893dbcc2aec4f5fa28ddc8eb35
Gail Badner <gbadner@redhat.com>
2014-07-29 16:54:18 -0700

    HHH-9305 : HQL FromElement is not reused in some cases resulting in an additional join

---
+++ hibernate-core/src/test/java/org/hibernate/test/hql/Employee.java
@@ -0,0 +1,107 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2014, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.test.hql;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee")
+public class Employee implements Serializable  {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id_emp")
+    private Integer id;
+    
+    private String firstName;
+    private String lastName;
+    
+    @OneToOne
+    @JoinColumn(name="id_title")
+    private Title title;
+    
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="id_depto")
+    private Department department;
+    
+    public Employee() {}
+    
+    public Employee(Integer _id, String _lastName, Integer _idTitle, String _descriptionTitle, Department _dept, String _fname) {
+        setId(_id);
+        setLastName(_lastName);
+        Title _title = new Title();
+        _title.setId(_idTitle);
+        _title.setDescription(_descriptionTitle);
+        setTitle(_title);
+        setDepartment(_dept);
+        setFirstName(_fname);
+    }
+    
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public Title getTitle() {
+        return title;
+    }
+    public void setTitle(Title title) {
+        this.title = title;
+    }
+    public Department getDepartment() {
+        return department;
+    }
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+    
+    
+    
+    
+    
+}

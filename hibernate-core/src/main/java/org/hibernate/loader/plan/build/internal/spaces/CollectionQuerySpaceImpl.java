35edd5690700fdea153fa20487c2c7e3271dc2f2
Gail Badner <gbadner@redhat.com>
2014-08-18 14:00:02 -0700

    HHH-8839 : Eager Map with entity key causes IllegalStateException: Collection element (many-to-many) table alias cannot be empty

--- hibernate-core/src/main/java/org/hibernate/loader/plan/build/internal/spaces/CollectionQuerySpaceImpl.java
+++ hibernate-core/src/main/java/org/hibernate/loader/plan/build/internal/spaces/CollectionQuerySpaceImpl.java
@@ -26,6 +26,7 @@ package org.hibernate.loader.plan.build.internal.spaces;
 import org.hibernate.loader.plan.build.spi.ExpandingCollectionQuerySpace;
 import org.hibernate.loader.plan.build.spi.ExpandingQuerySpaces;
 import org.hibernate.loader.plan.spi.Join;
+import org.hibernate.loader.plan.spi.JoinDefinedByMetadata;
 import org.hibernate.persister.collection.CollectionPersister;
 import org.hibernate.persister.collection.CollectionPropertyNames;
 import org.hibernate.persister.collection.QueryableCollection;
@@ -36,6 +37,8 @@ import org.hibernate.persister.entity.PropertyMapping;
  */
 public class CollectionQuerySpaceImpl extends AbstractQuerySpace implements ExpandingCollectionQuerySpace {
 	private final CollectionPersister persister;
+	private JoinDefinedByMetadata elementJoin;
+	private JoinDefinedByMetadata indexJoin;
 
 	public CollectionQuerySpaceImpl(
 			CollectionPersister persister,
@@ -78,6 +81,35 @@ public class CollectionQuerySpaceImpl extends AbstractQuerySpace implements Expa
 
 	@Override
 	public void addJoin(Join join) {
+		if ( JoinDefinedByMetadata.class.isInstance( join ) ) {
+			final JoinDefinedByMetadata joinDefinedByMetadata = (JoinDefinedByMetadata) join;
+			if ( joinDefinedByMetadata.getJoinedPropertyName().equals( CollectionPropertyNames.COLLECTION_ELEMENTS ) ) {
+				if ( elementJoin == null ) {
+					elementJoin = joinDefinedByMetadata;
+				}
+				else {
+					throw new IllegalStateException( "Attempt to add an element join, but an element join already exists." );
+				}
+			}
+			else if ( joinDefinedByMetadata.getJoinedPropertyName().equals( CollectionPropertyNames.COLLECTION_INDICES ) ) {
+				if ( indexJoin == null ) {
+					indexJoin = joinDefinedByMetadata;
+				}
+				else {
+					throw new IllegalStateException( "Attempt to add an index join, but an index join already exists." );
+				}
+			}
+			else {
+				throw new IllegalArgumentException(
+						String.format(
+								"Collection propertyName must be either %s or %s; instead the joined property name was %s.",
+								CollectionPropertyNames.COLLECTION_ELEMENTS,
+								CollectionPropertyNames.COLLECTION_INDICES,
+								joinDefinedByMetadata.getJoinedPropertyName()
+						)
+				);
+			}
+		}
 		internalGetJoins().add( join );
 	}
 
@@ -86,4 +118,7 @@ public class CollectionQuerySpaceImpl extends AbstractQuerySpace implements Expa
 		return super.getExpandingQuerySpaces();
 	}
 
+	public void addJoin(JoinDefinedByMetadata join) {
+		addJoin( (Join) join );
+	}
 }

ea2d26e92e05a2b489d58de231103a3d4c73245f
Etienne Miret <etienne.miret@ens-lyon.org>
2014-08-24 08:58:02 +0200

    HHH-9356 Add a bunch of tests.
    Conflicts:
    	hibernate-entitymanager/src/test/java/org/hibernate/jpa/test/convert/QueryTest.java

---
+++ hibernate-core/src/test/java/org/hibernate/test/jpa/convert/Item.java
@@ -0,0 +1,86 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2014, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.test.jpa.convert;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * @author Etienne Miret
+ */
+@Entity
+public class Item {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Convert( converter = BigDecimalToStringConverter.class )
+	private BigDecimal price;
+
+	private String description;
+
+	@Convert( converter = AddTenConverter.class )
+	private Integer quantity;
+
+	public Long getId() {
+		return id;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(final BigDecimal price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(final String description) {
+		this.description = description;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(final Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	/*
+	 * For those who hate auto (un)boxing.
+	 */
+	public void setQuantity(final int quantity) {
+		this.quantity = Integer.valueOf( quantity );
+	}
+
+}

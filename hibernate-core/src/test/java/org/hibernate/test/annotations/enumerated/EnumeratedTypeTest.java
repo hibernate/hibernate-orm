40efe9fc1c70d491012da0600119f7c937376089
andreas <andreas@berger-ecommerce.com>
2014-08-29 10:01:52 +0200

    HHH-9369 Fix @Formula of enum type results in ClassCastException

--- hibernate-core/src/test/java/org/hibernate/test/annotations/enumerated/EnumeratedTypeTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/annotations/enumerated/EnumeratedTypeTest.java
@@ -397,6 +397,28 @@ public class EnumeratedTypeTest extends BaseCoreFunctionalTestCase {
         s.close();
 	}
 
+	@Test
+	@TestForIssue(jiraKey = "HHH-9369")
+	public void testFormula() throws SQLException {
+		// use native SQL to insert, forcing whitespace to occur
+		final Session s = openSession();
+		final Connection connection = ((SessionImplementor)s).connection();
+		final Statement statement = connection.createStatement();
+		statement.execute("insert into EntityEnum (id) values(1)");
+
+		s.getTransaction().begin();
+
+		// ensure EnumType can do #fromName with the trimming
+		List<EntityEnum> resultList = s.createQuery("select e from EntityEnum e").list();
+		assertEquals( resultList.size(), 1 );
+		assertEquals( resultList.get(0).getFormula(), Trimmed.A );
+
+		statement.execute( "delete from EntityEnum" );
+
+		s.getTransaction().commit();
+		s.close();
+	}
+
 	@Override
 	protected Class[] getAnnotatedClasses() {
 		return new Class[] { EntityEnum.class };

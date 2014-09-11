9193d8ce9789736d48d54ea26315d029b851b3e4
Gail Badner <gbadner@redhat.com>
2014-09-11 12:41:15 -0700

    HHH-9387 HHH-9389 : Default collection table and join column names generated for @ElementCollection (test cases)
    HHH-9387 HHH-9389 : Default collection join column names generated for @ElementCollection (test cases)
    
    (cherry picked from commit f2ae61abf63e71367542b2a3f05c41f3d04bbed1)
    
    Conflicts:
    	hibernate-core/src/test/java/org/hibernate/test/annotations/collectionelement/CollectionElementTest.java

---
+++ hibernate-core/src/test/java/org/hibernate/test/annotations/collectionelement/Owner.java
@@ -0,0 +1,62 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2014, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.test.annotations.collectionelement;
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+
+/**
+ * @author Gail Badner
+ */
+@Entity( name="OWNER")
+@Table( name="OWNER_TABLE")
+public class Owner {
+	private Integer id;
+	private Set<String> elements = new HashSet<String>();
+
+	@Id
+	@GeneratedValue
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@ElementCollection
+	public Set<String> getElements() {
+		return elements;
+	}
+
+	public void setElements(Set<String> elements) {
+		this.elements = elements;
+	}
+}

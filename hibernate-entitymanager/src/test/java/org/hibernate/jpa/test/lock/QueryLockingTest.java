5047f6fcc63e4598d673a38d6cec1a1ade068ddd
Arcadiy Ivanov <arcadiy.ivanov@servicemesh.com>
2014-09-27 02:19:01 -0400

    HHH-9419 : Remove after get with optimistic lock fails
    (cherry picked from commit c80a8c442a91b880a24ea32a79e48da3c4a205a8)

--- hibernate-entitymanager/src/test/java/org/hibernate/jpa/test/lock/QueryLockingTest.java
+++ hibernate-entitymanager/src/test/java/org/hibernate/jpa/test/lock/QueryLockingTest.java
@@ -294,6 +294,31 @@ public class QueryLockingTest extends BaseEntityManagerFunctionalTestCase {
 	}
 
 	@Test
+	@TestForIssue(jiraKey = "HHH-9419")
+	public void testNoVersionCheckAfterRemove() {
+		EntityManager em = getOrCreateEntityManager();
+		em.getTransaction().begin();
+		Lockable lock = new Lockable( "name" );
+		em.persist( lock );
+		em.getTransaction().commit();
+		em.close();
+		Integer initial = lock.getVersion();
+		assertNotNull( initial );
+
+		em = getOrCreateEntityManager();
+		em.getTransaction().begin();
+		Lockable reread = em.createQuery( "from Lockable", Lockable.class )
+				.setLockMode( LockModeType.OPTIMISTIC )
+				.getSingleResult();
+		assertEquals( initial, reread.getVersion() );
+		assertTrue( em.unwrap( SessionImpl.class ).getActionQueue().hasBeforeTransactionActions() );
+		em.remove( reread );
+		em.getTransaction().commit();
+		em.close();
+		assertEquals( initial, reread.getVersion() );
+	}
+
+	@Test
 	public void testOptimisticSpecific() {
 		EntityManager em = getOrCreateEntityManager();
 		em.getTransaction().begin();

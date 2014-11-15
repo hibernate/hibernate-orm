439ec926270f6b9a16a5c6756d4eb41b963b8058
Sanne Grinovero <sanne@hibernate.org>
2014-11-15 19:04:17 +0000

    HHH-9498 Using C3P0 connection pool but not setting hibernate.connection.isolation results in a NumberFormatException

--- hibernate-c3p0/src/main/java/org/hibernate/c3p0/internal/C3P0ConnectionProvider.java
+++ hibernate-c3p0/src/main/java/org/hibernate/c3p0/internal/C3P0ConnectionProvider.java
@@ -38,6 +38,7 @@ import org.hibernate.boot.registry.classloading.spi.ClassLoadingException;
 import org.hibernate.cfg.Environment;
 import org.hibernate.engine.jdbc.connections.internal.ConnectionProviderInitiator;
 import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
+import org.hibernate.internal.util.StringHelper;
 import org.hibernate.internal.util.config.ConfigurationHelper;
 import org.hibernate.service.UnknownUnwrapTypeException;
 import org.hibernate.service.spi.Configurable;
@@ -214,7 +215,7 @@ public class C3P0ConnectionProvider
 		}
 
 		final String i = (String) props.get( Environment.ISOLATION );
-		if ( i == null ) {
+		if ( StringHelper.isEmpty( i ) ) {
 			isolation = null;
 		}
 		else {

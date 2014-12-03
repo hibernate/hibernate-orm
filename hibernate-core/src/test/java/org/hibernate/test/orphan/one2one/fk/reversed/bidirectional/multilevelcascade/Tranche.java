c068b6f70c84476db7a0cd26e80171ff69e51e90
Gail Badner <gbadner@redhat.com>
2014-12-02 22:06:53 -0800

    HHH-9091 : Cascaded collection delete fails due to wrong execution sequence (test case)
    (cherry picked from commit 3b57f1612d40ae74eb0001aa67e86182e3dfedcd)

--- hibernate-core/src/test/java/org/hibernate/test/orphan/one2one/fk/reversed/bidirectional/multilevelcascade/Tranche.java
+++ hibernate-core/src/test/java/org/hibernate/test/orphan/one2one/fk/reversed/bidirectional/multilevelcascade/Tranche.java
@@ -21,52 +21,36 @@
  * 51 Franklin Street, Fifth Floor
  * Boston, MA  02110-1301  USA
  */
-package org.hibernate.test.orphan.onetomany;
+package org.hibernate.test.orphan.one2one.fk.reversed.bidirectional.multilevelcascade;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 
 @Entity
-@Table(name="feature")
-public class Feature {
-	@Id @GeneratedValue(strategy= GenerationType.AUTO)
-	private Long id;
+public class Tranche {
 
-	@ManyToOne()
-	private Product product;
+    @Id
+    private Long id;
 
-	private String name;
+    @ManyToOne(optional = false, fetch = FetchType.EAGER)
+    private Tranchenmodell tranchenmodell;
 
-	public Feature() {
-	}
 
-	public Feature(Product product) {
-		this.product = product;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public Long getId() {
-		return id;
-	}
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
+    public Tranchenmodell getTranchenmodell() {
+        return tranchenmodell;
+    }
 
-	public Product getProduct() {
-		return product;
-	}
-	public void setProduct(Product product) {
-		this.product = product;
-	}
+    public void setTranchenmodell(Tranchenmodell tranchenmodell) {
+        this.tranchenmodell = tranchenmodell;
+    }
 }
-

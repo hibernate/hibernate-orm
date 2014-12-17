7854bc6769c1f0b73a6b54216fd1586fc19feffa
George Gastaldi <gegastaldi@gmail.com>
2014-12-17 18:29:10 -0200

    HHH-9500: H2Dialect shoud not drop constraints
    The constraints are automatically dropped when the table is also dropped.
    Trying to drop a constraint on a table that doesn't exist present unnecessary error messages.

--- hibernate-core/src/main/java/org/hibernate/dialect/H2Dialect.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/H2Dialect.java
@@ -427,4 +427,11 @@ public class H2Dialect extends Dialect {
 	public boolean supportsTuplesInSubqueries() {
 		return false;
 	}
+	
+	@Override
+	public boolean dropConstraints() {
+		// We don't need to drop constraints before dropping tables, that just leads to error
+		// messages about missing tables when we don't have a schema in the database
+		return false;
+	}	
 }

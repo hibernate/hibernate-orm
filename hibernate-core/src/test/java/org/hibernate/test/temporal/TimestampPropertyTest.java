b4446f368eea107036782802ee6ae62e72a5f3a9
Gail Badner <gbadner@redhat.com>
2014-12-16 22:10:35 -0800

    HHH-8401 : Support fractional seconds on MySQL 5.7 (tweak test cases for 4.2)

--- hibernate-core/src/test/java/org/hibernate/test/temporal/TimestampPropertyTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/temporal/TimestampPropertyTest.java
@@ -36,7 +36,6 @@ import org.junit.Test;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
-import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.Generated;
 import org.hibernate.annotations.GenerationTime;
 import org.hibernate.dialect.MySQLDialect;
@@ -91,54 +90,6 @@ public class TimestampPropertyTest extends BaseCoreFunctionalTestCase {
 		s = openSession();
 		s.getTransaction().begin();
 		final Query queryWithTimestamp = s.createQuery( "from TimestampPropertyTest$Entity where ts=?" ).setTimestamp( 0, eOrig.ts );
-		final Entity eQueriedWithTimestamp = (Entity) queryWithTimestamp.uniqueResult();
-		assertNotNull( eQueriedWithTimestamp );
-		s.getTransaction().commit();
-		s.close();
-
-		s = openSession();
-		s.getTransaction().begin();
-		s.delete( eQueriedWithTimestamp );
-		s.getTransaction().commit();
-		s.close();
-	}
-
-	@Test
-	public void testTimeGeneratedByColumnDefault() {
-		final Entity eOrig = new Entity();
-
-		Session s = openSession();
-		s.getTransaction().begin();
-		s.persist( eOrig );
-		s.getTransaction().commit();
-		s.close();
-
-		assertNotNull( eOrig.tsColumnDefault );
-
-		s = openSession();
-		s.getTransaction().begin();
-		final Entity eGotten = (Entity) s.get( Entity.class, eOrig.id );
-		final String tsColumnDefaultOrigFormatted = timestampFormat.format( eOrig.tsColumnDefault );
-		final String tsColumnDefaultGottenFormatted = timestampFormat.format( eGotten.tsColumnDefault );
-		assertEquals( tsColumnDefaultOrigFormatted , tsColumnDefaultGottenFormatted );
-		s.getTransaction().commit();
-		s.close();
-
-		s = openSession();
-		s.getTransaction().begin();
-		final Query queryWithParameter =
-				s.createQuery( "from TimestampPropertyTest$Entity where tsColumnDefault=?" )
-						.setParameter( 0, eOrig.tsColumnDefault );
-		final Entity eQueriedWithParameter = (Entity) queryWithParameter.uniqueResult();
-		assertNotNull( eQueriedWithParameter );
-		s.getTransaction().commit();
-		s.close();
-
-		s = openSession();
-		s.getTransaction().begin();
-		final Query queryWithTimestamp =
-				s.createQuery( "from TimestampPropertyTest$Entity where tsColumnDefault=?" )
-						.setTimestamp( 0, eOrig.tsColumnDefault );
 		final Entity eQueriedWithTimestamp = (Entity) queryWithTimestamp.uniqueResult();
 		assertNotNull( eQueriedWithTimestamp );
 		s.getTransaction().commit();
@@ -213,11 +164,6 @@ public class TimestampPropertyTest extends BaseCoreFunctionalTestCase {
 
 		@Temporal( value = TemporalType.TIMESTAMP)
 		private Date ts;
-
-		@Temporal( value = TemporalType.TIMESTAMP )
-		@Generated( value = GenerationTime.INSERT )
-		@ColumnDefault( value = "CURRENT_TIMESTAMP" )
-		private Date tsColumnDefault;
 
 		@Temporal( value = TemporalType.TIMESTAMP )
 		@Generated( value = GenerationTime.INSERT )

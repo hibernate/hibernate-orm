0ca0f7290c16761e4289c52319e5d6ea11e14df0
Petar Tahchiev <paranoiabla@gmail.com>
2015-01-06 14:00:05 +0200

    fixing the codestyle

--- hibernate-core/src/main/java/org/hibernate/mapping/Constraint.java
+++ hibernate-core/src/main/java/org/hibernate/mapping/Constraint.java
@@ -23,11 +23,6 @@
  */
 package org.hibernate.mapping;
 
-import org.hibernate.HibernateException;
-import org.hibernate.annotations.common.util.StringHelper;
-import org.hibernate.dialect.Dialect;
-import org.hibernate.engine.spi.Mapping;
-
 import java.io.Serializable;
 import java.math.BigInteger;
 import java.security.MessageDigest;
@@ -38,6 +33,11 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
+import org.hibernate.HibernateException;
+import org.hibernate.annotations.common.util.StringHelper;
+import org.hibernate.dialect.Dialect;
+import org.hibernate.engine.spi.Mapping;
+
 /**
  * A relational constraint.
  *
@@ -46,211 +46,239 @@ import java.util.List;
  */
 public abstract class Constraint implements RelationalModel, Serializable {
 
-    private String name;
+	private String name;
 
-    private final ArrayList<Column> columns = new ArrayList<Column>();
+	private final ArrayList<Column> columns = new ArrayList<Column>();
 
-    private Table table;
+	private Table table;
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    /**
-     * If a constraint is not explicitly named, this is called to generate
-     * a unique hash using the table and column names.
-     * Static so the name can be generated prior to creating the Constraint.
-     * They're cached, keyed by name, in multiple locations.
-     *
-     * @param prefix  Appended to the beginning of the generated name
-     * @param table
-     * @param columns
-     * @return String The generated name
-     */
-    public static String generateName(String prefix, String referencedColumnName, Table table, Column... columns) {
-        // Use a concatenation that guarantees uniqueness, even if identical names
-        // exist between all table and column identifiers.
+	/**
+	 * If a constraint is not explicitly named, this is called to generate
+	 * a unique hash using the table and column names.
+	 * Static so the name can be generated prior to creating the Constraint.
+	 * They're cached, keyed by name, in multiple locations.
+	 *
+	 * @param prefix Appended to the beginning of the generated name
+	 * @param table
+	 * @param columns
+	 *
+	 * @return String The generated name
+	 */
+	public static String generateName(String prefix, String referencedColumnName, Table table, Column... columns) {
+		// Use a concatenation that guarantees uniqueness, even if identical names
+		// exist between all table and column identifiers.
 
-        StringBuilder sb = new StringBuilder("table`" + table.getName() + "`");
+		StringBuilder sb = new StringBuilder( "table`" + table.getName() + "`" );
 
-        // Ensure a consistent ordering of columns, regardless of the order
-        // they were bound.
-        // Clone the list, as sometimes a set of order-dependent Column
-        // bindings are given.
-        Column[] alphabeticalColumns = columns.clone();
-        Arrays.sort(alphabeticalColumns, ColumnComparator.INSTANCE);
-        for (Column column : alphabeticalColumns) {
-            String columnName = column == null ? "" : column.getName();
-            sb.append("column`" + columnName + "`");
-        }
+		// Ensure a consistent ordering of columns, regardless of the order
+		// they were bound.
+		// Clone the list, as sometimes a set of order-dependent Column
+		// bindings are given.
+		Column[] alphabeticalColumns = columns.clone();
+		Arrays.sort( alphabeticalColumns, ColumnComparator.INSTANCE );
+		for ( Column column : alphabeticalColumns ) {
+			String columnName = column == null ? "" : column.getName();
+			sb.append( "column`" + columnName + "`" );
+		}
 
-        if (referencedColumnName != null) {
-            sb.append("column`" + referencedColumnName + "`");
-        }
+		if ( referencedColumnName != null ) {
+			sb.append( "column`" + referencedColumnName + "`" );
+		}
 
-        return prefix + hashedName(sb.toString());
-    }
+		return prefix + hashedName( sb.toString() );
+	}
 
-    /**
-     * Helper method for {@link #generateName(String, String, Table, Column...)}.
-     *
-     * @param prefix  Appended to the beginning of the generated name
-     * @param table
-     * @param columns
-     * @return String The generated name
-     */
-    public static String generateName(String prefix, Table table, Column... columns) {
-        return generateName(prefix, null, table, columns);
-    }
+	/**
+	 * Helper method for {@link #generateName(String, String, Table, Column...)}.
+	 *
+	 * @param prefix Appended to the beginning of the generated name
+	 * @param table
+	 * @param columns
+	 *
+	 * @return String The generated name
+	 */
+	public static String generateName(String prefix, Table table, Column... columns) {
+		return generateName( prefix, null, table, columns );
+	}
 
-    /**
-     * Helper method for {@link #generateName(String, String, Table, Column...)}.
-     *
-     * @param prefix  Appended to the beginning of the generated name
-     * @param table
-     * @param columns
-     * @return String The generated name
-     */
-    public static String generateName(String prefix, Table table, List<Column> columns) {
-        return generateName(prefix, null, table, columns.toArray(new Column[columns.size()]));
-    }
+	/**
+	 * Helper method for {@link #generateName(String, String, Table, Column...)}.
+	 *
+	 * @param prefix Appended to the beginning of the generated name
+	 * @param table
+	 * @param columns
+	 *
+	 * @return String The generated name
+	 */
+	public static String generateName(String prefix, Table table, List<Column> columns) {
+		return generateName( prefix, null, table, columns.toArray( new Column[columns.size()] ) );
+	}
 
-    /**
-     * Helper method for {@link #generateName(String, String, Table, Column...)}.
-     *
-     * @param prefix  Appended to the beginning of the generated name
-     * @param table
-     * @param columns
-     * @return String The generated name
-     */
-    public static String generateName(String prefix, String referencedColumnName, Table table, List<Column> columns) {
-        return generateName(prefix, referencedColumnName, table, columns.toArray(new Column[columns.size()]));
-    }
+	/**
+	 * Helper method for {@link #generateName(String, String, Table, Column...)}.
+	 *
+	 * @param prefix Appended to the beginning of the generated name
+	 * @param table
+	 * @param columns
+	 *
+	 * @return String The generated name
+	 */
+	public static String generateName(String prefix, String referencedColumnName, Table table, List<Column> columns) {
+		return generateName( prefix, referencedColumnName, table, columns.toArray( new Column[columns.size()] ) );
+	}
 
-    /**
-     * Hash a constraint name using MD5. Convert the MD5 digest to base 35
-     * (full alphanumeric), guaranteeing
-     * that the length of the name will always be smaller than the 30
-     * character identifier restriction enforced by a few dialects.
-     *
-     * @param s The name to be hashed.
-     * @return String The hased name.
-     */
-    public static String hashedName(String s) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            md.reset();
-            md.update(s.getBytes());
-            byte[] digest = md.digest();
-            BigInteger bigInt = new BigInteger(1, digest);
-            // By converting to base 35 (full alphanumeric), we guarantee
-            // that the length of the name will always be smaller than the 30
-            // character identifier restriction enforced by a few dialects.
-            return bigInt.toString(35);
-        } catch (NoSuchAlgorithmException e) {
-            throw new HibernateException("Unable to generate a hashed Constraint name!", e);
-        }
-    }
+	/**
+	 * Hash a constraint name using MD5. Convert the MD5 digest to base 35
+	 * (full alphanumeric), guaranteeing
+	 * that the length of the name will always be smaller than the 30
+	 * character identifier restriction enforced by a few dialects.
+	 *
+	 * @param s The name to be hashed.
+	 *
+	 * @return String The hased name.
+	 */
+	public static String hashedName(String s) {
+		try {
+			MessageDigest md = MessageDigest.getInstance( "MD5" );
+			md.reset();
+			md.update( s.getBytes() );
+			byte[] digest = md.digest();
+			BigInteger bigInt = new BigInteger( 1, digest );
+			// By converting to base 35 (full alphanumeric), we guarantee
+			// that the length of the name will always be smaller than the 30
+			// character identifier restriction enforced by a few dialects.
+			return bigInt.toString( 35 );
+		}
+		catch (NoSuchAlgorithmException e) {
+			throw new HibernateException( "Unable to generate a hashed Constraint name!", e );
+		}
+	}
 
-    private static class ColumnComparator implements Comparator<Column> {
-        public static ColumnComparator INSTANCE = new ColumnComparator();
+	private static class ColumnComparator implements Comparator<Column> {
+		public static ColumnComparator INSTANCE = new ColumnComparator();
 
-        public int compare(Column col1, Column col2) {
-            return col1.getName().compareTo(col2.getName());
-        }
-    }
+		public int compare(Column col1, Column col2) {
+			return col1.getName().compareTo( col2.getName() );
+		}
+	}
 
-    public void addColumn(Column column) {
-        if (!columns.contains(column))
-            columns.add(column);
-    }
+	public void addColumn(Column column) {
+		if ( !columns.contains( column ) ) {
+			columns.add( column );
+		}
+	}
 
-    public void addColumns(Iterator columnIterator) {
-        while (columnIterator.hasNext()) {
-            Selectable col = (Selectable) columnIterator.next();
-            if (!col.isFormula())
-                addColumn((Column) col);
-        }
-    }
+	public void addColumns(Iterator columnIterator) {
+		while ( columnIterator.hasNext() ) {
+			Selectable col = (Selectable) columnIterator.next();
+			if ( !col.isFormula() ) {
+				addColumn( (Column) col );
+			}
+		}
+	}
 
-    /**
-     * @param column
-     * @return true if this constraint already contains a column with same name.
-     */
-    public boolean containsColumn(Column column) {
-        return columns.contains(column);
-    }
+	/**
+	 * @param column
+	 *
+	 * @return true if this constraint already contains a column with same name.
+	 */
+	public boolean containsColumn(Column column) {
+		return columns.contains( column );
+	}
 
-    public int getColumnSpan() {
-        return columns.size();
-    }
+	public int getColumnSpan() {
+		return columns.size();
+	}
 
-    public Column getColumn(int i) {
-        return columns.get(i);
-    }
+	public Column getColumn(int i) {
+		return columns.get( i );
+	}
 
-    //todo duplicated method, remove one
-    public Iterator<Column> getColumnIterator() {
-        return columns.iterator();
-    }
+	//todo duplicated method, remove one
+	public Iterator<Column> getColumnIterator() {
+		return columns.iterator();
+	}
 
-    public Iterator<Column> columnIterator() {
-        return columns.iterator();
-    }
+	public Iterator<Column> columnIterator() {
+		return columns.iterator();
+	}
 
-    public Table getTable() {
-        return table;
-    }
+	public Table getTable() {
+		return table;
+	}
 
-    public void setTable(Table table) {
-        this.table = table;
-    }
+	public void setTable(Table table) {
+		this.table = table;
+	}
 
-    public boolean isGenerated(Dialect dialect) {
-        return true;
-    }
+	public boolean isGenerated(Dialect dialect) {
+		return true;
+	}
 
-    public String sqlDropString(Dialect dialect, String defaultCatalog, String defaultSchema) {
-        if (isGenerated(dialect)) {
-            return new StringBuilder().append("alter table ").append(getTable().getQualifiedName(dialect, defaultCatalog, defaultSchema)).append(
-                            " drop constraint ").append(dialect.quote(getName())).toString();
-        } else {
-            return null;
-        }
-    }
+	public String sqlDropString(Dialect dialect, String defaultCatalog, String defaultSchema) {
+		if ( isGenerated( dialect ) ) {
+			return new StringBuilder().append( "alter table " ).append(
+					getTable().getQualifiedName(
+							dialect,
+							defaultCatalog,
+							defaultSchema
+					)
+			).append(
+					" drop constraint "
+			).append( dialect.quote( getName() ) ).toString();
+		}
+		else {
+			return null;
+		}
+	}
 
-    public String sqlCreateString(Dialect dialect, Mapping p, String defaultCatalog, String defaultSchema) {
-        if (isGenerated(dialect)) {
-            // Certain dialects (ex: HANA) don't support FKs as expected, but other constraints can still be created.
-            // If that's the case, hasAlterTable() will be true, but getAddForeignKeyConstraintString will return
-            // empty string.  Prevent blank "alter table" statements.
-            String constraintString = sqlConstraintString(dialect, getName(), defaultCatalog, defaultSchema);
-            if (!StringHelper.isEmpty(constraintString)) {
-                StringBuilder buf = new StringBuilder("alter table ").append(getTable().getQualifiedName(dialect, defaultCatalog, defaultSchema)).append(
-                                constraintString);
-                return buf.toString();
-            }
-        }
-        return null;
-    }
+	public String sqlCreateString(Dialect dialect, Mapping p, String defaultCatalog, String defaultSchema) {
+		if ( isGenerated( dialect ) ) {
+			// Certain dialects (ex: HANA) don't support FKs as expected, but other constraints can still be created.
+			// If that's the case, hasAlterTable() will be true, but getAddForeignKeyConstraintString will return
+			// empty string.  Prevent blank "alter table" statements.
+			String constraintString = sqlConstraintString( dialect, getName(), defaultCatalog, defaultSchema );
+			if ( !StringHelper.isEmpty( constraintString ) ) {
+				StringBuilder buf = new StringBuilder( "alter table " ).append(
+						getTable().getQualifiedName(
+								dialect,
+								defaultCatalog,
+								defaultSchema
+						)
+				).append(
+						constraintString
+				);
+				return buf.toString();
+			}
+		}
+		return null;
+	}
 
-    public List getColumns() {
-        return columns;
-    }
+	public List getColumns() {
+		return columns;
+	}
 
-    public abstract String sqlConstraintString(Dialect d, String constraintName, String defaultCatalog, String defaultSchema);
+	public abstract String sqlConstraintString(
+			Dialect d,
+			String constraintName,
+			String defaultCatalog,
+			String defaultSchema);
 
-    public String toString() {
-        return getClass().getName() + '(' + getTable().getName() + getColumns() + ") as " + name;
-    }
+	public String toString() {
+		return getClass().getName() + '(' + getTable().getName() + getColumns() + ") as " + name;
+	}
 
-    /**
-     * @return String The prefix to use in generated constraint names.  Examples:
-     * "UK_", "FK_", and "PK_".
-     */
-    public abstract String generatedConstraintNamePrefix();
+	/**
+	 * @return String The prefix to use in generated constraint names.  Examples:
+	 * "UK_", "FK_", and "PK_".
+	 */
+	public abstract String generatedConstraintNamePrefix();
 }

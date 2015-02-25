fa157745b26217c5eeb4fbb7ba99ae545a75d112
Steve Ebersole <steve@hibernate.org>
2015-02-25 14:00:31 -0600

    HHH-9633 - Add tests that explicitly test the "main" NamingStrategy impls

---
+++ hibernate-core/src/test/java/org/hibernate/test/namingstrategy/complete/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Defines tests of the "main" NamingStrategies, mainly so that we can leverage this
+ * in the 5.0 work to assert that the same names are produced.
+ */
+package org.hibernate.test.namingstrategy.complete;

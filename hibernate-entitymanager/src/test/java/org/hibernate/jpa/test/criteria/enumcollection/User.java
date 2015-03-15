6718079a6e5ca7a23e129cdaa3dc78a55c4a3a11
obr <obr@xima.de>
2015-03-15 20:00:50 +0100

    HHH-9605: JPA Criteria Test for querying a collection of enums

---
+++ hibernate-entitymanager/src/test/java/org/hibernate/jpa/test/criteria/enumcollection/User.java
@@ -0,0 +1,69 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2015, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.jpa.test.criteria.enumcollection;
+
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class User {
+	private Long id;
+	private Set<Role> roles = new HashSet<Role>();
+
+	@Id
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Enumerated(EnumType.STRING)
+	@ElementCollection(targetClass = Role.class, fetch = FetchType.LAZY)
+	@JoinTable(name = "user_role",
+			   joinColumns = {@JoinColumn(name = "user_id")})
+	@Column(name = "role_name", nullable = false)
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+	public enum Role {
+		Admin;
+	}
+}
+

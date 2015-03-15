2c8ded5ff3244590ca30eef6e3f5fabc702d0139
obr <obr@xima.de>
2015-03-15 20:02:21 +0100

    HHH-9605: Test for querying a collection of enums

--- hibernate-core/src/test/java/org/hibernate/test/annotations/enumerated/EnumeratedTypeTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/annotations/enumerated/EnumeratedTypeTest.java
@@ -16,6 +16,8 @@ import org.hibernate.mapping.PersistentClass;
 import org.hibernate.type.EnumType;
 import org.hibernate.type.Type;
 
+import org.junit.Test;
+
 import org.hibernate.testing.SkipForDialect;
 import org.hibernate.testing.TestForIssue;
 import org.hibernate.testing.junit4.BaseNonConfigCoreFunctionalTestCase;
@@ -23,7 +25,6 @@ import org.hibernate.test.annotations.enumerated.EntityEnum.Common;
 import org.hibernate.test.annotations.enumerated.EntityEnum.FirstLetter;
 import org.hibernate.test.annotations.enumerated.EntityEnum.LastNumber;
 import org.hibernate.test.annotations.enumerated.EntityEnum.Trimmed;
-import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
@@ -401,6 +402,47 @@ public class EnumeratedTypeTest extends BaseNonConfigCoreFunctionalTestCase {
 		s.getTransaction().commit();
 		s.close();
 	}
+
+
+	@Test
+	@TestForIssue(jiraKey = "HHH-9605")
+	public void testSet() throws SQLException {
+
+		// **************
+		Session session = openSession();
+		session.getTransaction().begin();
+
+		// persist
+		EntityEnum entityEnum = new EntityEnum();
+		entityEnum.setString( Common.B2 );
+		entityEnum.getSet().add( Common.B2 );
+		Serializable id = session.save( entityEnum );
+
+		session.getTransaction().commit();
+		session.close();
+		session = openSession();
+		session.getTransaction().begin();
+
+		String sql = "select e from EntityEnum e where :param in elements( e.set ) ";
+		Query queryObject = session.createQuery( sql );
+		queryObject.setParameter( "param", Common.B2 );
+
+		// ensure EnumType can do #fromName with the trimming
+		List<EntityEnum> resultList = queryObject.list();
+		assertEquals( resultList.size(), 1 );
+		entityEnum = resultList.get( 0 );
+
+		assertEquals( id, entityEnum.getId() );
+		assertEquals( Common.B2, entityEnum.getSet().iterator().next() );
+
+		// delete
+		assertEquals( 1, session.createSQLQuery( "DELETE FROM set_enum" ).executeUpdate() );
+		assertEquals( 1, session.createSQLQuery( "DELETE FROM EntityEnum" ).executeUpdate() );
+
+		session.getTransaction().commit();
+		session.close();
+	}
+
 
 	@Override
 	protected Class[] getAnnotatedClasses() {

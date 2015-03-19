c543cdbc078a6cf57f5be4b071c4c8ac45a9153e
Yann Diorcet <diorcet.yann@gmail.com>
2015-03-19 11:21:46 +0100

    DDL typo fix: Add only one space before unique keyword

--- hibernate-core/src/main/java/org/hibernate/dialect/unique/DefaultUniqueDelegate.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/unique/DefaultUniqueDelegate.java
@@ -58,7 +58,7 @@ public class DefaultUniqueDelegate implements UniqueDelegate {
 
 	protected String uniqueConstraintSql(UniqueKey uniqueKey) {
 		final StringBuilder sb = new StringBuilder();
-		sb.append( " unique (" );
+		sb.append( "unique (" );
 		final Iterator<org.hibernate.mapping.Column> columnIterator = uniqueKey.columnIterator();
 		while ( columnIterator.hasNext() ) {
 			final org.hibernate.mapping.Column column = columnIterator.next();

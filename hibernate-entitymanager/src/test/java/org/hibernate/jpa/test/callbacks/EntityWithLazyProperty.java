e7475f12b0eafd37c7134b6643e0ade36b9716a0
ballm <martin.ball@grg.com>
2015-03-27 14:50:31 +0000

    HHH-7573 Added check for handle LazyPropertyInitializer.UNFETCHED_PROPERTY
    Added test to check that the change handles the various possible scenarios.
    Test requires build time instrumentation.

---
+++ hibernate-entitymanager/src/test/java/org/hibernate/jpa/test/callbacks/EntityWithLazyProperty.java
@@ -0,0 +1,63 @@
+package org.hibernate.jpa.test.callbacks;
+
+import javax.persistence.*;
+
+/**
+ * Test entity with a lazy property which requires build time instrumentation.
+ */
+@Entity
+public class EntityWithLazyProperty {
+
+    public static final byte[] PRE_UPDATE_VALUE = new byte[]{0x2A, 0x2A, 0x2A, 0x2A};
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Basic(fetch = FetchType.LAZY)
+    private byte[] lazyData;
+
+    private String someField;
+
+    private boolean updateLazyFieldInPreUpdate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public byte[] getLazyData() {
+        return lazyData;
+    }
+
+    public void setLazyData(final byte[] lazyData) {
+        this.lazyData = lazyData;
+    }
+
+    public String getSomeField() {
+        return someField;
+    }
+
+    public void setSomeField(String someField) {
+        this.someField = someField;
+    }
+
+    public boolean isUpdateLazyFieldInPreUpdate() {
+        return updateLazyFieldInPreUpdate;
+    }
+
+    public void setUpdateLazyFieldInPreUpdate(boolean updateLazyFieldInPreUpdate) {
+        this.updateLazyFieldInPreUpdate = updateLazyFieldInPreUpdate;
+    }
+
+    @PreUpdate
+    public void onPreUpdate() {
+        //Allow the update of the lazy field from within the pre update to check that this does not break things.
+        if(isUpdateLazyFieldInPreUpdate()) {
+            this.setLazyData(PRE_UPDATE_VALUE);
+        }
+    }
+}

4541f7b5011d3b952de18c6adc2ec0e6d843cda9
Steve Ebersole <steve@hibernate.org>
2015-04-20 15:21:37 -0500

    HHH-9727 - Slight redesign to bootstrapping APIs to allow for OGM bootstrapping

--- hibernate-core/src/main/java/org/hibernate/boot/registry/StandardServiceRegistryBuilder.java
+++ hibernate-core/src/main/java/org/hibernate/boot/registry/StandardServiceRegistryBuilder.java
@@ -286,15 +286,14 @@ public class StandardServiceRegistryBuilder {
 	 */
 	@SuppressWarnings("unchecked")
 	public StandardServiceRegistry build() {
-		final Map settingsCopy = new HashMap();
-		settingsCopy.putAll( settings );
-		Environment.verifyProperties( settingsCopy );
-		ConfigurationHelper.resolvePlaceHolders( settingsCopy );
-
 		applyServiceContributingIntegrators();
 		applyServiceContributors();
 
+		final Map settingsCopy = new HashMap();
+		settingsCopy.putAll( settings );
 		settingsCopy.put( org.hibernate.boot.cfgxml.spi.CfgXmlAccessService.LOADED_CONFIG_KEY, aggregatedCfgXml );
+		Environment.verifyProperties( settingsCopy );
+		ConfigurationHelper.resolvePlaceHolders( settingsCopy );
 
 		return new StandardServiceRegistryImpl(
 				autoCloseRegistry,

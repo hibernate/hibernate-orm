58fd078e5022beff6df0a4522c801ffd060b7e95
barreiro <lbbbarreiro@gmail.com>
2015-05-22 03:30:12 +0100

    HHH-9811 - Enforce uniqueness on EntityPersister#resolveAttributeIndexes

--- hibernate-core/src/main/java/org/hibernate/persister/entity/AbstractEntityPersister.java
+++ hibernate-core/src/main/java/org/hibernate/persister/entity/AbstractEntityPersister.java
@@ -1869,16 +1869,30 @@ public abstract class AbstractEntityPersister
 
 	@Override
 	public int[] resolveAttributeIndexes(String[] attributeNames) {
+		if ( attributeNames == null || attributeNames.length == 0 ) {
+			return new int[0];
+		}
 		int[] fields = new int[attributeNames.length];
 		int counter = 0;
 
-		for ( String attribute : attributeNames ) {
-			Integer index = entityMetamodel.getPropertyIndexOrNull( attribute );
-			if ( index != null ) {
-				fields[counter++] = index;
+		// We sort to get rid of duplicates
+		Arrays.sort( attributeNames );
+
+		Integer index0 = entityMetamodel.getPropertyIndexOrNull( attributeNames[0] );
+		if ( index0 != null ) {
+			fields[counter++] = index0;
+		}
+
+		for ( int i = 0, j = 1; j < attributeNames.length; ++i, ++j ) {
+			if ( !attributeNames[i].equals( attributeNames[j] ) ) {
+				Integer index = entityMetamodel.getPropertyIndexOrNull( attributeNames[j] );
+				if ( index != null ) {
+					fields[counter++] = index;
+				}
 			}
 		}
-		return fields;
+
+		return Arrays.copyOf( fields, counter );
 	}
 
 	protected String[] getSubclassPropertySubclassNameClosure() {

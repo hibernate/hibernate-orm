fdd7fb80807b18b12a7c417bf15b862bf38e2efb
Andrea Boriero <andrea@hibernate.org>
2015-05-26 12:25:54 +0100

    HHH-9798 - Add test for Unique constraint of @JoinColumn in @JoinTable not generated

---
+++ hibernate-core/src/test/java/org/hibernate/test/annotations/onetoone/hhh9798/Item.java
@@ -0,0 +1,41 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.test.annotations.onetoone.hhh9798;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Item {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	protected Long id;
+
+	protected String name;
+
+	public Item() {
+	}
+
+	public Item(String name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}

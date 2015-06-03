16351924b025ceebcb50bd8333d9061812bcde8f
Radim Vansa <rvansa@redhat.com>
2015-06-03 10:10:04 +0200

    HHH-9800 Numerous hibernate-infinispan tests continue to fail transiently The original cause was JGRP-1931, but this replaces fixed delays with eventual asserts (with 10 seconds timeouts).

--- hibernate-infinispan/src/test/java/org/hibernate/test/cache/infinispan/util/CacheTestUtil.java
+++ hibernate-infinispan/src/test/java/org/hibernate/test/cache/infinispan/util/CacheTestUtil.java
@@ -9,6 +9,9 @@ package org.hibernate.test.cache.infinispan.util;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import org.hibernate.boot.internal.SessionFactoryBuilderImpl;
 import org.hibernate.boot.internal.SessionFactoryOptionsImpl;
@@ -18,8 +21,8 @@ import org.hibernate.cache.infinispan.InfinispanRegionFactory;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.engine.config.spi.ConfigurationService;
 import org.hibernate.engine.config.spi.StandardConverters;
+import org.hibernate.internal.util.compare.EqualsHelper;
 import org.hibernate.service.ServiceRegistry;
-
 import org.hibernate.test.cache.infinispan.functional.SingleNodeTestCase;
 
 /**
@@ -135,9 +138,41 @@ public class CacheTestUtil {
    }
 
    /**
+    * Executes {@link #assertEqualsEventually(Object, Callable, long, TimeUnit)} without time limit.
+    * @param expected
+    * @param callable
+    * @param <T>
+    */
+   public static <T> void assertEqualsEventually(T expected, Callable<T> callable) throws Exception {
+      assertEqualsEventually(expected, callable, -1, TimeUnit.SECONDS);
+   }
+
+   /**
+    * Periodically calls callable and compares returned value with expected value. If the value matches to expected,
+    * the method returns. If callable throws an exception, this is propagated. If the returned value does not match to
+    * expected before timeout, {@link TimeoutException} is thrown.
+    * @param expected
+    * @param callable
+    * @param timeout If non-positive, there is no limit.
+    * @param timeUnit
+    * @param <T>
+    */
+   public static <T> void assertEqualsEventually(T expected, Callable<T> callable, long timeout, TimeUnit timeUnit) throws Exception {
+      long now, deadline = timeout <= 0 ? Long.MAX_VALUE : System.currentTimeMillis() + timeUnit.toMillis(timeout);
+      for (;;) {
+         T value = callable.call();
+         if (EqualsHelper.equals(value, expected)) return;
+         now = System.currentTimeMillis();
+         if (now < deadline) {
+            Thread.sleep(Math.min(100, deadline - now));
+         } else break;
+      }
+      throw new TimeoutException();
+   }
+
+   /**
     * Prevent instantiation.
     */
    private CacheTestUtil() {
    }
-
 }

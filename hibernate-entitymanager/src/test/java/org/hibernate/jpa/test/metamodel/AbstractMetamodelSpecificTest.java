70910c3e82e5b276c22927069704d5ba7e414d23
Gail Badner <gbadner@redhat.com>
2015-06-22 17:56:18 -0700

    HHH-9637 : Join is reused when 2 explicit joins are used for the same ToOne association

--- hibernate-entitymanager/src/test/java/org/hibernate/jpa/test/metamodel/AbstractMetamodelSpecificTest.java
+++ hibernate-entitymanager/src/test/java/org/hibernate/jpa/test/metamodel/AbstractMetamodelSpecificTest.java
@@ -15,6 +15,7 @@ public abstract class AbstractMetamodelSpecificTest extends BaseEntityManagerFun
 	public Class[] getAnnotatedClasses() {
 		return new Class[] {
 				Address.class, Alias.class, Country.class, CreditCard.class, Customer.class,
+				Entity1.class, Entity2.class, Entity3.class,
 				Info.class, LineItem.class, Order.class, Phone.class, Product.class,
 				ShelfLife.class, Spouse.class, Thing.class, ThingWithQuantity.class,
 				VersionedEntity.class

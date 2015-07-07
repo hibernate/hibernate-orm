0e9a839ca163a6fdad113a05910860f7b8ab33a1
Andrea Boriero <andrea@hibernate.org>
2015-07-07 16:39:40 +0100

    HHH-9899 : Fix checkstyle errors

--- hibernate-core/src/main/java/org/hibernate/boot/spi/AbstractDelegatingMetadataBuilderImplementor.java
+++ hibernate-core/src/main/java/org/hibernate/boot/spi/AbstractDelegatingMetadataBuilderImplementor.java
@@ -1,7 +1,7 @@
 /*
- * Hibernate OGM, Domain model persistence for NoSQL datastores
+ * Hibernate, Relational Persistence for Idiomatic Java
  *
- * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
 package org.hibernate.boot.spi;

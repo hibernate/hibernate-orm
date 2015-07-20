6f566bc186d812b936074b6819a08bbab3d63955
Craig Andrews <candrews@integralblue.com>
2015-07-20 11:23:28 -0400

    HHH-9954 Arrays should be considered for equality

--- hibernate-core/src/main/java/org/hibernate/internal/util/compare/EqualsHelper.java
+++ hibernate-core/src/main/java/org/hibernate/internal/util/compare/EqualsHelper.java
@@ -24,6 +24,7 @@
  */
 package org.hibernate.internal.util.compare;
 
+import java.util.Arrays;
 
 /**
  * @author Gavin King
@@ -31,7 +32,43 @@ package org.hibernate.internal.util.compare;
 public final class EqualsHelper {
 
 	public static boolean equals(final Object x, final Object y) {
-		return x == y || ( x != null && y != null && x.equals( y ) );
+		if (x == y || (x != null && y != null && x.equals(y))) {
+			return true;
+		}
+		if (x == null && y != null) {
+			return false;
+		}
+		if (x != null && y == null) {
+			return false;
+		}
+		Class<?> xClass = x.getClass();
+		Class<?> yClass = y.getClass();
+		if (xClass.isArray()) {
+			if (xClass != yClass.getClass()) {
+				// different dimensions, ex: int[][] to a int[]
+				return false;
+			}
+			if (x instanceof long[]) {
+				return Arrays.equals((long[]) x, (long[]) y);
+			} else if (x instanceof int[]) {
+				return Arrays.equals((int[]) x, (int[]) y);
+			} else if (x instanceof short[]) {
+				return Arrays.equals((short[]) x, (short[]) y);
+			} else if (x instanceof char[]) {
+				return Arrays.equals((char[]) x, (char[]) y);
+			} else if (x instanceof byte[]) {
+				return Arrays.equals((byte[]) x, (byte[]) y);
+			} else if (x instanceof double[]) {
+				return Arrays.equals((double[]) x, (double[]) y);
+			} else if (x instanceof float[]) {
+				return Arrays.equals((float[]) x, (float[]) y);
+			} else if (x instanceof boolean[]) {
+				return Arrays.equals((boolean[]) x, (boolean[]) y);
+			} else {
+				return Arrays.deepEquals((Object[]) x, (Object[]) y);
+			}
+		}
+		return false;
 	}
 	
 	private EqualsHelper() {}

9626603e7514cbe3950660335556b782b13b529d
Brett Meyer <brett@3riverdev.com>
2015-08-25 08:34:10 -0400

    HHH-10044 checkstyle fix

--- hibernate-osgi/src/main/java/org/hibernate/osgi/OsgiPersistenceProviderService.java
+++ hibernate-osgi/src/main/java/org/hibernate/osgi/OsgiPersistenceProviderService.java
@@ -40,13 +40,13 @@ public class OsgiPersistenceProviderService implements ServiceFactory {
 	public Object getService(Bundle requestingBundle, ServiceRegistration registration) {
 		final OsgiClassLoader osgiClassLoader = new OsgiClassLoader();
 
-        // First, add the client bundle that's requesting the OSGi services.
+		// First, add the client bundle that's requesting the OSGi services.
 		osgiClassLoader.addBundle( requestingBundle );
 
-        // Then, automatically add hibernate-core and hibernate-entitymanager.  These are needed to load resources
-        // contained in those jars, such as em's persistence.xml schemas.
-        osgiClassLoader.addBundle( FrameworkUtil.getBundle( SessionFactory.class ) );
-        osgiClassLoader.addBundle( FrameworkUtil.getBundle( HibernateEntityManagerFactory.class ) );
+		// Then, automatically add hibernate-core and hibernate-entitymanager.  These are needed to load resources
+		// contained in those jars, such as em's persistence.xml schemas.
+		osgiClassLoader.addBundle( FrameworkUtil.getBundle( SessionFactory.class ) );
+		osgiClassLoader.addBundle( FrameworkUtil.getBundle( HibernateEntityManagerFactory.class ) );
 
 		// Some "boot time" code does still rely on TCCL.  "run time" code should all be using
 		// ClassLoaderService now.

9b34880baf1f99e9814e2123938da75cce776cf3
Andrea Boriero <andrea@hibernate.org>
2015-10-16 15:08:15 +0100

    HHH-10180 - Add test for issue

---
+++ hibernate-core/src/test/java/org/hibernate/test/schemaupdate/SchemaUpdateGeneratingOnlyScriptFileTest.java
@@ -0,0 +1,78 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.test.schemaupdate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.File;
+import java.nio.file.Files;
+
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.boot.spi.MetadataImplementor;
+import org.hibernate.cfg.Environment;
+import org.hibernate.tool.hbm2ddl.SchemaUpdate;
+
+import org.junit.Test;
+
+import org.hibernate.testing.TestForIssue;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Andrea Boriero
+ */
+@TestForIssue(jiraKey = "10180")
+public class SchemaUpdateGeneratingOnlyScriptFileTest {
+
+	@Test
+	public void testSchemaUpdateScriptGeneration() throws Exception {
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder()
+				.applySetting( Environment.HBM2DDL_AUTO, "none" )
+				.build();
+		try {
+			File output = File.createTempFile( "update_script", ".sql" );
+			output.deleteOnExit();
+
+			final MetadataImplementor metadata = (MetadataImplementor) new MetadataSources( ssr )
+					.addAnnotatedClass( TestEntity.class )
+					.buildMetadata();
+			metadata.validate();
+
+			SchemaUpdate su = new SchemaUpdate( ssr, metadata );
+			su.setHaltOnError( true );
+			su.setOutputFile( output.getAbsolutePath() );
+			su.setDelimiter( ";" );
+			su.setFormat( true );
+			su.execute( true, false );
+			
+			String fileContent = new String( Files.readAllBytes( output.toPath() ) );
+			assertThat( fileContent.toLowerCase().contains( "create table test_entity" ), is( true ) );
+		}
+		finally {
+			StandardServiceRegistryBuilder.destroy( ssr );
+		}
+	}
+
+	@Entity
+	@Table(name = "test_entity")
+	public static class TestEntity {
+		@Id
+		private String field;
+
+		public String getField() {
+			return field;
+		}
+
+		public void setField(String field) {
+			this.field = field;
+		}
+	}
+}

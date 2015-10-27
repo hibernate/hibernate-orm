6a09dea2e4795c7c7d1b5356498ee0e2c89d46c7
Steve Ebersole <steve@hibernate.org>
2015-10-26 20:19:14 -0500

    HHH-10206 - Primary key not created for a Set after loading from XML mapping file
    (cherry picked from commit e71627ab00046d9a574720f2e72ed3f14fb1f488)

--- hibernate-core/src/test/java/org/hibernate/test/hbm/index/PersonGroup.java
+++ hibernate-core/src/test/java/org/hibernate/test/hbm/index/PersonGroup.java
@@ -6,8 +6,50 @@
  */
 package org.hibernate.test.hbm.index;
 
-/**
- * @author Steve Ebersole
- */
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class PersonGroup {
+	private long id;
+	private String name;
+	private Set<Person> persons = new HashSet<Person>();
+	private Map<String, String> comments = new HashMap<String,String>();
+
+	public PersonGroup(String name) {
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<Person> getPersons() {
+		return persons;
+	}
+
+	public void setPersons(Set<Person> persons) {
+		this.persons = persons;
+	}
+
+	public Map<String, String> getComments() {
+		return comments;
+	}
+
+	public void setComments(Map<String, String> comments) {
+		this.comments = comments;
+	}
 }

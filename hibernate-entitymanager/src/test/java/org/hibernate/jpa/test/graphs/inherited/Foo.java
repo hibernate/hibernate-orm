90b74ad428c6c44f7b925aa97a1426989c3c72dc
Oliver Breidenbach <obr@xima.de>
2015-11-11 06:09:43 +0100

    HHH-10261 - Test showing Exception if subgraph is created on an inherited attribute

---
+++ hibernate-entitymanager/src/test/java/org/hibernate/jpa/test/graphs/inherited/Foo.java
@@ -0,0 +1,18 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.jpa.test.graphs.inherited;
+
+import javax.persistence.Entity;
+
+
+/**
+ * @author Oliver Breidenbach
+ */
+@Entity
+public class Foo extends MappedSupperclass {
+
+}

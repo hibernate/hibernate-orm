95fa4aeb517e7724ed8c6bf6f1df91dbe1e96e65
vladmihalcea <mih_vlad@yahoo.com>
2015-12-02 16:27:13 +0200

    Fixed transformation errors between docbook and adoc for the first two chapters of the mapping docs

---
+++ documentation/src/main/asciidoc/mapping/extras/entity/SimpleEntityWithTable.java
@@ -0,0 +1,14 @@
+@Entity
+@Table( catalog="CRM", schema="purchasing", name="t_simple" )
+public class Simple {
+	@Id
+	private Integer id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+}

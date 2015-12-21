504dd8353db88258c2a12b8c3b607c76e885fdfc
Andrea Boriero <andrea@hibernate.org>
2015-12-21 13:39:16 +0000

    HHH-10385 HHH-10386 - Add tests for issues

---
+++ hibernate-core/src/test/java/org/hibernate/test/schemaupdate/foreignkeys/Phone.java
@@ -0,0 +1,23 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.test.schemaupdate.foreignkeys;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author Andrea Boriero
+ */
+@Entity
+@Table(name = "PHONE")
+public class Phone {
+	@Id
+	private Long id;
+
+	private String number;
+}

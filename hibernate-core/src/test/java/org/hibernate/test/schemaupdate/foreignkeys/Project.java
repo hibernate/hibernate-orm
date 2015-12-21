90c1de55cfc00d92d721674cae452aa38d66bc4c
Andrea Boriero <andrea@hibernate.org>
2015-12-21 13:39:16 +0000

    HHH-10385 HHH-10386 - Add tests for issues

---
+++ hibernate-core/src/test/java/org/hibernate/test/schemaupdate/foreignkeys/Project.java
@@ -0,0 +1,25 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.test.schemaupdate.foreignkeys;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Entity
+@Table(name = "PROJECT")
+public class Project {
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@ManyToMany(mappedBy="projects")
+	private Set<Employee> employees;
+}

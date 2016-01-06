0c38cfc9d7e8f4c49364f91d381817034269628a
Emmanuel Bernard <emmanuel@hibernate.org>
2016-01-06 15:02:38 +0100

    HHH-10419 Detect if the user inappropriately left  @Convert.converter to void

--- hibernate-core/src/main/java/org/hibernate/cfg/AbstractPropertyHolder.java
+++ hibernate-core/src/main/java/org/hibernate/cfg/AbstractPropertyHolder.java
@@ -94,10 +94,24 @@ public abstract class AbstractPropertyHolder implements PropertyHolder {
 	}
 
 	protected IllegalStateException buildExceptionFromInstantiationError(AttributeConversionInfo info, Exception e) {
-		return new IllegalStateException(
-				String.format( "Unable to instantiate AttributeConverter [%s]", info.getConverterClass().getName() ),
-				e
-		);
+		if ( void.class.equals( info.getConverterClass() ) ) {
+			// the user forgot to set @Convert.converter
+			// we already know it's not a @Convert.disableConversion
+			return new IllegalStateException(
+					"Unable to instantiate AttributeConverter: you left @Convert.converter to its default value void.",
+					e
+			);
+
+		}
+		else {
+			return new IllegalStateException(
+					String.format(
+							"Unable to instantiate AttributeConverter [%s]",
+							info.getConverterClass().getName()
+					),
+					e
+			);
+		}
 	}
 
 	protected AttributeConverterDescriptor makeAttributeConverterDescriptor(AttributeConversionInfo conversion) {

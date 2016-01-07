3580b4dd9b14836656b589d77e3dfd7746839efc
Andrea Boriero <andrea@hibernate.org>
2016-01-07 10:19:51 +0000

    HHH-10420 - Add test for issue

--- hibernate-core/src/test/java/org/hibernate/test/schemaupdate/foreignkeys/ForeignKeyGenerationTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/schemaupdate/foreignkeys/ForeignKeyGenerationTest.java
@@ -1,8 +1,8 @@
 /*
  * Hibernate, Relational Persistence for Idiomatic Java
  *
- * License: Apache License, Version 2.0
- * See the LICENSE file in the root directory or visit http://www.apache.org/licenses/LICENSE-2.0
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
 package org.hibernate.test.schemaupdate.foreignkeys;
 

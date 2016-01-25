c721bbe8f9922dfa655d0380212127153038e959
Steve Ebersole <steve@hibernate.org>
2016-01-25 10:18:27 -0600

    HHH-9876 - Ability to filter objects from Database for schema tooling

--- hibernate-core/src/main/java/org/hibernate/tool/schema/internal/DefaultSchemaFilter.java
+++ hibernate-core/src/main/java/org/hibernate/tool/schema/internal/DefaultSchemaFilter.java
@@ -1,3 +1,9 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
 package org.hibernate.tool.schema.internal;
 
 import org.hibernate.boot.model.relational.Namespace;
@@ -5,6 +11,9 @@ import org.hibernate.boot.model.relational.Sequence;
 import org.hibernate.mapping.Table;
 import org.hibernate.tool.schema.spi.SchemaFilter;
 
+/**
+ * Default implementation of the SchemaFilter contract, which is to just include everything.
+ */
 public class DefaultSchemaFilter implements SchemaFilter {
 	public static final DefaultSchemaFilter INSTANCE = new DefaultSchemaFilter();
 

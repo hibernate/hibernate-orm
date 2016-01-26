3b4c167482a2f9786c2831bf31e427e328f8c59e
Steve Ebersole <steve@hibernate.org>
2016-01-26 16:41:53 -0600

    HHH-8706 - Delay registering of the entity listener class as long as possible

--- hibernate-entitymanager/src/main/java/org/hibernate/jpa/event/internal/jpa/ListenerFactoryBeanManagerDelayedImpl.java
+++ hibernate-entitymanager/src/main/java/org/hibernate/jpa/event/internal/jpa/ListenerFactoryBeanManagerDelayedImpl.java
@@ -41,6 +41,12 @@ public class ListenerFactoryBeanManagerDelayedImpl implements ListenerFactory {
 	 */
 	@SuppressWarnings("unused")
 	public static ListenerFactoryBeanManagerDelayedImpl fromBeanManagerReference(Object reference) {
+		if ( !BeanManager.class.isInstance( reference ) ) {
+			throw new IllegalArgumentException(
+					"Expecting BeanManager reference that implements CDI BeanManager contract : " +
+							reference
+			);
+		}
 		return new ListenerFactoryBeanManagerDelayedImpl( (BeanManager) reference );
 	}
 

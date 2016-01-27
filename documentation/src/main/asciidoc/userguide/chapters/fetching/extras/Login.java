d30bdcef0db1a759b9a928929f2c6782ccf6d9cc
vladmihalcea <mih_vlad@yahoo.com>
2016-01-27 16:26:20 +0200

    Migrate User Guide Fetching chapter extras to test folder

--- documentation/src/main/asciidoc/userguide/chapters/fetching/extras/Login.java
+++
@@ -1,4 +0,0 @@
-Employee employee=( Employee )session.createQuery(
-    "select e from Employee e where e.userid = :userid and e.password = :password" )
-    ...
-    .uniqueResult();
--- documentation/src/main/asciidoc/userguide/chapters/fetching/extras/ProjectsForAnEmployeeHql.java
+++
@@ -1,5 +0,0 @@
-String userid = ...;
-Employee e = ( Employee )session.createQuery(
-    "select e from Employee e join fetch e.projects where e.userid = :userid" )
-    .setParameter( "userid", userid )
-    .uniqueResult();

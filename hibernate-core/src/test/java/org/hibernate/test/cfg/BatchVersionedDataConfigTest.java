55ba985adc3b3c5340c1d35b6bef7f01eb8ee30c
Martin Simka <msimka@redhat.com>
2016-01-28 14:48:05 +0100

    [HHH-10290] ignore environment property hibernate.jdbc.batch_versioned_data in BatchVersionedDataConfigTest

--- hibernate-core/src/test/java/org/hibernate/test/cfg/BatchVersionedDataConfigTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/cfg/BatchVersionedDataConfigTest.java
@@ -54,6 +54,12 @@ public class BatchVersionedDataConfigTest extends BaseUnitTestCase {
 	@Before
 	public void setUp() {
 		cfg = new Configuration();
+
+		// HHH-10290 ignore environment property hibernate.jdbc.batch_versioned_data
+		if (cfg.getProperties().getProperty(AvailableSettings.BATCH_VERSIONED_DATA) != null) {
+			cfg.getProperties().remove(AvailableSettings.BATCH_VERSIONED_DATA);
+			cfg.getStandardServiceRegistryBuilder().getSettings().remove(AvailableSettings.BATCH_VERSIONED_DATA);
+		}
 	}
 
 	@After

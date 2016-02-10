5963541755460f55813eccc2ed889aeb89742ed6
Steve Ebersole <steve@hibernate.org>
2016-02-09 22:35:12 -0600

    HHH-10345 - HHH-3422 still a problem with oracle

--- hibernate-core/src/main/java/org/hibernate/dialect/Oracle12cDialect.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/Oracle12cDialect.java
@@ -6,12 +6,18 @@
  */
 package org.hibernate.dialect;
 
+import org.hibernate.boot.model.TypeContributions;
 import org.hibernate.cfg.Environment;
 import org.hibernate.dialect.identity.IdentityColumnSupport;
 import org.hibernate.dialect.identity.Oracle12cIdentityColumnSupport;
 import org.hibernate.dialect.pagination.LimitHandler;
 import org.hibernate.dialect.pagination.SQL2008StandardLimitHandler;
+import org.hibernate.engine.config.spi.ConfigurationService;
+import org.hibernate.engine.config.spi.StandardConverters;
 import org.hibernate.id.enhanced.SequenceStyleGenerator;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.type.MaterializedBlobType;
+import org.hibernate.type.WrappedMaterializedBlobType;
 
 /**
  * An SQL dialect for Oracle 12c.
@@ -19,9 +25,29 @@ import org.hibernate.id.enhanced.SequenceStyleGenerator;
  * @author zhouyanming (zhouyanming@gmail.com)
  */
 public class Oracle12cDialect extends Oracle10gDialect {
+	public static final String PREFER_LONG_RAW = "hibernate.dialect.oracle.prefer_long_raw";
+
 	public Oracle12cDialect() {
 		super();
 		getDefaultProperties().setProperty( Environment.BATCH_VERSIONED_DATA, "true" );
+	}
+
+	@Override
+	public void contributeTypes(TypeContributions typeContributions, ServiceRegistry serviceRegistry) {
+		super.contributeTypes( typeContributions, serviceRegistry );
+
+		// account for Oracle's deprecated support for LONGVARBINARY...
+		// 		prefer BLOB, unless the user opts out of it
+		boolean preferLong = serviceRegistry.getService( ConfigurationService.class ).getSetting(
+				PREFER_LONG_RAW,
+				StandardConverters.BOOLEAN,
+				false
+		);
+
+		if ( !preferLong ) {
+			typeContributions.contributeType( MaterializedBlobType.INSTANCE, "byte[]", byte[].class.getName() );
+			typeContributions.contributeType( WrappedMaterializedBlobType.INSTANCE, "Byte[]", Byte[].class.getName() );
+		}
 	}
 
 	@Override

b5a86ea45c0dd38a0f6486059be9ade4d0bcf448
Gail Badner <gbadner@redhat.com>
2016-02-10 00:10:52 -0800

    HHH-5855 : checkstyle corrections

--- hibernate-core/src/main/java/org/hibernate/collection/internal/PersistentSet.java
+++ hibernate-core/src/main/java/org/hibernate/collection/internal/PersistentSet.java
@@ -14,7 +14,6 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import org.hibernate.HibernateException;

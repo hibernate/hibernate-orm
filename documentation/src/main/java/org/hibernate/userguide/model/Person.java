2167b442a420260393060f5d29c3894814ba81d8
Vlad Mihalcea <mih_vlad@yahoo.com>
2016-02-10 18:07:48 +0200

    Migrate User Guide Native chapter extras to test folder up to stored procedures

--- documentation/src/main/java/org/hibernate/userguide/model/Person.java
+++ documentation/src/main/java/org/hibernate/userguide/model/Person.java
@@ -1,3 +1,9 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
 package org.hibernate.userguide.model;
 
 import java.util.ArrayList;
@@ -6,16 +12,24 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.persistence.CascadeType;
+import javax.persistence.ColumnResult;
+import javax.persistence.ConstructorResult;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.EntityResult;
 import javax.persistence.EnumType;
+import javax.persistence.FieldResult;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MapKeyEnumerated;
+import javax.persistence.NamedNativeQueries;
+import javax.persistence.NamedNativeQuery;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.OrderColumn;
+import javax.persistence.SqlResultSetMapping;
+import javax.persistence.SqlResultSetMappings;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Version;
@@ -23,6 +37,115 @@ import javax.persistence.Version;
 /**
  * @author Vlad Mihalcea
  */
+@NamedNativeQueries({
+    //tag::sql-scalar-NamedNativeQuery-example[]
+    @NamedNativeQuery(
+        name = "find_person_name",
+        query =
+            "SELECT name " +
+            "FROM person "
+    ),
+    //end::sql-scalar-NamedNativeQuery-example[]
+    //tag::sql-multiple-scalar-values-NamedNativeQuery-example[]
+    @NamedNativeQuery(
+        name = "find_person_name_and_nickName",
+        query =
+            "SELECT " +
+            "   name, " +
+            "   nickName " +
+            "FROM person "
+    ),
+    //end::sql-multiple-scalar-values-NamedNativeQuery-example[]
+    // tag::sql-multiple-scalar-values-dto-NamedNativeQuery-example[]
+    @NamedNativeQuery(
+        name = "find_person_name_and_nickName_dto",
+        query =
+            "SELECT " +
+            "   name, " +
+            "   nickName " +
+            "FROM person ",
+        resultSetMapping = "name_and_nickName_dto"
+    ),
+    //end::sql-multiple-scalar-values-dto-NamedNativeQuery-example[]
+    //tag::sql-entity-NamedNativeQuery-example[]
+    @NamedNativeQuery(
+        name = "find_person_by_name",
+        query =
+            "SELECT " +
+            "   p.id AS \"id\", " +
+            "   p.name AS \"name\", " +
+            "   p.nickName AS \"nickName\", " +
+            "   p.address AS \"address\", " +
+            "   p.createdOn AS \"createdOn\", " +
+            "   p.version AS \"version\" " +
+            "FROM person p " +
+            "WHERE p.name LIKE :name",
+        resultClass = Person.class
+    ),
+    //end::sql-entity-NamedNativeQuery-example[]
+    //tag::sql-entity-associations-NamedNativeQuery-example[]
+    @NamedNativeQuery(
+        name = "find_person_with_phones_by_name",
+        query =
+            "SELECT " +
+            "   pr.id AS \"pr.id\", " +
+            "   pr.name AS \"pr.name\", " +
+            "   pr.nickName AS \"pr.nickName\", " +
+            "   pr.address AS \"pr.address\", " +
+            "   pr.createdOn AS \"pr.createdOn\", " +
+            "   pr.version AS \"pr.version\", " +
+            "   ph.id AS \"ph.id\", " +
+            "   ph.person_id AS \"ph.person_id\", " +
+            "   ph.number AS \"ph.number\", " +
+            "   ph.type AS \"ph.type\" " +
+            "FROM person pr " +
+            "JOIN phone ph ON pr.id = ph.person_id " +
+            "WHERE pr.name LIKE :name",
+        resultSetMapping = "person_with_phones"
+    )
+    //end::sql-entity-associations-NamedNativeQuery-example[]
+})
+@SqlResultSetMappings({
+    //tag::sql-entity-associations-NamedNativeQuery-example[]
+     @SqlResultSetMapping(
+         name = "person_with_phones",
+         entities = {
+             @EntityResult(
+                 entityClass = Person.class,
+                 fields = {
+                     @FieldResult( name = "id", column = "pr.id" ),
+                     @FieldResult( name = "name", column = "pr.name" ),
+                     @FieldResult( name = "nickName", column = "pr.nickName" ),
+                     @FieldResult( name = "address", column = "pr.address" ),
+                     @FieldResult( name = "createdOn", column = "pr.createdOn" ),
+                     @FieldResult( name = "version", column = "pr.version" ),
+                 }
+             ),
+             @EntityResult(
+                 entityClass = Phone.class,
+                 fields = {
+                     @FieldResult( name = "id", column = "ph.id" ),
+                     @FieldResult( name = "person", column = "ph.person_id" ),
+                     @FieldResult( name = "number", column = "ph.number" ),
+                     @FieldResult( name = "type", column = "ph.type" ),
+                 }
+             )
+         }
+     ),
+    //end::sql-entity-associations-NamedNativeQuery-example[]
+    //tag::sql-multiple-scalar-values-dto-NamedNativeQuery-example[]
+    @SqlResultSetMapping(
+        name = "name_and_nickName_dto",
+        classes = @ConstructorResult(
+            targetClass = PersonNames.class,
+            columns = {
+                @ColumnResult(name = "name"),
+                @ColumnResult(name = "nickName")
+            }
+        )
+    )
+    //end::sql-multiple-scalar-values-dto-NamedNativeQuery-example[]
+})
 //tag::hql-examples-domain-model-example[]
 //tag::jpql-api-named-query-example[]
 @NamedQueries(
---
+++ documentation/src/test/java/org/hibernate/userguide/sql/Dimensions.java
@@ -0,0 +1,40 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+
+//$Id$
+package org.hibernate.userguide.sql;
+import javax.persistence.Embeddable;
+
+/**
+ * @author Emmanuel Bernard
+ * @author Vlad MIhalcea
+ */
+//tag::sql-composite-key-entity-associations_named-query-example[]
+@Embeddable
+public class Dimensions {
+
+    private int length;
+
+    private int width;
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+}
+//end::sql-composite-key-entity-associations_named-query-example[]

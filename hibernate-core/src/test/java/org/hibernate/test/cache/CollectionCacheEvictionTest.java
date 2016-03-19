66d2436759cb5ffa426846aa0ac00f625915c6d9
Zhenlei Huang <zlei.huang@gmail.com>
2016-03-19 23:13:16 +0800

    HHH-10631 - Auto evict collection cache not work when owner side update parent to null

--- hibernate-core/src/test/java/org/hibernate/test/cache/CollectionCacheEvictionTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/cache/CollectionCacheEvictionTest.java
@@ -250,6 +250,33 @@ public class CollectionCacheEvictionTest extends BaseCoreFunctionalTestCase {
 	}
 
 	@Test
+	@TestForIssue(jiraKey = "HHH-10631")
+	public void testCollectionCacheEvictionUpdateWhenChildIsSetToNull() {
+		Session s = openSession();
+		s.beginTransaction();
+
+		Company company1 = (Company) s.get( Company.class, 1 );
+		Company company2 = (Company) s.get( Company.class, 2 );
+
+		// init cache of collection
+		assertEquals( 1, company1.getUsers().size() );
+		assertEquals( 0, company2.getUsers().size() );
+
+		User user = (User) s.get( User.class, 1 );
+		user.setCompany( null );
+
+		s.getTransaction().commit();
+		s.close();
+
+		s = openSession();
+
+		company2 = (Company) s.get( Company.class, 2 );
+
+		assertEquals( 0, company2.getUsers().size() );
+		s.close();
+	}
+
+	@Test
 	public void testCollectionCacheEvictionUpdateWithEntityOutOfContext() {
 		Session s = openSession();
 		Company company1 = s.get( Company.class, 1 );

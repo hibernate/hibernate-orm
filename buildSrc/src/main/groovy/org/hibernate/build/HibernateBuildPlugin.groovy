8ddd61b061c01ce3dd2e18bfd549f687fe1cc1bc
Steve Ebersole <steve@hibernate.org>
2016-03-31 12:04:10 -0500

    HHH-10664 - Prep 6.0 feature branch - baseline Java 8

--- buildSrc/src/main/groovy/org/hibernate/build/HibernateBuildPlugin.groovy
+++ buildSrc/src/main/groovy/org/hibernate/build/HibernateBuildPlugin.groovy
@@ -10,14 +10,8 @@ import org.gradle.api.GradleException
 import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.plugins.JavaPluginConvention
 import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
-import org.gradle.api.tasks.compile.JavaCompile
-import org.gradle.api.tasks.testing.Test
-import org.gradle.internal.jvm.Jvm
-
-import org.hibernate.build.gradle.animalsniffer.AnimalSnifferExtension
 
 /**
  * @author Steve Ebersole
@@ -29,174 +23,10 @@ class HibernateBuildPlugin implements Plugin<Project> {
 			throw new GradleException( "Gradle must be run with Java 8" )
 		}
 
-		project.apply( plugin: 'org.hibernate.build.gradle.animalSniffer' )
-
-		final Jvm java6Home;
-		if ( project.rootProject.extensions.extraProperties.has( 'java6Home' ) ) {
-			java6Home = project.rootProject.extensions.extraProperties.get( 'java6Home' ) as Jvm
-		}
-		else {
-			String java6HomeDirSetting = null;
-			if ( project.hasProperty( "JAVA6_HOME" ) ) {
-				java6HomeDirSetting = project.property( "JAVA6_HOME" ) as String;
-			}
-			if ( java6HomeDirSetting == null ) {
-				java6HomeDirSetting = System.getProperty( "JAVA6_HOME" );
-			}
-			if ( java6HomeDirSetting == null ) {
-				java6HomeDirSetting = System.getenv( "JAVA6_HOME" );
-			}
-
-			if ( java6HomeDirSetting != null ) {
-				project.logger.info( "Using JAVA6_HOME setting [${java6HomeDirSetting}]" )
-
-				final File specifiedJava6Home = project.file( java6HomeDirSetting );
-				if ( specifiedJava6Home == null ) {
-					throw new GradleException( "Could not resolve specified java home ${java6HomeDirSetting}" )
-				}
-				if ( !specifiedJava6Home.exists() ) {
-					throw new GradleException( "Specified java home [${java6HomeDirSetting}] does not exist" )
-				}
-				if ( !specifiedJava6Home.isDirectory() ) {
-					throw new GradleException( "Specified java home [${java6HomeDirSetting}] is not a directory" )
-				}
-
-				java6Home = Jvm.forHome( specifiedJava6Home ) as Jvm;
-
-				if ( java6Home == null ) {
-					throw new GradleException( "Could not resolve JAVA6_HOME [${java6HomeDirSetting}] to proper JAVA_HOME" );
-				}
-
-				project.rootProject.extensions.extraProperties.set( 'java6Home', java6Home )
-			}
-			else {
-				project.logger.warn( "JAVA6_HOME setting not specified, some build features will be disabled" )
-				java6Home = null;
-			}
-		}
-
-		JavaTargetExtension javaTargetExtension = project.extensions.create( "javaTarget", JavaTargetExtension, project )
 		MavenPublishingExtension publishingExtension = project.extensions.create( "mavenPom", MavenPublishingExtension )
 
 		project.afterEvaluate {
-			applyJavaTarget( javaTargetExtension, project, java6Home )
 			applyPublishing( publishingExtension, project )
-		}
-	}
-
-	def applyJavaTarget(JavaTargetExtension javaTargetExtension, Project project, Jvm java6Home) {
-
-		project.logger.info( "Setting target Java version : ${javaTargetExtension.version} (${project.name})" )
-		project.properties.put( 'sourceCompatibility', "${javaTargetExtension.version}" )
-		project.properties.put( 'targetCompatibility', "${javaTargetExtension.version}" )
-
-
-		// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-		// apply AnimalSniffer
-
-		if ( javaTargetExtension.version.java8Compatible ) {
-			AnimalSnifferExtension animalSnifferExtension = project.extensions.findByType( AnimalSnifferExtension )
-			if ( animalSnifferExtension == null ) {
-				throw new GradleException( "Unable to locate AnimalSniffer extension" )
-			}
-			animalSnifferExtension.skip = true
-		}
-		else {
-			// todo : we could really disable this if we set executable/bootClasspath below
-			def sigConfig = project.configurations.animalSnifferSignature
-			sigConfig.incoming.beforeResolve {
-				sigConfig.dependencies.add( project.dependencies.create( 'org.codehaus.mojo.signature:java16:1.0@signature' ) )
-			}
-		}
-
-
-		// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-		// Apply to compile task
-
-		project.getConvention().findPlugin( JavaPluginConvention.class ).sourceSets.each { sourceSet ->
-			JavaCompile javaCompileTask = project.tasks.findByName( sourceSet.compileJavaTaskName ) as JavaCompile
-
-			// NOTE : this aptDir stuff is needed until we can have IntelliJ run annotation processors for us
-			//		which cannot happen until we can fold hibernate-testing back into hibernate-core/src/test
-			//		which cannot happen until... ugh
-			File aptDir = project.file( "${project.buildDir}/generated-src/apt/${sourceSet.name}" )
-			sourceSet.allJava.srcDir( aptDir )
-
-			javaCompileTask.options.compilerArgs += [
-					"-nowarn",
-					"-encoding", "UTF-8",
-					"-s", "${aptDir.absolutePath}"
-			]
-			javaCompileTask.doFirst {
-				aptDir.mkdirs()
-			}
-
-
-			if ( sourceSet.name == 'main' ) {
-				if ( javaTargetExtension.version.java8Compatible ) {
-					javaCompileTask.options.compilerArgs += [
-							"-source", '1.8',
-							"-target", '1.8'
-					]
-				}
-				else {
-					javaCompileTask.options.compilerArgs += [
-							"-source", '1.6',
-							"-target", '1.6'
-					]
-
-					if ( java6Home != null ) {
-						if ( javaTargetExtension.shouldApplyTargetToCompile ) {
-							// Technically we need only one here between:
-							//      1) setting the javac executable
-							//      2) setting the bootClasspath
-							// However, (1) requires fork=true whereas (2) does not.
-							//					javaCompileTask.options.fork = true
-							//					javaCompileTask.options.forkOptions.executable = java6Home.javacExecutable
-							javaCompileTask.options.bootClasspath = java6Home.runtimeJar.absolutePath
-						}
-					}
-				}
-			}
-		}
-//
-//
-//		// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//		// Apply to test compile task
-//
-//		SourceSet testSourceSet = project.getConvention().findPlugin( JavaPluginConvention.class ).sourceSets.findByName( "test" )
-//		JavaCompile compileTestTask = project.tasks.findByName( testSourceSet.compileJavaTaskName ) as JavaCompile
-//
-//		// NOTE : see the note above wrt aptDir
-//		File testAptDir = project.file( "${project.buildDir}/generated-src/apt/test" )
-//		testSourceSet.allJava.srcDir( testAptDir )
-//
-//		compileTestTask.options.compilerArgs += [
-//				"-nowarn",
-//				"-encoding", "UTF-8",
-//				"-s", "${testAptDir.absolutePath}"
-//		]
-//		compileTestTask.doFirst {
-//			testAptDir.mkdirs()
-//		}
-
-		// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-		// Apply to test tasks
-
-		project.tasks.withType( Test.class ).all { task->
-			task.jvmArgs += ['-XX:+HeapDumpOnOutOfMemoryError', "-XX:HeapDumpPath=${project.file("${project.buildDir}/OOM-dump.hprof").absolutePath}"]
-
-//			if ( !javaTargetExtension.version.java8Compatible && javaTargetExtension.shouldApplyTargetToTest ) {
-//				// use Java 6 settings
-//				task.executable = java6Home.javaExecutable
-//				task.maxHeapSize = '2G'
-//				task.jvmArgs += ['-XX:MaxPermGen=512M']
-//			}
-//			else {
-				// use Java 8 settings
-				task.maxHeapSize = '2G'
-				task.jvmArgs += ['-XX:MetaspaceSize=512M']
-//			}
 		}
 	}
 

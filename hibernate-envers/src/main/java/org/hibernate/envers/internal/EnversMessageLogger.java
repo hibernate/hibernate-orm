3b5376d2b42227212d86f729f9a25cfcdd348071
Naros <crancran@gmail.com>
2016-04-04 13:24:22 -0500

    HHH-10670 - Removed deprecated ValidTimeAuditStrategy

--- hibernate-envers/src/main/java/org/hibernate/envers/internal/EnversMessageLogger.java
+++ hibernate-envers/src/main/java/org/hibernate/envers/internal/EnversMessageLogger.java
@@ -7,11 +7,7 @@
 package org.hibernate.envers.internal;
 
 import org.jboss.logging.BasicLogger;
-import org.jboss.logging.annotations.LogMessage;
-import org.jboss.logging.annotations.Message;
 import org.jboss.logging.annotations.MessageLogger;
-
-import static org.jboss.logging.Logger.Level.WARN;
 
 /**
  * The jboss-logging {@link MessageLogger} for the hibernate-envers module.  It reserves message ids ranging from
@@ -21,10 +17,5 @@ import static org.jboss.logging.Logger.Level.WARN;
  */
 @MessageLogger(projectCode = "HHH")
 public interface EnversMessageLogger extends BasicLogger {
-	/**
-	 * Message indicating that user attempted to use the deprecated ValidTimeAuditStrategy
-	 */
-	@LogMessage(level = WARN)
-	@Message(value = "ValidTimeAuditStrategy is deprecated, please use ValidityAuditStrategy instead", id = 25001)
-	void validTimeAuditStrategyDeprecated();
+
 }

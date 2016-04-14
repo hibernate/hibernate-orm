9bd723474f2f5be0c4432e9409db114174292885
Vlad Mihalcea <mih_vlad@yahoo.com>
2016-04-14 13:04:32 +0300

    HHH-10750 - Add stored procedure unit tests for Oracle, MySQL, PostgreSQL, and SQL Server

---
+++ hibernate-core/src/test/java/org/hibernate/test/procedure/Phone.java
@@ -0,0 +1,62 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.test.procedure;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+/**
+ * @author Vlad Mihalcea
+ */
+@Entity
+public class Phone {
+
+    @Id
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Person person;
+
+    @Column(name = "phone_number")
+    private String number;
+
+    @ElementCollection
+    private List<Date> repairTimestamps = new ArrayList<>(  );
+
+    public Phone() {}
+
+    public Phone(String number) {
+        this.number = number;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+}

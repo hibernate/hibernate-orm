a467cdff155cd508555b3fea239cf10356284107
christophe <ctaret@deolan.com>
2016-04-21 17:49:30 +0200

    HHH-10471: Fixed indentation formatting

--- hibernate-java8/src/main/java/org/hibernate/ScrollableResultsStreamSupport.java
+++ hibernate-java8/src/main/java/org/hibernate/ScrollableResultsStreamSupport.java
@@ -14,43 +14,40 @@ import static java.util.Spliterators.spliteratorUnknownSize;
 import static java.util.stream.StreamSupport.stream;
 
 /**
- *
  * Utilities for obtaining {@link Stream} with an underlying {@link ScrollableResults}
  *
  * @author Christophe Taret
  */
 public class ScrollableResultsStreamSupport {
 
-	private ScrollableResultsStreamSupport(){
-	}
-
 	/**
 	 * @see Spliterator for description
 	 */
 	private static final int DEFAULT_CHARACTERISTICS =
-		Spliterator.NONNULL |
-		Spliterator.IMMUTABLE;
+			Spliterator.NONNULL |
+					Spliterator.IMMUTABLE;
 
-	/**
-	 *
-	 * @param scrollableResults the {@link ScrollableResults}
-	 * @param type the class of the {@link Stream}
-	 * @param <T> the type of the {@link Stream}
-	 * @return a {@link Stream} with values from the {@link ScrollableResults}
-	 */
-	public static <T> Stream<T> asStream(ScrollableResults scrollableResults, Class<T> type){
-		return asStream( scrollableResults, type, DEFAULT_CHARACTERISTICS, false );
+	private ScrollableResultsStreamSupport() {
 	}
 
 	/**
-	 *
 	 * @param scrollableResults the {@link ScrollableResults}
-	 * @param type the class of the {@link Stream}
-	 * @param <T> the type of the {@link Stream}
+	 * @param type              the class of the {@link Stream}
+	 * @param <T>               the type of the {@link Stream}
+	 * @return a {@link Stream} with values from the {@link ScrollableResults}
+	 */
+	public static <T> Stream<T> asStream(ScrollableResults scrollableResults, Class<T> type) {
+		return asStream(scrollableResults, type, DEFAULT_CHARACTERISTICS, false);
+	}
+
+	/**
+	 * @param scrollableResults the {@link ScrollableResults}
+	 * @param type              the class of the {@link Stream}
+	 * @param <T>               the type of the {@link Stream}
 	 * @return a parallel {@link Stream} with values from the {@link ScrollableResults}
 	 */
-	public static <T> Stream<T> asParallelStream(ScrollableResults scrollableResults, Class<T> type){
-		return asStream( scrollableResults, type, DEFAULT_CHARACTERISTICS, true );
+	public static <T> Stream<T> asParallelStream(ScrollableResults scrollableResults, Class<T> type) {
+		return asStream(scrollableResults, type, DEFAULT_CHARACTERISTICS, true);
 	}
 
 	private static <T> Stream<T> asStream(
@@ -61,13 +58,14 @@ public class ScrollableResultsStreamSupport {
 		// build the stream whether parallel or not, with all required information
 		return stream(
 				spliteratorUnknownSize(
-						new ScrollableResultIterator<>( scrollableResults, type ),
-						characteristics ),
-				parallel );
+						new ScrollableResultIterator<>(scrollableResults, type),
+						characteristics),
+				parallel);
 	}
 
 	/**
 	 * A {@link Iterator} implementation with an underlying {@link ScrollableResults}
+	 *
 	 * @param <T>
 	 */
 	private static class ScrollableResultIterator<T> implements Iterator<T> {
@@ -86,8 +84,8 @@ public class ScrollableResultsStreamSupport {
 
 		@Override
 		public T next() {
-			if (sr.next()){
-				return type.cast( sr.get()[0] );
+			if (sr.next()) {
+				return type.cast(sr.get()[0]);
 			}
 			return null;
 		}

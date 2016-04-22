4b2e96c6e9c185a2f94b5289e49a22940db17489
Andrea Boriero <andrea@hibernate.org>
2016-04-22 22:08:38 +0100

    Fix SecondaryTableTest

--- hibernate-core/src/test/java/org/hibernate/test/annotations/filter/secondarytable/SecondaryTableTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/annotations/filter/secondarytable/SecondaryTableTest.java
@@ -6,6 +6,8 @@
  */
 package org.hibernate.test.annotations.filter.secondarytable;
 
+import org.hibernate.Session;
+
 import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
 import org.junit.Assert;
 import org.junit.Test;
@@ -19,12 +21,18 @@ public class SecondaryTableTest extends BaseCoreFunctionalTestCase {
 
 	@Override
 	protected void prepareTest() throws Exception {
-		openSession();
-		insertUser("q@s.com", 21, false, "a1", "b");
-		insertUser("r@s.com", 22, false, "a2", "b");
-		insertUser("s@s.com", 23, true, "a3", "b");
-		insertUser("t@s.com", 24, false, "a4", "b");
-		session.flush();
+		Session s = openSession();
+		s.beginTransaction();
+		try {
+			insertUser( "q@s.com", 21, false, "a1", "b" );
+			insertUser( "r@s.com", 22, false, "a2", "b" );
+			insertUser( "s@s.com", 23, true, "a3", "b" );
+			insertUser( "t@s.com", 24, false, "a4", "b" );
+			session.flush();
+			s.getTransaction().commit();
+		}catch (Exception e){
+			s.getTransaction().rollback();
+		}
 	}
 	
 	@Test

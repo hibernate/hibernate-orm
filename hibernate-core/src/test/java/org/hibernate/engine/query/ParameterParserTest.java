980d243d05078adbf9393b55221515fb49f8161c
Michael Schnell <michael@fuin.org>
2016-04-23 12:28:18 +0200

    Revert "Removed extending base test (not needed)."
    This reverts commit 28eb8ad3e85baa373439e7fac9ca6d2eb32e404e.

--- hibernate-core/src/test/java/org/hibernate/engine/query/ParameterParserTest.java
+++ hibernate-core/src/test/java/org/hibernate/engine/query/ParameterParserTest.java
@@ -6,24 +6,20 @@
  */
 package org.hibernate.engine.query;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.junit.Test;
 
 import org.hibernate.engine.query.spi.ParameterParser;
-import org.hibernate.engine.query.spi.ParameterParser.Recognizer;
-import org.junit.Test;
+import org.hibernate.testing.junit4.BaseUnitTestCase;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit tests of the ParameterParser class
  *
  * @author Steve Ebersole
  */
-public class ParameterParserTest {
+public class ParameterParserTest extends BaseUnitTestCase {
 	@Test
 	public void testEscapeCallRecognition() {
 		assertTrue( ParameterParser.startsWithEscapeCallTemplate( "{ ? = call abc(?) }" ) );
@@ -31,66 +27,4 @@ public class ParameterParserTest {
 				"from User u where u.userName = ? and u.userType = 'call'"
 		) );
 	}
-	
-	@Test
-	public void testParseColonCharacterEscaped() {
-	    final StringBuilder captured = new StringBuilder();
-	    Recognizer recognizer = new Recognizer() {
-            @Override
-            public void outParameter(int position) {
-                fail();
-            }
-            @Override
-            public void ordinalParameter(int position) {
-                fail();
-            }
-            @Override
-            public void namedParameter(String name, int position) {
-                fail();
-            }
-            @Override
-            public void jpaPositionalParameter(String name, int position) {
-                fail();
-            }
-            @Override
-            public void other(char character) {
-                captured.append(character);
-            }
-	    };
-	    ParameterParser.parse("SELECT @a,(@a::=20) FROM tbl_name", recognizer);
-	    assertEquals("SELECT @a,(@a:=20) FROM tbl_name", captured.toString());
-	}
-	
-    @Test
-    public void testParseNamedParameter() {
-        final StringBuilder other = new StringBuilder();
-        final List<String> params = new ArrayList<String>();
-        Recognizer recognizer = new Recognizer() {
-            @Override
-            public void outParameter(int position) {
-                fail();
-            }
-            @Override
-            public void ordinalParameter(int position) {
-                fail();
-            }
-            @Override
-            public void namedParameter(String name, int position) {
-                params.add(name);
-            }
-            @Override
-            public void jpaPositionalParameter(String name, int position) {
-                fail();
-            }
-            @Override
-            public void other(char character) {
-                other.append(character);
-            }
-        };
-        ParameterParser.parse("from Stock s where s.stockCode = :stockCode and s.xyz = :pxyz", recognizer);
-        assertEquals("from Stock s where s.stockCode =  and s.xyz = ", other.toString());
-        assertEquals("stockCode", params.get(0));
-        assertEquals("pxyz", params.get(1));
-    }
-    
 }

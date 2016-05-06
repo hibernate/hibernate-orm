69b8202c7574a9c8a365e569f273c4505fcc292e
Steve Ebersole <steve@hibernate.org>
2016-05-06 14:22:15 -0500

    HHH-9602 - Schema validation fails when materialized view is involved

--- hibernate-core/src/main/java/org/hibernate/dialect/PostgreSQL93Dialect.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/PostgreSQL93Dialect.java
@@ -1,18 +1,16 @@
 package org.hibernate.dialect;
 
+import java.util.List;
+
 /**
  * An SQL Dialect for PostgreSQL 9.3 and later. Adds support for Materialized view.
  *
  * @author Dionis Argiri
  */
 public class PostgreSQL93Dialect extends PostgreSQL9Dialect {
-    @Override
-    public boolean supportsMaterializedView() {
-        return true;
-    }
-
-    @Override
-    public String getMaterializedViewTypeTerm() {
-        return "MATERIALIZED VIEW";
-    }
+	@Override
+	public void augmentRecognizedTableTypes(List<String> tableTypesList) {
+		super.augmentRecognizedTableTypes( tableTypesList );
+		tableTypesList.add( "MATERIALIZED VIEW" );
+	}
 }

03aa47ab76b6b7515f6f516aff7fa079617c6387
Chris Cranford <chris@hibernate.org>
2016-05-11 22:18:41 -0500

    HHH-10468 - Fix Envers global_with_modified_flag to allow override behavior when set to false.

--- hibernate-envers/src/main/java/org/hibernate/envers/configuration/internal/metadata/reader/AuditedPropertiesReader.java
+++ hibernate-envers/src/main/java/org/hibernate/envers/configuration/internal/metadata/reader/AuditedPropertiesReader.java
@@ -560,8 +560,17 @@ public class AuditedPropertiesReader {
 	}
 
 	protected boolean checkUsingModifiedFlag(Audited aud) {
-		return globalCfg.hasSettingForUsingModifiedFlag() ?
-				globalCfg.isGlobalWithModifiedFlag() : aud.withModifiedFlag();
+		// HHH-10468
+		if ( globalCfg.hasSettingForUsingModifiedFlag() ) {
+			// HHH-10468
+			// Modify behavior so that if the global setting has been set by user properties, then
+			// the audit behavior should be a disjunction between the global setting and the field
+			// annotation.  This allows the annotation to take precedence when the global value is
+			// false and for the global setting to take precedence when true.
+			return globalCfg.isGlobalWithModifiedFlag() || aud.withModifiedFlag();
+		}
+		// no global setting enabled, use the annotation's value only.
+		return aud.withModifiedFlag();
 	}
 
 	private void setPropertyRelationMappedBy(XProperty property, PropertyAuditingData propertyData) {

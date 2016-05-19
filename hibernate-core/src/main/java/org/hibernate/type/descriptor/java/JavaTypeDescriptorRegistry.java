d65829502542444f7a04ae10eabdea24865c61ad
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2016-05-19 17:08:11 +0300

    Fix checkstyle issue due to improper use of spacing

--- hibernate-core/src/main/java/org/hibernate/type/descriptor/java/JavaTypeDescriptorRegistry.java
+++ hibernate-core/src/main/java/org/hibernate/type/descriptor/java/JavaTypeDescriptorRegistry.java
@@ -134,20 +134,21 @@ public class JavaTypeDescriptorRegistry {
 			super(type, createMutabilityPlan(type));
 		}
 
-        @SuppressWarnings("unchecked")
-        private static <T> MutabilityPlan<T> createMutabilityPlan(final Class<T> type) {
+		@SuppressWarnings("unchecked")
+		private static <T> MutabilityPlan<T> createMutabilityPlan(final Class<T> type) {
 			if ( type.isAnnotationPresent( Immutable.class ) ) {
 				return ImmutableMutabilityPlan.INSTANCE;
 			}
-            // MutableMutabilityPlan is the "safest" option, but we do not necessarily know how to deepCopy etc...
-            return new MutableMutabilityPlan<T>() {
-                @Override
-                protected T deepCopyNotNull(T value) {
-                    throw new HibernateException(
-                            "Not known how to deep copy value of type: [" + type.getName() + "]"
-                    );
-                }
-            };
+			// MutableMutabilityPlan is the "safest" option, but we do not necessarily know how to deepCopy etc...
+			return new MutableMutabilityPlan<T>() {
+				@Override
+				protected T deepCopyNotNull(T value) {
+					throw new HibernateException(
+							"Not known how to deep copy value of type: [" + type
+									.getName() + "]"
+					);
+				}
+			};
 		}
 
 		@Override

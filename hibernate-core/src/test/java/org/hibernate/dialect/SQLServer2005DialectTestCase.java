1223683192f9a0d437af82cf10566384cf849fc7
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2016-05-19 17:26:29 +0300

    Add a \r\n on the expected form too

--- hibernate-core/src/test/java/org/hibernate/dialect/SQLServer2005DialectTestCase.java
+++ hibernate-core/src/test/java/org/hibernate/dialect/SQLServer2005DialectTestCase.java
@@ -78,7 +78,7 @@ public class SQLServer2005DialectTestCase extends BaseUnitTestCase {
 		final String query = "select E.fieldA,E.fieldB\r\nFROM Employee E WHERE E.firstName = :firstName";
 		assertEquals(
 				"WITH query AS (SELECT inner_query.*, ROW_NUMBER() OVER (ORDER BY CURRENT_TIMESTAMP) as __hibernate_row_nr__ FROM ( " +
-						"select E.fieldA as page0_,E.fieldB as page1_" + System.lineSeparator() +
+						"select E.fieldA as page0_,E.fieldB as page1_\r\n" +
 						"FROM Employee E WHERE E.firstName = :firstName ) inner_query ) SELECT page0_, page1_ FROM query " +
 						"WHERE __hibernate_row_nr__ >= ? AND __hibernate_row_nr__ < ?",
 				dialect.getLimitHandler().processSql( query, toRowSelection( 1, 25 ) )

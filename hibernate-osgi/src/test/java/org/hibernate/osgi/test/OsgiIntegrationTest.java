1b18746b094ab68a3d4f4960d107f0e3b48a1bc4
Chris Cranford <chris@hibernate.org>
2016-05-26 16:00:49 -0500

    HHH-10365 - Add Envers native and JPA test cases.

--- hibernate-osgi/src/test/java/org/hibernate/osgi/test/OsgiIntegrationTest.java
+++ hibernate-osgi/src/test/java/org/hibernate/osgi/test/OsgiIntegrationTest.java
@@ -22,7 +22,10 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.selector.spi.StrategySelector;
 import org.hibernate.cfg.Environment;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.envers.AuditReader;
+import org.hibernate.envers.AuditReaderFactory;
 import org.hibernate.internal.util.config.ConfigurationHelper;
+import org.hibernate.osgi.test.client.AuditedDataPoint;
 import org.hibernate.osgi.test.client.DataPoint;
 import org.hibernate.osgi.test.client.SomeService;
 import org.hibernate.osgi.test.client.TestIntegrator;
@@ -288,6 +291,71 @@ public class OsgiIntegrationTest {
 		assertNull( dp );
 		s.getTransaction().commit();
 		s.close();
+	}
+
+	@Test
+	public void testNativeEnvers() throws Exception {
+		final ServiceReference sr = bundleContext.getServiceReference( SessionFactory.class.getName() );
+		final SessionFactory sf = ( SessionFactory )bundleContext.getService( sr );
+
+		final Integer adpId;
+
+		Session s = sf.openSession();
+		s.getTransaction().begin();
+		AuditedDataPoint adp = new AuditedDataPoint( "Chris" );
+		s.persist( adp );
+		s.getTransaction().commit();
+		adpId = adp.getId();
+		s.close();
+
+		s = sf.openSession();
+		s.getTransaction().begin();
+		adp = s.get( AuditedDataPoint.class, adpId );
+		adp.setName( "Chris2" );
+		s.getTransaction().commit();
+		s.close();
+
+		s = sf.openSession();
+		AuditReader ar = AuditReaderFactory.get( s );
+		assertEquals( 2, ar.getRevisions( AuditedDataPoint.class, adpId ).size() );
+		AuditedDataPoint rev1 = ar.find( AuditedDataPoint.class, adpId, 1 );
+		AuditedDataPoint rev2 = ar.find( AuditedDataPoint.class, adpId, 2 );
+		assertEquals( new AuditedDataPoint( adpId, "Chris" ), rev1 );
+		assertEquals( new AuditedDataPoint( adpId, "Chris2" ), rev2 );
+		s.close();
+	}
+
+	@Test
+	public void testJpaEnvers() throws Exception {
+		final ServiceReference serviceReference = bundleContext.getServiceReference( PersistenceProvider.class.getName() );
+		final PersistenceProvider persistenceProvider = (PersistenceProvider) bundleContext.getService( serviceReference );
+		final EntityManagerFactory emf = persistenceProvider.createEntityManagerFactory( "hibernate-osgi-test", null );
+
+		final Integer adpId;
+
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		AuditedDataPoint adp = new AuditedDataPoint( "Chris" );
+		em.persist( adp );
+		em.getTransaction().commit();
+		adpId = adp.getId();
+		em.close();
+
+		em = emf.createEntityManager();
+		em.getTransaction().begin();
+		adp = em.find( AuditedDataPoint.class, adpId );
+		adp.setName( "Chris2" );
+		em.getTransaction().commit();
+		em.close();
+
+		em = emf.createEntityManager();
+		AuditReader ar = AuditReaderFactory.get( em );
+		assertEquals( 2, ar.getRevisions( AuditedDataPoint.class, adpId ).size() );
+		AuditedDataPoint rev1 = ar.find( AuditedDataPoint.class, adpId, 1 );
+		AuditedDataPoint rev2 = ar.find( AuditedDataPoint.class, adpId, 2 );
+		assertEquals( new AuditedDataPoint( adpId, "Chris" ), rev1 );
+		assertEquals( new AuditedDataPoint( adpId, "Chris2" ), rev2 );
+		em.close();
 	}
 
 	@Test

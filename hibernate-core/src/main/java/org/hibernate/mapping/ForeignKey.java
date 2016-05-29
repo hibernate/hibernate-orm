78de650efec58367a4196eb41c1d5f99b291f036
Matthias Kurz <m.kurz@irregular.at>
2016-05-29 22:17:48 +0200

    HHH-10643 - Attribute 'foreignKeyDefinition' of @javax.persistence.ForeignKey ignored by schema exporter

--- hibernate-core/src/main/java/org/hibernate/mapping/ForeignKey.java
+++ hibernate-core/src/main/java/org/hibernate/mapping/ForeignKey.java
@@ -22,6 +22,7 @@ import org.hibernate.internal.util.StringHelper;
 public class ForeignKey extends Constraint {
 	private Table referencedTable;
 	private String referencedEntityName;
+	private String keyDefinition;
 	private boolean cascadeDeleteEnabled;
 	private List<Column> referencedColumns = new ArrayList<Column>();
 	private boolean creationEnabled = true;
@@ -77,13 +78,23 @@ public class ForeignKey extends Constraint {
 			i++;
 		}
 
-		final String result = dialect.getAddForeignKeyConstraintString(
-				constraintName,
-				columnNames,
-				referencedTable.getQualifiedName( dialect, defaultCatalog, defaultSchema ),
-				referencedColumnNames,
-				isReferenceToPrimaryKey()
-		);
+		final String result = keyDefinition != null ?
+				dialect.getAddForeignKeyConstraintString(
+						constraintName,
+						keyDefinition
+				) :
+				dialect.getAddForeignKeyConstraintString(
+						constraintName,
+						columnNames,
+						referencedTable.getQualifiedName(
+								dialect,
+								defaultCatalog,
+								defaultSchema
+						),
+						referencedColumnNames,
+						isReferenceToPrimaryKey()
+				);
+		
 		return cascadeDeleteEnabled && dialect.supportsCascadeDelete()
 				? result + " on delete cascade"
 				: result;
@@ -153,6 +164,14 @@ public class ForeignKey extends Constraint {
 		this.referencedEntityName = referencedEntityName;
 	}
 
+	public String getKeyDefinition() {
+		return keyDefinition;
+	}
+
+	public void setKeyDefinition(String keyDefinition) {
+		this.keyDefinition = keyDefinition;
+	}
+	
 	public String sqlDropString(Dialect dialect, String defaultCatalog, String defaultSchema) {
 		final StringBuilder buf = new StringBuilder( "alter table " );
 		buf.append( getTable().getQualifiedName( dialect, defaultCatalog, defaultSchema ) );
---
+++ hibernate-core/src/test/java/org/hibernate/test/schemaupdate/foreignkeys/definition/ForeignKeyDefinitionSecondaryTableTest.java
@@ -0,0 +1,56 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.test.schemaupdate.foreignkeys.definition;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SecondaryTable;
+
+import org.hibernate.dialect.H2Dialect;
+
+import org.hibernate.testing.RequiresDialect;
+
+/**
+ * @author Vlad Mihalcea
+ */
+@RequiresDialect(value = H2Dialect.class)
+public class ForeignKeyDefinitionSecondaryTableTest
+		extends AbstractForeignKeyDefinitionTest {
+
+	@Override
+	protected Class<?>[] getAnnotatedClasses() {
+		return new Class<?>[] {
+				User.class,
+		};
+	}
+
+	@Entity(name = "Users")
+	@SecondaryTable(name = "User_details", foreignKey = @ForeignKey(name = "secondary", foreignKeyDefinition = "foreign key /* FK */ (id) references Users"))
+	public class User {
+
+		@Id
+		@GeneratedValue
+		private int id;
+
+		private String emailAddress;
+
+		@Column(name = "SECURITY_USERNAME", table = "User_details")
+		private String username;
+
+		@Column(name = "SECURITY_PASSWORD", table = "User_details")
+		private String password;
+	}
+
+
+	@Override
+	protected boolean validate(String fileContent) {
+		return fileContent.contains( "/* FK */" );
+	}
+}

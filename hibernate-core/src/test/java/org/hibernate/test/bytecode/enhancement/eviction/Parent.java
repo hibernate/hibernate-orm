5f78189951ec889a18cad27a5be8df4f311cdcfe
Gail Badner <gbadner@redhat.com>
2016-06-03 00:14:04 -0700

    HHH-10562 : ManagedEntity linked list broken when non-associated enhanced entity is evicted

---
+++ hibernate-core/src/test/java/org/hibernate/test/bytecode/enhancement/eviction/Parent.java
@@ -0,0 +1,39 @@
+package org.hibernate.test.bytecode.enhancement.eviction;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ * Created by barreiro on 12/9/15.
+ */
+@Entity
+public class Parent {
+	private Long id;
+	private String name;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}

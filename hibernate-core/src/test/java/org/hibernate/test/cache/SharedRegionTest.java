80ae5dcf66cfb801ffc95f7b072721f247a4708a
Gail Badner <gbadner@redhat.com>
2016-06-03 20:43:54 -0700

    HHH-10287 - Made necessary changes to backport

--- hibernate-core/src/test/java/org/hibernate/test/cache/SharedRegionTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/cache/SharedRegionTest.java
@@ -59,14 +59,14 @@ public class SharedRegionTest extends BaseNonConfigCoreFunctionalTestCase {
 		// now try to load a ZipCodes using the same id : should just return null rather than blow up :)
 		s = openSession();
 		s.beginTransaction();
-		ZipCodes zc = s.find( ZipCodes.class, 1 );
+		ZipCodes zc = s.get( ZipCodes.class, 1 );
 		assertNull( zc );
 		s.getTransaction().commit();
 		s.close();
 
 		s = openSession();
 		s.beginTransaction();
-		s.find( ZipCodes.class, 1 );
+		s.get( ZipCodes.class, 1 );
 		s.getTransaction().commit();
 		s.close();
 	}

3d74c45e1d81621ac02c58a59644f2717817db1f
Andrea Boriero <andrea@hibernate.org>
2016-06-10 13:31:36 +0100

    HHH-10820 - Fix InformationExtractorJdbcDatabaseMetaDataImpl quoted table names

--- hibernate-core/src/main/java/org/hibernate/tool/schema/extract/internal/InformationExtractorJdbcDatabaseMetaDataImpl.java
+++ hibernate-core/src/main/java/org/hibernate/tool/schema/extract/internal/InformationExtractorJdbcDatabaseMetaDataImpl.java
@@ -362,7 +362,7 @@ public class InformationExtractorJdbcDatabaseMetaDataImpl implements Information
 			boolean found = false;
 			TableInformation tableInformation = null;
 			while ( resultSet.next() ) {
-				if ( tableName.equals( identifierHelper().toIdentifier( resultSet.getString( "TABLE_NAME" ) ) ) ) {
+				if ( tableName.equals( identifierHelper().toIdentifier( resultSet.getString( "TABLE_NAME" ), tableName.isQuoted() ) ) ) {
 					if ( found ) {
 						log.multipleTablesFound( tableName.render() );
 						final String catalogName = catalog == null ? "" : catalog.render();

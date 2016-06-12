a5ef99df22e1223fe646832c6c210114f0c7ecc3
Chris Cranford <chris@hibernate.org>
2016-06-11 21:34:31 -0500

    HHH-10824 - Fix query stream API to support returning R rather than Object[] when selecting one type.

--- hibernate-core/src/test/java/org/hibernate/test/stream/basic/BasicStreamTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/stream/basic/BasicStreamTest.java
@@ -6,6 +6,7 @@
  */
 package org.hibernate.test.stream.basic;
 
+import java.util.Optional;
 import java.util.stream.Stream;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -15,11 +16,15 @@ import org.hibernate.Session;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.engine.spi.SessionImplementor;
 
+import org.hibernate.query.Query;
+import org.hibernate.testing.TestForIssue;
 import org.hibernate.testing.junit4.BaseNonConfigCoreFunctionalTestCase;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.hibernate.testing.junit4.ExtraAssertions.assertTyping;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * @author Steve Ebersole
@@ -48,6 +53,45 @@ public class BasicStreamTest extends BaseNonConfigCoreFunctionalTestCase {
 
 		session.getTransaction().commit();
 		session.close();
+	}
+
+	@Test
+	@TestForIssue(jiraKey = "HHH-10824")
+	public void testQueryStream() {
+		Session session = openSession();
+		try {
+
+			session.getTransaction().begin();
+			MyEntity e= new MyEntity();
+			e.id = 1;
+			e.name = "Test";
+			session.persist( e );
+			session.getTransaction().commit();
+			session.clear();
+
+			// Test stream query without type.
+			Object result = session.createQuery( "From MyEntity" ).stream().findFirst().orElse( null );
+			assertTyping( MyEntity.class, result );
+
+			// Test stream query with type.
+			result = session.createQuery( "From MyEntity", MyEntity.class ).stream().findFirst().orElse( null );
+			assertTyping( MyEntity.class, result );
+
+			// Test stream query using forEach
+			session.createQuery( "From MyEntity", MyEntity.class ).stream().forEach( i -> {
+				assertTyping( MyEntity.class, i );
+			} );
+
+			Stream<Object[]> data = session.createQuery( "SELECT me.id, me.name FROM MyEntity me" ).stream();
+			data.forEach( i -> {
+				assertTyping( Integer.class, i[0] );
+				assertTyping( String.class, i[1] );
+			});
+
+		}
+		finally {
+			session.close();
+		}
 	}
 
 	@Entity(name = "MyEntity")

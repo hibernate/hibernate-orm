5be28c85a0fb2155d1cd1db78ca962adfd140515
Gail Badner <gbadner@redhat.com>
2016-06-13 14:23:20 -0700

    HHH-10796 : Correct to use org.hibernate.Query for backport
    (cherry picked from commit 61bc59af15f38611eb321c017559aed53acc0128)

--- hibernate-core/src/test/java/org/hibernate/test/hql/ParameterTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/hql/ParameterTest.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.hibernate.query.Query;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.resource.transaction.spi.TransactionStatus;

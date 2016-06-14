01c169570784cdd68d8c7f040236119808c63cb3
Chris Cranford <chris@hibernate.org>
2016-06-13 22:23:17 -0500

    HHH-7510 - Fix LazyInitializationException for ManyToMany when deleting and global_with_modified_flag is enabled.

--- hibernate-envers/src/main/java/org/hibernate/envers/event/spi/EnversPreCollectionRemoveEventListenerImpl.java
+++ hibernate-envers/src/main/java/org/hibernate/envers/event/spi/EnversPreCollectionRemoveEventListenerImpl.java
@@ -32,13 +32,21 @@ public class EnversPreCollectionRemoveEventListenerImpl
 	@Override
 	public void onPreRemoveCollection(PreCollectionRemoveEvent event) {
 		final CollectionEntry collectionEntry = getCollectionEntry( event );
-		if ( collectionEntry != null && !collectionEntry.getLoadedPersister().isInverse() ) {
-			Serializable oldColl = collectionEntry.getSnapshot();
-			if ( !event.getCollection().wasInitialized() && shouldGenerateRevision( event ) ) {
-				// In case of uninitialized collection we need a fresh snapshot to properly calculate audit data.
-				oldColl = initializeCollection( event );
+		if ( collectionEntry != null ) {
+			if ( !collectionEntry.getLoadedPersister().isInverse() ) {
+				Serializable oldColl = collectionEntry.getSnapshot();
+				if ( !event.getCollection().wasInitialized() && shouldGenerateRevision( event ) ) {
+					// In case of uninitialized collection we need a fresh snapshot to properly calculate audit data.
+					oldColl = initializeCollection( event );
+				}
+				onCollectionAction( event, null, oldColl, collectionEntry );
 			}
-			onCollectionAction( event, null, oldColl, collectionEntry );
+			else {
+				// HHH-7510 - Avoid LazyInitializationException when global_with_modified_flag = true
+				if ( getEnversService().getGlobalConfiguration().isGlobalWithModifiedFlag() ) {
+					initializeCollection( event );
+				}
+			}
 		}
 	}
 }

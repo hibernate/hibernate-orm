8f1dbf51f0920cb780ab55372918d65a263ff799
Chris Cranford <chris@hibernate.org>
2016-06-13 19:13:32 -0500

    HHH-10841 - Fix columns with generated annotation not being tracked by Envers.

--- hibernate-envers/src/main/java/org/hibernate/envers/configuration/internal/metadata/AuditMetadataGenerator.java
+++ hibernate-envers/src/main/java/org/hibernate/envers/configuration/internal/metadata/AuditMetadataGenerator.java
@@ -37,6 +37,9 @@ import org.hibernate.mapping.Property;
 import org.hibernate.mapping.Table;
 import org.hibernate.mapping.Value;
 import org.hibernate.service.ServiceRegistry;
+import org.hibernate.tuple.GeneratedValueGeneration;
+import org.hibernate.tuple.GenerationTiming;
+import org.hibernate.tuple.ValueGeneration;
 import org.hibernate.type.CollectionType;
 import org.hibernate.type.ComponentType;
 import org.hibernate.type.ManyToOneType;
@@ -375,12 +378,26 @@ public final class AuditMetadataGenerator {
 						entityName,
 						xmlMappingData,
 						propertyAuditingData,
-						property.isInsertable(),
+						isPropertyInsertable( property ),
 						firstPass,
 						true
 				);
 			}
 		}
+	}
+
+	private boolean isPropertyInsertable(Property property) {
+		if ( !property.isInsertable() ) {
+			final ValueGeneration generation = property.getValueGenerationStrategy();
+			if ( generation instanceof GeneratedValueGeneration ) {
+				final GeneratedValueGeneration valueGeneration = (GeneratedValueGeneration) generation;
+				if ( GenerationTiming.INSERT == valueGeneration.getGenerationTiming()
+					|| GenerationTiming.ALWAYS == valueGeneration.getGenerationTiming() ) {
+					return true;
+				}
+			}
+		}
+		return property.isInsertable();
 	}
 
 	private boolean checkPropertiesAudited(Iterator<Property> properties, ClassAuditingData auditingData) {

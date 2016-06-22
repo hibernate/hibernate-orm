a5050285334d294b09280afa5970b6784f962abb
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2016-06-22 15:12:30 +0300

    HHH-10876 - DefaultIdentifierGeneratorFactory does not consider the hibernate.id.new_generator_mappings setting
    (cherry picked from commit eec01edcca9c01f3a748bdc3d203e7818447e312)
    
    Conflicts:
    	hibernate-core/src/main/java/org/hibernate/dialect/Dialect.java
    
    (cherry picked from commit d4b81560bf06ffff5cef4429ea7887bdd40dff98)
    
    Conflicts:
    	hibernate-core/src/main/java/org/hibernate/dialect/PostgreSQL81Dialect.java

--- hibernate-core/src/test/java/org/hibernate/test/id/SequenceGeneratorTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/id/SequenceGeneratorTest.java
@@ -6,44 +6,67 @@
  */
 package org.hibernate.test.id;
 
-import static org.junit.Assert.assertTrue;
+import java.util.Map;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.boot.SessionFactoryBuilder;
+import org.hibernate.cfg.Environment;
 import org.hibernate.dialect.SQLServer2012Dialect;
+
 import org.hibernate.testing.DialectChecks;
 import org.hibernate.testing.RequiresDialectFeature;
 import org.hibernate.testing.SkipForDialect;
 import org.hibernate.testing.TestForIssue;
-import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
+import org.hibernate.testing.junit4.BaseNonConfigCoreFunctionalTestCase;
+import org.hibernate.test.util.jdbc.SQLStatementInterceptor;
 import org.junit.Test;
 
-public class SequenceGeneratorTest extends BaseCoreFunctionalTestCase {
+import static org.junit.Assert.assertTrue;
 
-    @Override
-    public String[] getMappings() {
-        return new String[] { "id/Person.hbm.xml" };
-    }
+public class SequenceGeneratorTest extends BaseNonConfigCoreFunctionalTestCase {
 
-    /**
-     * This seems a little trivial, but we need to guarantee that all Dialects start their sequences on a non-0 value.
-     */
-    @Test
-    @TestForIssue(jiraKey = "HHH-8814")
-    @RequiresDialectFeature(DialectChecks.SupportsSequences.class)
+	private SQLStatementInterceptor sqlStatementInterceptor;
+
+	@Override
+	protected void configureSessionFactoryBuilder(SessionFactoryBuilder sfb) {
+		sqlStatementInterceptor = new SQLStatementInterceptor( sfb );
+	}
+
+	@Override
+	public String[] getMappings() {
+		return new String[] { "id/Person.hbm.xml" };
+	}
+
+	@Override
+	protected void addSettings(Map settings) {
+		settings.put( Environment.USE_NEW_ID_GENERATOR_MAPPINGS, "false" );
+	}
+
+	/**
+	 * This seems a little trivial, but we need to guarantee that all Dialects start their sequences on a non-0 value.
+	 */
+	@Test
+	@TestForIssue(jiraKey = "HHH-8814")
+	@RequiresDialectFeature(DialectChecks.SupportsSequences.class)
 	@SkipForDialect(
-			value= SQLServer2012Dialect.class,
-			comment="SQLServer2012Dialect initializes sequence to minimum value (e.g., Long.MIN_VALUE; Hibernate assumes it is uninitialized."
+			value = SQLServer2012Dialect.class,
+			comment = "SQLServer2012Dialect initializes sequence to minimum value (e.g., Long.MIN_VALUE; Hibernate assumes it is uninitialized."
 	)
-    public void testStartOfSequence() throws Exception {
-        Session s = openSession();
-        Transaction tx = s.beginTransaction();
-        final Person person = new Person();
-        s.persist(person);
-        tx.commit();
-        s.close();
-        
-        assertTrue(person.getId() > 0);
-    }
+	public void testStartOfSequence() throws Exception {
+		Session s = openSession();
+		Transaction tx = s.beginTransaction();
+		final Person person = new Person();
+		s.persist( person );
+		tx.commit();
+		s.close();
+
+		assertTrue( person.getId() > 0 );
+		assertTrue( sqlStatementInterceptor.getSqlQueries()
+							.stream()
+							.filter( sql -> sql.contains( "product_sequence" ) )
+							.findFirst()
+							.isPresent() );
+	}
 
 }

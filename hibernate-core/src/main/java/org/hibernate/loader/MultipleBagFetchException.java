59c0d48f8ddce2ebf5bff9cde96c2451ba4c8df8
Gail Badner <gbadner@redhat.com>
2016-06-23 00:05:07 -0700

    HHH-10648 : No MultipleBagFetchException is thrown when fetching two bags eagerly

--- hibernate-core/src/main/java/org/hibernate/loader/MultipleBagFetchException.java
+++ hibernate-core/src/main/java/org/hibernate/loader/MultipleBagFetchException.java
@@ -19,7 +19,7 @@ public class MultipleBagFetchException extends HibernateException {
 	private final List bagRoles;
 
 	public MultipleBagFetchException(List bagRoles) {
-		super( "cannot simultaneously fetch multiple bags" );
+		super( "cannot simultaneously fetch multiple bags: " + bagRoles );
 		this.bagRoles = bagRoles;
 	}
 
--- hibernate-core/src/main/java/org/hibernate/loader/plan/exec/internal/AbstractLoadQueryDetails.java
+++ hibernate-core/src/main/java/org/hibernate/loader/plan/exec/internal/AbstractLoadQueryDetails.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.loader.MultipleBagFetchException;
 import org.hibernate.loader.plan.build.spi.LoadPlanTreePrinter;
 import org.hibernate.loader.plan.exec.process.internal.ResultSetProcessorImpl;
 import org.hibernate.loader.plan.exec.process.spi.CollectionReferenceInitializer;
@@ -20,6 +21,7 @@ import org.hibernate.loader.plan.exec.query.internal.SelectStatementBuilder;
 import org.hibernate.loader.plan.exec.query.spi.QueryBuildingParameters;
 import org.hibernate.loader.plan.exec.spi.AliasResolutionContext;
 import org.hibernate.loader.plan.exec.spi.LoadQueryDetails;
+import org.hibernate.loader.plan.spi.CollectionAttributeFetch;
 import org.hibernate.loader.plan.spi.CollectionReturn;
 import org.hibernate.loader.plan.spi.FetchSource;
 import org.hibernate.loader.plan.spi.LoadPlan;
@@ -166,6 +168,14 @@ public abstract class AbstractLoadQueryDetails implements LoadQueryDetails {
 				);
 			}
 			// TODO: what about index???
+		}
+
+		if ( fetchStats != null && fetchStats.getJoinedBagAttributeFetches().size() > 1 ) {
+			final List<String> bagRoles = new ArrayList<>();
+			for ( CollectionAttributeFetch bagFetch : fetchStats.getJoinedBagAttributeFetches() ) {
+				bagRoles.add( bagFetch.getCollectionPersister().getRole() );
+			}
+			throw new MultipleBagFetchException( bagRoles );
 		}
 
 		LoadPlanTreePrinter.INSTANCE.logTree( loadPlan, queryProcessor.getAliasResolutionContext() );

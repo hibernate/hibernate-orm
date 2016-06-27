653ee1273889db1768a08ef9aafc7925521856ee
Andrea Boriero <andrea@hibernate.org>
2016-06-27 16:03:41 +0200

    HHH-10893 - Fix Reusing Query but changing collection parameters returns wrong result

--- hibernate-core/src/main/java/org/hibernate/engine/query/spi/NamedParameterDescriptor.java
+++ hibernate-core/src/main/java/org/hibernate/engine/query/spi/NamedParameterDescriptor.java
@@ -81,4 +81,22 @@ public class NamedParameterDescriptor implements QueryParameter {
 	public Type getType() {
 		return expectedType;
 	}
+
+	@Override
+	public boolean equals(Object o) {
+		if ( this == o ) {
+			return true;
+		}
+		if ( o == null || getClass() != o.getClass() ) {
+			return false;
+		}
+
+		NamedParameterDescriptor that = (NamedParameterDescriptor) o;
+		return getName().equals( that.getName() );
+	}
+
+	@Override
+	public int hashCode() {
+		return getName().hashCode();
+	}
 }

5e82c2e414c819275897bd180047c826e602f2ff
Gabriel Belingueres <belingueres@gmail.com>
2016-07-14 10:13:32 -0300

    Fixes HHH-10800 - InformixDialect: add support for current_timestamp() and current_date()

--- hibernate-core/src/test/java/org/hibernate/engine/query/InformixFunctionTest.java
+++ hibernate-core/src/test/java/org/hibernate/engine/query/InformixFunctionTest.java
@@ -1,12 +1,13 @@
 package org.hibernate.engine.query;
 
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-import org.hibernate.Session;
-import org.hibernate.Transaction;
 import org.hibernate.dialect.InformixDialect;
 
 import org.hibernate.testing.RequiresDialect;
@@ -14,7 +15,10 @@ import org.hibernate.testing.TestForIssue;
 import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
 import org.junit.Test;
 
+import static org.hibernate.testing.transaction.TransactionUtil.doInHibernate;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Vlad Mihalcea
@@ -26,26 +30,18 @@ public class InformixFunctionTest extends BaseCoreFunctionalTestCase {
 
 	@Override
 	protected void prepareTest() throws Exception {
-		Session s = openSession();
-		try {
-			Transaction transaction = s.beginTransaction();
+		doInHibernate( this::sessionFactory, session -> {
 			event = new Event();
 			event.country = "Romania";
 			event.city = "Cluj-Napoca";
-			s.persist( event );
-			transaction.commit();
-		}
-		finally {
-			s.close();
-		}
+			session.persist( event );
+		} );
 	}
 
 	@Test
 	@TestForIssue( jiraKey = "HHH-10846" )
 	public void testConcat() throws Exception {
-		Session s = openSession();
-		try {
-			s.beginTransaction();
+		doInHibernate( this::sessionFactory, session -> {
 			String location = (String) session.createQuery(
 				"select concat(e.country, ' - ',  e.city) " +
 				"from Event e " +
@@ -53,20 +49,13 @@ public class InformixFunctionTest extends BaseCoreFunctionalTestCase {
 			.setParameter( "id", event.id )
 			.getSingleResult();
 			assertEquals( "Romania - Cluj-Napoca", location);
-
-			s.getTransaction().commit();
-		}
-		finally {
-			s.close();
-		}
+		} );
 	}
 
 	@Test
 	@TestForIssue( jiraKey = "HHH-10846" )
 	public void testSubstring() throws Exception {
-		Session s = openSession();
-		try {
-			s.beginTransaction();
+		doInHibernate( this::sessionFactory, session -> {
 			String location = (String) session.createQuery(
 				"select substring(e.city, 0, 5) " +
 				"from Event e " +
@@ -74,20 +63,13 @@ public class InformixFunctionTest extends BaseCoreFunctionalTestCase {
 			.setParameter( "id", event.id )
 			.getSingleResult();
 			assertEquals( "Cluj", location);
-
-			s.getTransaction().commit();
-		}
-		finally {
-			s.close();
-		}
+		} );
 	}
 
 	@Test
 	@TestForIssue( jiraKey = "HHH-10846" )
 	public void testSubstr() throws Exception {
-		Session s = openSession();
-		try {
-			s.beginTransaction();
+		doInHibernate( this::sessionFactory, session -> {
 			String location = (String) session.createQuery(
 				"select substr(e.city, 0, 4) " +
 				"from Event e " +
@@ -95,20 +77,13 @@ public class InformixFunctionTest extends BaseCoreFunctionalTestCase {
 			.setParameter( "id", event.id )
 			.getSingleResult();
 			assertEquals( "Cluj", location);
-
-			s.getTransaction().commit();
-		}
-		finally {
-			s.close();
-		}
+		} );
 	}
 
 	@Test
 	@TestForIssue( jiraKey = "HHH-10846" )
 	public void testCoalesceAndNvl() throws Exception {
-		Session s = openSession();
-		try {
-			s.beginTransaction();
+		doInHibernate( this::sessionFactory, session -> {
 			String location = (String) session.createQuery(
 				"select coalesce(e.district, 'N/A') " +
 				"from Event e " +
@@ -124,12 +99,72 @@ public class InformixFunctionTest extends BaseCoreFunctionalTestCase {
 			.setParameter( "id", event.id )
 			.getSingleResult();
 			assertEquals( "N/A", location);
+		} );
+	}
 
-			s.getTransaction().commit();
-		}
-		finally {
-			s.close();
-		}
+	@Test
+	@TestForIssue( jiraKey = "HHH-10800" )
+	public void testCurrentDate() throws Exception {
+		doInHibernate( this::sessionFactory, session -> {
+			Date date = (Date) session.createQuery(
+				"select current_date() " +
+				"from Event e " +
+				"where e.id = :id")
+			.setParameter( "id", event.id )
+			.getSingleResult();
+
+			assertNotNull( date );
+			assertTrue( date.getTime() > 0 );
+
+			Calendar resultCalendar = Calendar.getInstance();
+			resultCalendar.setTime(date);
+
+			assertEquals( 0, todayCalendar().compareTo(resultCalendar) );
+		} );
+	}
+
+	@Test
+	@TestForIssue( jiraKey = "HHH-10800" )
+	public void testCurrentTimestamp() throws Exception {
+		doInHibernate( this::sessionFactory, session -> {
+			int tries = 2;
+			while ( tries-- > 0 ) {
+				Timestamp timestamp = (Timestamp) session.createQuery(
+					"select current_timestamp() " +
+					"from Event e " +
+					"where e.id = :id" )
+				.setParameter( "id", event.id )
+				.getSingleResult();
+
+				assertNotNull( timestamp );
+				assertTrue( timestamp != null && timestamp.getTime() > 0 );
+
+				Calendar resultCalendar = Calendar.getInstance();
+				resultCalendar.setTime( timestamp );
+
+				long millis = resultCalendar.getTime().getTime() - todayCalendar().getTime().getTime();
+
+				if(millis == 0) {
+					//What are the odds that ou've run this test exactly at midnight?
+					try {
+						Thread.sleep( 1000 );
+					}
+					catch ( InterruptedException ignore ) {}
+					continue;
+				}
+
+				assertTrue( millis > 0 );
+			}
+		} );
+	}
+
+	private Calendar todayCalendar() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+		return calendar;
 	}
 
 	@Override

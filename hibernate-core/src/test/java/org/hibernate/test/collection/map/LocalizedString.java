cc94c57097df1ebf3944250741a3d4b43fa8581e
Gail Badner <gbadner@redhat.com>
2016-07-13 20:27:36 -0700

    HHH-5393 : MappingException when @MapKeyColumn refers to a column mapped in embeddable map value
    (cherry picked from commit c893577efceff237f84d7200e1ef1a2895c95639)
    
    Conflicts:
    	hibernate-core/src/main/java/org/hibernate/mapping/SimpleValue.java

---
+++ hibernate-core/src/test/java/org/hibernate/test/collection/map/LocalizedString.java
@@ -0,0 +1,34 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.test.collection.map;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class LocalizedString {
+	private String language;
+	private String text;
+
+	public LocalizedString() {
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+}

ea69a0589f1abc88ca3c2bf5e97656179a81ed03
Andrea Boriero <andrea@hibernate.org>
2016-07-15 11:24:57 +0200

    HHH-10953 - Add test for issue

---
+++ hibernate-core/src/test/java/in/from/Any.java
@@ -0,0 +1,35 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package in.from;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * @author Andrea Boriero
+ */
+@Entity
+public class Any {
+	@Id
+	@GeneratedValue
+	private long id;
+
+	private String prop;
+
+	public long getId() {
+		return id;
+	}
+
+	public String getProp() {
+		return prop;
+	}
+
+	public void setProp(String prop) {
+		this.prop = prop;
+	}
+}

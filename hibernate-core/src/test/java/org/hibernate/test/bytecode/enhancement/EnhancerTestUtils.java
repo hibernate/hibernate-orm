e66ec8f77e29c1f5b29f44ea88a2487ecb55eaa7
Gail Badner <gbadner@redhat.com>
2016-07-19 15:55:39 -0700

    HHH-10922 - Correcions due to backporting
    (cherry picked from commit 58b3f69555536c9b6d4cb5103a99323a75915b1c)

--- hibernate-core/src/test/java/org/hibernate/test/bytecode/enhancement/EnhancerTestUtils.java
+++ hibernate-core/src/test/java/org/hibernate/test/bytecode/enhancement/EnhancerTestUtils.java
@@ -124,7 +124,7 @@ public abstract class EnhancerTestUtils extends BaseUnitTestCase {
 		}
 	}
 
-	private static ClassLoader getEnhancerClassLoader(EnhancementContext context, String packageName) {
+	private static ClassLoader getEnhancerClassLoader(final EnhancementContext context, final String packageName) {
 		return new ClassLoader() {
 			@Override
 			public Class<?> loadClass(String name) throws ClassNotFoundException {

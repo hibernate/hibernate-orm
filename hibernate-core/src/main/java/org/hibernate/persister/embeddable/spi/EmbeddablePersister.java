ea7edc78f382e27d1d9f60a49696c1ab6b08e34e
Chris Cranford <chris@hibernate.org>
2016-07-21 13:52:06 -0500

    HHH-10873 - Moved EmbeddablePersister to SPI

--- hibernate-core/src/main/java/org/hibernate/persister/embeddable/spi/EmbeddablePersister.java
+++ hibernate-core/src/main/java/org/hibernate/persister/embeddable/spi/EmbeddablePersister.java
@@ -4,7 +4,7 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.persister.embeddable;
+package org.hibernate.persister.embeddable.spi;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,12 +17,12 @@ import org.hibernate.sqm.domain.Attribute;
 import org.hibernate.sqm.domain.EmbeddableType;
 import org.hibernate.sqm.domain.ManagedType;
 import org.hibernate.sqm.domain.Type;
+import org.hibernate.type.spi.CompositeType;
 
 /**
  * @author Steve Ebersole
  */
 public class EmbeddablePersister implements EmbeddableType {
-	// todo : move to org.hibernate.persister.embeddable.spi
 
 	private final String compositeName;
 	private final String roleName;

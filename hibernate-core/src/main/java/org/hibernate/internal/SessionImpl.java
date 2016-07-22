87e31f291afe2094bd280da69a8c5a1d2ebf1281
mhyeon.lee <mhyeon.lee@navercorp.com>
2016-07-22 17:00:42 +0900

    HHH-10984 Refactor duplicated multiLoad logic

--- hibernate-core/src/main/java/org/hibernate/internal/SessionImpl.java
+++ hibernate-core/src/main/java/org/hibernate/internal/SessionImpl.java
@@ -2848,26 +2848,7 @@ public final class SessionImpl
 		@Override
 		@SuppressWarnings("unchecked")
 		public <K extends Serializable> List<T> multiLoad(List<K> ids) {
-			CacheMode sessionCacheMode = getCacheMode();
-			boolean cacheModeChanged = false;
-			if ( cacheMode != null ) {
-				// naive check for now...
-				// todo : account for "conceptually equal"
-				if ( cacheMode != sessionCacheMode ) {
-					setCacheMode( cacheMode );
-					cacheModeChanged = true;
-				}
-			}
-
-			try {
-				return entityPersister.multiLoad( ids.toArray( new Serializable[ ids.size() ] ), SessionImpl.this, this );
-			}
-			finally {
-				if ( cacheModeChanged ) {
-					// change it back
-					setCacheMode( sessionCacheMode );
-				}
-			}
+			return multiLoad(ids.toArray( new Serializable[ ids.size() ] ));
 		}
 	}
 

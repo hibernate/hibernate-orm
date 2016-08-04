b6ac8ec1bea53e39716d8eadd95229d1856b4487
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2016-08-04 12:41:51 +0300

    HHH-11027 - Document the PASS_DISTINCT_THROUGH Query Hint

---
+++ documentation/src/main/asciidoc/userguide/chapters/query/hql/extras/hql-distinct-entity-query-example.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT
+    p.id as id1_1_0_,
+    b.id as id1_0_1_,
+    p.first_name as first_na2_1_0_,
+    p.last_name as last_nam3_1_0_,
+    b.author_id as author_i3_0_1_,
+    b.title as title2_0_1_,
+    b.author_id as author_i3_0_0__,
+    b.id as id1_0_0__
+FROM person p
+LEFT OUTER JOIN book b ON p.id=b.author_id

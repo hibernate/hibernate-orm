9767072222c5d866d960138e1e9ba0854c042c50
姜禹 <rmfish@users.noreply.github.com>
2016-08-12 09:43:28 +0800

    HHH-10502 - Replace parameter in Order By FIELD()
    https://hibernate.atlassian.net/browse/HHH-10502

--- hibernate-core/src/main/java/org/hibernate/internal/util/StringHelper.java
+++ hibernate-core/src/main/java/org/hibernate/internal/util/StringHelper.java
@@ -182,10 +182,15 @@ public final class StringHelper {
 		// there is already a right-parenthesis; we assume there will be a matching right-parenthesis.
 		// 2) "... IN ?1", we assume that "?1" needs to be enclosed in parentheses, because there
 		// is no left-parenthesis.
+		
+		// We need to check the placeholder is not used in `Order By FIELD(...)` (HHH-10502)
+		// Examples:
+		// " ... Order By FIELD(id,?1)",  after expand parameters, the sql is "... Order By FIELD(id,?,?,?)"
 		boolean encloseInParens =
 				actuallyReplace
 						&& encloseInParensIfNecessary
-						&& !( getLastNonWhitespaceCharacter( beforePlaceholder ) == '(' );
+						&& !( getLastNonWhitespaceCharacter( beforePlaceholder ) == '(' ) && !(getLastNonWhitespaceCharacter(beforePlaceholder) == ',' && getFirstNonWhitespaceCharacter(afterPlaceholder) ==
+                ')');
 		StringBuilder buf = new StringBuilder( beforePlaceholder );
 		if ( encloseInParens ) {
 			buf.append( '(' );

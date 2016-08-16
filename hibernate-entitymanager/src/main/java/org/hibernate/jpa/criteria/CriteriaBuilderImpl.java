898fe4f5330592cc3a0ae1e0faa3472be6aa347c
Gail Badner <gbadner@redhat.com>
2016-08-15 23:41:44 -0700

    HHH-8488 : Replace jdk8-specific code with jdk6-compliant code

--- hibernate-entitymanager/src/main/java/org/hibernate/jpa/criteria/CriteriaBuilderImpl.java
+++ hibernate-entitymanager/src/main/java/org/hibernate/jpa/criteria/CriteriaBuilderImpl.java
@@ -15,7 +15,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.BiFunction;
 import javax.persistence.Tuple;
 import javax.persistence.criteria.CollectionJoin;
 import javax.persistence.criteria.CompoundSelection;

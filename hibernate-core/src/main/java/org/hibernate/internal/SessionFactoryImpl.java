97318b71f0eb22296d46077b0eac527fa454329e
johara <johnaohara80@gmail.com>
2016-08-16 16:29:18 +0100

    HHH-11051 : Cache EventListenerRegistry and StatisticsImplementor

--- hibernate-core/src/main/java/org/hibernate/internal/SessionFactoryImpl.java
+++ hibernate-core/src/main/java/org/hibernate/internal/SessionFactoryImpl.java
@@ -206,6 +206,8 @@ public final class SessionFactoryImpl implements SessionFactoryImplementor {
 	private final transient SessionFactoryOptions sessionFactoryOptions;
 	private final transient Map<String, RegionAccessStrategy> cacheAccessStrategiesMap = new HashMap();
 
+	private transient StatisticsImplementor statisticsImplementor;
+
 	public SessionFactoryImpl(final MetadataImplementor metadata, SessionFactoryOptions options) {
 		LOG.debug( "Building session factory" );
 
@@ -1170,7 +1172,10 @@ public final class SessionFactoryImpl implements SessionFactoryImplementor {
 	}
 
 	public StatisticsImplementor getStatisticsImplementor() {
-		return serviceRegistry.getService( StatisticsImplementor.class );
+		if ( statisticsImplementor == null ) {
+			statisticsImplementor = serviceRegistry.getService( StatisticsImplementor.class );
+		}
+		return statisticsImplementor;
 	}
 
 	public FilterDefinition getFilterDefinition(String filterName) throws HibernateException {

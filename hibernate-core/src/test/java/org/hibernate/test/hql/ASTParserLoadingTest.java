fb103fc281132e860e6833a11c53059e43480a56
Gail Badner <gbadner@redhat.com>
2016-08-25 22:06:46 -0700

    HHH-1830 : test case

--- hibernate-core/src/test/java/org/hibernate/test/hql/ASTParserLoadingTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/hql/ASTParserLoadingTest.java
@@ -231,7 +231,15 @@ public class ASTParserLoadingTest extends BaseCoreFunctionalTestCase {
 		session.getTransaction().commit();
 
 		assertEquals( 1, Constructor.getConstructorExecutionCount() );
-		assertEquals( new Constructor( constructor.getId(), true, true, constructor.getId() + 1, constructor.getId() + "foo" ), result );
+		assertEquals(
+				new Constructor(
+						constructor.getId(),
+						true,
+						true,
+						constructor.getId() + 1,
+						constructor.getId() + "foo"
+				), result
+		);
 
 		session.close();
 	}
@@ -569,7 +577,7 @@ public class ASTParserLoadingTest extends BaseCoreFunctionalTestCase {
 			Long count = (Long) s.createQuery( "select count(*) from Human h where VALUE(h.family) = :joe" ).setParameter( "joe", joe ).uniqueResult();
 			// ACTUALLY EXACTLY THE SAME AS:
 			// select count(*) from Human h where h.family = :joe
-			assertEquals( (Long)1L, count );
+			assertEquals( (Long) 1L, count );
 			s.getTransaction().commit();
 			s.close();
 		}
@@ -581,7 +589,7 @@ public class ASTParserLoadingTest extends BaseCoreFunctionalTestCase {
 			Long count = (Long) s.createQuery( "select count(*) from Human h join h.family f where value(f) = :joe" ).setParameter( "joe", joe ).uniqueResult();
 			// ACTUALLY EXACTLY THE SAME AS:
 			// select count(*) from Human h join h.family f where f = :joe
-			assertEquals( (Long)1L, count );
+			assertEquals( (Long) 1L, count );
 			s.getTransaction().commit();
 			s.close();
 		}
@@ -1024,7 +1032,11 @@ public class ASTParserLoadingTest extends BaseCoreFunctionalTestCase {
 		// resolved to a "id short cut" when part of the order by clause -> no inner join = no weeding out...
 		checkCounts( "from SimpleAssociatedEntity e order by e.owner", 2, "implicit-join in order-by clause" );
 		// resolved to a "id short cut" when part of the group by clause -> no inner join = no weeding out...
-		checkCounts( "select e.owner.id, count(*) from SimpleAssociatedEntity e group by e.owner", 2, "implicit-join in select and group-by clauses" );
+		checkCounts(
+				"select e.owner.id, count(*) from SimpleAssociatedEntity e group by e.owner",
+				2,
+				"implicit-join in select and group-by clauses"
+		);
 
 	 	s = openSession();
 		s.beginTransaction();
@@ -1178,9 +1190,9 @@ public class ASTParserLoadingTest extends BaseCoreFunctionalTestCase {
 		s.beginTransaction();
 		Object [] result = ( Object [] ) s.createQuery( "from User u, Human h where u.human = h" ).uniqueResult();
 		assertNotNull( result );
-		assertEquals( u.getUserName(), ( ( User ) result[ 0 ] ).getUserName() );
-		assertEquals( h.getName().getFirst(), ((Human) result[1]).getName().getFirst() );
-		assertSame( ((User) result[0]).getHuman(), result[1] );
+		assertEquals( u.getUserName(), ( (User) result[0] ).getUserName() );
+		assertEquals( h.getName().getFirst(), ( (Human) result[1] ).getName().getFirst() );
+		assertSame( ( (User) result[0] ).getHuman(), result[1] );
 		s.createQuery( "delete User" ).executeUpdate();
 		s.createQuery( "delete Human" ).executeUpdate();
 		s.getTransaction().commit();
@@ -1453,7 +1465,7 @@ public class ASTParserLoadingTest extends BaseCoreFunctionalTestCase {
 
 		s.createQuery( "from Human where nickname = ?1 or ( name.first = ?2 and name.last in ?3 )" )
 				.setParameter( "1", "Yogster" )
-				.setParameter( "2", "Yogi"  )
+				.setParameter( "2", "Yogi" )
 				.setParameterList( "3", params )
 				.list();
 
@@ -1715,6 +1727,16 @@ public class ASTParserLoadingTest extends BaseCoreFunctionalTestCase {
 		catch( QueryException expected ) {
 			// expected behavior
 		}
+		s.close();
+	}
+
+	@Test
+	@TestForIssue(jiraKey = "HHH-1830")
+	public void testAggregatedJoinAlias() {
+		Session s = openSession();
+		s.getTransaction().begin();
+		s.createQuery( "select p.id, size( descendants ) from Animal p left outer join p.offspring descendants group by p.id" ).list();
+		s.getTransaction().commit();
 		s.close();
 	}
 

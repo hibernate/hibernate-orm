efa7a421a35c1589768e41743a39b849797fea46
Gail Badner <gbadner@redhat.com>
2016-08-31 00:43:33 -0700

    HHH-11073 : test case

--- hibernate-entitymanager/src/test/java/org/hibernate/ejb/test/query/QueryWithLiteralsInSelectExpressionTest.java
+++ hibernate-entitymanager/src/test/java/org/hibernate/ejb/test/query/QueryWithLiteralsInSelectExpressionTest.java
@@ -63,6 +63,27 @@ public class QueryWithLiteralsInSelectExpressionTest extends BaseEntityManagerFu
 			).getResultList();
 			Assert.assertEquals( 1, elements.size() );
 			Assert.assertEquals( 3, elements.get( 0 ).length );
+			Assert.assertEquals( true, elements.get( 0 )[0] );
+			Assert.assertEquals( false, elements.get( 0 )[1] );
+			Assert.assertEquals( "Fab", elements.get( 0 )[2] );
+		}
+		finally {
+			entityManager.close();
+		}
+	}
+
+
+	@Test
+	@TestForIssue(jiraKey = "HHH-10230")
+	public void testSelectNonNullLiteralsCastToBoolean() {
+		final EntityManager entityManager = getOrCreateEntityManager();
+		try {
+			final List<Object[]> elements = entityManager.createQuery(
+					"SELECT cast( true as boolean ), cast( false as boolean ), e.name FROM MyEntity e",
+					Object[].class
+			).getResultList();
+			Assert.assertEquals( 1, elements.size() );
+			Assert.assertEquals( 3, elements.get( 0 ).length );
 			Assert.assertEquals( true, elements.get( 0 )[ 0 ] );
 			Assert.assertEquals( false, elements.get( 0 )[ 1 ] );
 			Assert.assertEquals( "Fab", elements.get( 0 )[ 2 ] );

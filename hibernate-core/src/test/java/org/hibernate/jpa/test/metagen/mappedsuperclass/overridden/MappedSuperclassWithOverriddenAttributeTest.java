0794e103d75644283270b74462ccc5757b636a34
Oliver Breidenbach <olly.breidenbach@googlemail.com>
2016-09-01 11:38:09 +0200

    HHH-11078 - Attribute in metamodel is null for overridden property
    - Add replicating test case

--- hibernate-core/src/test/java/org/hibernate/jpa/test/metagen/mappedsuperclass/overridden/MappedSuperclassWithOverriddenAttributeTest.java
+++ hibernate-core/src/test/java/org/hibernate/jpa/test/metagen/mappedsuperclass/overridden/MappedSuperclassWithOverriddenAttributeTest.java
@@ -4,7 +4,7 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.jpa.test.metagen.mappedsuperclass.attribute;
+package org.hibernate.jpa.test.metagen.mappedsuperclass.overridden;
 
 import java.util.Arrays;
 import javax.persistence.EntityManagerFactory;
@@ -12,6 +12,7 @@ import javax.persistence.EntityManagerFactory;
 import org.hibernate.jpa.AvailableSettings;
 import org.hibernate.jpa.test.TestingEntityManagerFactoryGenerator;
 
+import org.hibernate.testing.FailureExpected;
 import org.hibernate.testing.TestForIssue;
 import org.hibernate.testing.junit4.BaseUnitTestCase;
 import org.junit.Test;
@@ -19,21 +20,29 @@ import org.junit.Test;
 import static org.junit.Assert.assertNotNull;
 
 /**
- * @author Steve Ebersole
+ * @author Oliver Breidenbach
  */
-@TestForIssue( jiraKey = "HHH-5024" )
-public class MappedSuperclassWithAttributesTest extends BaseUnitTestCase {
+@TestForIssue(jiraKey = "HHH-11078")
+public class MappedSuperclassWithOverriddenAttributeTest
+		extends BaseUnitTestCase {
+
 	@Test
-	public void testStaticMetamodel() {
+	@FailureExpected(jiraKey = "HHH-11078")
+	public void testStaticMetamodelOverridden() {
 		EntityManagerFactory emf = TestingEntityManagerFactoryGenerator.generateEntityManagerFactory(
 				AvailableSettings.LOADED_CLASSES,
-				Arrays.asList( Product.class )
+				Arrays.asList( Product2.class )
 		);
 		try {
-			assertNotNull( "'Product_.id' should not be null)", Product_.id );
-			assertNotNull( "'Product_.name' should not be null)", Product_.name );
+			assertNotNull(
+					"'Product1_.overridenName' should not be null)",
+					Product1_.overridenName
+			);
 
-			assertNotNull( "'AbstractNameable_.name' should not be null)", AbstractNameable_.name );
+			assertNotNull(
+					"'Product2_.overridenName' should not be null)",
+					Product2_.overridenName
+			); // is null
 		}
 		finally {
 			emf.close();

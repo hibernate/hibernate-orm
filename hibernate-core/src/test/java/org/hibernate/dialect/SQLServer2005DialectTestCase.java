a85bd30aaa046b4d441f7f5870cb6b6f6bf27651
Chris Cranford <chris@hibernate.org>
2016-10-03 09:30:28 -0400

    HHH-11145 - Fix limit handler to ignore regex search keywords that are quoted.
    (cherry picked from commit 0e2b2f11e7ea04adf848dc7aa945cecd5dc15702)

--- hibernate-core/src/test/java/org/hibernate/dialect/SQLServer2005DialectTestCase.java
+++ hibernate-core/src/test/java/org/hibernate/dialect/SQLServer2005DialectTestCase.java
@@ -297,6 +297,41 @@ public class SQLServer2005DialectTestCase extends BaseUnitTestCase {
 	}
 
 	@Test
+	@TestForIssue(jiraKey = "HHH-11145")
+	public void testGetLimitStringWithFromInColumnName() {
+		final String query = "select [Created From Nonstock Item], field2 from table1";
+
+		assertEquals( "WITH query AS (SELECT inner_query.*, ROW_NUMBER() OVER (ORDER BY CURRENT_TIMESTAMP) as __hibernate_row_nr__ FROM ( " +
+						"select [Created From Nonstock Item] as page0_, field2 as page1_ from table1 ) inner_query ) " +
+						"SELECT page0_, page1_ FROM query WHERE __hibernate_row_nr__ >= ? AND __hibernate_row_nr__ < ?",
+				dialect.getLimitHandler().processSql( query, toRowSelection( 1, 5 ) )
+		);
+	}
+
+	@TestForIssue(jiraKey = "HHH-11145")
+	public void testGetLimitStringWithQuotedColumnNamesAndAlias() {
+		final String query = "select [Created From Item] c1, field2 from table1";
+
+		assertEquals( "WITH query AS (SELECT inner_query.*, ROW_NUMBER() OVER (ORDER BY CURRENT_TIMESTAMP) as __hibernate_row_nr__ FROM ( " +
+						"select [Created From Item] c1, field2 as page0_ from table1 ) inner_query ) " +
+						"SELECT c1, page0_ FROM query WHERE __hibernate_row_nr__ >= ? AND __hibernate_row_nr__ < ?",
+				dialect.getLimitHandler().processSql( query, toRowSelection( 1, 5 ) )
+		);
+	}
+
+	@Test
+	@TestForIssue(jiraKey = "HHH-11145")
+	public void testGetLimitStringWithQuotedColumnNamesAndAliasWithAs() {
+		final String query = "select [Created From Item] as c1, field2 from table1";
+
+		assertEquals( "WITH query AS (SELECT inner_query.*, ROW_NUMBER() OVER (ORDER BY CURRENT_TIMESTAMP) as __hibernate_row_nr__ FROM ( " +
+						"select [Created From Item] as c1, field2 as page0_ from table1 ) inner_query ) " +
+						"SELECT c1, page0_ FROM query WHERE __hibernate_row_nr__ >= ? AND __hibernate_row_nr__ < ?",
+				dialect.getLimitHandler().processSql( query, toRowSelection( 1, 5 ) )
+		);
+	}
+
+	@Test
 	@TestForIssue(jiraKey = "HHH-11324")
 	public void testGetLimitStringWithSelectClauseNestedQueryUsingParenthesis() {
 		final String query = "select t1.c1 as col_0_0, (select case when count(t2.c1)>0 then 'ADDED' else 'UNMODIFIED' end from table2 t2 WHERE (t2.c1 in (?))) as col_1_0 from table1 t1 WHERE 1=1 ORDER BY t1.c1 ASC";

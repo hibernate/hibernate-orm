e27acf731a44703904718460619fb786a6bbffa0
Martin Simka <msimka@redhat.com>
2016-10-24 16:55:23 +0200

    [HHH-11203] fix some minor test issues
    (cherry picked from commit 957ec3fa79427f3a3304bb5214c331cbc80f510d)

--- hibernate-core/src/test/java/org/hibernate/test/schemaupdate/uniqueconstraint/UniqueConstraintGenerationTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/schemaupdate/uniqueconstraint/UniqueConstraintGenerationTest.java
@@ -18,6 +18,8 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.boot.spi.MetadataImplementor;
 import org.hibernate.cfg.Environment;
+import org.hibernate.dialect.DB2Dialect;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
 import org.hibernate.tool.schema.TargetType;
 
@@ -62,11 +64,19 @@ public class UniqueConstraintGenerationTest {
 				.setOutputFile( output.getAbsolutePath() )
 				.create( EnumSet.of( TargetType.SCRIPT ), metadata );
 
-		assertThat(
-				"The test_entity_item table unique constraint has not been generated",
-				isUniqueConstraintGenerated( "test_entity_item", "item" ),
-				is( true )
-		);
+		if (ssr.getService(JdbcEnvironment.class).getDialect() instanceof DB2Dialect) {
+			assertThat(
+					"The test_entity_item table unique constraint has not been generated",
+					isCreateUniqueIndexGenerated("test_entity_item", "item"),
+					is(true)
+			);
+		} else {
+			assertThat(
+					"The test_entity_item table unique constraint has not been generated",
+					isUniqueConstraintGenerated("test_entity_item", "item"),
+					is(true)
+			);
+		}
 
 		assertThat(
 				"The test_entity_children table unique constraint has not been generated",
@@ -78,6 +88,22 @@ public class UniqueConstraintGenerationTest {
 	private boolean isUniqueConstraintGenerated(String tableName, String columnName) throws IOException {
 		boolean matches = false;
 		final String regex = "alter table " + tableName + " add constraint uk_(.)* unique \\(" + columnName + "\\)";
+
+		final String fileContent = new String( Files.readAllBytes( output.toPath() ) ).toLowerCase();
+		final String[] split = fileContent.split( System.lineSeparator() );
+		Pattern p = Pattern.compile( regex );
+		for ( String line : split ) {
+			final Matcher matcher = p.matcher( line );
+			if ( matcher.matches() ) {
+				matches = true;
+			}
+		}
+		return matches;
+	}
+
+	private boolean isCreateUniqueIndexGenerated(String tableName, String columnName) throws IOException {
+		boolean matches = false;
+		String regex = "create unique index uk_(.)* on " + tableName + " \\(" + columnName + "\\)";
 
 		final String fileContent = new String( Files.readAllBytes( output.toPath() ) ).toLowerCase();
 		final String[] split = fileContent.split( System.lineSeparator() );

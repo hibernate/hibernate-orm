b2df137ed6ea113176163d702e7bd0b0f0ea3780
stasal <al.stanislav@gmail.com>
2016-12-14 14:50:44 +0700

    HHH-11337 - Incorrect SQL generated when use both left join with unrelated entity and implicit join to another entity in select-clause

--- hibernate-core/src/test/java/org/hibernate/test/hql/EntityJoinTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/hql/EntityJoinTest.java
@@ -15,6 +15,7 @@ import javax.persistence.Table;
 
 import org.hibernate.Session;
 import org.hibernate.annotations.NaturalId;
+import org.hibernate.testing.TestForIssue;
 import org.hibernate.testing.junit4.BaseNonConfigCoreFunctionalTestCase;
 import org.junit.After;
 import org.junit.Before;
@@ -88,6 +89,37 @@ public class EntityJoinTest extends BaseNonConfigCoreFunctionalTestCase {
             // this should get all financial records even if their lastUpdateBy user is null
             List<Object[]> result = session.createQuery(
                     "select r.id, u.id, u.username " +
+                            "from FinancialRecord r " +
+                            "	left join User u on r.lastUpdateBy = u.username" +
+                            "   order by r.id"
+            ).list();
+            assertThat(result.size(), is(2));
+
+            Object[] stevesRecord = result.get(0);
+            assertThat(stevesRecord[0], is(1));
+            assertThat(stevesRecord[2], is("steve"));
+
+            Object[] noOnesRecord = result.get(1);
+            assertThat(noOnesRecord[0], is(2));
+            assertNull(noOnesRecord[2]);
+
+        } finally {
+            session.getTransaction().commit();
+            session.close();
+        }
+    }
+
+    @Test
+    @TestForIssue(jiraKey = "HHH-11337")
+    @SuppressWarnings("unchecked")
+    public void testLeftOuterEntityJoinsWithImplicitInnerJoinInSelectClause() {
+        Session session = openSession();
+        session.beginTransaction();
+
+        try {
+            // this should get all financial records even if their lastUpdateBy user is null
+            List<Object[]> result = session.createQuery(
+                    "select r.id, u.id, u.username, r.customer.name " +
                             "from FinancialRecord r " +
                             "	left join User u on r.lastUpdateBy = u.username" +
                             "   order by r.id"

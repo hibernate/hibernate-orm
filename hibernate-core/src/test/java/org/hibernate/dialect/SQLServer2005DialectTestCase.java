1304541d94d0794044d2ead7f0b63e88595956fc
Chris Cranford <chris@hibernate.org>
2016-12-16 12:50:24 -0500

    HHH-11084 - Fix invalid SQL used in test cases.

--- hibernate-core/src/test/java/org/hibernate/dialect/SQLServer2005DialectTestCase.java
+++ hibernate-core/src/test/java/org/hibernate/dialect/SQLServer2005DialectTestCase.java
@@ -151,12 +151,12 @@ public class SQLServer2005DialectTestCase extends BaseUnitTestCase {
 	public void testGetLimitStringWithSelectDistinctSubselect() {
 		final String selectDistinctSubselectSQL = "select page0_.CONTENTID as CONTENT1_12_ " +
 				"where page0_.CONTENTTYPE='PAGE' and (page0_.CONTENTID in " +
-				"(select distinct page2_.PREVVER from CONTENT page2_ where (page2_.PREVVER is not null))";
+				"(select distinct page2_.PREVVER from CONTENT page2_ where (page2_.PREVVER is not null)))";
 
 		assertEquals(
 				"select TOP(?) page0_.CONTENTID as CONTENT1_12_ " +
 						"where page0_.CONTENTTYPE='PAGE' and (page0_.CONTENTID in " +
-						"(select distinct page2_.PREVVER from CONTENT page2_ where (page2_.PREVVER is not null))",
+						"(select distinct page2_.PREVVER from CONTENT page2_ where (page2_.PREVVER is not null)))",
 				dialect.getLimitHandler().processSql( selectDistinctSubselectSQL, toRowSelection( 0, 5 ) )
 		);
 	}
@@ -164,14 +164,14 @@ public class SQLServer2005DialectTestCase extends BaseUnitTestCase {
 	@Test
 	@TestForIssue(jiraKey = "HHH-11084")
 	public void testGetLimitStringWithSelectDistinctSubselectNotFirst() {
-		final String selectDistinctSubselectSQL = "select page0_.CONTENTID as CONTENT1_12_ " +
+		final String selectDistinctSubselectSQL = "select page0_.CONTENTID as CONTENT1_12_ FROM CONTEXT page0_ " +
 				"where page0_.CONTENTTYPE='PAGE' and (page0_.CONTENTID in " +
-				"(select distinct page2_.PREVVER from CONTENT page2_ where (page2_.PREVVER is not null))";
+				"(select distinct page2_.PREVVER from CONTENT page2_ where (page2_.PREVVER is not null)))";
 
 		assertEquals(
 				"WITH query AS (SELECT inner_query.*, ROW_NUMBER() OVER (ORDER BY CURRENT_TIMESTAMP) as __hibernate_row_nr__ " +
 				"FROM ( " + selectDistinctSubselectSQL + " ) inner_query ) " +
-				"SELECT page2_.PREVVER FROM query WHERE __hibernate_row_nr__ >= ? AND __hibernate_row_nr__ < ?",
+				"SELECT CONTENT1_12_ FROM query WHERE __hibernate_row_nr__ >= ? AND __hibernate_row_nr__ < ?",
 				dialect.getLimitHandler().processSql( selectDistinctSubselectSQL, toRowSelection( 1, 5 ) )
 		);
 	}

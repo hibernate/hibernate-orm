75c4a879920d1672248c39efa3ff3cfb0cd5c88d
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2017-01-09 13:32:09 +0200

    HHH-10183 - Mapping for NVARCHAR in SqlServer not working with native queries; org.hibernate.MappingException: No Dialect mapping for JDBC type: -9
    Add replicating test case
    
    (cherry picked from commit 2a441d7f2dd3cff97210cb0654f75985d12d99d8)

--- hibernate-core/src/test/java/org/hibernate/test/scalar/NationalizedScalarQueryTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/scalar/NationalizedScalarQueryTest.java
@@ -4,46 +4,43 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.test.criterion;
+package org.hibernate.test.scalar;
 
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.hibernate.Criteria;
 import org.hibernate.annotations.Nationalized;
-import org.hibernate.criterion.Restrictions;
-
 import org.hibernate.dialect.DB2Dialect;
 import org.hibernate.dialect.PostgreSQL81Dialect;
-import org.hibernate.testing.SkipForDialect;
-import org.hibernate.testing.SkipForDialects;
-import org.junit.Test;
 
+import org.hibernate.testing.FailureExpected;
+import org.hibernate.testing.SkipForDialect;
 import org.hibernate.testing.TestForIssue;
 import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
+import org.junit.Test;
 
 import static org.hibernate.testing.transaction.TransactionUtil.doInHibernate;
 import static org.junit.Assert.assertEquals;
 
 /**
- * @author Chris Cranford (aka Naros)
+ * @author Vlad Mihalcea
  */
-@TestForIssue( jiraKey = "HHH-8657" )
-@SkipForDialects({
-		@SkipForDialect(value = DB2Dialect.class, comment = "DB2 jdbc driver doesn't support setNString"),
-		@SkipForDialect(value = PostgreSQL81Dialect.class, comment = "PostgreSQL jdbc driver doesn't support setNString"),
-})
-public class NationalizedIgnoreCaseTest extends BaseCoreFunctionalTestCase {
+@TestForIssue( jiraKey = "HHH-10183" )
+@SkipForDialect(value = DB2Dialect.class, comment = "DB2 jdbc driver doesn't support setNString")
+@SkipForDialect(value = PostgreSQL81Dialect.class, comment = "PostgreSQL jdbc driver doesn't support setNString")
+public class NationalizedScalarQueryTest extends BaseCoreFunctionalTestCase {
 	
 	@Override
 	protected Class<?>[] getAnnotatedClasses() {
 		return new Class[] { User.class };
 	}
-	
+
 	@Test
-	public void testIgnoreCaseCriteria() {
+	@FailureExpected( jiraKey = "HHH-10183" )
+	public void testScalarResult() {
 
 		User user1 = new User(1, "Chris");
 		User user2 = new User(2, "Steve");
@@ -54,9 +51,8 @@ public class NationalizedIgnoreCaseTest extends BaseCoreFunctionalTestCase {
 		} );
 
 		doInHibernate( this::sessionFactory, session -> {
-			Criteria criteria = session.createCriteria(User.class);
-			criteria.add(Restrictions.eq("name", user1.getName().toLowerCase()).ignoreCase());
-			assertEquals(1, criteria.list().size());
+			List<Object[]> users = session.createNativeQuery( "select * from users").getResultList();
+			assertEquals( 2, users.size() );
 		} );
 	}
 	

0b2a698106f19dcf83d5c5977f4328639f3b8f48
Andrea Boriero <andrea@hibernate.org>
2017-02-01 22:00:52 +0000

    Disable java-gradle-plugin automatically publishing the plugins

---
+++ buildSrc/src/main/groovy/org/hibernate/build/JavaGradlePluginDisablePublishing.java
@@ -0,0 +1,40 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.build;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.plugin.devel.plugins.JavaGradlePluginPlugin;
+
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.agent.ByteBuddyAgent;
+import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
+import net.bytebuddy.implementation.FixedValue;
+
+import static net.bytebuddy.matcher.ElementMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
+
+/**
+ * @author Andrea Boriero
+ */
+public class JavaGradlePluginDisablePublishing implements Plugin<Project> {
+
+	public JavaGradlePluginDisablePublishing() {
+		ByteBuddyAgent.install();
+		new ByteBuddy()
+				.redefine( JavaGradlePluginPlugin.class )
+				.method( named( "configurePublishing" ).and( takesArguments( 1 ) ) )
+				.intercept( FixedValue.value( "" ) )
+				.make()
+				.load( JavaGradlePluginPlugin.class.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent() );
+	}
+
+	@Override
+	public void apply(Project project) {
+
+	}
+}

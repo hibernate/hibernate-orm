54503bf7f4255afacb5717a992086bd59d68b51b
Fábio Ueno <fabio.t.ueno@gmail.com>
2017-02-13 18:50:09 -0200

    HHH-11186 - Add examples for all Hibernate annotations
    - CascadeType.REFRESH and CascadeType.REPLICATE

--- documentation/src/test/java/org/hibernate/userguide/pc/CascadeRefreshTest.java
+++ documentation/src/test/java/org/hibernate/userguide/pc/CascadeRefreshTest.java
@@ -5,22 +5,23 @@ import org.hibernate.jpa.test.BaseEntityManagerFunctionalTestCase;
 import org.junit.Test;
 
 import static org.hibernate.testing.transaction.TransactionUtil.doInJPA;
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author Fábio Takeo Ueno
  */
-public class CascadeRemoveTest extends BaseEntityManagerFunctionalTestCase {
+public class CascadeRefreshTest extends BaseEntityManagerFunctionalTestCase {
 
 	@Override
 	protected Class<?>[] getAnnotatedClasses() {
 		return new Class<?>[] {
-				Person.class,
-				Phone.class
+			Person.class,
+			Phone.class
 		};
 	}
 
 	@Test
-	public void removeTest() {
+	public void refreshTest() {
 		doInJPA( this::entityManagerFactory, entityManager -> {
 			Person person = new Person();
 			person.setId( 1L );
@@ -35,11 +36,19 @@ public class CascadeRemoveTest extends BaseEntityManagerFunctionalTestCase {
 		} );
 
 		doInJPA( this::entityManagerFactory, entityManager -> {
-			//tag::pc-cascade-remove-example[]
-			Person person = entityManager.find( Person.class, 1L );
 
-			entityManager.remove( person );
-			//end::pc-cascade-remove-example[]
+			//tag::pc-cascade-refresh-example[]
+			Person person = entityManager.find( Person.class, 1L );
+			Phone phone = person.getPhones().get( 0 );
+
+			person.setName( "John Doe Jr." );
+			phone.setNumber( "987-654-3210" );
+
+			entityManager.refresh( person );
+
+			assertEquals( "John Doe", person.getName() );
+			assertEquals( "123-456-7890", phone.getNumber() );
+			//end::pc-cascade-refresh-example[]
 		} );
 	}
 }

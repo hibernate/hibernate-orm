dc714695e4c0d5435a281577a093ec829becd0d3
Andrea Boriero <andrea@hibernate.org>
2017-02-23 09:31:48 +0000

    HHH-11511 - QuerySyntaxException when sorting by a column using a JPQL reserved keyword

--- hibernate-core/src/main/java/org/hibernate/hql/internal/ast/HqlParser.java
+++ hibernate-core/src/main/java/org/hibernate/hql/internal/ast/HqlParser.java
@@ -390,6 +390,21 @@ public final class HqlParser extends HqlBaseParser {
 	}
 
 	@Override
+	public void handlePrimaryExpressionDotIdent() throws TokenStreamException {
+		if ( LA( 2 ) == DOT && LA( 3 ) != IDENT ) {
+			// See if the second lookahead token can be an identifier.
+			HqlToken t = (HqlToken) LT( 3 );
+			if ( t.isPossibleID() ) {
+				// Set it!
+				t.setType( IDENT );
+				if ( LOG.isDebugEnabled() ) {
+					LOG.debugf( "handleDotIdent() : new LT(3) token - %s", LT( 1 ) );
+				}
+			}
+		}
+	}
+
+	@Override
 	public void weakKeywords() throws TokenStreamException {
 
 		int t = LA( 1 );

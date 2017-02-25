1048a19709734b974029710478c757784076a6b5
Andrea Boriero <andrea@hibernate.org>
2017-02-25 14:12:40 +0000

    HHH-11397 - Query parameter binding validation issue

--- hibernate-core/src/main/java/org/hibernate/query/internal/QueryParameterListBindingImpl.java
+++ hibernate-core/src/main/java/org/hibernate/query/internal/QueryParameterListBindingImpl.java
@@ -9,6 +9,7 @@ package org.hibernate.query.internal;
 import java.util.Collection;
 import javax.persistence.TemporalType;
 
+import org.hibernate.query.spi.QueryParameterBindingValidator;
 import org.hibernate.query.spi.QueryParameterListBinding;
 import org.hibernate.type.Type;
 
@@ -16,30 +17,39 @@ import org.hibernate.type.Type;
  * @author Steve Ebersole
  */
 public class QueryParameterListBindingImpl<T> implements QueryParameterListBinding<T> {
+	private final boolean isBindingValidationRequired;
+
 	private Collection<T> bindValues;
 	private Type bindType;
 
-	public QueryParameterListBindingImpl(Type type) {
+	public QueryParameterListBindingImpl(Type type, boolean isBindingValidationRequired) {
 		this.bindType = type;
+		this.isBindingValidationRequired = isBindingValidationRequired;
 	}
 
 	@Override
 	public void setBindValues(Collection<T> bindValues) {
-		if ( bindValues == null ) {
-			throw new IllegalArgumentException( "Collection must be not null!" );
+		if ( isBindingValidationRequired ) {
+			validate( bindValues );
 		}
-		this.bindValues = bindValues;
+		bindValue( bindValues );
 	}
 
 	@Override
 	public void setBindValues(Collection<T> values, Type clarifiedType) {
-		setBindValues( values );
+		if ( isBindingValidationRequired ) {
+			validate( bindValues, clarifiedType );
+		}
+		bindValue( values );
 		this.bindType = clarifiedType;
 	}
 
 	@Override
 	public void setBindValues(Collection<T> values, TemporalType clarifiedTemporalType) {
-		setBindValues( values );
+		if ( isBindingValidationRequired ) {
+			validate( values, clarifiedTemporalType );
+		}
+		bindValue( values );
 		final Object anElement = values.isEmpty() ? null : values.iterator().next();
 		this.bindType = BindingTypeHelper.INSTANCE.determineTypeForTemporalType( clarifiedTemporalType, bindType, anElement );
 	}
@@ -52,5 +62,24 @@ public class QueryParameterListBindingImpl<T> implements QueryParameterListBindi
 	@Override
 	public Type getBindType() {
 		return bindType;
+	}
+
+	private void bindValue(Collection<T> bindValues) {
+		if ( bindValues == null ) {
+			throw new IllegalArgumentException( "Collection must be not null!" );
+		}
+		this.bindValues = bindValues;
+	}
+
+	private void validate(Collection<T> value) {
+		QueryParameterBindingValidator.INSTANCE.validate( getBindType(), value );
+	}
+
+	private void validate(Collection<T> value, Type clarifiedType) {
+		QueryParameterBindingValidator.INSTANCE.validate( clarifiedType, value );
+	}
+
+	private void validate(Collection<T> value, TemporalType clarifiedTemporalType) {
+		QueryParameterBindingValidator.INSTANCE.validate( getBindType(), value, clarifiedTemporalType );
 	}
 }

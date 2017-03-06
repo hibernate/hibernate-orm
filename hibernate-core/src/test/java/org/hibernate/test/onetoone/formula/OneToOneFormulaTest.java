521ee44f8e0c4040546b9ce14d04b7bf4eebf1b4
Andrea Boriero <andrea@hibernate.org>
2017-03-06 11:45:59 +0000

    HHH-5757 - OneToOne SQL missing parameter

--- hibernate-core/src/test/java/org/hibernate/test/onetoone/formula/OneToOneFormulaTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/onetoone/formula/OneToOneFormulaTest.java
@@ -6,26 +6,29 @@
  */
 package org.hibernate.test.onetoone.formula;
 
-import org.junit.Test;
-
 import org.hibernate.FetchMode;
 import org.hibernate.Hibernate;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.criterion.Property;
 import org.hibernate.dialect.Oracle8iDialect;
-import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
 import org.hibernate.type.AbstractSingleColumnStandardBasicType;
 import org.hibernate.type.TextType;
 import org.hibernate.type.descriptor.sql.ClobTypeDescriptor;
 
+import org.hibernate.testing.TestForIssue;
+import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
+import org.hibernate.testing.transaction.TransactionUtil;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -55,130 +58,145 @@ public class OneToOneFormulaTest extends BaseCoreFunctionalTestCase {
 		cfg.setProperty(Environment.DEFAULT_BATCH_FETCH_SIZE, "2");
 	}
 
+	private Person person;
+	private Address address;
+
+	@Before
+	public void setUp(){
+		person = new Person();
+		person.setName( "Gavin King" );
+		address = new Address();
+		address.setPerson( person );
+		address.setType( "HOME" );
+		address.setZip( "3181" );
+		address.setState( "VIC" );
+		address.setStreet( "Karbarook Ave" );
+		person.setAddress( address );
+
+		TransactionUtil.doInHibernate( this::sessionFactory, session -> {
+			session.persist( person );
+		} );
+	}
+
+	@Override
+	protected void cleanupTest() {
+		TransactionUtil.doInHibernate( this::sessionFactory, session -> {
+			session.delete( person );
+		} );
+	}
+
 	@Test
 	public void testOneToOneFormula() {
-		Person p = new Person();
-		p.setName("Gavin King");
-		Address a = new Address();
-		a.setPerson(p);
-		a.setType("HOME");
-		a.setZip("3181");
-		a.setState("VIC");
-		a.setStreet("Karbarook Ave");
-		p.setAddress(a);
-		
-		Session s = openSession();
-		Transaction t = s.beginTransaction();
-		s.persist(p);
-		t.commit();
-		s.close();
-		
-		s = openSession();
-		t = s.beginTransaction();
-		p = (Person) s.createQuery("from Person").uniqueResult();
-		
-		assertNotNull( p.getAddress() );
-		assertTrue( Hibernate.isInitialized( p.getAddress() ) );
-		assertNull( p.getMailingAddress() );
 
-		s.clear();
+		TransactionUtil.doInHibernate( this::sessionFactory, s -> {
+			Person p = (Person) s.createQuery( "from Person" ).uniqueResult();
 
-		p = (Person) s.createQuery("from Person p left join fetch p.mailingAddress left join fetch p.address").uniqueResult();
+			assertNotNull( p.getAddress() );
+			assertTrue( Hibernate.isInitialized( p.getAddress() ) );
+			assertNull( p.getMailingAddress() );
+		} );
 
-		assertNotNull( p.getAddress() );
-		assertTrue( Hibernate.isInitialized( p.getAddress() ) );
-		assertNull( p.getMailingAddress() );
+		TransactionUtil.doInHibernate( this::sessionFactory, s -> {
+			Person p = (Person) s.createQuery(
+					"from Person p left join fetch p.mailingAddress left join fetch p.address" ).uniqueResult();
 
-		s.clear();
+			assertNotNull( p.getAddress() );
+			assertTrue( Hibernate.isInitialized( p.getAddress() ) );
+			assertNull( p.getMailingAddress() );
+		} );
 
-		p = (Person) s.createQuery("from Person p left join fetch p.address").uniqueResult();
+		TransactionUtil.doInHibernate( this::sessionFactory, s -> {
+			Person p = (Person) s.createQuery( "from Person p left join fetch p.address" ).uniqueResult();
 
-		assertNotNull( p.getAddress() );
-		assertTrue( Hibernate.isInitialized( p.getAddress() ) );
-		assertNull( p.getMailingAddress() );
+			assertNotNull( p.getAddress() );
+			assertTrue( Hibernate.isInitialized( p.getAddress() ) );
+			assertNull( p.getMailingAddress() );
+		} );
 
-		s.clear();
+		TransactionUtil.doInHibernate( this::sessionFactory, s -> {
+			Person p = (Person) s.createCriteria( Person.class )
+					.createCriteria( "address" )
+					.add( Property.forName( "zip" ).eq( "3181" ) )
+					.uniqueResult();
+			assertNotNull( p );
+		} );
 
-		p = (Person) s.createCriteria(Person.class)
-			.createCriteria("address")
-				.add( Property.forName("zip").eq("3181") )
-			.uniqueResult();
-		assertNotNull(p);
-		
-		s.clear();
+		TransactionUtil.doInHibernate( this::sessionFactory, s -> {
+			Person p = (Person) s.createCriteria( Person.class )
+					.setFetchMode( "address", FetchMode.JOIN )
+					.uniqueResult();
 
-		p = (Person) s.createCriteria(Person.class)
-			.setFetchMode("address", FetchMode.JOIN)
-			.uniqueResult();
+			assertNotNull( p.getAddress() );
+			assertTrue( Hibernate.isInitialized( p.getAddress() ) );
+			assertNull( p.getMailingAddress() );
+		} );
 
-		assertNotNull( p.getAddress() );
-		assertTrue( Hibernate.isInitialized( p.getAddress() ) );
-		assertNull( p.getMailingAddress() );
-		
-		s.clear();
+		TransactionUtil.doInHibernate( this::sessionFactory, s -> {
+			Person p = (Person) s.createCriteria( Person.class )
+					.setFetchMode( "mailingAddress", FetchMode.JOIN )
+					.uniqueResult();
 
-		p = (Person) s.createCriteria(Person.class)
-			.setFetchMode("mailingAddress", FetchMode.JOIN)
-			.uniqueResult();
+			assertNotNull( p.getAddress() );
+			assertTrue( Hibernate.isInitialized( p.getAddress() ) );
+			assertNull( p.getMailingAddress() );
 
-		assertNotNull( p.getAddress() );
-		assertTrue( Hibernate.isInitialized( p.getAddress() ) );
-		assertNull( p.getMailingAddress() );
-		
-		s.delete(p);
-		
-		t.commit();
-		s.close();
+		} );
+	}
+
+
+	@Test
+	@TestForIssue(jiraKey = "HHH-5757")
+	public void testQuery() {
+		TransactionUtil.doInHibernate( this::sessionFactory, session -> {
+			Person p = (Person) session.createQuery( "from Person p where p.address = :address" ).setParameter(
+					"address",
+					address ).uniqueResult();
+			assertThat( p, notNullValue() );
+		} );
+
+		TransactionUtil.doInHibernate( this::sessionFactory, session -> {
+			Address a = (Address) session.createQuery( "from Address a where a.person = :person" ).setParameter(
+					"person",
+					person ).uniqueResult();
+			assertThat( a, notNullValue() );
+
+		} );
 	}
 
 	@Test
 	public void testOneToOneEmbeddedCompositeKey() {
-		Person p = new Person();
-		p.setName("Gavin King");
-		Address a = new Address();
-		a.setPerson(p);
-		a.setType("HOME");
-		a.setZip("3181");
-		a.setState("VIC");
-		a.setStreet("Karbarook Ave");
-		p.setAddress(a);
+		TransactionUtil.doInHibernate( this::sessionFactory, session -> {
+			Address a = new Address();
+			a.setType("HOME");
+			a.setPerson(person);
+			a = session.load(Address.class, a);
+			assertFalse( Hibernate.isInitialized(a) );
+			a.getPerson();
+			a.getType();
+			assertFalse( Hibernate.isInitialized(a) );
+			assertEquals(a.getZip(), "3181");
+		} );
+
+		TransactionUtil.doInHibernate( this::sessionFactory, session -> {
+			Address a = new Address();
+			a.setType("HOME");
+			a.setPerson(person);
+			Address a2 = session.get(Address.class, a);
+			assertTrue( Hibernate.isInitialized(a) );
+			assertSame(a2, a);
+			assertSame(a2.getPerson(), person); //this is a little bit desirable
+			assertEquals(a.getZip(), "3181");
+		} );
 		
-		Session s = openSession();
-		Transaction t = s.beginTransaction();
-		s.persist(p);
-		t.commit();
-		s.close();
+
+
 		
-		s = openSession();
-		t = s.beginTransaction();
-		
-		a = new Address();
-		a.setType("HOME");
-		a.setPerson(p);
-		a = (Address) s.load(Address.class, a);
-		assertFalse( Hibernate.isInitialized(a) );
-		a.getPerson();
-		a.getType();
-		assertFalse( Hibernate.isInitialized(a) );
-		assertEquals(a.getZip(), "3181");
-		
-		s.clear();
-		
-		a = new Address();
-		a.setType("HOME");
-		a.setPerson(p);
-		Address a2 = (Address) s.get(Address.class, a);
-		assertTrue( Hibernate.isInitialized(a) );
-		assertSame(a2, a);
-		assertSame(a2.getPerson(), p); //this is a little bit desirable
-		assertEquals(a.getZip(), "3181");
-		
-		s.delete(a2);
-		s.delete( s.get( Person.class, p.getName() ) ); //this is certainly undesirable! oh well...
-		
-		t.commit();
-		s.close();
-		
+//		s.delete(a2);
+//		s.delete( s.get( Person.class, p.getName() ) ); //this is certainly undesirable! oh well...
+//
+//		t.commit();
+//		s.close();
+
 	}
 
 }

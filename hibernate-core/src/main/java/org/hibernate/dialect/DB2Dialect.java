1d7ed4aa55811564b083c9b617675974c5d902b9
Gail Badner <gbadner@redhat.com>
2017-05-12 13:49:29 -0700

    HHH-11740 : Default MultiTableBulkIdStrategy for DB2 does not work with connection pools
    (cherry picked from commit 061a1cae31e08f06283abf53195717aba0c307c8)

--- hibernate-core/src/main/java/org/hibernate/dialect/DB2Dialect.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/DB2Dialect.java
@@ -33,6 +33,7 @@ import org.hibernate.hql.spi.id.IdTableSupportStandardImpl;
 import org.hibernate.hql.spi.id.MultiTableBulkIdStrategy;
 import org.hibernate.hql.spi.id.global.GlobalTemporaryTableBulkIdStrategy;
 import org.hibernate.hql.spi.id.local.AfterUseAction;
+import org.hibernate.hql.spi.id.local.LocalTemporaryTableBulkIdStrategy;
 import org.hibernate.internal.CoreLogging;
 import org.hibernate.internal.CoreMessageLogger;
 import org.hibernate.internal.util.JdbcExceptionHelper;
@@ -397,7 +398,10 @@ public class DB2Dialect extends Dialect {
 
 	@Override
 	public MultiTableBulkIdStrategy getDefaultMultiTableBulkIdStrategy() {
-		return new GlobalTemporaryTableBulkIdStrategy(
+		// Prior to DB2 9.7, "real" global temporary tables that can be shared between sessions
+		// are *not* supported; even though the DB2 command says to declare a "global" temp table
+		// Hibernate treats it as a "local" temp table.
+		return new LocalTemporaryTableBulkIdStrategy(
 				new IdTableSupportStandardImpl() {
 					@Override
 					public String generateIdTableName(String baseName) {
@@ -414,7 +418,8 @@ public class DB2Dialect extends Dialect {
 						return "not logged";
 					}
 				},
-				AfterUseAction.CLEAN
+				AfterUseAction.DROP,
+				null
 		);
 	}
 

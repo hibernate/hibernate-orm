216e61fc9bc99cd4958fff9696aab14ac81a04cb
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2017-05-23 16:10:00 +0300

    HHH-11764 - JTS geometry being bound to byte array instead of PGgeometry

--- hibernate-spatial/src/test/java/org/hibernate/spatial/testing/dialects/postgis/PostgisBufferTest.java
+++ hibernate-spatial/src/test/java/org/hibernate/spatial/testing/dialects/postgis/PostgisBufferTest.java
@@ -4,35 +4,32 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.userguide.spatial;
+package org.hibernate.spatial.testing.dialects.postgis;
 
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-import org.hibernate.jpa.test.BaseEntityManagerFunctionalTestCase;
-import org.hibernate.spatial.dialect.postgis.PostgisDialect;
 import org.hibernate.spatial.dialect.postgis.PostgisPG95Dialect;
 
 import org.hibernate.testing.RequiresDialect;
+import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
 import org.junit.Test;
 
 import com.vividsolutions.jts.geom.Coordinate;
-//tag::spatial-types-mapping-example[]
-import com.vividsolutions.jts.geom.Point;
-
-//end::spatial-types-mapping-example[]
+import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.geom.Polygon;
 
+import static org.hibernate.testing.transaction.TransactionUtil.doInHibernate;
 import static org.junit.Assert.assertEquals;
-
-import static org.hibernate.testing.transaction.TransactionUtil.doInJPA;
 
 /**
  * @author Vlad Mihalcea
  */
 @RequiresDialect(PostgisPG95Dialect.class)
-public class SpatialTest extends BaseEntityManagerFunctionalTestCase {
+public class PostgisBufferTest extends BaseCoreFunctionalTestCase {
 
     private GeometryFactory geometryFactory = new GeometryFactory();
 
@@ -45,47 +42,42 @@ public class SpatialTest extends BaseEntityManagerFunctionalTestCase {
 
     @Test
     public void test() {
-        Long addressId = doInJPA( this::entityManagerFactory, entityManager -> {
-            //tag::spatial-types-point-creation-example[]
+        Long addressId = doInHibernate( this::sessionFactory, session -> {
             Event event = new Event();
             event.setId( 1L);
             event.setName( "Hibernate ORM presentation");
             Point point = geometryFactory.createPoint( new Coordinate( 10, 5 ) );
             event.setLocation( point );
 
-            entityManager.persist( event );
-            //end::spatial-types-point-creation-example[]
+            session.persist( event );
             return event.getId();
         });
 
-        doInJPA( this::entityManagerFactory, entityManager -> {
-            Event event = entityManager.find( Event.class, addressId);
-            Coordinate coordinate = event.getLocation().getCoordinate();
-            assertEquals( 10.0d, coordinate.getOrdinate( Coordinate.X), 0.1);
-            assertEquals( 5.0d, coordinate.getOrdinate( Coordinate.Y), 0.1);
-        });
-
-        doInJPA( this::entityManagerFactory, entityManager -> {
+        doInHibernate( this::sessionFactory, session -> {
             Coordinate [] coordinates = new Coordinate[] {
-                new Coordinate(1,1), new Coordinate(20,1), new Coordinate(20,20),
-                new Coordinate(1,20), new Coordinate(1,1)
+                    new Coordinate(1,1), new Coordinate(20,1), new Coordinate(20,20),
+                    new Coordinate(1,20), new Coordinate(1,1)
             };
-            //tag::spatial-types-query-example[]
             Polygon window = geometryFactory.createPolygon( coordinates );
-            Event event = entityManager.createQuery(
+
+            List<Event> events = session.createQuery(
                 "select e " +
                 "from Event e " +
-                "where within(e.location, :window) = true", Event.class)
+                "where buffer(:window, 100) is not null", Event.class)
             .setParameter("window", window)
-            .getSingleResult();
-            //end::spatial-types-query-example[]
-            Coordinate coordinate = event.getLocation().getCoordinate();
-            assertEquals( 10.0d, coordinate.getOrdinate( Coordinate.X), 0.1);
-            assertEquals( 5.0d, coordinate.getOrdinate( Coordinate.Y), 0.1);
+            .getResultList();
+
+            assertEquals(1, events.size());
+
+            List<Geometry> locations = session.createQuery(
+                "select buffer(e.location, 10) " +
+                "from Event e ", Geometry.class)
+            .getResultList();
+
+            assertEquals(1, locations.size());
         });
     }
 
-//tag::spatial-types-mapping-example[]
 @Entity(name = "Event")
 public static class Event {
 
@@ -96,8 +88,6 @@ public static class Event {
 
     private Point location;
 
-    //Getters and setters are omitted for brevity
-//end::spatial-types-mapping-example[]
     public Long getId() {
         return id;
     }
@@ -121,7 +111,5 @@ public static class Event {
     public void setLocation(Point location) {
         this.location = location;
     }
-//tag::spatial-types-mapping-example[]
 }
-//end::spatial-types-mapping-example[]
 }

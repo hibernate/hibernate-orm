473f43cdd5f9214767cd255a9fe895e99451145d
Gail Badner <gbadner@redhat.com>
2017-06-14 12:57:41 -0700

    HHH-11818 : ClassCastException when binding a MaterializedNClobType as NClob
    (cherry picked from commit 3f2c36ab698e658b576676d38c99745e58b9768d)

--- hibernate-core/src/main/java/org/hibernate/type/descriptor/java/StringTypeDescriptor.java
+++ hibernate-core/src/main/java/org/hibernate/type/descriptor/java/StringTypeDescriptor.java
@@ -48,11 +48,14 @@ public class StringTypeDescriptor extends AbstractTypeDescriptor<String> {
 		if ( CharacterStream.class.isAssignableFrom( type ) ) {
 			return (X) new CharacterStreamImpl( value );
 		}
-		if ( Clob.class.isAssignableFrom( type ) ) {
-			return (X) options.getLobCreator().createClob( value );
-		}
+		// Since NClob extends Clob, we need to check if type is an NClob
+		// before checking if type is a Clob. That will ensure that
+		// the correct type is returned.
 		if ( DataHelper.isNClob( type ) ) {
 			return (X) options.getLobCreator().createNClob( value );
+		}
+		if ( Clob.class.isAssignableFrom( type ) ) {
+			return (X) options.getLobCreator().createClob( value );
 		}
 
 		throw unknownUnwrap( type );

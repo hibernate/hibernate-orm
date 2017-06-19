67b01b50044767e719bb6330dcaf64f0073c7be5
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2017-06-19 17:16:26 +0300

    HHH-11186 - Add examples for all Hibernate annotations
    Document Hibernate @NativeQuery and @NamedNativeQuery annotation

--- documentation/src/test/java/org/hibernate/userguide/hql/HQLTest.java
+++ documentation/src/test/java/org/hibernate/userguide/hql/HQLTest.java
@@ -585,6 +585,19 @@ public class HQLTest extends BaseEntityManagerFunctionalTestCase {
 	}
 
 	@Test
+	public void test_jpql_api_hibernate_named_query_example() {
+		doInJPA( this::entityManagerFactory, entityManager -> {
+			//tag::jpql-api-hibernate-named-query-example[]
+			Phone phone = entityManager
+				.createNamedQuery( "get_phone_by_number", Phone.class )
+				.setParameter( "number", "123-456-7890" )
+				.getSingleResult();
+			//end::jpql-api-hibernate-named-query-example[]
+			assertNotNull( phone );
+		});
+	}
+
+	@Test
 	public void test_jpql_api_basic_usage_example() {
 		doInJPA( this::entityManagerFactory, entityManager -> {
 			//tag::jpql-api-basic-usage-example[]

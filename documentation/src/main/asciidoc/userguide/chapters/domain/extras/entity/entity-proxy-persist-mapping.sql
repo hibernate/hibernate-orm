a53e11822bb70238ec5e269b9d3d0f3286944706
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2017-06-22 17:33:29 +0300

    HHH-11186 - Add examples for all Hibernate annotations
    Document @Proxy annotation

---
+++ documentation/src/main/asciidoc/userguide/chapters/domain/extras/entity/entity-proxy-persist-mapping.sql
@@ -0,0 +1,9 @@
+into
+    Book
+    (author, title, id)
+values
+    (?, ?, ?)
+
+-- binding parameter [1] as [VARCHAR] - [Vlad Mihalcea]
+-- binding parameter [2] as [VARCHAR] - [High-Performance Java Persistence]
+-- binding parameter [3] as [BIGINT]  - [1]

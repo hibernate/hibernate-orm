4752ed50250a0f8e6d2f67a84c82ab9d80ecb304
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2017-06-22 18:00:14 +0300

    HHH-11186 - Add examples for all Hibernate annotations
    Document @Persister annotation

---
+++ documentation/src/test/java/org/hibernate/userguide/persister/EntityPersister.java
@@ -0,0 +1,34 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.userguide.persister;
+
+import org.hibernate.HibernateException;
+import org.hibernate.cache.spi.access.EntityRegionAccessStrategy;
+import org.hibernate.cache.spi.access.NaturalIdRegionAccessStrategy;
+import org.hibernate.mapping.PersistentClass;
+import org.hibernate.persister.entity.SingleTableEntityPersister;
+import org.hibernate.persister.spi.PersisterCreationContext;
+
+/**
+ * @author Shawn Clowater
+ */
+//tag::entity-persister-mapping[]
+
+public class EntityPersister
+    extends SingleTableEntityPersister {
+
+    public EntityPersister(
+            PersistentClass persistentClass,
+            EntityRegionAccessStrategy cache,
+            NaturalIdRegionAccessStrategy naturalIdRegionAccessStrategy,
+            PersisterCreationContext creationContext)
+            throws HibernateException {
+        super( persistentClass, cache, naturalIdRegionAccessStrategy, creationContext );
+    }
+}
+//end::entity-persister-mapping[]
+

f8b4a674e651e8e80f55152c9496b6c4b945e7bd
barreiro <lbbbarreiro@gmail.com>
2017-09-06 00:49:17 +0100

    HHH-12252 - Initial Agroal integration

---
+++ hibernate-agroal/src/test/java/org/hibernate/test/agroal/AgroalSkipAutoCommitTest.java
@@ -0,0 +1,113 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.test.agroal;
+
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.test.agroal.util.PreparedStatementSpyConnectionProvider;
+import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
+import org.junit.Test;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.hibernate.testing.transaction.TransactionUtil.doInHibernate;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+/**
+ * @author Vlad Mihalcea
+ */
+public class AgroalSkipAutoCommitTest extends BaseCoreFunctionalTestCase {
+
+	private PreparedStatementSpyConnectionProvider connectionProvider = new PreparedStatementSpyConnectionProvider();
+
+	@Override
+	protected void configure(Configuration configuration) {
+		configuration.getProperties().put( AvailableSettings.CONNECTION_PROVIDER, connectionProvider );
+		configuration.getProperties().put( AvailableSettings.CONNECTION_PROVIDER_DISABLES_AUTOCOMMIT, Boolean.TRUE );
+		configuration.getProperties().put( AvailableSettings.AUTOCOMMIT, Boolean.FALSE.toString() );
+	}
+
+	@Override
+	public void releaseSessionFactory() {
+		super.releaseSessionFactory();
+		connectionProvider.stop();
+	}
+
+	@Override
+	protected Class<?>[] getAnnotatedClasses() {
+		return new Class<?>[]{ City.class };
+	}
+
+	@Test
+	public void test() {
+		connectionProvider.clear();
+		doInHibernate( this::sessionFactory, session -> {
+			City city = new City();
+			city.setId( 1L );
+			city.setName( "Cluj-Napoca" );
+			session.persist( city );
+
+			assertTrue( connectionProvider.getAcquiredConnections().isEmpty() );
+			assertTrue( connectionProvider.getReleasedConnections().isEmpty() );
+		} );
+		verifyConnections();
+
+		connectionProvider.clear();
+		doInHibernate( this::sessionFactory, session -> {
+			City city = session.find( City.class, 1L );
+			assertEquals( "Cluj-Napoca", city.getName() );
+		} );
+		verifyConnections();
+	}
+
+	private void verifyConnections() {
+		assertTrue( connectionProvider.getAcquiredConnections().isEmpty() );
+
+		List<Connection> connections = connectionProvider.getReleasedConnections();
+		assertEquals( 1, connections.size() );
+		Connection connection = connections.get( 0 );
+		try {
+			verify(connection, never()).setAutoCommit( false );
+		}
+		catch (SQLException e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Entity(name = "City" )
+	public static class City {
+
+		@Id
+		private Long id;
+
+		private String name;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+	}
+}
---
+++ hibernate-agroal/src/test/java/org/hibernate/test/agroal/util/PreparedStatementSpyConnectionProvider.java
@@ -0,0 +1,116 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.test.agroal.util;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.hibernate.agroal.internal.AgroalConnectionProvider;
+import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
+
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.mockito.internal.util.MockUtil;
+
+/**
+ * This {@link ConnectionProvider} extends any other ConnectionProvider that would be used by default taken the current configuration properties, and it
+ * intercept the underlying {@link PreparedStatement} method calls.
+ *
+ * @author Vlad Mihalcea
+ */
+public class PreparedStatementSpyConnectionProvider extends AgroalConnectionProvider {
+
+	private final Map<PreparedStatement, String> preparedStatementMap = new LinkedHashMap<>();
+
+	private final List<Connection> acquiredConnections = new ArrayList<>( );
+	private final List<Connection> releasedConnections = new ArrayList<>( );
+
+	public PreparedStatementSpyConnectionProvider() {
+	}
+
+	protected Connection actualConnection() throws SQLException {
+		return super.getConnection();
+	}
+
+	@Override
+	public Connection getConnection() throws SQLException {
+		Connection connection = spy( actualConnection() );
+		acquiredConnections.add( connection );
+		return connection;
+	}
+
+	@Override
+	public void closeConnection(Connection conn) throws SQLException {
+		acquiredConnections.remove( conn );
+		releasedConnections.add( conn );
+		super.closeConnection( conn );
+	}
+
+	@Override
+	public void stop() {
+		clear();
+		super.stop();
+	}
+
+	private Connection spy(Connection connection) {
+		if ( MockUtil.isMock( connection ) ) {
+			return connection;
+		}
+		Connection connectionSpy = Mockito.spy( connection );
+		try {
+			Mockito.doAnswer( invocation -> {
+				PreparedStatement statement = (PreparedStatement) invocation.callRealMethod();
+				PreparedStatement statementSpy = Mockito.spy( statement );
+				String sql = (String) invocation.getArguments()[0];
+				preparedStatementMap.put( statementSpy, sql );
+				return statementSpy;
+			} ).when( connectionSpy ).prepareStatement( ArgumentMatchers.anyString() );
+
+			Mockito.doAnswer( invocation -> {
+				Statement statement = (Statement) invocation.callRealMethod();
+				Statement statementSpy = Mockito.spy( statement );
+				return statementSpy;
+			} ).when( connectionSpy ).createStatement();
+		}
+		catch ( SQLException e ) {
+			throw new IllegalArgumentException( e );
+		}
+		return connectionSpy;
+	}
+
+	/**
+	 * Clears the recorded PreparedStatements and reset the associated Mocks.
+	 */
+	public void clear() {
+		acquiredConnections.clear();
+		releasedConnections.clear();
+		preparedStatementMap.keySet().forEach( Mockito::reset );
+		preparedStatementMap.clear();
+	}
+
+	/**
+	 * Get a list of current acquired Connections.
+	 * @return list of current acquired Connections
+	 */
+	public List<Connection> getAcquiredConnections() {
+		return acquiredConnections;
+	}
+
+	/**
+	 * Get a list of current released Connections.
+	 * @return list of current released Connections
+	 */
+	public List<Connection> getReleasedConnections() {
+		return releasedConnections;
+	}
+}

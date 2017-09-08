9f2edb654dd55cee67d1b0445bdc0f6ae716f5e9
Gail Badner <gbadner@redhat.com>
2017-09-07 22:01:17 -0700

    HHH-11614 : Revert "HHH-11477 - HQL query against field marked with @Lob fails on PostgreSQL"
    This reverts commit f0016db20190c3f4dc014486c2f6e64428939f33.

--- hibernate-core/src/test/java/org/hibernate/test/lob/LongStringTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/lob/LongStringTest.java
@@ -5,24 +5,15 @@
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
 package org.hibernate.test.lob;
-import java.util.List;
-
 import org.junit.Test;
 
 import org.hibernate.Session;
-import org.hibernate.dialect.Oracle8iDialect;
 import org.hibernate.dialect.SybaseASE15Dialect;
-import org.hibernate.query.Query;
 
-import org.hibernate.testing.SkipForDialect;
-import org.hibernate.testing.TestForIssue;
 import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
-import org.hibernate.testing.transaction.TransactionUtil;
 
-import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
 
 /**
  * Tests eager materialization and mutation of long strings.
@@ -32,13 +23,13 @@ import static org.junit.Assert.assertThat;
 @SuppressWarnings( {"UnusedDeclaration"})
 public abstract class LongStringTest extends BaseCoreFunctionalTestCase {
 	private static final int LONG_STRING_SIZE = 10000;
-	private static final String EMPTY = "";
-
-	private final String original = buildRecursively( LONG_STRING_SIZE, 'x' );
-	private final String changed = buildRecursively( LONG_STRING_SIZE, 'y' );
 
 	@Test
 	public void testBoundedLongStringAccess() {
+		String original = buildRecursively( LONG_STRING_SIZE, 'x' );
+		String changed = buildRecursively( LONG_STRING_SIZE, 'y' );
+		String empty = "";
+
 		Session s = openSession();
 		s.beginTransaction();
 		LongStringHolder entity = new LongStringHolder();
@@ -48,7 +39,7 @@ public abstract class LongStringTest extends BaseCoreFunctionalTestCase {
 
 		s = openSession();
 		s.beginTransaction();
-		entity = s.get( LongStringHolder.class, entity.getId() );
+		entity = ( LongStringHolder ) s.get( LongStringHolder.class, entity.getId() );
 		assertNull( entity.getLongString() );
 		entity.setLongString( original );
 		s.getTransaction().commit();
@@ -56,7 +47,7 @@ public abstract class LongStringTest extends BaseCoreFunctionalTestCase {
 
 		s = openSession();
 		s.beginTransaction();
-		entity = s.get( LongStringHolder.class, entity.getId() );
+		entity = ( LongStringHolder ) s.get( LongStringHolder.class, entity.getId() );
 		assertEquals( LONG_STRING_SIZE, entity.getLongString().length() );
 		assertEquals( original, entity.getLongString() );
 		entity.setLongString( changed );
@@ -65,7 +56,7 @@ public abstract class LongStringTest extends BaseCoreFunctionalTestCase {
 
 		s = openSession();
 		s.beginTransaction();
-		entity = s.get( LongStringHolder.class, entity.getId() );
+		entity = ( LongStringHolder ) s.get( LongStringHolder.class, entity.getId() );
 		assertEquals( LONG_STRING_SIZE, entity.getLongString().length() );
 		assertEquals( changed, entity.getLongString() );
 		entity.setLongString( null );
@@ -74,73 +65,28 @@ public abstract class LongStringTest extends BaseCoreFunctionalTestCase {
 
 		s = openSession();
 		s.beginTransaction();
-		entity = s.get( LongStringHolder.class, entity.getId() );
+		entity = ( LongStringHolder ) s.get( LongStringHolder.class, entity.getId() );
 		assertNull( entity.getLongString() );
-		entity.setLongString( EMPTY );
+		entity.setLongString( empty );
 		s.getTransaction().commit();
 		s.close();
 
 		s = openSession();
 		s.beginTransaction();
-		entity = s.get( LongStringHolder.class, entity.getId() );
+		entity = ( LongStringHolder ) s.get( LongStringHolder.class, entity.getId() );
 		if ( entity.getLongString() != null ) {
             if(getDialect() instanceof SybaseASE15Dialect){
-                //Sybase uses a single blank to denote an EMPTY string (this is by design). So, when inserting an EMPTY string '', it is interpreted as single blank ' '.
-                assertEquals( EMPTY.length(), entity.getLongString().trim().length() );
-                assertEquals( EMPTY, entity.getLongString().trim() );
+                //Sybase uses a single blank to denote an empty string (this is by design). So, when inserting an empty string '', it is interpreted as single blank ' '.
+                assertEquals( empty.length(), entity.getLongString().trim().length() );
+                assertEquals( empty, entity.getLongString().trim() );
             }else{
-			    assertEquals( EMPTY.length(), entity.getLongString().length() );
-                assertEquals( EMPTY, entity.getLongString() );
+			    assertEquals( empty.length(), entity.getLongString().length() );
+                assertEquals( empty, entity.getLongString() );
             }
 		}
 		s.delete( entity );
 		s.getTransaction().commit();
 		s.close();
-	}
-
-	@Test
-	@SkipForDialect(Oracle8iDialect.class)
-	@TestForIssue( jiraKey = "HHH-11477")
-	public void testUsingLobPropertyInHqlQuery() {
-		TransactionUtil.doInHibernate( this::sessionFactory, session -> {
-			LongStringHolder entity = new LongStringHolder();
-			entity.setLongString( original );
-			session.save( entity );
-		} );
-
-		TransactionUtil.doInHibernate( this::sessionFactory, session -> {
-			final Query query = session.createQuery( "from LongStringHolder where longString = :stringValue" );
-			query.setParameter( "stringValue", original );
-			final List<LongStringHolder> results = query.list();
-			assertThat( results.size(), is( 1 ) );
-
-			assertThat( results.get( 0 ).getLongString(), is( original ) );
-		} );
-	}
-
-	@Test
-	@SkipForDialect(Oracle8iDialect.class)
-	@TestForIssue( jiraKey = "HHH-11477")
-	public void testSelectLobPropertyInHqlQuery() {
-		TransactionUtil.doInHibernate( this::sessionFactory, session -> {
-			LongStringHolder entity = new LongStringHolder();
-			entity.setLongString( original );
-			session.save( entity );
-		} );
-
-		TransactionUtil.doInHibernate( this::sessionFactory, session -> {
-			final Query query = session.createQuery( "select l.longString from LongStringHolder l where l.longString = :stringValue" );
-			query.setParameter( "stringValue", original );
-			final List<String> results = query.list();
-			assertThat( results.size(), is( 1 ) );
-
-			assertThat( results.get( 0 ), is( original ) );
-		} );
-	}
-
-	@Override
-	protected boolean isCleanupTestDataRequired() {
-		return true;
 	}
 
 	private String buildRecursively(int size, char baseChar) {

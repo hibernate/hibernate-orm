3292a83223662d54b4e177ac519d8abbafc8d115
Viefhaus <Viefhaus@his4502.his.de>
2017-09-14 10:05:55 +0200

    Testcase for HHH-11986 added
    HHH-11986 : Changed test code to follow conventions of other bytecode enhancement tests; reformatted test code

---
+++ hibernate-core/src/test/java/org/hibernate/test/bytecode/enhancement/lazy/group/LGMB_To.java
@@ -0,0 +1,78 @@
+package org.hibernate.test.bytecode.enhancement.lazy.group;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import org.hibernate.annotations.AccessType;
+
+/**
+ * Target of a LazyToOne - relationship (Foreignkey on this side)
+ *
+ * @author Jan-Oliver Lustig, Sebastian Viefhaus
+ */
+@Entity
+public class LGMB_To {
+	@Id
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(nullable = false)
+	@AccessType("property")
+	private Long id;
+
+	@OneToOne
+	LGMB_From fromRelation;
+
+	@Column(length = 50, nullable = false)
+	private String name;
+
+	/**
+	 * Default Constructor
+	 */
+	public LGMB_To() {
+		super();
+	}
+
+	/**
+	 * Constructor
+	 */
+	public LGMB_To(String name) {
+		super();
+		this.name = name;
+	}
+
+	/**
+	 * @return the fromRelation
+	 */
+	public LGMB_From getFromRelation() {
+		return fromRelation;
+	}
+
+	/**
+	 * @param fromRelation the fromRelation to set
+	 */
+	public void setFromRelation(LGMB_From fromRelation) {
+		this.fromRelation = fromRelation;
+	}
+
+	/**
+	 * @return id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+}

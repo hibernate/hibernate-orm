401936f94e6de39460225b9ff50ebda8d8f1d769
Sanne Grinovero <sanne@hibernate.org>
2017-10-23 15:06:09 +0100

    HHH-12055 Revert to using the Javassist version included in WildFly 11

--- hibernate-core/src/test/java/org/hibernate/jpa/test/cdi/BasicCdiTest.java
+++ hibernate-core/src/test/java/org/hibernate/jpa/test/cdi/BasicCdiTest.java
@@ -51,7 +51,6 @@ public class BasicCdiTest {
 				.addClass( EventQueue.class )
 				.addClass( Event.class )
 				.addClass( Monitor.class )
-				.addAsManifestResource( "jboss-deployment-structure.xml" )
 				.addAsManifestResource( EmptyAsset.INSTANCE, "beans.xml" )
 				.addAsManifestResource( new StringAsset( persistenceXml().exportAsString() ), "persistence.xml" );
 	}

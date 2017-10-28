c3f61c73dd702e9f28875c8f44c8602ea99b37ef
Archie L. Cobbs <archie.cobbs@gmail.com>
2017-10-28 10:59:49 -0500

    HHH-12068 - Fix generated @SuppressWarnings to work for all compilers

--- tooling/metamodel-generator/src/test/java/org/hibernate/jpamodelgen/test/supresswarnings/SuppressWarningsAnnotationGeneratedTest.java
+++ tooling/metamodel-generator/src/test/java/org/hibernate/jpamodelgen/test/supresswarnings/SuppressWarningsAnnotationGeneratedTest.java
@@ -32,7 +32,7 @@ public class SuppressWarningsAnnotationGeneratedTest extends CompilationTest {
 		String metaModelSource = getMetaModelSourceAsString( TestEntity.class );
 		assertTrue(
 				"@SuppressWarnings should be added to the metamodel.",
-				metaModelSource.contains( "@SuppressWarnings(\"all\")" )
+				metaModelSource.contains( "@SuppressWarnings({ \"deprecation\", \"rawtypes\" })" )
 		);
 	}
 }

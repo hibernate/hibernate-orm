8d23ce934f743829e808a96575d9f365d37bac69
barreiro <lbbbarreiro@gmail.com>
2017-10-30 13:53:37 +0000

    HHH-12054 - Handle UNFETCHED_PROPERTY in CollectionType.replace()

--- hibernate-core/src/main/java/org/hibernate/type/TypeHelper.java
+++ hibernate-core/src/main/java/org/hibernate/type/TypeHelper.java
@@ -156,25 +156,8 @@ public class TypeHelper {
 			final Map copyCache) {
 		Object[] copied = new Object[original.length];
 		for ( int i = 0; i < types.length; i++ ) {
-			if ( original[i] == LazyPropertyInitializer.UNFETCHED_PROPERTY
-				|| original[i] == PropertyAccessStrategyBackRefImpl.UNKNOWN ) {
+			if ( original[i] == LazyPropertyInitializer.UNFETCHED_PROPERTY || original[i] == PropertyAccessStrategyBackRefImpl.UNKNOWN ) {
 				copied[i] = target[i];
-			}
-			else if ( target[i] == LazyPropertyInitializer.UNFETCHED_PROPERTY ) {
-				// Should be no need to check for target[i] == PropertyAccessStrategyBackRefImpl.UNKNOWN
-				// because PropertyAccessStrategyBackRefImpl.get( object ) returns
-				// PropertyAccessStrategyBackRefImpl.UNKNOWN, so target[i] == original[i].
-				//
-				// We know from above that original[i] != LazyPropertyInitializer.UNFETCHED_PROPERTY &&
-				// original[i] != PropertyAccessStrategyBackRefImpl.UNKNOWN;
-				// This is a case where the entity being merged has a lazy property
-				// that has been initialized. Copy the initialized value from original.
-				if ( types[i].isMutable() ) {
-					copied[i] = types[i].deepCopy( original[i], session.getFactory() );
-				}
-				else {
-					copied[i] = original[i];
-				}
 			}
 			else {
 				copied[i] = types[i].replace( original[i], target[i], session, owner, copyCache );

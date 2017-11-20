dd561e5f9fe5fc5c0a9d6e850dc9956b11013c68
Gail Badner <gbadner@redhat.com>
2017-11-20 12:07:53 -0800

    HHH-12098 - prep 5.3

--- hibernate-core/src/test/java/org/hibernate/test/wf/ddl/cmt/emf/DdlInWildFlyUsingBmtAndEmfTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/wf/ddl/cmt/emf/DdlInWildFlyUsingBmtAndEmfTest.java
@@ -66,7 +66,7 @@ public class DdlInWildFlyUsingBmtAndEmfTest {
 				.jtaDataSource( "java:jboss/datasources/ExampleDS" )
 				.clazz( WildFlyDdlEntity.class.getName() )
 				.excludeUnlistedClasses( true )
-				.getOrCreateProperties().createProperty().name( "jboss.as.jpa.providerModule" ).value( "org.hibernate:5.2" ).up().up()
+				.getOrCreateProperties().createProperty().name( "jboss.as.jpa.providerModule" ).value( "org.hibernate:5.3" ).up().up()
 				.getOrCreateProperties().createProperty().name( "hibernate.hbm2ddl.auto" ).value( "create-drop" ).up().up()
 				// this should not be needed, but...
 				.getOrCreateProperties().createProperty().name( AvailableSettings.JTA_PLATFORM ).value( JBossAppServerJtaPlatform.class.getName() ).up().up()

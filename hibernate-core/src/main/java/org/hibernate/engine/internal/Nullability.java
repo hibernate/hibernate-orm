a4d98419158802ea8db71bc79020c5367192dca4
Steve Ebersole <steve@hibernate.org>
2017-11-27 15:29:52 -0600

    challenge

--- hibernate-core/src/main/java/org/hibernate/engine/internal/Nullability.java
+++ hibernate-core/src/main/java/org/hibernate/engine/internal/Nullability.java
@@ -11,11 +11,14 @@ import java.util.Iterator;
 import org.hibernate.HibernateException;
 import org.hibernate.PropertyValueException;
 import org.hibernate.bytecode.enhance.spi.LazyPropertyInitializer;
+import org.hibernate.engine.spi.CascadeStyle;
 import org.hibernate.engine.spi.CascadingActions;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.engine.spi.Status;
 import org.hibernate.persister.entity.EntityPersister;
 import org.hibernate.type.CollectionType;
 import org.hibernate.type.CompositeType;
+import org.hibernate.type.EntityType;
 import org.hibernate.type.Type;
 
 /**
@@ -49,7 +52,21 @@ public final class Nullability {
 	public void checkNullability(
 			final Object[] values,
 			final EntityPersister persister,
-			final boolean isUpdate) throws HibernateException {
+			final boolean isUpdate) {
+		checkNullability( values, persister, isUpdate ? NullabilityCheckType.UPDATE : NullabilityCheckType.CREATE );
+	}
+
+	public enum NullabilityCheckType {
+		CREATE,
+		UPDATE,
+		DELETE
+	}
+
+	public void checkNullability(
+			final Object[] values,
+			final EntityPersister persister,
+			final NullabilityCheckType checkType) {
+
 		/*
 		 * Typically when Bean Validation is on, we don't want to validate null values
 		 * at the Hibernate Core level. Hence the checkNullability setting.
@@ -60,7 +77,7 @@ public final class Nullability {
 			  * Check for any level one nullability breaks
 			  * Look at non null components to
 			  *   recursively check next level of nullability breaks
-			  * Look at Collections contraining component to
+			  * Look at Collections containing components to
 			  *   recursively check next level of nullability breaks
 			  *
 			  *
@@ -74,9 +91,9 @@ public final class Nullability {
 			  */
 
 			final boolean[] nullability = persister.getPropertyNullability();
-			final boolean[] checkability = isUpdate ?
-				persister.getPropertyUpdateability() :
-				persister.getPropertyInsertability();
+			final boolean[] checkability = checkType == NullabilityCheckType.CREATE
+					? persister.getPropertyInsertability()
+					: persister.getPropertyUpdateability();
 			final Type[] propertyTypes = persister.getPropertyTypes();
 
 			for ( int i = 0; i < values.length; i++ ) {
@@ -84,6 +101,22 @@ public final class Nullability {
 				if ( checkability[i] && values[i]!= LazyPropertyInitializer.UNFETCHED_PROPERTY ) {
 					final Object value = values[i];
 					if ( !nullability[i] && value == null ) {
+
+						// generally speaking this is an exception as we will throw below
+						// but first, we check for a special condition in which:
+						// 		1) we are processing a delete
+						//		2) the property represents the "other side" of this association
+						//		3) the other side is currently in the "being deleted" status
+						//		4) the property is defined to cascade deletes from the other side to
+						// 			this association property
+						//
+						// in such a case we allow this to continue
+						if ( checkType == NullabilityCheckType.DELETE ) {
+							if ( propertyTypes[i].isEntityType() ) {
+								final EntityType associationType = (EntityType) propertyTypes[i];
+
+							}
+						}
 
 						//check basic level one nullablilty
 						throw new PropertyValueException(

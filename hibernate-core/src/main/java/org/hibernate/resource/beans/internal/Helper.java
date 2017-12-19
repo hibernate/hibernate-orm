8a7d44aae14e7fbfc286faa77e32f9dfbd30b8f3
Yoann Rodière <yoann@hibernate.org>
2017-12-19 15:52:52 +0100

    HHH-12133 Use the BeanManager-provided ambiguous dependency resolution when possible
    This should take care of @Alternative in particular.

--- hibernate-core/src/main/java/org/hibernate/resource/beans/internal/Helper.java
+++ hibernate-core/src/main/java/org/hibernate/resource/beans/internal/Helper.java
@@ -6,10 +6,6 @@
  */
 package org.hibernate.resource.beans.internal;
 
-import java.util.Set;
-import javax.enterprise.inject.spi.Bean;
-import javax.enterprise.inject.spi.BeanManager;
-
 /**
  * @author Steve Ebersole
  */
@@ -20,24 +16,6 @@ public class Helper {
 	public static final Helper INSTANCE = new Helper();
 
 	private Helper() {
-	}
-
-	@SuppressWarnings("unchecked")
-	public <T> Bean<T> getNamedBean(String beanName, Class<T> beanContract, BeanManager beanManager) {
-		Set<Bean<?>> beans = beanManager.getBeans( beanContract, new NamedBeanQualifier( beanName ) );
-
-		if ( beans.isEmpty() ) {
-			throw new IllegalArgumentException(
-					"BeanManager returned no matching beans: name = " + beanName + "; contract = " + beanContract.getName()
-			);
-		}
-		if ( beans.size() > 1 ) {
-			throw new IllegalArgumentException(
-					"BeanManager returned multiple matching beans: name = " + beanName + "; contract = " + beanContract.getName()
-			);
-		}
-
-		return (Bean<T>) beans.iterator().next();
 	}
 
 	public CdiLifecycleManagementStrategy getLifecycleManagementStrategy(boolean shouldRegistryManageLifecycle) {

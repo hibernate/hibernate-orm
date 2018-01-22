02035cbd5bd87b7f54191a4e724d313bc096a323
Frank Langelage <Frank.Langelage@osnanet.de>
2018-01-22 21:43:04 +0100

    Combine the 2 if statements.

--- hibernate-core/src/main/java/org/hibernate/internal/CacheImpl.java
+++ hibernate-core/src/main/java/org/hibernate/internal/CacheImpl.java
@@ -406,11 +406,9 @@ public class CacheImpl implements CacheImplementor {
 		if ( settings.isQueryCacheEnabled() ) {
 			allCacheRegions.put( updateTimestampsCache.getRegion().getName(), updateTimestampsCache.getRegion() );
 			allCacheRegions.put( queryCache.getRegion().getName(), queryCache.getRegion() );
-		}
-		// keys in queryCaches may not be equal to the Region names
-		// obtained from queryCaches values; we need to be sure we are adding
-		// the actual QueryCache region name as the key in allCacheRegions.
-		if ( settings.isQueryCacheEnabled() ) {
+			// keys in queryCaches may not be equal to the Region names
+			// obtained from queryCaches values; we need to be sure we are adding
+			// the actual QueryCache region name as the key in allCacheRegions.
 			for ( QueryCache queryCacheValue : queryCaches.values() ) {
 				allCacheRegions.put( queryCacheValue.getRegion().getName(), queryCacheValue.getRegion() );
 			}

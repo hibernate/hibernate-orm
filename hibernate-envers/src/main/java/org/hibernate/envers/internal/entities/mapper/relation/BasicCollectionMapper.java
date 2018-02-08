5f9bcb8c7f2ba40f89ac93722d044ae6642bbc92
Chris Cranford <chris@hibernate.org>
2018-02-08 11:29:12 -0500

    HHH-11901 - Fix audited collections that contain null values.
    (cherry picked from commit 2977d8f)

--- hibernate-envers/src/main/java/org/hibernate/envers/internal/entities/mapper/relation/BasicCollectionMapper.java
+++ hibernate-envers/src/main/java/org/hibernate/envers/internal/entities/mapper/relation/BasicCollectionMapper.java
@@ -8,7 +8,9 @@ package org.hibernate.envers.internal.entities.mapper.relation;
 
 import java.io.Serializable;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.hibernate.collection.spi.PersistentCollection;
 import org.hibernate.engine.spi.SessionImplementor;
@@ -20,6 +22,7 @@ import org.hibernate.envers.internal.reader.AuditReaderImplementor;
 
 /**
  * @author Adam Warski (adam at warski dot org)
+ * @author Chris Cranford
  */
 public class BasicCollectionMapper<T extends Collection> extends AbstractCollectionMapper<T> implements PropertyMapper {
 	protected final MiddleComponentData elementComponentData;
@@ -79,5 +82,18 @@ public class BasicCollectionMapper<T extends Collection> extends AbstractCollect
 			Map<String, Object> data,
 			Object changed) {
 		elementComponentData.getComponentMapper().mapToMapFromObject( session, idData, data, changed );
+	}
+
+	@Override
+	protected Set<Object> buildCollectionChangeSet(Object eventCollection, Collection collection) {
+		final Set<Object> changeSet = new HashSet<>();
+		if ( eventCollection != null ) {
+			for ( Object entry : collection ) {
+				if ( entry != null ) {
+					changeSet.add( entry );
+				}
+			}
+		}
+		return changeSet;
 	}
 }

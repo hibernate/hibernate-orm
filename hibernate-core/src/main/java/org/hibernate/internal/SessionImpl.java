848217146cff9be3a1e59f2952966272c1cfb55e
Andrea Boriero <andrea@hibernate.org>
2018-02-13 13:53:58 +0000

    HHH-12289 - One call of the SessionImpl#listeners( ) method from SessionImpl#autoFlushIfRequired() is useless.

--- hibernate-core/src/main/java/org/hibernate/internal/SessionImpl.java
+++ hibernate-core/src/main/java/org/hibernate/internal/SessionImpl.java
@@ -1393,7 +1393,6 @@ public final class SessionImpl
 			return false;
 		}
 		AutoFlushEvent event = new AutoFlushEvent( querySpaces, this );
-		listeners( EventType.AUTO_FLUSH );
 		for ( AutoFlushEventListener listener : listeners( EventType.AUTO_FLUSH ) ) {
 			listener.onAutoFlush( event );
 		}

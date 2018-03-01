c3e5390048a38dc609199d2337409109bb8c5841
Sanne Grinovero <sanne@hibernate.org>
2018-03-01 13:45:37 +0000

    HHH-12334 ASTUtil improvements in Map usage

--- hibernate-core/src/main/java/org/hibernate/hql/internal/ast/util/ASTUtil.java
+++ hibernate-core/src/main/java/org/hibernate/hql/internal/ast/util/ASTUtil.java
@@ -389,13 +389,13 @@ public final class ASTUtil {
 	 *
 	 * @return The generated map.
 	 */
-	public static Map generateTokenNameCache(Class tokenTypeInterface) {
+	public static Map<Integer,String> generateTokenNameCache(Class tokenTypeInterface) {
 		final Field[] fields = tokenTypeInterface.getFields();
 		Map cache = new HashMap( (int) ( fields.length * .75 ) + 1 );
 		for ( final Field field : fields ) {
 			if ( Modifier.isStatic( field.getModifiers() ) ) {
 				try {
-					cache.put( field.get( null ), field.getName() );
+					cache.put( Integer.valueOf( field.getInt( null ) ), field.getName() );
 				}
 				catch (Throwable ignore) {
 				}

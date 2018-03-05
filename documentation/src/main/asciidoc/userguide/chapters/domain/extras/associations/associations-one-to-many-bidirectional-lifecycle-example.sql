a17a8df9e3b3dd5527c9fbd2b19a5be0e8ca3cbe
Roland Illig <rillig@it-economics.de>
2018-03-05 17:09:14 +0100

    HHH-12388 - User Guide and Javadoc typo fixes
    Fix SQL for bidirectional OneToMany The documented SQL didn't match what the test actually produces.

--- documentation/src/main/asciidoc/userguide/chapters/domain/extras/associations/associations-one-to-many-bidirectional-lifecycle-example.sql
+++ documentation/src/main/asciidoc/userguide/chapters/domain/extras/associations/associations-one-to-many-bidirectional-lifecycle-example.sql
@@ -1,10 +1,14 @@
-INSERT INTO Phone
-       ( number, person_id, id )
-VALUES ( '123-456-7890', NULL, 2 )
+INSERT INTO Person
+       ( id )
+VALUES ( 1 )
 
 INSERT INTO Phone
-       ( number, person_id, id )
-VALUES ( '321-654-0987', NULL, 3 )
+       ( "number", person_id, id )
+VALUES ( '123-456-7890', 1, 2 )
+
+INSERT INTO Phone
+       ( "number", person_id, id )
+VALUES ( '321-654-0987', 1, 3 )
 
 DELETE FROM Phone
 WHERE  id = 2

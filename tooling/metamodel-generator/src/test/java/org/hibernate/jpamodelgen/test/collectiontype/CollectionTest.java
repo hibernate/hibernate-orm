4c5e53552e4160007fa24f6610cf4e51cfa6e7e1
helloztt <347871727@qq.com>
2018-03-06 11:35:43 +0800

    fix ElementCollectionTest.testListAttributeWithGenericTypeForJavaBeanGetter

--- tooling/metamodel-generator/src/test/java/org/hibernate/jpamodelgen/test/collectiontype/CollectionTest.java
+++ tooling/metamodel-generator/src/test/java/org/hibernate/jpamodelgen/test/collectiontype/CollectionTest.java
@@ -1,20 +1,37 @@
 package org.hibernate.jpamodelgen.test.collectiontype;
 
 import org.hibernate.jpamodelgen.test.util.CompilationTest;
+import org.hibernate.jpamodelgen.test.util.TestForIssue;
 import org.hibernate.jpamodelgen.test.util.WithClasses;
 import org.junit.Test;
 
-import static org.hibernate.jpamodelgen.test.util.TestUtil.assertMetamodelClassGeneratedFor;
+import java.util.List;
+
+import static org.hibernate.jpamodelgen.test.util.TestUtil.*;
 
 /**
  * Created by helloztt on 2017-08-19.
  */
 public class CollectionTest extends CompilationTest {
 
-    @Test
-    @WithClasses({Goods.class, Product.class})
-    public void testPrimitiveArray() {
-        assertMetamodelClassGeneratedFor(Product.class);
-        assertMetamodelClassGeneratedFor(Goods.class);
-    }
+	@Test
+	@TestForIssue(jiraKey = "HHH-12338")
+	@WithClasses({Goods.class, Product.class})
+	public void testPrimitiveArray() throws ClassNotFoundException {
+		assertMetamodelClassGeneratedFor(Product.class);
+		assertMetamodelClassGeneratedFor(Goods.class);
+		assertListAttributeTypeInMetaModelFor(
+				Goods.class,
+				"productList",
+				Product.class,
+				"ListAttribute generic type should be Product"
+		);
+		assertAttributeTypeInMetaModelFor(
+				Goods.class,
+				"tags",
+				List.class,
+				"Wrong meta model type"
+		);
+
+	}
 }

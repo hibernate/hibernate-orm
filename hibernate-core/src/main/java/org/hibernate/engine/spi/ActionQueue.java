b7ddbdf68d380e88d15baaaa6bebdc8aedbd6743
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2018-03-13 15:28:06 +0200

    HHH-12374 - Order inserts sorting code gives up too soon
    (cherry picked from commit 097112353e4afaa1aad4e1d9de19951ed0875860)

--- hibernate-core/src/main/java/org/hibernate/engine/spi/ActionQueue.java
+++ hibernate-core/src/main/java/org/hibernate/engine/spi/ActionQueue.java
@@ -1168,7 +1168,7 @@ public class ActionQueue {
 
 			boolean sorted = false;
 
-			long maxIterations = latestBatches.size() * 2;
+			long maxIterations = latestBatches.size() * latestBatches.size();
 			long iterations = 0;
 
 			sort:

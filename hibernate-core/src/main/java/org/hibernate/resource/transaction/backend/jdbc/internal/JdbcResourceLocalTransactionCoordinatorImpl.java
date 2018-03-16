593d93d28f291fd5c6111d31cf0f80e7c5aaefc3
Andrea Boriero <andrea@hibernate.org>
2018-03-16 11:54:56 +0000

    HHH-12391 - with IronJacamar an EntityTransaction.rollback() with a non active Transaction that was marked for rollback only causes a failure

--- hibernate-core/src/main/java/org/hibernate/resource/transaction/backend/jdbc/internal/JdbcResourceLocalTransactionCoordinatorImpl.java
+++ hibernate-core/src/main/java/org/hibernate/resource/transaction/backend/jdbc/internal/JdbcResourceLocalTransactionCoordinatorImpl.java
@@ -245,10 +245,15 @@ public class JdbcResourceLocalTransactionCoordinatorImpl implements TransactionC
 
 		@Override
 		public void rollback() {
-			if ( rollbackOnly || getStatus() == TransactionStatus.ACTIVE ) {
+			try {
+				TransactionStatus status = jdbcResourceTransaction.getStatus();
+				if ( ( rollbackOnly && status != TransactionStatus.NOT_ACTIVE ) || status == TransactionStatus.ACTIVE ) {
+					jdbcResourceTransaction.rollback();
+					JdbcResourceLocalTransactionCoordinatorImpl.this.afterCompletionCallback( false );
+				}
+			}
+			finally {
 				rollbackOnly = false;
-				jdbcResourceTransaction.rollback();
-				JdbcResourceLocalTransactionCoordinatorImpl.this.afterCompletionCallback( false );
 			}
 
 			// no-op otherwise.

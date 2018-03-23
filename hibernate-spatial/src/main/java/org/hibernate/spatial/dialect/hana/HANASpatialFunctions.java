352a9e5a226f165a1217f2de00247805719fd1d0
Jonathan Bregler <jonathan.bregler@sap.com>
2018-03-23 16:56:42 +0100

    HHH-12426: fix checkstyle errors

--- hibernate-spatial/src/main/java/org/hibernate/spatial/dialect/hana/HANASpatialFunctions.java
+++ hibernate-spatial/src/main/java/org/hibernate/spatial/dialect/hana/HANASpatialFunctions.java
@@ -1,3 +1,9 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
 package org.hibernate.spatial.dialect.hana;
 
 public enum HANASpatialFunctions {
@@ -109,7 +115,7 @@ public enum HANASpatialFunctions {
 	
 	zmax("ST_ZMax"),
 	
-	zmin("ST_ZMin");;
+	zmin("ST_ZMin");
 
 	private final String functionName;
 

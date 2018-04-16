029753cc290b9450b76c446d58aaac2d7e4f6f5b
Rodrigo Turini <rdrg.turini@gmail.com>
2018-04-15 21:30:29 -0300

    HHH-12365 - User Guide: call_key should change to call_timestamp_epoch

--- documentation/src/main/asciidoc/userguide/chapters/domain/extras/collections/collections-map-custom-key-type-sql-example.sql
+++ documentation/src/main/asciidoc/userguide/chapters/domain/extras/collections/collections-map-custom-key-type-sql-example.sql
@@ -7,7 +7,7 @@ create table call_register (
     person_id int8 not null,
     phone_number int4,
     call_timestamp_epoch int8 not null,
-    primary key (person_id, call_key)
+    primary key (person_id, call_timestamp_epoch)
 )
 
 alter table if exists call_register

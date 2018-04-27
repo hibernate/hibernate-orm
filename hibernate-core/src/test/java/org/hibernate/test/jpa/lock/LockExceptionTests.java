c6dc398fc51fd897c156e381ad1fd6d3304d7530
Steve Ebersole <steve@hibernate.org>
2018-04-27 09:41:44 -0500

    HHH-8786 - find, refresh, lock should throw LockTimeoutException or PessimisticLockException when lock problems occur

--- hibernate-core/src/test/java/org/hibernate/test/jpa/lock/LockExceptionTests.java
+++ hibernate-core/src/test/java/org/hibernate/test/jpa/lock/LockExceptionTests.java
@@ -14,8 +14,11 @@ import javax.persistence.PessimisticLockException;
 import org.hibernate.LockOptions;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.dialect.MariaDBDialect;
 import org.hibernate.dialect.SQLServerDialect;
 
+import org.hibernate.testing.SkipForDialect;
+import org.hibernate.testing.SkipForDialects;
 import org.hibernate.testing.TestForIssue;
 import org.hibernate.testing.jdbc.SQLServerSnapshotIsolationConnectionProvider;
 import org.hibernate.testing.transaction.TransactionUtil2;
@@ -28,6 +31,9 @@ import static org.junit.Assert.fail;
 /**
  * @author Steve Ebersole
  */
+@SkipForDialects(
+		@SkipForDialect( value = MariaDBDialect.class, jiraKey = "HHH-8786", comment = "https://hibernate.atlassian.net/browse/HHH-8786")
+)
 public class LockExceptionTests extends AbstractJPATest {
 	@Override
 	public void configure(Configuration cfg) {

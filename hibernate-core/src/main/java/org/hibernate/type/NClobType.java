c502d7375925dafa0c807d58d42575f3e3a354c8
Guillaume Smet <guillaume.smet@gmail.com>
2018-05-28 12:35:37 +0200

    HHH-12555 Fix merging of lazy loaded blobs/clobs/nclobs

--- hibernate-core/src/main/java/org/hibernate/type/NClobType.java
+++ hibernate-core/src/main/java/org/hibernate/type/NClobType.java
@@ -7,12 +7,15 @@
 package org.hibernate.type;
 
 import java.sql.NClob;
+import java.sql.SQLException;
 
+import org.hibernate.engine.jdbc.LobCreator;
+import org.hibernate.engine.jdbc.spi.JdbcServices;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.type.descriptor.java.NClobTypeDescriptor;
 
 /**
- * A type that maps between {@link java.sql.Types#CLOB CLOB} and {@link java.sql.Clob}
+ * A type that maps between {@link java.sql.Types#NCLOB NCLOB} and {@link java.sql.NClob}
  *
  * @author Gavin King
  * @author Steve Ebersole
@@ -36,7 +39,21 @@ public class NClobType extends AbstractSingleColumnStandardBasicType<NClob> {
 
 	@Override
 	protected NClob getReplacement(NClob original, NClob target, SharedSessionContractImplementor session) {
+		if ( target == null ) {
+			return copyOriginalNClob( original, session );
+		}
 		return session.getJdbcServices().getJdbcEnvironment().getDialect().getLobMergeStrategy().mergeNClob( original, target, session );
 	}
 
+	private NClob copyOriginalNClob(NClob original, SharedSessionContractImplementor session) {
+		try {
+			final LobCreator lobCreator = session.getFactory().getServiceRegistry().getService( JdbcServices.class ).getLobCreator( session );
+			return original == null
+					? lobCreator.createNClob( "" )
+					: lobCreator.createNClob( original.getCharacterStream(), original.length() );
+		}
+		catch (SQLException e) {
+			throw session.getJdbcServices().getSqlExceptionHelper().convert( e, "unable to merge NCLOB data" );
+		}
+	}
 }

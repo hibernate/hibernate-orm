1af878166fe0883ceb3d9130afa2790850492624
Guillaume Smet <guillaume.smet@gmail.com>
2018-05-28 12:35:37 +0200

    HHH-12555 Fix merging of lazy loaded blobs/clobs/nclobs
    It's better to avoid pushing UNFETCHED_PROPERTY to the types as it
    requires all the types to take it into account.
    
    TypeHelper looks like the only sensible caller that needs change.

--- hibernate-core/src/main/java/org/hibernate/type/ClobType.java
+++ hibernate-core/src/main/java/org/hibernate/type/ClobType.java
@@ -7,7 +7,10 @@
 package org.hibernate.type;
 
 import java.sql.Clob;
+import java.sql.SQLException;
 
+import org.hibernate.engine.jdbc.LobCreator;
+import org.hibernate.engine.jdbc.spi.JdbcServices;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.type.descriptor.java.ClobTypeDescriptor;
 
@@ -36,7 +39,21 @@ public class ClobType extends AbstractSingleColumnStandardBasicType<Clob> {
 
 	@Override
 	protected Clob getReplacement(Clob original, Clob target, SharedSessionContractImplementor session) {
-		return session.getJdbcServices().getJdbcEnvironment().getDialect().getLobMergeStrategy().mergeClob( original, target, session );
+		if ( target == null ) {
+			return copyOriginalClob( (Clob) original, session );
+		}
+		return session.getJdbcServices().getJdbcEnvironment().getDialect().getLobMergeStrategy().mergeClob( (Clob) original, (Clob) target, session );
 	}
 
+	private Clob copyOriginalClob(Clob original, SharedSessionContractImplementor session) {
+		try {
+			final LobCreator lobCreator = session.getFactory().getServiceRegistry().getService( JdbcServices.class ).getLobCreator( session );
+			return original == null
+					? lobCreator.createClob( "" )
+					: lobCreator.createClob( original.getCharacterStream(), original.length() );
+		}
+		catch (SQLException e) {
+			throw session.getJdbcServices().getSqlExceptionHelper().convert( e, "unable to merge CLOB data" );
+		}
+	}
 }

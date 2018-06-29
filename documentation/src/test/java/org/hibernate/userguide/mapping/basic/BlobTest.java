b9cb71772b89126f419c7ddb9522302c76740399
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2018-06-29 10:25:10 +0300

    HHH-12341 - Documentation refers to nonexistent "image clob"

--- documentation/src/test/java/org/hibernate/userguide/mapping/basic/BlobTest.java
+++ documentation/src/test/java/org/hibernate/userguide/mapping/basic/BlobTest.java
@@ -30,103 +30,104 @@ import static org.junit.Assert.fail;
  */
 public class BlobTest extends BaseEntityManagerFunctionalTestCase {
 
-	@Override
-	protected Class<?>[] getAnnotatedClasses() {
-		return new Class<?>[] {
-			Product.class
-		};
-	}
+    @Override
+    protected Class<?>[] getAnnotatedClasses() {
+        return new Class<?>[] {
+            Product.class
+        };
+    }
 
-	@Test
-	public void test() {
-		Integer productId = doInJPA( this::entityManagerFactory, entityManager -> {
-			Session session = entityManager.unwrap( Session.class );
+    @Test
+    public void test() {
+        Integer productId = doInJPA( this::entityManagerFactory, entityManager -> {
+            Session session = entityManager.unwrap( Session.class );
 
-			//tag::basic-blob-persist-example[]
-			byte[] image = new byte[] {1, 2, 3};
+            //tag::basic-blob-persist-example[]
+            byte[] image = new byte[] {1, 2, 3};
 
-			final Product product = new Product();
-			product.setId( 1 );
-			product.setName( "Mobile phone" );
+            final Product product = new Product();
+            product.setId( 1 );
+            product.setName( "Mobile phone" );
 
-			session.doWork( connection -> {
-				product.setImage( BlobProxy.generateProxy( image ) );
-			} );
+            product.setImage( BlobProxy.generateProxy( image ) );
 
-			entityManager.persist( product );
-			//end::basic-blob-persist-example[]
+            entityManager.persist( product );
+            //end::basic-blob-persist-example[]
 
-			return product.getId();
-		} );
-		doInJPA( this::entityManagerFactory, entityManager -> {
-			try {
-				//tag::basic-blob-find-example[]
+            return product.getId();
+        } );
+        doInJPA( this::entityManagerFactory, entityManager -> {
+            try {
+                //tag::basic-blob-find-example[]
 
-				Product product = entityManager.find( Product.class, productId );
-				
-				try (InputStream inputStream = product.getImage().getBinaryStream()) {
-					assertArrayEquals(new byte[] {1, 2, 3}, toBytes( inputStream ) );
-				}
-				//end::basic-blob-find-example[]
-			}
-			catch (Exception e) {
-				fail( e.getMessage() );
-			}
-		} );
-	}
+                Product product = entityManager.find( Product.class, productId );
 
-	private byte[] toBytes(InputStream inputStream) throws IOException {
-		BufferedInputStream bufferedInputStream = new BufferedInputStream( inputStream);
-		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-		int result = bufferedInputStream.read();
-		while(result != -1) {
-			byteArrayOutputStream.write((byte) result);
-			result = bufferedInputStream.read();
-		}
-		return byteArrayOutputStream.toByteArray();
-	}
+                try (InputStream inputStream = product.getImage().getBinaryStream()) {
+                    assertArrayEquals(new byte[] {1, 2, 3}, toBytes( inputStream ) );
+                }
+                //end::basic-blob-find-example[]
+            }
+            catch (Exception e) {
+                fail( e.getMessage() );
+            }
+        } );
+    }
+
+    private byte[] toBytes(InputStream inputStream) throws IOException {
+        BufferedInputStream bufferedInputStream = new BufferedInputStream( inputStream);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        int result = bufferedInputStream.read();
+
+        while(result != -1) {
+            byteArrayOutputStream.write((byte) result);
+            result = bufferedInputStream.read();
+        }
+
+        return byteArrayOutputStream.toByteArray();
+    }
 
 
-	//tag::basic-blob-example[]
-	@Entity(name = "Product")
-	public static class Product {
+    //tag::basic-blob-example[]
+    @Entity(name = "Product")
+    public static class Product {
 
-		@Id
-		private Integer id;
+        @Id
+        private Integer id;
 
-		private String name;
+        private String name;
 
-		@Lob
-		private Blob image;
+        @Lob
+        private Blob image;
 
-		//Getters and setters are omitted for brevity
+        //Getters and setters are omitted for brevity
 
-	//end::basic-blob-example[]
-		public Integer getId() {
-			return id;
-		}
+    //end::basic-blob-example[]
+        public Integer getId() {
+            return id;
+        }
 
-		public void setId(Integer id) {
-			this.id = id;
-		}
+        public void setId(Integer id) {
+            this.id = id;
+        }
 
-		public String getName() {
-			return name;
-		}
+        public String getName() {
+            return name;
+        }
 
-		public void setName(String name) {
-			this.name = name;
-		}
+        public void setName(String name) {
+            this.name = name;
+        }
 
-		public Blob getImage() {
-			return image;
-		}
+        public Blob getImage() {
+            return image;
+        }
 
-		public void setImage(Blob image) {
-			this.image = image;
-		}
+        public void setImage(Blob image) {
+            this.image = image;
+        }
 
-		//tag::basic-blob-example[]
-	}
-	//end::basic-blob-example[]
+        //tag::basic-blob-example[]
+    }
+    //end::basic-blob-example[]
 }

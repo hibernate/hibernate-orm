a0d2f539378e5f9ac6f14f804205b8b869c3e433
Yoann Rodière <yoann@hibernate.org>
2018-07-04 09:25:20 +0200

    HHH-12695 Explicitly mark contracts as @Incubating, since a comment in QueryParameter implies they are in fact incubating
    I did not mark ParameterRegistration as incubating, because it's a
    pre-existing interface that we can't reasonably consider as incubating.
    
    It's a bit odd to have a non-incubating contract (ParameterRegistration)
    extend an incubating one (ProcedureParameter), though...

--- hibernate-core/src/main/java/org/hibernate/engine/query/spi/AbstractParameterDescriptor.java
+++ hibernate-core/src/main/java/org/hibernate/engine/query/spi/AbstractParameterDescriptor.java
@@ -6,12 +6,16 @@
  */
 package org.hibernate.engine.query.spi;
 
+import org.hibernate.Incubating;
 import org.hibernate.query.QueryParameter;
 import org.hibernate.type.Type;
 
 /**
+ * NOTE: Consider this contract (and its sub-contracts) as incubating as we transition to 6.0 and SQM
+ *
  * @author Steve Ebersole
  */
+@Incubating
 public abstract class AbstractParameterDescriptor implements QueryParameter {
 	private final int[] sourceLocations;
 

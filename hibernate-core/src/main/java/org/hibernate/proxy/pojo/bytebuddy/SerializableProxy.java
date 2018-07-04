3336489e40af55c57558636d6dc465fa2f3610d9
Yoann Rodière <yoann@hibernate.org>
2018-07-04 13:06:06 +0200

    HHH-12720 Allow lazy loading outside of a transaction after proxy deserialization if the proper settings were enabled

--- hibernate-core/src/main/java/org/hibernate/proxy/pojo/bytebuddy/SerializableProxy.java
+++ hibernate-core/src/main/java/org/hibernate/proxy/pojo/bytebuddy/SerializableProxy.java
@@ -26,6 +26,10 @@ public final class SerializableProxy extends AbstractSerializableProxy {
 
 	private final CompositeType componentIdType;
 
+	/**
+	 * @deprecated use {@link #SerializableProxy(String, Class, Class[], Serializable, Boolean, String, boolean, Method, Method, CompositeType)} instead.
+	 */
+	@Deprecated
 	public SerializableProxy(
 			String entityName,
 			Class persistentClass,
@@ -35,7 +39,24 @@ public final class SerializableProxy extends AbstractSerializableProxy {
 			Method getIdentifierMethod,
 			Method setIdentifierMethod,
 			CompositeType componentIdType) {
-		super( entityName, id, readOnly );
+		this(
+				entityName, persistentClass, interfaces, id, readOnly, null, false,
+				getIdentifierMethod, setIdentifierMethod, componentIdType
+		);
+	}
+
+	public SerializableProxy(
+			String entityName,
+			Class persistentClass,
+			Class[] interfaces,
+			Serializable id,
+			Boolean readOnly,
+			String sessionFactoryUuid,
+			boolean allowLoadOutsideTransaction,
+			Method getIdentifierMethod,
+			Method setIdentifierMethod,
+			CompositeType componentIdType) {
+		super( entityName, id, readOnly, sessionFactoryUuid, allowLoadOutsideTransaction );
 		this.persistentClass = persistentClass;
 		this.interfaces = interfaces;
 		if ( getIdentifierMethod != null ) {
@@ -105,7 +126,7 @@ public final class SerializableProxy extends AbstractSerializableProxy {
 
 	private Object readResolve() {
 		HibernateProxy proxy = ByteBuddyProxyFactory.deserializeProxy( this );
-		setReadOnlyBeforeAttachedToSession( (ByteBuddyInterceptor) proxy.getHibernateLazyInitializer() );
+		afterDeserialization( (ByteBuddyInterceptor) proxy.getHibernateLazyInitializer() );
 		return proxy;
 	}
 }

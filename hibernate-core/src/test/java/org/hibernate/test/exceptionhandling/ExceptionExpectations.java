566a93d6c7bf5d55ec8a561a0442bc988ea94d78
Yoann Rodière <yoann@hibernate.org>
2018-07-18 13:18:29 +0200

    HHH-12666 Test the current behavior of exception conversion on persist/save/merge/flush/etc.
    Original tests by Gail Badner:
    https://github.com/gbadner/hibernate-core/commit/09aa6fbce2a9ba4fb82b6ce1075fd68fb09db510

---
+++ hibernate-core/src/test/java/org/hibernate/test/exceptionhandling/ExceptionExpectations.java
@@ -0,0 +1,108 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.test.exceptionhandling;
+
+import java.sql.SQLException;
+import javax.persistence.PersistenceException;
+
+import org.hibernate.TransientObjectException;
+import org.hibernate.exception.ConstraintViolationException;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
+
+interface ExceptionExpectations {
+
+	static ExceptionExpectations jpa() {
+		return new ExceptionExpectations() {
+			@Override
+			public void onConstraintViolationOnSaveAndSaveOrUpdate(RuntimeException e) {
+				assertThat( e, instanceOf( PersistenceException.class ) );
+			}
+
+			@Override
+			public void onConstraintViolationOnPersistAndMergeAndFlush(RuntimeException e) {
+				assertThat( e, instanceOf( PersistenceException.class ) );
+				assertThat( e.getCause(), instanceOf( ConstraintViolationException.class ) );
+				assertThat( e.getCause().getCause(), instanceOf( SQLException.class ) );
+			}
+
+			@Override
+			public void onTransientObjectOnSaveAndSaveOrUpdate(RuntimeException e) {
+				assertThat( e, instanceOf( TransientObjectException.class ) );
+			}
+
+			@Override
+			public void onTransientObjectOnPersistAndMergeAndFlush(RuntimeException e) {
+				assertThat( e, instanceOf( IllegalStateException.class ) );
+				assertThat( e.getCause(), instanceOf( TransientObjectException.class ) );
+			}
+		};
+	}
+
+	static ExceptionExpectations nativePre52() {
+		return new ExceptionExpectations() {
+			@Override
+			public void onConstraintViolationOnSaveAndSaveOrUpdate(RuntimeException e) {
+				assertThat( e, instanceOf( ConstraintViolationException.class ) );
+				assertThat( e.getCause(), instanceOf( SQLException.class ) );
+			}
+
+			@Override
+			public void onConstraintViolationOnPersistAndMergeAndFlush(RuntimeException e) {
+				assertThat( e, instanceOf( ConstraintViolationException.class ) );
+				assertThat( e.getCause(), instanceOf( SQLException.class ) );
+			}
+
+			@Override
+			public void onTransientObjectOnSaveAndSaveOrUpdate(RuntimeException e) {
+				assertThat( e, instanceOf( TransientObjectException.class ) );
+			}
+
+			@Override
+			public void onTransientObjectOnPersistAndMergeAndFlush(RuntimeException e) {
+				assertThat( e, instanceOf( TransientObjectException.class ) );
+			}
+		};
+	}
+
+	static ExceptionExpectations nativePost52() {
+		return new ExceptionExpectations() {
+			@Override
+			public void onConstraintViolationOnSaveAndSaveOrUpdate(RuntimeException e) {
+				assertThat( e, instanceOf( ConstraintViolationException.class ) );
+				assertThat( e.getCause(), instanceOf( SQLException.class ) );
+			}
+
+			@Override
+			public void onConstraintViolationOnPersistAndMergeAndFlush(RuntimeException e) {
+				assertThat( e, instanceOf( PersistenceException.class ) );
+				assertThat( e.getCause(), instanceOf( ConstraintViolationException.class ) );
+				assertThat( e.getCause().getCause(), instanceOf( SQLException.class ) );
+			}
+
+			@Override
+			public void onTransientObjectOnSaveAndSaveOrUpdate(RuntimeException e) {
+				assertThat( e, instanceOf( TransientObjectException.class ) );
+			}
+
+			@Override
+			public void onTransientObjectOnPersistAndMergeAndFlush(RuntimeException e) {
+				assertThat( e, instanceOf( IllegalStateException.class ) );
+				assertThat( e.getCause(), instanceOf( TransientObjectException.class ) );
+			}
+		};
+	}
+
+	void onConstraintViolationOnSaveAndSaveOrUpdate(RuntimeException e);
+
+	void onConstraintViolationOnPersistAndMergeAndFlush(RuntimeException e);
+
+	void onTransientObjectOnSaveAndSaveOrUpdate(RuntimeException e);
+
+	void onTransientObjectOnPersistAndMergeAndFlush(RuntimeException e);
+}

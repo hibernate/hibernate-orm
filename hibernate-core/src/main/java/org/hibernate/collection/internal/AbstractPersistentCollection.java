0c1ca113750f3e1c3658cd7c60b0af4a8f4e724d
Gail Badner <gbadner@redhat.com>
2018-07-26 16:00:22 -0700

    HHH-11209 : Test cases
    HHH-11209 : NullPointerException in EntityType.replace() with a PersistentBag
    
    HHH-11209 : Add test for merging a detached collection with queued operations
    
    HHH-11209 : Throw UnsupportedOperationException if a detached collection with queued operations is merged
    
    HHH-11209 : Ignore queued operations when merging a detached collection with queued operations; add warnings
    
    HHH-11209 : Fix typo in comment
    (cherry picked from commit 6f5b1e554386662e02d4ef3b91c23863258ff481)

--- hibernate-core/src/main/java/org/hibernate/collection/internal/AbstractPersistentCollection.java
+++ hibernate-core/src/main/java/org/hibernate/collection/internal/AbstractPersistentCollection.java
@@ -497,6 +497,7 @@ public abstract class AbstractPersistentCollection implements Serializable, Pers
 		for ( DelayedOperation operation : operationQueue ) {
 			operation.operate();
 		}
+		clearOperationQueue();
 	}
 
 	@Override
@@ -508,9 +509,13 @@ public abstract class AbstractPersistentCollection implements Serializable, Pers
 
 	@Override
 	public void postAction() {
-		operationQueue = null;
+		clearOperationQueue();
 		cachedSize = -1;
 		clearDirty();
+	}
+
+	public final void clearOperationQueue() {
+		operationQueue = null;
 	}
 
 	@Override
@@ -534,9 +539,8 @@ public abstract class AbstractPersistentCollection implements Serializable, Pers
 	public boolean afterInitialize() {
 		setInitialized();
 		//do this bit after setting initialized to true or it will recurse
-		if ( operationQueue != null ) {
+		if ( hasQueuedOperations() ) {
 			performQueuedOperations();
-			operationQueue = null;
 			cachedSize = -1;
 			return false;
 		}
@@ -605,6 +609,9 @@ public abstract class AbstractPersistentCollection implements Serializable, Pers
 		prepareForPossibleLoadingOutsideTransaction();
 		if ( currentSession == this.session ) {
 			if ( !isTempSession ) {
+				if ( hasQueuedOperations() ) {
+					LOG.queuedOperationWhenDetachFromSession( MessageHelper.collectionInfoString( getRole(), getKey() ) );
+				}
 				this.session = null;
 			}
 			return true;
@@ -632,25 +639,22 @@ public abstract class AbstractPersistentCollection implements Serializable, Pers
 		if ( session == this.session ) {
 			return false;
 		}
-		else {
-			if ( this.session != null ) {
-				final String msg = generateUnexpectedSessionStateMessage( session );
-				if ( isConnectedToSession() ) {
-					throw new HibernateException(
-							"Illegal attempt to associate a collection with two open sessions. " + msg
-					);
-				}
-				else {
-					LOG.logUnexpectedSessionInCollectionNotConnected( msg );
-					this.session = session;
-					return true;
-				}
+		else if ( this.session != null ) {
+			final String msg = generateUnexpectedSessionStateMessage( session );
+			if ( isConnectedToSession() ) {
+				throw new HibernateException(
+						"Illegal attempt to associate a collection with two open sessions. " + msg
+				);
 			}
 			else {
-				this.session = session;
-				return true;
+				LOG.logUnexpectedSessionInCollectionNotConnected( msg );
 			}
 		}
+		if ( hasQueuedOperations() ) {
+			LOG.queuedOperationWhenAttachToSession( MessageHelper.collectionInfoString( getRole(), getKey() ) );
+		}
+		this.session = session;
+		return true;
 	}
 
 	private String generateUnexpectedSessionStateMessage(SharedSessionContractImplementor session) {

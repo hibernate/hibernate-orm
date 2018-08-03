70a39d91cbfaff47bed6bb2180477b8897d9d585
Gail Badner <gbadner@redhat.com>
2018-08-03 01:22:48 -0700

    HHH-12875 HHH-12882 : test cases
    (cherry picked from commit cdf5e45514e348f6eae196e3abe23ac60d899fd7)

--- hibernate-core/src/test/java/org/hibernate/test/where/hbm/File.java
+++ hibernate-core/src/test/java/org/hibernate/test/where/hbm/File.java
@@ -6,7 +6,7 @@
  */
 
 //$Id: File.java 8043 2005-08-30 15:20:42Z oneovthafew $
-package org.hibernate.test.where;
+package org.hibernate.test.where.hbm;
 import java.util.Set;
 
 public class File {

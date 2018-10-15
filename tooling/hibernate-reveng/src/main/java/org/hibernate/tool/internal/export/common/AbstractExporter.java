be843168a038d37a774f7569da71dfdcfa726746
Koen Aers <koen.aers@gmail.com>
2018-10-15 16:48:32 +0200

    HBX-1744: Remove methods Exporter#getOutputDirectory() and Exporter#setOutputDirectory(File) from interface org.hibernate.tool.api.export.Exporter - Make AbstractExporter#getOutputDirectory() protected
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- tooling/hibernate-reveng/src/main/java/org/hibernate/tool/internal/export/common/AbstractExporter.java
+++ tooling/hibernate-reveng/src/main/java/org/hibernate/tool/internal/export/common/AbstractExporter.java
@@ -58,7 +58,7 @@ public abstract class AbstractExporter implements Exporter, ExporterConstants {
 		return metadata;
 	}
 	
-	public File getOutputDirectory() {
+	protected File getOutputDirectory() {
 		return (File)getProperties().get(OUTPUT_FOLDER);
 	}
 

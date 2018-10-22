a9102f944cf145739c4d6eaebade799f37355a43
Chris Cranford <chris@hibernate.org>
2018-10-22 18:15:58 -0400

    HHH-13164 - Revert change made for HHH-12464.
    (cherry picked from commit dc873c3d3672f24bf6f6a1bd15e53d31cfdaf523)

--- hibernate-core/src/test/java/org/hibernate/id/CreateDeleteTest.java
+++ hibernate-core/src/test/java/org/hibernate/id/CreateDeleteTest.java
@@ -7,44 +7,20 @@
 package org.hibernate.id;
 
 import org.hibernate.FlushMode;
-import org.hibernate.action.internal.EntityAction;
 import org.hibernate.dialect.AbstractHANADialect;
-import org.hibernate.internal.CoreMessageLogger;
 
 import org.hibernate.testing.DialectChecks;
 import org.hibernate.testing.RequiresDialectFeature;
 import org.hibernate.testing.SkipForDialect;
 import org.hibernate.testing.TestForIssue;
 import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
-import org.hibernate.testing.logger.LoggerInspectionRule;
-import org.hibernate.testing.logger.Triggerable;
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 
-import org.jboss.logging.Logger;
-
-import static junit.framework.TestCase.assertTrue;
 import static org.hibernate.testing.transaction.TransactionUtil.doInHibernate;
 
 @RequiresDialectFeature(DialectChecks.SupportsIdentityColumns.class)
 @TestForIssue(jiraKey = "HHH-12464")
 public class CreateDeleteTest extends BaseCoreFunctionalTestCase {
-
-	@Rule
-	public LoggerInspectionRule logInspection = new LoggerInspectionRule(
-			Logger.getMessageLogger( CoreMessageLogger.class, EntityAction.class.getName() )
-	);
-
-	private Triggerable triggerable;
-
-	@Before
-	public void setUp() {
-		triggerable = logInspection.watchForLogMessages(
-				"Skipping action - the persistence context does not contain any entry for the entity" );
-		triggerable.reset();
-	}
-
 	@Test
 	@SkipForDialect(value = AbstractHANADialect.class, comment = " HANA doesn't support tables consisting of only a single auto-generated column")
 	public void createAndDeleteAnEntityInTheSameTransactionTest() {
@@ -54,8 +30,6 @@ public class CreateDeleteTest extends BaseCoreFunctionalTestCase {
 			session.persist( entity );
 			session.delete( entity );
 		} );
-
-		assertTrue( triggerable.wasTriggered() );
 	}
 
 	@Override

80291d3c4fc4be71095b907d9e2ee9909f698a99
Jan-Willem Gmelig Meyling <jan-willem@youngmediaexperts.nl>
2018-10-25 12:22:19 +0200

    Remove invalid workaround from ea77c1f for isNullable regression from 2bfe300

--- hibernate-core/src/main/java/org/hibernate/persister/entity/AbstractPropertyMapping.java
+++ hibernate-core/src/main/java/org/hibernate/persister/entity/AbstractPropertyMapping.java
@@ -386,7 +386,7 @@ public abstract class AbstractPropertyMapping implements PropertyMapping {
 			}
 		}
 
-		if ( (! etype.isNullable() || etype.isReferenceToPrimaryKey() ) && idPropName != null ) {
+		if ( (! etype.isNullable() ) && idPropName != null ) {
 			String idpath2 = extendPath( path, idPropName );
 			addPropertyPath( idpath2, idtype, columns, columnReaders, columnReaderTemplates, null, factory );
 			initPropertyPaths( idpath2, idtype, columns, columnReaders, columnReaderTemplates, null, factory );

29db09ddac86be485feb033d4f7b7b633f5ca435
Guillaume Smet <guillaume.smet@gmail.com>
2018-12-17 13:53:54 +0100

    HHH-12555 Add a DialectCheck for NClob support
    (cherry picked from commit 855f34c77181fd9ff103a8d2bf9c9bfb37360f2c)

--- hibernate-testing/src/main/java/org/hibernate/testing/DialectChecks.java
+++ hibernate-testing/src/main/java/org/hibernate/testing/DialectChecks.java
@@ -8,6 +8,9 @@ package org.hibernate.testing;
 
 import org.hibernate.dialect.DB2Dialect;
 import org.hibernate.dialect.Dialect;
+import org.hibernate.dialect.MySQLDialect;
+import org.hibernate.dialect.PostgreSQL81Dialect;
+import org.hibernate.dialect.SybaseDialect;
 
 /**
  * Container class for different implementation of the {@link DialectCheck} interface.
@@ -153,13 +156,13 @@ abstract public class DialectChecks {
 			return dialect.supportsExistsInSelect();
 		}
 	}
-	
+
 	public static class SupportsLobValueChangePropogation implements DialectCheck {
 		public boolean isMatch(Dialect dialect) {
 			return dialect.supportsLobValueChangePropogation();
 		}
 	}
-	
+
 	public static class SupportsLockTimeouts implements DialectCheck {
 		public boolean isMatch(Dialect dialect) {
 			return dialect.supportsLockTimeouts();
@@ -253,6 +256,18 @@ abstract public class DialectChecks {
 		public boolean isMatch(Dialect dialect) {
 			return !(
 				dialect instanceof DB2Dialect
+			);
+		}
+	}
+
+	public static class SupportsNClob implements DialectCheck {
+		@Override
+		public boolean isMatch(Dialect dialect) {
+			return !(
+				dialect instanceof DB2Dialect ||
+				dialect instanceof PostgreSQL81Dialect ||
+				dialect instanceof SybaseDialect ||
+				dialect instanceof MySQLDialect
 			);
 		}
 	}

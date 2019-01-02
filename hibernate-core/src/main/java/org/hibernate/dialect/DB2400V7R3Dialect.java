a712395280f524ecef03aa7d44789c038da40a75
Pierrick Rouxel <pierrick.rouxel@me.com>
2019-01-02 23:12:58 +0100

    HHH-12856 Reintroduce limitation for legacy OS400

--- hibernate-core/src/main/java/org/hibernate/dialect/DB2400V7R3Dialect.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/DB2400V7R3Dialect.java
@@ -45,6 +45,18 @@ public class DB2400V7R3Dialect extends DB2400Dialect {
 	}
 
 	@Override
+	@SuppressWarnings("deprecation")
+	public String getLimitString(String sql, int offset, int limit) {
+		if ( offset == 0 ) {
+			return sql + " fetch first " + limit + " rows only";
+		}
+		//nest the main query in an outer select
+		return "select * from ( select inner2_.*, rownumber() over(order by order of inner2_) as rownumber_ from ( "
+				+ sql + " fetch first " + limit + " rows only ) as inner2_ ) as inner1_ where rownumber_ > "
+				+ offset + " order by rownumber_";
+	}
+
+	@Override
 	public IdentityColumnSupport getIdentityColumnSupport() {
 		return new DB2IdentityColumnSupport();
 	}

0c9c3fc7a3babb1d023caf07bc273d8e111d9d94
Pierrick Rouxel <pierrick.rouxel@me.com>
2019-01-02 23:34:14 +0100

    HHH-12856 Add AS400V7R3Dialect as default for V7R3 of IBM i

--- hibernate-core/src/main/java/org/hibernate/dialect/Database.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/Database.java
@@ -47,7 +47,7 @@ public enum Database {
 	DB2 {
 		@Override
 		public Class<? extends Dialect> latestDialect() {
-			return DB2400Dialect.class;
+			return DB2400V7R3Dialect.class;
 		}
 
 		@Override
@@ -55,8 +55,17 @@ public enum Database {
 			final String databaseName = info.getDatabaseName();
 
 			if ( "DB2 UDB for AS/400".equals( databaseName ) ) {
-				return new DB2400Dialect();
+				final int majorVersion = info.getDatabaseMajorVersion();
+				final int minorVersion = info.getDatabaseMinorVersion();
+
+				if ( majorVersion > 7 || ( majorVersion == 7 && minorVersion >= 3 ) ) {
+					return latestDialectInstance( this );
+				}
+				else {
+					return new DB2400Dialect();
+				}
 			}
+
 
 			if ( databaseName.startsWith( "DB2/" ) ) {
 				return new DB2Dialect();

4902eeef69e4dcc05b9154175d0e86511b129e5d
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2019-01-24 15:21:37 +0200

    HHH-13256 - Fix the fieldsPreUpdateNeeded property index allocation in AbstractEntityPersister#update

--- hibernate-core/src/test/java/org/hibernate/orm/test/util/jdbc/CallableStatementSpyConnectionProvider.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/util/jdbc/CallableStatementSpyConnectionProvider.java
@@ -4,11 +4,10 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.test.util.jdbc;
+package org.hibernate.orm.test.util.jdbc;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
--- hibernate-core/src/test2/java/org/hibernate/test/annotations/InMemoryCreationTimestampNullableColumnTest.java
+++ hibernate-core/src/test2/java/org/hibernate/test/annotations/InMemoryCreationTimestampNullableColumnTest.java
@@ -25,57 +25,56 @@ import static org.hibernate.testing.transaction.TransactionUtil.doInJPA;
 /**
  * @author Vlad Mihalcea
  */
-@TestForIssue( jiraKey = "HHH-11096" )
+@TestForIssue(jiraKey = "HHH-11096")
 public class InMemoryCreationTimestampNullableColumnTest extends BaseEntityManagerFunctionalTestCase {
 
-    @Override
-    protected Class<?>[] getAnnotatedClasses() {
-        return new Class<?>[]{
-                Person.class
-        };
-    }
+	@Override
+	protected Class<?>[] getAnnotatedClasses() {
+		return new Class<?>[] {
+				Person.class
+		};
+	}
 
-    @Entity(name = "Person")
-    public class Person {
+	@Entity(name = "Person")
+	public static class Person {
 
-        @Id
-        @GeneratedValue
-        private Long id;
+		@Id
+		@GeneratedValue
+		private Long id;
 
-        @NaturalId
-        private String name;
+		@NaturalId
+		private String name;
 
-        @Column(nullable = false)
-        @CreationTimestamp
-        private Date creationDate;
+		@Column(nullable = false)
+		@CreationTimestamp
+		private Date creationDate;
 
-        public String getName() {
-            return name;
-        }
+		public String getName() {
+			return name;
+		}
 
-        public void setName(String name) {
-            this.name = name;
-        }
+		public void setName(String name) {
+			this.name = name;
+		}
 
-        public Date getCreationDate() {
-            return creationDate;
-        }
+		public Date getCreationDate() {
+			return creationDate;
+		}
 
-        public void setCreationDate(Date creationDate) {
-            this.creationDate = creationDate;
-        }
+		public void setCreationDate(Date creationDate) {
+			this.creationDate = creationDate;
+		}
+	}
 
-    }
+	@Test
+	public void generatesCurrentTimestamp() {
+		doInJPA( this::entityManagerFactory, entityManager -> {
+			Person person = new Person();
+			person.setName( "John Doe" );
+			entityManager.persist( person );
 
-    @Test
-    public void generatesCurrentTimestamp() {
-        doInJPA(this::entityManagerFactory, entityManager -> {
-            Person person = new Person();
-            person.setName("John Doe");
-            entityManager.persist(person);
-
-            entityManager.flush();
-            Assert.assertNotNull(person.getCreationDate());
-        });
-    }
+			entityManager.flush();
+			Assert.assertNotNull( person.getCreationDate() );
+		} );
+	}
 }

14ff202a4067c6d2640a338db005f0829cdeb481
Paul Chapman <pchapman@nuodb.com>
2019-01-30 13:56:42 +0700

    Use eOrig.tAsDate instead of eGotten.tAsDated + comments

--- hibernate-core/src/test/java/org/hibernate/test/temporal/TimePropertyTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/temporal/TimePropertyTest.java
@@ -10,6 +10,8 @@ import java.sql.Time;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.logging.Logger;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -33,17 +35,23 @@ import static junit.framework.Assert.assertNotNull;
 public class TimePropertyTest extends BaseCoreFunctionalTestCase {
 	private final DateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
 
+	// NuoDB: add logger
+	Logger logger = Logger.getLogger(BaseCoreFunctionalTestCase.class.getName());
+
 	@Test
 	public void testTimeAsDate() {
 		final Entity eOrig = new Entity();
-		Calendar calendar = Calendar.getInstance();
-		// See javadoc for java.sql.Time: 'The date components should be set to the "zero epoch" value of January 1, 1970 and should not be accessed'
-		// Other dates can potentially lead to errors in JDBC drivers, in particular MySQL ConnectorJ 8.x.
-		calendar.set( 1970, Calendar.JANUARY, 1 );
-		// H2Dialect uses TIME (without fractional seconds precision) so H2 would round half up if milliseconds were set
-		// See also: http://h2database.com/html/datatypes.html#time_type
-		calendar.set( Calendar.MILLISECOND, 0 );
-		eOrig.tAsDate = new Time( calendar.getTimeInMillis() );
+
+		// NuoDB: Modified to use original time in queries not the time in
+		// eGotten. They are different - testTime has no milliseconds
+		// but eGotten.tAsDate does. NuoDB query ignores millis so it
+		// doesn't find a match.
+		// Exact match times are probably as bogus as exact match floating
+		// point numbers so this is reasonable behaviour.  The assert at
+		// line 68 ignores millis but the queries do not.
+		Time testTime = new Time( new Date().getTime() );
+
+		eOrig.tAsDate = testTime;
 
 		Session s = openSession();
 		s.getTransaction().begin();
@@ -58,20 +66,31 @@ public class TimePropertyTest extends BaseCoreFunctionalTestCase {
 		// Some databases retain the milliseconds when being inserted and some don't;
 		final String tAsDateOrigFormatted = timeFormat.format( eOrig.tAsDate );
 		final String tAsDateGottenFormatted = timeFormat.format( eGotten.tAsDate );
+		logger.info("orig = " + eOrig.tAsDate + " gotten = " + eGotten.tAsDate );
 		assertEquals( tAsDateOrigFormatted, tAsDateGottenFormatted );
+
+		// NuoDB - check what's actually in table
+		Time t = (Time)session.createNativeQuery("SELECT tAsDate FROM Entity WHERE id = 1").getSingleResult();
+		logger.info("T is a " + t.getClass());
+		assertEquals(t, testTime);
+		logger.info("value in table is " + t);
+		logger.info("t = " + t.getTime() + " eGotten.tAsDate = " + eGotten.tAsDate.getTime());
+		// End NuoDB
+
+
 		s.getTransaction().commit();
 		s.close();
 
 		s = openSession();
 		s.getTransaction().begin();
-
 		String queryString = "from TimePropertyTest$Entity where tAsDate = ?1";
 
 		if( SQLServerDialect.class.isAssignableFrom( getDialect().getClass() )) {
 			queryString = "from TimePropertyTest$Entity where tAsDate = cast ( ?1 as time )";
 		}
 
-		final Query queryWithParameter = s.createQuery( queryString ).setParameter( 1, eGotten.tAsDate );
+		// NuoDB: replaced eGotten.tAsDate by testTime to fix query
+		final Query queryWithParameter = s.createQuery( queryString ).setParameter( 1, testTime );
 		final Entity eQueriedWithParameter = (Entity) queryWithParameter.uniqueResult();
 		assertNotNull( eQueriedWithParameter );
 		s.getTransaction().commit();
@@ -80,7 +99,8 @@ public class TimePropertyTest extends BaseCoreFunctionalTestCase {
 		s = openSession();
 		s.getTransaction().begin();
 
-		final Query query = s.createQuery( queryString ).setTime( 1, eGotten.tAsDate );
+		// NuoDB: replaced eGotten.tAsDate by testTime to fix query
+		final Query query = s.createQuery( queryString ).setTime( 1, testTime );
 		final Entity eQueried = (Entity) query.uniqueResult();
 		assertNotNull( eQueried );
 		s.getTransaction().commit();

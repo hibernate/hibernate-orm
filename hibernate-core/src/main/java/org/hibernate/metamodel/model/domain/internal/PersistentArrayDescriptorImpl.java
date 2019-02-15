f4d8b7229eb8831a73be869665ec882072ab2c51
Andrea Boriero <andrea@hibernate.org>
2019-02-15 23:36:33 +0000

    Implement PeristentArrayDescriptorImpl#indexOf method

--- hibernate-core/src/main/java/org/hibernate/metamodel/model/domain/internal/PersistentArrayDescriptorImpl.java
+++ hibernate-core/src/main/java/org/hibernate/metamodel/model/domain/internal/PersistentArrayDescriptorImpl.java
@@ -139,7 +139,14 @@ public class PersistentArrayDescriptorImpl<O,E> extends AbstractPersistentCollec
 	}
 
 	@Override
-	public Object indexOf(Object collection, Object element) {
-		throw new NotYetImplementedFor6Exception( getClass() );
+	public Object indexOf(Object array, Object element) {
+		int length = Array.getLength( array );
+		for ( int i = 0; i < length; i++ ) {
+			//TODO: proxies!
+			if ( Array.get( array, i ) == element ) {
+				return i;
+			}
+		}
+		return null;
 	}
 }

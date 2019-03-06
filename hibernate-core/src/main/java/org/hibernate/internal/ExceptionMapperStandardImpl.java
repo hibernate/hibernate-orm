d353a1038254ad41ee89ee34c4fd96d3f88f25fa
Sanne Grinovero <sanne@hibernate.org>
2019-03-06 12:50:50 +0000

    HHH-13306 And remove the unused LOG instance as well

--- hibernate-core/src/main/java/org/hibernate/internal/ExceptionMapperStandardImpl.java
+++ hibernate-core/src/main/java/org/hibernate/internal/ExceptionMapperStandardImpl.java
@@ -16,7 +16,6 @@ import org.hibernate.resource.transaction.backend.jta.internal.synchronization.E
  * @author Steve Ebersole
  */
 public class ExceptionMapperStandardImpl implements ExceptionMapper {
-	private static final CoreMessageLogger log = CoreLogging.messageLogger( ExceptionMapperStandardImpl.class );
 
 	public static final ExceptionMapper INSTANCE = new ExceptionMapperStandardImpl();
 

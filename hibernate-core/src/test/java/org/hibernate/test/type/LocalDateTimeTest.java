951066aa20279f3ef67e95fe3ef9b28eef8c8943
Yoann Rodière <yoann@hibernate.org>
2019-03-11 16:32:44 +0100

    HHH-13266 Add an abstract class for all java.time tests for this ticket
    So that we can hopefully factorize the upcoming additions.

--- hibernate-core/src/test/java/org/hibernate/test/type/LocalDateTimeTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/type/LocalDateTimeTest.java
@@ -6,86 +6,52 @@
  */
 package org.hibernate.test.type;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.Arrays;
 import java.util.List;
-import java.util.TimeZone;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
-import org.hibernate.dialect.Dialect;
-import org.hibernate.dialect.PostgreSQL81Dialect;
-
-import org.hibernate.testing.TestForIssue;
-import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
-import org.hibernate.testing.junit4.CustomParameterized;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-
-import static org.junit.Assert.assertEquals;
 
 /**
  * Tests for storage of LocalDateTime properties.
  */
-@RunWith(CustomParameterized.class)
-public class LocalDateTimeTest extends BaseCoreFunctionalTestCase {
+public class LocalDateTimeTest extends AbstractJavaTimeTypeTest<LocalDateTime, LocalDateTimeTest.EntityWithLocalDateTime> {
 
-	private static Dialect DIALECT;
-	private static Dialect determineDialect() {
-		try {
-			return Dialect.getDialect();
-		}
-		catch (Exception e) {
-			return new Dialect() {
-			};
+	private static class ParametersBuilder extends AbstractParametersBuilder<ParametersBuilder> {
+		public ParametersBuilder add(int year, int month, int day,
+				int hour, int minute, int second, int nanosecond, ZoneId defaultTimeZone) {
+			if ( !isNanosecondPrecisionSupported() ) {
+				nanosecond = 0;
+			}
+			return add( defaultTimeZone, year, month, day, hour, minute, second, nanosecond );
 		}
 	}
 
-	/*
-	 * The default timezone affects conversions done using java.util,
-	 * which is why we take it into account even when testing LocalDateTime.
-	 */
-	@Parameterized.Parameters(name = "{0}-{1}-{2}T{3}:{4}:{5}.{6} [JVM TZ: {7}]")
+	@Parameterized.Parameters(name = "{1}-{2}-{3}T{4}:{5}:{6}.{7} {0}")
 	public static List<Object[]> data() {
-		DIALECT = determineDialect();
-		return Arrays.asList(
+		return new ParametersBuilder()
 				// Not affected by HHH-13266 (JDK-8061577)
-				data( 2017, 11, 6, 19, 19, 1, 0, ZoneId.of( "UTC-8" ) ),
-				data( 2017, 11, 6, 19, 19, 1, 0, ZoneId.of( "Europe/Paris" ) ),
-				data( 2017, 11, 6, 19, 19, 1, 500, ZoneId.of( "Europe/Paris" ) ),
-				data( 1970, 1, 1, 0, 0, 0, 0, ZoneId.of( "GMT" ) ),
-				data( 1900, 1, 1, 0, 0, 0, 0, ZoneId.of( "GMT" ) ),
-				data( 1900, 1, 1, 0, 0, 0, 0, ZoneId.of( "Europe/Oslo" ) ),
-				data( 1900, 1, 2, 0, 9, 21, 0, ZoneId.of( "Europe/Paris" ) ),
-				data( 1900, 1, 2, 0, 19, 32, 0, ZoneId.of( "Europe/Amsterdam" ) ),
+				.add( 2017, 11, 6, 19, 19, 1, 0, ZONE_UTC_MINUS_8 )
+				.add( 2017, 11, 6, 19, 19, 1, 0, ZONE_PARIS )
+				.add( 2017, 11, 6, 19, 19, 1, 500, ZONE_PARIS )
+				.add( 1970, 1, 1, 0, 0, 0, 0, ZONE_GMT )
+				.add( 1900, 1, 1, 0, 0, 0, 0, ZONE_GMT )
+				.add( 1900, 1, 1, 0, 0, 0, 0, ZONE_OSLO )
+				.add( 1900, 1, 2, 0, 9, 21, 0, ZONE_PARIS )
+				.add( 1900, 1, 2, 0, 19, 32, 0, ZONE_AMSTERDAM )
 				// Affected by HHH-13266 (JDK-8061577)
-				data( 1892, 1, 1, 0, 0, 0, 0, ZoneId.of( "Europe/Oslo" ) ),
-				data( 1900, 1, 1, 0, 9, 20, 0, ZoneId.of( "Europe/Paris" ) ),
-				data( 1900, 1, 1, 0, 19, 31, 0, ZoneId.of( "Europe/Amsterdam" ) ),
-				data( 1600, 1, 1, 0, 0, 0, 0, ZoneId.of( "Europe/Amsterdam" ) )
-		);
-	}
-
-	private static Object[] data(int year, int month, int day,
-			int hour, int minute, int second, int nanosecond, ZoneId defaultTimeZone) {
-		if ( DIALECT instanceof PostgreSQL81Dialect ) {
-			// PostgreSQL apparently doesn't support nanosecond precision correctly
-			nanosecond = 0;
-		}
-		return new Object[] { year, month, day, hour, minute, second, nanosecond, defaultTimeZone };
+				.add( 1892, 1, 1, 0, 0, 0, 0, ZONE_OSLO )
+				.add( 1900, 1, 1, 0, 9, 20, 0, ZONE_PARIS )
+				.add( 1900, 1, 1, 0, 19, 31, 0, ZONE_AMSTERDAM )
+				.add( 1600, 1, 1, 0, 0, 0, 0, ZONE_AMSTERDAM )
+				.build();
 	}
 
 	private final int year;
@@ -95,10 +61,10 @@ public class LocalDateTimeTest extends BaseCoreFunctionalTestCase {
 	private final int minute;
 	private final int second;
 	private final int nanosecond;
-	private final ZoneId defaultTimeZone;
 
-	public LocalDateTimeTest(int year, int month, int day,
-			int hour, int minute, int second, int nanosecond, ZoneId defaultTimeZone) {
+	public LocalDateTimeTest(EnvironmentParameters env, int year, int month, int day,
+			int hour, int minute, int second, int nanosecond) {
+		super( env );
 		this.year = year;
 		this.month = month;
 		this.day = day;
@@ -106,14 +72,30 @@ public class LocalDateTimeTest extends BaseCoreFunctionalTestCase {
 		this.minute = minute;
 		this.second = second;
 		this.nanosecond = nanosecond;
-		this.defaultTimeZone = defaultTimeZone;
 	}
 
-	private LocalDateTime getExpectedLocalDateTime() {
+	@Override
+	protected Class<EntityWithLocalDateTime> getEntityType() {
+		return EntityWithLocalDateTime.class;
+	}
+
+	@Override
+	protected EntityWithLocalDateTime createEntity(int id) {
+		return new EntityWithLocalDateTime( id, getExpectedPropertyValue() );
+	}
+
+	@Override
+	protected LocalDateTime getExpectedPropertyValue() {
 		return LocalDateTime.of( year, month, day, hour, minute, second, nanosecond );
 	}
 
-	private Timestamp getExpectedTimestamp() {
+	@Override
+	protected LocalDateTime getActualPropertyValue(EntityWithLocalDateTime entity) {
+		return entity.value;
+	}
+
+	@Override
+	protected Object getExpectedJdbcValue() {
 		return new Timestamp(
 				year - 1900, month - 1, day,
 				hour, minute, second, nanosecond
@@ -121,104 +103,18 @@ public class LocalDateTimeTest extends BaseCoreFunctionalTestCase {
 	}
 
 	@Override
-	protected Class<?>[] getAnnotatedClasses() {
-		return new Class[] { EntityWithLocalDateTime.class };
+	protected Object getActualJdbcValue(ResultSet resultSet, int columnIndex) throws SQLException {
+		return resultSet.getTimestamp( columnIndex );
 	}
 
-	@Before
-	public void cleanup() {
-		inTransaction( session -> {
-			session.createNativeQuery( "DELETE FROM theentity" ).executeUpdate();
-		} );
-	}
-
-	@Test
-	@TestForIssue(jiraKey = "HHH-13266")
-	public void writeThenRead() {
-		withDefaultTimeZone( defaultTimeZone, () -> {
-			inTransaction( session -> {
-				session.persist( new EntityWithLocalDateTime( 1, getExpectedLocalDateTime() ) );
-			} );
-			inTransaction( session -> {
-				LocalDateTime read = session.find( EntityWithLocalDateTime.class, 1 ).value;
-				assertEquals(
-						"Writing then reading a value should return the original value",
-						getExpectedLocalDateTime(), read
-				);
-			} );
-		} );
-	}
-
-	@Test
-	@TestForIssue(jiraKey = "HHH-13266")
-	public void writeThenNativeRead() {
-		withDefaultTimeZone( defaultTimeZone, () -> {
-			inTransaction( session -> {
-				session.persist( new EntityWithLocalDateTime( 1, getExpectedLocalDateTime() ) );
-			} );
-			inTransaction( session -> {
-				session.doWork( connection -> {
-					final PreparedStatement statement = connection.prepareStatement(
-							"SELECT thevalue FROM theentity WHERE theid = ?"
-					);
-					statement.setInt( 1, 1 );
-					statement.execute();
-					final ResultSet resultSet = statement.getResultSet();
-					resultSet.next();
-					Timestamp nativeRead = resultSet.getTimestamp( 1 );
-					assertEquals(
-							"Raw values written in database should match the original value (same day, hour, ...)",
-							getExpectedTimestamp(),
-							nativeRead
-					);
-				} );
-			} );
-		} );
-	}
-
-	private static void withDefaultTimeZone(ZoneId zoneId, Runnable runnable) {
-		TimeZone timeZoneBefore = TimeZone.getDefault();
-		TimeZone.setDefault( TimeZone.getTimeZone( zoneId ) );
-		/*
-		 * Run the code in a new thread, because some libraries (looking at you, h2 JDBC driver)
-		 * cache data dependent on the default timezone in thread local variables,
-		 * and we want this data to be reinitialized with the new default time zone.
-		 */
-		try {
-			ExecutorService executor = Executors.newSingleThreadExecutor();
-			Future<?> future = executor.submit( runnable );
-			executor.shutdown();
-			future.get();
-		}
-		catch (InterruptedException e) {
-			throw new IllegalStateException( "Interrupted while testing", e );
-		}
-		catch (ExecutionException e) {
-			Throwable cause = e.getCause();
-			if ( cause instanceof RuntimeException ) {
-				throw (RuntimeException) cause;
-			}
-			else if ( cause instanceof Error ) {
-				throw (Error) cause;
-			}
-			else {
-				throw new IllegalStateException( "Unexpected exception while testing", cause );
-			}
-		}
-		finally {
-			TimeZone.setDefault( timeZoneBefore );
-		}
-	}
-
-	@Entity
-	@Table(name = "theentity")
-	private static final class EntityWithLocalDateTime {
+	@Entity(name = ENTITY_NAME)
+	static final class EntityWithLocalDateTime {
 		@Id
-		@Column(name = "theid")
+		@Column(name = ID_COLUMN_NAME)
 		private Integer id;
 
 		@Basic
-		@Column(name = "thevalue")
+		@Column(name = PROPERTY_COLUMN_NAME)
 		private LocalDateTime value;
 
 		protected EntityWithLocalDateTime() {
--- hibernate-core/src/test/java/org/hibernate/test/type/ZonedDateTimeTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/type/ZonedDateTimeTest.java
@@ -6,127 +6,96 @@
  */
 package org.hibernate.test.type;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
-import java.util.Arrays;
+import java.time.ZonedDateTime;
 import java.util.List;
-import java.util.TimeZone;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.function.Consumer;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 import org.hibernate.Query;
-import org.hibernate.dialect.Dialect;
-import org.hibernate.dialect.PostgreSQL81Dialect;
 import org.hibernate.type.ZonedDateTimeType;
 
 import org.hibernate.testing.TestForIssue;
-import org.hibernate.testing.junit4.BaseNonConfigCoreFunctionalTestCase;
-import org.hibernate.testing.junit4.CustomParameterized;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 /**
  * @author Andrea Boriero
  */
-@RunWith(CustomParameterized.class)
 @TestForIssue(jiraKey = "HHH-10372")
-public class ZonedDateTimeTest extends BaseNonConfigCoreFunctionalTestCase {
+public class ZonedDateTimeTest extends AbstractJavaTimeTypeTest<ZonedDateTime, ZonedDateTimeTest.EntityWithZonedDateTime> {
 
-	private static Dialect DIALECT;
-	private static Dialect determineDialect() {
-		try {
-			return Dialect.getDialect();
-		}
-		catch (Exception e) {
-			return new Dialect() {
-			};
+	private static class ParametersBuilder extends AbstractParametersBuilder<ParametersBuilder> {
+		public ParametersBuilder add(int year, int month, int day,
+				int hour, int minute, int second, int nanosecond, String zone, ZoneId defaultTimeZone) {
+			if ( !isNanosecondPrecisionSupported() ) {
+				nanosecond = 0;
+			}
+			return add( defaultTimeZone, year, month, day, hour, minute, second, nanosecond, zone );
 		}
 	}
 
-	/*
-	 * The default timezone affects conversions done using java.util,
-	 * which is why we take it into account even when testing ZonedDateTime.
-	 */
-	@Parameterized.Parameters(name = "{0}-{1}-{2}T{3}:{4}:{5}.{6}[{7}] [JVM TZ: {8}]")
+	@Parameterized.Parameters(name = "{1}-{2}-{3}T{4}:{5}:{6}.{7}[{8}] {0}")
 	public static List<Object[]> data() {
-		DIALECT = determineDialect();
-		return Arrays.asList(
+		return new ParametersBuilder()
 				// Not affected by HHH-13266
-				data( 2017, 11, 6, 19, 19, 1, 0, "GMT+10:00", ZoneId.of( "UTC-8" ) ),
-				data( 2017, 11, 6, 19, 19, 1, 0, "GMT+07:00", ZoneId.of( "UTC-8" ) ),
-				data( 2017, 11, 6, 19, 19, 1, 0, "GMT+01:30", ZoneId.of( "UTC-8" ) ),
-				data( 2017, 11, 6, 19, 19, 1, 0, "GMT+01:00", ZoneId.of( "UTC-8" ) ),
-				data( 2017, 11, 6, 19, 19, 1, 0, "Europe/Paris", ZoneId.of( "UTC-8" ) ),
-				data( 2017, 11, 6, 19, 19, 1, 0, "Europe/London", ZoneId.of( "UTC-8" ) ),
-				data( 2017, 11, 6, 19, 19, 1, 0, "GMT+00:30", ZoneId.of( "UTC-8" ) ),
-				data( 2017, 11, 6, 19, 19, 1, 0, "GMT-02:00", ZoneId.of( "UTC-8" ) ),
-				data( 2017, 11, 6, 19, 19, 1, 0, "GMT-06:00", ZoneId.of( "UTC-8" ) ),
-				data( 2017, 11, 6, 19, 19, 1, 0, "GMT-08:00", ZoneId.of( "UTC-8" ) ),
-				data( 2017, 11, 6, 19, 19, 1, 0, "GMT+10:00", ZoneId.of( "Europe/Paris" ) ),
-				data( 2017, 11, 6, 19, 19, 1, 0, "GMT+07:00", ZoneId.of( "Europe/Paris" ) ),
-				data( 2017, 11, 6, 19, 19, 1, 0, "GMT+01:30", ZoneId.of( "Europe/Paris" ) ),
-				data( 2017, 11, 6, 19, 19, 1, 0, "GMT+01:00", ZoneId.of( "Europe/Paris" ) ),
-				data( 2017, 11, 6, 19, 19, 1, 500, "GMT+01:00", ZoneId.of( "Europe/Paris" ) ),
-				data( 2017, 11, 6, 19, 19, 1, 0, "Europe/Paris", ZoneId.of( "Europe/Paris" ) ),
-				data( 2017, 11, 6, 19, 19, 1, 0, "Europe/London", ZoneId.of( "Europe/Paris" ) ),
-				data( 2017, 11, 6, 19, 19, 1, 0, "GMT+00:30", ZoneId.of( "Europe/Paris" ) ),
-				data( 2017, 11, 6, 19, 19, 1, 0, "GMT-02:00", ZoneId.of( "Europe/Paris" ) ),
-				data( 2017, 11, 6, 19, 19, 1, 0, "GMT-06:00", ZoneId.of( "Europe/Paris" ) ),
-				data( 2017, 11, 6, 19, 19, 1, 0, "GMT-08:00", ZoneId.of( "Europe/Paris" ) ),
-				data( 1970, 1, 1, 0, 0, 0, 0, "GMT+01:00", ZoneId.of( "GMT" ) ),
-				data( 1970, 1, 1, 0, 0, 0, 0, "GMT+00:00", ZoneId.of( "GMT" ) ),
-				data( 1970, 1, 1, 0, 0, 0, 0, "GMT-01:00", ZoneId.of( "GMT" ) ),
-				data( 1900, 1, 1, 0, 0, 0, 0, "GMT+01:00", ZoneId.of( "GMT" ) ),
-				data( 1900, 1, 1, 0, 0, 0, 0, "GMT+00:00", ZoneId.of( "GMT" ) ),
-				data( 1900, 1, 1, 0, 0, 0, 0, "GMT-01:00", ZoneId.of( "GMT" ) ),
-				data( 1900, 1, 1, 0, 0, 0, 0, "GMT+00:00", ZoneId.of( "Europe/Oslo" ) ),
-				data( 1900, 1, 1, 0, 9, 21, 0, "GMT+00:09:21", ZoneId.of( "Europe/Paris" ) ),
-				data( 1900, 1, 1, 0, 9, 21, 0, "Europe/Paris", ZoneId.of( "Europe/Paris" ) ),
-				data( 1900, 1, 1, 0, 19, 31, 0, "Europe/Paris", ZoneId.of( "Europe/Paris" ) ),
-				data( 1900, 1, 1, 0, 19, 32, 0, "GMT+00:19:32", ZoneId.of( "Europe/Paris" ) ),
-				data( 1900, 1, 1, 0, 19, 32, 0, "Europe/Amsterdam", ZoneId.of( "Europe/Paris" ) ),
-				data( 1900, 1, 1, 0, 19, 32, 0, "GMT+00:19:32", ZoneId.of( "Europe/Amsterdam" ) ),
-				data( 1900, 1, 1, 0, 19, 32, 0, "Europe/Amsterdam", ZoneId.of( "Europe/Amsterdam" ) ),
+				.add( 2017, 11, 6, 19, 19, 1, 0, "GMT+10:00", ZONE_UTC_MINUS_8 )
+				.add( 2017, 11, 6, 19, 19, 1, 0, "GMT+07:00", ZONE_UTC_MINUS_8 )
+				.add( 2017, 11, 6, 19, 19, 1, 0, "GMT+01:30", ZONE_UTC_MINUS_8 )
+				.add( 2017, 11, 6, 19, 19, 1, 0, "GMT+01:00", ZONE_UTC_MINUS_8 )
+				.add( 2017, 11, 6, 19, 19, 1, 0, "Europe/Paris", ZONE_UTC_MINUS_8 )
+				.add( 2017, 11, 6, 19, 19, 1, 0, "Europe/London", ZONE_UTC_MINUS_8 )
+				.add( 2017, 11, 6, 19, 19, 1, 0, "GMT+00:30", ZONE_UTC_MINUS_8 )
+				.add( 2017, 11, 6, 19, 19, 1, 0, "GMT-02:00", ZONE_UTC_MINUS_8 )
+				.add( 2017, 11, 6, 19, 19, 1, 0, "GMT-06:00", ZONE_UTC_MINUS_8 )
+				.add( 2017, 11, 6, 19, 19, 1, 0, "GMT-08:00", ZONE_UTC_MINUS_8 )
+				.add( 2017, 11, 6, 19, 19, 1, 0, "GMT+10:00", ZONE_PARIS )
+				.add( 2017, 11, 6, 19, 19, 1, 0, "GMT+07:00", ZONE_PARIS )
+				.add( 2017, 11, 6, 19, 19, 1, 0, "GMT+01:30", ZONE_PARIS )
+				.add( 2017, 11, 6, 19, 19, 1, 0, "GMT+01:00", ZONE_PARIS )
+				.add( 2017, 11, 6, 19, 19, 1, 500, "GMT+01:00", ZONE_PARIS )
+				.add( 2017, 11, 6, 19, 19, 1, 0, "Europe/Paris", ZONE_PARIS )
+				.add( 2017, 11, 6, 19, 19, 1, 0, "Europe/London", ZONE_PARIS )
+				.add( 2017, 11, 6, 19, 19, 1, 0, "GMT+00:30", ZONE_PARIS )
+				.add( 2017, 11, 6, 19, 19, 1, 0, "GMT-02:00", ZONE_PARIS )
+				.add( 2017, 11, 6, 19, 19, 1, 0, "GMT-06:00", ZONE_PARIS )
+				.add( 2017, 11, 6, 19, 19, 1, 0, "GMT-08:00", ZONE_PARIS )
+				.add( 1970, 1, 1, 0, 0, 0, 0, "GMT+01:00", ZONE_GMT )
+				.add( 1970, 1, 1, 0, 0, 0, 0, "GMT+00:00", ZONE_GMT )
+				.add( 1970, 1, 1, 0, 0, 0, 0, "GMT-01:00", ZONE_GMT )
+				.add( 1900, 1, 1, 0, 0, 0, 0, "GMT+01:00", ZONE_GMT )
+				.add( 1900, 1, 1, 0, 0, 0, 0, "GMT+00:00", ZONE_GMT )
+				.add( 1900, 1, 1, 0, 0, 0, 0, "GMT-01:00", ZONE_GMT )
+				.add( 1900, 1, 1, 0, 0, 0, 0, "GMT+00:00", ZONE_OSLO )
+				.add( 1900, 1, 1, 0, 9, 21, 0, "GMT+00:09:21", ZONE_PARIS )
+				.add( 1900, 1, 1, 0, 9, 21, 0, "Europe/Paris", ZONE_PARIS )
+				.add( 1900, 1, 1, 0, 19, 31, 0, "Europe/Paris", ZONE_PARIS )
+				.add( 1900, 1, 1, 0, 19, 32, 0, "GMT+00:19:32", ZONE_PARIS )
+				.add( 1900, 1, 1, 0, 19, 32, 0, "Europe/Amsterdam", ZONE_PARIS )
+				.add( 1900, 1, 1, 0, 19, 32, 0, "GMT+00:19:32", ZONE_AMSTERDAM )
+				.add( 1900, 1, 1, 0, 19, 32, 0, "Europe/Amsterdam", ZONE_AMSTERDAM )
 				// Affected by HHH-13266
-				data( 1892, 1, 1, 0, 0, 0, 0, "GMT+00:00", ZoneId.of( "Europe/Oslo" ) ),
-				data( 1892, 1, 1, 0, 0, 0, 0, "Europe/Oslo", ZoneId.of( "Europe/Oslo" ) ),
-				data( 1900, 1, 1, 0, 9, 20, 0, "GMT+00:09:21", ZoneId.of( "Europe/Paris" ) ),
-				data( 1900, 1, 1, 0, 9, 20, 0, "Europe/Paris", ZoneId.of( "Europe/Paris" ) ),
-				data( 1900, 1, 1, 0, 19, 31, 0, "GMT+00:19:32", ZoneId.of( "Europe/Paris" ) ),
-				data( 1900, 1, 1, 0, 19, 31, 0, "GMT+00:19:32", ZoneId.of( "Europe/Amsterdam" ) ),
-				data( 1900, 1, 1, 0, 19, 31, 0, "Europe/Amsterdam", ZoneId.of( "Europe/Amsterdam" ) ),
-				data( 1600, 1, 1, 0, 0, 0, 0, "GMT+00:19:32", ZoneId.of( "Europe/Amsterdam" ) ),
-				data( 1600, 1, 1, 0, 0, 0, 0, "Europe/Amsterdam", ZoneId.of( "Europe/Amsterdam" ) )
-		);
-	}
-
-	private static Object[] data(int year, int month, int day,
-			int hour, int minute, int second, int nanosecond, String zone, ZoneId defaultTimeZone) {
-		if ( DIALECT instanceof PostgreSQL81Dialect ) {
-			// PostgreSQL apparently doesn't support nanosecond precision correctly
-			nanosecond = 0;
-		}
-		return new Object[] { year, month, day, hour, minute, second, nanosecond, zone, defaultTimeZone };
+				.add( 1892, 1, 1, 0, 0, 0, 0, "GMT+00:00", ZONE_OSLO )
+				.add( 1892, 1, 1, 0, 0, 0, 0, "Europe/Oslo", ZONE_OSLO )
+				.add( 1900, 1, 1, 0, 9, 20, 0, "GMT+00:09:21", ZONE_PARIS )
+				.add( 1900, 1, 1, 0, 9, 20, 0, "Europe/Paris", ZONE_PARIS )
+				.add( 1900, 1, 1, 0, 19, 31, 0, "GMT+00:19:32", ZONE_PARIS )
+				.add( 1900, 1, 1, 0, 19, 31, 0, "GMT+00:19:32", ZONE_AMSTERDAM )
+				.add( 1900, 1, 1, 0, 19, 31, 0, "Europe/Amsterdam", ZONE_AMSTERDAM )
+				.add( 1600, 1, 1, 0, 0, 0, 0, "GMT+00:19:32", ZONE_AMSTERDAM )
+				.add( 1600, 1, 1, 0, 0, 0, 0, "Europe/Amsterdam", ZONE_AMSTERDAM )
+				.build();
 	}
 
 	private final int year;
@@ -137,10 +106,10 @@ public class ZonedDateTimeTest extends BaseNonConfigCoreFunctionalTestCase {
 	private final int second;
 	private final int nanosecond;
 	private final String zone;
-	private final ZoneId defaultTimeZone;
 
-	public ZonedDateTimeTest(int year, int month, int day,
-			int hour, int minute, int second, int nanosecond, String zone, ZoneId defaultTimeZone) {
+	public ZonedDateTimeTest(EnvironmentParameters env, int year, int month, int day,
+			int hour, int minute, int second, int nanosecond, String zone) {
+		super( env );
 		this.year = year;
 		this.month = month;
 		this.day = day;
@@ -149,18 +118,37 @@ public class ZonedDateTimeTest extends BaseNonConfigCoreFunctionalTestCase {
 		this.second = second;
 		this.nanosecond = nanosecond;
 		this.zone = zone;
-		this.defaultTimeZone = defaultTimeZone;
+	}
+
+	@Override
+	protected Class<EntityWithZonedDateTime> getEntityType() {
+		return EntityWithZonedDateTime.class;
+	}
+
+	@Override
+	protected EntityWithZonedDateTime createEntity(int id) {
+		return new EntityWithZonedDateTime(
+				id,
+				getOriginalZonedDateTime()
+		);
 	}
 
 	private ZonedDateTime getOriginalZonedDateTime() {
 		return ZonedDateTime.of( year, month, day, hour, minute, second, nanosecond, ZoneId.of( zone ) );
 	}
 
-	private ZonedDateTime getExpectedZonedDateTime() {
+	@Override
+	protected ZonedDateTime getExpectedPropertyValue() {
 		return getOriginalZonedDateTime().withZoneSameInstant( ZoneId.systemDefault() );
 	}
 
-	private Timestamp getExpectedTimestamp() {
+	@Override
+	protected ZonedDateTime getActualPropertyValue(EntityWithZonedDateTime entity) {
+		return entity.value;
+	}
+
+	@Override
+	protected Object getExpectedJdbcValue() {
 		LocalDateTime dateTimeInDefaultTimeZone = getOriginalZonedDateTime().withZoneSameInstant( ZoneId.systemDefault() )
 				.toLocalDateTime();
 		return new Timestamp(
@@ -173,129 +161,36 @@ public class ZonedDateTimeTest extends BaseNonConfigCoreFunctionalTestCase {
 	}
 
 	@Override
-	protected Class<?>[] getAnnotatedClasses() {
-		return new Class[] { EntityWithZonedDateTime.class };
-	}
-
-	@Before
-	public void cleanup() {
-		inTransaction( session -> {
-			session.createNativeQuery( "DELETE FROM theentity" ).executeUpdate();
-		} );
-	}
-
-	@Test
-	@TestForIssue(jiraKey = "HHH-13266")
-	public void writeThenRead() {
-		withDefaultTimeZone( defaultTimeZone, () -> {
-			inTransaction( session -> {
-				session.persist( new EntityWithZonedDateTime( 1, getOriginalZonedDateTime() ) );
-			} );
-			inTransaction( session -> {
-				ZonedDateTime read = session.find( ZonedDateTimeTest.EntityWithZonedDateTime.class, 1 ).value;
-				assertEquals(
-						"Writing then reading a value should return the original value",
-						getExpectedZonedDateTime(), read
-				);
-				assertTrue(
-						getExpectedZonedDateTime().isEqual( read )
-				);
-				assertEquals(
-						0,
-						ZonedDateTimeType.INSTANCE.getComparator().compare( getExpectedZonedDateTime(), read )
-				);
-			} );
-		} );
-	}
-
-	@Test
-	@TestForIssue(jiraKey = "HHH-13266")
-	public void writeThenNativeRead() {
-		withDefaultTimeZone( defaultTimeZone, () -> {
-			inTransaction( session -> {
-				session.persist( new EntityWithZonedDateTime( 1, getOriginalZonedDateTime() ) );
-			} );
-			inTransaction( session -> {
-				session.doWork( connection -> {
-					final PreparedStatement statement = connection.prepareStatement(
-							"SELECT thevalue FROM theentity WHERE theid = ?"
-					);
-					statement.setInt( 1, 1 );
-					statement.execute();
-					final ResultSet resultSet = statement.getResultSet();
-					resultSet.next();
-					Timestamp nativeRead = resultSet.getTimestamp( 1 );
-					assertEquals(
-							"Raw values written in database should match the original value (same instant)",
-							getExpectedTimestamp(),
-							nativeRead
-					);
-				} );
-			} );
-		} );
+	protected Object getActualJdbcValue(ResultSet resultSet, int columnIndex) throws SQLException {
+		return resultSet.getTimestamp( columnIndex );
 	}
 
 	@Test
 	public void testRetrievingEntityByZonedDateTime() {
-		withDefaultTimeZone( defaultTimeZone, () -> {
+		withDefaultTimeZone( () -> {
 			inTransaction( session -> {
 				session.persist( new EntityWithZonedDateTime( 1, getOriginalZonedDateTime() ) );
 			} );
 			Consumer<ZonedDateTime> checkOneMatch = expected -> inSession( s -> {
-				Query query = s.createQuery( "from EntityWithZonedDateTime o where o.value = :date" );
+				Query query = s.createQuery( "from " + ENTITY_NAME + " o where o.value = :date" );
 				query.setParameter( "date", expected, ZonedDateTimeType.INSTANCE );
 				List<EntityWithZonedDateTime> list = query.list();
 				assertThat( list.size(), is( 1 ) );
 			} );
 			checkOneMatch.accept( getOriginalZonedDateTime() );
-			checkOneMatch.accept( getExpectedZonedDateTime() );
-			checkOneMatch.accept( getExpectedZonedDateTime().withZoneSameInstant( ZoneOffset.UTC ) );
+			checkOneMatch.accept( getExpectedPropertyValue() );
+			checkOneMatch.accept( getExpectedPropertyValue().withZoneSameInstant( ZoneOffset.UTC ) );
 		} );
 	}
 
-	private static void withDefaultTimeZone(ZoneId zoneId, Runnable runnable) {
-		TimeZone timeZoneBefore = TimeZone.getDefault();
-		TimeZone.setDefault( TimeZone.getTimeZone( zoneId ) );
-		/*
-		 * Run the code in a new thread, because some libraries (looking at you, h2 JDBC driver)
-		 * cache data dependent on the default timezone in thread local variables,
-		 * and we want this data to be reinitialized with the new default time zone.
-		 */
-		try {
-			ExecutorService executor = Executors.newSingleThreadExecutor();
-			Future<?> future = executor.submit( runnable );
-			executor.shutdown();
-			future.get();
-		}
-		catch (InterruptedException e) {
-			throw new IllegalStateException( "Interrupted while testing", e );
-		}
-		catch (ExecutionException e) {
-			Throwable cause = e.getCause();
-			if ( cause instanceof RuntimeException ) {
-				throw (RuntimeException) cause;
-			}
-			else if ( cause instanceof Error ) {
-				throw (Error) cause;
-			}
-			else {
-				throw new IllegalStateException( "Unexpected exception while testing", cause );
-			}
-		}
-		finally {
-			TimeZone.setDefault( timeZoneBefore );
-		}
-	}
-
-	@Entity(name = "EntityWithZonedDateTime")
-	@Table(name = "theentity")
-	private static final class EntityWithZonedDateTime {
+	@Entity(name = ENTITY_NAME)
+	static final class EntityWithZonedDateTime {
 		@Id
-		@Column(name = "theid")
+		@Column(name = ID_COLUMN_NAME)
 		private Integer id;
 
 		@Basic
-		@Column(name = "thevalue")
+		@Column(name = PROPERTY_COLUMN_NAME)
 		private ZonedDateTime value;
 
 		protected EntityWithZonedDateTime() {
@@ -305,10 +200,5 @@ public class ZonedDateTimeTest extends BaseNonConfigCoreFunctionalTestCase {
 			this.id = id;
 			this.value = value;
 		}
-	}
-
-	@Override
-	protected boolean isCleanupTestDataRequired() {
-		return true;
 	}
 }

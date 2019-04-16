4d79ffb62f31365127628f3a9e0ecefee254ab0c
Steve Ebersole <steve@hibernate.org>
2019-04-16 13:45:38 -0500

    HHH-13152 - managed/cached entities and fetching (test case)

---
+++ hibernate-core/src/test/java/org/hibernate/test/fetch/runtime/managed/package-info.java
@@ -0,0 +1,15 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
+
+/**
+ * Tests related to runtime fetching -
+ *
+ * 		* HQL / Criteria join fetches
+ * 		* EntityGraph
+ * 		* fetch profile (?)
+ */
+package org.hibernate.test.fetch.runtime.managed;

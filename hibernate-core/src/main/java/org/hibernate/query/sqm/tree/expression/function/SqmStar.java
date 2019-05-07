0a490a72e80e3ab88bc07fe3939e0aa8a1eeb60e
gavinking <gavin@ceylon-lang.org>
2019-05-07 02:58:46 +0200

    fix checkstyle violations

--- hibernate-core/src/main/java/org/hibernate/query/sqm/tree/expression/function/SqmStar.java
+++ hibernate-core/src/main/java/org/hibernate/query/sqm/tree/expression/function/SqmStar.java
@@ -1,3 +1,9 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
 package org.hibernate.query.sqm.tree.expression.function;
 
 import org.hibernate.query.sqm.NodeBuilder;
@@ -13,12 +19,12 @@ import org.hibernate.sql.ast.produce.metamodel.spi.ExpressableType;
  */
 public class SqmStar extends AbstractSqmExpression<Object> {
 
-    public SqmStar(NodeBuilder builder) {
-        super( null, builder );
-    }
+	public SqmStar(NodeBuilder builder) {
+		super( null, builder );
+	}
 
-    @Override
-    public <X> X accept(SemanticQueryWalker<X> walker) {
-        return walker.visitStar(this);
-    }
+	@Override
+	public <X> X accept(SemanticQueryWalker<X> walker) {
+		return walker.visitStar(this);
+	}
 }

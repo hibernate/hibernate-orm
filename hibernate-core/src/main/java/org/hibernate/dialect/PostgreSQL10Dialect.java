18ee208cb21ab861cb58be05c08ecab0678c01d3
Kedar Joshi <kedar.joshi@locationguru.com>
2019-05-16 21:59:33 +0530

    HHH-13106 : Fixes schema validation failure with PostgreSQL 10

--- hibernate-core/src/main/java/org/hibernate/dialect/PostgreSQL10Dialect.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/PostgreSQL10Dialect.java
@@ -14,6 +14,30 @@ import org.hibernate.dialect.identity.PostgreSQL10IdentityColumnSupport;
  */
 public class PostgreSQL10Dialect extends PostgreSQL95Dialect {
 
+	private static final String SEQUENCES_QUERY_STRING = "SELECT current_database() sequence_catalog, " +
+			"    n.nspname AS sequence_schema, " +
+			"    c.relname AS sequence_name, " +
+			"    s.seqtypid::REGTYPE AS data_type, " +
+			"    (information_schema._pg_numeric_precision(s.seqtypid, '-1'::INTEGER)) " +
+			" ::information_schema.CARDINAL_NUMBER AS numeric_precision, " +
+			"    (2)::information_schema.CARDINAL_NUMBER AS numeric_precision_radix, " +
+			"    (0)::information_schema.CARDINAL_NUMBER AS numeric_scale, " +
+			"    (s.seqstart)::information_schema.CHARACTER_DATA AS start_value, " +
+			"    (s.seqmin)::information_schema.CHARACTER_DATA AS minimum_value, " +
+			"    (s.seqmax)::information_schema.CHARACTER_DATA AS maximum_value, " +
+			"    (s.seqincrement)::information_schema.CHARACTER_DATA AS increment, " +
+			"    (CASE WHEN s.seqcycle THEN 'YES'::TEXT ELSE 'NO'::TEXT END) " +
+			" ::information_schema.YES_OR_NO AS cycle_option " +
+			"FROM pg_catalog.pg_sequence s " +
+			"  JOIN pg_catalog.pg_class c ON s.seqrelid = c.oid " +
+			"  LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace " +
+			"WHERE (NOT pg_is_other_temp_schema(n.oid)) AND c.relkind = 'S';";
+
+	@Override
+	public String getQuerySequencesString() {
+		return SEQUENCES_QUERY_STRING;
+	}
+
 	@Override
 	public IdentityColumnSupport getIdentityColumnSupport() {
 		return new PostgreSQL10IdentityColumnSupport();

97ea90702df94465eb5a16ae6b1895499a822e92
gavinking <gavin@ceylon-lang.org>
2019-05-30 15:17:46 +0200

    support more extract() field units in Oracle
    - use to_number(to_char(arg, 'D')) instead of extract()

---
+++ hibernate-core/src/main/java/org/hibernate/dialect/function/OracleExtractEmulation.java
@@ -0,0 +1,95 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
+package org.hibernate.dialect.function;
+
+import org.hibernate.metamodel.model.domain.spi.AllowableFunctionReturnType;
+import org.hibernate.query.TemporalUnit;
+import org.hibernate.query.spi.QueryEngine;
+import org.hibernate.query.sqm.produce.function.StandardArgumentsValidators;
+import org.hibernate.query.sqm.produce.function.StandardFunctionReturnTypeResolvers;
+import org.hibernate.query.sqm.produce.function.internal.SelfRenderingSqmFunction;
+import org.hibernate.query.sqm.produce.function.spi.AbstractSqmFunctionTemplate;
+import org.hibernate.query.sqm.tree.SqmTypedNode;
+import org.hibernate.query.sqm.tree.expression.function.SqmExtractUnit;
+import org.hibernate.type.spi.StandardSpiBasicTypes;
+import org.hibernate.type.spi.TypeConfiguration;
+
+import java.util.List;
+
+import static org.hibernate.query.sqm.produce.function.StandardFunctionReturnTypeResolvers.useArgType;
+
+/**
+ * Oracle supports a limited list of temporal fields in the
+ * extract() function, but we can emulate some of them by
+ * using to_char() with a format string instead of extract().
+ *
+ * Thus, the additional supported fields are
+ * {@link TemporalUnit#DAY_OF_YEAR},
+ * {@link TemporalUnit#DAY_OF_MONTH},
+ * {@link TemporalUnit#DAY_OF_YEAR},
+ * {@link TemporalUnit#WEEK},
+ * {@link TemporalUnit#MILLISECOND}, and
+ * {@link TemporalUnit#MICROSECOND}.
+ *
+ * @author Gavin King
+ */
+public class OracleExtractEmulation
+		extends AbstractSqmFunctionTemplate {
+
+	public OracleExtractEmulation() {
+		super(
+				StandardArgumentsValidators.exactly( 2 ),
+				StandardFunctionReturnTypeResolvers.invariant( StandardSpiBasicTypes.STRING )
+		);
+	}
+
+	@Override
+	protected <T> SelfRenderingSqmFunction generateSqmFunctionExpression(
+			List<SqmTypedNode<?>> arguments,
+			AllowableFunctionReturnType<T> impliedResultType,
+			QueryEngine queryEngine,
+			TypeConfiguration typeConfiguration) {
+		TemporalUnit unit = ((SqmExtractUnit<?>) arguments.get(0)).getUnit();
+		String pattern;
+		switch (unit) {
+			case DAY_OF_WEEK:
+				pattern = "to_number(to_char(?2,'D'))";
+				break;
+			case DAY_OF_MONTH:
+				pattern = "to_number(to_char(?2,'DD'))";
+				break;
+			case DAY_OF_YEAR:
+				pattern = "to_number(to_char(?2,'DDD'))";
+				break;
+			case WEEK:
+				pattern = "to_number(to_char(?2,'IW'))";
+				break;
+			case MILLISECOND:
+				pattern = "to_number(to_char(?2,'FF3'))"; //TODO: include whole seconds? SSFF3
+				break;
+			case MICROSECOND:
+				pattern = "to_number(to_char(?2,'FF6'))"; //TODO: include whole seconds? SSFF6
+				break;
+			default:
+				pattern = "extract(?1 from ?2)";
+				break;
+		}
+		return queryEngine.getSqmFunctionRegistry()
+				.patternTemplateBuilder("extract", pattern)
+				.setReturnTypeResolver( useArgType(1) )
+				.setExactArgumentCount( 2 )
+				.template()
+				.makeSqmFunctionExpression(
+						arguments,
+						impliedResultType,
+						queryEngine,
+						typeConfiguration
+				);
+	}
+
+
+}

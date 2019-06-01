hibernate/hibernate-orm

commit 762f6cfa9062d8142fb9c86025d0cb4cd8b3963c

remove millisecond/microsecond as legal targets for extract()
- but add nanosecond instead
- and standardize the semantics of 'extract(second from arg)' to include
  fractional seconds.

hibernate-core/src/main/java/org/hibernate/query/TemporalUnit.java
@@ -13,8 +13,7 @@ import org.hibernate.query.sqm.SemanticException;
  */
 public enum TemporalUnit {
 	YEAR(true), QUARTER(true), MONTH(true), WEEK(true), DAY(true),
-	HOUR(false), MINUTE(false),
-	SECOND(false), MILLISECOND(false), MICROSECOND(false),
+	HOUR(false), MINUTE(false), SECOND(false),
 	NANOSECOND(false),
 	DAY_OF_WEEK(true), DAY_OF_YEAR(true), DAY_OF_MONTH(true),
 	WEEK_OF_MONTH(true), WEEK_OF_YEAR(true),
@@ -32,10 +31,17 @@ public enum TemporalUnit {
 		throw new SemanticException("illegal unit conversion " + from + " to " + to);
 	}
 
+	public String conversionFactor(TemporalUnit toUnit) {
+		return conversionFactor(this, toUnit);
+	}
+
 	public static String conversionFactor(TemporalUnit fromUnit, TemporalUnit toUnit) {
 		long factor = 1;
 		boolean reciprocal = false;
-		if ( toUnit != fromUnit ) {
+		if (toUnit == fromUnit) {
+			return "";
+		}
+		else {
 			switch (toUnit) {
 				case NANOSECOND:
 					switch (fromUnit) {
@@ -49,57 +55,6 @@ public enum TemporalUnit {
 							factor *= 60;
 						case SECOND:
 							factor *= 1e3;
-						case MILLISECOND:
-							factor *= 1e3;
-						case MICROSECOND:
-							factor *= 1e3;
-							break;
-						default:
-							illegalConversion(fromUnit, toUnit);
-					}
-					break;
-				case MICROSECOND:
-					switch (fromUnit) {
-						case WEEK:
-							factor *= 7;
-						case DAY:
-							factor *= 24;
-						case HOUR:
-							factor *= 60;
-						case MINUTE:
-							factor *= 60;
-						case SECOND:
-							factor *= 1e3;
-						case MILLISECOND:
-							factor *= 1e3;
-							break;
-						case NANOSECOND:
-							factor *= 1e3;
-							reciprocal = true;
-							break;
-						default:
-							illegalConversion(fromUnit, toUnit);
-					}
-					break;
-				case MILLISECOND:
-					switch (fromUnit) {
-						case NANOSECOND:
-							factor *= 1e3;
-						case MICROSECOND:
-							factor *= 1e3;
-							reciprocal = true;
-							break;
-						case WEEK:
-							factor *= 7;
-						case DAY:
-							factor *= 24;
-						case HOUR:
-							factor *= 60;
-						case MINUTE:
-							factor *= 60;
-							break;
-						case SECOND:
-							factor *= 1e3;
 							break;
 						default:
 							illegalConversion(fromUnit, toUnit);
@@ -108,10 +63,6 @@ public enum TemporalUnit {
 				case SECOND:
 					switch (fromUnit) {
 						case NANOSECOND:
-							factor *= 1e3;
-						case MICROSECOND:
-							factor *= 1e3;
-						case MILLISECOND:
 							factor *= 1e3;
 							reciprocal = true;
 							break;
@@ -132,10 +83,6 @@ public enum TemporalUnit {
 					switch (fromUnit) {
 						case NANOSECOND:
 							factor *= 1e3;
-						case MICROSECOND:
-							factor *= 1e3;
-						case MILLISECOND:
-							factor *= 1e3;
 						case SECOND:
 							factor *= 60;
 							reciprocal = true;
@@ -154,10 +101,6 @@ public enum TemporalUnit {
 				case HOUR:
 					switch (fromUnit) {
 						case NANOSECOND:
-							factor *= 1e3;
-						case MICROSECOND:
-							factor *= 1e3;
-						case MILLISECOND:
 							factor *= 1e3;
 						case SECOND:
 							factor *= 60;
@@ -178,10 +121,6 @@ public enum TemporalUnit {
 					switch (fromUnit) {
 						case NANOSECOND:
 							factor *= 1e3;
-						case MICROSECOND:
-							factor *= 1e3;
-						case MILLISECOND:
-							factor *= 1e3;
 						case SECOND:
 							factor *= 60;
 						case MINUTE:
@@ -200,10 +139,6 @@ public enum TemporalUnit {
 				case WEEK:
 					switch (fromUnit) {
 						case NANOSECOND:
-							factor *= 1e3;
-						case MICROSECOND:
-							factor *= 1e3;
-						case MILLISECOND:
 							factor *= 1e3;
 						case SECOND:
 							factor *= 60;
@@ -261,15 +196,12 @@ public enum TemporalUnit {
 			String string = String.valueOf(factor);
 			int len = string.length();
 			int chop;
-			for ( chop = len; chop>0 && string.charAt(chop-1)=='0'; chop-- ) {}
+			for (chop = len; chop>0 && string.charAt(chop-1)=='0'; chop--) {}
 			int e = len-chop;
-			if ( chop>0 && e>2 ) {
+			if (chop>0 && e>2) {
 				string = string.substring(0, chop) + "e" + e;
 			}
 			return (reciprocal ? "/" : "*") + string;
-		}
-		else {
-			return "";
 		}
 	}
 

hibernate-core/src/main/antlr/org/hibernate/query/hql/internal/HqlLexer.g4
@@ -214,14 +214,13 @@ MAX					: [mM] [aA] [xX];
 MAXELEMENT			: [mM] [aA] [xX] [eE] [lL] [eE] [mM] [eE] [nN] [tT];
 MAXINDEX			: [mM] [aA] [xX] [iI] [nN] [dD] [eE] [xX];
 MEMBER				: [mM] [eE] [mM] [bB] [eE] [rR];
-MICROSECOND			: [mM] [iI] [cC] [rR] [oO] [sS] [eE] [cC] [oO] [nN] [dD];
-MILLISECOND			: [mM] [iI] [lL] [lL] [iI] [sS] [eE] [cC] [oO] [nN] [dD];
 MIN					: [mM] [iI] [nN];
 MINELEMENT			: [mM] [iI] [nN] [eE] [lL] [eE] [mM] [eE] [nN] [tT];
 MININDEX			: [mM] [iI] [nN] [iI] [nN] [dD] [eE] [xX];
 MINUTE				: [mM] [iI] [nN] [uU] [tT] [eE];
 MOD					: [mM] [oO] [dD];
 MONTH				: [mM] [oO] [nN] [tT] [hH];
+NANOSECOND			: [nN] [aA] [nN] [oO] [sS] [eE] [cC] [oO] [nN] [dD];
 NEW					: [nN] [eE] [wW];
 NOT					: [nN] [oO] [tT];
 NULLIF				: [nN] [uU] [lL] [lL] [iI] [fF];

hibernate-core/src/main/antlr/org/hibernate/query/hql/internal/HqlParser.g4
@@ -397,8 +397,8 @@ expression
 	| path											# PathExpression
 	| function										# FunctionExpression
 	| signOperator expression						# UnaryExpression
-	| expression intervalField  					# ToDurationExpression
-	| expression BY intervalField					# FromDurationExpression
+	| expression datetimeField  					# ToDurationExpression
+	| expression BY datetimeField					# FromDurationExpression
 	| expression multiplicativeOperator expression	# MultiplicationExpression
 	| expression additiveOperator expression		# AdditionExpression
 	| expression DOUBLE_PIPE expression				# ConcatenationExpression
@@ -559,12 +559,7 @@ nonStandardFunctionName
 	;
 
 nonStandardFunctionArguments
-	: (datetimeFieldArgument COMMA)? expression (COMMA expression)*
-	;
-
-datetimeFieldArgument
-	: datetimeField
-	| secondsField
+	: (datetimeField COMMA)? expression (COMMA expression)*
 	;
 
 jpaCollectionFunction
@@ -848,11 +843,6 @@ format
 	: STRING_LITERAL
 	;
 
-intervalField
-	: datetimeField
-	| secondsField
-	;
-
 extractFunction
 	: EXTRACT LEFT_PAREN extractField FROM expression RIGHT_PAREN
 	| datetimeField LEFT_PAREN expression RIGHT_PAREN
@@ -863,7 +853,6 @@ extractField
 	| dayField
 	| weekField
 	| timeZoneField
-	| secondsField
 	| dateOrTimeField
 	;
 
@@ -876,6 +865,7 @@ datetimeField
 	| HOUR
 	| MINUTE
 	| SECOND
+	| NANOSECOND
 	;
 
 dayField
@@ -887,11 +877,6 @@ dayField
 weekField
 	: WEEK OF MONTH
 	| WEEK OF YEAR
-	;
-
-secondsField
-	: MILLISECOND
-	| MICROSECOND
 	;
 
 timeZoneField
@@ -981,8 +966,6 @@ identifier
 	| LOWER
 	| MAP
 	| MAX
-	| MICROSECOND
-	| MILLISECOND
 	| MIN
 	| MINUTE
 	| MEMBER

hibernate-core/src/main/java/org/hibernate/dialect/CUBRIDDialect.java
@@ -23,7 +23,6 @@ import org.hibernate.tool.schema.extract.spi.SequenceInformationExtractor;
 import org.hibernate.type.spi.StandardSpiBasicTypes;
 
 import static org.hibernate.query.TemporalUnit.HOUR;
-import static org.hibernate.query.TemporalUnit.MILLISECOND;
 import static org.hibernate.query.TemporalUnit.MINUTE;
 import static org.hibernate.query.TemporalUnit.NANOSECOND;
 import static org.hibernate.query.TemporalUnit.SECOND;
@@ -295,18 +294,15 @@ public class CUBRIDDialect extends Dialect {
 		sqlAppender.append("adddate(");
 		to.render();
 		sqlAppender.append(",interval ");
-		if ( unit == MILLISECOND || unit == NANOSECOND ) {
+		if ( unit == NANOSECOND ) {
 			sqlAppender.append("(");
 		}
 		magnitude.render();
-		if ( unit == MILLISECOND ) {
-			sqlAppender.append(")/1e3");
-		}
 		if ( unit == NANOSECOND ) {
 			sqlAppender.append(")/1e6");
 		}
 		sqlAppender.append(" ");
-		if ( unit == MILLISECOND || unit == NANOSECOND ) {
+		if ( unit == NANOSECOND ) {
 			sqlAppender.append("microsecond");
 		}
 		else {
@@ -326,48 +322,60 @@ public class CUBRIDDialect extends Dialect {
 				sqlAppender.append(")");
 				break;
 			case HOUR:
-				timediff(from, to, sqlAppender, HOUR);
+				timediff(from, to, sqlAppender, HOUR, unit);
 				break;
 			case MINUTE:
 				sqlAppender.append("(");
-				timediff(from, to, sqlAppender, MINUTE);
-				sqlAppender.append("+60*");
-				timediff(from, to, sqlAppender, HOUR);
+				timediff(from, to, sqlAppender, MINUTE, unit);
+				sqlAppender.append("+");
+				timediff(from, to, sqlAppender, HOUR, unit);
 				sqlAppender.append(")");
+				break;
 			case SECOND:
 				sqlAppender.append("(");
-				timediff(from, to, sqlAppender, SECOND);
-				sqlAppender.append("+60*");
-				timediff(from, to, sqlAppender, MINUTE);
-				sqlAppender.append("+3600*");
-				timediff(from, to, sqlAppender, HOUR);
+				timediff(from, to, sqlAppender, SECOND, unit);
+				sqlAppender.append("+");
+				timediff(from, to, sqlAppender, MINUTE, unit);
+				sqlAppender.append("+");
+				timediff(from, to, sqlAppender, HOUR, unit);
 				sqlAppender.append(")");
-			case MILLISECOND:
+				break;
 			case NANOSECOND:
 				sqlAppender.append("(");
-				timediff(from, to, sqlAppender, MILLISECOND);
-				sqlAppender.append("+1e3*");
-				timediff(from, to, sqlAppender, SECOND);
-				sqlAppender.append("+6e4*");
-				timediff(from, to, sqlAppender, MINUTE);
-				sqlAppender.append("+36e5*");
-				timediff(from, to, sqlAppender, HOUR);
+				timediff(from, to, sqlAppender, NANOSECOND, unit);
+				sqlAppender.append("+");
+				timediff(from, to, sqlAppender, SECOND, unit);
+				sqlAppender.append("+");
+				timediff(from, to, sqlAppender, MINUTE, unit);
+				sqlAppender.append("+");
+				timediff(from, to, sqlAppender, HOUR, unit);
 				sqlAppender.append(")");
-				if (unit==NANOSECOND) {
-					sqlAppender.append("*1e3");
-				}
+				break;
 			default:
 				throw new SemanticException("unsupported temporal unit for CUBRID: " + unit);
 		}
 	}
 
-	private void timediff(Renderer from, Renderer to, Appender sqlAppender, TemporalUnit unit) {
+	private void timediff(
+			Renderer from, Renderer to,
+			Appender sqlAppender,
+			TemporalUnit diffUnit,
+			TemporalUnit toUnit) {
+		if ( diffUnit == NANOSECOND ) {
+			sqlAppender.append("1e6*");
+		}
 		sqlAppender.append("extract(");
-		sqlAppender.append( unit.toString() );
+		if ( diffUnit == NANOSECOND ) {
+			sqlAppender.append("millisecond");
+		}
+		else {
+			sqlAppender.append( diffUnit.toString() );
+		}
 		sqlAppender.append(",timediff(");
 		to.render();
 		sqlAppender.append(",");
 		from.render();
 		sqlAppender.append("))");
+		sqlAppender.append( diffUnit.conversionFactor(toUnit) );
 	}
 }

hibernate-core/src/main/java/org/hibernate/dialect/Cache71Dialect.java
@@ -43,7 +43,6 @@ import org.hibernate.sql.CacheJoinFragment;
 import org.hibernate.sql.JoinFragment;
 import org.hibernate.type.spi.StandardSpiBasicTypes;
 
-import static org.hibernate.query.TemporalUnit.MICROSECOND;
 import static org.hibernate.query.TemporalUnit.NANOSECOND;
 
 /**
@@ -314,7 +313,7 @@ public class Cache71Dialect extends Dialect {
 	@Override
 	public void timestampadd(TemporalUnit unit, Renderer magnitude, Renderer to, Appender sqlAppender, boolean timestamp) {
 		sqlAppender.append("dateadd(");
-		if ( unit == NANOSECOND || unit == MICROSECOND ) {
+		if ( unit == NANOSECOND ) {
 			sqlAppender.append("millisecond");
 		}
 		else {
@@ -324,9 +323,6 @@ public class Cache71Dialect extends Dialect {
 		magnitude.render();
 		if ( unit == NANOSECOND ) {
 			sqlAppender.append("/1e6");
-		}
-		if ( unit == MICROSECOND ) {
-			sqlAppender.append("/1e3");
 		}
 		sqlAppender.append(", ");
 		to.render();

hibernate-core/src/main/java/org/hibernate/dialect/DB2Dialect.java
@@ -197,8 +197,6 @@ public class DB2Dialect extends Dialect {
 		boolean castFrom = !fromTimestamp && !unit.isDateUnit();
 		boolean castTo = !toTimestamp && !unit.isDateUnit();
 		switch (unit) {
-			case MILLISECOND:
-			case MICROSECOND:
 			case NANOSECOND:
 				sqlAppender.append("(second");
 				break;
@@ -230,20 +228,6 @@ public class DB2Dialect extends Dialect {
 		}
 		sqlAppender.append(")");
 		switch (unit) {
-			case MILLISECOND:
-				sqlAppender.append("*1e3+(microsecond(");
-				to.render();
-				sqlAppender.append(")-microsecond(");
-				from.render();
-				sqlAppender.append("))/1e3)");
-				break;
-			case MICROSECOND:
-				sqlAppender.append("*1e6+microsecond(");
-				to.render();
-				sqlAppender.append(")-microsecond(");
-				from.render();
-				sqlAppender.append("))");
-				break;
 			case NANOSECOND:
 				sqlAppender.append("*1e9+(microsecond(");
 				to.render();
@@ -266,8 +250,6 @@ public class DB2Dialect extends Dialect {
 		sqlAppender.append("add_");
 		switch (unit) {
 			case NANOSECOND:
-			case MILLISECOND:
-			case MICROSECOND:
 				sqlAppender.append("second");
 				break;
 			case WEEK:
@@ -290,8 +272,6 @@ public class DB2Dialect extends Dialect {
 		}
 		sqlAppender.append(",");
 		switch (unit) {
-			case MILLISECOND:
-			case MICROSECOND:
 			case NANOSECOND:
 			case WEEK:
 			case QUARTER:
@@ -302,12 +282,6 @@ public class DB2Dialect extends Dialect {
 		switch (unit) {
 			case NANOSECOND:
 				sqlAppender.append(")/1e9");
-				break;
-			case MILLISECOND:
-				sqlAppender.append(")/1e3");
-				break;
-			case MICROSECOND:
-				sqlAppender.append(")/1e6");
 				break;
 			case WEEK:
 				sqlAppender.append(")*7");

hibernate-core/src/main/java/org/hibernate/dialect/H2Dialect.java
@@ -13,6 +13,7 @@ import org.hibernate.JDBCException;
 import org.hibernate.PessimisticLockException;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.dialect.function.CommonFunctionFactory;
+import org.hibernate.dialect.function.H2ExtractEmulation;
 import org.hibernate.dialect.hint.IndexQueryHintHandler;
 import org.hibernate.dialect.identity.H2IdentityColumnSupport;
 import org.hibernate.dialect.identity.IdentityColumnSupport;
@@ -173,6 +174,8 @@ public class H2Dialect extends Dialect {
 		CommonFunctionFactory.trim1( queryEngine );
 		CommonFunctionFactory.concat_operator( queryEngine );
 		CommonFunctionFactory.formatdatetime( queryEngine );
+
+		queryEngine.getSqmFunctionRegistry().register( "extract", new H2ExtractEmulation() );
 
 		queryEngine.getSqmFunctionRegistry().noArgsBuilder( "rownum" )
 				.setInvariantType( StandardSpiBasicTypes.LONG )

hibernate-core/src/main/java/org/hibernate/dialect/HSQLDialect.java
@@ -55,7 +55,6 @@ import org.hibernate.type.spi.StandardSpiBasicTypes;
 
 import org.jboss.logging.Logger;
 
-import static org.hibernate.query.TemporalUnit.MICROSECOND;
 import static org.hibernate.query.TemporalUnit.NANOSECOND;
 
 /**
@@ -231,7 +230,7 @@ public class HSQLDialect extends Dialect {
 	@Override
 	public void timestampadd(TemporalUnit unit, Renderer magnitude, Renderer to, Appender sqlAppender, boolean timestamp) {
 		boolean castTo = !timestamp && !unit.isDateUnit();
-		if ( unit == MICROSECOND || unit == NANOSECOND ) {
+		if ( unit == NANOSECOND ) {
 			sqlAppender.append("timestampadd(sql_tsi_frac_second"); //nanos
 		}
 		else {
@@ -239,13 +238,7 @@ public class HSQLDialect extends Dialect {
 			sqlAppender.append( unit.toString() );
 		}
 		sqlAppender.append(", ");
-		if ( unit == MICROSECOND ) {
-			sqlAppender.append("1e3*(");
-		}
 		magnitude.render();
-		if ( unit == MICROSECOND ) {
-			sqlAppender.append(")");
-		}
 		sqlAppender.append(", ");
 		if (castTo) {
 			sqlAppender.append("cast(");
@@ -261,7 +254,7 @@ public class HSQLDialect extends Dialect {
 	public void timestampdiff(TemporalUnit unit, Renderer from, Renderer to, Appender sqlAppender, boolean fromTimestamp, boolean toTimestamp) {
 		boolean castFrom = !fromTimestamp && !unit.isDateUnit();
 		boolean castTo = !toTimestamp && !unit.isDateUnit();
-		if ( unit == MICROSECOND || unit == NANOSECOND ) {
+		if ( unit == NANOSECOND ) {
 			sqlAppender.append("timestampdiff(sql_tsi_frac_second"); //nanos
 		}
 		else {
@@ -285,9 +278,6 @@ public class HSQLDialect extends Dialect {
 			sqlAppender.append(" as timestamp)");
 		}
 		sqlAppender.append(")");
-		if ( unit == MICROSECOND ) {
-			sqlAppender.append("/1e3");
-		}
 	}
 
 	@Override

commit 99ec7869071047c892c3d87e57403fc234d9f618

fix the conversion factor for NANOSECOND
I broke it in 762f6cfa9062d8142fb9c86025d0cb4cd8b3963c

hibernate-core/src/main/java/org/hibernate/query/TemporalUnit.java
@@ -54,7 +54,7 @@ public enum TemporalUnit {
 						case MINUTE:
 							factor *= 60;
 						case SECOND:
-							factor *= 1e3;
+							factor *= 1e9;
 							break;
 						default:
 							illegalConversion(fromUnit, toUnit);
@@ -63,7 +63,7 @@ public enum TemporalUnit {
 				case SECOND:
 					switch (fromUnit) {
 						case NANOSECOND:
-							factor *= 1e3;
+							factor *= 1e9;
 							reciprocal = true;
 							break;
 						case WEEK:
@@ -82,7 +82,7 @@ public enum TemporalUnit {
 				case MINUTE:
 					switch (fromUnit) {
 						case NANOSECOND:
-							factor *= 1e3;
+							factor *= 1e9;
 						case SECOND:
 							factor *= 60;
 							reciprocal = true;
@@ -101,7 +101,7 @@ public enum TemporalUnit {
 				case HOUR:
 					switch (fromUnit) {
 						case NANOSECOND:
-							factor *= 1e3;
+							factor *= 1e9;
 						case SECOND:
 							factor *= 60;
 						case MINUTE:
@@ -120,7 +120,7 @@ public enum TemporalUnit {
 				case DAY:
 					switch (fromUnit) {
 						case NANOSECOND:
-							factor *= 1e3;
+							factor *= 1e9;
 						case SECOND:
 							factor *= 60;
 						case MINUTE:
@@ -139,7 +139,7 @@ public enum TemporalUnit {
 				case WEEK:
 					switch (fromUnit) {
 						case NANOSECOND:
-							factor *= 1e3;
+							factor *= 1e9;
 						case SECOND:
 							factor *= 60;
 						case MINUTE:
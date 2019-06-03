65a028fa0527e47f598b472e4297fdc964bc0ed7
gavinking <gavin@ceylon-lang.org>
2019-06-03 23:27:35 +0200

    rationalize Teradata dialects using a version number

--- hibernate-core/src/main/java/org/hibernate/dialect/TeradataDialect.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/TeradataDialect.java
@@ -6,16 +6,34 @@
  */
 package org.hibernate.dialect;
 
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Types;
+import java.util.Map;
 
+import org.hibernate.LockOptions;
 import org.hibernate.cfg.Environment;
 import org.hibernate.dialect.function.CommonFunctionFactory;
+import org.hibernate.dialect.identity.IdentityColumnSupport;
+import org.hibernate.dialect.identity.Teradata14IdentityColumnSupport;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+import org.hibernate.engine.jdbc.spi.JdbcServices;
+import org.hibernate.exception.spi.TemplatedViolatedConstraintNameExtracter;
+import org.hibernate.exception.spi.ViolatedConstraintNameExtracter;
+import org.hibernate.metamodel.model.relational.spi.Index;
+import org.hibernate.metamodel.model.relational.spi.PhysicalColumn;
+import org.hibernate.naming.QualifiedNameImpl;
+import org.hibernate.naming.QualifiedTableName;
 import org.hibernate.query.TemporalUnit;
 import org.hibernate.query.spi.QueryEngine;
+import org.hibernate.query.spi.QueryOptions;
 import org.hibernate.query.sqm.mutation.spi.SqmMutationStrategy;
 import org.hibernate.query.sqm.mutation.spi.idtable.GlobalTempTableExporter;
 import org.hibernate.query.sqm.mutation.spi.idtable.GlobalTemporaryTableStrategy;
 import org.hibernate.query.sqm.mutation.spi.idtable.IdTable;
+import org.hibernate.sql.ForUpdateFragment;
+import org.hibernate.tool.schema.internal.StandardIndexExporter;
 import org.hibernate.tool.schema.spi.Exporter;
 import org.hibernate.type.spi.StandardSpiBasicTypes;
 
@@ -28,6 +46,10 @@ import static org.hibernate.query.TemporalUnit.NANOSECOND;
  * @author Jay Nance
  */
 public class TeradataDialect extends Dialect {
+
+	int getVersion() {
+		return 1200;
+	}
 	
 	private static final int PARAM_LIST_SIZE_LIMIT = 1024;
 
@@ -42,11 +64,18 @@ public class TeradataDialect extends Dialect {
 		registerColumnType( Types.BIT, "byteint" );
 
 		registerColumnType( Types.TINYINT, "byteint" );
-		registerColumnType( Types.BIGINT, "numeric(19,0)" );
 
 		registerColumnType( Types.BINARY, "byte($l)" );
 		registerColumnType( Types.VARBINARY, "varbyte($l)" );
 		registerColumnType( Types.LONGVARBINARY, "varbyte($l)" );
+
+		if ( getVersion() < 1300 ) {
+			registerColumnType( Types.BIGINT, "numeric(19,0)" );
+		}
+		else {
+			//'bigint' has been there since at least version 13
+			registerColumnType( Types.BIGINT, "bigint" );
+		}
 
 		registerKeyword( "password" );
 		registerKeyword( "type" );
@@ -61,15 +90,22 @@ public class TeradataDialect extends Dialect {
 		registerKeyword( "account" );
 		registerKeyword( "class" );
 
-		// Tell hibernate to use getBytes instead of getBinaryStream
-		getDefaultProperties().setProperty( Environment.USE_STREAMS_FOR_BINARY, "false" );
-		// No batch statements
-		getDefaultProperties().setProperty( Environment.STATEMENT_BATCH_SIZE, NO_BATCH );
+		if ( getVersion() < 1400 ) {
+			// use getBytes instead of getBinaryStream
+			getDefaultProperties().setProperty( Environment.USE_STREAMS_FOR_BINARY, "false" );
+			// no batch statements
+			getDefaultProperties().setProperty( Environment.STATEMENT_BATCH_SIZE, NO_BATCH );
+		}
+		else {
+			getDefaultProperties().setProperty( Environment.USE_STREAMS_FOR_BINARY, "true" );
+			getDefaultProperties().setProperty( Environment.STATEMENT_BATCH_SIZE, DEFAULT_BATCH_SIZE );
+		}
+
 	}
 
 	@Override
 	public int getDefaultDecimalPrecision() {
-		return 18;
+		return getVersion() < 1400 ? 18 : 38;
 	}
 
 	public void timestampdiff(TemporalUnit unit, Renderer from, Renderer to, Appender sqlAppender, boolean fromTimestamp, boolean toTimestamp) {
@@ -170,6 +206,24 @@ public class TeradataDialect extends Dialect {
 				.setExactArgumentCount( 2 )
 				.register();
 
+		if ( getVersion() >= 1400 ) {
+
+			//list actually taken from Teradata 15 docs
+			CommonFunctionFactory.lastDay( queryEngine );
+			CommonFunctionFactory.initcap( queryEngine );
+			CommonFunctionFactory.pad( queryEngine );
+			CommonFunctionFactory.trim2( queryEngine );
+			CommonFunctionFactory.soundex( queryEngine );
+			CommonFunctionFactory.ascii( queryEngine );
+			CommonFunctionFactory.char_chr( queryEngine );
+			CommonFunctionFactory.trunc( queryEngine );
+			CommonFunctionFactory.moreHyperbolic( queryEngine );
+			CommonFunctionFactory.monthsBetween( queryEngine );
+			CommonFunctionFactory.addMonths( queryEngine );
+			CommonFunctionFactory.stddevPopSamp( queryEngine );
+			CommonFunctionFactory.varPopSamp( queryEngine );
+		}
+
 	}
 
 	/**
@@ -189,7 +243,7 @@ public class TeradataDialect extends Dialect {
 
 	@Override
 	public String getAddColumnString() {
-		return "Add Column";
+		return getVersion() < 1400 ? "Add Column" : "Add";
 	}
 
 	@Override
@@ -227,8 +281,18 @@ public class TeradataDialect extends Dialect {
 	}
 
 	@Override
+	public boolean supportsTupleDistinctCounts() {
+		return false;
+	}
+
+	@Override
+	public boolean supportsExistsInSelect() {
+		return false;
+	}
+
+	@Override
 	public boolean areStringComparisonsCaseInsensitive() {
-		return true;
+		return getVersion() < 1400;
 	}
 
 	@Override
@@ -284,6 +348,11 @@ public class TeradataDialect extends Dialect {
 	}
 
 	@Override
+	public boolean supportsUnboundedLobLocatorMaterialization() {
+		return false;
+	}
+
+	@Override
 	public String getCreateMultisetTableString() {
 		return "create multiset table ";
 	}
@@ -312,4 +381,162 @@ public class TeradataDialect extends Dialect {
 	public int getInExpressionCountLimit() {
 		return PARAM_LIST_SIZE_LIMIT;
 	}
+
+	@Override
+	public int registerResultSetOutParameter(CallableStatement statement, int col) throws SQLException {
+		statement.registerOutParameter( col, Types.REF );
+		col++;
+		return col;
+	}
+
+	@Override
+	public ResultSet getResultSet(CallableStatement cs) throws SQLException {
+		boolean isResultSet = cs.execute();
+		while ( !isResultSet && cs.getUpdateCount() != -1 ) {
+			isResultSet = cs.getMoreResults();
+		}
+		return cs.getResultSet();
+	}
+
+	@Override
+	public ViolatedConstraintNameExtracter getViolatedConstraintNameExtracter() {
+		return getVersion() < 1400 ? super.getViolatedConstraintNameExtracter() : EXTRACTER;
+	}
+
+	private static ViolatedConstraintNameExtracter EXTRACTER = new TemplatedViolatedConstraintNameExtracter() {
+		/**
+		 * Extract the name of the violated constraint from the given SQLException.
+		 *
+		 * @param sqle The exception that was the result of the constraint violation.
+		 * @return The extracted constraint name.
+		 */
+		@Override
+		protected String doExtractConstraintName(SQLException sqle) throws NumberFormatException {
+			String constraintName = null;
+
+			int errorCode = sqle.getErrorCode();
+			switch (errorCode) {
+				case 27003:
+					constraintName = extractUsingTemplate( "Unique constraint (", ") violated.", sqle.getMessage() );
+					break;
+				case 2700:
+					constraintName = extractUsingTemplate( "Referential constraint", "violation:", sqle.getMessage() );
+					break;
+				case 5317:
+					constraintName = extractUsingTemplate( "Check constraint (", ") violated.", sqle.getMessage() );
+					break;
+			}
+
+			if ( constraintName != null ) {
+				int i = constraintName.indexOf( '.' );
+				if ( i != -1 ) {
+					constraintName = constraintName.substring( i + 1 );
+				}
+			}
+			return constraintName;
+		}
+	};
+
+	@Override
+	public boolean supportsLockTimeouts() {
+		return false;
+	}
+
+	@Override
+	public boolean useFollowOnLocking(String sql, QueryOptions queryOptions) {
+		return getVersion() >= 1400;
+	}
+
+	@Override
+	public String getWriteLockString(int timeout) {
+		if ( getVersion() < 1400 ) {
+			return super.getWriteLockString( timeout );
+		}
+		String sMsg = " Locking row for write ";
+		if ( timeout == LockOptions.NO_WAIT ) {
+			return sMsg + " nowait ";
+		}
+		return sMsg;
+	}
+
+	@Override
+	public String getReadLockString(int timeout) {
+		if ( getVersion() < 1400 ) {
+			return super.getReadLockString( timeout );
+		}
+		String sMsg = " Locking row for read  ";
+		if ( timeout == LockOptions.NO_WAIT ) {
+			return sMsg + " nowait ";
+		}
+		return sMsg;
+	}
+
+	@Override
+	public Exporter<Index> getIndexExporter() {
+		return new TeradataIndexExporter(this);
+	}
+
+	private static class TeradataIndexExporter extends StandardIndexExporter implements Exporter<Index> {
+
+		private TeradataIndexExporter(Dialect dialect) {
+			super(dialect);
+		}
+
+		@Override
+		public String[] getSqlCreateStrings(Index index, JdbcServices jdbcServices) {
+			final JdbcEnvironment jdbcEnvironment = jdbcServices.getJdbcEnvironment();
+			QualifiedTableName qualifiedTableName = index.getTable().getQualifiedTableName();
+			final String tableName = jdbcEnvironment.getQualifiedObjectNameFormatter().format(
+					qualifiedTableName,
+					jdbcEnvironment.getDialect()
+			);
+
+			final String indexNameForCreation;
+			if ( getDialect().qualifyIndexName() ) {
+				indexNameForCreation = jdbcEnvironment.getQualifiedObjectNameFormatter().format(
+						new QualifiedNameImpl(
+								qualifiedTableName.getCatalogName(),
+								qualifiedTableName.getSchemaName(),
+								index.getName()
+						),
+						jdbcEnvironment.getDialect()
+				);
+			}
+			else {
+				indexNameForCreation = index.getName().render( jdbcEnvironment.getDialect() );
+			}
+
+			StringBuilder columnList = new StringBuilder();
+			boolean first = true;
+			for ( PhysicalColumn column : index.getColumns() ) {
+				if ( first ) {
+					first = false;
+				}
+				else {
+					columnList.append( ", " );
+				}
+				columnList.append( column.getName().render( jdbcEnvironment.getDialect() ) );
+			}
+
+			return new String[] {
+					"create index " + indexNameForCreation
+							+ "(" + columnList + ") on " + tableName
+			};
+		}
+	}
+
+	@Override
+	public IdentityColumnSupport getIdentityColumnSupport() {
+		return getVersion() < 1400
+				? super.getIdentityColumnSupport()
+				: new Teradata14IdentityColumnSupport();
+	}
+
+	@Override
+	public String applyLocksToSql(String sql, LockOptions aliasedLockOptions, Map<String, String[]> keyColumnNames) {
+		return getVersion() < 1400
+				? super.applyLocksToSql( sql, aliasedLockOptions, keyColumnNames )
+				: new ForUpdateFragment( this, aliasedLockOptions, keyColumnNames ).toFragmentString() + " " + sql;
+	}
+
 }

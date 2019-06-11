d76dd4da581f1a4492ec5cc73daef1df7aee9c88
Scott Marlow <smarlow@redhat.com>
2019-06-11 09:40:36 -0400

    HHH-13433 EntityManager.find() should only check for roll-back-only condition if there is an active JTA transaction, otherwise ORM should throw throw convert( e, lockOptions )

--- hibernate-core/src/main/java/org/hibernate/internal/SessionImpl.java
+++ hibernate-core/src/main/java/org/hibernate/internal/SessionImpl.java
@@ -3511,7 +3511,7 @@ public final class SessionImpl
 			throw exceptionConverter.convert( new IllegalArgumentException( e.getMessage(), e ) );
 		}
 		catch ( JDBCException e ) {
-			if ( accessTransaction().getRollbackOnly() ) {
+			if ( accessTransaction().isActive() && accessTransaction().getRollbackOnly() ) {
 				// assume this is the similar to the WildFly / IronJacamar "feature" described under HHH-12472
 				return null;
 			}

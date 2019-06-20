33f02f411f71f75ff78fe476ea836048709bf400
Sanne Grinovero <sanne@hibernate.org>
2019-06-20 10:33:45 +0100

    HHH-13447 Minimize number of EventListenerRegistry lookups within a Session use

--- hibernate-core/src/main/java/org/hibernate/internal/SessionImpl.java
+++ hibernate-core/src/main/java/org/hibernate/internal/SessionImpl.java
@@ -242,6 +242,7 @@ public final class SessionImpl
 	private transient boolean discardOnClose;
 
 	private transient TransactionObserver transactionObserver;
+	private transient EventListenerRegistry eventListenerRegistry;
 
 	public SessionImpl(SessionFactoryImpl factory, SessionCreationOptions options) {
 		super( factory, options );
@@ -678,7 +679,10 @@ public final class SessionImpl
 	}
 
 	private <T> EventListenerGroup<T> eventListenerGroup(EventType<T> type) {
-		return getFactory().getServiceRegistry().getService( EventListenerRegistry.class ).getEventListenerGroup( type );
+		if ( this.eventListenerRegistry == null ) {
+			this.eventListenerRegistry = getFactory().getServiceRegistry().getService( EventListenerRegistry.class );
+		}
+		return eventListenerRegistry.getEventListenerGroup( type );
 	}
 
 

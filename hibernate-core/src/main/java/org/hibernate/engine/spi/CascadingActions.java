2ea0505e47559f7a1af3e0acd622595aa6d60076
Sanne Grinovero <sanne@hibernate.org>
2019-06-22 23:59:32 +0100

    HHH-13453 Optimise CascadingActions for the most likely case

--- hibernate-core/src/main/java/org/hibernate/engine/spi/CascadingActions.java
+++ hibernate-core/src/main/java/org/hibernate/engine/spi/CascadingActions.java
@@ -369,21 +369,20 @@ public class CascadingActions {
 				int propertyIndex) {
 			if ( propertyType.isEntityType() ) {
 				Object child = persister.getPropertyValue( parent, propertyIndex );
-				String childEntityName = ((EntityType) propertyType).getAssociatedEntityName( session.getFactory() );
-
 				if ( child != null
 						&& !isInManagedState( child, session )
-						&& !(child instanceof HibernateProxy) //a proxy cannot be transient and it breaks ForeignKeys.isTransient
-						&& ForeignKeys.isTransient( childEntityName, child, null, session ) ) {
-					String parentEntityName = persister.getEntityName();
-					String propertyName = persister.getPropertyNames()[propertyIndex];
-					throw new TransientPropertyValueException(
+						&& !(child instanceof HibernateProxy) ) { //a proxy cannot be transient and it breaks ForeignKeys.isTransient
+					final String childEntityName = ((EntityType) propertyType).getAssociatedEntityName(session.getFactory());
+					if (ForeignKeys.isTransient(childEntityName, child, null, session)) {
+						String parentEntityName = persister.getEntityName();
+						String propertyName = persister.getPropertyNames()[propertyIndex];
+						throw new TransientPropertyValueException(
 							"object references an unsaved transient instance - save the transient instance before flushing",
 							childEntityName,
 							parentEntityName,
 							propertyName
-					);
-
+						);
+					}
 				}
 			}
 		}

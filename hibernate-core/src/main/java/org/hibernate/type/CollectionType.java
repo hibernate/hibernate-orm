7d54b59743f72267ac78021d0f0b178ef1f92def
Gail Badner <gbadner@redhat.com>
2019-06-26 12:09:05 -0700

    HHH-13466 : ClassCastException when changing a Collection association to a Set if @PreUpdate listener exists

--- hibernate-core/src/main/java/org/hibernate/type/CollectionType.java
+++ hibernate-core/src/main/java/org/hibernate/type/CollectionType.java
@@ -45,7 +45,6 @@ import org.hibernate.persister.entity.Joinable;
 import org.hibernate.pretty.MessageHelper;
 import org.hibernate.proxy.HibernateProxy;
 import org.hibernate.proxy.LazyInitializer;
-import org.hibernate.type.spi.TypeConfiguration;
 
 import org.jboss.logging.Logger;
 
@@ -113,8 +112,12 @@ public abstract class CollectionType extends AbstractType implements Association
 	@Override
 	public final boolean isEqual(Object x, Object y) {
 		return x == y
-			|| ( x instanceof PersistentCollection && ( (PersistentCollection) x ).wasInitialized() && ( (PersistentCollection) x ).isWrapper( y ) )
-			|| ( y instanceof PersistentCollection && ( (PersistentCollection) y ).wasInitialized() && ( (PersistentCollection) y ).isWrapper( x ) );
+			|| ( x instanceof PersistentCollection && isEqual( (PersistentCollection) x, y ) )
+			|| ( y instanceof PersistentCollection && isEqual( (PersistentCollection) y, x ) );
+	}
+
+	private boolean isEqual(PersistentCollection x, Object y) {
+		return x.wasInitialized() && ( x.isWrapper( y ) || x.isDirectlyProvidedCollection( y ) );
 	}
 
 	@Override

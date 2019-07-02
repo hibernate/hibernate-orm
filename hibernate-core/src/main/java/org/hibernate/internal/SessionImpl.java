078c0df460a31313f6cda0777ddad483a9a32cec
Sanne Grinovero <sanne@hibernate.org>
2019-07-02 10:12:27 +0100

    HHH-13475 SessionImpl#applyQuerySettingsAndHints should not rely on defensive copies to just read properties

--- hibernate-core/src/main/java/org/hibernate/internal/SessionImpl.java
+++ hibernate-core/src/main/java/org/hibernate/internal/SessionImpl.java
@@ -323,12 +323,12 @@ public final class SessionImpl
 		if ( lockOptions.getLockMode() != LockMode.NONE ) {
 			query.setLockMode( getLockMode( lockOptions.getLockMode() ) );
 		}
-		Object queryTimeout;
-		if ( (queryTimeout = getProperties().get( QueryHints.SPEC_HINT_TIMEOUT ) ) != null ) {
+		final Object queryTimeout;
+		if ( ( queryTimeout = properties.get( QueryHints.SPEC_HINT_TIMEOUT ) ) != null ) {
 			query.setHint( QueryHints.SPEC_HINT_TIMEOUT, queryTimeout );
 		}
-		Object lockTimeout;
-		if( (lockTimeout = getProperties().get( JPA_LOCK_TIMEOUT ))!=null){
+		final Object lockTimeout;
+		if ( ( lockTimeout = properties.get( JPA_LOCK_TIMEOUT ) ) != null ) {
 			query.setHint( JPA_LOCK_TIMEOUT, lockTimeout );
 		}
 	}

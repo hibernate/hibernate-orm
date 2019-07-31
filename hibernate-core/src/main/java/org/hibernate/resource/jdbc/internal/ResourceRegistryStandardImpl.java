5699258aa278b8d04e6fce42655d5bc02d4b62b4
Sanne Grinovero <sanne@hibernate.org>
2019-07-31 10:06:29 +0100

    HHH-13526 Optimise HashMap lookup operations on ResourceRegistryStandardImpl xref

--- hibernate-core/src/main/java/org/hibernate/resource/jdbc/internal/ResourceRegistryStandardImpl.java
+++ hibernate-core/src/main/java/org/hibernate/resource/jdbc/internal/ResourceRegistryStandardImpl.java
@@ -81,18 +81,16 @@ public class ResourceRegistryStandardImpl implements ResourceRegistry {
 	public void release(Statement statement) {
 		log.tracev( "Releasing statement [{0}]", statement );
 
-		// Keep this at DEBUG level, rather than warn.  Numerous connection pool implementations can return a
-		// proxy/wrapper around the JDBC Statement, causing excessive logging here.  See HHH-8210.
-		if ( log.isDebugEnabled() && !xref.containsKey( statement ) ) {
-			log.unregisteredStatement();
+		final Set<ResultSet> resultSets = xref.remove( statement );
+		if ( resultSets != null ) {
+			closeAll( resultSets );
 		}
 		else {
-			final Set<ResultSet> resultSets = xref.get( statement );
-			if ( resultSets != null ) {
-				closeAll( resultSets );
-			}
-			xref.remove( statement );
+			// Keep this at DEBUG level, rather than warn.  Numerous connection pool implementations can return a
+			// proxy/wrapper around the JDBC Statement, causing excessive logging here.  See HHH-8210.
+			log.unregisteredStatement();
 		}
+
 		close( statement );
 
 		if ( lastQuery == statement ) {

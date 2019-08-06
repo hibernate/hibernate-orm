bff1afe4a655f3413ad67052bac3ec21ea0f0b3c
Sanne Grinovero <sanne@hibernate.org>
2019-08-06 09:02:06 +0100

    HHH-13527 Optimise DefaultLoadEventListener#getFromSharedCache hot path

--- hibernate-core/src/main/java/org/hibernate/event/internal/DefaultLoadEventListener.java
+++ hibernate-core/src/main/java/org/hibernate/event/internal/DefaultLoadEventListener.java
@@ -731,23 +731,25 @@ public class DefaultLoadEventListener extends AbstractLockUpgradeEventListener i
 		final EntityPersister persister,
 		SessionImplementor source ) {
 		final EntityDataAccess cache = persister.getCacheAccessStrategy();
+		final SessionFactoryImplementor factory = source.getFactory();
 		final Object ck = cache.generateCacheKey(
 				event.getEntityId(),
 				persister,
-				source.getFactory(),
+				factory,
 				source.getTenantIdentifier()
 		);
 
 		final Object ce = CacheHelper.fromSharedCache( source, ck, persister.getCacheAccessStrategy() );
-		if ( source.getFactory().getStatistics().isStatisticsEnabled() ) {
+		final StatisticsImplementor statistics = factory.getStatistics();
+		if ( statistics.isStatisticsEnabled() ) {
 			if ( ce == null ) {
-				source.getFactory().getStatistics().entityCacheMiss(
+				statistics.entityCacheMiss(
 						StatsHelper.INSTANCE.getRootEntityRole( persister ),
 						cache.getRegion().getName()
 				);
 			}
 			else {
-				source.getFactory().getStatistics().entityCacheHit(
+				statistics.entityCacheHit(
 						StatsHelper.INSTANCE.getRootEntityRole( persister ),
 						cache.getRegion().getName()
 				);

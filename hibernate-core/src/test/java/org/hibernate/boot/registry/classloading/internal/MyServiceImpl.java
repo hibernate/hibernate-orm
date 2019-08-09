a59a5064f7e9cd43b1f91757a5c3f4a7d0323306
Yoann Rodière <yoann@hibernate.org>
2019-08-09 17:42:18 +0200

    HHH-13551 Test the retrieval of a service when an "incompatible" classloader is provided

---
+++ hibernate-core/src/test/java/org/hibernate/boot/registry/classloading/internal/MyServiceImpl.java
@@ -0,0 +1,10 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
+package org.hibernate.boot.registry.classloading.internal;
+
+public class MyServiceImpl implements MyService {
+}

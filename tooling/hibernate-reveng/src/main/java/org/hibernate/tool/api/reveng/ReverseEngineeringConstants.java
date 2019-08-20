3a5fabb7e2b53cb0c809531461b217492a45fc1b
Koen Aers <koen.aers@gmail.com>
2019-08-20 12:30:29 +0200

    HBX-1836: Move the ant tasks from the core hibernate tools into the hibernate-tools-ant project - Add interface 'org.hibernate.tool.api.reveng.ReverseEngineeringConstants
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ tooling/hibernate-reveng/src/main/java/org/hibernate/tool/api/reveng/ReverseEngineeringConstants.java
@@ -0,0 +1,7 @@
+package org.hibernate.tool.api.reveng;
+
+public interface ReverseEngineeringConstants {
+
+	public static final String PREFER_BASIC_COMPOSITE_IDS = "org.hibernate.tool.api.reveng.ReverseEngineeringConstants.PreferBasicCompositeIds";
+
+}

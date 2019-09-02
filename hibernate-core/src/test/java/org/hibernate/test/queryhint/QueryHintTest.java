d1757cfcf956450c430dbbfc1a12f920d6e3bc8a
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2019-09-02 09:31:15 +0300

    HHH-13608 Oracle8iDialect should use CASE_INSENSITIVE pattern matching when checking the statement type

--- hibernate-core/src/test/java/org/hibernate/test/queryhint/QueryHintTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/queryhint/QueryHintTest.java
@@ -152,8 +152,33 @@ public class QueryHintTest extends BaseNonConfigCoreFunctionalTestCase {
 		assertTrue( sqlStatementInterceptor.getSqlQueries().get( 0 ).contains( "/* My_Query */ select /*+ ALL_ROWS */" ) );
 		sqlStatementInterceptor.clear();
 	}
+
+	@Test
+	@TestForIssue( jiraKey = "HHH-13608")
+	public void testQueryHintCaseInsensitive() {
+		sqlStatementInterceptor.clear();
+
+		doInHibernate( this::sessionFactory, s -> {
+			List results = s.createNativeQuery(
+				"SELECT e.id as id " +
+				"FROM Employee e " +
+				"JOIN Department d ON e.department_id = d.id " +
+				"WHERE d.name = :departmentName" )
+			.addQueryHint( "ALL_ROWS" )
+			.setComment( "My_Query" )
+			.setParameter( "departmentName", "Sales" )
+			.getResultList();
+
+			assertEquals(results.size(), 2);
+		} );
+
+		sqlStatementInterceptor.assertExecutedCount( 1 );
+
+		assertTrue( sqlStatementInterceptor.getSqlQueries().get( 0 ).contains( "/* My_Query */ SELECT /*+ ALL_ROWS */" ) );
+		sqlStatementInterceptor.clear();
+	}
 	
-	@Entity
+	@Entity(name = "Employee")
 	public static class Employee {
 		@Id
 		@GeneratedValue
@@ -162,8 +187,8 @@ public class QueryHintTest extends BaseNonConfigCoreFunctionalTestCase {
 		@ManyToOne(fetch = FetchType.LAZY)
 		public Department department;
 	}
-	
-	@Entity
+
+	@Entity(name = "Department")
 	public static class Department {
 		@Id
 		@GeneratedValue

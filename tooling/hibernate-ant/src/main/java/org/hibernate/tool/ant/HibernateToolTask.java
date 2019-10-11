fb818e92aa9f0aee6481698e702af7533146c7fb
Koen Aers <koen.aers@gmail.com>
2019-10-11 09:57:46 +0200

    HBX-1876: Extract 'HibernateToolTask#getProbableSolutionOrCause(Throwable)' into 'org.hibernate.tool.api.export.ExporterUtil#getProblemSolutionOrCause(Throwable)'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- tooling/hibernate-ant/src/main/java/org/hibernate/tool/ant/HibernateToolTask.java
+++ tooling/hibernate-ant/src/main/java/org/hibernate/tool/ant/HibernateToolTask.java
@@ -17,8 +17,7 @@ import org.apache.tools.ant.Task;
 import org.apache.tools.ant.types.Environment;
 import org.apache.tools.ant.types.Path;
 import org.apache.tools.ant.types.PropertySet;
-import org.hibernate.boot.MappingNotFoundException;
-import org.hibernate.boot.jaxb.Origin;
+import org.hibernate.tool.api.export.ExporterUtil;
 import org.hibernate.tool.api.metadata.MetadataDescriptor;
 import org.hibernate.tool.util.StringUtil;
 
@@ -203,7 +202,7 @@ public class HibernateToolTask extends Task {
 			log(ex, Project.MSG_ERR);
 		}
 
-		String newbieMessage = getProbableSolutionOrCause(re);
+		String newbieMessage = ExporterUtil.getProblemSolutionOrCause(re);
 		if(newbieMessage!=null) {
 			log(newbieMessage);
 		} 		
@@ -213,42 +212,6 @@ public class HibernateToolTask extends Task {
 		} else {
 			throw new BuildException(re, getLocation());
 		}
-	}
-
-	private String getProbableSolutionOrCause(Throwable re) {
-		if(re==null) return null;
-		
-		if(re instanceof MappingNotFoundException) {
-			MappingNotFoundException mnf = (MappingNotFoundException)re;
-			Origin origin = mnf.getOrigin();
-			return "A " + origin.getType() + " located at " + origin.getName() + " was not found.\n" +
-				"Check the following:\n" +
-				"\n" +
-				"1) Is the spelling/casing correct ?\n" +
-				"2)	Is " + mnf.getOrigin().getName() + " available via the classpath ?\n" +
-				"3) Does it actually exist ?\n";						
-		}
-
-		if(re instanceof ClassNotFoundException || re instanceof NoClassDefFoundError) {
-			
-			return "A class were not found in the classpath of the Ant task.\n" +
-					"Ensure that the classpath contains the classes needed for Hibernate and your code are in the classpath.\n"; 			
-			
-		}
-		
-		if(re instanceof UnsupportedClassVersionError) {
-			return "You are most likely running the ant task with a JRE that is older than the JRE required to use the classes.\n" +
-					"e.g. running with JRE 1.3 or 1.4 when using JDK 1.5 annotations is not possible.\n" +
-					"Ensure that you are using a correct JRE.";
-		}
-		
-		
-		
-		if(re.getCause()!=re) {
-			return getProbableSolutionOrCause( re.getCause() );					
-		}
-		
-		return null;
 	}
 
 	private void validateParameters() {
---
+++ tooling/hibernate-reveng/src/main/java/org/hibernate/tool/api/export/ExporterUtil.java
@@ -0,0 +1,44 @@
+package org.hibernate.tool.api.export;
+
+import org.hibernate.boot.MappingNotFoundException;
+import org.hibernate.boot.jaxb.Origin;
+
+public class ExporterUtil {
+
+	public static String getProblemSolutionOrCause(Throwable re) {
+		if(re==null) return null;
+		
+		if(re instanceof MappingNotFoundException) {
+			MappingNotFoundException mnf = (MappingNotFoundException)re;
+			Origin origin = mnf.getOrigin();
+			return "A " + origin.getType() + " located at " + origin.getName() + " was not found.\n" +
+				"Check the following:\n" +
+				"\n" +
+				"1) Is the spelling/casing correct ?\n" +
+				"2)	Is " + mnf.getOrigin().getName() + " available via the classpath ?\n" +
+				"3) Does it actually exist ?\n";						
+		}
+
+		if(re instanceof ClassNotFoundException || re instanceof NoClassDefFoundError) {
+			
+			return "A class were not found in the classpath of the Ant task.\n" +
+					"Ensure that the classpath contains the classes needed for Hibernate and your code are in the classpath.\n"; 			
+			
+		}
+		
+		if(re instanceof UnsupportedClassVersionError) {
+			return "You are most likely running the ant task with a JRE that is older than the JRE required to use the classes.\n" +
+					"e.g. running with JRE 1.3 or 1.4 when using JDK 1.5 annotations is not possible.\n" +
+					"Ensure that you are using a correct JRE.";
+		}
+		
+		
+		
+		if(re.getCause()!=re) {
+			return getProblemSolutionOrCause( re.getCause() );					
+		}
+		
+		return null;
+	}
+
+}

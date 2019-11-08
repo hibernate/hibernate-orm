7a62e74ef31f1597b95e76eebecdfb925d638956
gavinking <gavin@ceylon-lang.org>
2019-11-08 11:58:35 +0100

    replicate fix for HHH-13698
    see https://github.com/hibernate/hibernate-orm/pull/3089

--- hibernate-core/src/main/java/org/hibernate/dialect/MySQLDialect.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/MySQLDialect.java
@@ -662,6 +662,7 @@ public class MySQLDialect extends Dialect {
 			public JDBCException convert(SQLException sqlException, String message, String sql) {
 				switch ( sqlException.getErrorCode() ) {
 					case 1205:
+					case 3572:
 						return new PessimisticLockException( message, sqlException, sql );
 					case 1207:
 					case 1206:

5935545d6560a1cb9651f1ed23b865ca4947eaf9
Koen Aers <koen.aers@gmail.com>
2019-11-22 12:36:36 +0100

    HBX-1910: Refactor JdbcMetadataBuilder into smaller responsibilities - Move RevEngUtils#getColumnToMetaAttributesInRevengStrategy(...) to MetaAttributesBinder#getColumnToMetaAttributesInRevengStrategy(...)
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- tooling/hibernate-reveng/src/main/java/org/hibernate/tool/internal/reveng/RevEngUtils.java
+++ tooling/hibernate-reveng/src/main/java/org/hibernate/tool/internal/reveng/RevEngUtils.java
@@ -44,24 +44,6 @@ public class RevEngUtils {
 		return result;	
 	}
 
-	public static Map<String,MetaAttribute> getColumnToMetaAttributesInRevengStrategy(
-			ReverseEngineeringStrategy revengStrat,
-			Table table,
-			String defaultCatalog,
-			String defaultSchema,
-			String column) {
-		Map<String,MetaAttribute> result = null;
-		TableIdentifier tableIdentifier = TableIdentifier.create(table);
-		result = revengStrat.columnToMetaAttributes(tableIdentifier, column);
-		if (result == null) {
-			String catalog = getCatalogForModel(table.getCatalog(), defaultCatalog);
-			String schema = getSchemaForModel(table.getSchema(), defaultSchema);
-			tableIdentifier = new TableIdentifier(catalog, schema, table.getName());
-			result = revengStrat.columnToMetaAttributes(tableIdentifier, column);
-		}
-		return result;
-	}
-	
 	public static Map<String,MetaAttribute> getTableToMetaAttributesInRevengStrategy(
 			ReverseEngineeringStrategy revengStrat,
 			Table table,
@@ -108,14 +90,14 @@ public class RevEngUtils {
 	}
 
 	/** If catalog is equal to defaultCatalog then we return null so it will be null in the generated code. */
-	private static String getCatalogForModel(String catalog, String defaultCatalog) {
+	public static String getCatalogForModel(String catalog, String defaultCatalog) {
 		if(catalog==null) return null;
 		if(catalog.equals(defaultCatalog)) return null;
 		return catalog;
 	}
 
 	/** If catalog is equal to defaultSchema then we return null so it will be null in the generated code. */
-	private static String getSchemaForModel(String schema, String defaultSchema) {
+	public static String getSchemaForModel(String schema, String defaultSchema) {
 		if(schema==null) return null;
 		if(schema.equals(defaultSchema)) return null;
 		return schema;
--- tooling/hibernate-reveng/src/main/java/org/hibernate/tool/internal/reveng/binder/MetaAttributesBinder.java
+++ tooling/hibernate-reveng/src/main/java/org/hibernate/tool/internal/reveng/binder/MetaAttributesBinder.java
@@ -9,6 +9,7 @@ import org.hibernate.mapping.Property;
 import org.hibernate.mapping.Selectable;
 import org.hibernate.mapping.Table;
 import org.hibernate.tool.api.reveng.ReverseEngineeringStrategy;
+import org.hibernate.tool.api.reveng.TableIdentifier;
 import org.hibernate.tool.internal.reveng.RevEngUtils;
 
 public class MetaAttributesBinder {
@@ -22,7 +23,7 @@ public class MetaAttributesBinder {
     	Iterator<Selectable> columnIterator = property.getValue().getColumnIterator();
 		while(columnIterator.hasNext()) {
 			Column col = (Column) columnIterator.next();
-			Map<String,MetaAttribute> map = RevEngUtils.getColumnToMetaAttributesInRevengStrategy(
+			Map<String,MetaAttribute> map = getColumnToMetaAttributesInRevengStrategy(
 					revengStrategy, 
 					table, 
 					defaultCatalog, 
@@ -36,4 +37,22 @@ public class MetaAttributesBinder {
 		return property;
     }
 
+	private static Map<String,MetaAttribute> getColumnToMetaAttributesInRevengStrategy(
+			ReverseEngineeringStrategy revengStrat,
+			Table table,
+			String defaultCatalog,
+			String defaultSchema,
+			String column) {
+		Map<String,MetaAttribute> result = null;
+		TableIdentifier tableIdentifier = TableIdentifier.create(table);
+		result = revengStrat.columnToMetaAttributes(tableIdentifier, column);
+		if (result == null) {
+			String catalog = RevEngUtils.getCatalogForModel(table.getCatalog(), defaultCatalog);
+			String schema = RevEngUtils.getSchemaForModel(table.getSchema(), defaultSchema);
+			tableIdentifier = new TableIdentifier(catalog, schema, table.getName());
+			result = revengStrat.columnToMetaAttributes(tableIdentifier, column);
+		}
+		return result;
+	}
+	
 }

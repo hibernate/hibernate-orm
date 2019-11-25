d5ccc2f466cf86f0a8ce82cbc13e1f4495bd6446
Koen Aers <koen.aers@gmail.com>
2019-11-25 18:12:15 +0100

    HBX-1910: Refactor JdbcMetadataBuilder into smaller responsibilities - Extract JdbcMetadataBuilder#bindPrimaryKeyToProperties(...) into new class 'org.hibernate.tool.internal.reveng.binder.PrimaryKeyBinder'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- tooling/hibernate-reveng/src/main/java/org/hibernate/tool/internal/reveng/JdbcMetadataBuilder.java
+++ tooling/hibernate-reveng/src/main/java/org/hibernate/tool/internal/reveng/JdbcMetadataBuilder.java
@@ -6,8 +6,6 @@ package org.hibernate.tool.internal.reveng;
 
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
@@ -33,12 +31,10 @@ import org.hibernate.engine.spi.Mapping;
 import org.hibernate.internal.util.StringHelper;
 import org.hibernate.mapping.Collection;
 import org.hibernate.mapping.Column;
-import org.hibernate.mapping.Component;
 import org.hibernate.mapping.DependantValue;
 import org.hibernate.mapping.ForeignKey;
 import org.hibernate.mapping.KeyValue;
 import org.hibernate.mapping.ManyToOne;
-import org.hibernate.mapping.MetaAttribute;
 import org.hibernate.mapping.OneToMany;
 import org.hibernate.mapping.OneToOne;
 import org.hibernate.mapping.PersistentClass;
@@ -56,10 +52,9 @@ import org.hibernate.tool.api.reveng.TableIdentifier;
 import org.hibernate.tool.internal.reveng.binder.BasicPropertyBinder;
 import org.hibernate.tool.internal.reveng.binder.BinderUtils;
 import org.hibernate.tool.internal.reveng.binder.EntityPropertyBinder;
-import org.hibernate.tool.internal.reveng.binder.ForeignKeyUtils;
 import org.hibernate.tool.internal.reveng.binder.ManyToOneBinder;
+import org.hibernate.tool.internal.reveng.binder.PrimaryKeyBinder;
 import org.hibernate.tool.internal.reveng.binder.PropertyBinder;
-import org.hibernate.tool.internal.reveng.binder.SimpleValueBinder;
 import org.hibernate.tool.internal.reveng.binder.TypeUtils;
 import org.hibernate.type.ForeignKeyDirection;
 import org.jboss.logging.Logger;
@@ -204,8 +199,21 @@ public class JdbcMetadataBuilder {
 
 			Set<Column> processed = new HashSet<Column>();
 
-
-			PrimaryKeyInfo pki = bindPrimaryKeyToProperties(table, rc, processed, mapping, collector);
+			PrimaryKeyInfo pki = PrimaryKeyBinder
+					.create(
+							metadataBuildingContext, 
+							metadataCollector, 
+							revengStrategy, 
+							defaultCatalog, 
+							defaultSchema, 
+							preferBasicCompositeIds)
+					.bind(
+							table, 
+							rc, 
+							processed, 
+							mapping, 
+							collector);
+			
 			bindColumnsToVersioning(table, rc, processed, mapping);
 			bindOutgoingForeignKeys(table, rc, processed);
 			bindColumnsToProperties(table, rc, processed, mapping);
@@ -573,100 +581,6 @@ public class JdbcMetadataBuilder {
 		return true;
 	}
 
-	private PrimaryKeyInfo bindPrimaryKeyToProperties(Table table, RootClass rc, Set<Column> processed, Mapping mapping, DatabaseCollector collector) {
-		SimpleValue id = null;
-		String idPropertyname = null;
-
-		PrimaryKeyInfo pki = new PrimaryKeyInfo();
-
-		List<Column> keyColumns = null;
-		if (table.getPrimaryKey()!=null) {
-			keyColumns = table.getPrimaryKey().getColumns();
-		}
-		else {
-			log.debug("No primary key found for " + table + ", using all properties as the identifier.");
-			keyColumns = new ArrayList<Column>();
-			Iterator<?> iter = table.getColumnIterator();
-			while (iter.hasNext() ) {
-				Column col = (Column) iter.next();
-				keyColumns.add(col);
-			}
-		}
-
-		final TableIdentifier tableIdentifier = TableIdentifier.create(table);
-
-		String tableIdentifierStrategyName = "assigned";
-
-		boolean naturalId;
-
-		if (keyColumns.size()>1) {
-			log.debug("id strategy for " + rc.getEntityName() + " since it has a multiple column primary key");
-			naturalId = true;
-
-			id = handleCompositeKey(rc, processed, keyColumns, mapping);
-			idPropertyname = revengStrategy.tableToIdentifierPropertyName(tableIdentifier);
-			if(idPropertyname==null) {
-				idPropertyname = "id";
-			}
-		}
-		else {
-			pki.suggestedStrategy = RevEngUtils.getTableIdentifierStrategyNameInRevengStrategy(revengStrategy, table, defaultCatalog, defaultSchema);
-			String suggestedStrategy = pki.suggestedStrategy;
-			if(suggestedStrategy==null) {
-				suggestedStrategy = collector.getSuggestedIdentifierStrategy( tableIdentifier.getCatalog(), tableIdentifier.getSchema(), tableIdentifier.getName() );
-				if(suggestedStrategy==null) {
-					suggestedStrategy = "assigned";
-				}
-				tableIdentifierStrategyName = suggestedStrategy;
-			} else {
-				tableIdentifierStrategyName = suggestedStrategy;
-			}
-
-			naturalId = "assigned".equals( tableIdentifierStrategyName );
-			Column pkc = (Column) keyColumns.get(0);
-			BinderUtils.checkColumnForMultipleBinding(pkc);
-
-			id = SimpleValueBinder.bind(
-					metadataBuildingContext, 
-					metadataCollector, 
-					revengStrategy, 
-					table, 
-					pkc, 
-					mapping, 
-					!naturalId);
-
-			idPropertyname = revengStrategy.tableToIdentifierPropertyName(tableIdentifier);
-			if(idPropertyname==null) {
-				idPropertyname = revengStrategy.columnToPropertyName(tableIdentifier, pkc.getName() );
-			}
-
-			processed.add(pkc);
-		}
-		id.setIdentifierGeneratorStrategy(tableIdentifierStrategyName);
-		pki.suggestedProperties = revengStrategy.getTableIdentifierProperties(tableIdentifier);
-		id.setIdentifierGeneratorProperties(pki.suggestedProperties);
-		if(naturalId) {
-			id.setNullValue("undefined");
-		}
-
-		Property property = PropertyBinder.bind(
-				table, 
-				defaultCatalog,
-				defaultSchema,
-				BinderUtils.makeUnique(rc,idPropertyname), 
-				id, 
-				true, 
-				true, 
-				false, 
-				null, 
-				null,
-				revengStrategy);
-		rc.setIdentifierProperty(property);
-		rc.setIdentifier(id);
-
-		return pki;
-	}
-
 	/**
 	 * bind many-to-ones
 	 * @param table
@@ -813,93 +727,6 @@ public class JdbcMetadataBuilder {
             }
         }
         return false;
-    }
-
-	/**
-     * Basically create an [classname]Id.class and add  properties for it.
-	 * @param rc
-	 * @param compositeKeyColumns
-	 * @param processed
-	 * @return
-	 */
-	private SimpleValue handleCompositeKey(RootClass rc, Set<Column> processedColumns, List<Column> keyColumns, Mapping mapping) {
-		Component pkc = new Component(metadataBuildingContext, rc);
-        pkc.setMetaAttributes(Collections.EMPTY_MAP);
-        pkc.setEmbedded(false);
-
-        String compositeIdName = revengStrategy.tableToCompositeIdName(TableIdentifier.create(rc.getTable()));
-        if(compositeIdName==null) {
-        	compositeIdName = revengStrategy.classNameToCompositeIdName(rc.getClassName());
-        }
-        pkc.setComponentClassName(compositeIdName);
-		Table table = rc.getTable();
-        List<?> list = null;
-		if (preferBasicCompositeIds ) {
-            list = new ArrayList<Object>(keyColumns);
-        }
-		else {
-            list = ForeignKeyUtils.findForeignKeys(table.getForeignKeyIterator(), keyColumns);
-        }
-        for (Iterator<?> iter = list.iterator(); iter.hasNext();) {
-            Object element = iter.next();
-			Property property;
-            if (element instanceof Column) {
-                Column column = (Column) element;
-                if ( processedColumns.contains(column) ) {
-                    throw new RuntimeException("Binding column twice for primary key should not happen: " + column);
-                }
-				else {
-					BinderUtils.checkColumnForMultipleBinding(column);
-                    String propertyName = revengStrategy.columnToPropertyName( TableIdentifier.create(table), column.getName() );
-                    property = BasicPropertyBinder
-                    		.create(metadataBuildingContext, metadataCollector, revengStrategy, defaultCatalog, defaultSchema)
-                    		.bind(BinderUtils.makeUnique(pkc, propertyName), table, column, mapping);
-                    processedColumns.add(column);
-                }
-            }
-			else if (element instanceof ForeignKeyUtils.ForeignKeyForColumns) {
-				ForeignKeyUtils.ForeignKeyForColumns fkfc = (ForeignKeyUtils.ForeignKeyForColumns) element;
-                ForeignKey foreignKey = fkfc.key;
-                String propertyName = revengStrategy.foreignKeyToEntityName(
-						foreignKey.getName(),
-						TableIdentifier.create(foreignKey.getTable() ),
-						foreignKey.getColumns(), TableIdentifier.create(foreignKey.getReferencedTable() ), foreignKey.getReferencedColumns(), true
-					);
-                property = ManyToOneBinder
-                		.create(
-                				metadataBuildingContext, 
-                				revengStrategy,
-                				defaultCatalog, 
-                				defaultSchema)
-                		.bind(
-                				BinderUtils.makeUnique(pkc, propertyName), 
-                				true, 
-                				table, 
-                				foreignKey, 
-                				processedColumns);
-                processedColumns.addAll(fkfc.columns);
-            }
-			else {
-				throw new RuntimeException("unknown thing");
-			}
-
-            markAsUseInEquals(property);
-            pkc.addProperty(property);
-
-		}
-
-		return pkc;
-	}
-
-    /**
-     * @param property
-     */
-    private void markAsUseInEquals(Property property) {
-        Map<String, MetaAttribute> m = new HashMap<String, MetaAttribute>();
-        MetaAttribute ma = new MetaAttribute("use-in-equals");
-        ma.addValue("true");
-        m.put(ma.getName(),ma);
-        property.setMetaAttributes(m);
     }
 
  }
--- tooling/hibernate-reveng/src/main/java/org/hibernate/tool/internal/reveng/binder/BasicPropertyBinder.java
+++ tooling/hibernate-reveng/src/main/java/org/hibernate/tool/internal/reveng/binder/BasicPropertyBinder.java
@@ -1,6 +1,6 @@
 package org.hibernate.tool.internal.reveng.binder;
 
-import org.hibernate.boot.internal.InFlightMetadataCollectorImpl;
+import org.hibernate.boot.spi.InFlightMetadataCollector;
 import org.hibernate.boot.spi.MetadataBuildingContext;
 import org.hibernate.engine.spi.Mapping;
 import org.hibernate.mapping.Column;
@@ -12,14 +12,14 @@ import org.hibernate.tool.api.reveng.ReverseEngineeringStrategy;
 public class BasicPropertyBinder {
 	
 	private final MetadataBuildingContext metadataBuildingContext;	
-	private final InFlightMetadataCollectorImpl metadataCollector;	
+	private final InFlightMetadataCollector metadataCollector;	
 	private final ReverseEngineeringStrategy revengStrategy;
 	private final String defaultCatalog;
 	private final String defaultSchema;
 	
 	public static BasicPropertyBinder create(
 			MetadataBuildingContext metadataBuildingContext,
-			InFlightMetadataCollectorImpl metadataCollector,
+			InFlightMetadataCollector metadataCollector,
 			ReverseEngineeringStrategy revengStrategy,
 			String defaultCatalog,
 			String defaultSchema) {
@@ -33,7 +33,7 @@ public class BasicPropertyBinder {
 	
 	private BasicPropertyBinder(
 			MetadataBuildingContext metadataBuildingContext,
-			InFlightMetadataCollectorImpl metadataCollector,
+			InFlightMetadataCollector metadataCollector,
 			ReverseEngineeringStrategy revengStrategy,
 			String defaultCatalog,
 			String defaultSchema) {

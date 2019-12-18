c17dba180c8e23a472782aff5e627e978ae7313a
Adam Pantel <adam@cockroachlabs.com>
2019-12-18 13:37:51 -0500

    partial identity column support

--- hibernate-core/src/main/java/org/hibernate/dialect/identity/CockroachDB1920IdentityColumnSupport.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/identity/CockroachDB1920IdentityColumnSupport.java
@@ -6,10 +6,30 @@
  */
 package org.hibernate.dialect.identity;
 
+import java.sql.Types;
+
 public class CockroachDB1920IdentityColumnSupport extends IdentityColumnSupportImpl {
     @Override
     public boolean supportsIdentityColumns() {
         // Full support requires setting the sql.defaults.serial_normalization=sql_sequence in CockroachDB.
+        return false;
+    }
+
+    @Override
+    // CockroachDB does not create a sequence for id columns
+    public String getIdentitySelectString(String table, String column, int type) {
+        return "select 1";
+    }
+
+    @Override
+    public String getIdentityColumnString(int type) {
+        return type == Types.SMALLINT ?
+                "serial4 not null" :
+                "serial8 not null";
+    }
+
+    @Override
+    public boolean hasDataTypeInIdentityColumn() {
         return false;
     }
 }

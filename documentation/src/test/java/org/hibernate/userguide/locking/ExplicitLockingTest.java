82adbc76f22fa864bca686baa24b39f09b733875
Nathan Xu <nathan_xu@ultimatesoftware.com>
2020-01-11 22:35:53 -0500

    HHH-13809 finish 'locking' chapter

--- documentation/src/test/java/org/hibernate/userguide/locking/ExplicitLockingTest.java
+++ documentation/src/test/java/org/hibernate/userguide/locking/ExplicitLockingTest.java
@@ -149,36 +149,36 @@ public class ExplicitLockingTest extends BaseEntityManagerFunctionalTestCase {
 		doInJPA( this::entityManagerFactory, entityManager -> {
 			//tag::locking-follow-on-example[]
 			List<Person> persons = entityManager.createQuery(
-				"select DISTINCT p from Person p", Person.class)
-			.setLockMode( LockModeType.PESSIMISTIC_WRITE )
-			.getResultList();
+					"select DISTINCT p from Person p", Person.class)
+				.setLockMode( LockModeType.PESSIMISTIC_WRITE )
+				.getResultList();
 			//end::locking-follow-on-example[]
 		} );
 
 		doInJPA( this::entityManagerFactory, entityManager -> {
 			//tag::locking-follow-on-secondary-query-example[]
 			List<Person> persons = entityManager.createQuery(
-				"select DISTINCT p from Person p", Person.class)
-			.getResultList();
+					"select DISTINCT p from Person p", Person.class)
+				.getResultList();
 
 			entityManager.createQuery(
-				"select p.id from Person p where p in :persons")
-			.setLockMode( LockModeType.PESSIMISTIC_WRITE )
-			.setParameter( "persons", persons )
-			.getResultList();
+					"select p.id from Person p where p in :persons")
+				.setLockMode( LockModeType.PESSIMISTIC_WRITE )
+				.setParameter( "persons", persons )
+				.getResultList();
 			//end::locking-follow-on-secondary-query-example[]
 		} );
 
 		doInJPA( this::entityManagerFactory, entityManager -> {
 			//tag::locking-follow-on-explicit-example[]
 			List<Person> persons = entityManager.createQuery(
-				"select p from Person p", Person.class)
-			.setMaxResults( 10 )
-			.unwrap( Query.class )
-			.setLockOptions(
-				new LockOptions( LockMode.PESSIMISTIC_WRITE )
-					.setFollowOnLocking( false ) )
-			.getResultList();
+					"select p from Person p", Person.class)
+				.setMaxResults( 10 )
+				.unwrap( Query.class )
+				.setLockOptions(
+					new LockOptions( LockMode.PESSIMISTIC_WRITE )
+						.setFollowOnLocking( false ) )
+				.getResultList();
 			//end::locking-follow-on-explicit-example[]
 		} );
 	}

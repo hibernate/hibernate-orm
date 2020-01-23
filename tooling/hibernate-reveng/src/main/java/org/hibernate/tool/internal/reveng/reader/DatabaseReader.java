204bf714eb7cebff3acab8ae1e8608dce93da6be
Koen Aers <koen.aers@gmail.com>
2020-01-23 09:28:19 +0100

    HBX-1964: Improve implementation of class 'org.hibernate.tool.internal.reveng.reader.DatabaseReader' - Reorganize imports
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- tooling/hibernate-reveng/src/main/java/org/hibernate/tool/internal/reveng/reader/DatabaseReader.java
+++ tooling/hibernate-reveng/src/main/java/org/hibernate/tool/internal/reveng/reader/DatabaseReader.java
@@ -1,9 +1,5 @@
 package org.hibernate.tool.internal.reveng.reader;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;

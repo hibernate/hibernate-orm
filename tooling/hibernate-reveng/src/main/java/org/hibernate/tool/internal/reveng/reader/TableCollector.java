54aebbc296b1b67d6726db17c9a13652c92ac228
Koen Aers <koen.aers@gmail.com>
2020-02-07 12:34:35 +0100

    HBX-1965: Improve implementation of class 'org.hibernate.tool.internal.reveng.reader.TableCollector' - Modularize TableCollector#processTable(...)
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- tooling/hibernate-reveng/src/main/java/org/hibernate/tool/internal/reveng/reader/TableCollector.java
+++ tooling/hibernate-reveng/src/main/java/org/hibernate/tool/internal/reveng/reader/TableCollector.java
@@ -49,50 +49,62 @@ public class TableCollector {
 		  Iterator<Map<String,Object>> tableIterator = null;
 		  HashMap<Table, Boolean> processedTables = new HashMap<Table, Boolean>();
 		  try {			  
-		     String matchCatalog = StringHelper.replace(schemaSelection.getMatchCatalog(),".*", "%");
-		     String matchSchema = StringHelper.replace(schemaSelection.getMatchSchema(),".*", "%");
-		     String matchTable = StringHelper.replace(schemaSelection.getMatchTable(),".*", "%");
-		     tableIterator = metaDataDialect.getTables(matchCatalog, matchSchema, matchTable);
+		     tableIterator = metaDataDialect.getTables(
+		    		 StringHelper.replace(schemaSelection.getMatchCatalog(),".*", "%"), 
+		    		 StringHelper.replace(schemaSelection.getMatchSchema(),".*", "%"), 
+		    		 StringHelper.replace(schemaSelection.getMatchTable(),".*", "%"));
 		     while (tableIterator.hasNext() ) {
-		    	Map<String,Object> tableRs = tableIterator.next();
-		        String tableName = (String) tableRs.get("TABLE_NAME");
-				String schemaName = (String) tableRs.get("TABLE_SCHEM");
-		        String catalogName = (String) tableRs.get("TABLE_CAT");
-		        TableIdentifier tableIdentifier = TableIdentifier.create(quote(catalogName), quote(schemaName), quote(tableName));		        
-				if(revengStrategy.excludeTable(tableIdentifier) ) {
-					log.debug("Table " + tableIdentifier + " excluded by strategy");
-		        	continue;
-		        }								
-				String comment = (String) tableRs.get("REMARKS");
-				String tableType = (String) tableRs.get("TABLE_TYPE");
-				if(revengMetadataCollector.getTable(tableIdentifier)!=null) {
-					  log.debug("Ignoring " + tableName + " since it has already been processed");
-					  continue;
-				  } else {
-					  if ( ("TABLE".equalsIgnoreCase(tableType) || "VIEW".equalsIgnoreCase(tableType) || "SYNONYM".equals(tableType) ) ) { //||
-						  log.debug("Adding table " + tableName + " of type " + tableType);
-						  Table table = revengMetadataCollector.addTable(tableIdentifier);
-						  table.setComment(comment);
-						  if(tableType.equalsIgnoreCase("TABLE")) {
-							  processedTables.put(table, true);
-						  } else 
-							  processedTables.put(table, false );
-					  }
-					  else {
-						  log.debug("Ignoring table " + tableName + " of type " + tableType);
-					  }
-				  }
+		    	processTable(tableIterator.next(), processedTables);
 		     }
 		  } 
 		  finally {
-			  try {
-				  if (tableIterator!=null) metaDataDialect.close(tableIterator);
-			  } 
-			  catch (Exception ignore) {
+			  if (tableIterator!=null) {
+				  metaDataDialect.close(tableIterator);
 			  }
 		  }
 		  return processedTables;
 	}
+	
+	private void processTable(Map<String, Object> tableRs, HashMap<Table, Boolean> processedTables) {
+        TableIdentifier tableIdentifier = TableIdentifier.create(
+        		quote((String) tableRs.get("TABLE_CAT")), 
+        		quote((String) tableRs.get("TABLE_SCHEM")), 
+        		quote((String) tableRs.get("TABLE_NAME")));		        
+		if(revengStrategy.excludeTable(tableIdentifier) ) {
+			log.debug("Table " + tableIdentifier + " excluded by strategy");
+        } else if (revengMetadataCollector.getTable(tableIdentifier)!=null)	{
+        	log.debug("Ignoring " + tableIdentifier + " since it has already been processed");
+        } else {
+        	addTable(
+        			tableIdentifier, 
+        			(String) tableRs.get("TABLE_TYPE"), 
+        			(String) tableRs.get("REMARKS"), 
+        			processedTables);
+         }	
+	}
+	
+	private void addTable(
+			TableIdentifier tableIdentifier, 
+			String tableType, 
+			String comment, 
+			HashMap<Table, Boolean> processedTables) {
+    	if (isTypeToAdd(tableType)) { //||
+    		log.debug("Adding table " + tableIdentifier + " of type " + tableType);
+    		Table table = revengMetadataCollector.addTable(tableIdentifier);
+    		table.setComment(comment);
+    		processedTables.put(table, tableType.equalsIgnoreCase("TABLE"));
+    	}
+    	else {
+    		log.debug("Ignoring table " + tableIdentifier + " of type " + tableType);
+    	}
+	}
+	
+	private boolean isTypeToAdd(String tableType) {
+		return "TABLE".equalsIgnoreCase(tableType) || 
+				"VIEW".equalsIgnoreCase(tableType) || 
+				"SYNONYM".equals(tableType);
+	}
+	
 	
 	private String quote(String name) {
 		if (name == null)

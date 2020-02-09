a9b98bf7ff82f2995951803ce1cdbbcdcef41b75
Andrea Boriero <andrea@hibernate.org>
2020-02-09 11:52:12 +0000

    HHH-13856 Fix Oracle failing tests

--- hibernate-core/src/test/java/org/hibernate/jpa/test/graphs/mappedbyid/LoadGraphFindByIdTest.java
+++ hibernate-core/src/test/java/org/hibernate/jpa/test/graphs/mappedbyid/LoadGraphFindByIdTest.java
@@ -4,7 +4,7 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.jpa.test.graphs.mapped_by_id;
+package org.hibernate.jpa.test.graphs.mappedbyid;
 
 import java.util.HashMap;
 import java.util.Map;

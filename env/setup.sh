cceddf6f5f95bd7c51d69b0d4cbe45e2a8186605
Eusebio Resende <eresende@nuodb.com>
2020-02-19 17:59:31 +0000

    Improve environment setup script

--- env/setup.sh
+++ env/setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(cd `dirname $0` && pwd)
 IMG_NAME=nuodb/nuodb-ce:4.0.4
-rm -fr vol1 vol2 && mkdir vol1 vol2 && chmod a+rw vol1 vol2
+rm -fr "$SCRIPT_DIR"/{vol1,vol2}
+mkdir "$SCRIPT_DIR"/{vol1,vol2}
+chmod a+rw "$SCRIPT_DIR"/{vol1,vol2}
 
 docker network create nuodb-net
 
@@ -14,13 +17,13 @@ docker run -d --name ad1 --rm \
     -p 48004:48004 \
     -p 48005:48005 \
     -e "NUODB_DOMAIN_ENTRYPOINT=ad1" \
-    ${IMG_NAME} nuoadmin
+    "${IMG_NAME}" nuoadmin
 
 sleep 3
 docker run -d --name sm1 --hostname sm1 --rm \
-    --volume $(pwd)/vol1:/var/opt/nuodb/backup \
-    --volume $(pwd)/vol2:/var/opt/nuodb/archive \
-    --net nuodb-net ${IMG_NAME} nuodocker \
+    --volume "$SCRIPT_DIR/vol1":/var/opt/nuodb/backup \
+    --volume "$SCRIPT_DIR/vol2":/var/opt/nuodb/archive \
+    --net nuodb-net "${IMG_NAME}" nuodocker \
     --api-server ad1:8888 start sm \
     --db-name hibernate_orm_test --server-id ad1 \
     --dba-user hibernate_orm_test --dba-password hibernate_orm_test \
@@ -29,10 +32,10 @@ docker run -d --name sm1 --hostname sm1 --rm \
 
 sleep 3
 docker run -d --name te1 --hostname te1 --rm \
-    --net nuodb-net ${IMG_NAME} nuodocker \
+    --net nuodb-net "${IMG_NAME}" nuodocker \
     --api-server ad1:8888 start te \
     --db-name hibernate_orm_test --server-id ad1
 
 sleep 5
-docker run -t --net nuodb-net ${IMG_NAME} \
+docker run -t --net nuodb-net "${IMG_NAME}" \
     nuocmd --api-server ad1:8888 show domain

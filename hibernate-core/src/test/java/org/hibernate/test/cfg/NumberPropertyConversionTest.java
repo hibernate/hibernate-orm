78b23b7c066637c85a650e36a615858ca4e5339b
Cedomir Igaly <cedomir.igaly@e-dama.net>
2020-03-07 09:14:57 +0100

    HHH-13893 - Each instance of java.lang.Number can be converted to long or int by user longValue or intValue respectively.

--- hibernate-core/src/test/java/org/hibernate/test/cfg/NumberPropertyConversionTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/cfg/NumberPropertyConversionTest.java
@@ -2,6 +2,7 @@ package org.hibernate.test.cfg;
 
 import java.util.Collections;
 
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Environment;
 import org.hibernate.engine.jdbc.spi.JdbcServices;
 import org.hibernate.service.ServiceRegistry;
@@ -18,5 +19,6 @@ public class NumberPropertyConversionTest extends BaseUnitTestCase {
 				Collections.singletonMap( Environment.LOG_SLOW_QUERY, 25 )
 		);
 		sr.getService( JdbcServices.class );
+		StandardServiceRegistryBuilder.destroy( sr );
 	}
 }

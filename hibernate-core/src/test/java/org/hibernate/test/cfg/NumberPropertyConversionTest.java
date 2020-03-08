58cdb2468cd80f0d57f35c1ce5ee0cb39e31150b
Cedomir Igaly <cedomir.igaly@e-dama.net>
2020-03-08 08:40:51 +0100

    Added test case PropertiesHelperTest.testIntLongConversion

--- hibernate-core/src/test/java/org/hibernate/test/cfg/NumberPropertyConversionTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/cfg/NumberPropertyConversionTest.java
@@ -3,20 +3,22 @@ package org.hibernate.test.cfg;
 import java.util.Collections;
 
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Environment;
+import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.engine.jdbc.spi.JdbcServices;
 import org.hibernate.service.ServiceRegistry;
 
+import org.hibernate.testing.TestForIssue;
 import org.hibernate.testing.boot.ServiceRegistryTestingImpl;
 import org.hibernate.testing.junit4.BaseUnitTestCase;
 import org.junit.Test;
 
+@TestForIssue(jiraKey = "HHH-13893")
 public class NumberPropertyConversionTest extends BaseUnitTestCase {
 
 	@Test
 	public void testIntegerToLongConversion() {
 		ServiceRegistry sr = ServiceRegistryTestingImpl.forUnitTesting(
-				Collections.singletonMap( Environment.LOG_SLOW_QUERY, 25 )
+				Collections.singletonMap( AvailableSettings.LOG_SLOW_QUERY, Integer.valueOf( 25 ) )
 		);
 		sr.getService( JdbcServices.class );
 		StandardServiceRegistryBuilder.destroy( sr );

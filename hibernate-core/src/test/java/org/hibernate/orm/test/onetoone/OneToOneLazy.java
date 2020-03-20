38eb64e5e9420e8b1001bff66fb0c4d827d248e7
Andrea Boriero <andrea@hibernate.org>
2020-03-20 12:37:25 +0000

    Fix ToOne @Fetch(FetchMode.SELECT)

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/onetoone/OneToOneLazy.java
@@ -0,0 +1,138 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
+package org.hibernate.orm.test.onetoone;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import org.hibernate.Hibernate;
+
+import org.hibernate.testing.orm.junit.DomainModel;
+import org.hibernate.testing.orm.junit.SessionFactory;
+import org.hibernate.testing.orm.junit.SessionFactoryScope;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * @author Andrea Boriero
+ */
+@DomainModel(
+		annotatedClasses = {
+				OneToOneLazy.Title.class,
+				OneToOneLazy.Book.class
+		}
+)
+@SessionFactory
+public class OneToOneLazy {
+
+	@BeforeEach
+	public void setUp(SessionFactoryScope scope) {
+		scope.inTransaction(
+				sesison -> {
+					Title title = new Title( 1L );
+					Book book = new Book( 2L, title );
+
+					sesison.save( title );
+					sesison.save( book );
+				}
+		);
+	}
+
+	@Test
+	public void testLazyLoading(SessionFactoryScope scope) {
+		scope.inTransaction(
+				session -> {
+					Book book = session.find( Book.class, 2L );
+					assertThat( Hibernate.isInitialized( book.getTitle() ), is( false ) );
+				}
+		);
+
+		scope.inTransaction(
+				session -> {
+					Title title = session.find( Title.class, 1L );
+					assertThat( Hibernate.isInitialized( title.getBook() ), is( true ) );
+
+				}
+		);
+	}
+
+	@Entity
+	public static class Book {
+
+		@Id
+		private Long id;
+
+		@OneToOne(fetch = FetchType.LAZY)
+		private Title title;
+
+		public Book() {
+		}
+
+		public Book(Long id, Title title) {
+			this.id = id;
+			this.title = title;
+			title.setBook( this );
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public Title getTitle() {
+			return title;
+		}
+	}
+
+	@Entity
+	public static class Title {
+
+		@Id
+		private Long id;
+
+		@OneToOne(fetch = FetchType.LAZY, mappedBy = "title")
+		private Book book;
+
+		public Title() {
+		}
+
+		public Title(Long id) {
+			this.id = id;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public Book getBook() {
+			return book;
+		}
+
+		public void setBook(Book book) {
+			this.book = book;
+		}
+	}
+}

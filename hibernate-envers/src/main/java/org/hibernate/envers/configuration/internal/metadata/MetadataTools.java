14359b4070d82624afa2ffc104abd879f3c051b5
Chris Cranford <chris@hibernate.org>
2020-04-01 16:56:57 -0400

    HHH-13886 Revert behavior changes introduced by HHH-10844

--- hibernate-envers/src/main/java/org/hibernate/envers/configuration/internal/metadata/MetadataTools.java
+++ hibernate-envers/src/main/java/org/hibernate/envers/configuration/internal/metadata/MetadataTools.java
@@ -9,10 +9,6 @@ package org.hibernate.envers.configuration.internal.metadata;
 import java.util.Iterator;
 import javax.persistence.JoinColumn;
 
-import org.hibernate.boot.Metadata;
-import org.hibernate.dialect.Dialect;
-import org.hibernate.engine.spi.Mapping;
-import org.hibernate.envers.internal.EnversMessageLogger;
 import org.hibernate.envers.internal.tools.StringTools;
 import org.hibernate.mapping.Column;
 import org.hibernate.mapping.Formula;
@@ -22,19 +18,12 @@ import org.dom4j.Attribute;
 import org.dom4j.Document;
 import org.dom4j.Element;
 
-import org.jboss.logging.Logger;
-
 /**
  * @author Adam Warski (adam at warski dot org)
  * @author Lukasz Antoniak (lukasz dot antoniak at gmail dot com)
  * @author Michal Skowronek (mskowr at o2 dot pl)
  */
 public final class MetadataTools {
-
-	private static final EnversMessageLogger LOG = Logger.getMessageLogger(
-			EnversMessageLogger.class,
-			MetadataTools.class.getName()
-	);
 
 	private MetadataTools() {
 	}
@@ -297,17 +286,13 @@ public final class MetadataTools {
 		return joinMapping;
 	}
 
-	public static void addColumns(Element anyMapping, Iterator<?> selectables, Metadata metadata) {
-		addColumns( anyMapping, selectables, metadata, metadata.getDatabase().getDialect() );
-	}
-
-	public static void addColumns(Element anyMapping, Iterator<?> selectables, Mapping mapping, Dialect dialect) {
+	public static void addColumns(Element anyMapping, Iterator<?> selectables) {
 		while ( selectables.hasNext() ) {
 			final Selectable selectable = (Selectable) selectables.next();
 			if ( selectable.isFormula() ) {
 				throw new FormulaNotSupportedException();
 			}
-			addColumn( anyMapping, (Column) selectable, mapping, dialect );
+			addColumn( anyMapping, (Column) selectable );
 		}
 	}
 
@@ -326,37 +311,19 @@ public final class MetadataTools {
 	 *
 	 * @param anyMapping parent element
 	 * @param column column descriptor
-	 * @param mapping the metadata mapping
-	 * @param dialect the dialect
 	 */
-	public static void addColumn(Element anyMapping, Column column, Mapping mapping, Dialect dialect) {
+	public static void addColumn(Element anyMapping, Column column) {
 		addColumn(
 				anyMapping,
 				column.getName(),
 				column.getLength(),
 				column.getScale(),
 				column.getPrecision(),
-				resolveSqlType( column, mapping, dialect ),
+				column.getSqlType(),
 				column.getCustomRead(),
 				column.getCustomWrite(),
 				column.isQuoted()
 		);
-	}
-
-	private static String resolveSqlType(Column column, Mapping mapping, Dialect dialect) {
-		String columnDefinition = column.getSqlType();
-		if ( !StringTools.isEmpty( columnDefinition ) ) {
-			final int sqlTypeCode = column.getSqlTypeCode( mapping );
-			final String sqlType = dialect.getTypeName( sqlTypeCode, column.getLength(), column.getPrecision(), column.getScale() );
-			LOG.infof(
-					"Column [%s] uses a column-definition of [%s], resolved sql-type as [%s].",
-					column.getName(),
-					columnDefinition,
-					sqlType
-			);
-			columnDefinition = sqlType;
-		}
-		return columnDefinition;
 	}
 
 	@SuppressWarnings({"unchecked"})
@@ -434,13 +401,12 @@ public final class MetadataTools {
 	 * @param element Parent element.
 	 * @param columnIterator Iterator pointing at {@link org.hibernate.mapping.Column} and/or
 	 * {@link org.hibernate.mapping.Formula} objects.
-	 * @param metadata The boot-time entity model metadata
 	 */
-	public static void addColumnsOrFormulas(Element element, Iterator columnIterator, Metadata metadata) {
+	public static void addColumnsOrFormulas(Element element, Iterator columnIterator) {
 		while ( columnIterator.hasNext() ) {
 			final Object o = columnIterator.next();
 			if ( o instanceof Column ) {
-				addColumn( element, (Column) o, metadata, metadata.getDatabase().getDialect() );
+				addColumn( element, (Column) o );
 			}
 			else if ( o instanceof Formula ) {
 				addFormula( element, (Formula) o );

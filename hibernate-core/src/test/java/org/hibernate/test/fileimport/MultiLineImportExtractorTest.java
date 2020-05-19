c1254cc205ac7262844b3207d1eadc026cc92798
Steve Ebersole <steve@hibernate.org>
2020-05-19 12:25:06 -0500

    HHH-14033 SQL script parsing problem with multi-line comments
    - Better handling of multi-line comments
    - Restructured some internal classes to consolidate packages
    - Added "system"-style SchemaToolingLogging

---
+++ hibernate-core/src/test/java/org/hibernate/test/fileimport/MultiLineImportExtractorTest.java
@@ -0,0 +1,59 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.test.fileimport;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.hibernate.tool.hbm2ddl.MultipleLinesSqlCommandExtractor;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * @author Steve Ebersole
+ */
+public class MultiLineImportExtractorTest {
+	public static final String IMPORT_FILE = "org/hibernate/test/fileimport/multi-line-statements.sql";
+
+	private final MultipleLinesSqlCommandExtractor extractor = new MultipleLinesSqlCommandExtractor();
+
+	@Test
+	public void testExtraction() throws IOException {
+		final ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+
+		try ( final InputStream stream = classLoader.getResourceAsStream( IMPORT_FILE ) ) {
+			assertThat( stream, notNullValue() );
+			try ( final InputStreamReader reader = new InputStreamReader( stream ) ) {
+				final String[] commands = extractor.extractCommands( reader );
+				assertThat( commands, notNullValue() );
+				assertThat( commands.length, is( 6 ) );
+
+				assertThat( commands[0], startsWith( "CREATE TABLE test_data" ) );
+
+				assertThat( commands[1], is( "INSERT INTO test_data VALUES (1, 'sample')" ) );
+
+				assertThat( commands[2], is( "DELETE  FROM test_data" ) );
+
+				assertThat( commands[3], startsWith( "INSERT INTO test_data VALUES (2," ) );
+				assertThat( commands[3], containsString( "-- line 2" ) );
+
+				assertThat( commands[4], startsWith( "INSERT INTO test_data VALUES (3" ) );
+				assertThat( commands[4], not( containsString( "third record" ) ) );
+
+				assertThat( commands[5], containsString( "INSERT INTO test_data (id, text)" ) );
+			}
+		}
+	}
+}

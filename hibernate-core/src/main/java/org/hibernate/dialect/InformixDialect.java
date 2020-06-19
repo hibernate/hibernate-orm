2517965c09eaa2be9fc4faff8def93c17a7232b2
a10804 <kuruc.v@chemosvit.sk>
2020-06-19 14:53:13 +0200

    HHH-14080 Informix union all support

--- hibernate-core/src/main/java/org/hibernate/dialect/InformixDialect.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/InformixDialect.java
@@ -312,4 +312,19 @@ public class InformixDialect extends Dialect {
 	public String toBooleanValueString(boolean bool) {
 		return bool ? "'t'" : "'f'";
 	}
+	@Override
+	public boolean supportsUnionAll() {
+		return true;
+	}
+
+	@Override
+	public String getSelectClauseNullString(int sqlType) {
+		String typeName = getTypeName( sqlType, 1, 1, 0 );
+		//trim off the length/precision/scale
+		final int loc = typeName.indexOf( '(' );
+		if ( loc > -1 ) {
+			typeName = typeName.substring( 0, loc );
+		}
+		return "null::" + typeName;
+	}
 }

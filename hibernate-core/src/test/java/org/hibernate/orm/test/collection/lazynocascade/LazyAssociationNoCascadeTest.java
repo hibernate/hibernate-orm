dca850f5add24892722b6fd471076990911c7f3d
Andrea Boriero <andrea@hibernate.org>
2020-06-24 16:37:23 +0100

    Re-enabled additional tests

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/collection/lazynocascade/LazyAssociationNoCascadeTest.java
@@ -0,0 +1,65 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.orm.test.collection.lazynocascade;
+
+import org.hibernate.testing.orm.junit.DomainModel;
+import org.hibernate.testing.orm.junit.SessionFactory;
+import org.hibernate.testing.orm.junit.SessionFactoryScope;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author Vasily Kochnev
+ */
+@DomainModel(
+		xmlMappings = "org/hibernate/orm/test/collection/lazynocascade/Parent.hbm.xml"
+)
+@SessionFactory
+public class LazyAssociationNoCascadeTest {
+
+	@Test
+	public void testNoCascadeCache(SessionFactoryScope scope) {
+		Parent parent = new Parent();
+
+		BaseChild firstChild = new BaseChild();
+		parent.getChildren().add( firstChild );
+
+		Parent mergedParent = scope.fromSession(
+				session -> {
+					session.beginTransaction();
+					try {
+						session.save( parent );
+						session.getTransaction().commit();
+						session.clear();
+
+						Child secondChild = new Child();
+						secondChild.setName( "SecondChildName" );
+						parent.getChildren().add( secondChild );
+
+						firstChild.setDependency( secondChild );
+
+						session.beginTransaction();
+						Parent merged = (Parent) session.merge( parent );
+						session.getTransaction().commit();
+						return merged;
+					}
+					catch (Exception exception) {
+						if ( session.getTransaction().isActive() ) {
+							session.getTransaction().rollback();
+						}
+						throw exception;
+					}
+
+				}
+		);
+
+		assertNotNull( mergedParent );
+		assertEquals( mergedParent.getChildren().size(), 2 );
+	}
+}

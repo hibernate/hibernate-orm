2ef7a8c1cfe39a700bf6cb4df9a6ebabf2c36e31
Sanne Grinovero <sanne@hibernate.org>
2020-07-03 13:01:18 +0100

    HHH-14096 Adapt the Hibernate Envers module to use its private copy of XMLHelper

--- hibernate-envers/src/main/java/org/hibernate/envers/boot/internal/EnversServiceImpl.java
+++ hibernate-envers/src/main/java/org/hibernate/envers/boot/internal/EnversServiceImpl.java
@@ -30,7 +30,6 @@ import org.hibernate.envers.internal.tools.ReflectionTools;
 import org.hibernate.envers.strategy.AuditStrategy;
 import org.hibernate.internal.util.ReflectHelper;
 import org.hibernate.internal.util.config.ConfigurationHelper;
-import org.hibernate.internal.util.xml.XMLHelper;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.spi.Configurable;
 import org.hibernate.service.spi.Stoppable;
@@ -74,8 +73,6 @@ public class EnversServiceImpl implements EnversService, Configurable, Stoppable
 	private RevisionInfoNumberReader revisionInfoNumberReader;
 	private ModifiedEntityNamesReader modifiedEntityNamesReader;
 
-	private XMLHelper xmlHelper;
-
 	@Override
 	public void configure(Map configurationValues) {
 		if ( configurationValues.containsKey( LEGACY_AUTO_REGISTER ) ) {
@@ -111,7 +108,6 @@ public class EnversServiceImpl implements EnversService, Configurable, Stoppable
 
 		this.serviceRegistry = metadata.getMetadataBuildingOptions().getServiceRegistry();
 		this.classLoaderService = serviceRegistry.getService( ClassLoaderService.class );
-		this.xmlHelper = new XMLHelper();
 
 		doInitialize( metadata, mappingCollector, serviceRegistry );
 	}
@@ -184,11 +180,6 @@ public class EnversServiceImpl implements EnversService, Configurable, Stoppable
 		strategy.postInitialize( revisionInfoClass, revisionInfoTimestampData, serviceRegistry );
 
 		return strategy;
-	}
-
-	@Override
-	public XMLHelper getXmlHelper() {
-		return xmlHelper;
 	}
 
 	/**

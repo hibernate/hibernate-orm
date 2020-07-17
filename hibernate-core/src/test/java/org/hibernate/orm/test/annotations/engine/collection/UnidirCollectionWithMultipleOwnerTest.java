cb2a2bbd58ef394d9f086e5193b864cd6469a47c
Andrea Boriero <andrea@hibernate.org>
2020-07-17 10:36:17 +0100

    Re-enabled additional tests

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/annotations/engine/collection/UnidirCollectionWithMultipleOwnerTest.java
@@ -0,0 +1,70 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.orm.test.annotations.engine.collection;
+
+import org.hibernate.Transaction;
+
+import org.hibernate.testing.orm.junit.DomainModel;
+import org.hibernate.testing.orm.junit.SessionFactory;
+import org.hibernate.testing.orm.junit.SessionFactoryScope;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Emmanuel Bernard
+ */
+@DomainModel(
+		annotatedClasses = {
+				Father.class,
+				Mother.class,
+				Son.class
+		}
+)
+@SessionFactory
+public class UnidirCollectionWithMultipleOwnerTest {
+
+	@Test
+	public void testUnidirCollectionWithMultipleOwner(SessionFactoryScope scope) {
+		scope.inSession(
+				session -> {
+					Transaction tx = session.beginTransaction();
+					try {
+						Father father = new Father();
+						Mother mother = new Mother();
+						session.save( father );
+						//s.save( mother );
+						Son son = new Son();
+						father.getOrderedSons().add( son );
+						son.setFather( father );
+						mother.getSons().add( son );
+						son.setMother( mother );
+						session.save( mother );
+						session.save( father );
+						tx.commit();
+
+						session.clear();
+
+						tx = session.beginTransaction();
+						son = session.get( Son.class, son.getId() );
+						session.delete( son );
+						session.flush();
+						father = session.get( Father.class, father.getId() );
+						mother = session.get( Mother.class, mother.getId() );
+						session.delete( father );
+						session.delete( mother );
+						tx.commit();
+					}
+					catch (Exception e) {
+						if ( tx.isActive() ) {
+							tx.rollback();
+						}
+						throw e;
+					}
+				}
+		);
+
+	}
+}

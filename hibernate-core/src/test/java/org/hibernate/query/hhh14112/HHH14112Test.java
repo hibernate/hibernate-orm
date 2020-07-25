b57ad86500bfd90076ea549f620015000441247e
Andrea Boriero <andrea@hibernate.org>
2020-07-25 15:31:52 +0100

    HHH-13724 Fix failing tests

--- hibernate-core/src/test/java/org/hibernate/query/hhh14112/HHH14112Test.java
+++ hibernate-core/src/test/java/org/hibernate/query/hhh14112/HHH14112Test.java
@@ -36,7 +36,7 @@ public class HHH14112Test extends BaseCoreFunctionalTestCase {
 
 	@Entity(name = "Super")
 	@Inheritance(strategy = InheritanceType.JOINED)
-	@Where(clause = "DELETED = 0")
+	@Where(clause = "DELETED = false")
 	public static class Super {
 		@Id
 		@GeneratedValue(strategy = GenerationType.AUTO)

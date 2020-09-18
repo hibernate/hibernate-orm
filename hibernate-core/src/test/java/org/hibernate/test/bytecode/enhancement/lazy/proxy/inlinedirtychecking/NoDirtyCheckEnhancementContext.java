d768609e7919b5bf1548982a4c247f367b7366b6
Andrea Boriero <andrea@hibernate.org>
2020-09-18 16:29:35 +0100

    HHH-14220 Add test for issue

--- hibernate-core/src/test/java/org/hibernate/test/bytecode/enhancement/lazy/proxy/inlinedirtychecking/NoDirtyCheckEnhancementContext.java
+++ hibernate-core/src/test/java/org/hibernate/test/bytecode/enhancement/lazy/proxy/inlinedirtychecking/NoDirtyCheckEnhancementContext.java
@@ -7,13 +7,14 @@
 package org.hibernate.test.bytecode.enhancement.lazy.proxy.inlinedirtychecking;
 
 import org.hibernate.bytecode.enhance.spi.UnloadedClass;
+import org.hibernate.bytecode.enhance.spi.UnloadedField;
 
 import org.hibernate.testing.bytecode.enhancement.EnhancerTestContext;
 
 /**
  * @author Andrea Boriero
  */
-public class NoDirtyCheckEnhancementContext  extends EnhancerTestContext {
+public class NoDirtyCheckEnhancementContext extends EnhancerTestContext {
 	@Override
 	public boolean doDirtyCheckingInline(UnloadedClass classDescriptor) {
 		return false;
@@ -21,6 +22,11 @@ public class NoDirtyCheckEnhancementContext  extends EnhancerTestContext {
 
 	@Override
 	public boolean doExtendedEnhancement(UnloadedClass classDescriptor) {
+		return false;
+	}
+
+	@Override
+	public boolean doBiDirectionalAssociationManagement(UnloadedField field) {
 		return false;
 	}
 }

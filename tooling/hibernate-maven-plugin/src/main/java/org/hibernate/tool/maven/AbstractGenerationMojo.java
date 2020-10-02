29f28caf3f1e80dbb866bf6028de6719eacaa994
Koen Aers <koen.aers@gmail.com>
2020-10-02 15:03:25 +0200

    HBX-1893: Maven plugin blows up when propertyFile is not configured while it should be optional
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- tooling/hibernate-maven-plugin/src/main/java/org/hibernate/tool/maven/AbstractGenerationMojo.java
+++ tooling/hibernate-maven-plugin/src/main/java/org/hibernate/tool/maven/AbstractGenerationMojo.java
@@ -86,9 +86,11 @@ public abstract class AbstractGenerationMojo extends AbstractMojo {
     public void execute() {
         getLog().info("Starting " + this.getClass().getSimpleName() + "...");
         RevengStrategy strategy = setupReverseEngineeringStrategy();
-        Properties properties = loadPropertiesFile();
-        MetadataDescriptor jdbcDescriptor = createJdbcDescriptor(strategy, properties);
-        executeExporter(jdbcDescriptor);
+        if (propertyFile.exists()) {
+        	executeExporter(createJdbcDescriptor(strategy, loadPropertiesFile()));
+        } else {
+        	getLog().info("Property file '" + propertyFile + "' cannot be found, aborting...");
+        }
         getLog().info("Finished " + this.getClass().getSimpleName() + "!");
     }
 
@@ -114,14 +116,10 @@ public abstract class AbstractGenerationMojo extends AbstractMojo {
     }
 
     private Properties loadPropertiesFile() {
-        if (propertyFile == null) {
-            return null;
-        }
-
-        Properties properties = new Properties();
         try (FileInputStream is = new FileInputStream(propertyFile)) {
-            properties.load(is);
-            return properties;
+            Properties result = new Properties();
+            result.load(is);
+            return result;
         } catch (FileNotFoundException e) {
             throw new BuildException(propertyFile + " not found.", e);
         } catch (IOException e) {

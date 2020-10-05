de6736ba3217c3965121f43ee4fdffab5afc7c1f
Nathan Xu <nathan.qingyang.xu@gmail.com>
2020-10-05 10:57:58 +0100

    HHH-14227 Insert statements are not ordered with entities that use inheritance and reference a subclass

--- hibernate-core/src/main/java/org/hibernate/engine/spi/ActionQueue.java
+++ hibernate-core/src/main/java/org/hibernate/engine/spi/ActionQueue.java
@@ -1268,7 +1268,9 @@ public class ActionQueue {
 				for ( int i = 0; i < propertyValues.length; i++ ) {
 					Object value = propertyValues[i];
 					Type type = propertyTypes[i];
-					addParentChildEntityNameByPropertyAndValue( action, batchIdentifier, type, value );
+					if ( value != null ) {
+						addParentChildEntityNameByPropertyAndValue( action, batchIdentifier, type, value );
+					}
 				}
 
 				if ( identifierType.isComponentType() ) {

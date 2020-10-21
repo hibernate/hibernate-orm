44ca255b5035893bce17106b91dafa8fd50b27a1
info@kawoolutions.com <info@kawoolutions.com>
2020-10-21 22:51:15 +0200

    HHH-14276 test case for strange AnnotationException: MapsId in IdClass context

---
+++ hibernate-core/src/test/java/org/hibernate/test/mapping/hhh14276/NestedIdClassDerivedIdentifiersTest.java
@@ -0,0 +1,53 @@
+package org.hibernate.test.mapping.hhh14276;
+
+import static org.hibernate.testing.transaction.TransactionUtil.doInJPA;
+
+import java.util.Map;
+
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.hql.spi.id.inline.InlineIdsOrClauseBulkIdStrategy;
+import org.hibernate.jpa.test.BaseEntityManagerFunctionalTestCase;
+import org.hibernate.test.mapping.hhh14276.entity.PlayerStat;
+import org.hibernate.test.mapping.hhh14276.entity.Score;
+import org.hibernate.testing.TestForIssue;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
+ */
+@TestForIssue( jiraKey = "HHH-14276" )
+public class NestedIdClassDerivedIdentifiersTest extends BaseEntityManagerFunctionalTestCase
+{
+    @Override
+    protected Class<?>[] getAnnotatedClasses()
+    {
+        return new Class<?>[] { PlayerStat.class,
+                                Score.class };
+    }
+    
+    @Override
+    protected void addConfigOptions( Map options )
+    {
+        options.put( AvailableSettings.GLOBALLY_QUOTED_IDENTIFIERS, Boolean.TRUE );
+        options.put( AvailableSettings.HQL_BULK_ID_STRATEGY, InlineIdsOrClauseBulkIdStrategy.class.getName() );
+    }
+    
+    @Before
+    public void setUp()
+    {
+        doInJPA( this::entityManagerFactory, em ->
+        {
+            // do nothing
+        } );
+    }
+    
+    @Test
+    public void testNestedIdClassDerivedIdentifiers() throws Exception
+    {
+        doInJPA( this::entityManagerFactory, em ->
+        {
+            // do nothing
+        });
+    }
+}

13fb23d44e02e8b9251e608392a97aa961f6a2f3
Steve Ebersole <steve@hibernate.org>
2020-11-10 08:36:01 -0600

    HHH-14312 - entity graph is ignored for 'padded' and 'dynamic' batch style entity loader

---
+++ hibernate-core/src/main/java/org/hibernate/loader/entity/plan/DynamicBatchingEntityLoaderBuilder.java
@@ -0,0 +1,44 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.loader.entity.plan;
+
+import org.hibernate.LockMode;
+import org.hibernate.LockOptions;
+import org.hibernate.engine.spi.LoadQueryInfluencers;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.loader.entity.UniqueEntityLoader;
+import org.hibernate.persister.entity.OuterJoinLoadable;
+
+/**
+ * @author Steve Ebersole
+ */
+public class DynamicBatchingEntityLoaderBuilder extends AbstractBatchingEntityLoaderBuilder {
+	/**
+	 * Singleton access
+	 */
+	public static final DynamicBatchingEntityLoaderBuilder INSTANCE = new DynamicBatchingEntityLoaderBuilder();
+
+	@Override
+	protected UniqueEntityLoader buildBatchingLoader(
+			OuterJoinLoadable persister,
+			int batchSize,
+			LockMode lockMode,
+			SessionFactoryImplementor factory,
+			LoadQueryInfluencers influencers) {
+		return buildBatchingLoader( persister, batchSize, LockOptions.interpret( lockMode ), factory, influencers );
+	}
+
+	@Override
+	protected UniqueEntityLoader buildBatchingLoader(
+			OuterJoinLoadable persister,
+			int batchSize,
+			LockOptions lockOptions,
+			SessionFactoryImplementor factory,
+			LoadQueryInfluencers influencers) {
+		return new DynamicBatchingEntityLoader( persister, batchSize, lockOptions, factory, influencers );
+	}
+}

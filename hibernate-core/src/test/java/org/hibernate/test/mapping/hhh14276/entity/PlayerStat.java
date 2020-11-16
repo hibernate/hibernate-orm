fe2230f813983554b404f1ec0548d4a28edfc210
Sanne Grinovero <sanne@hibernate.org>
2020-11-16 12:07:42 +0000

    HHH-14276 Amend style and formatting

--- hibernate-core/src/test/java/org/hibernate/test/mapping/hhh14276/entity/PlayerStat.java
+++ hibernate-core/src/test/java/org/hibernate/test/mapping/hhh14276/entity/PlayerStat.java
@@ -1,3 +1,9 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
 package org.hibernate.test.mapping.hhh14276.entity;
 
 import java.io.Serializable;
@@ -15,75 +21,62 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "\"PlayerStats\"")
 @IdClass(PlayerStatId.class)
-public class PlayerStat implements Serializable
-{
-    private static final long serialVersionUID = 1L;
+public class PlayerStat implements Serializable {
 
-    @Id
-    @Column(name = "player_id")
-    private Integer playerId;
+	@Id
+	@Column(name = "player_id")
+	private Integer playerId;
 
-    @Basic(optional = false)
-    @Column(name = "jersey_nbr")
-    private Integer jerseyNbr;
+	@Basic(optional = false)
+	@Column(name = "jersey_nbr")
+	private Integer jerseyNbr;
 
-    @Id
-    @ManyToOne(optional = false, fetch = FetchType.EAGER)
-    @JoinColumn(name = "game_id", referencedColumnName = "game_id")
-    @JoinColumn(name = "is_home", referencedColumnName = "is_home")
-    private Score score;
+	@Id
+	@ManyToOne(optional = false, fetch = FetchType.EAGER)
+	@JoinColumn(name = "game_id", referencedColumnName = "game_id")
+	@JoinColumn(name = "is_home", referencedColumnName = "is_home")
+	private Score score;
 
-    public PlayerStat()
-    {
-    }
+	public PlayerStat() {
+	}
 
-    public Integer getGameId()
-    {
-        return score.getGameId();
-    }
+	public Integer getGameId() {
+		return score.getGameId();
+	}
 
-    public void setGameId(Integer gameId)
-    {
-        score.setGameId(gameId);
-    }
+	public void setGameId(Integer gameId) {
+		score.setGameId( gameId );
+	}
 
-    public Boolean getHome()
-    {
-        return score.getHome();
-    }
+	public Boolean getHome() {
+		return score.getHome();
+	}
 
-    public void setHome(Boolean home)
-    {
-        score.setHome(home);
-    }
+	public void setHome(Boolean home) {
+		score.setHome( home );
+	}
 
-    public Integer getPlayerId()
-    {
-        return playerId;
-    }
+	public Integer getPlayerId() {
+		return playerId;
+	}
 
-    public void setPlayerId(Integer playerId)
-    {
-        this.playerId = playerId;
-    }
+	public void setPlayerId(Integer playerId) {
+		this.playerId = playerId;
+	}
 
-    public Integer getJerseyNbr()
-    {
-        return jerseyNbr;
-    }
+	public Integer getJerseyNbr() {
+		return jerseyNbr;
+	}
 
-    public void setJerseyNbr(Integer jerseyNbr)
-    {
-        this.jerseyNbr = jerseyNbr;
-    }
+	public void setJerseyNbr(Integer jerseyNbr) {
+		this.jerseyNbr = jerseyNbr;
+	}
 
-    public Score getScore()
-    {
-        return score;
-    }
+	public Score getScore() {
+		return score;
+	}
 
-    public void setScore(Score score)
-    {
-        this.score = score;
-    }
+	public void setScore(Score score) {
+		this.score = score;
+	}
 }

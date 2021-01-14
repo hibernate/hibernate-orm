be774d16a107c8b3c7e71afa001132a7207ff8e7
Luis Carlos Limas <luiscla27@hotmail.com>
2021-01-13 18:29:14 -0600

    Added deprecated annotation

--- hibernate-core/src/main/java/org/hibernate/engine/jdbc/BlobProxy.java
+++ hibernate-core/src/main/java/org/hibernate/engine/jdbc/BlobProxy.java
@@ -105,6 +105,18 @@ public final class BlobProxy implements Blob, BlobImplementer {
 	}
 
 	@Override
+	@Deprecated
+	public byte[] getBytes(final long start, final int length) throws SQLException {
+		if ( start < 1 ) {
+			throw new SQLException( "Start position 1-based; must be 1 or more." );
+		}
+		if ( length < 0 ) {
+			throw new SQLException( "Length must be great-than-or-equal to zero." );
+		}
+		return DataHelper.extractBytes( getBinaryStream() );
+	}
+
+	@Override
 	public byte[] getBytes(final long start, final long length) throws SQLException {
 		if ( start < 1 ) {
 			throw new SQLException( "Start position 1-based; must be 1 or more." );

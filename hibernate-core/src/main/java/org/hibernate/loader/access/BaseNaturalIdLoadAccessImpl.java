270fba830ad07466cf4817dcaf924cbc80ec0d87
Andrea Boriero <andrea@hibernate.org>
2021-01-27 18:40:13 +0100

    Fix checkstyle errors

--- hibernate-core/src/main/java/org/hibernate/loader/access/BaseNaturalIdLoadAccessImpl.java
+++ hibernate-core/src/main/java/org/hibernate/loader/access/BaseNaturalIdLoadAccessImpl.java
@@ -1,3 +1,9 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
 package org.hibernate.loader.access;
 
 import java.util.Map;
@@ -185,7 +191,8 @@ public abstract class BaseNaturalIdLoadAccessImpl<T> implements NaturalIdLoadOpt
 
 			if ( cachedResolution != null ) {
 				loaded = (T) getIdentifierLoadAccess().load( cachedResolution );
-			} else {
+			}
+			else {
 				loaded = (T) entityPersister().getNaturalIdLoader().load( normalizedNaturalIdValue, this, session );
 			}
 

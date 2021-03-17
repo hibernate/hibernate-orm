b9fb1b183ee42aef6a9d399ba9d93921f964c932
Karel Maesen <karel@geovise.com>
2021-03-17 20:29:13 +0100

    HHH-14037 Add PostgisPG10Dialect

--- hibernate-spatial/src/main/java/org/hibernate/spatial/dialect/postgis/PostgisPG10Dialect.java
+++ hibernate-spatial/src/main/java/org/hibernate/spatial/dialect/postgis/PostgisPG10Dialect.java
@@ -4,25 +4,19 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
+
 package org.hibernate.spatial.dialect.postgis;
 
 import java.util.Map;
 
 import org.hibernate.boot.model.TypeContributions;
-import org.hibernate.dialect.PostgreSQL95Dialect;
+import org.hibernate.dialect.PostgreSQL10Dialect;
 import org.hibernate.dialect.function.SQLFunction;
 import org.hibernate.service.ServiceRegistry;
 
-/**
- * Extends the {@code PostgreSQL95Dialect} to add support for the Postgis spatial types, functions and operators .
- * Created by Karel Maesen, Geovise BVBA on 01/11/16.
- */
-public class PostgisPG95Dialect extends PostgreSQL95Dialect implements PGSpatialDialectTrait {
+public class PostgisPG10Dialect extends PostgreSQL10Dialect implements PGSpatialDialectTrait {
 
-	/**
-	 * Creates an instance
-	 */
-	public PostgisPG95Dialect() {
+	public PostgisPG10Dialect() {
 		super();
 		registerColumnType(
 				PGGeometryTypeDescriptor.INSTANCE_WKB_1.getSqlType(),

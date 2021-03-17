00b711ac433346b46eb832a61d7c097299a67e1c
Gautham Kumar <gautham.kumar@growthsourceft.com>
2021-03-17 18:25:56 +0530

    HHH-14449 Initial commit to remove StreamDecorator usage

--- hibernate-core/src/main/java/org/hibernate/query/internal/AbstractProducedQuery.java
+++ hibernate-core/src/main/java/org/hibernate/query/internal/AbstractProducedQuery.java
@@ -83,7 +83,6 @@ import org.hibernate.query.spi.QueryParameterBinding;
 import org.hibernate.query.spi.QueryParameterBindings;
 import org.hibernate.query.spi.QueryParameterListBinding;
 import org.hibernate.query.spi.ScrollableResultsImplementor;
-import org.hibernate.query.spi.StreamDecorator;
 import org.hibernate.transform.ResultTransformer;
 import org.hibernate.type.Type;
 
@@ -1584,12 +1583,7 @@ public abstract class AbstractProducedQuery<R> implements QueryImplementor<R> {
 		final ScrollableResultsIterator<R> iterator = new ScrollableResultsIterator<>( scrollableResults );
 		final Spliterator<R> spliterator = Spliterators.spliteratorUnknownSize( iterator, Spliterator.NONNULL );
 
-		final Stream<R> stream = new StreamDecorator(
-				StreamSupport.stream( spliterator, false ),
-				scrollableResults::close
-		);
-
-		return stream;
+		return StreamSupport.stream(spliterator, false).onClose(iterator::close);
 	}
 
 	@Override

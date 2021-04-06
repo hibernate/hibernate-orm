09469715e3b29d8a88bddf1df71a7abe3eb8cc13
Christian Beikov <christian.beikov@gmail.com>
2021-04-06 14:24:13 +0200

    Skip certain LockTests for Oracle on GH Actions which don't work there

--- ci/database-start.sh
+++ ci/database-start.sh
@@ -2,19 +2,13 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-java -version
-
 if [ "$RDBMS" == 'mysql' ]; then
-  sudo service mysql stop
   bash $DIR/../docker_db.sh mysql_5_7
 elif [ "$RDBMS" == 'mysql8' ]; then
-  sudo service mysql stop
   bash $DIR/../docker_db.sh mysql_8_0
 elif [ "$RDBMS" == 'mariadb' ]; then
-  sudo service mysql stop
   bash $DIR/../docker_db.sh mariadb
 elif [ "$RDBMS" == 'postgresql' ]; then
-  sudo service postgres stop
   bash $DIR/../docker_db.sh postgresql_9_5
 elif [ "$RDBMS" == 'db2' ]; then
   bash $DIR/../docker_db.sh db2
@@ -22,6 +16,6 @@ elif [ "$RDBMS" == 'oracle' ]; then
   bash $DIR/../docker_db.sh oracle
 elif [ "$RDBMS" == 'mssql' ]; then
   bash $DIR/../docker_db.sh mssql
+elif [ "$RDBMS" == 'hana' ]; then
+  bash $DIR/../docker_db.sh hana
 fi
-
-exec bash $DIR/build.sh

ba785fb6147b9cbbab20160260ede20ccfbfa633
Yoann Rodière <yoann@hibernate.org>
2021-05-25 12:46:19 +0200

    HHH-14635 Upgrade to log4j 2

--- hibernate-testing/src/main/java/org/hibernate/testing/logger/LogInspectionHelper.java
+++ hibernate-testing/src/main/java/org/hibernate/testing/logger/LogInspectionHelper.java
@@ -32,7 +32,7 @@ final class LogInspectionHelper {
 		convertType( log ).clearAllListeners();
 	}
 
-	private static Log4DelegatingLogger convertType(BasicLogger log) {
+	private static Log4J2DelegatingLogger convertType(BasicLogger log) {
 		if ( log instanceof DelegatingBasicLogger) {
 			//Most loggers generated via the annotation processor are of this type
 			DelegatingBasicLogger wrapper = (DelegatingBasicLogger) log;
@@ -43,13 +43,13 @@ final class LogInspectionHelper {
 				throw new RuntimeException( cause );
 			}
 		}
-		if ( ! ( log instanceof Log4DelegatingLogger ) ) {
+		if ( ! ( log instanceof Log4J2DelegatingLogger ) ) {
 			throw new AssertionFailure( "Unexpected log type: JBoss Logger didn't register the custom TestableLoggerProvider as logger provider" );
 		}
-		return (Log4DelegatingLogger) log;
+		return (Log4J2DelegatingLogger) log;
 	}
 
-	private static Log4DelegatingLogger extractFromWrapper(DelegatingBasicLogger wrapper) throws Exception {
+	private static Log4J2DelegatingLogger extractFromWrapper(DelegatingBasicLogger wrapper) throws Exception {
 		Field field = DelegatingBasicLogger.class.getDeclaredField( "log" );
 		field.setAccessible( true );
 		Object object = field.get( wrapper );

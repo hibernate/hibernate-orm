46b550a75f17f46695306bcb86797e2d65318f0e
bb7133 <bb7133@gmail.com>
2021-06-18 17:35:38 +0800

    HHH-14576 : Introduce the dialect for TiDB

--- hibernate-core/src/test/java/org/hibernate/test/schemaupdate/SchemaUpdateTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/schemaupdate/SchemaUpdateTest.java
@@ -41,6 +41,7 @@ import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.dialect.Dialect;
 import org.hibernate.dialect.SQLServerDialect;
 import org.hibernate.dialect.SybaseDialect;
+import org.hibernate.dialect.TiDBDialect;
 import org.hibernate.engine.jdbc.env.spi.IdentifierHelper;
 import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
@@ -84,9 +85,11 @@ public class SchemaUpdateTest {
 	@Before
 	public void setUp() throws IOException {
 		if(SQLServerDialect.class.isAssignableFrom( Dialect.getDialect().getClass() )
-				|| SybaseDialect.class.isAssignableFrom(Dialect.getDialect().getClass())) {
-			// SQLServerDialect and SybaseDialect stores case-insensitive quoted identifiers in mixed case,
+				|| SybaseDialect.class.isAssignableFrom(Dialect.getDialect().getClass())
+				|| TiDBDialect.class.isAssignableFrom(Dialect.getDialect().getClass())) {
+			// SQLServerDialect, SybaseDialect and TiDB store case-insensitive quoted identifiers in mixed case,
 			// so the checks at the end of this method won't work.
+			// For TiDB, only 'lower_case_table_names=2' is supported.
 			skipTest = true;
 			return;
 		}

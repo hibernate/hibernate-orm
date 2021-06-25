fcee504c99c7692a226470df5c1b73369aa28131
Andrea Boriero <andrea@hibernate.org>
2021-06-25 12:43:45 +0200

    Re-enable additional tests

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/iterate/ScrollTest.java
@@ -0,0 +1,72 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
+package org.hibernate.orm.test.iterate;
+
+import org.hibernate.Hibernate;
+import org.hibernate.ScrollableResults;
+import org.hibernate.cfg.Environment;
+
+import org.hibernate.testing.orm.junit.DomainModel;
+import org.hibernate.testing.orm.junit.ServiceRegistry;
+import org.hibernate.testing.orm.junit.SessionFactory;
+import org.hibernate.testing.orm.junit.SessionFactoryScope;
+import org.hibernate.testing.orm.junit.Setting;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@DomainModel(
+		xmlMappings = "org/hibernate/orm/test/iterate/Item.hbm.xml"
+)
+@SessionFactory(
+		generateStatistics = true
+)
+@ServiceRegistry(
+		settings = {
+				@Setting(name = Environment.USE_QUERY_CACHE, value = "true"),
+				@Setting(name = Environment.CACHE_REGION_PREFIX, value = "foo"),
+				@Setting(name = Environment.USE_SECOND_LEVEL_CACHE, value = "true")
+		}
+)
+public class ScrollTest {
+
+	@Test
+	public void testScroll(SessionFactoryScope scope) {
+		scope.inTransaction(
+				s -> {
+					Item i1 = new Item( "foo" );
+					Item i2 = new Item( "bar" );
+					s.persist( "Item", i1 );
+					s.persist( "Item", i2 );
+				}
+		);
+
+		scope.inTransaction(
+				s -> {
+					ScrollableResults sr = s.getNamedQuery( "Item.nameDesc" ).scroll();
+					assertTrue( sr.next() );
+					Item i1 = (Item) sr.get();
+					assertTrue( sr.next() );
+					Item i2 = (Item) sr.get();
+					assertTrue( Hibernate.isInitialized( i1 ) );
+					assertTrue( Hibernate.isInitialized( i2 ) );
+					assertThat( i1.getName(), is( "foo" ) );
+					assertThat( i2.getName(), is( "bar" ) );
+					assertFalse( sr.next() );
+					s.delete( i1 );
+					s.delete( i2 );
+				}
+		);
+
+		assertThat( scope.getSessionFactory().getStatistics().getEntityFetchCount(), is( 0L ) );
+	}
+
+}

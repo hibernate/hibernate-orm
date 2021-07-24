21dd7a35f106e462fe0651b1163162863be6cbea
Karel Maesen <karel@geovise.com>
2021-07-24 17:46:43 +0200

    HHH-14745 Add FunctionContributor contract

---
+++ hibernate-core/src/main/java/org/hibernate/boot/model/FunctionContributor.java
@@ -0,0 +1,34 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+
+package org.hibernate.boot.model;
+
+import org.hibernate.query.sqm.function.SqmFunctionRegistry;
+import org.hibernate.service.ServiceRegistry;
+
+/**
+ * Contract for contributing functions
+ *
+ * @author Karel Maesen
+ */
+public interface FunctionContributor {
+
+	/**
+	 *  Contribute functions
+	 *
+	 * @param functionContributions The callback for contributing functions
+	 * @param serviceRegistry The service registry
+	 */
+	void contributeTypes(FunctionContributions functionContributions, ServiceRegistry serviceRegistry);
+}

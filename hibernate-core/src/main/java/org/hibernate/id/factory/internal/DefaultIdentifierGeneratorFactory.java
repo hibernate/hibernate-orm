288d8f2753af65d7ebc16ba9e31d00835983866c
Sanne Grinovero <sanne@hibernate.org>
2021-07-27 16:21:05 +0100

    HHH-14755 Remove some dead code from DefaultIdentifierGeneratorFactory

--- hibernate-core/src/main/java/org/hibernate/id/factory/internal/DefaultIdentifierGeneratorFactory.java
+++ hibernate-core/src/main/java/org/hibernate/id/factory/internal/DefaultIdentifierGeneratorFactory.java
@@ -95,21 +95,6 @@ public class DefaultIdentifierGeneratorFactory
 
 	@Override
 	public void setDialect(Dialect dialect) {
-//		LOG.debugf( "Setting dialect [%s]", dialect );
-//		this.dialect = dialect;
-//
-//		if ( dialect == jdbcEnvironment.getDialect() ) {
-//			LOG.debugf(
-//					"Call to unsupported method IdentifierGeneratorFactory#setDialect; " +
-//							"ignoring as passed Dialect matches internal Dialect"
-//			);
-//		}
-//		else {
-//			throw new UnsupportedOperationException(
-//					"Call to unsupported method IdentifierGeneratorFactory#setDialect attempting to" +
-//							"set a non-matching Dialect : " + dialect.getClass().getName()
-//			);
-//		}
 	}
 
 	@SuppressWarnings("unchecked")

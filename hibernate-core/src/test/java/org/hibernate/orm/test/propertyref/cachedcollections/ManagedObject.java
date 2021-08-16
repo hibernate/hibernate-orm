5028577d90993e6c7f5c689af27e7e2e3cf5da20
Andrea Boriero <andrea@hibernate.org>
2021-08-16 23:01:04 +0200

    Re-enabled additional tests

--- hibernate-core/src/test/java/org/hibernate/orm/test/propertyref/cachedcollections/ManagedObject.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/propertyref/cachedcollections/ManagedObject.java
@@ -4,7 +4,7 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.test.propertyref.cachedcollections;
+package org.hibernate.orm.test.propertyref.cachedcollections;
 
 import java.util.HashSet;
 import java.util.Set;

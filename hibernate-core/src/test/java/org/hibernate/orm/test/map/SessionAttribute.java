92a95274aa53aaeeb0c714dbec774f18f315943c
Andrea Boriero <andrea@hibernate.org>
2021-08-16 13:43:35 +0200

    Re-enabled additional tests

--- hibernate-core/src/test/java/org/hibernate/orm/test/map/SessionAttribute.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/map/SessionAttribute.java
@@ -6,7 +6,7 @@
  */
 
 //$Id: SessionAttribute.java 5686 2005-02-12 07:27:32Z steveebersole $
-package org.hibernate.test.map;
+package org.hibernate.orm.test.map;
 import java.io.Serializable;
 
 /**

86679a455a02d92d8bd5e8bf679b4ab5ad5e3d2b
Andrea Boriero <andrea@hibernate.org>
2021-08-23 16:25:41 +0200

    Re-enabled envers tests

--- hibernate-envers/src/test/java/org/hibernate/orm/test/envers/integration/nativequery/EntityResultNativeQueryTest.java
+++ hibernate-envers/src/test/java/org/hibernate/orm/test/envers/integration/nativequery/EntityResultNativeQueryTest.java
@@ -4,7 +4,7 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.envers.test.integration.nativequery;
+package org.hibernate.orm.test.envers.integration.nativequery;
 
 import java.util.List;
 import javax.persistence.Query;

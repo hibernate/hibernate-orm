7c36fd023fbfb159ae087655e7c10e5f329599ed
Andrea Boriero <andrea@hibernate.org>
2021-08-23 15:38:03 +0200

    Re-enabled envers tests

--- hibernate-envers/src/test/java/org/hibernate/orm/test/envers/integration/ids/embeddedid/RelationInsideEmbeddableTest.java
+++ hibernate-envers/src/test/java/org/hibernate/orm/test/envers/integration/ids/embeddedid/RelationInsideEmbeddableTest.java
@@ -4,17 +4,13 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.envers.test.integration.ids.embeddedid;
+package org.hibernate.orm.test.envers.integration.ids.embeddedid;
 
 import java.util.Arrays;
 import javax.persistence.EntityManager;
 
 import org.hibernate.orm.test.envers.BaseEnversJPAFunctionalTestCase;
 import org.hibernate.orm.test.envers.Priority;
-import org.hibernate.orm.test.envers.integration.ids.embeddedid.Item;
-import org.hibernate.orm.test.envers.integration.ids.embeddedid.ItemId;
-import org.hibernate.orm.test.envers.integration.ids.embeddedid.Producer;
-import org.hibernate.orm.test.envers.integration.ids.embeddedid.PurchaseOrder;
 
 import org.hibernate.testing.TestForIssue;
 import org.junit.Assert;

83fba3064f373aef561e441be238c1a7b43a15ea
Andrea Boriero <andrea@hibernate.org>
2021-08-23 15:38:03 +0200

    Re-enabled envers tests

--- hibernate-envers/src/test/java/org/hibernate/orm/test/envers/integration/ids/idclass/IdClassWithRelationTest.java
+++ hibernate-envers/src/test/java/org/hibernate/orm/test/envers/integration/ids/idclass/IdClassWithRelationTest.java
@@ -4,16 +4,13 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.envers.test.integration.ids.idclass;
+package org.hibernate.orm.test.envers.integration.ids.idclass;
 
 import java.util.Arrays;
 import javax.persistence.EntityManager;
 
 import org.hibernate.orm.test.envers.BaseEnversJPAFunctionalTestCase;
 import org.hibernate.orm.test.envers.Priority;
-import org.hibernate.orm.test.envers.integration.ids.idclass.ClassType;
-import org.hibernate.orm.test.envers.integration.ids.idclass.RelationalClassId;
-import org.hibernate.orm.test.envers.integration.ids.idclass.SampleClass;
 
 import org.hibernate.testing.TestForIssue;
 import org.junit.Test;

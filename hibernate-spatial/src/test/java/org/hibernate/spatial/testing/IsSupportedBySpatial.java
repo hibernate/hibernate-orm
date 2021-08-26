a07075ec2e09286109cc2dae114fdbf2fa152625
Karel Maesen <karel@geovise.com>
2021-08-26 22:26:37 +0200

    HHH-14738 Ignore spatial tests for dialects not supported by Spatial

---
+++ hibernate-spatial/src/test/java/org/hibernate/spatial/testing/IsSupportedBySpatial.java
@@ -0,0 +1,22 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+
+package org.hibernate.spatial.testing;
+
+import org.hibernate.dialect.CockroachDialect;
+import org.hibernate.dialect.Dialect;
+import org.hibernate.dialect.PostgreSQLDialect;
+
+import org.hibernate.testing.orm.junit.DialectFeatureCheck;
+
+public class IsSupportedBySpatial implements DialectFeatureCheck {
+	@Override
+	public boolean apply(Dialect dialect) {
+		return dialect instanceof PostgreSQLDialect
+				|| dialect instanceof CockroachDialect;
+	}
+}

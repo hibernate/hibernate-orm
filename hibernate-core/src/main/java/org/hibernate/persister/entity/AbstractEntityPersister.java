d1806c5e92d744360abe9dbb6f24b10081f7fb5b
Gavin King <gavin@hibernate.org>
2021-09-06 13:04:45 +0200

    insert with @DynamicInsert should use generateIdentityInsertString()
    see the comment in generateInsertString()

--- hibernate-core/src/main/java/org/hibernate/persister/entity/AbstractEntityPersister.java
+++ hibernate-core/src/main/java/org/hibernate/persister/entity/AbstractEntityPersister.java
@@ -4035,7 +4035,7 @@ public abstract class AbstractEntityPersister
 		if ( entityMetamodel.isDynamicInsert() ) {
 			// For the case of dynamic-insert="true", we need to generate the INSERT SQL
 			boolean[] notNull = getPropertiesToInsert( fields );
-			id = insert( fields, notNull, generateInsertString( true, notNull ), object, session );
+			id = insert( fields, notNull, generateIdentityInsertString( notNull ), object, session );
 			for ( int j = 1; j < span; j++ ) {
 				insert( id, fields, notNull, j, generateInsertString( notNull, j ), object, session );
 			}

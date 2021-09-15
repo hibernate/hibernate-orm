9d131ce69f76a3f8b1527923b9d6d5af234209af
Andrea Boriero <andrea@hibernate.org>
2021-09-15 09:31:45 +0200

    Minor code cleaning

--- hibernate-core/src/main/java/org/hibernate/query/sqm/internal/QuerySqmImpl.java
+++ hibernate-core/src/main/java/org/hibernate/query/sqm/internal/QuerySqmImpl.java
@@ -547,12 +547,14 @@ public class QuerySqmImpl<R>
 		throw new PersistenceException( "Unrecognized unwrap type [" + cls.getName() + "]" );
 	}
 
+	@Override
 	protected boolean applyNativeQueryLockMode(Object value) {
 		throw new IllegalStateException(
 				"Illegal attempt to set lock mode on non-native query via hint; use Query#setLockMode instead"
 		);
 	}
 
+	@Override
 	protected boolean applySynchronizeSpacesHint(Object value) {
 		throw new IllegalStateException(
 				"Illegal attempt to set synchronized spaces on non-native query via hint"

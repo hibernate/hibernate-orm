a699bf4dcaae27d1469b12097d524b22f47bdc28
taghizadeh87 <84373927+taghizadeh87@users.noreply.github.com>
2021-09-27 16:50:25 +0330

    Fix typo

--- hibernate-testing/src/main/java/org/hibernate/testing/junit4/BaseCoreFunctionalTestCase.java
+++ hibernate-testing/src/main/java/org/hibernate/testing/junit4/BaseCoreFunctionalTestCase.java
@@ -447,7 +447,7 @@ public abstract class BaseCoreFunctionalTestCase extends BaseUnitTestCase {
 		}
 		else {
 			// Because of https://hibernate.atlassian.net/browse/HHH-5529,
-			// we can'trely on a Bulk Delete query which will not clear the link tables in @ElementCollection or unidirectional collections
+			// we can't rely on a Bulk Delete query which will not clear the link tables in @ElementCollection or unidirectional collections
 			doInHibernate( this::sessionFactory, s -> {
 				s.createQuery( "from java.lang.Object" ).list().forEach( s::remove );
 			} );

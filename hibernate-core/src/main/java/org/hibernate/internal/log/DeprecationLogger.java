3570115635d1fa1d54870a234b94bed133a25580
Steve Ebersole <steve@hibernate.org>
2021-09-27 12:39:26 -0500

    HHH-14845 - Deprecate JACC support

--- hibernate-core/src/main/java/org/hibernate/internal/log/DeprecationLogger.java
+++ hibernate-core/src/main/java/org/hibernate/internal/log/DeprecationLogger.java
@@ -249,4 +249,28 @@ public interface DeprecationLogger extends BasicLogger {
 					"5.3 in upgrading.  It will be removed in a later version."
 	)
 	void logUseOfDeprecatedZeroBasedJdbcStyleParams();
+
+	@LogMessage(level = WARN)
+	@Message(
+		id = 90000025,
+		value = "Encountered multiple component mappings for the same java class [%s] with different property mappings. " +
+			"This is deprecated and will be removed in a future version. Every property mapping combination should have its own java class"
+	)
+	void deprecatedComponentMapping(String name);
+
+	@LogMessage(level = WARN)
+	@Message(
+			id = 90000026,
+			value = "JACC integration was enabled.  Support for JACC integration will be removed in version 6.0.  Use of" +
+					"`%s`, `%s` or `%s` settings is discouraged"
+	)
+	void deprecatedJaccUsage(String jaccEnabled, String jaccContextId, String jaccPrefix);
+
+	@LogMessage(level = WARN)
+	@Message(
+			id = 90000027,
+			value = "JACC settings encountered in hibernate `cfg.xml` file.  JACC integration is deprecated and will be removed in version 6.0"
+	)
+	void deprecatedJaccCfgXmlSettings();
+
 }

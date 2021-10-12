6b7c21a18d018020b38112a91777c4e10c23c255
Andrea Boriero <andrea@hibernate.org>
2021-10-12 18:44:49 +0200

    Re-enabled additional tests

--- hibernate-core/src/test/java/org/hibernate/orm/test/bytecode/enhancement/lazy/proxy/MergeProxyTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/bytecode/enhancement/lazy/proxy/MergeProxyTest.java
@@ -4,7 +4,7 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later
  * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
  */
-package org.hibernate.test.bytecode.enhancement.lazy.proxy;
+package org.hibernate.orm.test.bytecode.enhancement.lazy.proxy;
 
 import org.hibernate.Hibernate;
 import org.hibernate.boot.MetadataSources;
@@ -20,9 +20,6 @@ import org.hibernate.testing.TestForIssue;
 import org.hibernate.testing.bytecode.enhancement.BytecodeEnhancerRunner;
 import org.hibernate.testing.bytecode.enhancement.EnhancementOptions;
 import org.hibernate.testing.junit4.BaseNonConfigCoreFunctionalTestCase;
-import org.hibernate.test.bytecode.enhancement.lazy.proxy.Activity;
-import org.hibernate.test.bytecode.enhancement.lazy.proxy.Instruction;
-import org.hibernate.test.bytecode.enhancement.lazy.proxy.WebApplication;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/bytecode/enhancement/lazy/proxy/WebApplication.java
@@ -0,0 +1,69 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
+package org.hibernate.orm.test.bytecode.enhancement.lazy.proxy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.hibernate.annotations.LazyGroup;
+import org.hibernate.annotations.NaturalId;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+/**
+ * @author Steve Ebersole
+ */
+@Entity
+@Table( name="web_app" )
+public class WebApplication extends BaseEntity {
+	private String name;
+	private String siteUrl;
+
+	private Set<Activity> activities = new HashSet<>();
+
+	@SuppressWarnings("unused")
+	public WebApplication() {
+	}
+
+	public WebApplication(Integer id, String siteUrl) {
+		super( id );
+		this.siteUrl = siteUrl;
+	}
+
+	@NaturalId
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Basic( fetch = FetchType.LAZY )
+	public String getSiteUrl() {
+		return siteUrl;
+	}
+
+	public void setSiteUrl(String siteUrl) {
+		this.siteUrl = siteUrl;
+	}
+
+	@OneToMany(mappedBy="webApplication", fetch= FetchType.LAZY)
+	@LazyGroup("app_activity_group")
+//	@CollectionType(type="baseutil.technology.hibernate.IskvLinkedSetCollectionType")
+	public Set<Activity> getActivities() {
+		return activities;
+	}
+
+	public void setActivities(Set<Activity> activities) {
+		this.activities = activities;
+	}
+}

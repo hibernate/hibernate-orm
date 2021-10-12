cc31223b78db02811348384009ef75eaa559b3a6
Andrea Boriero <andrea@hibernate.org>
2021-10-12 18:44:49 +0200

    Re-enabled additional tests

--- hibernate-core/src/test/java/org/hibernate/orm/test/bytecode/enhancement/lazy/group/SimpleLazyGroupUpdateTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/bytecode/enhancement/lazy/group/SimpleLazyGroupUpdateTest.java
@@ -4,7 +4,7 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later
  * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
  */
-package org.hibernate.test.bytecode.enhancement.lazy.group;
+package org.hibernate.orm.test.bytecode.enhancement.lazy.group;
 
 import org.hibernate.annotations.LazyGroup;
 import org.hibernate.bytecode.enhance.spi.UnloadedClass;

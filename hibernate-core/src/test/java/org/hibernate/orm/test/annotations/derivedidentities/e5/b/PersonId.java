eae9a39b147db1f778f25d565dc6a48d93df77a3
Andrea Boriero <andrea@hibernate.org>
2021-11-01 21:59:12 +0100

    Fix throw exception when an Entity has an EmbeddedId being also an IdClass of another Entity

--- hibernate-core/src/test/java/org/hibernate/orm/test/annotations/derivedidentities/e5/b/PersonId.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/annotations/derivedidentities/e5/b/PersonId.java
@@ -4,7 +4,7 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.test.annotations.derivedidentities.e5.b;
+package org.hibernate.orm.test.annotations.derivedidentities.e5.b;
 import java.io.Serializable;
 
 /**
@@ -13,4 +13,12 @@ import java.io.Serializable;
 public class PersonId implements Serializable {
 	String firstName;
 	String lastName;
+
+	public PersonId() {
+	}
+
+	public PersonId(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
 }

86227948633716d9c4cb83fe9d07ee421e7120f6
Andrea Boriero <andrea@hibernate.org>
2021-11-26 13:06:37 +0100

    Fix TypeDefinitionBinder registering TypeDefinition

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/typeparameters/TypeParameterTest.java
@@ -0,0 +1,145 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.orm.test.typeparameters;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.hibernate.Session;
+import org.hibernate.engine.spi.SessionImplementor;
+import org.hibernate.jdbc.Work;
+
+import org.hibernate.testing.orm.junit.DomainModel;
+import org.hibernate.testing.orm.junit.SessionFactory;
+import org.hibernate.testing.orm.junit.SessionFactoryScope;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Test for parameterizable types.
+ *
+ * @author Michael Gloegl
+ */
+@DomainModel(
+		xmlMappings = {
+				"org/hibernate/orm/test/typeparameters/Typedef.hbm.xml",
+				"org/hibernate/orm/test/typeparameters/Widget.hbm.xml"
+		}
+)
+@SessionFactory
+public class TypeParameterTest {
+
+	@AfterEach
+	public void tearDown(SessionFactoryScope scope) {
+		scope.inTransaction(
+				session ->
+						session.createQuery( "delete from Widget" ).executeUpdate()
+
+		);
+	}
+
+	@Test
+	public void testSave(SessionFactoryScope scope) {
+		final Integer id = (Integer) scope.fromTransaction(
+				session -> {
+					Widget obj = new Widget();
+					obj.setValueThree( 5 );
+					return session.save( obj );
+				}
+		);
+
+		scope.inSession(
+				session ->
+						doWork( id, session )
+		);
+	}
+
+	private void doWork(final Integer id, final Session s) {
+		s.doWork(
+				new Work() {
+					@Override
+					public void execute(Connection connection) throws SQLException {
+						PreparedStatement statement = ( (SessionImplementor) s ).getJdbcCoordinator()
+								.getStatementPreparer()
+								.prepareStatement( "SELECT * FROM STRANGE_TYPED_OBJECT WHERE ID=?" );
+						statement.setInt( 1, id.intValue() );
+						ResultSet resultSet = ( (SessionImplementor) s ).getJdbcCoordinator()
+								.getResultSetReturn()
+								.extract( statement );
+
+						assertTrue( "A row should have been returned", resultSet.next() );
+						assertTrue(
+								"Default value should have been mapped to null",
+								resultSet.getObject( "VALUE_ONE" ) == null
+						);
+						assertTrue(
+								"Default value should have been mapped to null",
+								resultSet.getObject( "VALUE_TWO" ) == null
+						);
+						assertEquals( "Non-Default value should not be changed", resultSet.getInt( "VALUE_THREE" ), 5 );
+						assertTrue(
+								"Default value should have been mapped to null",
+								resultSet.getObject( "VALUE_FOUR" ) == null
+						);
+					}
+				}
+		);
+	}
+
+	@Test
+	public void testLoading(SessionFactoryScope scope) throws Exception {
+		initData( scope );
+		scope.inTransaction(
+				session -> {
+					Widget obj = (Widget) session.createQuery( "from Widget o where o.string = :string" ).setParameter(
+							"string",
+							"all-normal"
+					).uniqueResult();
+					assertEquals( "Non-Default value incorrectly loaded", obj.getValueOne(), 7 );
+					assertEquals( "Non-Default value incorrectly loaded", obj.getValueTwo(), 8 );
+					assertEquals( "Non-Default value incorrectly loaded", obj.getValueThree(), 9 );
+					assertEquals( "Non-Default value incorrectly loaded", obj.getValueFour(), 10 );
+
+					obj = (Widget) session.createQuery( "from Widget o where o.string = :string" )
+							.setParameter( "string", "all-default" )
+							.uniqueResult();
+					assertEquals( "Default value incorrectly loaded", obj.getValueOne(), 1 );
+					assertEquals( "Default value incorrectly loaded", obj.getValueTwo(), 2 );
+					assertEquals( "Default value incorrectly loaded", obj.getValueThree(), -1 );
+					assertEquals( "Default value incorrectly loaded", obj.getValueFour(), -5 );
+
+				}
+		);
+	}
+
+	public void initData(SessionFactoryScope scope) {
+		scope.inTransaction(
+				session -> {
+					Widget obj = new Widget();
+					obj.setValueOne( 7 );
+					obj.setValueTwo( 8 );
+					obj.setValueThree( 9 );
+					obj.setValueFour( 10 );
+					obj.setString( "all-normal" );
+					session.save( obj );
+
+					obj = new Widget();
+					obj.setValueOne( 1 );
+					obj.setValueTwo( 2 );
+					obj.setValueThree( -1 );
+					obj.setValueFour( -5 );
+					obj.setString( "all-default" );
+					session.save( obj );
+				}
+		);
+	}
+}

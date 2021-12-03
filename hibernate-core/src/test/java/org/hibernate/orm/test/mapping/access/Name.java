86799d6c28d56bdfdf6a9c629ea281302ff817d3
Steve Ebersole <steve@hibernate.org>
2021-12-03 15:22:15 -0600

    HHH-14949 - Support @Access on @ElementCollection (for embeddable elements)
    Test

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/mapping/access/Name.java
@@ -0,0 +1,42 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
+package org.hibernate.orm.test.mapping.access;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Name {
+	private String first;
+	private String last;
+
+	private Name() {
+	}
+
+	public Name(String first, String last) {
+		this.first = first;
+		this.last = last;
+	}
+
+	@Column(name = "first_name")
+	public String getFirstName() {
+		return first;
+	}
+
+	public void setFirstName(String first) {
+		this.first = first;
+	}
+
+	@Column(name = "last_name")
+	public String getLastName() {
+		return last;
+	}
+
+	public void setLastName(String last) {
+		this.last = last;
+	}
+}
---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/mapping/access/Person.java
@@ -0,0 +1,79 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
+package org.hibernate.orm.test.mapping.access; /**
+ * @author Steve Ebersole
+ */
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Access;
+import jakarta.persistence.AccessType;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import static jakarta.persistence.AccessType.*;
+
+@Entity
+@Table(name = "persons")
+public class Person {
+	@Id
+	public Integer id;
+
+	@Embedded
+	@Access( PROPERTY )
+	public Name name;
+
+	@ElementCollection
+	@Embedded
+	@Access( PROPERTY )
+	public Set<Name> aliases;
+
+	private Person() {
+		// for Hibernate use
+	}
+
+	public Person(Integer id, Name name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public Name getName() {
+		return name;
+	}
+
+	public void setName(Name name) {
+		this.name = name;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Set<Name> getAliases() {
+		return aliases;
+	}
+
+	public void setAliases(Set<Name> aliases) {
+		this.aliases = aliases;
+	}
+
+	public void addAlias(Name alias) {
+		if ( aliases == null ) {
+			aliases = new HashSet<>();
+		}
+		aliases.add( alias );
+	}
+}
+

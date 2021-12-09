75fc936b95e6a97e2df5ed58afe8c653b3ba8b32
Gavin King <gavin@hibernate.org>
2021-12-09 11:56:17 +0100

    improve the javadoc for AttributeBinder+AttributedBinderType

--- hibernate-core/src/main/java/org/hibernate/annotations/AttributeBinderType.java
+++ hibernate-core/src/main/java/org/hibernate/annotations/AttributeBinderType.java
@@ -18,6 +18,10 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 /**
  * Associates a user-defined annotation with an {@link AttributeBinder},
  * allowing the annotation to drive some custom model binding.
+ * <p>
+ * The user-defined annotation may be used to annotate fields and
+ * properties of entity and embeddable classes. The {@code AttributeBinder}
+ * will be called when the annotation is discovered by Hibernate.
  *
  * @author Gavin King
  */

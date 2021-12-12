8fef9c4de40ec6d13d842a676b1e68f75d11e67a
Steve Ebersole <steve@hibernate.org>
2021-12-11 20:08:38 -0600

    Test for https://hibernate.atlassian.net/browse/HHH-11784

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/query/hql/FunnyNamesTests.java
@@ -0,0 +1,29 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
+package org.hibernate.orm.test.query.hql;
+
+import org.hibernate.testing.orm.junit.DomainModel;
+import org.hibernate.testing.orm.junit.JiraKey;
+import org.hibernate.testing.orm.junit.SessionFactory;
+import org.hibernate.testing.orm.junit.SessionFactoryScope;
+
+import org.junit.jupiter.api.Test;
+
+import in.from.Any;
+
+@DomainModel( annotatedClasses = Any.class )
+@SessionFactory
+@JiraKey( "HHH-11784" )
+public class FunnyNamesTests {
+	@Test
+	public void basicTest(SessionFactoryScope scope) {
+		scope.inTransaction( (session) -> {
+			session.createQuery( "from Any" ).list();
+			session.createQuery( "from in.from.Any" ).list();
+		} );
+	}
+}

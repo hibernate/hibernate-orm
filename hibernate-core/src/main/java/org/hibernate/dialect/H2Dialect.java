11552b3e93a4f07b2d4968b490810dd6a04d62cc
Filipe Roque <froque@premium-minds.com>
2022-01-03 19:01:16 +0000

    HHH-15002 H2 supports 'true' and 'false' boolean literals
    Cherry picked from 1706141ffc154c1b61e304445a603ebf9a3e0a2e

--- hibernate-core/src/main/java/org/hibernate/dialect/H2Dialect.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/H2Dialect.java
@@ -246,6 +246,11 @@ public class H2Dialect extends Dialect {
 	}
 
 	@Override
+	public String toBooleanValueString(boolean bool) {
+		return String.valueOf( bool );
+	}
+
+	@Override
 	public LimitHandler getLimitHandler() {
 		return LIMIT_HANDLER;
 	}

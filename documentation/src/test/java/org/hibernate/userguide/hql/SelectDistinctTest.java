a1ea84a3ac4196d8e566835ca572cb17052f371a
Gavin King <gavin@hibernate.org>
2022-01-06 23:26:21 +0100

    two very minor doc fixes

--- documentation/src/test/java/org/hibernate/userguide/hql/SelectDistinctTest.java
+++ documentation/src/test/java/org/hibernate/userguide/hql/SelectDistinctTest.java
@@ -43,18 +43,18 @@ public class SelectDistinctTest extends BaseEntityManagerFunctionalTestCase {
 	public void init() {
 		doInJPA(this::entityManagerFactory, entityManager -> {
 			Person gavinKing = new Person("Gavin", "King");
-			Person stephanKing = new Person("Stephen", "King");
+			Person stephenKing = new Person("Stephen", "King");
 			Person vladMihalcea = new Person("Vlad", "Mihalcea");
 
 			gavinKing.addBook(new Book("Hibernate in Action"));
 			gavinKing.addBook(new Book("Java Persistence with Hibernate"));
 
-			stephanKing.addBook(new Book("The Green Mile"));
+			stephenKing.addBook(new Book("The Green Mile"));
 
 			vladMihalcea.addBook(new Book("High-Performance Java Persistence"));
 
 			entityManager.persist(gavinKing);
-			entityManager.persist(stephanKing);
+			entityManager.persist(stephenKing);
 			entityManager.persist(vladMihalcea);
 		});
 	}

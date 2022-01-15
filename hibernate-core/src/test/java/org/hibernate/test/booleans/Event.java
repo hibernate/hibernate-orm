cfa1a70d1719646eec3c3a9c1d002cd2c8002885
Sanne Grinovero <sanne@hibernate.org>
2022-01-15 23:30:52 +0000

    HHH-15002 Add integration test for the H2 Dialect change

---
+++ hibernate-core/src/test/java/org/hibernate/test/booleans/Event.java
@@ -0,0 +1,38 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.test.booleans;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Event {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private Boolean active;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+
+}

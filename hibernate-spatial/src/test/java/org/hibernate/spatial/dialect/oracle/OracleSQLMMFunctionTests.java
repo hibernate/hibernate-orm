6017aefdbb1e990cf97b1e1bbb1aa2b6e46ff59c
Karel Maesen <karel@geovise.com>
2022-01-23 17:04:36 +0100

    HHH-14805 Fix test class for Sql Server

--- hibernate-spatial/src/test/java/org/hibernate/spatial/dialect/oracle/OracleSQLMMFunctionTests.java
+++ hibernate-spatial/src/test/java/org/hibernate/spatial/dialect/oracle/OracleSQLMMFunctionTests.java
@@ -5,10 +5,18 @@
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
 
-package org.hibernate.spatial.integration.functions;
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+
+package org.hibernate.spatial.dialect.oracle;
 
 import org.hibernate.dialect.OracleDialect;
 import org.hibernate.spatial.HibernateSpatialConfigurationSettings;
+import org.hibernate.spatial.integration.functions.CommonFunctionTests;
 import org.hibernate.spatial.testing.dialects.oracle.OracleSTNativeSqlTemplates;
 
 import org.hibernate.testing.orm.junit.RequiresDialect;
@@ -18,11 +26,11 @@ import org.hibernate.testing.orm.junit.Setting;
 /**
  * Only for Oracle: run the tests in "OGC_STRICT" mode (i.e. using the SQL MultiMedia functions)
  */
-@RequiresDialect( value = OracleDialect.class)
+@RequiresDialect(value = OracleDialect.class)
 @ServiceRegistry(settings = {
 		@Setting(name = HibernateSpatialConfigurationSettings.ORACLE_OGC_STRICT, value = "true")
 })
-public class OracleSQLMMFunctionTests extends CommonFunctionTests{
+public class OracleSQLMMFunctionTests extends CommonFunctionTests {
 	public OracleSQLMMFunctionTests() {
 		this.templates = new OracleSTNativeSqlTemplates();
 	}

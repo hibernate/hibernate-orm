791a37832a83c29dcdb52fb8b34ff20378a0e2c6
Christian Beikov <christian.beikov@gmail.com>
2022-01-26 09:53:01 +0100

    HHH-15051 Add property mapping for target FK attributes for associations with id classes

--- hibernate-core/src/main/java/org/hibernate/persister/entity/AbstractPropertyMapping.java
+++ hibernate-core/src/main/java/org/hibernate/persister/entity/AbstractPropertyMapping.java
@@ -26,6 +26,7 @@ import org.hibernate.type.AnyType;
 import org.hibernate.type.AssociationType;
 import org.hibernate.type.CollectionType;
 import org.hibernate.type.CompositeType;
+import org.hibernate.type.EmbeddedComponentType;
 import org.hibernate.type.EntityType;
 import org.hibernate.type.ManyToOneType;
 import org.hibernate.type.OneToOneType;
@@ -409,6 +410,9 @@ public abstract class AbstractPropertyMapping implements PropertyMapping {
 			String idpath2 = extendPath( path, idPropName );
 			addPropertyPath( idpath2, idtype, columns, columnReaders, columnReaderTemplates, formulaTemplates, factory );
 			initPropertyPaths( idpath2, idtype, columns, columnReaders, columnReaderTemplates, formulaTemplates, factory );
+		}
+		else if ( (! etype.isNullable() ) && idtype.isComponentType() && idtype instanceof EmbeddedComponentType ) {
+			initComponentPropertyPaths( path, (CompositeType) idtype, columns, columnReaders, columnReaderTemplates, formulaTemplates, factory );
 		}
 	}
 

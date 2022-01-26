b6607e874a9d9080cc8c835f9a2b95abf9ac717a
Christian Beikov <christian.beikov@gmail.com>
2022-01-26 15:17:20 +0100

    Fix race condition that allowed Component.getType() to return null

--- hibernate-core/src/main/java/org/hibernate/mapping/Component.java
+++ hibernate-core/src/main/java/org/hibernate/mapping/Component.java
@@ -196,7 +196,8 @@ public class Component extends SimpleValue implements MetaAttributable, Sortable
 
 		if ( localType == null ) {
 			synchronized ( this ) {
-				if ( type == null ) {
+				localType = type;
+				if ( localType == null ) {
 					// Make sure this is sorted which is important especially for synthetic components
 					// Other components should be sorted already
 					sortProperties( true );
@@ -206,8 +207,6 @@ public class Component extends SimpleValue implements MetaAttributable, Sortable
 							: new ComponentType( this, originalPropertyOrder, getBuildingContext() );
 
 					this.type = localType;
-				} else {
-					localType = type;
 				}
 			}
 		}

6cf76688c5fecb79b30917d3cc7a04cc71b47821
Steve Ebersole <steve@hibernate.org>
2022-02-05 09:41:40 -0600

    HHH-15060 - Associations with @NotFound should always be left joined when de-referenced in HQL/Criteria
    - fix AbstractNonLazyEntityFetch wrt "referenced ModelPart"
    - `@NotFound` should disable exporting a physical foreign-key
    - tests

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/notfound/exception/NotFoundExceptionLogicalOneToOneTest.java
@@ -0,0 +1,219 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.orm.test.notfound.exception;
+
+import java.io.Serializable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+import org.hibernate.Hibernate;
+import org.hibernate.ObjectNotFoundException;
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
+
+import org.hibernate.testing.orm.junit.DomainModel;
+import org.hibernate.testing.orm.junit.FailureExpected;
+import org.hibernate.testing.orm.junit.JiraKey;
+import org.hibernate.testing.orm.junit.SessionFactory;
+import org.hibernate.testing.orm.junit.SessionFactoryScope;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.assertj.core.api.Assertions;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+
+/**
+ * Tests for `@OneToOne @NotFound(EXCEPTION)`
+ *
+ * NOTES:<ol>
+ *     <li>`@NotFound` should force the association to be eager - `Coin#currency` should be loaded immediately</li>
+ *     <li>When loading the `Coin#currency`, `EXCEPTION` should trigger an exception since the particular `Coin#currency` fk is broken</li>
+ * </ol>
+ *
+ * @author Steve Ebersole
+ */
+@DomainModel( annotatedClasses = { NotFoundExceptionLogicalOneToOneTest.Coin.class, NotFoundExceptionLogicalOneToOneTest.Currency.class } )
+@SessionFactory
+public class NotFoundExceptionLogicalOneToOneTest {
+	@Test
+	@JiraKey( "HHH-15060" )
+	public void testProxy(SessionFactoryScope scope) {
+		scope.inTransaction( (session) -> {
+			// the non-existent Child
+			final Currency proxy = session.byId( Currency.class ).getReference( 1 );
+			try {
+				Hibernate.initialize( proxy );
+				Assertions.fail( "Expecting ObjectNotFoundException" );
+			}
+			catch (ObjectNotFoundException expected) {
+				assertThat( expected.getEntityName() ).endsWith( "Currency" );
+				assertThat( expected.getIdentifier() ).isEqualTo( 1 );
+			}
+		} );
+	}
+
+	@Test
+	@JiraKey( "HHH-15060" )
+	@FailureExpected(
+			reason = "We return a proxy here for `Coin#currency`, which violates NOTE #1.  The exception happens " +
+					"when we reference the proxy, but thats not correct"
+	)
+	public void testGet(SessionFactoryScope scope) {
+		scope.inTransaction( (session) -> {
+			try {
+				session.get( Coin.class, 1 );
+				fail( "Expecting ObjectNotFoundException" );
+			}
+			catch (ObjectNotFoundException expected) {
+				assertThat( expected.getEntityName() ).isEqualTo( Currency.class.getName() );
+				assertThat( expected.getIdentifier() ).isEqualTo( 1 );
+			}
+		} );
+	}
+
+	@Test
+	@JiraKey( "HHH-15060" )
+	@FailureExpected(
+			reason = "We return a proxy here for `Coin#currency`, which violates NOTE #1.  The exception happens " +
+					"when we reference the proxy, but thats not correct"
+	)
+	public void testQueryImplicitPathDereferencePredicate(SessionFactoryScope scope) {
+		scope.inTransaction( (session) -> {
+			final String hql = "select c from Coin c where c.currency.id = 1";
+			try {
+				session.createQuery( hql, Coin.class ).getResultList();
+				fail( "Expecting ObjectNotFoundException for broken fk" );
+			}
+			catch (ObjectNotFoundException expected) {
+				assertThat( expected.getEntityName() ).isEqualTo( Currency.class.getName() );
+				assertThat( expected.getIdentifier() ).isEqualTo( 1 );
+			}
+		} );
+	}
+
+	@Test
+	@JiraKey( "HHH-15060" )
+	@FailureExpected(
+			reason = "We return a proxy here for `Coin#currency`, which violates NOTE #1.  The exception happens " +
+					"when we reference the proxy, but thats not correct"
+	)
+	public void testQueryOwnerSelection(SessionFactoryScope scope) {
+		scope.inTransaction( (session) -> {
+			final String hql = "select c from Coin c";
+			try {
+				session.createQuery( hql, Coin.class ).getResultList();
+				fail( "Expecting ObjectNotFoundException for broken fk" );
+			}
+			catch (ObjectNotFoundException expected) {
+				assertThat( expected.getEntityName() ).isEqualTo( Currency.class.getName() );
+				assertThat( expected.getIdentifier() ).isEqualTo( 1 );
+			}
+		} );
+	}
+
+	@BeforeEach
+	public void prepareTestData(SessionFactoryScope scope) {
+		scope.inTransaction( (session) -> {
+			Currency euro = new Currency( 1, "Euro" );
+			Coin fiveC = new Coin( 1, "Five cents", euro );
+
+			session.persist( euro );
+			session.persist( fiveC );
+		} );
+
+		scope.inTransaction( (session) -> {
+			session.createMutationQuery( "delete Currency where id = 1" ).executeUpdate();
+		} );
+	}
+
+	@AfterEach
+	public void cleanupTest(SessionFactoryScope scope) throws Exception {
+		scope.inTransaction( (session) -> {
+			session.createMutationQuery( "delete Coin where id = 1" ).executeUpdate();
+		} );
+	}
+
+	@Entity(name = "Coin")
+	public static class Coin {
+		private Integer id;
+		private String name;
+		private Currency currency;
+
+		public Coin() {
+		}
+
+		public Coin(Integer id, String name, Currency currency) {
+			this.id = id;
+			this.name = name;
+			this.currency = currency;
+		}
+
+		@Id
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		@OneToOne(fetch = FetchType.LAZY)
+		@NotFound(action = NotFoundAction.EXCEPTION)
+		public Currency getCurrency() {
+			return currency;
+		}
+
+		public void setCurrency(Currency currency) {
+			this.currency = currency;
+		}
+	}
+
+	@Entity(name = "Currency")
+	public static class Currency implements Serializable {
+		private Integer id;
+		private String name;
+
+		public Currency() {
+		}
+
+		public Currency(Integer id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		@Id
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+	}
+
+}

4a67817b0f72844dc7b7c49fdaa0aca485dacdce
Jan Schatteman <jschatte@redhat.com>
2022-03-03 00:06:14 +0100

    Removed deprecated InFlightMetadataCollector.getClassmateContext()
    Signed-off-by: Jan Schatteman <jschatte@redhat.com>

--- hibernate-core/src/main/java/org/hibernate/boot/spi/InFlightMetadataCollector.java
+++ hibernate-core/src/main/java/org/hibernate/boot/spi/InFlightMetadataCollector.java
@@ -295,17 +295,6 @@ public interface InFlightMetadataCollector extends Mapping, MetadataImplementor 
 	NaturalIdUniqueKeyBinder locateNaturalIdUniqueKeyBinder(String entityName);
 	void registerNaturalIdUniqueKeyBinder(String entityName, NaturalIdUniqueKeyBinder ukBinder);
 
-	/**
-	 * Access to the shared Classmate objects used throughout Hibernate's
-	 * bootstrap process.
-	 *
-	 * @return Access to the shared Classmate delegates.
-	 *
-	 * @deprecated Use {@link BootstrapContext#getClassmateContext()} instead.
-	 */
-	@Deprecated
-	ClassmateContext getClassmateContext();
-
 	void registerValueMappingResolver(Function<MetadataBuildingContext,Boolean> resolver);
 
 	void addJavaTypeRegistration(Class<?> javaType, JavaType<?> jtd);

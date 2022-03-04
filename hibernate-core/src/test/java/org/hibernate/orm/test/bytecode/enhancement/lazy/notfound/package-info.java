ceb7df0c510d580ce487b1f05940f6b291848489
Steve Ebersole <steve@hibernate.org>
2022-03-04 07:11:06 -0600

    HHH-15099 - Improve handling of associations marked with @NotFound
    - Javadoc improvements for `@NotFound`-related tests

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/bytecode/enhancement/lazy/notfound/package-info.java
@@ -0,0 +1,17 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
+
+/**
+ * Tests for how {@link org.hibernate.annotations.NotFound} mappings
+ * work with bytecode enhancement.
+ *
+ * See tests in {@link org.hibernate.orm.test.notfound} for tests
+ * specific to {@link org.hibernate.annotations.NotFound} mappings
+ *
+ * @author Steve Ebersole
+ */
+package org.hibernate.orm.test.bytecode.enhancement.lazy.notfound;

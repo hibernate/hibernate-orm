3b6776b2765ea54afcf3043293f0af594788343d
CHAPEL Guillaume <guillaume.chapel@soprasteria.com>
2022-03-11 22:31:46 +0100

    HHH-15118 Fix duplicate ids with PooledOptimizer when sequence value is initialValue

--- hibernate-core/src/main/java/org/hibernate/id/enhanced/PooledOptimizer.java
+++ hibernate-core/src/main/java/org/hibernate/id/enhanced/PooledOptimizer.java
@@ -70,22 +70,21 @@ public class PooledOptimizer extends AbstractOptimizer implements InitialValueAw
 		final GenerationState generationState = locateGenerationState( callback.getTenantIdentifier() );
 
 		if ( generationState.hiValue == null ) {
-			generationState.value = callback.getNextValue();
+			generationState.hiValue = callback.getNextValue();
 			// unfortunately not really safe to normalize this
 			// to 1 as an initial value like we do for the others
 			// because we would not be able to control this if
 			// we are using a sequence...
-			if ( generationState.value.lt( 1 ) ) {
-				log.pooledOptimizerReportedInitialValue( generationState.value );
+			if ( generationState.hiValue.lt( 1 ) ) {
+				log.pooledOptimizerReportedInitialValue( generationState.hiValue );
 			}
 			// the call to obtain next-value just gave us the initialValue
 			if ( ( initialValue == -1
-					&& generationState.value.lt( incrementSize ) )
-					|| generationState.value.eq( initialValue ) ) {
-				generationState.hiValue = callback.getNextValue();
+					&& generationState.hiValue.lt( incrementSize ) )
+					|| generationState.hiValue.eq( initialValue ) ) {
+				generationState.value = generationState.hiValue.copy();
 			}
 			else {
-				generationState.hiValue = generationState.value;
 				generationState.value = generationState.hiValue.copy().subtract( incrementSize - 1 );
 			}
 		}

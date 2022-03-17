94f450aa7ae9d346ca79d60d1ef3f3fd4b2a41e8
Christian Beikov <christian.beikov@gmail.com>
2022-03-17 17:13:10 +0100

    HHH-15131 Fix JPA Compliance issue with Envers

---
+++ hibernate-envers/src/test/java/org/hibernate/envers/test/integration/query/StrictJpaComplianceTest.java
@@ -0,0 +1,68 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.envers.test.integration.query;
+
+import java.util.Map;
+
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.envers.AuditReaderFactory;
+import org.hibernate.envers.Audited;
+import org.hibernate.orm.test.envers.BaseEnversJPAFunctionalTestCase;
+
+import org.hibernate.testing.transaction.TransactionUtil;
+import org.junit.Test;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@SuppressWarnings("unchecked")
+public class StrictJpaComplianceTest extends BaseEnversJPAFunctionalTestCase {
+	@Override
+	protected void addConfigOptions(Map options) {
+		super.addConfigOptions( options );
+		options.put( AvailableSettings.JPA_QUERY_COMPLIANCE, "true" );
+	}
+
+	@Override
+	protected Class[] getAnnotatedClasses() {
+		return new Class[] {
+				Organization.class
+		};
+	}
+
+	@Test
+	public void testIt() {
+		TransactionUtil.doInJPA( this::entityManagerFactory, entityManager -> {
+			AuditReaderFactory.get( entityManager ).getRevisions( Organization.class, 1 );
+		} );
+
+	}
+
+	/**
+	 * @author Madhumita Sadhukhan
+	 */
+	@Entity
+	@Table(name = "ORG")
+	public static class Organization {
+
+		@Id
+		@GeneratedValue
+		@Audited
+		private int id;
+
+		@Audited
+		@Column(name = "ORG_NAME")
+		private String name;
+
+		public Organization() {
+		}
+
+	}
+}

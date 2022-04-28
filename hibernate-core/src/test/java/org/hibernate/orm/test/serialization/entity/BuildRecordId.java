5f0c215d6dc818127e963d5c95b8fc348cb6a260
Andrea Boriero <andrea@hibernate.org>
2022-04-28 18:14:34 +0200

    HHH-14843 Add test for issue

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/serialization/entity/BuildRecordId.java
@@ -0,0 +1,43 @@
+package org.hibernate.orm.test.serialization.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class BuildRecordId implements Serializable {
+	private long id;
+
+	public BuildRecordId() {
+	}
+
+	public BuildRecordId(long id) {
+		this.id = id;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if ( this == o ) {
+			return true;
+		}
+		if ( o == null || getClass() != o.getClass() ) {
+			return false;
+		}
+		BuildRecordId longId = (BuildRecordId) o;
+		return id == longId.id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash( id );
+	}
+
+}

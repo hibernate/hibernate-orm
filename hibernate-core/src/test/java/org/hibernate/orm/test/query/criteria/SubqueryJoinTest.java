e5c408d51ede69a2884506591009a06b387c1bb7
Thomas Heigl <thomas.heigl@gmail.com>
2022-05-09 20:06:02 +0200

    HHH-15260 Add issue key

--- hibernate-core/src/test/java/org/hibernate/orm/test/query/criteria/SubqueryJoinTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/query/criteria/SubqueryJoinTest.java
@@ -3,6 +3,7 @@ package org.hibernate.orm.test.query.criteria;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.testing.TestForIssue;
 import org.hibernate.testing.orm.junit.EntityManagerFactoryScope;
 import org.hibernate.testing.orm.junit.Jpa;
 import org.junit.jupiter.api.Test;
@@ -29,24 +30,25 @@ import jakarta.validation.constraints.NotNull;
 public class SubqueryJoinTest {
 
 	@Test
+	@TestForIssue(jiraKey = "HHH-15260")
 	public void subqueryJoinTest(EntityManagerFactoryScope scope) {
 		scope.inEntityManager(
 				entityManager -> {
 					final CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-					
-					final CriteriaQuery<TestContext> c = cb.createQuery(TestContext.class);
-					final Root<TestContext> from = c.from(TestContext.class);
 
-					final Subquery<TestUser.TestMarker> subQuery = c.subquery(TestUser.TestMarker.class);
-					final Root<TestUser> sRoot = subQuery.from(TestUser.class);
-					final Join<TestUser, TestUser.TestMarker> join = sRoot.join( "markers");
-					subQuery.where(cb.equal(sRoot.get("id"), from.get("user").get("id")));
-					subQuery.select(join);
+					final CriteriaQuery<TestContext> c = cb.createQuery( TestContext.class );
+					final Root<TestContext> from = c.from( TestContext.class );
 
-					from.join("user");
-					c.where(cb.and(cb.exists(subQuery).not()));
+					final Subquery<TestUser.TestMarker> subQuery = c.subquery( TestUser.TestMarker.class );
+					final Root<TestUser> sRoot = subQuery.from( TestUser.class );
+					final Join<TestUser, TestUser.TestMarker> join = sRoot.join( "markers" );
+					subQuery.where( cb.equal( sRoot.get( "id" ), from.get( "user" ).get( "id" ) ) );
+					subQuery.select( join );
 
-					entityManager.createQuery(c).getResultList();
+					from.join( "user" );
+					c.where( cb.and( cb.exists( subQuery ).not() ) );
+
+					entityManager.createQuery( c ).getResultList();
 				}
 		);
 	}

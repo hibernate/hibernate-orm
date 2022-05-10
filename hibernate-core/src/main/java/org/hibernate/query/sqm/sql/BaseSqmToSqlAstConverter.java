f37ae15db5bcd42003f3b35f403e243711a7ca1a
Andrea Boriero <andrea@hibernate.org>
2022-05-10 16:27:35 +0200

    HHH-15260 Criteria query is unable to determine TableReference when combining subquery with join

--- hibernate-core/src/main/java/org/hibernate/query/sqm/sql/BaseSqmToSqlAstConverter.java
+++ hibernate-core/src/main/java/org/hibernate/query/sqm/sql/BaseSqmToSqlAstConverter.java
@@ -3105,6 +3105,9 @@ public abstract class BaseSqmToSqlAstConverter<T extends Statement> extends Base
 				}
 				else {
 					tableGroup = compatibleTableGroup;
+					// Also register the table group under its original navigable path, which possibly contains an alias
+					// This is important, as otherwise we might create new joins in subqueries which are unnecessary
+					fromClauseIndex.registerTableGroup( tableGroup.getNavigablePath(), tableGroup );
 				}
 			}
 

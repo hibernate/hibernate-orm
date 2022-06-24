bb29e3b060109e5965b8a62a513c859f5fc09cfa
Gavin King <gavin@hibernate.org>
2022-06-24 15:48:41 +0200

    document root joins

--- documentation/src/test/java/org/hibernate/userguide/hql/HQLTest.java
+++ documentation/src/test/java/org/hibernate/userguide/hql/HQLTest.java
@@ -309,6 +309,23 @@ public class HQLTest extends BaseEntityManagerFunctionalTestCase {
 	}
 
 	@Test
+	public void test_hql_explicit_root_join_example_1() {
+		doInJPA(this::entityManagerFactory, entityManager -> {
+			//tag::hql-explicit-root-join-example[]
+			List<Person> persons = entityManager.createQuery(
+				"select distinct pr " +
+				"from Person pr " +
+				"join Phone ph on ph.person = pr " +
+				"where ph.type = :phoneType",
+				Person.class)
+			.setParameter("phoneType", PhoneType.MOBILE)
+			.getResultList();
+			//end::hql-explicit-root-join-example[]
+			assertEquals(1, persons.size());
+		});
+	}
+
+	@Test
 	public void test_hql_explicit_inner_join_example_1() {
 		doInJPA(this::entityManagerFactory, entityManager -> {
 			//tag::hql-explicit-inner-join-example[]

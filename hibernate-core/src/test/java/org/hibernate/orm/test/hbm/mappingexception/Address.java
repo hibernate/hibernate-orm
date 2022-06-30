c553d35a86eac07a3e8c2ab46786d014c4bf613c
Andrea Boriero <andrea@hibernate.org>
2022-06-30 12:03:47 +0200

    HHH-15379 Add test for issue

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/hbm/mappingexception/Address.java
@@ -0,0 +1,23 @@
+package org.hibernate.orm.test.hbm.mappingexception;
+
+
+public class Address {
+	private Long id;
+	private String details;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+
+	public void setDetails(String details) {
+		this.details = details;
+	}
+}

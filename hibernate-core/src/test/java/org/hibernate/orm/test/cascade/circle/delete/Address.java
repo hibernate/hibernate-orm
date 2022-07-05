4b943ec08c559dddb094193b69f8cff8ee3ff802
Andrea Boriero <andrea@hibernate.org>
2022-07-05 14:52:52 +0200

    HHH-15218 Add test for issue

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/cascade/circle/delete/Address.java
@@ -0,0 +1,49 @@
+package org.hibernate.orm.test.cascade.circle.delete;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    private Person person;
+
+    private String description;
+
+    public Address() {
+    }
+
+    public Address(String description) {
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

28dc63c09bfef201ce23f8daf06c4b1670e97a8a
blafond <blafond@redhat.com>
2022-07-05 15:17:19 -0500

    HBX-2383: Update version identifier to 6.1.1.Final
    Signed-off-by: blafond <blafond@redhat.com>

--- tooling/hibernate-reveng/src/main/java/org/hibernate/tool/api/version/Version.java
+++ tooling/hibernate-reveng/src/main/java/org/hibernate/tool/api/version/Version.java
@@ -2,6 +2,6 @@ package org.hibernate.tool.api.version;
 
 public interface Version {
 	
-	final static String CURRENT_VERSION = "6.1.1-SNAPSHOT";
+	final static String CURRENT_VERSION = "6.1.1.Final";
 
 }

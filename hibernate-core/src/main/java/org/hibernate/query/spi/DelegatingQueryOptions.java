8a892906a91380461a63dedf7fb2519185e6d0e0
Andrea Boriero <andrea@hibernate.org>
2022-08-10 16:53:39 +0200

    HHH-15479 Add ListResultsConsumer.UniqueSemantic to QueryOptions

--- hibernate-core/src/main/java/org/hibernate/query/spi/DelegatingQueryOptions.java
+++ hibernate-core/src/main/java/org/hibernate/query/spi/DelegatingQueryOptions.java
@@ -16,6 +16,7 @@ import org.hibernate.LockOptions;
 import org.hibernate.graph.spi.AppliedGraph;
 import org.hibernate.query.ResultListTransformer;
 import org.hibernate.query.TupleTransformer;
+import org.hibernate.sql.results.spi.ListResultsConsumer;
 
 /**
  * @author Christian Beikov
@@ -126,5 +127,10 @@ public class DelegatingQueryOptions implements QueryOptions {
 	@Override
 	public boolean hasLimit() {
 		return queryOptions.hasLimit();
+	}
+
+	@Override
+	public ListResultsConsumer.UniqueSemantic getUniqueSemantic() {
+		return queryOptions.getUniqueSemantic();
 	}
 }

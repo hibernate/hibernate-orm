c4b2920dce6405fab836fa8d2eff7aa830e34e80
Christian Beikov <christian.beikov@gmail.com>
2022-09-20 09:26:06 +0200

    HHH-15533 Introduce versionless CI matrix entries for the PostgreSQL and MySQL minimum supported version

--- ci/build.sh
+++ ci/build.sh
@@ -11,7 +11,7 @@ elif [ "$RDBMS" == "mysql" ]; then
   goal="-Pdb=mysql_ci"
 elif [ "$RDBMS" == "mariadb" ]; then
   goal="-Pdb=mariadb_ci"
-elif [ "$RDBMS" == "postgresql_9_5" ]; then
+elif [ "$RDBMS" == "postgresql" ]; then
   goal="-Pdb=pgsql_ci"
 elif [ "$RDBMS" == "postgresql_13" ]; then
   goal="-Pdb=pgsql_ci"

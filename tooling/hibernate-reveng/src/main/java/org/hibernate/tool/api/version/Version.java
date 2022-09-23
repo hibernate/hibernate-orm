1dd62df1660fd51c642dc08c5d5c686bd6e1a55a
Koen Aers <koen.aers@gmail.com>
2022-09-23 11:18:40 +0200

    HBX-2403: Update Hibernate ORM Dependency to Version 6.1.3.Final
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- tooling/hibernate-reveng/src/main/java/org/hibernate/tool/api/version/Version.java
+++ tooling/hibernate-reveng/src/main/java/org/hibernate/tool/api/version/Version.java
@@ -2,6 +2,6 @@ package org.hibernate.tool.api.version;
 
 public interface Version {
 	
-	final static String CURRENT_VERSION = "6.1.3-SNAPSHOT";
+	final static String CURRENT_VERSION = "6.1.3.Final";
 
 }

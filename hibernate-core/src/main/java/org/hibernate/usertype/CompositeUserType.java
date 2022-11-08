86f793c3afac4ef7f30e65637baa29ef43416113
Gavin King <gavin@hibernate.org>
2022-11-08 08:56:43 +0100

    work on javadoc for UserType and CompositeUserType

--- hibernate-core/src/main/java/org/hibernate/usertype/CompositeUserType.java
+++ hibernate-core/src/main/java/org/hibernate/usertype/CompositeUserType.java
@@ -15,39 +15,47 @@ import org.hibernate.metamodel.spi.EmbeddableInstantiator;
 import org.hibernate.metamodel.spi.ValueAccess;
 
 /**
- * A {@link UserType} that may be dereferenced in a query.
+ * This interface should be implemented by user-defined custom types
+ * that have persistent attributes and can be thought of as something
+ * more like an {@linkplain jakarta.persistence.Embeddable embeddable}
+ * object. However, these persistent "attributes" need not necessarily
+ * correspond directly to Java fields or properties.
  * <p>
- * This interface allows a custom type to define "properties".
- * These need not necessarily correspond to physical JavaBeans
- * style properties.
+ * A value type managed by a {@code CompositeUserType} may be used in
+ * almost every way that a regular embeddable type may be used. It may
+ * even contain many-to-one associations.
  * <p>
- * A {@code CompositeUserType} may be used in almost every way
- * that an {@linkplain jakarta.persistence.Embeddable embeddable}
- * type may be used. It may even contain many-to-one associations.
+ * To "map" the attributes of a composite custom type, each
+ * {@code CompositeUserType} provides a {@linkplain #embeddable()
+ * regular embeddable class} with the same logical structure as the
+ * {@linkplain #returnedClass() value type managed by the custom
+ * type}.
  * <p>
- * Implementors must be immutable and must declare a public
- * default constructor.
+ * For example, if we implement a {@code CompositeUserType} for a
+ * {@code MonetaryAmount} class, we would also provide a
+ * {@code MonetaryAmountEmbeddable} class with a field for each
+ * logical persistent attribute of the custom type. Of course,
+ * {@code MonetaryAmountEmbeddable} is never instantiated at runtime,
+ * and is never referenced in any entity class. It is a source of
+ * metadata only.
  * <p>
- * Unlike {@code UserType}, cacheability does not depend upon
- * serializability. Instead, {@link #assemble(Serializable, Object)}
- * and {@link #disassemble(Object)} provide conversion to and
- * from a cacheable representation.
+ * Properties of this embeddable class are sorted alphabetically by
+ * name, and assigned an index based on this ordering.
  * <p>
- * Properties are ordered by name, that is, they are sprted
- * alphabetically such that
- * {@code properties[i].name < properties[i+1].name}
- * for all {@code i >= 0}.
+ * Every implementor of {@code CompositeUserType} must be immutable
+ * and must declare a public default constructor.
  */
 @Incubating
 public interface CompositeUserType<J> extends EmbeddableInstantiator {
 
 	/**
-	 * Get the value of a property.
+	 * Get the value of the property with the given index. Properties
+	 * of the {@link #embeddable()} are sorted by name and assigned an
+	 * index based on this ordering.
 	 *
 	 * @param component an instance of class mapped by this "type"
 	 * @param property the property index
 	 * @return the property value
-	 * @throws HibernateException
 	 */
 	Object getPropertyValue(J component, int property) throws HibernateException;
 
@@ -56,15 +64,11 @@ public interface CompositeUserType<J> extends EmbeddableInstantiator {
 
 	/**
 	 * The class that represents the embeddable mapping of the type.
-	 *
-	 * @return Class
 	 */
 	Class<?> embeddable();
 
 	/**
 	 * The class returned by {@code instantiate()}.
-	 *
-	 * @return Class
 	 */
 	Class<J> returnedClass();
 

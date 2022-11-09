13c53b6ef1cf2c4d39ca3d7139e5eee21a56bc04
Jan Schatteman <jschatte@redhat.com>
2022-11-09 16:32:04 +0100

    HHH-13106 - Remove erroneous (incomplete) test and add a new one for this issue
    Signed-off-by: Jan Schatteman <jschatte@redhat.com>

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/schemavalidation/IdentityGenerationValidationTest.java
@@ -0,0 +1,60 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.orm.test.schemavalidation;
+
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.dialect.PostgreSQLDialect;
+import org.hibernate.tool.hbm2ddl.SchemaValidator;
+
+import org.hibernate.testing.RequiresDialect;
+import org.hibernate.testing.TestForIssue;
+import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
+import org.junit.Test;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+/**
+ * @author Jan Schatteman
+ */
+@RequiresDialect(PostgreSQLDialect.class)
+@TestForIssue( jiraKey = "HHH-13106" )
+public class IdentityGenerationValidationTest extends BaseCoreFunctionalTestCase {
+
+	@Override
+	protected Class<?>[] getAnnotatedClasses() {
+		return new Class<?>[] {TestEntity.class};
+	}
+
+	@Test
+	public void testSynonymUsingIndividuallySchemaValidator() {
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().build();
+		try {
+			final MetadataSources metadataSources = new MetadataSources( ssr );
+			metadataSources.addAnnotatedClass( TestEntity.class );
+
+			new SchemaValidator().validate( metadataSources.buildMetadata() );
+		}
+		finally {
+			StandardServiceRegistryBuilder.destroy( ssr );
+		}
+	}
+
+	@Entity
+	@Table(name = "test_entity")
+	private static class TestEntity {
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Long id;
+	}
+
+}

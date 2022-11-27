c9caf292e693478f40d34e8c05eafacdadf9466f
Gavin <gavin@hibernate.org>
2022-11-27 01:17:21 +0100

    HHH-15770 javadoc and cleanup for @OnDelete, OnDeleteAction

--- hibernate-core/src/main/java/org/hibernate/annotations/OnDelete.java
+++ hibernate-core/src/main/java/org/hibernate/annotations/OnDelete.java
@@ -14,11 +14,15 @@ import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-
 /**
- * Strategy to use on collections, arrays and on joined subclasses delete.
- *
- * OnDelete of secondary tables currently not supported.
+ * Specifies an {@code on delete} action for a foreign key constraint.
+ * The most common usage is {@code @OnDelete(action = CASCADE)}.
+ * Note that this results in an {@code on delete cascade} clause in
+ * the DDL definition of the foreign key. It's completely different
+ * to {@link jakarta.persistence.CascadeType#REMOVE}.
+ * <p>
+ * Like database triggers, {@code on delete} actions can cause state
+ * held in memory to lose synchronization with the database.
  *
  * @author Emmanuel Bernard
  */
@@ -26,7 +30,8 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Retention(RUNTIME)
 public @interface OnDelete {
 	/**
-	 * The on-delete action.
+	 * The action to taken by the database when deletion of a row
+	 * would cause the constraint to be violated.
 	 */
 	OnDeleteAction action();
 }

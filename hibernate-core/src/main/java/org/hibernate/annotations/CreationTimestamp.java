0b81064bbd7b91d7473cb596959000096cd389a9
Gavin <gavin@hibernate.org>
2022-11-28 19:22:56 +0100

    @CreationTimestamp should not be declared @Inherited

--- hibernate-core/src/main/java/org/hibernate/annotations/CreationTimestamp.java
+++ hibernate-core/src/main/java/org/hibernate/annotations/CreationTimestamp.java
@@ -8,9 +8,9 @@ package org.hibernate.annotations;
 
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import org.hibernate.tuple.CreationTimestampGeneration;
@@ -29,7 +29,7 @@ import org.hibernate.tuple.CreationTimestampGeneration;
  * @see CurrentTimestamp
  */
 @ValueGenerationType(generatedBy = CreationTimestampGeneration.class)
-@Retention(RetentionPolicy.RUNTIME)
+@Retention(RUNTIME)
 @Target({ FIELD, METHOD })
 public @interface CreationTimestamp {
 	/**

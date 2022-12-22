d49518e5e4122f9ecb7c2235a04c212e62db87aa
Steve Ebersole <steve@hibernate.org>
2022-12-22 13:03:03 -0600

    HHH-15798 - Clean up uses of NotYetImplementedFor6Exceptions

--- hibernate-core/src/main/java/org/hibernate/NotImplementedYetException.java
+++ hibernate-core/src/main/java/org/hibernate/NotImplementedYetException.java
@@ -10,6 +10,10 @@ package org.hibernate;
  * Marker interface for exceptions that indicate that something hasn't been implemented yet for a certain version
  *
  * @author Jan Schatteman
+ *
+ * @deprecated By definition, something "not yet implemented" is something we are actively seeking to remove
  */
+@SuppressWarnings("DeprecatedIsStillUsed")
+@Deprecated(forRemoval = true)
 public interface NotImplementedYetException {
 }

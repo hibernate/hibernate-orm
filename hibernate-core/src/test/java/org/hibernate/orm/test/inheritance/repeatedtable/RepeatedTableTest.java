61d9119884d655e6ae11230f302dc372c32cd2c2
Gavin <gavin@hibernate.org>
2022-12-25 12:36:03 +0100

    add failing test for @ManyToOne referencing secondary table

--- hibernate-core/src/test/java/org/hibernate/orm/test/inheritance/repeatedtable/RepeatedTableTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/inheritance/repeatedtable/RepeatedTableTest.java
@@ -4,6 +4,7 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.annotations.DiscriminatorOptions;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.testing.TestForIssue;
 import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
 import org.junit.Test;
 
@@ -19,6 +20,7 @@ import static junit.framework.TestCase.assertTrue;
 import static org.hibernate.cfg.AvailableSettings.FORMAT_SQL;
 import static org.hibernate.cfg.AvailableSettings.SHOW_SQL;
 
+@TestForIssue(jiraKey = "HHH-14526")
 public class RepeatedTableTest extends BaseCoreFunctionalTestCase {
 
 	@Override

7f2e2c5d794c0cb0021302f5df21bc284dad4bdf
Gavin <gavin@hibernate.org>
2022-12-26 17:23:34 +0100

    spaces to tabs

--- hibernate-core/src/main/java/org/hibernate/boot/model/internal/InheritanceState.java
+++ hibernate-core/src/main/java/org/hibernate/boot/model/internal/InheritanceState.java
@@ -201,10 +201,10 @@ public class InheritanceState {
 	}
 
 	/*
-     * Get the annotated elements and determine access type from hierarchy,
-     * guessing from @Id or @EmbeddedId presence if not specified.
-     * Change EntityBinder by side effect
-     */
+	 * Get the annotated elements and determine access type from hierarchy,
+	 * guessing from @Id or @EmbeddedId presence if not specified.
+	 * Change EntityBinder by side effect
+	 */
 	private ElementsToProcess getElementsToProcess() {
 		if ( elementsToProcess == null ) {
 			InheritanceState inheritanceState = inheritanceStatePerClass.get( clazz );
@@ -249,12 +249,12 @@ public class InheritanceState {
 				return AccessType.getAccessStrategy( xclass.getAnnotation( Access.class ).value() );
 			}
 		}
-        // Guess from identifier.
-        // FIX: Shouldn't this be determined by the first attribute (i.e., field or property) with annotations, but without an
-        //      explicit Access annotation, according to JPA 2.0 spec 2.3.1: Default Access Type?
+		// Guess from identifier.
+		// FIX: Shouldn't this be determined by the first attribute (i.e., field or property) with annotations,
+		// but without an explicit Access annotation, according to JPA 2.0 spec 2.3.1: Default Access Type?
 		for ( XClass xclass = clazz;
-			  xclass != null && !Object.class.getName().equals( xclass.getName() );
-			  xclass = xclass.getSuperclass() ) {
+				xclass != null && !Object.class.getName().equals( xclass.getName() );
+				xclass = xclass.getSuperclass() ) {
 			if ( xclass.isAnnotationPresent( Entity.class ) || xclass.isAnnotationPresent( MappedSuperclass.class ) ) {
 				for ( XProperty prop : xclass.getDeclaredProperties( AccessType.PROPERTY.getType() ) ) {
 					final boolean isEmbeddedId = prop.isAnnotationPresent( EmbeddedId.class );

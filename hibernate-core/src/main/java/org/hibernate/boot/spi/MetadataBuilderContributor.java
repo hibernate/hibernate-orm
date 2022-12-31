1db1c08d3b593b491f0efc32caa64a5ac256926a
Gavin <gavin@hibernate.org>
2022-12-31 13:27:03 +0100

    document a couple of limitations

--- hibernate-core/src/main/java/org/hibernate/boot/spi/MetadataBuilderContributor.java
+++ hibernate-core/src/main/java/org/hibernate/boot/spi/MetadataBuilderContributor.java
@@ -14,13 +14,18 @@ import org.hibernate.boot.MetadataBuilder;
  * In implementation may be registered with the JPA provider using the property
  * {@value org.hibernate.jpa.boot.spi.JpaSettings#METADATA_BUILDER_CONTRIBUTOR}.
  *
+ * @apiNote Currently, this API is only supported in the JPA persistence provider.
+ *          It's unfortunate that implementations are not discoverable like
+ *          {@link org.hibernate.boot.model.TypeContributor} and
+ *          {@link org.hibernate.boot.model.FunctionContributor}.
+ *
  * @author Vlad Mihalcea
  *
  * @since 5.3
  */
 public interface MetadataBuilderContributor {
 	/**
-	 * Perform the process of contributing to MetadataSources.
+	 * Perform the process of contributing to the {@link MetadataBuilder}.
 	 *
 	 * @param metadataBuilder The {@link MetadataBuilder}, to which to contribute.
 	 */

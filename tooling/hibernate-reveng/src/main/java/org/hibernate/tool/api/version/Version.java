a75d7764fc8a3aa61220c021f912b624f2ece87f
blafond <blafond@redhat.com>
2023-01-03 09:25:55 -0600

    HBX-2468: Update version identifier to 6.2.0-SNAPSHOT
    Signed-off-by: blafond <blafond@redhat.com>

--- tooling/hibernate-reveng/src/main/java/org/hibernate/tool/api/version/Version.java
+++ tooling/hibernate-reveng/src/main/java/org/hibernate/tool/api/version/Version.java
@@ -2,6 +2,6 @@ package org.hibernate.tool.api.version;
 
 public interface Version {
 	
-	final static String CURRENT_VERSION = "6.1.7-SNAPSHOT";
+	final static String CURRENT_VERSION = "6.2.0-SNAPSHOT";
 
 }

e3f1c2741d0981291f142c65a8d21d7edd5ba750
Gavin <gavin@hibernate.org>
2023-01-04 11:54:57 +0100

    HHH-15960 make @Comment annotation repeatable and properly test it
    I didn't quite nail this one first time round :-/

---
+++ hibernate-core/src/main/java/org/hibernate/annotations/Comments.java
@@ -0,0 +1,34 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.annotations;
+
+import org.hibernate.binder.internal.CommentBinder;
+import org.hibernate.binder.internal.CommentsBinder;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * A list of {@link Comment}s.
+ * <p>
+ * If there are multiple {@link Comment}s on a class or attribute,
+ * they must have distinct {@link Comment#on() on} members.
+ *
+ * @author Gavin King
+ */
+@TypeBinderType(binder = CommentsBinder.class)
+@AttributeBinderType(binder = CommentsBinder.class)
+@Target({METHOD, FIELD, TYPE})
+@Retention(RUNTIME)
+public @interface Comments {
+	Comment[] value();
+}

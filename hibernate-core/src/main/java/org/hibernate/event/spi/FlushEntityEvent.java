74e4b4cb7b631c3d4f8962214c318b1d57675f4c
Sanne Grinovero <sanne@hibernate.org>
2023-01-15 23:19:50 +0000

    HHH-16047 Allow reusing of FlushEntityEvent instances

--- hibernate-core/src/main/java/org/hibernate/event/spi/FlushEntityEvent.java
+++ hibernate-core/src/main/java/org/hibernate/event/spi/FlushEntityEvent.java
@@ -12,15 +12,18 @@ import org.hibernate.engine.spi.EntityEntry;
  * @author Gavin King
  */
 public class FlushEntityEvent extends AbstractEvent {
-	private final Object entity;
+
+	private Object entity;
 	private Object[] propertyValues;
 	private Object[] databaseSnapshot;
 	private int[] dirtyProperties;
 	private boolean hasDirtyCollection;
 	private boolean dirtyCheckPossible;
 	private boolean dirtyCheckHandledByInterceptor;
-	private final EntityEntry entityEntry;
-	
+	private EntityEntry entityEntry;
+	private boolean allowedToReuse;//allows this event instance to be reused for multiple events: special case to GC
+	private int instanceGenerationId;//in support of event instance reuse: to double check no recursive/nested use is happening
+
 	public FlushEntityEvent(EventSource source, Object entity, EntityEntry entry) {
 		super(source);
 		this.entity = entity;
@@ -74,5 +77,46 @@ public class FlushEntityEvent extends AbstractEvent {
 	}
 	public Object getEntity() {
 		return entity;
+	}
+
+	/**
+	 * This is a terrible anti-pattern, but particular circumstances call for being
+	 * able to reuse the same event instance: this is otherwise allocated in hot loops
+	 * and since each event is escaping the scope it's actually causing allocation issues.
+	 * The flush event does not appear to be used recursively so this is currently safe to
+	 * do, nevertheless we add an allowedToReuse flag to ensure only instances whose
+	 * purpose has completed are being reused.
+	 * N.B. two out of three parameters from the constructor are reset: the same EventSource is implied
+	 * on reuse.
+	 * @param entity same as constructor parameter
+	 * @param entry same as constructor parameter
+	 */
+	public void resetAndReuseEventInstance(Object entity, EntityEntry entry) {
+		this.entity = entity;
+		this.entityEntry = entry;
+		this.allowedToReuse = false;
+		//and reset other fields to the default:
+		this.propertyValues = null;
+		this.databaseSnapshot = null;
+		this.dirtyProperties = null;
+		this.hasDirtyCollection = false;
+		this.dirtyCheckPossible = false;
+		this.dirtyCheckHandledByInterceptor = false;
+	}
+
+	public boolean isAllowedToReuse() {
+		return this.allowedToReuse;
+	}
+
+	public void setAllowedToReuse(final boolean allowedToReuse) {
+		this.allowedToReuse = allowedToReuse;
+	}
+
+	public int getInstanceGenerationId() {
+		return this.instanceGenerationId;
+	}
+
+	public void setInstanceGenerationId(final int instanceGenerationId) {
+		this.instanceGenerationId = instanceGenerationId;
 	}
 }

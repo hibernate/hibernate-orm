c9c11d560028b5218efd089d2329bc1bb8d740bd
Sanne Grinovero <sanne@hibernate.org>
2023-01-16 12:27:17 +0000

    HHH-16052 EntityKey and CollectionKey equality optimisation

--- hibernate-core/src/main/java/org/hibernate/event/internal/DefaultFlushEntityEventListener.java
+++ hibernate-core/src/main/java/org/hibernate/event/internal/DefaultFlushEntityEventListener.java
@@ -82,6 +82,13 @@ public class DefaultFlushEntityEventListener implements FlushEntityEventListener
 					+ " entry (don't flush the Session after an exception occurs)" );
 		}
 
+		//Small optimisation: always try to avoid getIdentifierType().isEqual(..) when possible.
+		//(However it's not safe to invoke the equals() method as it might trigger side-effects)
+		if ( id == oid ) {
+			//No further checks necessary:
+			return;
+		}
+
 		if ( !persister.getIdentifierType().isEqual( id, oid, session.getFactory() ) ) {
 			throw new HibernateException( "identifier of an instance of " + persister.getEntityName()
 					+ " was altered from " + oid + " to " + id );

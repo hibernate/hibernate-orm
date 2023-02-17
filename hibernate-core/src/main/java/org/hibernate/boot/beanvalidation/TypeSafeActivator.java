0b0799d567046793f4268167140819d6e9ed9f42
Sanne Grinovero <sanne@hibernate.org>
2023-02-17 14:50:51 +0000

    HHH-16194 Failure to automatically integrate with Bean Validation is excessively noisy

--- hibernate-core/src/main/java/org/hibernate/boot/beanvalidation/TypeSafeActivator.java
+++ hibernate-core/src/main/java/org/hibernate/boot/beanvalidation/TypeSafeActivator.java
@@ -483,11 +483,6 @@ class TypeSafeActivator {
 			return Validation.buildDefaultValidatorFactory();
 		}
 		catch ( Exception e ) {
-			LOG.infof(
-					e,
-					"Error calling `%s`",
-					"jakarta.validation.Validation#buildDefaultValidatorFactory"
-			);
 			throw new IntegrationException( "Unable to build the default ValidatorFactory", e );
 		}
 	}

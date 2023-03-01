abc44004caa910e2be5336635af325a2e930f090
Steve Ebersole <steve@hibernate.org>
2023-03-01 17:20:12 -0600

    test cleanup

--- hibernate-core/src/test/java/org/hibernate/orm/test/annotations/manytoonewithformula/ManyToOneWithFormulaTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/annotations/manytoonewithformula/ManyToOneWithFormulaTest.java
@@ -6,222 +6,215 @@
  */
 package org.hibernate.orm.test.annotations.manytoonewithformula;
 
-import org.hibernate.Session;
-import org.hibernate.Transaction;
 import org.hibernate.dialect.HSQLDialect;
 import org.hibernate.dialect.OracleDialect;
 import org.hibernate.dialect.SQLServerDialect;
 
-import org.hibernate.testing.RequiresDialect;
-import org.hibernate.testing.SkipForDialect;
-import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
-import org.junit.Test;
+import org.hibernate.testing.orm.junit.DomainModel;
+import org.hibernate.testing.orm.junit.RequiresDialect;
+import org.hibernate.testing.orm.junit.SessionFactory;
+import org.hibernate.testing.orm.junit.SessionFactoryScope;
+import org.hibernate.testing.orm.junit.SkipForDialect;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 /**
  * @author Sharath Reddy
  */
-public class ManyToOneWithFormulaTest extends BaseCoreFunctionalTestCase {
+@DomainModel( annotatedClasses = {
+		Menu.class,
+		FoodItem.class,
+		Company.class,
+		Person.class,
+		Message.class,
+		Language.class,
+		Contract.class,
+		ContractId.class,
+		Model.class,
+		ModelId.class,
+		Manufacturer.class,
+		ManufacturerId.class,
+		Product.class,
+		ProductSqlServer.class
+} )
+@SessionFactory
+public class ManyToOneWithFormulaTest {
 	@Test
-	public void testManyToOneFromNonPk() throws Exception {
-		Session s = openSession();
-		Transaction tx = s.beginTransaction();
-		Menu menu = new Menu();
-		menu.setOrderNbr( "123" );
-		menu.setDefault( "F" );
-		s.persist( menu );
-		FoodItem foodItem = new FoodItem();
-		foodItem.setItem( "Mouse" );
-		foodItem.setOrder( menu );
-		s.persist( foodItem );
-		s.flush();
-		s.clear();
-		foodItem = ( FoodItem ) s.get( FoodItem.class, foodItem.getId() );
-		assertNotNull( foodItem.getOrder() );
-		assertEquals( "123", foodItem.getOrder().getOrderNbr() );
-		tx.rollback();
-		s.close();
+	public void testManyToOneFromNonPk(SessionFactoryScope scope) {
+		scope.inTransaction( (session) -> {
+			Menu menu = new Menu();
+			menu.setOrderNbr( "123" );
+			menu.setDefault( "F" );
+			session.persist( menu );
+			FoodItem foodItem = new FoodItem();
+			foodItem.setItem( "Mouse" );
+			foodItem.setOrder( menu );
+			session.persist( foodItem );
+			session.flush();
+			session.clear();
+
+			foodItem = session.get( FoodItem.class, foodItem.getId() );
+			assertThat( foodItem.getOrder() ).isNotNull();
+			assertThat( foodItem.getOrder().getOrderNbr() ).isEqualTo( "123" );
+
+			session.getTransaction().markRollbackOnly();
+		} );
 	}
 
 	@Test
-	public void testManyToOneFromPk() throws Exception {
-		Session s = openSession();
-		Transaction tx = s.beginTransaction();
+	public void testManyToOneFromPk(SessionFactoryScope scope) {
+		scope.inTransaction( (session) -> {
+			Company company = new Company();
+			session.persist( company );
 
-		Company company = new Company();
-		s.persist( company );
+			Person person = new Person();
+			person.setDefaultFlag( "T" );
+			person.setCompanyId( company.getId() );
+			session.persist( person );
 
-		Person person = new Person();
-		person.setDefaultFlag( "T" );
-		person.setCompanyId( company.getId() );
-		s.persist( person );
+			session.flush();
+			session.clear();
 
-		s.flush();
-		s.clear();
+			company = session.get( Company.class, company.getId() );
+			assertThat( company.getDefaultContactPerson() ).isNotNull();
+			assertThat( company.getDefaultContactPerson().getId() ).isEqualTo( person.getId() );
 
-		company = ( Company ) s.get( Company.class, company.getId() );
-		assertNotNull( company.getDefaultContactPerson() );
-		assertEquals( person.getId(), company.getDefaultContactPerson().getId() );
-		tx.rollback();
-		s.close();
+			session.getTransaction().markRollbackOnly();
+		} );
 	}
 
 	@Test
-	@SkipForDialect(value = { HSQLDialect.class }, comment = "The used join conditions does not work in HSQLDB. See HHH-4497")
-	public void testManyToOneToPkWithOnlyFormula() throws Exception {
-		Session s = openSession();
-		Transaction tx = s.beginTransaction();
+	@SkipForDialect( dialectClass = HSQLDialect.class, reason = "The used join conditions does not work in HSQLDB. See HHH-4497" )
+	public void testManyToOneToPkWithOnlyFormula(SessionFactoryScope scope) {
+		scope.inTransaction( (session) -> {
+			Language language = new Language();
+			language.setCode( "EN" );
+			language.setName( "English" );
+			session.persist( language );
 
-		Language language = new Language();
-		language.setCode( "EN" );
-		language.setName( "English" );
-		s.persist( language );
+			Message msg = new Message();
+			msg.setLanguageCode( "en" );
+			msg.setLanguageName( "English" );
+			session.persist( msg );
 
-		Message msg = new Message();
-		msg.setLanguageCode( "en" );
-		msg.setLanguageName( "English" );
-		s.persist( msg );
+			session.flush();
+			session.clear();
 
-		s.flush();
-		s.clear();
+			msg = session.get( Message.class, msg.getId() );
+			assertThat( msg.getLanguage() ).isNotNull();
+			assertThat( msg.getLanguage().getCode() ).isEqualTo( "EN" );
 
-		msg = ( Message ) s.get( Message.class, msg.getId() );
-		assertNotNull( msg.getLanguage() );
-		assertEquals( "EN", msg.getLanguage().getCode() );
-		tx.rollback();
-		s.close();
+			session.getTransaction().markRollbackOnly();
+		} );
 	}
 
 	@Test
-	public void testReferencedColumnNameBelongsToEmbeddedIdOfReferencedEntity() throws Exception {
-		Session session = openSession();
-		Transaction tx = session.beginTransaction();
+	public void testReferencedColumnNameBelongsToEmbeddedIdOfReferencedEntity(SessionFactoryScope scope) {
+		scope.inTransaction( (session) -> {		Integer companyCode = 10;
+			Integer mfgCode = 100;
+			String contractNumber = "NSAR97841";
+			ContractId contractId = new ContractId(companyCode, 12457l, 1);
 
-		Integer companyCode = 10;
-		Integer mfgCode = 100;
-		String contractNumber = "NSAR97841";
-		ContractId contractId = new ContractId(companyCode, 12457l, 1);
+			Manufacturer manufacturer = new Manufacturer(new ManufacturerId(
+					companyCode, mfgCode), "FORD");
 
-		Manufacturer manufacturer = new Manufacturer(new ManufacturerId(
-				companyCode, mfgCode), "FORD");
+			Model model = new Model(new ModelId(companyCode, mfgCode, "FOCUS"),
+					"FORD FOCUS");
 
-		Model model = new Model(new ModelId(companyCode, mfgCode, "FOCUS"),
-				"FORD FOCUS");
+			session.persist(manufacturer);
+			session.persist(model);
 
-		session.persist(manufacturer);
-		session.persist(model);
+			Contract contract = new Contract();
+			contract.setId(contractId);
+			contract.setContractNumber(contractNumber);
+			contract.setManufacturer(manufacturer);
+			contract.setModel(model);
 
-		Contract contract = new Contract();
-		contract.setId(contractId);
-		contract.setContractNumber(contractNumber);
-		contract.setManufacturer(manufacturer);
-		contract.setModel(model);
+			session.persist(contract);
 
-		session.persist(contract);
+			session.flush();
+			session.clear();
 
-		session.flush();
-		session.clear();
+			contract = session.get( Contract.class, contractId );
+			assertThat( contract.getContractNumber() ).isEqualTo( "NSAR97841" );
+			assertThat( contract.getManufacturer().getName() ).isEqualTo( "FORD" );
+			assertThat( contract.getModel().getName() ).isEqualTo( "FORD FOCUS" );
 
-		contract = (Contract) session.load(Contract.class, contractId);
-		assertEquals("NSAR97841", contract.getContractNumber());
-		assertEquals("FORD", contract.getManufacturer().getName());
-		assertEquals("FORD FOCUS", contract.getModel().getName());
-
-		tx.commit();
-		session.close();
+			session.getTransaction().markRollbackOnly();
+		} );
 	}
 
 	@Test
-	@SkipForDialect( value = { HSQLDialect.class }, comment = "The used join conditions does not work in HSQLDB. See HHH-4497." )
-	@SkipForDialect( value = { OracleDialect.class }, comment = "Oracle do not support 'substring' function" )
-	public void testManyToOneFromNonPkToNonPk() throws Exception {
-		// also tests usage of the stand-alone @JoinFormula annotation (i.e. not wrapped within @JoinColumnsOrFormulas)
-		Session s = openSession();
-		Transaction tx = s.beginTransaction();
+	@SkipForDialect( dialectClass =HSQLDialect.class, reason = "The used join conditions does not work in HSQLDB. See HHH-4497." )
+	@SkipForDialect( dialectClass = OracleDialect.class, reason = "Oracle do not support 'substring' function" )
+	public void testManyToOneFromNonPkToNonPk(SessionFactoryScope scope) {
+		// also tests usage of the stand-alone @JoinFormula annotation
+		// (i.e. not wrapped within @JoinColumnsOrFormulas)
 
-        Product kit = new Product();
-        kit.id = 1;
-        kit.productIdnf = "KIT";
-        kit.description = "Kit";
-        s.persist(kit);
+		scope.inTransaction( (session) -> {
+			Product kit = new Product();
+			kit.id = 1;
+			kit.productIdnf = "KIT";
+			kit.description = "Kit";
+			session.persist(kit);
 
-        Product kitkat = new Product();
-        kitkat.id = 2;
-        kitkat.productIdnf = "KIT_KAT";
-        kitkat.description = "Chocolate";
-        s.persist(kitkat);
+			Product kitkat = new Product();
+			kitkat.id = 2;
+			kitkat.productIdnf = "KIT_KAT";
+			kitkat.description = "Chocolate";
+			session.persist(kitkat);
 
-        s.flush();
-        s.clear();
+			session.flush();
+			session.clear();
 
-        kit = (Product) s.get(Product.class, 1);
-        kitkat = (Product) s.get(Product.class, 2);
-        System.out.println(kitkat.description);
-        assertNotNull(kitkat);
-        assertEquals(kit, kitkat.getProductFamily());
-        assertEquals(kit.productIdnf, kitkat.getProductFamily().productIdnf);
-        assertEquals("KIT_KAT", kitkat.productIdnf.trim());
-        assertEquals("Chocolate", kitkat.description.trim());
+			kit = session.get(Product.class, 1);
+			kitkat = session.get(Product.class, 2);
 
-        tx.rollback();
-		s.close();
+			assertThat( kitkat ).isNotNull();
+			assertThat( kitkat.getProductFamily() ).isEqualTo( kit );
+			assertThat( kit.productIdnf ).isEqualTo( kitkat.getProductFamily().productIdnf );
+			assertThat( kitkat.productIdnf.trim() ).isEqualTo( "KIT_KAT" );
+			assertThat( kitkat.description.trim() ).isEqualTo( "Chocolate" );
+
+			session.getTransaction().markRollbackOnly();
+		} );
     }
 
     @Test
-	@RequiresDialect(value = { SQLServerDialect.class})
-    public void testManyToOneFromNonPkToNonPkSqlServer() throws Exception {
-        // also tests usage of the stand-alone @JoinFormula annotation (i.e. not wrapped within @JoinColumnsOrFormulas)
-        Session s = openSession();
-        Transaction tx = s.beginTransaction();
+	@RequiresDialect( SQLServerDialect.class )
+    public void testManyToOneFromNonPkToNonPkSqlServer(SessionFactoryScope scope) {
+        // also tests usage of the stand-alone @JoinFormula annotation
+		// (i.e. not wrapped within @JoinColumnsOrFormulas)
 
-        ProductSqlServer kit = new ProductSqlServer();
-        kit.id = 1;
-        kit.productIdnf = "KIT";
-        kit.description = "Kit";
-        s.persist(kit);
+		scope.inTransaction( (session) -> {
+			ProductSqlServer kit = new ProductSqlServer();
+			kit.id = 1;
+			kit.productIdnf = "KIT";
+			kit.description = "Kit";
+			session.persist(kit);
 
-        ProductSqlServer kitkat = new ProductSqlServer();
-        kitkat.id = 2;
-        kitkat.productIdnf = "KIT_KAT";
-        kitkat.description = "Chocolate";
-        s.persist(kitkat);
+			ProductSqlServer kitkat = new ProductSqlServer();
+			kitkat.id = 2;
+			kitkat.productIdnf = "KIT_KAT";
+			kitkat.description = "Chocolate";
+			session.persist(kitkat);
 
-        s.flush();
-        s.clear();
+			session.flush();
+			session.clear();
 
-        kit = (ProductSqlServer) s.get(ProductSqlServer.class, 1);
-        kitkat = (ProductSqlServer) s.get(ProductSqlServer.class, 2);
-        System.out.println(kitkat.description);
-        assertNotNull(kitkat);
-        assertEquals(kit, kitkat.getProductFamily());
-        assertEquals(kit.productIdnf, kitkat.getProductFamily().productIdnf);
-        assertEquals("KIT_KAT", kitkat.productIdnf.trim());
-        assertEquals("Chocolate", kitkat.description.trim());
+			kit = session.get(ProductSqlServer.class, 1);
+			kitkat = session.get(ProductSqlServer.class, 2);
 
-        tx.rollback();
-        s.close();
+			assertThat( kitkat ).isNotNull();
+			assertThat( kitkat.getProductFamily() ).isEqualTo( kit );
+			assertThat( kit.productIdnf ).isEqualTo( kitkat.getProductFamily().productIdnf );
+			assertThat( kitkat.productIdnf.trim() ).isEqualTo( "KIT_KAT" );
+			assertThat( kitkat.description.trim() ).isEqualTo( "Chocolate" );
+
+			session.getTransaction().markRollbackOnly();
+		} );
     }
-
-	@Override
-	protected Class<?>[] getAnnotatedClasses() {
-		return new Class[] {
-				Menu.class,
-				FoodItem.class,
-				Company.class,
-				Person.class,
-				Message.class,
-				Language.class,
-				Contract.class,
-				ContractId.class,
-				Model.class,
-				ModelId.class,
-				Manufacturer.class,
-				ManufacturerId.class,
-				Product.class,
-                ProductSqlServer.class
-		};
-	}
 
 }

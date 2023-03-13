713be8eba6fa08daf2dcd2844ea6b781ea86e627
Christian Beikov <christian.beikov@gmail.com>
2023-03-13 11:32:37 +0100

    HHH-15664 Fix array section in documentation

---
+++ documentation/src/main/asciidoc/userguide/chapters/domain/extras/collections/collections-array-as-basic-example.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Person (
+    id BIGINT NOT NULL,
+    phones VARCHAR(255) ARRAY,
+    PRIMARY KEY ( id )
+)
--- documentation/src/main/asciidoc/userguide/chapters/domain/extras/collections/collections-array-binary-example.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE Person (
-    id BIGINT NOT NULL ,
-    phones VARBINARY(255) ,
-    PRIMARY KEY ( id )
-)

e7647d412429802d7d4c4265c457b398dc927495
Andrea Boriero <andrea@hibernate.org>
2023-03-14 13:27:46 +0100

    HHH-16297 Removing an element from a collection of elements removes the whole collection

--- hibernate-core/src/main/java/org/hibernate/metamodel/mapping/internal/MappingModelCreationHelper.java
+++ hibernate-core/src/main/java/org/hibernate/metamodel/mapping/internal/MappingModelCreationHelper.java
@@ -1337,6 +1337,7 @@ public class MappingModelCreationHelper {
 					basicElement.isColumnInsertable( 0 ),
 					basicElement.isColumnUpdateable( 0 ),
 					basicElement.isPartitionKey(),
+					true, // element collection does not support null elements
 					dialect,
 					creationProcess.getSqmFunctionRegistry()
 			);

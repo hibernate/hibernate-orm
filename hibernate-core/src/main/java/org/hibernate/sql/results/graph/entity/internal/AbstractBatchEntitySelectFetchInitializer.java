837d1a32cb3ffbf48653c0bb12679ccebd07a005
Marco Belladelli <marco@hibernate.org>
2023-03-16 17:40:05 +0100

    HHH-16248 Check referenced model part declaring type when batch fetching associations

--- hibernate-core/src/main/java/org/hibernate/sql/results/graph/entity/internal/AbstractBatchEntitySelectFetchInitializer.java
+++ hibernate-core/src/main/java/org/hibernate/sql/results/graph/entity/internal/AbstractBatchEntitySelectFetchInitializer.java
@@ -10,6 +10,7 @@ import java.util.function.Consumer;
 
 import org.hibernate.engine.spi.EntityKey;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.metamodel.mapping.AttributeMapping;
 import org.hibernate.metamodel.mapping.ModelPart;
 import org.hibernate.metamodel.mapping.internal.ToOneAttributeMapping;
 import org.hibernate.persister.entity.EntityPersister;
@@ -130,8 +131,16 @@ public abstract class AbstractBatchEntitySelectFetchInitializer extends Abstract
 		);
 	}
 
-	protected static int getPropertyIndex(EntityInitializer entityInitializer, String propertyName) {
-		return entityInitializer.getConcreteDescriptor().findAttributeMapping( propertyName ).getStateArrayPosition();
+	protected AttributeMapping getParentEntityAttribute(String attributeName) {
+		final AttributeMapping parentAttribute = firstEntityInitializer.getConcreteDescriptor()
+				.findAttributeMapping( attributeName );
+		if ( parentAttribute != null && parentAttribute.getDeclaringType() == referencedModelPart.getDeclaringType()
+				.findContainingEntityMapping() ) {
+			// These checks are needed to avoid setting the instance using the wrong (child's) model part or
+			// setting it multiple times in case parent and child share the same attribute name for the association.
+			return parentAttribute;
+		}
+		return null;
 	}
 
 	@Override

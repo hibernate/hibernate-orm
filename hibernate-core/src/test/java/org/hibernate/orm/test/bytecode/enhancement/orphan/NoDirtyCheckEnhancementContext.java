6c9d9666a4dd614ca0cf003ee015d2b182ada5cc
Andrea Boriero <andrea@hibernate.org>
2023-03-21 17:09:00 +0100

    HHH-16334 Add test for issue

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/bytecode/enhancement/orphan/NoDirtyCheckEnhancementContext.java
@@ -0,0 +1,22 @@
+package org.hibernate.orm.test.bytecode.enhancement.orphan;
+
+import org.hibernate.bytecode.enhance.spi.DefaultEnhancementContext;
+import org.hibernate.bytecode.enhance.spi.UnloadedClass;
+import org.hibernate.bytecode.enhance.spi.UnloadedField;
+
+public class NoDirtyCheckEnhancementContext extends DefaultEnhancementContext {
+	@Override
+	public boolean hasLazyLoadableAttributes(UnloadedClass classDescriptor) {
+		return true;
+	}
+
+	@Override
+	public boolean isLazyLoadable(UnloadedField field) {
+		return true;
+	}
+
+	@Override
+	public boolean doDirtyCheckingInline(UnloadedClass classDescriptor) {
+		return false;
+	}
+}

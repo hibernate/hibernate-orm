e8b0c288c87164bee93d9027b07de902eb68fc5e
Marco Belladelli <marco@hibernate.org>
2023-03-22 16:46:46 +0100

    HHH-16305 Skip checking JavaObjectType function arguments

--- hibernate-core/src/test/java/org/hibernate/orm/test/query/NamedParameterInSelectAndWhereTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/query/NamedParameterInSelectAndWhereTest.java
@@ -8,11 +8,14 @@ package org.hibernate.orm.test.query;
 
 import java.time.LocalDate;
 
+import org.hibernate.dialect.PostgreSQLDialect;
+
 import org.hibernate.testing.orm.domain.gambit.EntityOfBasics;
 import org.hibernate.testing.orm.junit.DomainModel;
 import org.hibernate.testing.orm.junit.Jira;
 import org.hibernate.testing.orm.junit.SessionFactory;
 import org.hibernate.testing.orm.junit.SessionFactoryScope;
+import org.hibernate.testing.orm.junit.SkipForDialect;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -31,6 +34,7 @@ public class NamedParameterInSelectAndWhereTest {
 			final EntityOfBasics e1 = new EntityOfBasics( 1 );
 			final EntityOfBasics e2 = new EntityOfBasics( 2 );
 			e2.setTheLocalDate( LocalDate.EPOCH );
+			e2.setTheInt( 1 );
 			session.persist( e1 );
 			session.persist( e2 );
 		} );
@@ -64,6 +68,7 @@ public class NamedParameterInSelectAndWhereTest {
 
 	@Test
 	@Jira( "https://hibernate.atlassian.net/browse/HHH-16305" )
+	@SkipForDialect( dialectClass = PostgreSQLDialect.class, reason = "PostgreSQL doesn't support parameters as arguments for timestampdiff" )
 	public void testSelectFunctionAndWhere(SessionFactoryScope scope) {
 		scope.inTransaction( session -> assertEquals( 0, session.createQuery(
 						"select timestampdiff(year, e.theLocalDate, :date) from EntityOfBasics e where e.theLocalDate <= :date",

ab81bb33a03849c10e503d91558999c53b30c247
Cedomir Igaly <cedomir.igaly@e-dama.net>
2023-04-02 12:34:16 +0200

    HHH-16261 - Fixed formatting

--- tooling/metamodel-generator/src/main/java/org/hibernate/jpamodelgen/JPAMetaModelEntityProcessor.java
+++ tooling/metamodel-generator/src/main/java/org/hibernate/jpamodelgen/JPAMetaModelEntityProcessor.java
@@ -252,7 +252,7 @@ public class JPAMetaModelEntityProcessor extends AbstractProcessor {
 	private MetaEntity tryGettingExistingEntityFromContext(AnnotationMirror mirror, String fqn) {
 		MetaEntity alreadyExistingMetaEntity = null;
 		if ( TypeUtils.isAnnotationMirrorOfType( mirror, Constants.ENTITY )
-				|| TypeUtils.isAnnotationMirrorOfType( mirror, Constants.MAPPED_SUPERCLASS )) {
+				|| TypeUtils.isAnnotationMirrorOfType( mirror, Constants.MAPPED_SUPERCLASS ) ) {
 			alreadyExistingMetaEntity = context.getMetaEntity( fqn );
 		}
 		else if ( TypeUtils.isAnnotationMirrorOfType( mirror, Constants.EMBEDDABLE ) ) {

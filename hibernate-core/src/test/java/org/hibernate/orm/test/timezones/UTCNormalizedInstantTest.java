98f8e7ecfa76193fe3948b03fbb74a81e3cbf74c
Marco Belladelli <marco@hibernate.org>
2023-04-07 10:15:28 +0200

    Fix fragile tests

--- hibernate-core/src/test/java/org/hibernate/orm/test/timezones/UTCNormalizedInstantTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/timezones/UTCNormalizedInstantTest.java
@@ -1,92 +1,88 @@
 package org.hibernate.orm.test.timezones;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.TimeZone;
+
 import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.dialect.Dialect;
 import org.hibernate.dialect.SybaseDialect;
+import org.hibernate.type.descriptor.DateTimeUtils;
+
 import org.hibernate.testing.orm.junit.DomainModel;
 import org.hibernate.testing.orm.junit.SessionFactory;
 import org.hibernate.testing.orm.junit.SessionFactoryScope;
-import org.hibernate.testing.orm.junit.SkipForDialect;
 import org.junit.jupiter.api.Test;
 
-import java.time.Instant;
-import java.util.TimeZone;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 import static java.sql.Types.TIMESTAMP;
-import static java.time.temporal.ChronoUnit.SECONDS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DomainModel(annotatedClasses = UTCNormalizedInstantTest.Zoned.class)
 @SessionFactory
 public class UTCNormalizedInstantTest {
 
-	@SkipForDialect(dialectClass = SybaseDialect.class, matchSubTypes = true)
 	@Test void test(SessionFactoryScope scope) {
-		Instant instant = Instant.now();
+		final Instant instant = Instant.now();
 		long id = scope.fromTransaction( s-> {
-			Zoned z = new Zoned();
+			final Zoned z = new Zoned();
 			z.utcInstant = instant;
 			z.localInstant = instant;
 			s.persist(z);
 			return z.id;
 		});
 		scope.inSession( s-> {
-			Zoned z = s.find(Zoned.class, id);
-			assertEquals( instant, z.utcInstant );
-			assertEquals( instant, z.localInstant );
+			final Zoned z = s.find(Zoned.class, id);
+			final Dialect dialect = scope.getSessionFactory().getJdbcServices().getDialect();
+			if ( dialect instanceof SybaseDialect) {
+				// Sybase with jTDS driver has 1/300th sec precision
+				assertEquals( z.utcInstant.truncatedTo(ChronoUnit.SECONDS), instant.truncatedTo(ChronoUnit.SECONDS) );
+				assertEquals( z.localInstant.truncatedTo(ChronoUnit.SECONDS), instant.truncatedTo(ChronoUnit.SECONDS) );
+			}
+			else {
+				assertEquals(
+						DateTimeUtils.roundToDefaultPrecision( z.utcInstant, dialect ),
+						DateTimeUtils.roundToDefaultPrecision( instant, dialect )
+				);
+				assertEquals(
+						DateTimeUtils.roundToDefaultPrecision( z.localInstant, dialect ),
+						DateTimeUtils.roundToDefaultPrecision( instant, dialect )
+				);
+			}
 		});
 	}
 
-	@SkipForDialect(dialectClass = SybaseDialect.class, matchSubTypes = true)
 	@Test void testWithSystemTimeZone(SessionFactoryScope scope) {
 		TimeZone.setDefault( TimeZone.getTimeZone("CET") );
-		Instant instant = Instant.now();
+		final Instant instant = Instant.now();
 		long id = scope.fromTransaction( s-> {
-			Zoned z = new Zoned();
+			final Zoned z = new Zoned();
 			z.utcInstant = instant;
 			z.localInstant = instant;
 			s.persist(z);
 			return z.id;
 		});
 		scope.inSession( s-> {
-			Zoned z = s.find(Zoned.class, id);
-			assertEquals( instant, z.utcInstant );
-			assertEquals( instant, z.localInstant );
-		});
-	}
-
-	@Test void testSybase(SessionFactoryScope scope) {
-		Instant instant = Instant.now().truncatedTo(SECONDS);
-		long id = scope.fromTransaction( s-> {
-			Zoned z = new Zoned();
-			z.utcInstant = instant;
-			z.localInstant = instant;
-			s.persist(z);
-			return z.id;
-		});
-		scope.inSession( s-> {
-			Zoned z = s.find(Zoned.class, id);
-			assertEquals( instant, z.utcInstant );
-			assertEquals( instant, z.localInstant );
-		});
-	}
-
-	@Test void testWithSystemTimeZoneSybase(SessionFactoryScope scope) {
-		TimeZone.setDefault( TimeZone.getTimeZone("CET") );
-		Instant instant = Instant.now().truncatedTo(SECONDS);
-		long id = scope.fromTransaction( s-> {
-			Zoned z = new Zoned();
-			z.utcInstant = instant;
-			z.localInstant = instant;
-			s.persist(z);
-			return z.id;
-		});
-		scope.inSession( s-> {
-			Zoned z = s.find(Zoned.class, id);
-			assertEquals( instant, z.utcInstant );
-			assertEquals( instant, z.localInstant );
+			final Zoned z = s.find(Zoned.class, id);
+			final Dialect dialect = scope.getSessionFactory().getJdbcServices().getDialect();
+			if ( dialect instanceof SybaseDialect) {
+				// Sybase with jTDS driver has 1/300th sec precision
+				assertEquals( z.utcInstant.truncatedTo(ChronoUnit.SECONDS), instant.truncatedTo(ChronoUnit.SECONDS) );
+				assertEquals( z.localInstant.truncatedTo(ChronoUnit.SECONDS), instant.truncatedTo(ChronoUnit.SECONDS) );
+			}
+			else {
+				assertEquals(
+						DateTimeUtils.roundToDefaultPrecision( z.utcInstant, dialect ),
+						DateTimeUtils.roundToDefaultPrecision( instant, dialect )
+				);
+				assertEquals(
+						DateTimeUtils.roundToDefaultPrecision( z.localInstant, dialect ),
+						DateTimeUtils.roundToDefaultPrecision( instant, dialect )
+				);
+			}
 		});
 	}
 

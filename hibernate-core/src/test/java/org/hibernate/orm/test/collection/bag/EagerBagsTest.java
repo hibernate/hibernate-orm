a16e50597248f23efd387ec6f7813798cf9082b5
Andrea Boriero <andrea@hibernate.org>
2023-04-11 14:21:12 +0200

    HHH-16423 Add tests for issue

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/collection/bag/EagerBagsTest.java
@@ -0,0 +1,165 @@
+package org.hibernate.orm.test.collection.bag;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+import org.hibernate.testing.orm.junit.DomainModel;
+import org.hibernate.testing.orm.junit.SessionFactory;
+import org.hibernate.testing.orm.junit.SessionFactoryScope;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+@DomainModel(
+		annotatedClasses = {
+				EagerBagsTest.EntityA.class,
+				EagerBagsTest.EntityB.class,
+				EagerBagsTest.EntityC.class
+		}
+)
+@SessionFactory
+public class EagerBagsTest {
+
+	@BeforeAll
+	public void setUp(SessionFactoryScope scope) {
+		scope.inTransaction(
+				session -> {
+					EntityC c = new EntityC( 1l, "c" );
+					EntityC c1 = new EntityC( 2l, "c1" );
+					EntityC c2 = new EntityC( 3l, "c2" );
+
+					EntityC c3 = new EntityC( 4l, "c3" );
+
+					EntityB b = new EntityB( 1l, "b" );
+
+					b.addAttribute( c );
+					b.addAttribute( c1 );
+
+					EntityB b1 = new EntityB( 2l, "b1" );
+
+					b1.addAttribute( c2 );
+					b1.addAttribute( c3 );
+
+					EntityA a = new EntityA( 1l, "a" );
+
+					a.addAttribute( b );
+					a.addAttribute( b1 );
+
+					session.save( c );
+					session.save( c1 );
+					session.save( c2 );
+					session.save( c3 );
+
+					session.save( b );
+					session.save( b1 );
+
+					session.save( a );
+				}
+		);
+	}
+
+	@Test
+	public void testIt(SessionFactoryScope scope) {
+		scope.inTransaction(
+				session -> {
+					EntityA entityA = session.find( EntityA.class, 1l );
+					assertThat( entityA.attributes.size() ).isEqualTo( 2 );
+				}
+		);
+	}
+
+	@Entity(name = "EntityA")
+	public static class EntityA {
+		@Id
+		private Long id;
+
+		private String name;
+
+		@OneToMany(fetch = FetchType.EAGER)
+		Collection<EntityB> attributes = new LinkedList<>();
+
+		public EntityA() {
+		}
+
+		public EntityA(Long id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public Collection<EntityB> getAttributes() {
+			return attributes;
+		}
+
+		public void addAttribute(EntityB entityB) {
+			attributes.add( entityB );
+		}
+	}
+
+	@Entity(name = "EntityB")
+	public static class EntityB {
+		@Id
+		private Long id;
+
+		private String name;
+
+		@OneToMany(fetch = FetchType.EAGER)
+		Collection<EntityC> attributes = new LinkedList<>();
+
+
+		public EntityB() {
+		}
+
+		public EntityB(Long id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public Collection<EntityC> getAttributes() {
+			return attributes;
+		}
+
+		public void addAttribute(EntityC entityC) {
+			this.attributes.add( entityC );
+		}
+	}
+
+	@Entity(name = "EntityC")
+	public static class EntityC {
+		@Id
+		private Long id;
+
+		private String name;
+
+		public EntityC() {
+		}
+
+		public EntityC(Long id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+	}
+
+}

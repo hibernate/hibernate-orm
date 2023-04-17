1399adce7d681e58dfcc5fa0631ea4ebc537a0f0
Christian Beikov <christian.beikov@gmail.com>
2023-04-17 15:33:49 +0200

    HHH-15602 Fix bidirectional association management code

--- hibernate-core/src/main/java/org/hibernate/bytecode/enhance/internal/bytebuddy/CodeTemplates.java
+++ hibernate-core/src/main/java/org/hibernate/bytecode/enhance/internal/bytebuddy/CodeTemplates.java
@@ -323,20 +323,34 @@ class CodeTemplates {
 
 	static class OneToOneHandler {
 		@Advice.OnMethodEnter
-		static void enter(@FieldValue Object field, @Advice.Argument(0) Object argument, @MappedBy String mappedBy) {
-			if ( field != null && Hibernate.isPropertyInitialized( field, mappedBy ) && argument != null ) {
-				setterNull( field, null );
+		static void enter(@FieldValue Object field, @Advice.Argument(0) Object argument, @InverseSide boolean inverseSide) {
+			// Unset the inverse attribute, which possibly initializes the old value,
+			// only if this is the inverse side, or the old value is already initialized
+			if ( ( inverseSide || Hibernate.isInitialized( field ) ) && getterSelf() != null ) {
+				// We copy the old value, then set the field to null which we must do before
+				// unsetting the inverse attribute, as we'd otherwise run into a stack overflow situation
+				// The field is writable, so setting it to null here is actually a field write.
+				Object fieldCopy = field;
+				field = null;
+				setterNull( fieldCopy, null );
 			}
 		}
 
 		@Advice.OnMethodExit
-		static void exit(@Advice.This Object self, @Advice.Argument(0) Object argument, @MappedBy String mappedBy) {
-			if ( argument != null && Hibernate.isPropertyInitialized( argument, mappedBy ) && getter( argument ) != self ) {
+		static void exit(@Advice.This Object self, @Advice.Argument(0) Object argument, @InverseSide boolean inverseSide) {
+			// Update the inverse attribute, which possibly initializes the argument value,
+			// only if this is the inverse side, or the argument value is already initialized
+			if ( argument != null && ( inverseSide || Hibernate.isInitialized( argument ) ) && getter( argument ) != self ) {
 				setterSelf( argument, self );
 			}
 		}
 
 		static Object getter(Object target) {
+			// is replaced by the actual method call
+			throw new AssertionError();
+		}
+
+		static Object getterSelf() {
 			// is replaced by the actual method call
 			throw new AssertionError();
 		}
@@ -354,11 +368,14 @@ class CodeTemplates {
 
 	static class OneToManyOnCollectionHandler {
 		@Advice.OnMethodEnter
-		static void enter(@FieldValue Collection<?> field, @Advice.Argument(0) Collection<?> argument, @MappedBy String mappedBy) {
-			if ( field != null && Hibernate.isPropertyInitialized( field, mappedBy ) ) {
+		static void enter(@FieldValue Collection<?> field, @Advice.Argument(0) Collection<?> argument, @InverseSide boolean inverseSide) {
+			// If this is the inverse side or the old collection is already initialized,
+			// we must unset the respective ManyToOne of the old collection elements,
+			// because only the owning side is responsible for persisting the state.
+			if ( ( inverseSide || Hibernate.isInitialized( field ) ) && getterSelf() != null ) {
 				Object[] array = field.toArray();
 				for ( int i = 0; i < array.length; i++ ) {
-					if ( argument == null || !argument.contains( array[i] ) ) {
+					if ( ( inverseSide || Hibernate.isInitialized( array[i] ) ) && ( argument == null || !argument.contains( array[i] ) ) ) {
 						setterNull( array[i], null );
 					}
 				}
@@ -366,11 +383,14 @@ class CodeTemplates {
 		}
 
 		@Advice.OnMethodExit
-		static void exit(@Advice.This Object self, @Advice.Argument(0) Collection<?> argument, @MappedBy String mappedBy) {
-			if ( argument != null && Hibernate.isPropertyInitialized( argument, mappedBy ) ) {
+		static void exit(@Advice.This Object self, @Advice.Argument(0) Collection<?> argument, @InverseSide boolean inverseSide) {
+			// If this is the inverse side or the new collection is already initialized,
+			// we must set the respective ManyToOne on the new collection elements,
+			// because only the owning side is responsible for persisting the state.
+			if ( argument != null && ( inverseSide || Hibernate.isInitialized( argument ) ) ) {
 				Object[] array = argument.toArray();
 				for ( int i = 0; i < array.length; i++ ) {
-					if ( Hibernate.isPropertyInitialized( array[i], mappedBy ) && getter( array[i] ) != self ) {
+					if ( ( inverseSide || Hibernate.isInitialized( array[i] ) ) && getter( array[i] ) != self ) {
 						setterSelf( array[i], self );
 					}
 				}
@@ -378,6 +398,11 @@ class CodeTemplates {
 		}
 
 		static Object getter(Object target) {
+			// is replaced by the actual method call
+			throw new AssertionError();
+		}
+
+		static Object getterSelf() {
 			// is replaced by the actual method call
 			throw new AssertionError();
 		}
@@ -395,11 +420,14 @@ class CodeTemplates {
 
 	static class OneToManyOnMapHandler {
 		@Advice.OnMethodEnter
-		static void enter(@FieldValue Map<?, ?> field, @Advice.Argument(0) Map<?, ?> argument, @MappedBy String mappedBy) {
-			if ( field != null && Hibernate.isPropertyInitialized( field, mappedBy ) ) {
+		static void enter(@FieldValue Map<?, ?> field, @Advice.Argument(0) Map<?, ?> argument, @InverseSide boolean inverseSide) {
+			// If this is the inverse side or the old collection is already initialized,
+			// we must unset the respective ManyToOne of the old collection elements,
+			// because only the owning side is responsible for persisting the state.
+			if ( ( inverseSide || Hibernate.isInitialized( field ) ) && getterSelf() != null ) {
 				Object[] array = field.values().toArray();
 				for ( int i = 0; i < array.length; i++ ) {
-					if ( argument == null || !argument.values().contains( array[i] ) ) {
+					if ( ( inverseSide || Hibernate.isInitialized( array[i] ) ) && ( argument == null || !argument.containsValue( array[i] ) ) ) {
 						setterNull( array[i], null );
 					}
 				}
@@ -407,11 +435,14 @@ class CodeTemplates {
 		}
 
 		@Advice.OnMethodExit
-		static void exit(@Advice.This Object self, @Advice.Argument(0) Map<?, ?> argument, @MappedBy String mappedBy) {
-			if ( argument != null && Hibernate.isPropertyInitialized( argument, mappedBy ) ) {
+		static void exit(@Advice.This Object self, @Advice.Argument(0) Map<?, ?> argument, @InverseSide boolean inverseSide) {
+			// If this is the inverse side or the new collection is already initialized,
+			// we must set the respective ManyToOne on the new collection elements,
+			// because only the owning side is responsible for persisting the state.
+			if ( argument != null && ( inverseSide || Hibernate.isInitialized( argument ) ) ) {
 				Object[] array = argument.values().toArray();
 				for ( int i = 0; i < array.length; i++ ) {
-					if ( Hibernate.isPropertyInitialized( array[i], mappedBy ) && getter( array[i] ) != self ) {
+					if ( ( inverseSide || Hibernate.isInitialized( array[i] ) ) && getter( array[i] ) != self ) {
 						setterSelf( array[i], self );
 					}
 				}
@@ -420,6 +451,11 @@ class CodeTemplates {
 
 		static Object getter(Object target) {
 			// is replaced with the actual getter call during instrumentation.
+			throw new AssertionError();
+		}
+
+		static Object getterSelf() {
+			// is replaced by the actual method call
 			throw new AssertionError();
 		}
 
@@ -436,8 +472,9 @@ class CodeTemplates {
 
 	static class ManyToOneHandler {
 		@Advice.OnMethodEnter
-		static void enter(@Advice.This Object self, @FieldValue Object field, @MappedBy String mappedBy) {
-			if ( field != null && Hibernate.isPropertyInitialized( field, mappedBy ) ) {
+		static void enter(@Advice.This Object self, @FieldValue Object field, @BidirectionalAttribute String inverseAttribute) {
+			// This is always the owning side, so we only need to update the inverse side if the collection is initialized
+			if ( getterSelf() != null && Hibernate.isPropertyInitialized( field, inverseAttribute ) ) {
 				Collection<?> c = getter( field );
 				if ( c != null ) {
 					c.remove( self );
@@ -446,8 +483,9 @@ class CodeTemplates {
 		}
 
 		@Advice.OnMethodExit
-		static void exit(@Advice.This Object self, @Advice.Argument(0) Object argument, @MappedBy String mappedBy) {
-			if ( argument != null && Hibernate.isPropertyInitialized( argument, mappedBy ) ) {
+		static void exit(@Advice.This Object self, @Advice.Argument(0) Object argument, @BidirectionalAttribute String inverseAttribute) {
+			// This is always the owning side, so we only need to update the inverse side if the collection is initialized
+			if ( argument != null && Hibernate.isPropertyInitialized( argument, inverseAttribute ) ) {
 				Collection<Object> c = getter( argument );
 				if ( c != null && !c.contains( self ) ) {
 					c.add( self );
@@ -459,15 +497,23 @@ class CodeTemplates {
 			// is replaced by the actual method call
 			throw new AssertionError();
 		}
+
+		static Object getterSelf() {
+			// is replaced by the actual method call
+			throw new AssertionError();
+		}
 	}
 
 	static class ManyToManyHandler {
 		@Advice.OnMethodEnter
-		static void enter(@Advice.This Object self, @FieldValue Collection<?> field, @Advice.Argument(0) Collection<?> argument, @MappedBy String mappedBy) {
-			if ( field != null && Hibernate.isPropertyInitialized( field, mappedBy ) ) {
+		static void enter(@Advice.This Object self, @FieldValue Collection<?> field, @Advice.Argument(0) Collection<?> argument, @InverseSide boolean inverseSide, @BidirectionalAttribute String bidirectionalAttribute) {
+			// If this is the inverse side or the old collection is already initialized,
+			// we must remove self from the respective old collection elements inverse collections,
+			// because only the owning side is responsible for persisting the state.
+			if ( ( inverseSide || Hibernate.isInitialized( argument ) ) && getterSelf() != null ) {
 				Object[] array = field.toArray();
 				for ( int i = 0; i < array.length; i++ ) {
-					if ( argument == null || !argument.contains( array[i] ) ) {
+					if ( ( inverseSide || Hibernate.isPropertyInitialized( array[i], bidirectionalAttribute ) ) && ( argument == null || !argument.contains( array[i] ) ) ) {
 						getter( array[i] ).remove( self );
 					}
 				}
@@ -475,13 +521,16 @@ class CodeTemplates {
 		}
 
 		@Advice.OnMethodExit
-		static void exit(@Advice.This Object self, @Advice.Argument(0) Collection<?> argument, @MappedBy String mappedBy) {
-			if ( argument != null && Hibernate.isPropertyInitialized( argument, mappedBy ) ) {
+		static void exit(@Advice.This Object self, @Advice.Argument(0) Collection<?> argument, @InverseSide boolean inverseSide, @BidirectionalAttribute String bidirectionalAttribute) {
+			// If this is the inverse side or the new collection is already initialized,
+			// we must add self to the respective new collection elements inverse collections,
+			// because only the owning side is responsible for persisting the state.
+			if ( argument != null && ( inverseSide || Hibernate.isInitialized( argument ) ) ) {
 				Object[] array = argument.toArray();
 				for ( int i = 0; i < array.length; i++ ) {
-					if ( Hibernate.isPropertyInitialized( array[i], mappedBy ) ) {
+					if ( inverseSide || Hibernate.isPropertyInitialized( array[i], bidirectionalAttribute ) ) {
 						Collection<Object> c = getter( array[i] );
-						if ( c != self && c != null ) {
+						if ( c != null && !c.contains( self ) ) {
 							c.add( self );
 						}
 					}
@@ -490,6 +539,11 @@ class CodeTemplates {
 		}
 
 		static Collection<Object> getter(Object self) {
+			// is replaced by the actual method call
+			throw new AssertionError();
+		}
+
+		static Object getterSelf() {
 			// is replaced by the actual method call
 			throw new AssertionError();
 		}
@@ -506,7 +560,12 @@ class CodeTemplates {
 	}
 
 	@Retention(RetentionPolicy.RUNTIME)
-	@interface MappedBy {
+	@interface InverseSide {
+
+	}
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@interface BidirectionalAttribute {
 
 	}
 }
---
+++ hibernate-core/src/test/java/org/hibernate/test/bytecode/enhancement/association/ManyToManyAssociationListTest.java
@@ -0,0 +1,94 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.test.bytecode.enhancement.association;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import org.hibernate.testing.bytecode.enhancement.BytecodeEnhancerRunner;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Luis Barreiro
+ */
+@RunWith( BytecodeEnhancerRunner.class )
+public class ManyToManyAssociationListTest {
+    @Test
+    public void testBidirectionalExisting() {
+        Group group = new Group();
+        Group anotherGroup = new Group();
+
+        User user = new User();
+        anotherGroup.users.add( user );
+
+        user.setGroups( new ArrayList<>( Collections.singleton( group ) ) );
+        user.setGroups( new ArrayList<>( Arrays.asList( group, anotherGroup ) ) );
+
+        Assert.assertEquals( 1, group.getUsers().size() );
+        Assert.assertEquals( 1, anotherGroup.getUsers().size() );
+    }
+
+    // -- //
+
+    @Entity
+    private static class Group {
+
+        @Id
+        Long id;
+
+        @Column
+        String name;
+
+        @ManyToMany( mappedBy = "groups" )
+        List<User> users = new ArrayList<>();
+
+        List<User> getUsers() {
+            return Collections.unmodifiableList( users );
+        }
+
+        void resetUsers() {
+            // this wouldn't trigger association management: users.clear();
+            users = new ArrayList<>();
+        }
+    }
+
+    @Entity
+    private static class User {
+
+        @Id
+        Long id;
+
+        String password;
+
+        @ManyToMany
+        List<Group> groups;
+
+        void addGroup(Group group) {
+            List<Group> groups = this.groups == null ? new ArrayList<>() : this.groups;
+            groups.add( group );
+            this.groups = groups;
+        }
+
+        List<Group> getGroups() {
+            return Collections.unmodifiableList( groups );
+        }
+
+        void setGroups(List<Group> groups) {
+            this.groups = groups;
+        }
+    }
+}

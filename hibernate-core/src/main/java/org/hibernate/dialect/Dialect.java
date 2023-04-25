c3ddd869c969eb85bcce83d2640e0b620a78d090
Steve Ebersole <steve@hibernate.org>
2023-04-25 07:28:34 -0500

    HHH-16270 - Support for null Query parameters on Sybase

--- hibernate-core/src/main/java/org/hibernate/dialect/Dialect.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/Dialect.java
@@ -195,7 +195,6 @@ import org.hibernate.type.spi.TypeConfiguration;
 
 import org.jboss.logging.Logger;
 
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.TemporalType;
 
 import static java.lang.Math.ceil;

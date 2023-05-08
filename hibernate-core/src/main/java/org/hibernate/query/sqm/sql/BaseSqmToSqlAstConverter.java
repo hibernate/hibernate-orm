09c16de3befccb34f0f2650453953220e1ce08ec
Marco Belladelli <marco@hibernate.org>
2023-05-08 11:11:38 +0200

    HHH-16494 Treat entity and attribute joins for inheritance subtypes
    (cherry picked from commit fed020e110fbe82aa9aeeed06bdc6de275df3140)

--- hibernate-core/src/main/java/org/hibernate/query/sqm/sql/BaseSqmToSqlAstConverter.java
+++ hibernate-core/src/main/java/org/hibernate/query/sqm/sql/BaseSqmToSqlAstConverter.java
@@ -49,7 +49,7 @@ import org.hibernate.id.CompositeNestedGeneratedValueGenerator;
 import org.hibernate.id.OptimizableGenerator;
 import org.hibernate.id.enhanced.Optimizer;
 import org.hibernate.internal.FilterHelper;
-import org.hibernate.internal.util.MutableObject;
+import org.hibernate.internal.util.MutableBoolean;
 import org.hibernate.internal.util.collections.CollectionHelper;
 import org.hibernate.internal.util.collections.Stack;
 import org.hibernate.internal.util.collections.StandardStack;
@@ -3365,6 +3365,23 @@ public abstract class BaseSqmToSqlAstConverter<T extends Statement> extends Base
 			registerEntityNameProjectionUsage( sqmJoin, getActualTableGroup( joinedTableGroup, sqmJoin ) );
 		}
 		registerPathAttributeEntityNameUsage( sqmJoin, ownerTableGroup );
+		if ( !sqmJoin.hasTreats() && sqmJoin.getNodeType().getSqmPathType() instanceof EntityDomainType ) {
+			final EntityDomainType<?> entityDomainType = (EntityDomainType<?>) sqmJoin.getNodeType().getSqmPathType();
+			final TableGroup elementTableGroup = joinedTableGroup instanceof PluralTableGroup ?
+					( (PluralTableGroup) joinedTableGroup ).getElementTableGroup() :
+					joinedTableGroup;
+			final EntityValuedModelPart entityModelPart = (EntityValuedModelPart) elementTableGroup.getModelPart();
+			final EntityPersister entityDescriptor = entityModelPart.getEntityMappingType().getEntityPersister();
+			if ( entityDescriptor.getSuperMappingType() != null ) {
+				// This is a non-treated join with an entity which is an inheritance subtype,
+				// register a TREAT entity name use to filter only the entities of the correct type.
+				registerEntityNameUsage(
+						getActualTableGroup( joinedTableGroup, sqmJoin ),
+						EntityNameUse.TREAT,
+						entityDomainType.getHibernateEntityName()
+				);
+			}
+		}
 
 		// Implicit joins in the predicate might alter the nested table group joins,
 		// so defer determination of the join for predicate until after the predicate was visited
@@ -3448,7 +3465,7 @@ public abstract class BaseSqmToSqlAstConverter<T extends Statement> extends Base
 	}
 
 	private TableGroup consumeEntityJoin(SqmEntityJoin<?> sqmJoin, TableGroup lhsTableGroup, boolean transitive) {
-		final MutableObject<Predicate> predicate = new MutableObject<>();
+		final MutableBoolean needsTreat = new MutableBoolean( false );
 		final EntityPersister entityDescriptor = resolveEntityPersister( sqmJoin.getReferencedPathSource() );
 
 		final SqlAstJoinType correspondingSqlJoinType = sqmJoin.getSqmJoinType().getCorrespondingSqlJoinType();
@@ -3457,16 +3474,21 @@ public abstract class BaseSqmToSqlAstConverter<T extends Statement> extends Base
 				sqmJoin.getNavigablePath(),
 				sqmJoin.getExplicitAlias(),
 				null,
-				() -> p -> predicate.set( combinePredicates( predicate.get(), p ) ),
+				() -> p -> needsTreat.setValue( true ),
 				this
 		);
 		registerSqmFromTableGroup( sqmJoin, tableGroup );
+
+		if ( needsTreat.getValue() ) {
+			// Register new treat to apply the discriminator condition to the table reference itself, see #pruneTableGroupJoins
+			registerEntityNameUsage( tableGroup, EntityNameUse.TREAT, entityDescriptor.getEntityName() );
+		}
 
 		final TableGroupJoin tableGroupJoin = new TableGroupJoin(
 				sqmJoin.getNavigablePath(),
 				correspondingSqlJoinType,
 				tableGroup,
-				predicate.get()
+				null
 		);
 		lhsTableGroup.addTableGroupJoin( tableGroupJoin );
 
@@ -4619,6 +4641,7 @@ public abstract class BaseSqmToSqlAstConverter<T extends Statement> extends Base
 				null,
 				this
 		);
+		registerProjectionUsageFromDescriptor( tableGroup, indexDescriptor );
 
 		final CollectionPart valueDescriptor = mapDescriptor.getElementDescriptor();
 		final NavigablePath valueNavigablePath = mapNavigablePath.append( valueDescriptor.getPartName() );
@@ -4628,6 +4651,7 @@ public abstract class BaseSqmToSqlAstConverter<T extends Statement> extends Base
 				null,
 				this
 		);
+		registerProjectionUsageFromDescriptor( tableGroup, valueDescriptor );
 
 		return new DomainResultProducer<Map.Entry<Object, Object>>() {
 			@Override
@@ -4645,6 +4669,14 @@ public abstract class BaseSqmToSqlAstConverter<T extends Statement> extends Base
 				throw new UnsupportedOperationException();
 			}
 		};
+	}
+
+	private void registerProjectionUsageFromDescriptor(TableGroup tableGroup, CollectionPart descriptor) {
+		if ( descriptor instanceof EntityCollectionPart ) {
+			final EntityCollectionPart entityCollectionPart = (EntityCollectionPart) descriptor;
+			final EntityMappingType entityMappingType = entityCollectionPart.getEntityMappingType();
+			registerEntityNameUsage( tableGroup, EntityNameUse.PROJECTION, entityMappingType.getEntityName(), true );
+		}
 	}
 
 	protected Expression createCorrelatedAggregateSubQuery(

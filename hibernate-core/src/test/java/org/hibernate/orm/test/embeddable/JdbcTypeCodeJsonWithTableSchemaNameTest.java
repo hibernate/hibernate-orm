649ddc6bd8edc55aa5a95bed1e8f8ad4166b8fc0
Cedomir Igaly <cedomir.igaly@e-dama.net>
2023-05-30 12:11:44 +0200

    HHH-16612 - Test case refactoring

--- hibernate-core/src/test/java/org/hibernate/orm/test/embeddable/JdbcTypeCodeJsonWithTableSchemaNameTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/embeddable/JdbcTypeCodeJsonWithTableSchemaNameTest.java
@@ -1,102 +1,108 @@
 package org.hibernate.orm.test.embeddable;
 
 import java.util.List;
-import java.util.Map;
 
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.dialect.PostgreSQLDialect;
-import org.hibernate.orm.test.jpa.BaseEntityManagerFunctionalTestCase;
 import org.hibernate.type.SqlTypes;
 
+import org.hibernate.testing.orm.junit.DomainModel;
+import org.hibernate.testing.orm.junit.DomainModelScope;
 import org.hibernate.testing.orm.junit.JiraKey;
 import org.hibernate.testing.orm.junit.RequiresDialect;
-import org.junit.Test;
+import org.hibernate.testing.orm.junit.ServiceRegistry;
+import org.hibernate.testing.orm.junit.SessionFactory;
+import org.hibernate.testing.orm.junit.SessionFactoryScope;
+import org.hibernate.testing.orm.junit.Setting;
+import org.junit.jupiter.api.Test;
 
 import jakarta.persistence.Access;
 import jakarta.persistence.AccessType;
-import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityManager;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Tuple;
 import jakarta.transaction.Transactional;
 
-import static org.hibernate.testing.transaction.TransactionUtil.doInJPA;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DomainModel(annotatedClasses = {
+		JdbcTypeCodeJsonWithTableSchemaNameTest.MyEntity.class,
+		JdbcTypeCodeJsonWithTableSchemaNameTest.MyJson.class
+})
+@SessionFactory
+@ServiceRegistry(
+		settings = {
+				@Setting(name = AvailableSettings.JAKARTA_HBM2DDL_CREATE_SCHEMAS, value = "true")
+		}
+)
 @RequiresDialect(PostgreSQLDialect.class)
-@JiraKey( "HHH-16612" )
-public class JdbcTypeCodeJsonWithTableSchemaNameTest extends BaseEntityManagerFunctionalTestCase {
-
-	@Override
-	protected Class<?>[] getAnnotatedClasses() {
-		return new Class<?>[] {
-				MyEntity.class,
-				MyJson.class
-		};
-	}
-
-	@Override
-	protected void addConfigOptions(Map options) {
-		options.put( AvailableSettings.JAKARTA_HBM2DDL_CREATE_SCHEMAS, true );
-		super.addConfigOptions( options );
-	}
-
-	private static final long PK = 123L;
+@JiraKey("HHH-16612")
+public class JdbcTypeCodeJsonWithTableSchemaNameTest {
 
 	@Test
 	@Transactional
-	@JiraKey( "HHH-16612" )
-	public void shouldQueryMyEntity() throws Exception {
-		doInJPA( this::entityManagerFactory, entityManager -> {
-			insert( entityManager );
-			findAndUpdate( entityManager );
-			List<MyEntity> resultWithoutFilter = selectWithoutFilter( entityManager );
-			selectFiltered( entityManager );
-		} );
-	}
+	@JiraKey("HHH-16612")
+	public void shouldQueryMyEntity(DomainModelScope domainModelScope, SessionFactoryScope sessionFactoryScope) {
+		final var pk = sessionFactoryScope.fromTransaction(
+				session -> {
+					MyEntity myEntity = new MyEntity();
+					MyJson myJson = new MyJson();
+					myJson.setLongProp( 100L );
+					myJson.setStringProp( "Hallo" );
+					myEntity.jsonProperty = myJson;
+					session.persist( myEntity );
+					return myEntity.id;
+				} );
 
-	@Transactional
-	void findAndUpdate(EntityManager em) {
-		MyEntity found = em.find( MyEntity.class, PK );
-		found.jsonProperty.setStringProp( "TEST" );
-	}
+		sessionFactoryScope.inTransaction(
+				session -> {
+					MyEntity found = session.find( MyEntity.class, pk );
+					found.jsonProperty.setStringProp( "TEST" );
+				} );
 
-	@Transactional
-	void selectFiltered(EntityManager em) {
-		//then
-		List<Tuple> resultWithFilter = em
-				.createQuery( "SELECT e FROM MyEntity e WHERE e.jsonProperty.longProp = :x", Tuple.class )
-				.setParameter( "x", 100L )
-				.getResultList();
-		assertEquals( 1, resultWithFilter.size() );
-	}
+		sessionFactoryScope.inSession(
+				session -> {
+					List<MyEntity> resultWithoutFilter =
+							session
+									.createQuery( "SELECT e FROM MyEntity e", MyEntity.class )
+									.getResultList();
+					assertEquals( 1, resultWithoutFilter.size() );
+				}
+		);
 
+		sessionFactoryScope.inSession(
+				session -> {
+					List<Tuple> resultWithFilter = session
+							.createQuery( "SELECT e FROM MyEntity e WHERE e.jsonProperty.stringProp = :x", Tuple.class )
+							.setParameter( "x", "TEST" )
+							.getResultList();
+					assertEquals( 1, resultWithFilter.size() );
+				}
+		);
 
-	@Transactional
-	List<MyEntity> selectWithoutFilter(EntityManager em) {
-		List<MyEntity> resultWithoutFilter = em
-				.createQuery( "SELECT e FROM MyEntity e", MyEntity.class )
-				.getResultList();
-		assertEquals( 1, resultWithoutFilter.size() );
-		return resultWithoutFilter;
-	}
+		sessionFactoryScope.inSession(
+				session -> {
+					List<Tuple> resultWithFilter = session
+							.createQuery( "SELECT e FROM MyEntity e WHERE e.jsonProperty.longProp = :x", Tuple.class )
+							.setParameter( "x", 100L )
+							.getResultList();
+					assertEquals( 1, resultWithFilter.size() );
+				}
+		);
 
-	@Transactional(Transactional.TxType.REQUIRES_NEW)
-	void insert(EntityManager em) {
-		//given
-		MyEntity myEntity = new MyEntity();
-		myEntity.id = PK;
-		MyJson myJson = new MyJson();
-		myJson.setLongProp( 100L );
-		myJson.setStringProp( "Hallo" );
-		myEntity.jsonProperty = myJson;
-//        myEntity.jsonProperty = new HashMap<>(Map.of("x", "Y"));
-		//when
-		em.persist( myEntity );
+		sessionFactoryScope.inSession(
+				session -> {
+					List<Tuple> resultWithFilter = session
+							.createQuery( "SELECT e FROM MyEntity e WHERE e.jsonProperty.longProp = :x", Tuple.class )
+							.setParameter( "x", 200L )
+							.getResultList();
+					assertEquals( 0, resultWithFilter.size() );
+				}
+		);
 	}
 
 	@Entity(name = "MyEntity")
@@ -104,11 +110,27 @@ public class JdbcTypeCodeJsonWithTableSchemaNameTest extends BaseEntityManagerFu
 	public static class MyEntity {
 
 		@Id
-		@Column
+		@GeneratedValue
 		public Long id;
 
 		@JdbcTypeCode(SqlTypes.JSON)
 		MyJson jsonProperty;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public MyJson getJsonProperty() {
+			return jsonProperty;
+		}
+
+		public void setJsonProperty(MyJson jsonProperty) {
+			this.jsonProperty = jsonProperty;
+		}
 	}
 
 	@Embeddable

08de0e15c11a54e862620becdee0b797e0ff38fd
Christian Beikov <christian.beikov@gmail.com>
2023-06-13 19:10:43 +0200

    HHH-16787 Multi-valued parameter with single element parameter list fails with no such element exception

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/query/hql/MultiValuedParameterInFunctionTest.java
@@ -0,0 +1,85 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
+package org.hibernate.orm.test.query.hql;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.hibernate.boot.model.FunctionContributions;
+import org.hibernate.boot.model.FunctionContributor;
+import org.hibernate.dialect.H2Dialect;
+
+import org.hibernate.testing.orm.junit.BootstrapServiceRegistry;
+import org.hibernate.testing.orm.junit.BootstrapServiceRegistry.JavaService;
+import org.hibernate.testing.orm.junit.DomainModel;
+import org.hibernate.testing.orm.junit.JiraKey;
+import org.hibernate.testing.orm.junit.RequiresDialect;
+import org.hibernate.testing.orm.junit.SessionFactory;
+import org.hibernate.testing.orm.junit.SessionFactoryScope;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@BootstrapServiceRegistry(
+		javaServices = {
+				@JavaService(role = FunctionContributor.class, impl = MultiValuedParameterInFunctionTest.MyFunctionContributor.class)
+		}
+)
+@DomainModel(annotatedClasses = MultiValuedParameterInFunctionTest.Post.class)
+@SessionFactory
+@RequiresDialect(H2Dialect.class)
+public class MultiValuedParameterInFunctionTest {
+
+	@Test
+	@JiraKey( "HHH-16787" )
+	public void testAsQueryParameter(SessionFactoryScope scope) {
+		scope.inTransaction( (session) -> {
+			session.createQuery( "from Post p where my_function((select p2.id from Post p2 where p2.title in (:name)))=1" )
+					.setParameter( "name", Collections.singletonList( "1" ) )
+					.list();
+		} );
+	}
+
+
+	@AfterEach
+	public void dropTestData(SessionFactoryScope scope) {
+		scope.inTransaction( (session) -> session.createQuery( "delete Post" ).executeUpdate() );
+	}
+
+
+	@Entity(name = "Post")
+	@Table(name = "Post")
+	public static class Post {
+		@Id
+		public Integer id;
+		@Basic
+		public String title;
+
+		private Post() {
+			// for Hibernate use
+		}
+
+		public Post(Integer id, String title) {
+			this.id = id;
+			this.title = title;
+		}
+	}
+	public static class MyFunctionContributor implements FunctionContributor {
+
+		@Override
+		public void contributeFunctions(FunctionContributions functionContributions) {
+			functionContributions.getFunctionRegistry().registerPattern(
+					"my_function",
+					"?1"
+			);
+		}
+	}
+}

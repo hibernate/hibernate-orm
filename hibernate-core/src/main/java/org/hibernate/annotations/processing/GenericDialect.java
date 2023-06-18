7d79760b9adeff81b46ab58914096c285cbc1e60
Gavin King <gavin@hibernate.org>
2023-06-18 03:21:05 +0200

    HHH-16633 make GenericDialect public
    oops, tests did not catch this

--- hibernate-core/src/main/java/org/hibernate/annotations/processing/GenericDialect.java
+++ hibernate-core/src/main/java/org/hibernate/annotations/processing/GenericDialect.java
@@ -6,9 +6,19 @@
  */
 package org.hibernate.annotations.processing;
 
+import org.hibernate.dialect.DatabaseVersion;
 import org.hibernate.dialect.Dialect;
 
-class GenericDialect extends Dialect {
-	GenericDialect() {
+/**
+ * A generic {@linkplain Dialect dialect} for ANSI-like SQL.
+ * Used by default in the HQL Query Validator.
+ *
+ * @author Gavin King
+ *
+ * @see CheckHQL#dialect
+ */
+public class GenericDialect extends Dialect {
+	public GenericDialect() {
+		super( (DatabaseVersion) null );
 	}
 }

1286109d525968685a6afd659edf566b52509b01
Jan Schatteman <jschatte@redhat.com>
2023-06-20 01:04:10 +0200

    HHH-16711 / HHH-16707 - Added tests for these related issues
    Signed-off-by: Jan Schatteman <jschatte@redhat.com>

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/inheritance/hhh16711/Inheriting.java
@@ -0,0 +1,24 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
+package org.hibernate.orm.test.inheritance.hhh16711;
+
+import org.hibernate.orm.test.inheritance.hhh16711.otherPackage.Inherited;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "inheriting")
+class Inheriting extends Inherited {
+
+	Inheriting(String id, String name) {
+		super(id, name);
+	}
+
+	Inheriting() {
+	}
+}

1f2f82e35881aa1be2fbb32d74fd24260fcc5498
Victor Bonnier <victor.bonnier@telenor.se>
2023-06-19 10:48:33 +0200

    HHH-16819: Changed JAKARTA_JPA_GROUP_PREFIX to correct value

--- hibernate-core/src/main/java/org/hibernate/boot/beanvalidation/GroupsPerOperation.java
+++ hibernate-core/src/main/java/org/hibernate/boot/beanvalidation/GroupsPerOperation.java
@@ -22,7 +22,7 @@ import jakarta.validation.groups.Default;
  */
 public class GroupsPerOperation {
 	private static final String JPA_GROUP_PREFIX = "javax.persistence.validation.group.";
-	private static final String JAKARTA_JPA_GROUP_PREFIX = "javax.persistence.validation.group.";
+	private static final String JAKARTA_JPA_GROUP_PREFIX = "jakarta.persistence.validation.group.";
 	private static final String HIBERNATE_GROUP_PREFIX = "org.hibernate.validator.group.";
 
 	private static final Class<?>[] DEFAULT_GROUPS = new Class<?>[] { Default.class };

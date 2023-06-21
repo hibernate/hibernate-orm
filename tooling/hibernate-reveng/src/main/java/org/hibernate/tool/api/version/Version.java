c3b543806da4a39a16c25772d099f3179e527bdd
Koen Aers <koen.aers@gmail.com>
2023-06-21 09:07:47 +0200

    HBX-2535: Update version identifier to 6.2.6-SNAPSHOT
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- tooling/hibernate-reveng/src/main/java/org/hibernate/tool/api/version/Version.java
+++ tooling/hibernate-reveng/src/main/java/org/hibernate/tool/api/version/Version.java
@@ -2,6 +2,6 @@ package org.hibernate.tool.api.version;
 
 public interface Version {
 	
-	final static String CURRENT_VERSION = "6.2.5.Final";
+	final static String CURRENT_VERSION = "6.2.6-SNAPSHOT";
 
 }

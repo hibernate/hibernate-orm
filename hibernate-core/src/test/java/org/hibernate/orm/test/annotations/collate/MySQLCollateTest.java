d6ffb75251642e9b894a78e4539b6ee356cb7812
Gavin King <gavin@hibernate.org>
2023-06-25 15:26:13 +0200

    HHH-16829 skip test on TiDB

--- hibernate-core/src/test/java/org/hibernate/orm/test/annotations/collate/MySQLCollateTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/annotations/collate/MySQLCollateTest.java
@@ -7,15 +7,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import org.hibernate.annotations.Collate;
 import org.hibernate.dialect.MySQLDialect;
+import org.hibernate.dialect.TiDBDialect;
 import org.hibernate.testing.orm.junit.DomainModel;
 import org.hibernate.testing.orm.junit.RequiresDialect;
 import org.hibernate.testing.orm.junit.SessionFactory;
 import org.hibernate.testing.orm.junit.SessionFactoryScope;
+import org.hibernate.testing.orm.junit.SkipForDialect;
 import org.junit.jupiter.api.Test;
 
 @SessionFactory
 @DomainModel(annotatedClasses = MySQLCollateTest.Message.class)
 @RequiresDialect(MySQLDialect.class)
+@SkipForDialect(dialectClass = TiDBDialect.class)
 public class MySQLCollateTest {
 
     @Test void test(SessionFactoryScope scope) {

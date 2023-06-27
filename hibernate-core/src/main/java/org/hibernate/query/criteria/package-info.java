de880d6982bb8ddc5794bc9e996ba9d96430f166
Gavin King <gavin@hibernate.org>
2023-06-27 11:43:47 +0200

    add decent javadoc blurb to HibernateCriteriaBuilder and package

--- hibernate-core/src/main/java/org/hibernate/query/criteria/package-info.java
+++ hibernate-core/src/main/java/org/hibernate/query/criteria/package-info.java
@@ -6,11 +6,39 @@
  */
 
 /**
- * Support for JPA criteria queries.
+ * The JPA-standard criteria query API defines all the operations needed express
+ * any query written in standard JPQL. This package extends the JPA-defined API,
+ * allowing any query written in HQL to be expressed via the criteria API.
+ * <p>
+ * The gateway to this functionality is
+ * {@link org.hibernate.query.criteria.HibernateCriteriaBuilder}, which extends
+ * {@link jakarta.persistence.criteria.CriteriaBuilder}.
+ * <p>
+ * Types defined in this package extend the equivalent types in
+ * {@link jakarta.persistence.criteria} with additional operations. For example,
+ * {@link org.hibernate.query.criteria.JpaCriteriaQuery} adds the methods:
+ * <ul>
+ * <li>{@link org.hibernate.query.criteria.JpaCriteriaQuery#from(Subquery)},
+ *     which allows the use of a subquery in the {@code from} clause of the
+ *     query, and
+ * <li>{@link org.hibernate.query.criteria.JpaCriteriaQuery#with(AbstractQuery)},
+ *     which allows the creation of {@link org.hibernate.query.criteria.JpaCteCriteria
+ *     common table expressions}.
+ * </ul>
  *
  * @see org.hibernate.query.criteria.HibernateCriteriaBuilder
+ * @see org.hibernate.query.criteria.JpaCriteriaQuery
+ * @see org.hibernate.query.criteria.JpaCriteriaUpdate
+ * @see org.hibernate.query.criteria.JpaCriteriaDelete
+ * @see org.hibernate.query.criteria.JpaCriteriaInsertValues
+ * @see org.hibernate.query.criteria.JpaCriteriaInsertSelect
+ * @see org.hibernate.query.criteria.JpaCteCriteria
+ * @see org.hibernate.query.criteria.JpaSubQuery
+ * @see org.hibernate.query.criteria.JpaExpression
  */
 @Incubating
 package org.hibernate.query.criteria;
 
+import jakarta.persistence.criteria.Subquery;
 import org.hibernate.Incubating;
+import jakarta.persistence.criteria.AbstractQuery;

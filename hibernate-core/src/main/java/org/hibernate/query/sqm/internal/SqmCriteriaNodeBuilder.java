e9954ac358985ac77d51917b50a112f1b031b802
Marco Belladelli <marco@hibernate.org>
2023-06-28 12:55:47 +0200

    HHH-16803 Specially handle enum classes in nullLiteral

--- hibernate-core/src/main/java/org/hibernate/query/sqm/internal/SqmCriteriaNodeBuilder.java
+++ hibernate-core/src/main/java/org/hibernate/query/sqm/internal/SqmCriteriaNodeBuilder.java
@@ -1229,6 +1229,10 @@ public class SqmCriteriaNodeBuilder implements NodeBuilder, SqmCreationContext, 
 
 	@Override
 	public <T> SqmExpression<T> nullLiteral(Class<T> resultClass) {
+		if ( resultClass.isEnum() ) {
+			// No basic types are registered for enum java types, we have to use an untyped null literal in this case
+			return new SqmLiteralNull<>( this );
+		}
 		final BasicType<T> basicTypeForJavaType = getTypeConfiguration().getBasicTypeForJavaType( resultClass );
 		// if there's no basic type, it might be an entity type
 		final SqmExpressible<T> sqmExpressible = basicTypeForJavaType == null

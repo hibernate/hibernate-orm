a07d515725ccdfbb1d676ddcef5364996ad69e6a
Marco Belladelli <marco@hibernate.org>
2023-07-04 10:27:57 +0200

    HHH-16845 Fix collection key parameter

--- hibernate-core/src/main/java/org/hibernate/loader/ast/internal/AbstractCollectionBatchLoader.java
+++ hibernate-core/src/main/java/org/hibernate/loader/ast/internal/AbstractCollectionBatchLoader.java
@@ -87,7 +87,7 @@ public abstract class AbstractCollectionBatchLoader implements CollectionBatchLo
 					persistenceContext,
 					entry.getLoadedPersister(),
 					collection,
-					collectionKey,
+					key,
 					true
 			);
 		}

09a3135070fdd0f55027fd6652b925a5c4e2effb
Gavin King <gavin@hibernate.org>
2023-07-10 23:16:37 +0200

    HHH-16633 completely rework validation for parameters of @HQL query methods

--- tooling/metamodel-generator/src/main/java/org/hibernate/jpamodelgen/validation/Validation.java
+++ tooling/metamodel-generator/src/main/java/org/hibernate/jpamodelgen/validation/Validation.java
@@ -9,7 +9,6 @@ package org.hibernate.jpamodelgen.validation;
 import org.antlr.v4.runtime.ANTLRErrorListener;
 import org.antlr.v4.runtime.BailErrorStrategy;
 import org.antlr.v4.runtime.DefaultErrorStrategy;
-import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.atn.PredictionMode;
 import org.antlr.v4.runtime.misc.ParseCancellationException;
 import org.checkerframework.checker.nullness.qual.Nullable;
@@ -26,13 +25,6 @@ import org.hibernate.query.sqm.TerminalPathException;
 import org.hibernate.query.sqm.tree.SqmStatement;
 import org.hibernate.type.descriptor.java.spi.JdbcTypeRecommendationException;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
-import static java.lang.Character.isJavaIdentifierStart;
-import static java.lang.Integer.parseInt;
-import static java.util.stream.Stream.concat;
 
 /**
  * The entry point for HQL validation.
@@ -50,33 +42,22 @@ public class Validation {
 
 	public static @Nullable SqmStatement<?> validate(
 			String hql,
-			boolean checkParams, boolean checkTyping,
-			Set<Integer> setParameterLabels,
-			Set<String> setParameterNames,
+			boolean checkTyping,
 			Handler handler,
 			SessionFactoryImplementor factory) {
-		return validate( hql, checkParams, checkTyping, setParameterLabels, setParameterNames, handler, factory, 0 );
+		return validate( hql, checkTyping, handler, factory, 0 );
 	}
 
 	public static @Nullable SqmStatement<?> validate(
 			String hql,
-			boolean checkParams, boolean checkTyping,
-			Set<Integer> setParameterLabels,
-			Set<String> setParameterNames,
+			boolean checkTyping,
 			Handler handler,
 			SessionFactoryImplementor factory,
 			int errorOffset) {
-//		handler = new Filter(handler, errorOffset);
-
 		try {
 			final HqlParser.StatementContext statementContext = parseAndCheckSyntax( hql, handler );
 			if ( checkTyping && handler.getErrorCount() == 0 ) {
-				final SqmStatement<?> statement =
-						checkTyping( hql, handler, factory, errorOffset, statementContext );
-				if ( checkParams ) {
-					checkParameterBinding( hql, setParameterLabels, setParameterNames, handler, errorOffset );
-				}
-				return statement;
+				return checkTyping( hql, handler, factory, errorOffset, statementContext );
 			}
 		}
 		catch (Exception e) {
@@ -130,112 +111,6 @@ public class Validation {
 			hqlParser.setErrorHandler( new DefaultErrorStrategy() );
 
 			return hqlParser.statement();
-		}
-	}
-
-	private static void checkParameterBinding(
-			String hql,
-			Set<Integer> setParameterLabels,
-			Set<String> setParameterNames,
-			Handler handler,
-			int errorOffset) {
-		try {
-			String unsetParams = null;
-			String notSet = null;
-			String parameters = null;
-			int start = -1;
-			int end = -1;
-			List<String> names = new ArrayList<>();
-			List<Integer> labels = new ArrayList<>();
-			final HqlLexer hqlLexer = HqlParseTreeBuilder.INSTANCE.buildHqlLexer( hql );
-			loop:
-			while (true) {
-				Token token = hqlLexer.nextToken();
-				int tokenType = token.getType();
-				switch (tokenType) {
-					case HqlLexer.EOF:
-						break loop;
-					case HqlLexer.QUESTION_MARK:
-					case HqlLexer.COLON:
-						Token next = hqlLexer.nextToken();
-						String text = next.getText();
-						switch (tokenType) {
-							case HqlLexer.COLON:
-								if ( !text.isEmpty()
-										&& isJavaIdentifierStart( text.codePointAt(0) ) ) {
-									names.add(text);
-									if ( setParameterNames.contains(text) ) {
-										continue;
-									}
-								}
-								else {
-									continue;
-								}
-								break;
-							case HqlLexer.QUESTION_MARK:
-								if ( next.getType() == HqlLexer.INTEGER_LITERAL ) {
-									int label;
-									try {
-										label = parseInt(text);
-									}
-									catch (NumberFormatException nfe) {
-										continue;
-									}
-									labels.add(label);
-									if ( setParameterLabels.contains(label) ) {
-										continue;
-									}
-								}
-								else {
-									continue;
-								}
-								break;
-							default:
-								continue;
-						}
-						parameters = unsetParams == null ? "Parameter " : "Parameters ";
-						notSet = unsetParams == null ? " is not set" : " are not set";
-						unsetParams = unsetParams == null ? "" : unsetParams + ", ";
-						unsetParams += token.getText() + text;
-						if (start == -1) {
-							start = token.getCharPositionInLine(); //TODO: wrong for multiline query strings!
-						}
-						end = token.getCharPositionInLine() + text.length() + 1;
-						break;
-				}
-			}
-			if ( unsetParams != null ) {
-				handler.warn( start-errorOffset+1, end-errorOffset, parameters + unsetParams + notSet );
-			}
-
-			setParameterNames.removeAll(names);
-			setParameterLabels.removeAll(labels);
-
-			reportMissingParams( setParameterLabels, setParameterNames, handler );
-		}
-		finally {
-			setParameterNames.clear();
-			setParameterLabels.clear();
-		}
-	}
-
-	private static void reportMissingParams(Set<Integer> setParameterLabels, Set<String> setParameterNames, Handler handler) {
-		final int count = setParameterNames.size() + setParameterLabels.size();
-		if (count > 0) {
-			final String missingParams =
-					concat( setParameterNames.stream().map(name -> ":" + name),
-							setParameterLabels.stream().map(label -> "?" + label) )
-							.reduce((x, y) -> x + ", " + y)
-							.orElse(null);
-			final String params =
-					count == 1 ?
-							"Parameter " :
-							"Parameters ";
-			final String notOccur =
-					count == 1 ?
-							" does not occur in the query" :
-							" do not occur in the query";
-			handler.warn(0, 0, params + missingParams + notOccur);
 		}
 	}
 }

187e637b68263917427b1083f219b1c4536a9873
Sanne Grinovero <sanne@hibernate.org>
2023-07-13 18:58:16 +0100

    HHH-16911 Introduce a testing utility to spot ClassLoader leaks

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/bootstrap/registry/classloading/NotLeakingTestAction.java
@@ -0,0 +1,23 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
+package org.hibernate.orm.test.bootstrap.registry.classloading;
+
+/**
+ * A Runnable used to test ClassLoaderLeakDetector
+ * @see ClassLoaderLeaksUtilityTest
+ */
+public class NotLeakingTestAction implements Runnable {
+
+	@Override
+	public void run() {
+		final ClassLoader owningClassloader = getClass().getClassLoader();
+		if ( !owningClassloader.getName().equals( "TestIsolatedIsolatedClassLoader" ) ) {
+			throw new IllegalStateException( "Not being loaded by the expected classloader" );
+		}
+	}
+
+}

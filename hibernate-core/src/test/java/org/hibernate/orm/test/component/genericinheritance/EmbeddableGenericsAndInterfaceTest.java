8ab82f1401c8f38332a046d231f4d6c712a2fc7b
Marco Belladelli <marco@hibernate.org>
2023-08-01 14:01:29 +0200

    HHH-16755 Add test for issue

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/component/genericinheritance/EmbeddableGenericsAndInterfaceTest.java
@@ -0,0 +1,70 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.orm.test.component.genericinheritance;
+
+import org.hibernate.testing.orm.junit.DomainModel;
+import org.hibernate.testing.orm.junit.Jira;
+import org.hibernate.testing.orm.junit.SessionFactory;
+import org.hibernate.testing.orm.junit.SessionFactoryScope;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Marco Belladelli
+ */
+@SessionFactory
+@DomainModel( annotatedClasses = {
+		UserEntity.class,
+		ExampleSuperClassEmbedded.class,
+		ExampleEmbedded.class,
+		ExampleEntity.class,
+} )
+@Jira( "https://hibernate.atlassian.net/browse/HHH-16755" )
+public class EmbeddableGenericsAndInterfaceTest {
+	@BeforeAll
+	public void setUp(SessionFactoryScope scope) {
+		scope.inTransaction( session -> {
+			final UserEntity user = new UserEntity();
+			user.setName( "Debbie" );
+			session.persist( user );
+			final ExampleEmbedded<?> embedded = new ExampleEmbedded<>();
+			embedded.setUser( user );
+			final ExampleEntity entity = new ExampleEntity();
+			entity.setExampleEmbedded( embedded );
+			session.persist( entity );
+		} );
+	}
+
+	@AfterAll
+	public void tearDown(SessionFactoryScope scope) {
+		scope.inTransaction( session -> {
+			session.createMutationQuery( "delete from ExampleEntity" ).executeUpdate();
+			session.createMutationQuery( "delete from UserEntity" ).executeUpdate();
+		} );
+	}
+
+	@Test
+	public void testMetamodelCriteria(SessionFactoryScope scope) {
+		scope.inTransaction( session -> {
+			final CriteriaBuilder cb = session.getCriteriaBuilder();
+			final CriteriaQuery<ExampleEntity> cq = cb.createQuery( ExampleEntity.class );
+			final Root<ExampleEntity> root = cq.from( ExampleEntity.class );
+			cq.select( root ).where( cb.isNotNull(
+					root.get( ExampleEntity_.exampleEmbedded ).get( ExampleEmbedded_.user )
+			) );
+			final ExampleEntity result = session.createQuery( cq ).getSingleResult();
+			assertThat( result.getExampleEmbedded().getUser().getName() ).isEqualTo( "Debbie" );
+		} );
+	}
+}

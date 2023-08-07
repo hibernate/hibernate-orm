005e28113e370946363e5145a26b2137053977e8
Andrea Boriero <andrea@hibernate.org>
2023-08-07 16:32:22 +0200

    HHH-17041 Add test for issue

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/bytecode/enhance/internal/bytebuddy/DirtyCheckingWithEmbeddableNonVisibleGenericExtendsSerializableMappedSuperclassTest.java
@@ -0,0 +1,126 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
+package org.hibernate.orm.test.bytecode.enhance.internal.bytebuddy;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.hibernate.testing.bytecode.enhancement.BytecodeEnhancerRunner;
+import org.hibernate.testing.bytecode.enhancement.EnhancementOptions;
+import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
+import org.hibernate.testing.orm.junit.JiraKey;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Tuple;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(BytecodeEnhancerRunner.class)
+@EnhancementOptions(inlineDirtyChecking = true)
+public class DirtyCheckingWithEmbeddableNonVisibleGenericExtendsSerializableMappedSuperclassTest extends
+		BaseCoreFunctionalTestCase {
+
+	@Override
+	protected Class<?>[] getAnnotatedClasses() {
+		return new Class[]{
+				MyEntity.class
+		};
+	}
+
+	@JiraKey("HHH-17041")
+	@Test
+	public void testQueryEmbeddableFields() {
+		inTransaction(
+				session -> {
+					MyEntity myEntity  = new MyEntity(1, "one");
+					session.persist( myEntity );
+				}
+		);
+		inTransaction(
+				session -> {
+					List<Tuple> result = session.createQuery( "select m.embedded.text, m.embedded.name from MyEntity m", Tuple.class ).list();
+					assertThat( result.size() ).isEqualTo( 1 );
+					Tuple tuple = result.get( 0 );
+					assertThat( tuple.get( 0 ) ).isEqualTo( "one" );
+					assertThat( tuple.get( 1 ) ).isNull();
+
+				}
+		);
+	}
+
+	@MappedSuperclass
+	public static abstract class MyAbstractEmbeddable implements Serializable {
+		private String name;
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+	}
+
+	@Embeddable
+	public static class MyEmbeddable extends MyAbstractEmbeddable {
+
+		private String text;
+
+		public MyEmbeddable() {
+		}
+
+		private MyEmbeddable(String text) {
+			this.text = text;
+		}
+
+		public String getText() {
+			return text;
+		}
+
+		public void setText(String text) {
+			this.text = text;
+		}
+	}
+
+
+	@MappedSuperclass
+	public static abstract class MyMappedSuperclass<C extends MyAbstractEmbeddable>
+			extends MyNonVisibleGenericExtendsSerializableMappedSuperclass<C> {
+	}
+
+	@Entity(name = "MyEntity")
+	public static class MyEntity extends
+			MyMappedSuperclass<MyEmbeddable> {
+
+		@Id
+		private Integer id;
+
+		public MyEntity() {
+		}
+
+		private MyEntity(Integer id, String text) {
+			this.id = id;
+			setEmbedded( new MyEmbeddable( text ) );
+		}
+
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+	}
+
+}

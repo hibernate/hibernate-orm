e338d4916574ef72e372709ceb7e78369776e5c1
marko-bekhta <marko.prykladna@gmail.com>
2023-08-11 17:44:45 +0200

    HHH-17072 Process tenant-id from XML mapping

--- hibernate-core/src/test/java/org/hibernate/orm/test/boot/jaxb/mapping/HibernateOrmSpecificAttributesMappingTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/boot/jaxb/mapping/HibernateOrmSpecificAttributesMappingTest.java
@@ -13,10 +13,16 @@ import java.util.Set;
 import java.util.UUID;
 
 import org.hibernate.boot.model.process.internal.UserTypeResolution;
+import org.hibernate.boot.model.relational.Database;
 import org.hibernate.generator.Generator;
+import org.hibernate.generator.GeneratorCreationContext;
+import org.hibernate.generator.internal.TenantIdGeneration;
 import org.hibernate.id.uuid.UuidGenerator;
 import org.hibernate.mapping.BasicValue;
+import org.hibernate.mapping.GeneratorCreator;
+import org.hibernate.mapping.PersistentClass;
 import org.hibernate.mapping.Property;
+import org.hibernate.service.ServiceRegistry;
 import org.hibernate.type.SqlTypes;
 import org.hibernate.usertype.UserTypeSupport;
 
@@ -44,6 +50,17 @@ public class HibernateOrmSpecificAttributesMappingTest {
 					.isInstanceOf( BasicValue.class );
 			assertThat( ( (BasicValue) tags.getValue() ).getResolution() )
 					.isInstanceOf( UserTypeResolution.class );
+		} );
+
+		scope.withHierarchy( HibernateOrmSpecificAttributesMappingTest.MyEntityWithTenantId.class, (entityDescriptor) -> {
+			Property tenantId = entityDescriptor.getProperty( "tenantId" );
+			GeneratorCreator valueGeneratorCreator = tenantId.getValueGeneratorCreator();
+			assertThat( valueGeneratorCreator ).isNotNull();
+			Generator generator = valueGeneratorCreator.createGenerator( new CustomTenantGeneratorContext( tenantId ) );
+
+			assertThat( generator )
+					.isInstanceOf( TenantIdGeneration.class );
+
 		} );
 	}
 
@@ -79,9 +96,69 @@ public class HibernateOrmSpecificAttributesMappingTest {
 		}
 	}
 
+	public static class MyEntityWithTenantId {
+		private Long id;
+
+		private String tenantId;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getTenantId() {
+			return tenantId;
+		}
+
+		public void setTenantId(String tenantId) {
+			this.tenantId = tenantId;
+		}
+	}
+
 	public static class DelimitedStringsJavaType extends UserTypeSupport<Set> {
 		public DelimitedStringsJavaType() {
 			super( Set.class, Types.VARCHAR );
+		}
+	}
+
+	private static class CustomTenantGeneratorContext implements GeneratorCreationContext {
+		private final Property tenantId;
+
+		public CustomTenantGeneratorContext(Property tenantId) {
+			this.tenantId = tenantId;
+		}
+
+		@Override
+		public Database getDatabase() {
+			return null;
+		}
+
+		@Override
+		public ServiceRegistry getServiceRegistry() {
+			return null;
+		}
+
+		@Override
+		public String getDefaultCatalog() {
+			return null;
+		}
+
+		@Override
+		public String getDefaultSchema() {
+			return null;
+		}
+
+		@Override
+		public PersistentClass getPersistentClass() {
+			return tenantId.getPersistentClass();
+		}
+
+		@Override
+		public Property getProperty() {
+			return tenantId;
 		}
 	}
 }

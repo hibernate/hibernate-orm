9c375e1a703cc04d1573648ed465d6ade71bbe82
Bruno Sofiato <bruno.sofiato@gmail.com>
2023-08-18 01:41:12 -0300

    HHH-17079 - Fixed NPE when mapping a generic class as a composite user type

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/cut/generic/GenericCompositeUserTypeTest.java
@@ -0,0 +1,34 @@
+package org.hibernate.orm.test.cut.generic;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import org.hibernate.testing.TestForIssue;
+import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
+import org.junit.Test;
+
+
+@TestForIssue(jiraKey = "HHH-17019")
+public class GenericCompositeUserTypeTest extends BaseCoreFunctionalTestCase {
+
+    @Override
+    protected Class<?>[] getAnnotatedClasses() {
+        return new Class<?>[] {
+                GenericCompositeUserTypeEntity.class
+        };
+    }
+
+    @Test
+    public void hhh17019Test() throws Exception {
+        Session s = openSession();
+        Transaction tx = s.beginTransaction();
+
+        EnumPlaceholder<Weekdays, Weekdays> placeholder = new EnumPlaceholder<>( Weekdays.MONDAY, Weekdays.SUNDAY );
+        GenericCompositeUserTypeEntity entity = new GenericCompositeUserTypeEntity( placeholder );
+
+        s.persist( entity );
+
+        tx.commit();
+        s.close();
+    }
+}

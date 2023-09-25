8f4afa625f0fa9c0d6a3193fb176e86849bf3e9d
Steve Ebersole <steve@hibernate.org>
2023-09-25 17:24:17 -0500

    natural-id + not-found
    https://hibernate.atlassian.net/browse/HHH-17197 - Add check for illegal combo of to-one + natural-id + not-found
    https://hibernate.atlassian.net/browse/HHH-17196 - Documentation for @NaturalId should be more explicit about non-nullability
    
    (cherry picked from commit 6c2e04381da4fdf26999fb3a31ca9514955da33b)

--- hibernate-core/src/main/java/org/hibernate/tuple/entity/EntityMetamodel.java
+++ hibernate-core/src/main/java/org/hibernate/tuple/entity/EntityMetamodel.java
@@ -19,6 +19,8 @@ import java.util.Set;
 import org.hibernate.EntityMode;
 import org.hibernate.HibernateException;
 import org.hibernate.MappingException;
+import org.hibernate.annotations.NotFoundAction;
+import org.hibernate.boot.spi.MetadataImplementor;
 import org.hibernate.bytecode.enhance.spi.interceptor.EnhancementHelper;
 import org.hibernate.bytecode.spi.BytecodeEnhancementMetadata;
 import org.hibernate.cfg.NotYetImplementedException;
@@ -30,8 +32,15 @@ import org.hibernate.internal.CoreMessageLogger;
 import org.hibernate.internal.util.ReflectHelper;
 import org.hibernate.internal.util.collections.ArrayHelper;
 import org.hibernate.mapping.Component;
+import org.hibernate.mapping.GeneratorCreator;
+import org.hibernate.mapping.ManyToOne;
 import org.hibernate.mapping.PersistentClass;
 import org.hibernate.mapping.Property;
+import org.hibernate.mapping.Subclass;
+import org.hibernate.mapping.ToOne;
+import org.hibernate.mapping.Value;
+import org.hibernate.metamodel.mapping.EntityMappingType;
+import org.hibernate.metamodel.spi.RuntimeModelCreationContext;
 import org.hibernate.persister.entity.EntityPersister;
 import org.hibernate.tuple.GenerationTiming;
 import org.hibernate.tuple.IdentifierProperty;
@@ -231,6 +240,7 @@ public class EntityMetamodel implements Serializable {
 			}
 
 			if ( prop.isNaturalIdentifier() ) {
+				verifyNaturalIdProperty( property );
 				naturalIdNumbers.add( i );
 				if ( prop.isUpdateable() ) {
 					foundUpdateableNaturalIdProperty = true;
@@ -656,6 +666,29 @@ public class EntityMetamodel implements Serializable {
 				return NO_GEN_PAIR;
 			}
 		}
+	}
+
+	private void verifyNaturalIdProperty(Property property) {
+		final Value value = property.getValue();
+		if ( value instanceof ManyToOne ) {
+			final ManyToOne toOne = (ManyToOne) value;
+			if ( toOne.getNotFoundAction() == NotFoundAction.IGNORE ) {
+				throw new MappingException(
+						"Attribute marked as natural-id can not also be a not-found association - "
+								+ propertyName( property )
+				);
+			}
+		}
+		else if ( value instanceof Component ) {
+			final Component component = (Component) value;
+			for ( Property componentProperty : component.getProperties() ) {
+				verifyNaturalIdProperty( componentProperty );
+			}
+		}
+	}
+
+	private String propertyName(Property property) {
+		return getName() + "." + property.getName();
 	}
 
 	private static class NoInMemoryValueGenerationStrategy implements InMemoryValueGenerationStrategy {

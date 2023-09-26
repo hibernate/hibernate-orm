59d254cae1cd5814700282dbed87d121b59a116a
Christian Beikov <christian.beikov@gmail.com>
2023-09-26 15:45:04 +0200

    HHH-17242 Partly revert original solution to retain correct expression types

--- hibernate-core/src/main/java/org/hibernate/dialect/DialectDelegateWrapper.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/DialectDelegateWrapper.java
@@ -301,15 +301,6 @@ public class DialectDelegateWrapper extends Dialect {
 	}
 
 	@Override
-	public String timestampaddPattern(
-			TemporalUnit unit,
-			TemporalType temporalType,
-			IntervalType intervalType,
-			boolean withTimeZone) {
-		return wrapped.timestampaddPattern( unit, temporalType, intervalType, withTimeZone );
-	}
-
-	@Override
 	public String timestampaddPattern(TemporalUnit unit, TemporalType temporalType, IntervalType intervalType) {
 		return wrapped.timestampaddPattern( unit, temporalType, intervalType );
 	}
@@ -1640,15 +1631,5 @@ public class DialectDelegateWrapper extends Dialect {
 	@Override
 	public String getRowIdColumnString(String rowId) {
 		return wrapped.getRowIdColumnString( rowId );
-	}
-
-	@Override
-	public boolean useArrayForMultiValuedParameters() {
-		return wrapped.useArrayForMultiValuedParameters();
-	}
-
-	@Override
-	public DmlTargetColumnQualifierSupport getDmlTargetColumnQualifierSupport() {
-		return wrapped.getDmlTargetColumnQualifierSupport();
 	}
 }

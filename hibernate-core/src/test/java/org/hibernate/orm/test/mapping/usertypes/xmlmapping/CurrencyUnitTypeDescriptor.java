697b74a807d74a7dcd0ce1b7fc7d9c9df227317c
Andrea Boriero <andrea@hibernate.org>
2023-10-09 12:44:37 +0200

    HHH-17262 Add test for issue

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/mapping/usertypes/xmlmapping/CurrencyUnitTypeDescriptor.java
@@ -0,0 +1,56 @@
+package org.hibernate.orm.test.mapping.usertypes.xmlmapping;
+
+import org.hibernate.type.descriptor.WrapperOptions;
+import org.hibernate.type.descriptor.java.AbstractClassJavaType;
+
+public class CurrencyUnitTypeDescriptor extends AbstractClassJavaType<CurrencyUnit> {
+
+	public static final CurrencyUnitTypeDescriptor INSTANCE = new CurrencyUnitTypeDescriptor();
+
+	public CurrencyUnitTypeDescriptor() {
+		super( CurrencyUnit.class );
+	}
+
+	@Override
+	public String toString(CurrencyUnit value) {
+		return value.getCurrencyCode();
+	}
+
+	@Override
+	public CurrencyUnit fromString(CharSequence currencyCode) {
+		return new CurrencyUnit() {
+			@Override
+			public String getCurrencyCode() {
+				return currencyCode.toString();
+			}
+
+			@Override
+			public int getNumericCode() {
+				return 0;
+			}
+		};
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public <X> X unwrap(CurrencyUnit currencyUnit, Class<X> type, WrapperOptions options) { // NOSONAR
+		if ( currencyUnit == null ) {
+			return null;
+		}
+		if ( String.class.isAssignableFrom( type ) ) {
+			return (X) currencyUnit.getCurrencyCode();
+		}
+		throw unknownUnwrap( type );
+	}
+
+	@Override
+	public <X> CurrencyUnit wrap(X currencyCode, WrapperOptions options) {
+		if ( currencyCode == null ) {
+			return null;
+		}
+		if ( currencyCode instanceof String ) {
+			return new AccountCurrencyUnit( currencyCode.toString(), 0 );
+		}
+		throw unknownWrap( currencyCode.getClass() );
+	}
+}
---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/mapping/usertypes/xmlmapping/CurrencyUnitUserType.java
@@ -0,0 +1,16 @@
+package org.hibernate.orm.test.mapping.usertypes.xmlmapping;
+
+import java.util.function.BiConsumer;
+
+import org.hibernate.type.descriptor.java.BasicJavaType;
+import org.hibernate.type.descriptor.jdbc.JdbcType;
+import org.hibernate.type.descriptor.jdbc.VarcharJdbcType;
+import org.hibernate.usertype.BaseUserTypeSupport;
+
+public class CurrencyUnitUserType extends BaseUserTypeSupport<CurrencyUnit> {
+
+	@Override
+	protected void resolve(BiConsumer<BasicJavaType<CurrencyUnit>, JdbcType> resolutionConsumer) {
+		resolutionConsumer.accept( CurrencyUnitTypeDescriptor.INSTANCE, VarcharJdbcType.INSTANCE );
+	}
+}

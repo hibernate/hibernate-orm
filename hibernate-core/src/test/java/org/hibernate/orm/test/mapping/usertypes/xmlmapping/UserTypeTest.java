6e630f84e4a26985c13235117d452337eaadc1c1
Andrea Boriero <andrea@hibernate.org>
2023-10-09 12:44:37 +0200

    HHH-17262 Add test for issue

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/mapping/usertypes/xmlmapping/UserTypeTest.java
@@ -0,0 +1,23 @@
+package org.hibernate.orm.test.mapping.usertypes.xmlmapping;
+
+import org.hibernate.testing.orm.junit.EntityManagerFactoryScope;
+import org.hibernate.testing.orm.junit.JiraKey;
+import org.hibernate.testing.orm.junit.Jpa;
+import org.junit.jupiter.api.Test;
+
+@Jpa(
+		xmlMappings = "org/hibernate/orm/test/mapping/usertypes/xmlmapping/entities.xml"
+)
+@JiraKey("HHH-17262")
+public class UserTypeTest {
+
+	@Test
+	public void testTypeAnnotationIsDetected(EntityManagerFactoryScope scope) {
+		scope.inTransaction(
+				entityManager -> {
+					Account account = new Account( "first", new AccountCurrencyUnit( "2", 0 ) );
+					entityManager.persist( account );
+				}
+		);
+	}
+}

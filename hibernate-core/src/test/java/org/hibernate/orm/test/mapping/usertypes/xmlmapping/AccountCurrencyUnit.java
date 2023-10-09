4b8c670be04094cec7125788f4e04dde8b9eff01
Andrea Boriero <andrea@hibernate.org>
2023-10-09 12:44:37 +0200

    HHH-17262 Add test for issue

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/mapping/usertypes/xmlmapping/AccountCurrencyUnit.java
@@ -0,0 +1,25 @@
+package org.hibernate.orm.test.mapping.usertypes.xmlmapping;
+
+public class AccountCurrencyUnit implements CurrencyUnit {
+	private String code;
+	private int numericCode;
+
+	public AccountCurrencyUnit() {
+	}
+
+	public AccountCurrencyUnit(String code, int numericCode) {
+		this.code = code;
+		this.numericCode = numericCode;
+	}
+
+	@Override
+	public String getCurrencyCode() {
+		return null;
+	}
+
+	@Override
+	public int getNumericCode() {
+		return 0;
+	}
+
+}

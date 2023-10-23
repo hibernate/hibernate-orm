0399b52539a361bb0aec47407862a45309503ba3
Steve Ebersole <steve@hibernate.org>
2023-10-23 18:29:23 -0500

    HHH-17345 - Adjust mapping XSD and JAXB
    https://hibernate.atlassian.net/browse/HHH-17345

--- hibernate-core/src/main/java/org/hibernate/boot/jaxb/mapping/spi/JaxbPluralAttribute.java
+++ hibernate-core/src/main/java/org/hibernate/boot/jaxb/mapping/spi/JaxbPluralAttribute.java
@@ -8,6 +8,8 @@ package org.hibernate.boot.jaxb.mapping.spi;
 
 import java.util.List;
 
+import org.hibernate.boot.internal.LimitedCollectionClassification;
+
 import jakarta.persistence.EnumType;
 import jakarta.persistence.TemporalType;
 
@@ -18,27 +20,28 @@ import jakarta.persistence.TemporalType;
  */
 public interface JaxbPluralAttribute extends JaxbFetchableAttribute {
 	JaxbPluralFetchModeImpl getFetchMode();
-
 	void setFetchMode(JaxbPluralFetchModeImpl mode);
 
-	String getOrderBy();
+	JaxbCollectionIdImpl getCollectionId();
+	void setCollectionId(JaxbCollectionIdImpl id);
 
+
+	LimitedCollectionClassification getClassification();
+	void setClassification(LimitedCollectionClassification value);
+
+	String getOrderBy();
 	void setOrderBy(String value);
 
 	JaxbOrderColumnImpl getOrderColumn();
-
 	void setOrderColumn(JaxbOrderColumnImpl value);
 
 	String getSort();
-
 	void setSort(String value);
 
 	JaxbMapKeyImpl getMapKey();
-
 	void setMapKey(JaxbMapKeyImpl value);
 
 	JaxbMapKeyClassImpl getMapKeyClass();
-
 	void setMapKeyClass(JaxbMapKeyClassImpl value);
 
 	TemporalType getMapKeyTemporal();

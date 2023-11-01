7e76dd7786f2571aaccea7f5cd58d05e4f90118b
Steve Ebersole <steve@hibernate.org>
2023-10-31 21:02:26 -0500

    HHH-17377 - Migrate to JPA 3.2
    https://hibernate.atlassian.net/browse/HHH-17377

--- hibernate-core/src/main/java/org/hibernate/query/sqm/tree/domain/SqmTreatedEntityJoin.java
+++ hibernate-core/src/main/java/org/hibernate/query/sqm/tree/domain/SqmTreatedEntityJoin.java
@@ -15,12 +15,12 @@ import org.hibernate.spi.NavigablePath;
 /**
  * @author Steve Ebersole
  */
-public class SqmTreatedEntityJoin<T, S extends T> extends SqmEntityJoin<S> implements SqmTreatedPath<T,S> {
-	private final SqmEntityJoin<T> wrappedPath;
+public class SqmTreatedEntityJoin<L,R,S extends R> extends SqmEntityJoin<L,S> implements SqmTreatedPath<R,S> {
+	private final SqmEntityJoin<L,R> wrappedPath;
 	private final EntityDomainType<S> treatTarget;
 
 	public SqmTreatedEntityJoin(
-			SqmEntityJoin<T> wrappedPath,
+			SqmEntityJoin<L,R> wrappedPath,
 			EntityDomainType<S> treatTarget,
 			String alias) {
 		super(
@@ -39,7 +39,7 @@ public class SqmTreatedEntityJoin<T, S extends T> extends SqmEntityJoin<S> imple
 
 	private SqmTreatedEntityJoin(
 			NavigablePath navigablePath,
-			SqmEntityJoin<T> wrappedPath,
+			SqmEntityJoin<L,R> wrappedPath,
 			EntityDomainType<S> treatTarget,
 			String alias) {
 		super(
@@ -54,12 +54,12 @@ public class SqmTreatedEntityJoin<T, S extends T> extends SqmEntityJoin<S> imple
 	}
 
 	@Override
-	public SqmTreatedEntityJoin<T, S> copy(SqmCopyContext context) {
-		final SqmTreatedEntityJoin<T, S> existing = context.getCopy( this );
+	public SqmTreatedEntityJoin<L,R,S> copy(SqmCopyContext context) {
+		final SqmTreatedEntityJoin<L,R,S> existing = context.getCopy( this );
 		if ( existing != null ) {
 			return existing;
 		}
-		final SqmTreatedEntityJoin<T, S> path = context.registerCopy(
+		final SqmTreatedEntityJoin<L,R,S> path = context.registerCopy(
 				this,
 				new SqmTreatedEntityJoin<>(
 						getNavigablePath(),
@@ -83,7 +83,7 @@ public class SqmTreatedEntityJoin<T, S extends T> extends SqmEntityJoin<S> imple
 	}
 
 	@Override
-	public SqmPath<T> getWrappedPath() {
+	public SqmPath<R> getWrappedPath() {
 		return wrappedPath;
 	}
 

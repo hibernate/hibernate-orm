dac7c9e00b7c3d220b78b78a62535cbcb63368aa
Steve Ebersole <steve@hibernate.org>
2023-11-01 17:37:38 -0500

    HHH-17377 - Migrate to JPA 3.2
    https://hibernate.atlassian.net/browse/HHH-17377
    
    Fixed SQM/JPA Criteria compilation errors.  Just Graph left

--- hibernate-core/src/main/java/org/hibernate/query/sqm/tree/domain/SqmTreatedPath.java
+++ hibernate-core/src/main/java/org/hibernate/query/sqm/tree/domain/SqmTreatedPath.java
@@ -7,13 +7,23 @@
 package org.hibernate.query.sqm.tree.domain;
 
 import org.hibernate.metamodel.model.domain.EntityDomainType;
+import org.hibernate.query.criteria.JpaTreatedPath;
 
 /**
+ * @param <T> The type of the treat source
+ * @param <S> The subtype of {@code <T>} that is the treat "target"
+ *
  * @author Steve Ebersole
  */
-public interface SqmTreatedPath<T, S extends T> extends SqmPathWrapper<T, S> {
+public interface SqmTreatedPath<T, S extends T> extends JpaTreatedPath<T,S>, SqmPathWrapper<T, S> {
 	EntityDomainType<S> getTreatTarget();
 
 	@Override
 	SqmPath<T> getWrappedPath();
+
+	@Override
+	<S1 extends S> SqmTreatedPath<S, S1> treatAs(Class<S1> treatJavaType);
+
+	@Override
+	<S1 extends S> SqmTreatedPath<S, S1> treatAs(EntityDomainType<S1> treatTarget);
 }
--- hibernate-core/src/main/java/org/hibernate/query/sqm/tree/from/SqmJoin.java
+++ hibernate-core/src/main/java/org/hibernate/query/sqm/tree/from/SqmJoin.java
@@ -6,16 +6,25 @@
  */
 package org.hibernate.query.sqm.tree.from;
 
+import jakarta.persistence.criteria.Expression;
 import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
 
 import org.hibernate.metamodel.model.domain.EntityDomainType;
+import org.hibernate.query.criteria.JpaExpression;
+import org.hibernate.query.criteria.JpaJoin;
+import org.hibernate.query.criteria.JpaPredicate;
 import org.hibernate.query.sqm.tree.SqmCopyContext;
 import org.hibernate.query.sqm.tree.SqmJoinType;
+import org.hibernate.query.sqm.tree.domain.SqmTreatedJoin;
+import org.hibernate.query.sqm.tree.predicate.SqmPredicate;
+
+import static org.hibernate.query.sqm.spi.SqmCreationHelper.combinePredicates;
 
 /**
  * @author Steve Ebersole
  */
-public interface SqmJoin<L, R> extends SqmFrom<L, R> {
+public interface SqmJoin<L, R> extends SqmFrom<L, R>, JpaJoin<L,R> {
 	/**
 	 * The type of join - inner, cross, etc
 	 */
@@ -25,6 +34,20 @@ public interface SqmJoin<L, R> extends SqmFrom<L, R> {
 	 * When applicable, whether this join should be included in an implicit select clause
 	 */
 	boolean isImplicitlySelectable();
+
+	/**
+	 * Obtain the join predicate
+	 *
+	 * @return The join predicate
+	 */
+	SqmPredicate getJoinPredicate();
+
+	/**
+	 * Inject the join predicate
+	 *
+	 * @param predicate The join predicate
+	 */
+	void setJoinPredicate(SqmPredicate predicate);
 
 	@Override
 	<X, Y> SqmAttributeJoin<X, Y> join(String attributeName);
@@ -36,14 +59,55 @@ public interface SqmJoin<L, R> extends SqmFrom<L, R> {
 	SqmJoin<L, R> copy(SqmCopyContext context);
 
 	@Override
-	<S extends R> SqmJoin<L, S> treatAs(Class<S> treatAsType);
+	<S extends R> SqmTreatedJoin<L,R,S> treatAs(Class<S> treatAsType);
 
 	@Override
-	<S extends R> SqmJoin<L, S> treatAs(EntityDomainType<S> treatAsType);
+	<S extends R> SqmTreatedJoin<L,R,S> treatAs(EntityDomainType<S> treatAsType);
 
 	@Override
-	<S extends R> SqmJoin<L, S> treatAs(Class<S> treatJavaType, String alias);
+	<S extends R> SqmTreatedJoin<L,R,S> treatAs(Class<S> treatJavaType, String alias);
 
 	@Override
-	<S extends R> SqmJoin<L, S> treatAs(EntityDomainType<S> treatTarget, String alias);
+	<S extends R> SqmTreatedJoin<L,R,S> treatAs(EntityDomainType<S> treatTarget, String alias);
+
+	@Override
+	default SqmPredicate getOn() {
+		return getJoinPredicate();
+	}
+
+	@Override
+	default SqmJoin<L, R> on(JpaExpression<Boolean> restriction) {
+		setJoinPredicate( combinePredicates(
+				getJoinPredicate(),
+				getJoinPredicate().nodeBuilder().wrap( restriction )
+		) );
+		return this;
+	}
+
+	@Override
+	default SqmJoin<L, R> on(Expression<Boolean> restriction) {
+		setJoinPredicate( combinePredicates(
+				getJoinPredicate(),
+				getJoinPredicate().nodeBuilder().wrap( restriction )
+		) );
+		return this;
+	}
+
+	@Override
+	default SqmJoin<L, R> on(JpaPredicate... restrictions) {
+		setJoinPredicate( combinePredicates(
+				getJoinPredicate(),
+				restrictions
+		) );
+		return this;
+	}
+
+	@Override
+	default SqmJoin<L, R> on(Predicate... restrictions) {
+		setJoinPredicate( combinePredicates(
+				getJoinPredicate(),
+				restrictions
+		) );
+		return this;
+	}
 }

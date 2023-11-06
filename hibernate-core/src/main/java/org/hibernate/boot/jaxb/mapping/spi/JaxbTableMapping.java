931c56bdfec02d362c868bc1fe2690e107d3df1c
Steve Ebersole <steve@hibernate.org>
2023-11-06 07:41:02 -0600

    HHH-17377 - Migrate to JPA 3.2
    https://hibernate.atlassian.net/browse/HHH-17377
    
    Latest JPA 3.2 XSD changes - https://github.com/jakartaee/persistence/blob/9cca8e24321037b7fb48d8deee8626f613ce98a3/api/src/main/resources/jakarta/persistence/orm_3_2.xsd

---
+++ hibernate-core/src/main/java/org/hibernate/boot/jaxb/mapping/spi/JaxbTableMapping.java
@@ -0,0 +1,15 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright: Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.boot.jaxb.mapping.spi;
+
+/**
+ * @author Steve Ebersole
+ */
+public interface JaxbTableMapping extends JaxbSchemaAware {
+	String getComment();
+	String getOptions();
+}

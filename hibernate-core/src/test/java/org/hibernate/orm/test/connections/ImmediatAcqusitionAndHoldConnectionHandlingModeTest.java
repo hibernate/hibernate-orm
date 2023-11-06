6483c0c849a0253c2077856369367963bd473edf
Andrea Boriero <andrea@hibernate.org>
2023-11-06 17:10:50 +0100

    HHH-17370 Add test for issue

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/connections/ImmediatAcqusitionAndHoldConnectionHandlingModeTest.java
@@ -0,0 +1,25 @@
+package org.hibernate.orm.test.connections;
+
+import org.hibernate.cfg.AvailableSettings;
+
+import org.hibernate.testing.orm.junit.DomainModel;
+import org.hibernate.testing.orm.junit.Jira;
+import org.hibernate.testing.orm.junit.ServiceRegistry;
+import org.hibernate.testing.orm.junit.SessionFactory;
+import org.hibernate.testing.orm.junit.SessionFactoryScope;
+import org.hibernate.testing.orm.junit.Setting;
+import org.junit.jupiter.api.Test;
+
+@DomainModel
+@SessionFactory
+@ServiceRegistry(
+		settings = @Setting( name = AvailableSettings.CONNECTION_HANDLING, value = "IMMEDIATE_ACQUISITION_AND_HOLD")
+)
+@Jira( "HHH-17370" )
+public class ImmediatAcqusitionAndHoldConnectionHandlingModeTest {
+
+	@Test
+	public void testIt(SessionFactoryScope scope){
+		// this is enough to reproduce the issue
+	}
+}

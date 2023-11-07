6802905b5487d53ecae37f4055fc46d5a0aa84d9
Steve Ebersole <steve@hibernate.org>
2023-11-07 14:49:27 -0600

    HHH-17377 - Migrate to JPA 3.2
    https://hibernate.atlassian.net/browse/HHH-17377
    
    JPA 3.2 B02

--- hibernate-core/src/main/java/org/hibernate/internal/AbstractSharedSessionContract.java
+++ hibernate-core/src/main/java/org/hibernate/internal/AbstractSharedSessionContract.java
@@ -831,6 +831,7 @@ public abstract class AbstractSharedSessionContract implements SharedSessionCont
 		return interpretAndCreateSelectionQuery( hqlString, expectedResultType );
 	}
 
+
 	@Override
 	public <R> SelectionQuery<R> createSelectionQuery(CriteriaQuery<R> criteria) {
 		if ( criteria instanceof CriteriaDefinition ) {
@@ -1447,7 +1448,7 @@ public abstract class AbstractSharedSessionContract implements SharedSessionCont
 		}
 	}
 
-	private <T> QueryImplementor<T> createCriteriaQuery(SqmStatement<T> criteria, Class<T> resultType) {
+	protected <T> QueryImplementor<T> createCriteriaQuery(SqmStatement<T> criteria, Class<T> resultType) {
 		final QuerySqmImpl<T> query = new QuerySqmImpl<>( criteria, resultType, this );
 		applyQuerySettingsAndHints( query );
 		return query;

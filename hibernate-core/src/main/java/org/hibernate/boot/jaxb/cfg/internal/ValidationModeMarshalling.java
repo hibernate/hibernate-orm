bb6f733aa7ce531b5d6c9dfb52d29164883319d8
Steve Ebersole <steve@hibernate.org>
2023-11-07 16:52:47 -0600

    HHH-17377 - Migrate to JPA 3.2
    https://hibernate.atlassian.net/browse/HHH-17377
    
    JPA 3.2 B02

---
+++ hibernate-core/src/main/java/org/hibernate/boot/jaxb/cfg/internal/ValidationModeMarshalling.java
@@ -0,0 +1,31 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html.
+ */
+package org.hibernate.boot.jaxb.cfg.internal;
+
+import org.hibernate.internal.util.StringHelper;
+
+import jakarta.persistence.SharedCacheMode;
+import jakarta.persistence.ValidationMode;
+
+/**
+ * @author Steve Ebersole
+ */
+public class ValidationModeMarshalling {
+	public static ValidationMode fromXml(String name) {
+		if ( StringHelper.isEmpty( name ) ) {
+			return ValidationMode.AUTO;
+		}
+		return ValidationMode.valueOf( name );
+	}
+
+	public static String toXml(ValidationMode validationMode) {
+		if ( validationMode == null ) {
+			return null;
+		}
+		return validationMode.name();
+	}
+}

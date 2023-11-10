7a5219b52af19aef30b6b5af0e310877eed3569f
Christian Beikov <christian.beikov@gmail.com>
2023-11-10 11:39:25 +0100

    HHH-17412 Special case SqmExpression passed as criteria value to help with surprising javac compiler choices

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/query/CriteriaExpressionAsValueTests.java
@@ -0,0 +1,50 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
+package org.hibernate.orm.test.query;
+
+import org.hibernate.query.criteria.HibernateCriteriaBuilder;
+import org.hibernate.query.criteria.JpaCriteriaQuery;
+import org.hibernate.query.criteria.JpaParameterExpression;
+import org.hibernate.query.criteria.JpaRoot;
+
+import org.hibernate.testing.orm.domain.StandardDomainModel;
+import org.hibernate.testing.orm.domain.contacts.Contact;
+import org.hibernate.testing.orm.junit.DomainModel;
+import org.hibernate.testing.orm.junit.JiraKey;
+import org.hibernate.testing.orm.junit.SessionFactory;
+import org.hibernate.testing.orm.junit.SessionFactoryScope;
+import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.Tuple;
+
+/**
+ * @author Christian Beikov
+ */
+@DomainModel(standardModels = StandardDomainModel.CONTACTS)
+@SessionFactory
+@JiraKey("HHH-17412")
+public class CriteriaExpressionAsValueTests {
+
+	@Test
+	public void testParameters(SessionFactoryScope scope) {
+		scope.inTransaction(
+				session -> {
+					final HibernateCriteriaBuilder cb = session.getCriteriaBuilder();
+					final JpaCriteriaQuery<Tuple> cq = cb.createTupleQuery();
+					final JpaRoot<Contact> root = cq.from( Contact.class );
+					cq.multiselect(
+							root.get( "id" ),
+							root.get( "name" )
+					);
+					final JpaParameterExpression<Contact.Gender> parameter = cb.parameter( Contact.Gender.class );
+					cq.where( root.get( "gender" ).equalTo( parameter ) );
+
+					session.createQuery( cq ).setParameter( parameter, Contact.Gender.FEMALE ).getResultList();
+				}
+		);
+	}
+}

551dde50ebd59a2d64088a4aa8fa6427e5063b0a
Steve Ebersole <steve@hibernate.org>
2023-11-18 08:06:54 -0600

    HHH-17377 - Migrate to JPA 3.2
    https://hibernate.atlassian.net/browse/HHH-17377
    
    XJB changes (JAXB "binding model")

---
+++ hibernate-core/src/main/java/org/hibernate/boot/jaxb/mapping/spi/db/JaxbColumnCommon.java
@@ -0,0 +1,55 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html.
+ */
+package org.hibernate.boot.jaxb.mapping.spi.db;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.hibernate.boot.jaxb.mapping.spi.JaxbCheckConstraintImpl;
+
+/**
+ * Composition of the aspects of column definition most commonly exposed in XSD "column types"
+ *
+ * @author Steve Ebersole
+ */
+public interface JaxbColumnCommon
+		extends JaxbColumn, JaxbColumnMutable, JaxbCheckable, JaxbColumnNullable, JaxbColumnUniqueable, JaxbColumnDefinable, JaxbCommentable {
+	@Override
+	default String getTable() {
+		return null;
+	}
+
+	@Override
+	default Boolean isNullable() {
+		return null;
+	}
+
+	@Override
+	default Boolean isInsertable() {
+		return null;
+	}
+
+	@Override
+	default Boolean isUpdatable() {
+		return null;
+	}
+
+	@Override
+	default String getComment() {
+		return null;
+	}
+
+	@Override
+	default Boolean isUnique() {
+		return null;
+	}
+
+	@Override
+	default List<JaxbCheckConstraintImpl> getCheckConstraints() {
+		return Collections.emptyList();
+	}
+}
---
+++ hibernate-core/src/main/java/org/hibernate/boot/jaxb/mapping/spi/db/JaxbColumnSizable.java
@@ -0,0 +1,22 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html.
+ */
+package org.hibernate.boot.jaxb.mapping.spi.db;
+
+/**
+ * @author Steve Ebersole
+ */
+public interface JaxbColumnSizable extends JaxbColumn {
+	Integer getLength();
+
+	default Integer getPrecision() {
+		return null;
+	}
+
+	default Integer getScale() {
+		return null;
+	}
+}

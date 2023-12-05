dc88069c8cbfc727f922aacf9c82575b0fdefb77
Steve Ebersole <steve@hibernate.org>
2023-12-05 11:21:44 -0600

    HHH-17504 - Ongoing JPA 32 work HHH-17350 - Work on hibernate-models, XSD and JAXB HHH-16114 - Improve boot metamodel binding HHH-15996 - Develop an abstraction for Annotation in annotation processing HHH-16012 - Develop an abstraction for domain model Class refs HHH-15997 - Support for dynamic models in orm.xml HHH-15698 - Support for entity-name in mapping.xsd

--- hibernate-core/src/test/java/org/hibernate/orm/test/boot/models/bind/inheritance/JoinedTests.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/boot/models/bind/inheritance/JoinedTests.java
@@ -4,7 +4,7 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
  * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html.
  */
-package org.hibernate.orm.test.boot.models.bind.joined;
+package org.hibernate.orm.test.boot.models.bind.inheritance;
 
 import org.hibernate.mapping.JoinedSubclass;
 import org.hibernate.mapping.RootClass;
@@ -29,7 +29,7 @@ import static org.hibernate.orm.test.boot.models.bind.BindingTestingHelper.check
 /**
  * @author Steve Ebersole
  */
-public class SimpleJoinedTests {
+public class JoinedTests {
 	/**
 	 * Allowing for something like:
 	 *

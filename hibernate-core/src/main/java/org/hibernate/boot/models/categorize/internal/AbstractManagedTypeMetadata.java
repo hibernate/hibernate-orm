afbea3c86ba6f2d053c6e60672e7b45c3bcfa1a0
Steve Ebersole <steve@hibernate.org>
2023-12-07 09:40:04 -0600

    HHH-17504 - Ongoing JPA 32 work HHH-17350 - Work on hibernate-models, XSD and JAXB HHH-16114 - Improve boot metamodel binding HHH-15996 - Develop an abstraction for Annotation in annotation processing HHH-16012 - Develop an abstraction for domain model Class refs HHH-15997 - Support for dynamic models in orm.xml HHH-15698 - Support for entity-name in mapping.xsd

--- hibernate-core/src/main/java/org/hibernate/boot/models/categorize/internal/AbstractManagedTypeMetadata.java
+++ hibernate-core/src/main/java/org/hibernate/boot/models/categorize/internal/AbstractManagedTypeMetadata.java
@@ -7,36 +7,19 @@
 package org.hibernate.boot.models.categorize.internal;
 
 import java.util.Collection;
-import java.util.EnumSet;
 import java.util.List;
 import java.util.Objects;
 
-import org.hibernate.annotations.Any;
-import org.hibernate.annotations.ManyToAny;
 import org.hibernate.boot.model.source.spi.AttributePath;
 import org.hibernate.boot.model.source.spi.AttributeRole;
 import org.hibernate.boot.model.source.spi.NaturalIdMutability;
-import org.hibernate.boot.models.HibernateAnnotations;
-import org.hibernate.boot.models.JpaAnnotations;
-import org.hibernate.boot.models.MultipleAttributeNaturesException;
-import org.hibernate.boot.models.categorize.ModelCategorizationLogging;
 import org.hibernate.boot.models.categorize.spi.AllMemberConsumer;
 import org.hibernate.boot.models.categorize.spi.AttributeMetadata;
 import org.hibernate.boot.models.categorize.spi.ManagedTypeMetadata;
 import org.hibernate.boot.models.categorize.spi.ModelCategorizationContext;
 import org.hibernate.internal.util.IndexedConsumer;
-import org.hibernate.models.spi.AnnotationUsage;
 import org.hibernate.models.spi.ClassDetails;
 import org.hibernate.models.spi.MemberDetails;
-
-import jakarta.persistence.Basic;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Embedded;
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 
 import static org.hibernate.internal.util.collections.CollectionHelper.arrayList;
 
@@ -151,14 +134,14 @@ public abstract class AbstractManagedTypeMetadata implements ManagedTypeMetadata
 	protected List<AttributeMetadata> resolveAttributes(AllMemberConsumer memberConsumer) {
 		final List<MemberDetails> backingMembers = getModelContext()
 				.getPersistentAttributeMemberResolver()
-				.resolveAttributesMembers( classDetails, getAccessType(), memberConsumer, modelContext );
+				.resolveAttributesMembers( classDetails, getAccessType(), memberConsumer );
 
 		final List<AttributeMetadata> attributeList = arrayList( backingMembers.size() );
 
 		for ( MemberDetails backingMember : backingMembers ) {
 			final AttributeMetadata attribute = new AttributeMetadataImpl(
 					backingMember.resolveAttributeName(),
-					determineAttributeNature( backingMember ),
+					CategorizationHelper.determineAttributeNature( backingMember ),
 					backingMember
 			);
 			attributeList.add( attribute );
@@ -167,115 +150,7 @@ public abstract class AbstractManagedTypeMetadata implements ManagedTypeMetadata
 		return attributeList;
 	}
 
-	/**
-	 * Determine the attribute's nature - is it a basic mapping, an embeddable, ...?
-	 *
-	 * Also performs some simple validation around multiple natures being indicated
-	 */
-	private AttributeMetadata.AttributeNature determineAttributeNature(MemberDetails backingMember) {
-		final EnumSet<AttributeMetadata.AttributeNature> natures = EnumSet.noneOf( AttributeMetadata.AttributeNature.class );
-
-		// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-		// first, look for explicit nature annotations
-
-		final AnnotationUsage<Any> any = backingMember.getAnnotationUsage( HibernateAnnotations.ANY );
-		final AnnotationUsage<Basic> basic = backingMember.getAnnotationUsage( JpaAnnotations.BASIC );
-		final AnnotationUsage<ElementCollection> elementCollection = backingMember.getAnnotationUsage( JpaAnnotations.ELEMENT_COLLECTION );
-		final AnnotationUsage<Embedded> embedded = backingMember.getAnnotationUsage( JpaAnnotations.EMBEDDED );
-		final AnnotationUsage<EmbeddedId> embeddedId = backingMember.getAnnotationUsage( JpaAnnotations.EMBEDDED_ID );
-		final AnnotationUsage<ManyToAny> manyToAny = backingMember.getAnnotationUsage( HibernateAnnotations.MANY_TO_ANY );
-		final AnnotationUsage<ManyToMany> manyToMany = backingMember.getAnnotationUsage( JpaAnnotations.MANY_TO_MANY );
-		final AnnotationUsage<ManyToOne> manyToOne = backingMember.getAnnotationUsage( JpaAnnotations.MANY_TO_ONE );
-		final AnnotationUsage<OneToMany> oneToMany = backingMember.getAnnotationUsage( JpaAnnotations.ONE_TO_MANY );
-		final AnnotationUsage<OneToOne> oneToOne = backingMember.getAnnotationUsage( JpaAnnotations.ONE_TO_ONE );
-
-		if ( basic != null ) {
-			natures.add( AttributeMetadata.AttributeNature.BASIC );
-		}
-
-		if ( embedded != null
-				|| embeddedId != null
-				|| ( backingMember.getType() != null && backingMember.getType().getAnnotationUsage( JpaAnnotations.EMBEDDABLE ) != null ) ) {
-			natures.add( AttributeMetadata.AttributeNature.EMBEDDED );
-		}
-
-		if ( any != null ) {
-			natures.add( AttributeMetadata.AttributeNature.ANY );
-		}
-
-		if ( oneToOne != null
-				|| manyToOne != null ) {
-			natures.add( AttributeMetadata.AttributeNature.TO_ONE );
-		}
-
-		final boolean plural = oneToMany != null
-				|| manyToMany != null
-				|| elementCollection != null
-				|| manyToAny != null;
-		if ( plural ) {
-			natures.add( AttributeMetadata.AttributeNature.PLURAL );
-		}
-
-		// look at annotations that imply a nature
-		//		NOTE : these could apply to the element or index of collection, so
-		//		only do these if it is not a collection
-
-		if ( !plural ) {
-			// first implicit basic nature
-			if ( backingMember.getAnnotationUsage( JpaAnnotations.TEMPORAL ) != null
-					|| backingMember.getAnnotationUsage( JpaAnnotations.LOB ) != null
-					|| backingMember.getAnnotationUsage( JpaAnnotations.ENUMERATED ) != null
-					|| backingMember.getAnnotationUsage( JpaAnnotations.CONVERT ) != null
-					|| backingMember.getAnnotationUsage( JpaAnnotations.VERSION ) != null
-					|| backingMember.getAnnotationUsage( HibernateAnnotations.GENERATED ) != null
-					|| backingMember.getAnnotationUsage( HibernateAnnotations.NATIONALIZED ) != null
-					|| backingMember.getAnnotationUsage( HibernateAnnotations.TZ_COLUMN ) != null
-					|| backingMember.getAnnotationUsage( HibernateAnnotations.TZ_STORAGE ) != null
-					|| backingMember.getAnnotationUsage( HibernateAnnotations.TYPE ) != null
-					|| backingMember.getAnnotationUsage( HibernateAnnotations.TENANT_ID ) != null
-					|| backingMember.getAnnotationUsage( HibernateAnnotations.JAVA_TYPE ) != null
-					|| backingMember.getAnnotationUsage( HibernateAnnotations.JDBC_TYPE_CODE ) != null
-					|| backingMember.getAnnotationUsage( HibernateAnnotations.JDBC_TYPE ) != null ) {
-				natures.add( AttributeMetadata.AttributeNature.BASIC );
-			}
-
-			// then embedded
-			if ( backingMember.getAnnotationUsage( HibernateAnnotations.EMBEDDABLE_INSTANTIATOR ) != null
-					|| backingMember.getAnnotationUsage( HibernateAnnotations.COMPOSITE_TYPE ) != null ) {
-				natures.add( AttributeMetadata.AttributeNature.EMBEDDED );
-			}
-
-			// and any
-			if ( backingMember.getAnnotationUsage( HibernateAnnotations.ANY_DISCRIMINATOR ) != null
-					|| backingMember.getAnnotationUsage( HibernateAnnotations.ANY_DISCRIMINATOR_VALUE ) != null
-					|| backingMember.getAnnotationUsage( HibernateAnnotations.ANY_DISCRIMINATOR_VALUES ) != null
-					|| backingMember.getAnnotationUsage( HibernateAnnotations.ANY_KEY_JAVA_TYPE ) != null
-					|| backingMember.getAnnotationUsage( HibernateAnnotations.ANY_KEY_JAVA_CLASS ) != null
-					|| backingMember.getAnnotationUsage( HibernateAnnotations.ANY_KEY_JDBC_TYPE ) != null
-					|| backingMember.getAnnotationUsage( HibernateAnnotations.ANY_KEY_JDBC_TYPE_CODE ) != null ) {
-				natures.add( AttributeMetadata.AttributeNature.ANY );
-			}
-		}
-
-		int size = natures.size();
-		switch ( size ) {
-			case 0: {
-				ModelCategorizationLogging.MODEL_CATEGORIZATION_LOGGER.debugf(
-						"Implicitly interpreting attribute `%s` as BASIC",
-						backingMember.resolveAttributeName()
-				);
-				return AttributeMetadata.AttributeNature.BASIC;
-			}
-			case 1: {
-				return natures.iterator().next();
-			}
-			default: {
-				throw new MultipleAttributeNaturesException( backingMember.resolveAttributeName(), natures );
-			}
-		}
-	}
-
-//	@Override
+	//	@Override
 //	public <A extends Annotation> List<AnnotationUsage<A>> findAnnotations(AnnotationDescriptor<A> type) {
 //		return classDetails.getAnnotations( type );
 //	}
--- hibernate-core/src/main/java/org/hibernate/boot/models/categorize/internal/CategorizationHelper.java
+++ hibernate-core/src/main/java/org/hibernate/boot/models/categorize/internal/CategorizationHelper.java
@@ -6,8 +6,29 @@
  */
 package org.hibernate.boot.models.categorize.internal;
 
+import java.util.EnumSet;
+
+import org.hibernate.annotations.Any;
+import org.hibernate.annotations.ManyToAny;
+import org.hibernate.boot.models.HibernateAnnotations;
 import org.hibernate.boot.models.JpaAnnotations;
+import org.hibernate.boot.models.MultipleAttributeNaturesException;
+import org.hibernate.boot.models.categorize.ModelCategorizationLogging;
+import org.hibernate.boot.models.categorize.spi.AttributeMetadata;
+import org.hibernate.models.spi.AnnotationUsage;
 import org.hibernate.models.spi.ClassDetails;
+import org.hibernate.models.spi.MemberDetails;
+
+import jakarta.persistence.Access;
+import jakarta.persistence.AccessType;
+import jakarta.persistence.Basic;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 
 /**
  * @author Steve Ebersole
@@ -23,5 +44,118 @@ public class CategorizationHelper {
 
 	public static boolean isIdentifiable(ClassDetails classDetails) {
 		return isEntity( classDetails ) || isMappedSuperclass( classDetails );
+	}
+
+	public static AccessType determineAccessType(ClassDetails classDetails, AccessType defaultAccessType) {
+		final AnnotationUsage<Access> annotation = classDetails.getAnnotationUsage( JpaAnnotations.ACCESS );
+		if ( annotation != null ) {
+			return annotation.getAttributeValue( "value" );
+		}
+
+		return defaultAccessType;
+	}
+
+	/**
+	 * Determine the attribute's nature - is it a basic mapping, an embeddable, ...?
+	 * </p>
+	 * Also performs some simple validation around multiple natures being indicated
+	 */
+	public static AttributeMetadata.AttributeNature determineAttributeNature(MemberDetails backingMember) {
+		final EnumSet<AttributeMetadata.AttributeNature> natures = EnumSet.noneOf( AttributeMetadata.AttributeNature.class );
+
+		// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		// first, look for explicit nature annotations
+
+		final AnnotationUsage<Any> any = backingMember.getAnnotationUsage( HibernateAnnotations.ANY );
+		final AnnotationUsage<Basic> basic = backingMember.getAnnotationUsage( JpaAnnotations.BASIC );
+		final AnnotationUsage<ElementCollection> elementCollection = backingMember.getAnnotationUsage( JpaAnnotations.ELEMENT_COLLECTION );
+		final AnnotationUsage<Embedded> embedded = backingMember.getAnnotationUsage( JpaAnnotations.EMBEDDED );
+		final AnnotationUsage<EmbeddedId> embeddedId = backingMember.getAnnotationUsage( JpaAnnotations.EMBEDDED_ID );
+		final AnnotationUsage<ManyToAny> manyToAny = backingMember.getAnnotationUsage( HibernateAnnotations.MANY_TO_ANY );
+		final AnnotationUsage<ManyToMany> manyToMany = backingMember.getAnnotationUsage( JpaAnnotations.MANY_TO_MANY );
+		final AnnotationUsage<ManyToOne> manyToOne = backingMember.getAnnotationUsage( JpaAnnotations.MANY_TO_ONE );
+		final AnnotationUsage<OneToMany> oneToMany = backingMember.getAnnotationUsage( JpaAnnotations.ONE_TO_MANY );
+		final AnnotationUsage<OneToOne> oneToOne = backingMember.getAnnotationUsage( JpaAnnotations.ONE_TO_ONE );
+
+		if ( basic != null ) {
+			natures.add( AttributeMetadata.AttributeNature.BASIC );
+		}
+
+		if ( embedded != null
+				|| embeddedId != null
+				|| ( backingMember.getType() != null && backingMember.getType().getAnnotationUsage( JpaAnnotations.EMBEDDABLE ) != null ) ) {
+			natures.add( AttributeMetadata.AttributeNature.EMBEDDED );
+		}
+
+		if ( any != null ) {
+			natures.add( AttributeMetadata.AttributeNature.ANY );
+		}
+
+		if ( oneToOne != null
+				|| manyToOne != null ) {
+			natures.add( AttributeMetadata.AttributeNature.TO_ONE );
+		}
+
+		final boolean plural = oneToMany != null
+				|| manyToMany != null
+				|| elementCollection != null
+				|| manyToAny != null;
+		if ( plural ) {
+			natures.add( AttributeMetadata.AttributeNature.PLURAL );
+		}
+
+		// look at annotations that imply a nature
+		//		NOTE : these could apply to the element or index of collection, so
+		//		only do these if it is not a collection
+
+		if ( !plural ) {
+			// first implicit basic nature
+			if ( backingMember.getAnnotationUsage( JpaAnnotations.TEMPORAL ) != null
+					|| backingMember.getAnnotationUsage( JpaAnnotations.LOB ) != null
+					|| backingMember.getAnnotationUsage( JpaAnnotations.ENUMERATED ) != null
+					|| backingMember.getAnnotationUsage( JpaAnnotations.CONVERT ) != null
+					|| backingMember.getAnnotationUsage( JpaAnnotations.VERSION ) != null
+					|| backingMember.getAnnotationUsage( HibernateAnnotations.GENERATED ) != null
+					|| backingMember.getAnnotationUsage( HibernateAnnotations.NATIONALIZED ) != null
+					|| backingMember.getAnnotationUsage( HibernateAnnotations.TZ_COLUMN ) != null
+					|| backingMember.getAnnotationUsage( HibernateAnnotations.TZ_STORAGE ) != null
+					|| backingMember.getAnnotationUsage( HibernateAnnotations.TYPE ) != null
+					|| backingMember.getAnnotationUsage( HibernateAnnotations.TENANT_ID ) != null
+					|| backingMember.getAnnotationUsage( HibernateAnnotations.JAVA_TYPE ) != null
+					|| backingMember.getAnnotationUsage( HibernateAnnotations.JDBC_TYPE_CODE ) != null
+					|| backingMember.getAnnotationUsage( HibernateAnnotations.JDBC_TYPE ) != null ) {
+				natures.add( AttributeMetadata.AttributeNature.BASIC );
+			}
+
+			// then embedded
+			if ( backingMember.getAnnotationUsage( HibernateAnnotations.EMBEDDABLE_INSTANTIATOR ) != null
+					|| backingMember.getAnnotationUsage( HibernateAnnotations.COMPOSITE_TYPE ) != null ) {
+				natures.add( AttributeMetadata.AttributeNature.EMBEDDED );
+			}
+
+			// and any
+			if ( backingMember.getAnnotationUsage( HibernateAnnotations.ANY_DISCRIMINATOR ) != null
+					|| backingMember.getAnnotationUsage( HibernateAnnotations.ANY_DISCRIMINATOR_VALUE ) != null
+					|| backingMember.getAnnotationUsage( HibernateAnnotations.ANY_DISCRIMINATOR_VALUES ) != null
+					|| backingMember.getAnnotationUsage( HibernateAnnotations.ANY_KEY_JAVA_TYPE ) != null
+					|| backingMember.getAnnotationUsage( HibernateAnnotations.ANY_KEY_JAVA_CLASS ) != null
+					|| backingMember.getAnnotationUsage( HibernateAnnotations.ANY_KEY_JDBC_TYPE ) != null
+					|| backingMember.getAnnotationUsage( HibernateAnnotations.ANY_KEY_JDBC_TYPE_CODE ) != null ) {
+				natures.add( AttributeMetadata.AttributeNature.ANY );
+			}
+		}
+
+		int size = natures.size();
+		return switch ( size ) {
+			case 0 -> {
+				ModelCategorizationLogging.MODEL_CATEGORIZATION_LOGGER.debugf(
+						"Implicitly interpreting attribute `%s` as BASIC",
+						backingMember.resolveAttributeName()
+				);
+				yield AttributeMetadata.AttributeNature.BASIC;
+			}
+			case 1 -> natures.iterator().next();
+			default -> throw new MultipleAttributeNaturesException( backingMember.resolveAttributeName(), natures );
+		};
 	}
 }

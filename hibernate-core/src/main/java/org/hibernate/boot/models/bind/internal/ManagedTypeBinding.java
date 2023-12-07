c8302a85a197f1511cbab99c91c041340384fb81
Steve Ebersole <steve@hibernate.org>
2023-12-07 09:40:04 -0600

    HHH-17504 - Ongoing JPA 32 work HHH-17350 - Work on hibernate-models, XSD and JAXB HHH-16114 - Improve boot metamodel binding HHH-15996 - Develop an abstraction for Annotation in annotation processing HHH-16012 - Develop an abstraction for domain model Class refs HHH-15997 - Support for dynamic models in orm.xml HHH-15698 - Support for entity-name in mapping.xsd

--- hibernate-core/src/main/java/org/hibernate/boot/models/bind/internal/ManagedTypeBinding.java
+++ hibernate-core/src/main/java/org/hibernate/boot/models/bind/internal/ManagedTypeBinding.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import org.hibernate.boot.models.bind.spi.BindingContext;
 import org.hibernate.boot.models.bind.spi.BindingOptions;
 import org.hibernate.boot.models.bind.spi.BindingState;
-import org.hibernate.boot.models.categorize.spi.ManagedTypeMetadata;
+import org.hibernate.models.spi.ClassDetails;
 
 /**
  * Binding for an {@linkplain jakarta.persistence.metamodel.ManagedType managed type}
@@ -19,19 +19,19 @@ import org.hibernate.boot.models.categorize.spi.ManagedTypeMetadata;
  * @author Steve Ebersole
  */
 public abstract class ManagedTypeBinding extends Binding {
-	protected final ManagedTypeMetadata typeMetadata;
+	protected final ClassDetails classDetails;
 
 	public ManagedTypeBinding(
-			ManagedTypeMetadata typeMetadata,
+			ClassDetails classDetails,
 			BindingOptions bindingOptions,
 			BindingState bindingState,
 			BindingContext bindingContext) {
 		super( bindingOptions, bindingState, bindingContext );
-		this.typeMetadata = typeMetadata;
+		this.classDetails = classDetails;
 	}
 
-	public ManagedTypeMetadata getTypeMetadata() {
-		return typeMetadata;
+	public ClassDetails getClassDetails() {
+		return classDetails;
 	}
 
 	public abstract Map<String, AttributeBinding> getAttributeBindings();

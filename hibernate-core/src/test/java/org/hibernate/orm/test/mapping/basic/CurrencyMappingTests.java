17c5168a9e9455af8c24ac7ed26b3b21bf2d6202
H.Lo <horvoje@gmail.com>
2023-12-25 13:13:43 +0100

    HHH-17574 Bugfix and corresponding test.

--- hibernate-core/src/test/java/org/hibernate/orm/test/mapping/basic/CurrencyMappingTests.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/mapping/basic/CurrencyMappingTests.java
@@ -6,22 +6,36 @@
  */
 package org.hibernate.orm.test.mapping.basic;
 
-import java.sql.Types;
-import java.util.Currency;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+import org.assertj.core.api.Assertions;
+import org.hibernate.HibernateException;
+import org.hibernate.engine.jdbc.LobCreator;
+import org.hibernate.engine.jdbc.NonContextualLobCreator;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.metamodel.mapping.JdbcMapping;
+import org.hibernate.metamodel.mapping.PluralAttributeMapping;
 import org.hibernate.metamodel.mapping.internal.BasicAttributeMapping;
+import org.hibernate.metamodel.mapping.internal.BasicValuedCollectionPart;
 import org.hibernate.metamodel.spi.MappingMetamodelImplementor;
 import org.hibernate.persister.entity.EntityPersister;
-import org.hibernate.type.descriptor.jdbc.spi.JdbcTypeRegistry;
-
 import org.hibernate.testing.orm.junit.DomainModel;
+import org.hibernate.testing.orm.junit.JiraKey;
 import org.hibernate.testing.orm.junit.SessionFactory;
 import org.hibernate.testing.orm.junit.SessionFactoryScope;
+import org.hibernate.type.descriptor.WrapperOptions;
+import org.hibernate.type.descriptor.java.CurrencyJavaType;
+import org.hibernate.type.descriptor.jdbc.spi.JdbcTypeRegistry;
 import org.junit.jupiter.api.Test;
+
+import java.sql.Types;
+import java.util.Currency;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TimeZone;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -31,10 +45,11 @@ import static org.hamcrest.Matchers.equalTo;
  */
 @DomainModel(annotatedClasses = CurrencyMappingTests.EntityWithCurrency.class)
 @SessionFactory
+@JiraKey("HHH-17574")
 public class CurrencyMappingTests {
 
 	@Test
-	public void verifyMappings(SessionFactoryScope scope) {
+	public void verifyMappings(final SessionFactoryScope scope) {
 		final MappingMetamodelImplementor mappingMetamodel = scope.getSessionFactory()
 				.getRuntimeMetamodels()
 				.getMappingMetamodel();
@@ -46,15 +61,126 @@ public class CurrencyMappingTests {
 		assertThat(jdbcMapping.getJavaTypeDescriptor().getJavaTypeClass(), equalTo(Currency.class));
 		assertThat( jdbcMapping.getJdbcType(), equalTo( jdbcRegistry.getDescriptor( Types.VARCHAR)));
 
+		final Currency currency = Currency.getInstance("USD");
+
+		final Set<Currency> currencies = new HashSet<>();
+		currencies.add(Currency.getInstance("CHF"));
+		currencies.add(Currency.getInstance("EUR"));
+
 		scope.inTransaction(
-				(session) -> {
-					session.persist(new EntityWithCurrency(1, Currency.getInstance("USD")));
-				}
+			(session) -> session.persist(new EntityWithCurrency(1, currency, currencies))
 		);
 
 		scope.inTransaction(
-				(session) -> session.find(EntityWithCurrency.class, 1)
+			(session) -> session.find(EntityWithCurrency.class, 1)
 		);
+	}
+
+	@Test
+	public void basicAssertions(final SessionFactoryScope scope) {
+		final SessionFactoryImplementor sessionFactory = scope.getSessionFactory();
+		final JdbcTypeRegistry jdbcTypeRegistry = sessionFactory.getTypeConfiguration().getJdbcTypeRegistry();
+		final EntityPersister entityDescriptor = sessionFactory.getMappingMetamodel().getEntityDescriptor(
+				CurrencyMappingTests.EntityWithCurrency.class );
+		{
+			final BasicAttributeMapping currencyAttribute = (BasicAttributeMapping) entityDescriptor.findAttributeMapping(
+					"currency");
+			Assertions.assertThat(currencyAttribute.getJdbcMapping().getJdbcType())
+					.isEqualTo(jdbcTypeRegistry.getDescriptor(Types.VARCHAR));
+			Assertions.assertThat(currencyAttribute.getJdbcMapping().getJavaTypeDescriptor().getJavaTypeClass()).isEqualTo(
+					Currency.class );
+		}
+		{
+			final PluralAttributeMapping currenciesAttribute = (PluralAttributeMapping) entityDescriptor.findAttributeMapping(
+					"currencies" );
+			final BasicValuedCollectionPart elementDescriptor = (BasicValuedCollectionPart) currenciesAttribute.getElementDescriptor();
+			Assertions.assertThat( elementDescriptor.getJdbcMapping().getJdbcType() )
+					.isEqualTo( jdbcTypeRegistry.getDescriptor( Types.VARCHAR ) );
+			Assertions.assertThat( elementDescriptor.getJdbcMapping().getJavaTypeDescriptor().getJavaTypeClass() ).isEqualTo(
+					Currency.class );
+		}
+	}
+
+	@Test
+	public void testCurrencyJavaTypeWrapNoOptionsPass(final SessionFactoryScope scope) {
+		final CurrencyJavaType currencyJavaType = new CurrencyJavaType();
+		{
+			final Currency usingNull = currencyJavaType.wrap(null, null);
+			Assertions.assertThat(usingNull).isNull();
+		}
+		{
+			final Currency usingString = currencyJavaType.wrap("CHF", null);
+			Assertions.assertThat(usingString).isNotNull();
+		}
+		{
+			final Currency usingCurrency = currencyJavaType.wrap(Currency.getInstance("CHF"), null);
+			Assertions.assertThat(usingCurrency).isNotNull();
+		}
+	}
+
+	@Test
+	public void testCurrencyJavaTypeWrapNoOptionsFail(final SessionFactoryScope scope) {
+		final CurrencyJavaType currencyJavaType = new CurrencyJavaType();
+		{
+			final String usingEmptyString = "";
+			Assertions.assertThatThrownBy(() ->
+				currencyJavaType.wrap(usingEmptyString, null)
+			).isInstanceOf(IllegalArgumentException.class);
+		}
+		{
+			final Integer usingInteger = Integer.valueOf(269);
+			Assertions.assertThatThrownBy(() ->
+				currencyJavaType.wrap(usingInteger, null)
+			).isInstanceOf(HibernateException.class);
+		}
+		{
+			final CurrencyJavaType usingSelf = new CurrencyJavaType();
+			Assertions.assertThatThrownBy(() ->
+				currencyJavaType.wrap(usingSelf, null)
+			).isInstanceOf(HibernateException.class);
+		}
+	}
+
+	@Test
+	public void testCurrencyJavaTypeWrapWithOptionsPass(final SessionFactoryScope scope) {
+		final CurrencyJavaType currencyJavaType = new CurrencyJavaType();
+		final MockWrapperOptions options = new MockWrapperOptions(false);
+		{
+			final Currency usingNull = currencyJavaType.wrap(null, options);
+			Assertions.assertThat(usingNull).isNull();
+		}
+		{
+			final Currency usingString = currencyJavaType.wrap("CHF", options);
+			Assertions.assertThat(usingString).isNotNull();
+		}
+		{
+			final Currency usingCurrency = currencyJavaType.wrap(Currency.getInstance("CHF"), options);
+			Assertions.assertThat(usingCurrency).isNotNull();
+		}
+	}
+
+	@Test
+	public void testCurrencyJavaTypeWrapWithOptionsFail(final SessionFactoryScope scope) {
+		final CurrencyJavaType currencyJavaType = new CurrencyJavaType();
+		final MockWrapperOptions options = new MockWrapperOptions(false);
+		{
+			final String usingEmptyString = "";
+			Assertions.assertThatThrownBy(() ->
+				currencyJavaType.wrap(usingEmptyString, options)
+			).isInstanceOf(IllegalArgumentException.class);
+		}
+		{
+			final Integer usingInteger = Integer.valueOf(269);
+			Assertions.assertThatThrownBy(() ->
+				currencyJavaType.wrap(usingInteger, options)
+			).isInstanceOf(HibernateException.class);
+		}
+		{
+			final CurrencyJavaType usingSelf = new CurrencyJavaType();
+			Assertions.assertThatThrownBy(() ->
+				currencyJavaType.wrap(usingSelf, options)
+			).isInstanceOf(HibernateException.class);
+		}
 	}
 
 	@Entity(name = "EntityWithCurrency")
@@ -68,12 +194,56 @@ public class CurrencyMappingTests {
 		private Currency currency;
 		//end::basic-Currency-example[]
 
-		public EntityWithCurrency() {
+		@ElementCollection
+		private Set<Currency> currencies;
+
+        public EntityWithCurrency() {
+			//
 		}
 
-		public EntityWithCurrency(Integer id, Currency currency) {
-			this.id = id;
-			this.currency = currency;
+		public EntityWithCurrency(final Integer id, final Currency currency, final Set<Currency> currencies) {
+			this.id         = id;
+			this.currency   = currency;
+			this.currencies = currencies;
+        }
+	}
+
+	private static class MockWrapperOptions implements WrapperOptions {
+		private final boolean useStreamForLobBinding;
+
+		public MockWrapperOptions(final boolean useStreamForLobBinding) {
+			this.useStreamForLobBinding = useStreamForLobBinding;
+		}
+
+		@Override
+		public SharedSessionContractImplementor getSession() {
+			return null;
+		}
+
+		@Override
+		public SessionFactoryImplementor getSessionFactory() {
+			return null;
+		}
+
+		@Override
+		public boolean useStreamForLobBinding() {
+			return useStreamForLobBinding;
+		}
+
+		@Override
+		public int getPreferredSqlTypeCodeForBoolean() {
+			return Types.BOOLEAN;
+		}
+
+		@Override
+		public LobCreator getLobCreator() {
+			return NonContextualLobCreator.INSTANCE;
+		}
+
+		@Override
+		public TimeZone getJdbcTimeZone() {
+			return null;
 		}
 	}
+
 }

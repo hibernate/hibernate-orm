8a4c945a4d24a66dfd1f15f1b64e501fa7379a04
marko-bekhta <marko.prykladna@gmail.com>
2024-01-04 15:58:13 +0100

    HHH-17619 Add the multitenancy filter in a stateless session

--- hibernate-core/src/main/java/org/hibernate/internal/StatelessSessionImpl.java
+++ hibernate-core/src/main/java/org/hibernate/internal/StatelessSessionImpl.java
@@ -80,6 +80,7 @@ public class StatelessSessionImpl extends AbstractSharedSessionContract implemen
 		connectionProvided = options.getConnection() != null;
 		temporaryPersistenceContext = new StatefulPersistenceContext( this );
 		influencers = new LoadQueryInfluencers( getFactory() );
+		setUpMultitenancy( factory, influencers );
 	}
 
 	@Override

24576ce5e569113777c5d4d2c5b7c77038992d53
Koen Aers <koen.aers@gmail.com>
2024-01-05 11:25:47 +0100

    HBX-2675: Update version identifier to 6.4.1.Final
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- tooling/hibernate-reveng/src/main/java/org/hibernate/tool/api/version/Version.java
+++ tooling/hibernate-reveng/src/main/java/org/hibernate/tool/api/version/Version.java
@@ -2,6 +2,6 @@ package org.hibernate.tool.api.version;
 
 public interface Version {
 	
-	final static String CURRENT_VERSION = "6.4.1-SNAPSHOT";
+	final static String CURRENT_VERSION = "6.4.1.Final";
 
 }

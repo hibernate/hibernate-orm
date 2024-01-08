a7aafff515d47708462654edfa841800a734a0f3
beng9re <kbh0581@gmail.com>
2024-01-09 01:15:45 +0900

    HHH-17622 - refactor add a parameter generic to the setQuerySpaces

--- hibernate-core/src/main/java/org/hibernate/event/spi/AutoFlushEvent.java
+++ hibernate-core/src/main/java/org/hibernate/event/spi/AutoFlushEvent.java
@@ -26,7 +26,7 @@ public class AutoFlushEvent extends FlushEvent {
 		return querySpaces;
 	}
 
-	public void setQuerySpaces(Set querySpaces) {
+	public void setQuerySpaces(Set<String> querySpaces) {
 		this.querySpaces = querySpaces;
 	}
 

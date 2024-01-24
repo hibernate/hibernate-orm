5f930818859721914f9eb16722f43755ed1d93d8
Gregorio Palamà <gregoriopalama@gmail.com>
2024-01-24 17:21:28 +0100

    HHH-14968 - fixed the check for resolver set to null

--- hibernate-core/src/main/java/org/hibernate/sql/ast/tree/predicate/FilterPredicate.java
+++ hibernate-core/src/main/java/org/hibernate/sql/ast/tree/predicate/FilterPredicate.java
@@ -167,7 +167,7 @@ public class FilterPredicate implements Predicate {
 			}
 
 			final Supplier filterParamResolver = filter.getParameterResolver( paramName );
-			return filterParamResolver.get() == null ? null : filterParamResolver.get();
+			return filterParamResolver == null ? null : filterParamResolver.get();
 		}
 	}
 }

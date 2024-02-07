5e6869c23aa3dcbe4ee84ba32955bc5100cba104
Marco Belladelli <marco@hibernate.org>
2024-02-07 10:40:06 +0100

    HHH-17695 Skip rendering null nested JSON aggregate embeddables

--- hibernate-core/src/main/java/org/hibernate/dialect/JsonHelper.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/JsonHelper.java
@@ -70,13 +70,13 @@ public class JsonHelper {
 				toString( attributeMapping.getMappedType(), values[i], options, appender );
 			}
 			else if ( attributeMapping instanceof EmbeddedAttributeMapping ) {
+				if ( values[i] == null ) {
+					// Skipping the update of the separator is on purpose
+					continue;
+				}
 				final EmbeddableMappingType mappingType = (EmbeddableMappingType) attributeMapping.getMappedType();
 				final SelectableMapping aggregateMapping = mappingType.getAggregateMapping();
 				if ( aggregateMapping == null ) {
-					if ( values[i] == null ) {
-						// Skipping the update of the separator is on purpose
-						continue;
-					}
 					toString(
 							mappingType,
 							options,

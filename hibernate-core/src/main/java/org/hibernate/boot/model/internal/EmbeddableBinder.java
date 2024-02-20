d7e5b3dcd21a7054ed1985717b94b53908eb9a65
Gavin King <gavin@hibernate.org>
2024-02-20 13:27:34 +0100

    fix spelling of method name

--- hibernate-core/src/main/java/org/hibernate/boot/model/internal/EmbeddableBinder.java
+++ hibernate-core/src/main/java/org/hibernate/boot/model/internal/EmbeddableBinder.java
@@ -367,7 +367,7 @@ public class EmbeddableBinder {
 		if ( baseClassElements != null
 				//useful to avoid breaking pre JPA 2 mappings
 				&& !hasAnnotationsOnIdClass( annotatedClass ) ) {
-			processIdClassElememts( propertyHolder, baseInferredData, classElements, baseClassElements );
+			processIdClassElements( propertyHolder, baseInferredData, classElements, baseClassElements );
 		}
 		for ( PropertyData propertyAnnotatedElement : classElements ) {
 			processElementAnnotations(
@@ -555,7 +555,7 @@ public class EmbeddableBinder {
 		}
 	}
 
-	private static void processIdClassElememts(
+	private static void processIdClassElements(
 			PropertyHolder propertyHolder,
 			PropertyData baseInferredData,
 			List<PropertyData> classElements,

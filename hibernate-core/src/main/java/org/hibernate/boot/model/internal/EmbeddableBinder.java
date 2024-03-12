49255d731794804a2ac7d03a542a4beed46d7bad
Marco Belladelli <marco@hibernate.org>
2024-03-12 13:05:55 +0100

    HHH-17460 - Ongoing JPA 32 work
    Work on generics and type resolution

--- hibernate-core/src/main/java/org/hibernate/boot/model/internal/EmbeddableBinder.java
+++ hibernate-core/src/main/java/org/hibernate/boot/model/internal/EmbeddableBinder.java
@@ -460,8 +460,11 @@ public class EmbeddableBinder {
 		ClassDetails superClass = annotatedClass.determineRawClass().getSuperClass();
 		while ( isValidSuperclass( superClass, isIdClass ) ) {
 			//FIXME: proper support of type variables incl var resolved at upper levels
-			final PropertyContainer superContainer =
-					new PropertyContainer( superClass, annotatedClass.determineRawClass(), propertyAccessor );
+			final PropertyContainer superContainer = new PropertyContainer(
+					superClass,
+					annotatedClass,
+					propertyAccessor
+			);
 			addElementsOfClass( classElements, superContainer, context );
 			superClass = superClass.getSuperClass();
 		}
@@ -491,7 +494,7 @@ public class EmbeddableBinder {
 			TypeDetails baseReturnedClassOrElement = baseInferredData.getClassOrElementType();
 			while ( !Object.class.getName().equals( baseReturnedClassOrElement.getName() ) ) {
 				final PropertyContainer container = new PropertyContainer(
-						baseReturnedClassOrElement,
+						baseReturnedClassOrElement.determineRawClass(),
 						annotatedClass,
 						propertyAccessor
 				);

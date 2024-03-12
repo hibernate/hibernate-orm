2f1134cbaf6fbd6efc9cc81bbfbed14bd3ba7930
Andrea Boriero <andrea@hibernate.org>
2024-03-12 17:26:41 +0100

    HHH-17504 - Ongoing JPA 32 work HHH-17350 - Work on hibernate-models, XSD and JAXB HHH-16114 - Improve boot metamodel binding HHH-15996 - Develop an abstraction for Annotation in annotation processing HHH-16012 - Develop an abstraction for domain model Class refs HHH-15997 - Support for dynamic models in orm.xml HHH-15698 - Support for entity-name in mapping.xsd

--- hibernate-core/src/main/java/org/hibernate/boot/model/internal/ColumnsBuilder.java
+++ hibernate-core/src/main/java/org/hibernate/boot/model/internal/ColumnsBuilder.java
@@ -316,7 +316,7 @@ class ColumnsBuilder {
 	 */
 	AnnotatedColumns overrideColumnFromMapperOrMapsIdProperty(boolean isId) {
 		final PropertyData override =
-				getPropertyOverriddenByMapperOrMapsId( isId, propertyHolder, property.getName(), buildingContext );
+				getPropertyOverriddenByMapperOrMapsId( isId, propertyHolder, property.resolveAttributeName(), buildingContext );
 		if ( override != null ) {
 			final AnnotatedJoinColumns joinColumns = buildExplicitJoinColumns( override.getAttributeMember(), override );
 			return joinColumns == null

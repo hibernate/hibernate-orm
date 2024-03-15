8ae0b9eaa52a54628c4ab0ceaaf53ba422534347
Steve Ebersole <steve@hibernate.org>
2024-03-15 11:53:03 -0500

    HHH-17460 - Ongoing JPA 32 work

--- hibernate-core/src/main/java/org/hibernate/boot/model/internal/CollectionBinder.java
+++ hibernate-core/src/main/java/org/hibernate/boot/model/internal/CollectionBinder.java
@@ -1345,8 +1345,9 @@ public abstract class CollectionBinder {
 					sqlInsert.getBoolean( "callable" ),
 					fromResultCheckStyle( sqlInsert.getEnum( "check" ) )
 			);
-			if ( sqlInsert.verify() != Expectation.class ) {
-				collection.setInsertExpectation( getDefaultSupplier( sqlInsert.verify() ) );
+			final ClassDetails verifier = sqlInsert.getClassDetails( "verify" );
+			if ( !verifier.getName().equals( Expectation.class.getName() ) ) {
+				collection.setInsertExpectation( getDefaultSupplier( verifier.toJavaClass() ) );
 			}
 		}
 
@@ -1357,8 +1358,9 @@ public abstract class CollectionBinder {
 					sqlUpdate.getBoolean( "callable" ),
 					fromResultCheckStyle( sqlUpdate.getEnum( "check" ) )
 			);
-			if ( sqlUpdate.verify() != Expectation.class ) {
-				collection.setUpdateExpectation( getDefaultSupplier( sqlUpdate.verify() ) );
+			final ClassDetails verifier = sqlUpdate.getClassDetails( "verify" );
+			if ( !verifier.getName().equals( Expectation.class.getName() ) ) {
+				collection.setUpdateExpectation( getDefaultSupplier( verifier.toJavaClass() ) );
 			}
 		}
 
@@ -1369,8 +1371,9 @@ public abstract class CollectionBinder {
 					sqlDelete.getBoolean( "callable" ),
 					fromResultCheckStyle( sqlDelete.getEnum( "check" ) )
 			);
-			if ( sqlDelete.verify() != Expectation.class ) {
-				collection.setDeleteExpectation( getDefaultSupplier( sqlDelete.verify() ) );
+			final ClassDetails verifier = sqlDelete.getClassDetails( "verify" );
+			if ( !verifier.getName().equals( Expectation.class.getName() ) ) {
+				collection.setDeleteExpectation( getDefaultSupplier( verifier.toJavaClass() ) );
 			}
 		}
 
@@ -1381,8 +1384,9 @@ public abstract class CollectionBinder {
 					sqlDeleteAll.getBoolean( "callable" ),
 					fromResultCheckStyle( sqlDeleteAll.getEnum( "check" ) )
 			);
-			if ( sqlDeleteAll.verify() != Expectation.class ) {
-				collection.setDeleteAllExpectation( getDefaultSupplier( sqlDeleteAll.verify() ) );
+			final ClassDetails verifier = sqlDeleteAll.getClassDetails( "verify" );
+			if ( !verifier.getName().equals( Expectation.class.getName() ) ) {
+				collection.setDeleteAllExpectation( getDefaultSupplier( verifier.toJavaClass() ) );
 			}
 		}
 

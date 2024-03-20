0d0f8754cacf4adc83907d08cd9424b03a2872ab
Gavin King <gavin@hibernate.org>
2024-03-20 20:26:08 +0100

    fix and test for KeyedResultList with EntityManager
    missing an unwrap()
    also fix up some whitespace

---
+++ tooling/metamodel-generator/src/test/java/org/hibernate/processor/test/keypage/Queries.java
@@ -0,0 +1,17 @@
+package org.hibernate.processor.test.keypage;
+
+import org.hibernate.annotations.processing.Find;
+import org.hibernate.annotations.processing.HQL;
+import org.hibernate.annotations.processing.Pattern;
+import org.hibernate.query.KeyedPage;
+import org.hibernate.query.KeyedResultList;
+
+import java.time.LocalDate;
+
+public interface Queries {
+	@HQL("where publicationDate > :minDate")
+	KeyedResultList<Book> booksFromDate(LocalDate minDate, KeyedPage<Book> page);
+
+	@Find
+	KeyedResultList<Book> booksWithTitleLike(@Pattern String title, KeyedPage<Book> page);
+}

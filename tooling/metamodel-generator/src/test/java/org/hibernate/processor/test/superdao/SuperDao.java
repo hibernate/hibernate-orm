9305610a32dbe9ff8f5d591637f96497342b18f0
Gavin King <gavin@hibernate.org>
2024-03-20 22:49:55 +0100

    HHH-17873 initial impl of repository inheritance
    needed for Jakarta Data, and useful
    limited to single inheritance for now

---
+++ tooling/metamodel-generator/src/test/java/org/hibernate/processor/test/superdao/SuperDao.java
@@ -0,0 +1,20 @@
+package org.hibernate.processor.test.superdao;
+
+import jakarta.persistence.EntityManager;
+import org.hibernate.annotations.processing.Find;
+import org.hibernate.annotations.processing.HQL;
+import org.hibernate.annotations.processing.Pattern;
+
+import java.util.List;
+
+public interface SuperDao {
+
+    EntityManager em();
+
+
+    @Find
+    List<Book> books1(@Pattern String title);
+
+    @HQL("where title like :title")
+    List<Book> books2(String title);
+}

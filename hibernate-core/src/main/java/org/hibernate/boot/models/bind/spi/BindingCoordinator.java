82c640c0d84c1d69a65638cae43492324933478c
Steve Ebersole <steve@hibernate.org>
2024-03-21 15:19:28 -0500

    HHH-17460 - Ongoing JPA 32 work

--- hibernate-core/src/main/java/org/hibernate/boot/models/bind/spi/BindingCoordinator.java
+++ hibernate-core/src/main/java/org/hibernate/boot/models/bind/spi/BindingCoordinator.java
@@ -1,8 +1,8 @@
 /*
  * Hibernate, Relational Persistence for Idiomatic Java
  *
- * SPDX-License-Identifier: Apache-2.0
- * Copyright: Red Hat Inc. and Hibernate Authors
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html.
  */
 package org.hibernate.boot.models.bind.spi;
 

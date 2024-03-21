1922b015da85aedeefe4666e59d8226325a6cac1
Steve Ebersole <steve@hibernate.org>
2024-03-20 20:59:53 -0500

    HHH-17460 - Ongoing JPA 32 work
    Support for auto-enabled filters (HHH-14968)

--- hibernate-core/src/main/java/org/hibernate/boot/models/categorize/spi/FilterDefRegistration.java
+++ hibernate-core/src/main/java/org/hibernate/boot/models/categorize/spi/FilterDefRegistration.java
@@ -6,29 +6,47 @@
  */
 package org.hibernate.boot.models.categorize.spi;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Supplier;
 
+import org.hibernate.boot.model.internal.FilterDefBinder;
+import org.hibernate.boot.spi.MetadataBuildingContext;
+import org.hibernate.engine.spi.FilterDefinition;
+import org.hibernate.internal.util.collections.CollectionHelper;
+import org.hibernate.metamodel.mapping.JdbcMapping;
 import org.hibernate.models.spi.ClassDetails;
+import org.hibernate.resource.beans.spi.ManagedBean;
+import org.hibernate.resource.beans.spi.ManagedBeanRegistry;
 
 /**
  * Global registration of a filter definition
  *
  * @see org.hibernate.annotations.FilterDef
- * @see org.hibernate.boot.jaxb.mapping.JaxbFilterDef
+ * @see org.hibernate.boot.jaxb.mapping.spi.JaxbFilterDefImpl
  *
  * @author Marco Belladelli
  */
 public class FilterDefRegistration {
 	private final String name;
-
 	private final String defaultCondition;
+	private final boolean autoEnabled;
 
-	private final Map<String, ClassDetails> parameters;
+	private final Map<String, ClassDetails> parameterTypes;
+	private final Map<String, ClassDetails> parameterResolvers;
 
-	public FilterDefRegistration(String name, String defaultCondition, Map<String, ClassDetails> parameters) {
+	public FilterDefRegistration(
+			String name,
+			String defaultCondition,
+			boolean autoEnabled,
+			Map<String, ClassDetails> parameterTypes,
+			Map<String, ClassDetails> parameterResolvers) {
 		this.name = name;
 		this.defaultCondition = defaultCondition;
-		this.parameters = parameters;
+		this.autoEnabled = autoEnabled;
+		this.parameterTypes = parameterTypes;
+		this.parameterResolvers = parameterResolvers;
 	}
 
 	public String getName() {
@@ -39,7 +57,55 @@ public class FilterDefRegistration {
 		return defaultCondition;
 	}
 
-	public Map<String, ClassDetails> getParameters() {
-		return parameters;
+	public boolean isAutoEnabled() {
+		return autoEnabled;
+	}
+
+	public Map<String, ClassDetails> getParameterTypes() {
+		return parameterTypes;
+	}
+
+	public Map<String, ClassDetails> getParameterResolvers() {
+		return parameterResolvers;
+	}
+
+	public FilterDefinition toFilterDefinition(MetadataBuildingContext buildingContext) {
+		final ManagedBeanRegistry beanRegistry = buildingContext
+				.getBootstrapContext()
+				.getServiceRegistry()
+				.getService( ManagedBeanRegistry.class );
+
+		final Map<String, JdbcMapping> parameterJdbcMappings;
+		if ( CollectionHelper.isEmpty( parameterTypes ) ) {
+			parameterJdbcMappings = Collections.emptyMap();
+		}
+		else {
+			parameterJdbcMappings = new HashMap<>();
+			parameterTypes.forEach( (key,value) -> {
+				parameterJdbcMappings.put(
+						key,
+						FilterDefBinder.resolveFilterParamType( value.toJavaClass(), buildingContext )
+				);
+			} );
+		}
+
+		final Map<String, ManagedBean<? extends Supplier>> parameterResolvers;
+		if ( CollectionHelper.isEmpty( this.parameterResolvers ) ) {
+			parameterResolvers = Collections.emptyMap();
+		}
+		else {
+			parameterResolvers = new HashMap<>();
+			this.parameterResolvers.forEach( (key,value) -> {
+				parameterResolvers.put( key, beanRegistry.getBean( value.toJavaClass() ) );
+			} );
+		}
+
+		return new FilterDefinition(
+				getName(),
+				getDefaultCondition(),
+				isAutoEnabled(),
+				parameterJdbcMappings,
+				parameterResolvers
+		);
 	}
 }

b862d574ec046ff4c9e9cc0f50b2f5811e74b56a
Vincent Bouthinon <v-bouthinon@efluid.fr>
2024-03-21 09:56:49 +0100

    HHH-17871 Error if defining a ManyToAny in the child classes which references a @MappedSuperclass

--- hibernate-core/src/main/java/org/hibernate/mapping/Any.java
+++ hibernate-core/src/main/java/org/hibernate/mapping/Any.java
@@ -236,10 +236,14 @@ public class Any extends SimpleValue {
 
 	public boolean isSame(Any other) {
 		return super.isSame( other )
-				&& Objects.equals( keyMapping.getTypeName(), other.keyMapping.getTypeName() )
-				&& Objects.equals( metaMapping.getTypeName(), other.keyMapping.getTypeName() )
+				&& Objects.equals( getTypeNameOrNull( keyMapping ), getTypeNameOrNull( other.keyMapping ) )
+				&& Objects.equals( getTypeNameOrNull( metaMapping ), getTypeNameOrNull( other.metaMapping ) )
 				&& Objects.equals( metaValueToEntityNameMap, other.metaValueToEntityNameMap )
 				&& lazy == other.lazy;
+	}
+
+	private String getTypeNameOrNull(SimpleValue simpleValue) {
+		return simpleValue != null ? simpleValue.getTypeName() : null;
 	}
 
 	public boolean isValid(Mapping mapping) throws MappingException {

ffafe0e82344496a24b29f42aa9c246a3d17bcde
Andrea Boriero <andrea@hibernate.org>
2024-03-23 11:23:13 +0100

    HHH-17460 - Ongoing JPA 32 work

--- hibernate-core/src/main/java/org/hibernate/boot/spi/InFlightMetadataCollector.java
+++ hibernate-core/src/main/java/org/hibernate/boot/spi/InFlightMetadataCollector.java
@@ -248,6 +248,8 @@ public interface InFlightMetadataCollector extends MetadataImplementor {
 
 	void addIdentifierGenerator(IdentifierGeneratorDefinition generatorDefinition);
 
+	void addPersistenceUnitMetadata(PersistenceUnitMetadata persistenceUnitMetadata);
+
 	/**
 	 * Obtain the {@link ConverterRegistry} which may be
 	 * used to register {@link AttributeConverter}s.

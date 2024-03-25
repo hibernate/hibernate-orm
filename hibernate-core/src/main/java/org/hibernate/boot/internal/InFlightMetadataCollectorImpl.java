84633de9b41c06c15e576569f799f4737da01fa2
Steve Ebersole <steve@hibernate.org>
2024-03-25 17:49:25 -0500

    HHH-17460 - Ongoing JPA 32 work
    - mapping defaults & persistence unit defaults

--- hibernate-core/src/main/java/org/hibernate/boot/internal/InFlightMetadataCollectorImpl.java
+++ hibernate-core/src/main/java/org/hibernate/boot/internal/InFlightMetadataCollectorImpl.java
@@ -145,7 +145,7 @@ public class InFlightMetadataCollectorImpl implements InFlightMetadataCollector,
 	private final MetadataBuildingOptions options;
 
 	private final GlobalRegistrations globalRegistrations;
-	private PersistenceUnitMetadata persistenceUnitMetadata;
+	private final PersistenceUnitMetadata persistenceUnitMetadata;
 
 	private final AttributeConverterManager attributeConverterManager = new AttributeConverterManager();
 
@@ -204,6 +204,7 @@ public class InFlightMetadataCollectorImpl implements InFlightMetadataCollector,
 		this.uuid = UUID.randomUUID();
 
 		this.globalRegistrations = new GlobalRegistrationsImpl( sourceModelBuildingContext );
+		this.persistenceUnitMetadata = new PersistenceUnitMetadataImpl();
 
 		for ( Map.Entry<String, SqmFunctionDescriptor> sqlFunctionEntry : bootstrapContext.getSqlFunctions().entrySet() ) {
 			if ( sqlFunctionMap == null ) {
@@ -726,11 +727,6 @@ public class InFlightMetadataCollectorImpl implements InFlightMetadataCollector,
 				log.duplicateGeneratorName( old.getName() );
 			}
 		}
-	}
-
-	@Override
-	public void addPersistenceUnitMetadata(PersistenceUnitMetadata persistenceUnitMetadata) {
-		this.persistenceUnitMetadata = persistenceUnitMetadata;
 	}
 
 	@Override

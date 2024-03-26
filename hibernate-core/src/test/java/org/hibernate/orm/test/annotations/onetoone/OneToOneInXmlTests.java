b8096002ead7610225fe7f33258489c7aaddfa96
Steve Ebersole <steve@hibernate.org>
2024-03-26 17:19:18 -0500

    HHH-17460 - Ongoing JPA 32 work

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/annotations/onetoone/OneToOneInXmlTests.java
@@ -0,0 +1,51 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html.
+ */
+package org.hibernate.orm.test.annotations.onetoone;
+
+import java.util.Iterator;
+
+import org.hibernate.mapping.Column;
+import org.hibernate.mapping.PersistentClass;
+import org.hibernate.mapping.Table;
+
+import org.hibernate.testing.orm.junit.DomainModel;
+import org.hibernate.testing.orm.junit.DomainModelScope;
+import org.hibernate.testing.orm.junit.JiraKey;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+/**
+ * @author Steve Ebersole
+ */
+public class OneToOneInXmlTests {
+
+	@Test
+	@JiraKey( "HHH-4606" )
+	@DomainModel( xmlMappings = "org/hibernate/orm/test/annotations/onetoone/orm.xml" )
+	@SuppressWarnings("JUnitMalformedDeclaration")
+	public void testJoinColumnConfiguredInXml(DomainModelScope scope) {
+		final PersistentClass pc = scope.getDomainModel().getEntityBinding( Son.class.getName() );
+		Table table = pc.getJoins().get( 0 ).getTable();
+		Iterator<Column> columnItr = table.getColumns().iterator();
+		boolean fooFound = false;
+		boolean barFound = false;
+		while ( columnItr.hasNext() ) {
+			Column column = columnItr.next();
+			if ( column.getName().equals( "foo" ) ) {
+				fooFound = true;
+			}
+			if ( column.getName().equals( "bar" ) ) {
+				barFound = true;
+			}
+		}
+
+		if ( !(fooFound && barFound) ) {
+			fail( "The mapping defines join columns which could not be found in the metadata." );
+		}
+	}
+}

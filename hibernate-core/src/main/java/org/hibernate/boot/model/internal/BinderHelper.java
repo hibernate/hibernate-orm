471dac1e97a8bf56a53286aa7a6ed348e9549d75
Steve Ebersole <steve@hibernate.org>
2024-03-25 22:05:07 -0500

    HHH-17460 - Ongoing JPA 32 work
    - org.hibernate.boot.spi.AdditionalMappingContributions.contributeManagedClass

--- hibernate-core/src/main/java/org/hibernate/boot/model/internal/BinderHelper.java
+++ hibernate-core/src/main/java/org/hibernate/boot/model/internal/BinderHelper.java
@@ -38,6 +38,7 @@ import org.hibernate.boot.spi.PropertyData;
 import org.hibernate.dialect.DatabaseVersion;
 import org.hibernate.dialect.Dialect;
 import org.hibernate.internal.log.DeprecationLogger;
+import org.hibernate.internal.util.StringHelper;
 import org.hibernate.mapping.Any;
 import org.hibernate.mapping.AttributeContainer;
 import org.hibernate.mapping.BasicValue;
@@ -883,7 +884,10 @@ public class BinderHelper {
 		final ClassDetailsRegistry classDetailsRegistry = buildingContext.getMetadataCollector()
 				.getSourceModelBuildingContext()
 				.getClassDetailsRegistry();
-		final ClassDetails classDetails = classDetailsRegistry.resolveClassDetails( propertyHolder.getPersistentClass().getClassName() );
+		final String name = StringHelper.isEmpty( propertyHolder.getPersistentClass().getClassName() )
+				? propertyHolder.getPersistentClass().getEntityName()
+				: propertyHolder.getPersistentClass().getClassName();
+		final ClassDetails classDetails = classDetailsRegistry.resolveClassDetails( name );
 		final InFlightMetadataCollector metadataCollector = buildingContext.getMetadataCollector();
 		if ( propertyHolder.isInIdClass() ) {
 			final PropertyData data = metadataCollector.getPropertyAnnotatedWithIdAndToOne( classDetails, propertyName );

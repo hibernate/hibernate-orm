c758755f61a3aca97f73a22abd2df13cd38bd3fe
Steve Ebersole <steve@hibernate.org>
2024-03-26 20:31:09 -0500

    HHH-17460 - Ongoing JPA 32 work
    HHH-17892 - Remove @Persister
    HHH-17893 - Remove MetadataContributor
    HHH-17894 - Remove AdditionalJaxbMappingProducer

--- hibernate-core/src/test/java/org/hibernate/orm/test/mapping/converted/converter/custom/CustomTypeConverterTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/mapping/converted/converter/custom/CustomTypeConverterTest.java
@@ -6,31 +6,19 @@
  */
 package org.hibernate.orm.test.mapping.converted.converter.custom;
 
-import java.util.List;
-
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.model.TypeContributions;
 import org.hibernate.boot.model.TypeContributor;
-import org.hibernate.boot.registry.BootstrapServiceRegistry;
-import org.hibernate.boot.registry.BootstrapServiceRegistryBuilder;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.spi.InFlightMetadataCollector;
 import org.hibernate.boot.spi.MetadataBuilderImplementor;
-import org.hibernate.boot.spi.MetadataContributor;
-import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.persister.entity.EntityPersister;
 import org.hibernate.service.ServiceRegistry;
-import org.hibernate.tool.schema.Action;
 import org.hibernate.type.spi.TypeConfiguration;
 
-import org.hibernate.testing.boot.ExtraJavaServicesClassLoaderService;
-import org.hibernate.testing.boot.ExtraJavaServicesClassLoaderService.JavaServiceDescriptor;
-import org.hibernate.testing.junit4.BaseUnitTestCase;
-import org.hibernate.testing.util.ServiceRegistryUtil;
-import org.junit.Test;
-
-import org.jboss.jandex.IndexView;
+import org.hibernate.testing.orm.junit.BootstrapServiceRegistry;
+import org.hibernate.testing.orm.junit.BootstrapServiceRegistry.JavaService;
+import org.hibernate.testing.orm.junit.ServiceRegistryScope;
+import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -38,31 +26,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 /**
  * @author Steve Ebersole
  */
-public class CustomTypeConverterTest extends BaseUnitTestCase {
+public class CustomTypeConverterTest {
+
 	@Test
-	public void testConverterAppliedScopedRegistration() {
-		final List<JavaServiceDescriptor<?>> services = List.of(
-				new JavaServiceDescriptor<>(
-						MetadataContributor.class,
-						PayloadWrapperMetadataContributor.class
-				)
-		);
-		final BootstrapServiceRegistry bsr = new BootstrapServiceRegistryBuilder().enableAutoClose()
-				.applyClassLoaderService( new ExtraJavaServicesClassLoaderService( services ) )
-				.build();
-		try (final StandardServiceRegistry ssr = ServiceRegistryUtil.serviceRegistryBuilder( bsr )
-				.applySetting( AvailableSettings.HBM2DDL_AUTO, Action.CREATE_DROP )
-				.build() ) {
-			final MetadataSources metadataSources = new MetadataSources( ssr )
-					.addAnnotatedClass( PayloadWrapperConverter.class )
-					.addAnnotatedClass( MyEntity.class );
-			final MetadataBuilderImplementor metadataBuilder = (MetadataBuilderImplementor) metadataSources.getMetadataBuilder();
-
-			// now the new scoped way
-			final TypeConfiguration bootTypeConfiguration = metadataBuilder.getBootstrapContext().getTypeConfiguration();
-
-			performAssertions( metadataBuilder, bootTypeConfiguration );
-		}
+	@BootstrapServiceRegistry( javaServices = @JavaService(
+			role = TypeContributor.class,
+			impl = PayloadWrapperTypeContributorImpl.class
+	) )
+	@SuppressWarnings("JUnitMalformedDeclaration")
+	public void testConverterAppliedScopedContributions(ServiceRegistryScope registryScope) {
+		final MetadataSources metadataSources = new MetadataSources( registryScope.getRegistry() )
+				.addAnnotatedClass( PayloadWrapperConverter.class )
+				.addAnnotatedClass( MyEntity.class );
+		final MetadataBuilderImplementor metadataBuilder = (MetadataBuilderImplementor) metadataSources.getMetadataBuilder();
+		final TypeConfiguration bootTypeConfiguration = metadataBuilder.getBootstrapContext().getTypeConfiguration();
+		performAssertions( metadataBuilder, bootTypeConfiguration );
 	}
 
 	protected void performAssertions(
@@ -86,41 +64,6 @@ public class CustomTypeConverterTest extends BaseUnitTestCase {
 
 			final EntityPersister entityPersister = sessionFactory.getMappingMetamodel().getEntityDescriptor( MyEntity.class );
 			entityPersister.getPropertyType( "customType" );
-		}
-	}
-
-	@Test
-	public void testConverterAppliedScopedContributions() {
-		final List<JavaServiceDescriptor<?>> services = List.of(
-				new JavaServiceDescriptor<>( TypeContributor.class, PayloadWrapperTypeContributorImpl.class )
-		);
-
-		final BootstrapServiceRegistry bsr = new BootstrapServiceRegistryBuilder().enableAutoClose()
-				.applyClassLoaderService( new ExtraJavaServicesClassLoaderService( services ) )
-				.build();
-		try ( final StandardServiceRegistry ssr = ServiceRegistryUtil.serviceRegistryBuilder( bsr )
-				.applySetting( AvailableSettings.HBM2DDL_AUTO, Action.CREATE_DROP )
-				.build() ) {
-			final MetadataSources metadataSources = new MetadataSources( ssr )
-					.addAnnotatedClass( PayloadWrapperConverter.class )
-					.addAnnotatedClass( MyEntity.class );
-			final MetadataBuilderImplementor metadataBuilder = (MetadataBuilderImplementor) metadataSources.getMetadataBuilder();
-
-			// now the new scoped way
-			final TypeConfiguration bootTypeConfiguration = metadataBuilder.getBootstrapContext().getTypeConfiguration();
-
-			performAssertions( metadataBuilder, bootTypeConfiguration );
-		}
-	}
-
-	public static class PayloadWrapperMetadataContributor implements MetadataContributor {
-		@Override
-		public void contribute(InFlightMetadataCollector metadataCollector, IndexView jandexIndex) {
-			final TypeConfiguration typeConfiguration = metadataCollector.getTypeConfiguration();
-			typeConfiguration.getJavaTypeRegistry()
-					.addDescriptor( PayloadWrapperJavaType.INSTANCE );
-			typeConfiguration.getJdbcTypeRegistry()
-					.addDescriptor( PayloadWrapperJdbcType.INSTANCE );
 		}
 	}
 

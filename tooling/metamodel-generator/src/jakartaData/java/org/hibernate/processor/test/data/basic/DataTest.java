8caa7096fb03079aca8b56563370e491c901726a
Gavin King <gavin@hibernate.org>
2024-03-28 16:04:44 +0100

    make fully-qualified enum literals in @Query pass the validation
    JD examples and TCK require this, though it's not really correct
    
    Signed-off-by: Gavin King <gavin@hibernate.org>

--- tooling/metamodel-generator/src/jakartaData/java/org/hibernate/processor/test/data/basic/DataTest.java
+++ tooling/metamodel-generator/src/jakartaData/java/org/hibernate/processor/test/data/basic/DataTest.java
@@ -4,7 +4,7 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.processor.test.data;
+package org.hibernate.processor.test.data.basic;
 
 import org.hibernate.processor.test.util.CompilationTest;
 import org.hibernate.processor.test.util.WithClasses;

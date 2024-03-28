7e29539153cf2d3d3cb0d0da2219def2d4e615ba
Christian Beikov <christian.beikov@gmail.com>
2024-03-28 16:42:31 +0100

    HHH-17493 Ensure case insensitivity is retained in negated copy

--- hibernate-core/src/main/java/org/hibernate/query/sqm/tree/predicate/SqmLikePredicate.java
+++ hibernate-core/src/main/java/org/hibernate/query/sqm/tree/predicate/SqmLikePredicate.java
@@ -143,12 +143,13 @@ public class SqmLikePredicate extends AbstractNegatableSqmPredicate {
 
 	@Override
 	protected SqmNegatablePredicate createNegatedNode() {
-		return new SqmLikePredicate( matchExpression, pattern, escapeCharacter, !isNegated(), nodeBuilder() );
-	}
-
-	@Override
-	public SqmNegatablePredicate not() {
-		negate();
-		return this;
+		return new SqmLikePredicate(
+				matchExpression,
+				pattern,
+				escapeCharacter,
+				!isNegated(),
+				isCaseSensitive,
+				nodeBuilder()
+		);
 	}
 }

68215f78d0b636041582c7262008820ab9f6a74e
Andrea Boriero <andrea@hibernate.org>
2024-03-28 14:40:50 +0100

    HHH-17460 - Ongoing JPA 32 work

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/schemaupdate/foreignkeys/CustomerInventory.java
@@ -0,0 +1,31 @@
+package org.hibernate.orm.test.schemaupdate.foreignkeys;
+
+public class CustomerInventory {
+	private Long id;
+	private String name;
+	private Customer customer;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+}

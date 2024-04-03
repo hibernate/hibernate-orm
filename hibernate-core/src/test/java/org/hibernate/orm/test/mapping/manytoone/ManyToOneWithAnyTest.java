b8b33aca658e8639429a0941e1f31e616409b6dc
Vincent Bouthinon <v-bouthinon@efluid.fr>
2024-04-03 18:17:35 +0200

    HHH-15722 : @OneToMany mappedBy with a @Any
    Since the collection, which is mapped by the return variable (mappedBy) of the referenced objects, knows its type, the identifying column of the foreign key is sufficient, without having to use the discrimination column.
    
    @startuml
    
    class Library {
        -Long id
        -Set<Book> books
        +void setId(Long)
        +Long getId()
        +Set<Book> getBooks()
        +void setBooks(Set<Book>)
    }
    
    class Book {
        -Long id
        -Store store
        +void setId(Long)
        +Long getId()
        +Store getStore()
        +void setStore(Store)
    }
    
    interface Store {
    }
    
    Library --|> Store : implements
    Library "1" -- "*" Book : contains
    
    @enduml

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/mapping/manytoone/ManyToOneWithAnyTest.java
@@ -0,0 +1,146 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
+package org.hibernate.orm.test.mapping.manytoone;
+
+import java.util.Set;
+
+import org.hibernate.Session;
+import org.hibernate.annotations.Any;
+import org.hibernate.annotations.AnyDiscriminator;
+import org.hibernate.annotations.AnyDiscriminatorValue;
+import org.hibernate.annotations.AnyKeyJavaClass;
+import org.hibernate.cfg.JdbcSettings;
+
+import org.hibernate.testing.orm.junit.EntityManagerFactoryScope;
+import org.hibernate.testing.orm.junit.JiraKey;
+import org.hibernate.testing.orm.junit.Jpa;
+import org.hibernate.testing.orm.junit.Setting;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import static jakarta.persistence.DiscriminatorType.STRING;
+
+/**
+ * @author Vincent Bouthinon
+ */
+@Jpa(
+		annotatedClasses = {
+				ManyToOneWithAnyTest.Library.class,
+				ManyToOneWithAnyTest.Book.class
+		},
+		integrationSettings = @Setting(name = JdbcSettings.SHOW_SQL, value = "true")
+)
+@JiraKey("HHH-15722")
+class ManyToOneWithAnyTest {
+
+	@Test
+	void testMappingManyToOneMappedByAny(EntityManagerFactoryScope scope) {
+
+		scope.inTransaction(
+				entityManager -> {
+					Library library = new Library();
+					Book firstBook = new Book();
+					final Set<Book> books = Set.of( firstBook, new Book() );
+					library.setBooks( books );
+
+					entityManager.persist( library );
+					entityManager.flush();
+					entityManager.clear();
+
+					firstBook = entityManager.unwrap( Session.class )
+							.byId( firstBook.getClass() )
+							.load( firstBook.getId() );
+
+					Assertions.assertNotNull( firstBook );
+
+					entityManager.clear();
+
+					library = entityManager.unwrap( Session.class )
+							.byId( library.getClass() )
+							.load( library.getId() );
+
+					Assertions.assertNotNull( library );
+					Assertions.assertEquals( 2, library.getBooks().size() );
+				}
+		);
+	}
+
+	@Entity(name = "library")
+	@Table(name = "TLIBRARY")
+	public static class Library implements Store {
+
+		@Id
+		@GeneratedValue
+		private Long id;
+
+		@OneToMany(mappedBy = "store", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+		private Set<Book> books;
+
+		public void setId(final Long id) {
+			this.id = id;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public Set<Book> getBooks() {
+			return books;
+		}
+
+		public void setBooks(final Set<Book> books) {
+			books.forEach( book -> book.setStore( this ) );
+			this.books = books;
+		}
+	}
+
+	@Entity(name = "book")
+	@Table(name = "TBOOK")
+	public static class Book {
+
+		@Id
+		@GeneratedValue
+		private Long id;
+
+		@Any
+		@AnyDiscriminator(STRING)
+		@AnyDiscriminatorValue(discriminator = "library", entity = Library.class)
+		@AnyKeyJavaClass(Long.class)
+		@Column(name = "STORE_ROLE")
+		@JoinColumn(name = "STORE_ID")
+		private Store store;
+
+		public void setId(final Long id) {
+			this.id = id;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public Store getStore() {
+			return store;
+		}
+
+		public void setStore(final Store store) {
+			this.store = store;
+		}
+	}
+
+	public interface Store {
+	}
+}

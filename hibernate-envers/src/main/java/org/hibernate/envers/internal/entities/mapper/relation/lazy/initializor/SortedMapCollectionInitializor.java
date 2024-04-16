a0ce4707fb3a7493057a5595cbeb72e9c4e9a386
Sanne Grinovero <sanne@hibernate.org>
2024-04-16 23:29:44 +0100

    HHH-17970 Remove support for running under a SecurityManager

--- hibernate-envers/src/main/java/org/hibernate/envers/internal/entities/mapper/relation/lazy/initializor/SortedMapCollectionInitializor.java
+++ hibernate-envers/src/main/java/org/hibernate/envers/internal/entities/mapper/relation/lazy/initializor/SortedMapCollectionInitializor.java
@@ -51,23 +51,21 @@ public class SortedMapCollectionInitializor extends MapCollectionInitializor<Sor
 		if ( comparator == null ) {
 			return super.initializeCollection( size );
 		}
-		return doPrivileged( () -> {
-			try {
-				return collectionClass.getConstructor( Comparator.class ).newInstance( comparator );
-			}
-			catch (InstantiationException e) {
-				throw new AuditException( e );
-			}
-			catch (IllegalAccessException e) {
-				throw new AuditException( e );
-			}
-			catch (NoSuchMethodException e) {
-				throw new AuditException( e );
-			}
-			catch (InvocationTargetException e) {
-				throw new AuditException( e );
-			}
-		} );
+		try {
+			return collectionClass.getConstructor( Comparator.class ).newInstance( comparator );
+		}
+		catch (InstantiationException e) {
+			throw new AuditException( e );
+		}
+		catch (IllegalAccessException e) {
+			throw new AuditException( e );
+		}
+		catch (NoSuchMethodException e) {
+			throw new AuditException( e );
+		}
+		catch (InvocationTargetException e) {
+			throw new AuditException( e );
+		}
 	}
 
 }

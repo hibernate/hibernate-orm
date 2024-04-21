c266de8b75030722373028d98e8d8fe6881390bc
Gavin King <gavin@hibernate.org>
2024-04-21 15:51:01 +0200

    HHH-17984 StatelessSession statistics for collections
    Signed-off-by: Gavin King <gavin@hibernate.org>

--- hibernate-core/src/test/java/org/hibernate/orm/test/stateless/StatelessSessionStatisticsTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/stateless/StatelessSessionStatisticsTest.java
@@ -1,7 +1,9 @@
 package org.hibernate.orm.test.stateless;
 
 import jakarta.persistence.Basic;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import org.hibernate.stat.spi.StatisticsImplementor;
@@ -12,7 +14,12 @@ import org.hibernate.testing.orm.junit.SessionFactoryScope;
 import org.hibernate.testing.orm.junit.Setting;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hibernate.cfg.StatisticsSettings.GENERATE_STATISTICS;
+import static org.hibernate.graph.GraphSemantic.FETCH;
+import static org.hibernate.graph.GraphSemantic.LOAD;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SessionFactory
@@ -28,25 +35,51 @@ public class StatelessSessionStatisticsTest {
 		assertEquals(0, statistics.getEntityLoadCount());
 		Person person = new Person();
 		person.name = "Gavin";
+		person.handles.add("@1ovthafew");
 		scope.inStatelessTransaction(s -> s.insert(person));
 		assertEquals(1, statistics.getEntityInsertCount());
+		assertEquals(1, statistics.getCollectionRecreateCount());
 		scope.inStatelessSession(s -> s.get(Person.class, person.id));
 		assertEquals(1, statistics.getEntityLoadCount());
+		assertEquals(0, statistics.getEntityFetchCount());
+		assertEquals(1, statistics.getCollectionLoadCount());
+		assertEquals(0, statistics.getCollectionFetchCount());
 		person.name = "Gavin King";
 		scope.inStatelessTransaction(s -> s.update(person));
 		assertEquals(1, statistics.getEntityUpdateCount());
-		scope.inStatelessSession(s -> s.get(Person.class, person.id));
+		assertEquals(1, statistics.getCollectionUpdateCount());
+		scope.inStatelessSession(s -> s.get(s.createEntityGraph(Person.class), LOAD, person.id));
 		assertEquals(2, statistics.getEntityLoadCount());
+		assertEquals(2, statistics.getCollectionLoadCount());
+		assertEquals(0, statistics.getCollectionFetchCount());
+		scope.inStatelessSession(s -> s.get(s.createEntityGraph(Person.class), FETCH, person.id));
+		assertEquals(3, statistics.getEntityLoadCount());
+		assertEquals(2, statistics.getCollectionLoadCount());
+		assertEquals(0, statistics.getCollectionFetchCount());
+		scope.inStatelessSession(s -> s.fetch(s.get(s.createEntityGraph(Person.class), FETCH, person.id).handles));
+		assertEquals(4, statistics.getEntityLoadCount());
+		assertEquals(3, statistics.getCollectionLoadCount());
+		assertEquals(1, statistics.getCollectionFetchCount());
+		scope.inStatelessSession(s -> s.createQuery("from Person", Person.class).getSingleResult());
+		assertEquals(5, statistics.getEntityLoadCount());
+		assertEquals(4, statistics.getCollectionLoadCount());
+		assertEquals(2, statistics.getCollectionFetchCount());
+		person.handles.add("hello world");
+		scope.inStatelessTransaction(s -> s.upsert(person));
+		assertEquals(2, statistics.getCollectionUpdateCount());
 		scope.inStatelessTransaction(s -> s.delete(person));
 		assertEquals(1, statistics.getEntityDeleteCount());
-		assertEquals(3, statistics.getTransactionCount());
+		assertEquals(1, statistics.getCollectionRemoveCount());
+		assertEquals(4, statistics.getTransactionCount());
 	}
 
-	@Entity(name="Entity")
+	@Entity(name="Person")
 	static class Person {
 		@Id @GeneratedValue
 		long id;
 		@Basic(optional = false)
 		String name;
+		@ElementCollection(fetch = FetchType.EAGER)
+		List<String> handles = new ArrayList<>();
 	}
 }

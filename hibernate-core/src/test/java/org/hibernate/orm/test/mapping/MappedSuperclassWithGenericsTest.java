20993b1f860903b1ca1eeb7dc7d24d010a151722
marko-bekhta <marko.prykladna@gmail.com>
2024-04-24 17:12:33 +0200

    HHH-18007 Add a testcase for the issue

--- hibernate-core/src/test/java/org/hibernate/orm/test/mapping/MappedSuperclassWithGenericsTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/mapping/MappedSuperclassWithGenericsTest.java
@@ -6,16 +6,24 @@
  */
 package org.hibernate.orm.test.mapping;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.io.Serializable;
 import java.util.Objects;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Path;
+import jakarta.persistence.criteria.Root;
 
 import org.hibernate.testing.TestForIssue;
 import org.hibernate.testing.orm.junit.DomainModel;
 import org.hibernate.testing.orm.junit.SessionFactory;
+import org.hibernate.testing.orm.junit.SessionFactoryScope;
 import org.junit.jupiter.api.Test;
 
 @TestForIssue(jiraKey = "HHH-14499")
@@ -24,6 +32,8 @@ import org.junit.jupiter.api.Test;
 				MappedSuperclassWithGenericsTest.IntermediateAbstractMapped.class,
 				MappedSuperclassWithGenericsTest.BaseEntity.class,
 				MappedSuperclassWithGenericsTest.AbstractGenericMappedSuperType.class,
+				MappedSuperclassWithGenericsTest.SimpleEntity.class,
+				MappedSuperclassWithGenericsTest.GenericIdBaseEntity.class
 		}
 )
 @SessionFactory
@@ -32,6 +42,25 @@ public class MappedSuperclassWithGenericsTest {
 	@Test
 	public void testIt() {
 
+	}
+
+	@Test
+	void testSelectCriteriaGenericId(SessionFactoryScope scope) {
+		scope.inTransaction( session -> {
+			CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+			CriteriaQuery<Long> criteriaQuery = criteriaBuilder.createQuery( Long.class );
+			Root<SimpleEntity> root = criteriaQuery.from( SimpleEntity.class );
+			Path<Long> idPath = root.get( "id" );
+			criteriaQuery.select( idPath );
+			assertThat( session.createQuery( criteriaQuery ).getResultList() ).isEmpty();
+		} );
+	}
+
+	@Test
+	void testSelectGenericId(SessionFactoryScope scope) {
+		scope.inTransaction( session -> {
+			assertThat( session.createQuery( "select e.id from SimpleEntity e", Long.class ).getResultList() ).isEmpty();
+		} );
 	}
 
 	@MappedSuperclass
@@ -85,6 +114,36 @@ public class MappedSuperclassWithGenericsTest {
 
 		String aString;
 
+	}
+
+	@MappedSuperclass
+	public static class GenericIdBaseEntity<T extends Serializable> {
+
+		@Id
+		private T id;
+
+		protected GenericIdBaseEntity(T id) {
+			this.id = id;
+		}
+
+		public T getId() {
+			return id;
+		}
+	}
+
+	@Entity(name = "SimpleEntity")
+	public static class SimpleEntity extends GenericIdBaseEntity<Long> {
+
+		@Column
+		String string;
+
+		public SimpleEntity() {
+			super( null );
+		}
+
+		protected SimpleEntity(Long id) {
+			super( id );
+		}
 	}
 
 }

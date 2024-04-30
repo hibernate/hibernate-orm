9763f3729057532abb1225cfc5614cbc13408667
Andrea Boriero <andrea@hibernate.org>
2024-04-30 21:29:43 +0200

    HHH-18041 Add test for issue

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/jpa/cacheable/disableselective/City.java
@@ -0,0 +1,31 @@
+package org.hibernate.orm.test.jpa.cacheable.disableselective;
+
+public class City {
+	private Long id;
+
+	private String name;
+
+	public City() {
+	}
+
+	public City(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}

7350251b29da2d0e00884d427fdf343b6650ff44
Andrea Boriero <andrea@hibernate.org>
2024-04-30 21:29:43 +0200

    HHH-18041 Add test for issue

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/jpa/cacheable/disableselective/Person.java
@@ -0,0 +1,43 @@
+package org.hibernate.orm.test.jpa.cacheable.disableselective;
+
+public class Person {
+
+	private String id;
+
+	private String firstName;
+
+	private String lastName;
+
+	public Person() {
+	}
+
+	public Person(String id, String firstName, String lastName) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}

eef1be31203605ac1df8155e3b49ad7219dda1fc
Steve Ebersole <steve@hibernate.org>
2024-04-30 12:40:23 -0500

    HHH-18039 - EntityListeners defined in XML should replace those from annotations, not add to

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/jpa/compliance/callback/listeneroverrides/LineItemSuper.java
@@ -0,0 +1,34 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html.
+ */
+package org.hibernate.orm.test.jpa.compliance.callback.listeneroverrides;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+
+/**
+ * @author Steve Ebersole
+ */
+@MappedSuperclass
+@EntityListeners({ListenerA.class, ListenerB.class})
+public class LineItemSuper extends CallbackTarget {
+	private int quantity;
+
+	public LineItemSuper() {
+	}
+
+	public LineItemSuper(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+}

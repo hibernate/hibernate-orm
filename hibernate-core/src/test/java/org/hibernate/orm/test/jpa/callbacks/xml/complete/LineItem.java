4697b53f7354ff13c8d812c055fd5f37cd098742
Steve Ebersole <steve@hibernate.org>
2024-05-01 10:26:08 -0500

    HHH-18049 - Handle <exclude-default-listeners/> and <exclude-superclass-listeners/>

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/jpa/callbacks/xml/complete/LineItem.java
@@ -0,0 +1,61 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html.
+ */
+package org.hibernate.orm.test.jpa.callbacks.xml.complete;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+/**
+ * @author Steve Ebersole
+ */
+@Entity
+public class LineItem extends LineItemSuper {
+	@Id
+	private Integer id;
+	@ManyToOne
+	@JoinColumn(name = "order_fk")
+	private Order order;
+	@ManyToOne
+	@JoinColumn(name = "product_fk")
+	private Product product;
+
+	public LineItem() {
+	}
+
+	public LineItem(Integer id, Order order, Product product, int quantity) {
+		super( quantity );
+		this.id = id;
+		this.order = order;
+		this.product = product;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+}

50943a1b594ccb23765fafebbf459f2c64e61fe0
Steve Ebersole <steve@hibernate.org>
2024-05-01 10:26:08 -0500

    HHH-18049 - Handle <exclude-default-listeners/> and <exclude-superclass-listeners/>

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/jpa/callbacks/xml/complete/ListenerC.java
@@ -0,0 +1,45 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html.
+ */
+package org.hibernate.orm.test.jpa.callbacks.xml.complete;
+
+import org.hibernate.orm.test.jpa.callbacks.xml.common.CallbackTarget;
+
+/**
+ * @author Steve Ebersole
+ */
+public class ListenerC {
+	public static final String NAME = "ListenerC";
+
+	protected void prePersist(CallbackTarget target) {
+		target.prePersistCalled( NAME );
+	}
+
+	protected void postPersist(CallbackTarget target) {
+		target.postPersistCalled( NAME );
+	}
+
+	protected void preRemove(CallbackTarget target) {
+		target.preRemoveCalled( NAME );
+	}
+
+	protected void postRemove(CallbackTarget target) {
+		target.postRemoveCalled( NAME );
+	}
+
+	protected void preUpdate(CallbackTarget target) {
+		target.preUpdateCalled( NAME );
+	}
+
+	protected void postUpdate(CallbackTarget target) {
+		target.postUpdateCalled( NAME );
+	}
+
+	protected void postLoad(CallbackTarget target) {
+		target.postLoadCalled( NAME );
+	}
+
+}
---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/jpa/callbacks/xml/replace/Product.java
@@ -0,0 +1,58 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html.
+ */
+package org.hibernate.orm.test.jpa.callbacks.xml.replace;
+
+import org.hibernate.orm.test.jpa.callbacks.xml.common.CallbackTarget;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
+
+/**
+ * @author Steve Ebersole
+ */
+@Entity
+@EntityListeners({ ListenerC.class, ListenerB.class})
+public class Product extends CallbackTarget {
+	@Id
+	private Integer id;
+	private String partNumber;
+	private double cost;
+
+	public Product() {
+	}
+
+	public Product(Integer id, String partNumber, double cost) {
+		this.id = id;
+		this.partNumber = partNumber;
+		this.cost = cost;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getPartNumber() {
+		return partNumber;
+	}
+
+	public void setPartNumber(String partNumber) {
+		this.partNumber = partNumber;
+	}
+
+	public double getCost() {
+		return cost;
+	}
+
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+}

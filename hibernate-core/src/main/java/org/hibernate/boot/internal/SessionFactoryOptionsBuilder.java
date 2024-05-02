f0a888b77d7fcc5aa7fb517b1d363ce94ae3c24f
Steve Ebersole <steve@hibernate.org>
2024-05-02 13:00:45 -0500

    HHH-18048 - Split notions of SessionFactory name and SessionFactory JNDI name

--- hibernate-core/src/main/java/org/hibernate/boot/internal/SessionFactoryOptionsBuilder.java
+++ hibernate-core/src/main/java/org/hibernate/boot/internal/SessionFactoryOptionsBuilder.java
@@ -171,7 +171,7 @@ public class SessionFactoryOptionsBuilder implements SessionFactoryOptions {
 	// SessionFactory behavior
 	private final boolean jpaBootstrap;
 	private String sessionFactoryName;
-	private boolean sessionFactoryNameAlsoJndiName;
+	private Boolean sessionFactoryNameAlsoJndiName;
 
 	// Session behavior
 	private boolean flushBeforeCompletionEnabled;
@@ -931,7 +931,7 @@ public class SessionFactoryOptionsBuilder implements SessionFactoryOptions {
 	}
 
 	@Override
-	public boolean isSessionFactoryNameAlsoJndiName() {
+	public Boolean isSessionFactoryNameAlsoJndiName() {
 		return sessionFactoryNameAlsoJndiName;
 	}
 

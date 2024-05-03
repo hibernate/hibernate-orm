daf53ff4b4fd7e2735172c062d620783d482ba06
Gavin King <gavin@hibernate.org>
2024-05-03 22:05:45 +0200

    HHH-18068 fix for H7
    Signed-off-by: Gavin King <gavin@hibernate.org>

--- hibernate-core/src/main/java/org/hibernate/query/sqm/internal/SqmUtil.java
+++ hibernate-core/src/main/java/org/hibernate/query/sqm/internal/SqmUtil.java
@@ -654,7 +654,7 @@ public class SqmUtil {
 			return new SqmSortSpecification(
 					new SqmAliasedNodeRef( element, builder.getIntegerType(), builder ),
 					order.getDirection(),
-					order.getNullPrecedence(),
+					order.getNullPrecedence().getJpaValue(),
 					order.isCaseInsensitive()
 			);
 		}

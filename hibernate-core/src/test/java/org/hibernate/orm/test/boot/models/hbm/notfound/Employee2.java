0d582326dd958de90980c1b1b8ec5214d0ee6a15
Steve Ebersole <steve@hibernate.org>
2024-05-05 21:31:45 -0500

    HHH-18072 - Transform hbm.xml not-found

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/boot/models/hbm/notfound/Employee2.java
@@ -0,0 +1,39 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html.
+ */
+package org.hibernate.orm.test.boot.models.hbm.notfound;
+
+/**
+ * @author Steve Ebersole
+ */
+public class Employee2 {
+	private Integer id;
+	private String name;
+
+	public Employee2() {
+	}
+
+	public Employee2(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}

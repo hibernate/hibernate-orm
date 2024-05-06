3f3b2b20c053ecd750c057e83c7efb4a2cf6d845
Andrea Boriero <andrea@hibernate.org>
2024-05-06 18:06:20 +0200

    HHH-18055 Support for JPA 3.2 table comment

--- hibernate-core/src/main/java/org/hibernate/boot/model/internal/EntityBinder.java
+++ hibernate-core/src/main/java/org/hibernate/boot/model/internal/EntityBinder.java
@@ -449,7 +449,8 @@ public class EntityBinder {
 		if ( jpaTableUsage != null ) {
 			final Table table = persistentClass.getTable();
 			TableBinder.addJpaIndexes( table, jpaTableUsage.getList( "indexes" ), context );
-			TableBinder.addTableCheck(  table, jpaTableUsage.findAttributeValue( "check" ) );
+			TableBinder.addTableCheck( table, jpaTableUsage.findAttributeValue( "check" ) );
+			TableBinder.addTableComment( table, jpaTableUsage.getString( "comment" ) );
 		}
 
 		final InFlightMetadataCollector.EntityTableXref entityTableXref = context
@@ -2174,7 +2175,9 @@ public class EntityBinder {
 				joinTable.getList( "joinColumns" ),
 				joinTable.getList( "uniqueConstraints" )
 		);
-		TableBinder.addTableCheck( join.getTable(), joinTable.findAttributeValue( "check" ));
+		final Table table = join.getTable();
+		TableBinder.addTableCheck( table, joinTable.findAttributeValue( "check" ) );
+		TableBinder.addTableComment( table, joinTable.getString( "comment" ) );
 		return join;
 	}
 
@@ -2191,7 +2194,8 @@ public class EntityBinder {
 		);
 		final Table table = join.getTable();
 		new IndexBinder( context ).bindIndexes( table, secondaryTable.getList( "indexes" ) );
-		TableBinder.addTableCheck( table, secondaryTable.findAttributeValue( "check" ));
+		TableBinder.addTableCheck( table, secondaryTable.findAttributeValue( "check" ) );
+		TableBinder.addTableComment( table, secondaryTable.getString( "comment" ) );
 		return join;
 	}
 

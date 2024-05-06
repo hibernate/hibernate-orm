93d167dc068f1d0652cc6c152d6ef523c1e8e22e
Gavin King <gavin@hibernate.org>
2024-05-06 21:41:15 +0200

    fix problem in definition of @WithProcessorOption
    Signed-off-by: Gavin King <gavin@hibernate.org>

--- tooling/metamodel-generator/src/test/java/org/hibernate/processor/test/util/WithProcessorOption.java
+++ tooling/metamodel-generator/src/test/java/org/hibernate/processor/test/util/WithProcessorOption.java
@@ -7,29 +7,26 @@
 package org.hibernate.processor.test.util;
 
 import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
-import static java.lang.annotation.ElementType.CONSTRUCTOR;
-import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * @author Hardy Ferentschik
  */
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ ElementType.TYPE, ElementType.METHOD })
+@Repeatable(WithProcessorOption.List.class)
+@Retention(RUNTIME)
+@Target({ TYPE, METHOD })
 public @interface WithProcessorOption {
 	String key();
 
 	String value();
 
-	@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER })
+	@Target({ METHOD, TYPE })
 	@Retention(RUNTIME)
 	@Documented
 	@interface List {

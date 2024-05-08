75c45f486f9e69c1b2fd0891d2b8e8362fc8fc05
Andrea Boriero <andrea@hibernate.org>
2024-05-08 13:39:38 +0200

    HHH-18054 Add tests for XML mapping

--- hibernate-core/src/test/java/org/hibernate/orm/test/schemaupdate/checkconstraint/table/AnotherTestEntity.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/schemaupdate/checkconstraint/table/AnotherTestEntity.java
@@ -11,4 +11,20 @@ public class AnotherTestEntity implements Another {
 
 	@Column(name = "FIRST_NAME")
 	private String firstName;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 }

814959b01d8350511a47d77b61b0eb6f44e747b5
Dmitry Kryukov <dk2k@ya.ru>
2024-05-13 20:46:01 +0300

    HHH-18107 fixed package name in 2 classes

--- hibernate-ucp/src/main/java/org/hibernate/ucp/internal/StrategyRegistrationProviderImpl.java
+++ hibernate-ucp/src/main/java/org/hibernate/ucp/internal/StrategyRegistrationProviderImpl.java
@@ -4,7 +4,7 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.oracleucp.internal;
+package org.hibernate.ucp.internal;
 
 import java.util.Collections;
 import java.util.List;
@@ -15,7 +15,7 @@ import org.hibernate.boot.registry.selector.StrategyRegistrationProvider;
 import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
 
 /**
- * Provides the {@link UCPCPConnectionProvider} to the
+ * Provides the {@link UCPConnectionProvider} to the
  * {@link org.hibernate.boot.registry.selector.spi.StrategySelector} service.
  * 
  */
@@ -23,10 +23,10 @@ public class StrategyRegistrationProviderImpl implements StrategyRegistrationPro
 	private static final List<StrategyRegistration> REGISTRATIONS = Collections.singletonList(
 			(StrategyRegistration) new SimpleStrategyRegistrationImpl<ConnectionProvider>(
 					ConnectionProvider.class,
-					UCPConnectionProvider.class,
+					org.hibernate.ucp.internal.UCPConnectionProvider.class,
 					"ucp",
 					"oracleucp",
-					UCPConnectionProvider.class.getSimpleName(),
+					org.hibernate.ucp.internal.UCPConnectionProvider.class.getSimpleName(),
 					"org.oracle.ucp.connection.UCPConnectionProvider"
 			)
 	);
--- hibernate-ucp/src/main/java/org/hibernate/ucp/internal/UCPConnectionProvider.java
+++ hibernate-ucp/src/main/java/org/hibernate/ucp/internal/UCPConnectionProvider.java
@@ -5,7 +5,7 @@
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
 
-package org.hibernate.oracleucp.internal;
+package org.hibernate.ucp.internal;
 
 import java.lang.reflect.Method;
 import java.sql.Connection;

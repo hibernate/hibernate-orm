f8c9f2a744cbf4eb8dd8459255c565bcdd33debe
Andrea Boriero <andrea@hibernate.org>
2024-05-14 12:17:53 +0200

    HHH-18096 Support for JPA 3.2 database generator options

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/schemaupdate/sequencegenerator/TestEntity.java
@@ -0,0 +1,35 @@
+package org.hibernate.orm.test.schemaupdate.sequencegenerator;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = SequenceGeneratorOptionsTest.TABLE_NAME)
+public class TestEntity {
+	@Id
+	@SequenceGenerator(name = "seq_gen", sequenceName = SequenceGeneratorOptionsTest.SEQUENCE_GENERATOR_NAME, options = SequenceGeneratorOptionsTest.SEQUENCE_GENERATOR_OPTIONS)
+	@GeneratedValue(generator = "seq_gen", strategy = GenerationType.SEQUENCE)
+	private Long id;
+
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}

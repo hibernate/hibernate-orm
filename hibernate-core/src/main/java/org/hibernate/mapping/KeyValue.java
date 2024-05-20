c24dc2f0952b8a0509a5bff178aaf5a475dcc6e6
Gavin King <gavin@hibernate.org>
2024-05-20 12:44:18 +0200

    HHH-18139 inline a deprecated method
    Signed-off-by: Gavin King <gavin@hibernate.org>

--- hibernate-core/src/main/java/org/hibernate/mapping/KeyValue.java
+++ hibernate-core/src/main/java/org/hibernate/mapping/KeyValue.java
@@ -7,7 +7,6 @@
 package org.hibernate.mapping;
 
 import org.hibernate.dialect.Dialect;
-import org.hibernate.id.IdentifierGenerator;
 import org.hibernate.generator.Generator;
 
 /**
@@ -30,16 +29,4 @@ public interface KeyValue extends Value {
 
 	Generator createGenerator(Dialect dialect, RootClass rootClass);
 
-	/**
-	 * @deprecated Use {@link #createGenerator(Dialect, RootClass)} instead.
-	 *             No longer used except in legacy tests.
-	 *
-	 * @return {@code null} if the {@code Generator} returned by {@link #createGenerator} is not an instance
-	 *         of {@link IdentifierGenerator}.
-	 */
-	@Deprecated(since="6.2", forRemoval = true)
-	default IdentifierGenerator createIdentifierGenerator(Dialect dialect, RootClass rootClass) {
-		final Generator generator = createGenerator( dialect, rootClass );
-		return generator instanceof IdentifierGenerator ? (IdentifierGenerator) generator : null;
-	}
 }

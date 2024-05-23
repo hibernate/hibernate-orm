0e0ad7b6ed826cdce36e20cb456f104b0530237f
Yanming Zhou <zhouyanming@gmail.com>
2024-05-23 15:28:52 +0800

    HHH-18156 Fix operation is not allowed from within a pluggable database

--- hibernate-testing/src/main/java/org/hibernate/testing/cleaner/OracleDatabaseCleaner.java
+++ hibernate-testing/src/main/java/org/hibernate/testing/cleaner/OracleDatabaseCleaner.java
@@ -76,7 +76,7 @@ public class OracleDatabaseCleaner implements DatabaseCleaner {
 										// Exclude the tables with names starting like 'DEF$_'
 										"      AND table_name NOT LIKE 'DEF$\\_%' ESCAPE '\\'" +
 										" UNION ALL " +
-										"SELECT 'DROP SEQUENCE ' || sequence_owner || '.' || sequence_name FROM all_sequences WHERE sequence_owner = sys_context('USERENV', 'SESSION_USER') and sequence_name not like 'ISEQ$$%'"
+										"SELECT 'DROP SEQUENCE ' || sequence_owner || '.' || sequence_name FROM all_sequences WHERE sequence_owner = sys_context('USERENV', 'SESSION_USER') and sequence_name not like 'ISEQ$$%' and sequence_name not like 'MVIEW$%'"
 						);
 					}
 					catch (SQLException sqlException) {

e721a3769134eb3bdd5ebf80404be52706b657b5
Dmitrii Karmanov <d.karmanov@celonis.com>
2024-05-24 17:20:09 +0200

    HHH-16830: apply filters to find() method

--- hibernate-core/src/main/java/org/hibernate/engine/spi/LoadQueryInfluencers.java
+++ hibernate-core/src/main/java/org/hibernate/engine/spi/LoadQueryInfluencers.java
@@ -13,6 +13,7 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 import org.hibernate.Filter;
 import org.hibernate.Internal;
@@ -166,6 +167,20 @@ public class LoadQueryInfluencers implements Serializable {
 			}
 			return enabledFilters;
 		}
+	}
+
+	/**
+	 * Returns a Map of enabled filters that have the applyToLoadByKey
+	 * flag set to true
+	 * @return a Map of enabled filters that have the applyToLoadByKey
+	 * flag set to true
+	 */
+	public Map<String, Filter> getEnabledFiltersForFind() {
+		return getEnabledFilters()
+				.entrySet()
+				.stream()
+				.filter(f -> f.getValue().isApplyToLoadByKey())
+				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 	}
 
 	/**

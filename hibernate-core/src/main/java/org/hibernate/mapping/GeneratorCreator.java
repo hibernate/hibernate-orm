b6ae0526c427aeee14ba4b8bd1ac57a15e9377f6
Gavin King <gavin@hibernate.org>
2024-05-29 19:28:19 +0200

    HHH-18139 clean up one more bifurcation in the Generator framework
    removes CustomIdGeneratorCreationContext and IdentifierGeneratorCreator
    (which were @Incubating and @Internal, respectively)
    
    Signed-off-by: Gavin King <gavin@hibernate.org>

--- hibernate-core/src/main/java/org/hibernate/mapping/GeneratorCreator.java
+++ hibernate-core/src/main/java/org/hibernate/mapping/GeneratorCreator.java
@@ -7,11 +7,31 @@
 package org.hibernate.mapping;
 
 import org.hibernate.Internal;
+import org.hibernate.generator.Assigned;
 import org.hibernate.generator.Generator;
 import org.hibernate.generator.GeneratorCreationContext;
 
+/**
+ * Instantiates a {@link Generator}.
+ *
+ * @since 6.2
+ *
+ * @author Gavin King
+ */
 @Internal
 @FunctionalInterface
 public interface GeneratorCreator {
+	/**
+	 * Create the generator.
+	 */
 	Generator createGenerator(GeneratorCreationContext context);
+
+	/**
+	 * Does this object create instances of {@link Assigned}?
+	 *
+	 * @since 7.0
+	 */
+	default boolean isAssigned() {
+		return false;
+	}
 }

8d5eb6501d09091b0ec84be88f53d3ef4fcbbd79
Ken Schosinsky <schosinsky@spirit-of-selling.de>
2024-05-29 17:16:10 +0200

    HHH-18181 Include property and class for PropertyBinder errors

--- hibernate-core/src/main/java/org/hibernate/boot/model/internal/PropertyBinder.java
+++ hibernate-core/src/main/java/org/hibernate/boot/model/internal/PropertyBinder.java
@@ -305,8 +305,9 @@ public class PropertyBinder {
 					binder.bind( metaAnnotatedTarget, buildingContext, persistentClass, property );
 				}
 				catch ( Exception e ) {
-					throw new AnnotationException( "error processing @AttributeBinderType annotation '" +
-							metaAnnotatedDescriptor.getAnnotationType().getName() + "'", e );
+					throw new AnnotationException( "error processing @AttributeBinderType annotation '" 
+							+ metaAnnotatedDescriptor.getAnnotationType().getName() + "' for property " 
+							+ qualify( holder.getPath(), name ), e );
 				}
 			}
 		}

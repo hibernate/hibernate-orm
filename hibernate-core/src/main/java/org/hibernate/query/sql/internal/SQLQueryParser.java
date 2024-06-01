c91aa8a14be5aade5aaf4d5cffae0f4b0d4c9ef3
Gavin King <gavin@hibernate.org>
2024-06-01 11:19:14 +0200

    remove SQLLoadable and SQLLoadableCollection
    and some obsolete related code
    
    Signed-off-by: Gavin King <gavin@hibernate.org>

--- hibernate-core/src/main/java/org/hibernate/query/sql/internal/SQLQueryParser.java
+++ hibernate-core/src/main/java/org/hibernate/query/sql/internal/SQLQueryParser.java
@@ -14,8 +14,8 @@ import org.hibernate.boot.model.naming.Identifier;
 import org.hibernate.boot.model.relational.SqlStringGenerationContext;
 import org.hibernate.dialect.Dialect;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
-import org.hibernate.persister.collection.SQLLoadableCollection;
-import org.hibernate.persister.entity.SQLLoadable;
+import org.hibernate.persister.collection.QueryableCollection;
+import org.hibernate.persister.entity.Loadable;
 
 /**
  * @author Gavin King
@@ -38,10 +38,10 @@ public class SQLQueryParser {
 
 	public interface ParserContext {
 		boolean isEntityAlias(String aliasName);
-		SQLLoadable getEntityPersister(String alias);
+		Loadable getEntityPersister(String alias);
 		String getEntitySuffix(String alias);
 		boolean isCollectionAlias(String aliasName);
-		SQLLoadableCollection getCollectionPersister(String alias);
+		QueryableCollection getCollectionPersister(String alias);
 		String getCollectionSuffix(String alias);
 		Map<String, String[]> getPropertyResultsMap(String alias);
 	}
@@ -176,7 +176,7 @@ public class SQLQueryParser {
 			String aliasName,
 			String propertyName) {
 		final Map<String, String[]> fieldResults = context.getPropertyResultsMap( aliasName );
-		final SQLLoadableCollection collectionPersister = context.getCollectionPersister( aliasName );
+		final QueryableCollection collectionPersister = context.getCollectionPersister( aliasName );
 		final String collectionSuffix = context.getCollectionSuffix( aliasName );
 
 		switch ( propertyName ) {
@@ -225,7 +225,7 @@ public class SQLQueryParser {
 	}
 	private String resolveProperties(String aliasName, String propertyName) {
 		final Map<String, String[]> fieldResults = context.getPropertyResultsMap( aliasName );
-		final SQLLoadable persister = context.getEntityPersister( aliasName );
+		final Loadable persister = context.getEntityPersister( aliasName );
 		final String suffix = context.getEntitySuffix( aliasName );
 
 		if ( "*".equals( propertyName ) ) {

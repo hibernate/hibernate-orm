9a1ed5bc4b3db1500137b360f0ac739e0e283163
Gavin King <gavin@hibernate.org>
2024-06-06 14:19:37 +0200

    HHH-17804 roll back change to semantics of 'null in ()'
    It turns out that SQL databases (IMO wrongly) treat the expression
    'null in (select 1 where false)' as false instead of null. And as
    of JPA 3.2, we're free to interpret 'null in ()' consistently with
    that, which we should do.
    
    So my change made things worse rather than better.
    
    Signed-off-by: Gavin King <gavin@hibernate.org>

--- hibernate-core/src/main/java/org/hibernate/sql/ast/spi/AbstractSqlAstTranslator.java
+++ hibernate-core/src/main/java/org/hibernate/sql/ast/spi/AbstractSqlAstTranslator.java
@@ -7606,7 +7606,7 @@ public abstract class AbstractSqlAstTranslator<T extends JdbcOperation> implemen
 	public void visitInListPredicate(InListPredicate inListPredicate) {
 		final List<Expression> listExpressions = inListPredicate.getListExpressions();
 		if ( listExpressions.isEmpty() ) {
-			emptyInList( inListPredicate );
+			appendSql( "1=" + ( inListPredicate.isNegated() ? "1" : "0" ) );
 			return;
 		}
 		Function<Expression, Expression> itemAccessor = Function.identity();
@@ -7711,16 +7711,6 @@ public abstract class AbstractSqlAstTranslator<T extends JdbcOperation> implemen
 		if ( parenthesis ) {
 			appendSql( CLOSE_PARENTHESIS );
 		}
-	}
-
-	protected void emptyInList(InListPredicate inListPredicate) {
-		appendSql("(");
-		appendSql( inListPredicate.isNegated() ? "0" : "1" );
-		appendSql(" = case when ");
-		inListPredicate.getTestExpression().accept( this );
-		appendSql( " is not null then 0");
-//		dialect.appendBooleanValueString( this, inListPredicate.isNegated() );
-		appendSql(" end)");
 	}
 
 	private void appendInClauseSeparator(InListPredicate inListPredicate) {

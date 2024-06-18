328f79f79f73267cd3f9753995f9f785aa99bb2f
Čedomir Igaly <cigaly@users.noreply.github.com>
2024-06-18 07:29:17 +0200

    Test case

---
+++ tooling/metamodel-generator/src/test/java/org/hibernate/processor/test/innerclass/InnerClassTest.java
@@ -0,0 +1,20 @@
+/*
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.processor.test.innerclass;
+
+import org.hibernate.processor.test.util.CompilationTest;
+import org.hibernate.processor.test.util.TestUtil;
+import org.hibernate.processor.test.util.WithClasses;
+
+import org.junit.Test;
+
+public class InnerClassTest extends CompilationTest {
+
+	@WithClasses({ Person.class })
+	@Test
+	public void test() {
+		System.out.println( TestUtil.getMetaModelSourceAsString( Person.class ) );
+	}
+}

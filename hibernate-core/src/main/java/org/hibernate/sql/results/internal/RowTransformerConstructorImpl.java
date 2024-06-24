f65b586ba6652ea1920fc668d0a194adc564c3dc
Čedomir Igaly <cigaly@users.noreply.github.com>
2024-06-24 11:03:23 +0200

    HHH-18322 Fix : when exact constructor is not found, try finding matching one

--- hibernate-core/src/main/java/org/hibernate/sql/results/internal/RowTransformerConstructorImpl.java
+++ hibernate-core/src/main/java/org/hibernate/sql/results/internal/RowTransformerConstructorImpl.java
@@ -17,6 +17,8 @@ import java.util.List;
 import org.hibernate.query.sqm.SqmExpressible;
 import org.hibernate.query.sqm.tree.SqmExpressibleAccessor;
 
+import static org.hibernate.query.sqm.tree.expression.Compatibility.areAssignmentCompatible;
+
 /**
  * {@link RowTransformer} instantiating an arbitrary class
  *
@@ -34,7 +36,7 @@ public class RowTransformerConstructorImpl<T> implements RowTransformer<T> {
 			sig[i] = resolveElementJavaType( elements.get( i ) );
 		}
 		try {
-			constructor = type.getDeclaredConstructor( sig );
+			constructor = findMatchingConstructor( type, sig );
 			constructor.setAccessible( true );
 		}
 		catch (Exception e) {
@@ -52,6 +54,36 @@ public class RowTransformerConstructorImpl<T> implements RowTransformer<T> {
 		}
 
 		return element.getJavaType();
+	}
+
+	private Constructor<T> findMatchingConstructor(
+			Class<T> type,
+			Class<?>[] sig) throws NoSuchMethodException {
+		try {
+			return type.getDeclaredConstructor( sig );
+		}
+		catch (NoSuchMethodException | SecurityException e) {
+			constructor_loop:
+			for ( final Constructor<?> constructor : type.getDeclaredConstructors() ) {
+				final Class<?>[] parameterTypes = constructor.getParameterTypes();
+				if ( parameterTypes.length == sig.length ) {
+					for ( int i = 0; i < sig.length; i++ ) {
+						final Class<?> parameterType = parameterTypes[i];
+						final Class<?> argType = sig[i];
+						final boolean assignmentCompatible;
+						assignmentCompatible = areAssignmentCompatible(
+								parameterType,
+								argType
+						);
+						if ( !assignmentCompatible ) {
+							continue constructor_loop;
+						}
+					}
+					return (Constructor<T>) constructor;
+				}
+			}
+			throw new NoSuchMethodException();
+		}
 	}
 
 	@Override

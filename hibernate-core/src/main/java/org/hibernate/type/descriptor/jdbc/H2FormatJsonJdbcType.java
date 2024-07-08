cee623c85baf5e4328f65a8bbc7f960c94dd9f4e
Marco Belladelli <marcobladel@gmail.com>
2024-07-08 11:12:01 +0200

    HHH-17840 Fix `H2FormatJsonJdbcType` deprecation

--- hibernate-core/src/main/java/org/hibernate/type/descriptor/jdbc/H2FormatJsonJdbcType.java
+++ hibernate-core/src/main/java/org/hibernate/type/descriptor/jdbc/H2FormatJsonJdbcType.java
@@ -18,6 +18,7 @@ import org.hibernate.sql.ast.spi.SqlAppender;
  * @author Marco Belladelli
  * @deprecated Use {@link org.hibernate.dialect.H2JsonJdbcType} instead
  */
+@Deprecated(forRemoval = true, since = "6.5")
 public class H2FormatJsonJdbcType extends JsonJdbcType {
 	/**
 	 * Singleton access

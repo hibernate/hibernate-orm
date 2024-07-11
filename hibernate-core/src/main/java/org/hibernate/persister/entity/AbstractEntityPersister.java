5c6523aacbcd0daa79b8b0b37502f300bef8f430
Andrea Boriero <andrea@hibernate.org>
2024-07-11 13:31:48 +0200

    HHH-18338 o.h.UnknownEntityTypeException: Unable to locate persister thrown when an embeddable object is loaded before the entity it references

--- hibernate-core/src/main/java/org/hibernate/persister/entity/AbstractEntityPersister.java
+++ hibernate-core/src/main/java/org/hibernate/persister/entity/AbstractEntityPersister.java
@@ -676,7 +676,7 @@ public abstract class AbstractEntityPersister
 				else {
 					final Column column = (Column) selectable;
 					colNames[k] = column.getQuotedName( dialect );
-					colWriters[k] = column.getWriteExpr( prop.getValue().getSelectableType( factory, k ), dialect );
+					colWriters[k] = column.getWriteExpr( prop.getValue().getSelectableType( creationContext.getMetadata(), k ), dialect );
 				}
 			}
 			propertyColumnNames[i] = colNames;

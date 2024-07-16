87b51eb1c4508745822a2b6816040b52e61e32c7
Christian Beikov <christian.beikov@gmail.com>
2024-07-16 17:25:57 +0200

    Fixes for nightly failures of PG and EDB v12, DB2 10.5 and MariaDB. Exclude Oracle 21 from nightly runs in favor of Atlas build

--- hibernate-community-dialects/src/main/java/org/hibernate/community/dialect/PostgresPlusLegacyDialect.java
+++ hibernate-community-dialects/src/main/java/org/hibernate/community/dialect/PostgresPlusLegacyDialect.java
@@ -61,13 +61,18 @@ public class PostgresPlusLegacyDialect extends PostgreSQLLegacyDialect {
 		functionFactory.sysdate();
 		functionFactory.systimestamp();
 
-		functionFactory.bitand();
-		functionFactory.bitor();
-		functionContributions.getFunctionRegistry().patternDescriptorBuilder( "bitxor", "(bitor(?1,?2)-bitand(?1,?2))" )
-				.setExactArgumentCount( 2 )
-				.setArgumentTypeResolver( StandardFunctionArgumentTypeResolvers.ARGUMENT_OR_IMPLIED_RESULT_TYPE )
-				.register();
-
+		if ( getVersion().isSameOrAfter( 14 ) ) {
+			// Support for these functions were apparently only added in version 14
+			functionFactory.bitand();
+			functionFactory.bitor();
+			functionContributions.getFunctionRegistry().patternDescriptorBuilder(
+							"bitxor",
+							"(bitor(?1,?2)-bitand(?1,?2))"
+					)
+					.setExactArgumentCount( 2 )
+					.setArgumentTypeResolver( StandardFunctionArgumentTypeResolvers.ARGUMENT_OR_IMPLIED_RESULT_TYPE )
+					.register();
+		}
 	}
 
 	@Override

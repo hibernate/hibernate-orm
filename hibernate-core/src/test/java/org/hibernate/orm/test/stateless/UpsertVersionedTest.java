f9401e65f442d4c3f1f1940d572b4cdb18d5f57a
Christian Beikov <christian.beikov@gmail.com>
2024-07-16 16:23:18 +0200

    HHH-17949 Fix upsert handling when optimistic locking is involved

--- hibernate-core/src/test/java/org/hibernate/orm/test/stateless/UpsertVersionedTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/stateless/UpsertVersionedTest.java
@@ -3,11 +3,7 @@ package org.hibernate.orm.test.stateless;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Version;
-import org.hibernate.dialect.H2Dialect;
-import org.hibernate.dialect.PostgreSQLDialect;
-import org.hibernate.dialect.SQLServerDialect;
 import org.hibernate.testing.orm.junit.DomainModel;
-import org.hibernate.testing.orm.junit.RequiresDialect;
 import org.hibernate.testing.orm.junit.SessionFactory;
 import org.hibernate.testing.orm.junit.SessionFactoryScope;
 import org.junit.jupiter.api.Test;
@@ -17,9 +13,6 @@ import static org.junit.Assert.assertEquals;
 @SessionFactory
 @DomainModel(annotatedClasses = UpsertVersionedTest.Record.class)
 public class UpsertVersionedTest {
-    @RequiresDialect(H2Dialect.class)
-    @RequiresDialect(SQLServerDialect.class)
-    @RequiresDialect(value = PostgreSQLDialect.class, matchSubTypes = false)
     @Test void test(SessionFactoryScope scope) {
         scope.inStatelessTransaction(s-> {
             s.upsert(new Record(123L,null,"hello earth"));
@@ -30,21 +23,21 @@ public class UpsertVersionedTest {
             assertEquals("hello mars",s.get(Record.class,456L).message);
         });
         scope.inStatelessTransaction(s-> {
-            s.upsert(new Record(123L,1L,"goodbye earth"));
+            s.upsert(new Record(123L,0L,"goodbye earth"));
         });
         scope.inStatelessTransaction(s-> {
             assertEquals("goodbye earth",s.get(Record.class,123L).message);
             assertEquals("hello mars",s.get(Record.class,456L).message);
         });
         scope.inStatelessTransaction(s-> {
-            s.upsert(new Record(456L,4L,"goodbye mars"));
+            s.upsert(new Record(456L,3L,"goodbye mars"));
         });
         scope.inStatelessTransaction(s-> {
             assertEquals("goodbye earth",s.get(Record.class,123L).message);
             assertEquals("goodbye mars",s.get(Record.class,456L).message);
         });
     }
-    @Entity
+    @Entity(name = "Record")
     static class Record {
         @Id Long id;
         @Version Long version;

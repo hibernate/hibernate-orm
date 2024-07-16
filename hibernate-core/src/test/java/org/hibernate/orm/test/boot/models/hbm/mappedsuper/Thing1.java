6ce63ec4c49489a834a1b0eb49d9cd8d63362531
Steve Ebersole <steve@hibernate.org>
2024-07-16 08:01:54 -0500

    HHH-18387 - Support implied mapped-superclass in hbm.xml transformation

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/boot/models/hbm/mappedsuper/Thing1.java
@@ -0,0 +1,22 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html.
+ */
+package org.hibernate.orm.test.boot.models.hbm.mappedsuper;
+
+/**
+ * @author Steve Ebersole
+ */
+public class Thing1 extends Base {
+	private String stuff;
+
+	public String getStuff() {
+		return stuff;
+	}
+
+	public void setStuff(String stuff) {
+		this.stuff = stuff;
+	}
+}
---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/boot/models/hbm/mappedsuper/Thing2.java
@@ -0,0 +1,22 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html.
+ */
+package org.hibernate.orm.test.boot.models.hbm.mappedsuper;
+
+/**
+ * @author Steve Ebersole
+ */
+public class Thing2 extends Base {
+	private String data;
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+}

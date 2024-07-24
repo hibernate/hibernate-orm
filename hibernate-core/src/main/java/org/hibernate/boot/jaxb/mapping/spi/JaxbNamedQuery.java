6fbdc6326aaf23665a5728c370fa8b96d28aa038
Jan Schatteman <jschatte@redhat.com>
2024-07-25 00:34:19 +0200

    HHH-17720 - Add common JAXB contracts for named queries
    Signed-off-by: Jan Schatteman <jschatte@redhat.com>

---
+++ hibernate-core/src/main/java/org/hibernate/boot/jaxb/mapping/spi/JaxbNamedQuery.java
@@ -0,0 +1,28 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
+package org.hibernate.boot.jaxb.mapping.spi;
+
+import java.util.List;
+
+import org.hibernate.CacheMode;
+import org.hibernate.FlushMode;
+
+/**
+ * @author Jan Schatteman
+ */
+public interface JaxbNamedQuery extends JaxbQueryHintContainer {
+	String getQuery();
+	String getComment();
+	Integer getTimeout();
+	Boolean isCacheable();
+	String getCacheRegion();
+	Integer getFetchSize();
+	Boolean isReadOnly();
+	List<JaxbQueryParamTypeImpl> getQueryParam();
+	CacheMode getCacheMode();
+	FlushMode getFlushMode();
+}

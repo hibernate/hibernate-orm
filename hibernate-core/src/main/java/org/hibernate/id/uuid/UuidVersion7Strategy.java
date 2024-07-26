b57e84e7367fe591b8cbe704256575805866119b
Čedomir Igaly <cedomir.igaly@e-dama.net>
2024-07-26 13:12:31 +0200

    HHH-18377 UUID Version 6 & UUID Version 7 implementations

---
+++ hibernate-core/src/main/java/org/hibernate/id/uuid/UuidVersion7Strategy.java
@@ -0,0 +1,131 @@
+/*
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.id.uuid;
+
+import java.security.SecureRandom;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.id.UUIDGenerationStrategy;
+
+import static java.time.Instant.EPOCH;
+import static java.time.temporal.ChronoUnit.MILLIS;
+
+/**
+ * Implements UUID Version 7 generation strategy as defined by the <a href="https://datatracker.ietf.org/doc/html/rfc9562#name-uuid-version-7">RFC 9562</a>.
+ *
+ * <ul>
+ *     <li>48 bits - 48-bit big-endian unsigned number of the Unix Epoch timestamp in milliseconds.</li>
+ *     <li>4 bits - version field, set to 0b0111 (7).</li>
+ *     <li>
+ *         12 bits - sub-milliseconds part of timestamp (resolution approximately 1/4 of millisecond)
+ *         to guarantee additional monotonicity.
+ *     </li>
+ *     <li>2 bits - variant field, set to 0b10.</li>
+ *     <li>14 bits - counter to guarantee additional monotonicity, resets to 0 when timestamp changes. </li>
+ *     <li>48 bits - pseudorandom data to provide uniqueness.</li>
+ * </ul>
+ *
+ * @author Cedomir Igaly
+ */
+public class UuidVersion7Strategy implements UUIDGenerationStrategy, UuidValueGenerator {
+
+	public static final UuidVersion7Strategy INSTANCE = new UuidVersion7Strategy();
+
+	private static class Holder {
+
+		static final SecureRandom numberGenerator = new SecureRandom();
+	}
+
+	private final Lock lock = new ReentrantLock( true );
+
+	private Duration currentTimestamp;
+
+	private final AtomicLong clockSequence;
+
+	public UuidVersion7Strategy() {
+		this( getCurrentTimestamp(), 0 );
+	}
+
+	public UuidVersion7Strategy(final Duration currentTimestamp, final long clockSequence) {
+		this.currentTimestamp = currentTimestamp;
+		this.clockSequence = new AtomicLong( clockSequence );
+	}
+
+	/**
+	 * A variant 7
+	 */
+	@Override
+	public int getGeneratedVersion() {
+		/*
+		 *	UUIDv7 features a time-ordered value field derived from the widely implemented and well-
+		 * known Unix Epoch timestamp source, the number of milliseconds since midnight 1 Jan 1970 UTC,
+		 * leap seconds excluded.
+		 */
+		return 7;
+	}
+
+	/**
+	 * Delegates to {@link #generateUuid}
+	 */
+	@Override
+	public UUID generateUUID(SharedSessionContractImplementor session) {
+		return generateUuid( session );
+	}
+
+	/**
+	 * @param session session
+	 *
+	 * @return UUID version 7
+	 * @see UuidValueGenerator#generateUuid(SharedSessionContractImplementor)
+	 */
+	@Override
+	public UUID generateUuid(SharedSessionContractImplementor session) {
+		final Duration currentTimestamp = getCurrentTimestamp();
+
+		final long seq = getSequence( currentTimestamp );
+
+		final long millis = currentTimestamp.getSeconds() * 1000 + currentTimestamp.getNano() / 1_000_000;
+		final long nanosPart = Math.round( ( currentTimestamp.getNano() % 1_000_000L ) * 0.004096 );
+
+		return new UUID(
+				// MSB bits 0-47 - 48-bit big-endian unsigned number of the Unix Epoch timestamp in milliseconds
+				millis << 16 & 0xFFFF_FFFF_FFFF_0000L
+				// MSB bits 48-51 - version = 7
+				| 0x7000L
+				// MSB bits 52-63 - sub-milliseconds part of timestamp
+				| nanosPart & 0xFFFL,
+				// LSB bits 0-1 - variant = 4
+				0x8000_0000_0000_0000L
+				// LSB bits 2-15 - counter
+				| ( seq & 0x3FFFL ) << 48
+				// LSB bits 16-63 - pseudorandom data
+				| Holder.numberGenerator.nextLong() & 0xFFFF_FFFF_FFFFL
+		);
+	}
+
+	private long getSequence(final Duration currentTimestamp) {
+		lock.lock();
+		try {
+			if ( !this.currentTimestamp.equals( currentTimestamp ) ) {
+				this.currentTimestamp = currentTimestamp;
+				clockSequence.set( 0 );
+			}
+		}
+		finally {
+			lock.unlock();
+		}
+		return clockSequence.getAndIncrement();
+	}
+
+	private static Duration getCurrentTimestamp() {
+		return Duration.between( EPOCH, Instant.now() ).truncatedTo( MILLIS );
+	}
+}

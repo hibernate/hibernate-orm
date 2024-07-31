0957052243b59b16d91d7bff47d712fa1a691dad
Marco Belladelli <marcobladel@gmail.com>
2024-07-31 10:16:08 +0200

    HHH-18445 Always preserve correct property order for java records

--- hibernate-core/src/main/java/org/hibernate/boot/model/internal/PropertyContainer.java
+++ hibernate-core/src/main/java/org/hibernate/boot/model/internal/PropertyContainer.java
@@ -97,8 +97,8 @@ public class PropertyContainer {
 
 		final Map<String, XProperty> localAttributeMap;
 		// If the record class has only record components which match up with fields and no additional getters,
-		// we can retain the property order, to match up with the record component order
-		if ( !recordComponents.isEmpty() && recordComponents.size() == fields.size() && getters.isEmpty() ) {
+		// we must retain the property order, to match up with the record component order
+		if ( !recordComponents.isEmpty() && recordComponents.size() == fields.size() ) {
 			localAttributeMap = new LinkedHashMap<>();
 		}
 		//otherwise we sort them in alphabetical order, since this is at least deterministic

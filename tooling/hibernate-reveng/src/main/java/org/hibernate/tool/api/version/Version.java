4ada3d262e0565e448e05ceec8646930651a4b32
Koen Aers <koen.aers@gmail.com>
2024-08-01 15:34:53 +0300

    HBX-2885: Update version identifier to 7.0.0-SNAPSHOT
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- tooling/hibernate-reveng/src/main/java/org/hibernate/tool/api/version/Version.java
+++ tooling/hibernate-reveng/src/main/java/org/hibernate/tool/api/version/Version.java
@@ -2,6 +2,6 @@ package org.hibernate.tool.api.version;
 
 public interface Version {
 	
-	final static String CURRENT_VERSION = "7.0.0.Alpha3";
+	final static String CURRENT_VERSION = "7.0.0-SNAPSHOT";
 
 }

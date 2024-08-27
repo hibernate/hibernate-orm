d0f25d3b6fa37585cb7507f8f7b09bdf308a11d9
Marco Belladelli <marcobladel@gmail.com>
2024-08-27 11:13:02 +0200

    HHH-18524 Fix binding of meta-annotated id generators for id-class

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/tenantidpk/TenantizedId.java
@@ -0,0 +1,24 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
+package org.hibernate.orm.test.tenantidpk;
+
+import java.util.UUID;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class TenantizedId {
+	Long id;
+	UUID tenantId;
+
+	public TenantizedId(Long id, UUID tenantId) {
+		this.id = id;
+		this.tenantId = tenantId;
+	}
+
+	TenantizedId() {}
+}

fe8d18a3ce1def4bc0e7a0a7ee3d93b73c0d7d72
Gavin King <gavin@hibernate.org>
2024-09-22 23:37:25 +0200

    HHH-18649 test

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/jpa/metadata/Record.java
@@ -0,0 +1,33 @@
+/*
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.orm.test.jpa.metadata;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedEntityGraph;
+import jakarta.persistence.NamedQuery;
+
+import java.time.LocalDateTime;
+
+@NamedQuery(name = "TextById",
+		query = "select text from Record where id = ?1")
+@NamedQuery(name = "AllRecords",
+		query = "from Record order by timestamp, id")
+@NamedQuery(name = "AllRecordsAsTuples",
+		query = "select id, text from Record order by timestamp, id")
+@NamedEntityGraph(name = "CompleteRecord",
+		includeAllAttributes = true)
+@Entity
+public class Record {
+	@Id @GeneratedValue
+	String id;
+	String text;
+	LocalDateTime timestamp = LocalDateTime.now();
+	Record() {}
+	public Record(String text) {
+		this.text = text;
+	}
+}

e06da88b4226cee14f7eb17e2217bafcc39a04f0
Marco Belladelli <marcobladel@gmail.com>
2024-09-30 11:37:18 +0200

    HHH-17612 HHH-18762 Separate envers default revision entity types from mapped-superclasses
    Also, ensure they're query-able through Criteria and HQL queries

--- hibernate-envers/src/test/java/org/hibernate/orm/test/envers/EntityTypeChangeAuditDefaultTrackingTest.java
+++ hibernate-envers/src/test/java/org/hibernate/orm/test/envers/EntityTypeChangeAuditDefaultTrackingTest.java
@@ -19,7 +19,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.envers.Audited;
-import org.hibernate.envers.DefaultTrackingModifiedEntitiesRevisionEntity;
+import org.hibernate.envers.TrackingModifiedEntitiesRevisionMapping;
 import org.hibernate.envers.RevisionEntity;
 import org.hibernate.jpa.boot.spi.Bootstrap;
 
@@ -198,7 +198,7 @@ public class EntityTypeChangeAuditDefaultTrackingTest extends EntityManagerFacto
 	@Table(name = "TRACKING_REV_INFO")
 	@RevisionEntity
 	public static class CustomTrackingRevisionEntity
-		extends DefaultTrackingModifiedEntitiesRevisionEntity {
+		extends TrackingModifiedEntitiesRevisionMapping {
 
 	}
 	//end::envers-tracking-modified-entities-revchanges-example[]

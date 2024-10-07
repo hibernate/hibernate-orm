8ffddc22bbdab843a44e8298250ee69ff5f7070a
Koen Aers <koen.aers@gmail.com>
2024-10-07 14:00:37 +0200

    HHH-18644: New and improved hibernate-maven-plugin   - Add 'target' and '.vscode' folders to .gitignore   - Rename 'org.hibernate.orm.MyMojo' to 'org.hibernate.orm.EnhanceMojo'   - Format 'pom.xml' and extract the version properties
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- src/main/java/org/hibernate/orm/EnhanceMojo.java
+++ src/main/java/org/hibernate/orm/EnhanceMojo.java
@@ -1,7 +1,7 @@
 package org.hibernate.orm;
 
 /*
- * Copyright 2001-2005 The Apache Software Foundation.
+ * Copyright 20024 The Apache Software Foundation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,13 +24,13 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 /**
- * Goal which touches a timestamp file.
+ * Maven mojo for performing build-time enhancement of entity objects.
  *
- * @goal touch
+ * @goal enhance
  * 
  * @phase process-sources
  */
-public class MyMojo
+public class EnhanceMojo
     extends AbstractMojo
 {
     /**

2fa308c4a9e4675160907c3aaeb9e05db5429644
Koen Aers <koen.aers@gmail.com>
2024-10-07 16:20:24 +0200

    HHH-18644: New and improved hibernate-maven-plugin   - Use version '4.0.0-beta-4' of maven-plugin-api   - Avoid some warnings by adding encoding and specifying compiler version 17   - Change the package info in class EnhanceMojo   - Use Maven annotations instead of Javadoc tags as per the Maven advice   - Add a simple dummy unit test
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ src/test/java/org/hibernate/orm/tooling/maven/enhance/EnhanceMojoTest.java
@@ -0,0 +1,14 @@
+package org.hibernate.orm.tooling.maven.enhance;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class EnhanceMojoTest {
+	
+    @Test
+    void testSomething() {
+        assertTrue(true);
+    }
+
+}

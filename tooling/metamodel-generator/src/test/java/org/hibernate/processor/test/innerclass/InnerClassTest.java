7852367c5134fe3cd9ab73e19cebb0e83c1f4419
Čedomir Igaly <cedomir.igaly@e-dama.net>
2024-10-08 13:29:44 +0200

    Test class

--- tooling/metamodel-generator/src/test/java/org/hibernate/processor/test/innerclass/InnerClassTest.java
+++ tooling/metamodel-generator/src/test/java/org/hibernate/processor/test/innerclass/InnerClassTest.java
@@ -5,18 +5,24 @@
 package org.hibernate.processor.test.innerclass;
 
 import org.hibernate.processor.test.util.CompilationTest;
-import org.hibernate.processor.test.util.TestUtil;
 import org.hibernate.processor.test.util.WithClasses;
 import org.junit.Test;
+
+import static org.hibernate.processor.test.util.TestUtil.assertMetamodelClassGeneratedFor;
+import static org.hibernate.processor.test.util.TestUtil.getMetaModelSourceAsString;
 
 public class InnerClassTest extends CompilationTest {
 
 	@WithClasses({Person.class, Dummy.class})
 	@Test
 	public void test() {
-		System.out.println( TestUtil.getMetaModelSourceAsString( Person.class ) );
-		System.out.println( TestUtil.getMetaModelSourceAsString( Person.PersonId.class ) );
-		System.out.println( TestUtil.getMetaModelSourceAsString( Dummy.DummyEmbeddable.class ) );
-		System.out.println( TestUtil.getMetaModelSourceAsString( Dummy.Inner.class ) );
+		assertMetamodelClassGeneratedFor( Person.class );
+		assertMetamodelClassGeneratedFor( Person.PersonId.class );
+		assertMetamodelClassGeneratedFor( Dummy.DummyEmbeddable.class );
+		assertMetamodelClassGeneratedFor( Dummy.Inner.class );
+		System.out.println( getMetaModelSourceAsString( Person.class ) );
+		System.out.println( getMetaModelSourceAsString( Person.PersonId.class ) );
+		System.out.println( getMetaModelSourceAsString( Dummy.DummyEmbeddable.class ) );
+		System.out.println( getMetaModelSourceAsString( Dummy.Inner.class ) );
 	}
 }

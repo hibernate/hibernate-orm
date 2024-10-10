b999bc0d55f58ab422e66766f18f3f61be36f859
Koen Aers <koen.aers@gmail.com>
2024-10-10 16:42:38 +0200

    HHH-18644: New and improved hibernate-maven-plugin   - Log the configuration of the Mojo to the 'build.log'   - Verify that this happens in the 'verify.groovy' script
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- src/test/java/org/hibernate/orm/tooling/maven/enhance/EnhanceMojoTest.java
+++ src/test/java/org/hibernate/orm/tooling/maven/enhance/EnhanceMojoTest.java
@@ -18,7 +18,6 @@ package org.hibernate.orm.tooling.maven.enhance;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;

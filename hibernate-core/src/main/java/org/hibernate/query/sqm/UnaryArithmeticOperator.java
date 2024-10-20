5db76293824b48642ee5fa3d47db0ac48f524b0b
Gavin King <gavin@hibernate.org>
2024-10-20 13:56:46 +0200

    improve jdoc of SQM enums
    Signed-off-by: Gavin King <gavin@hibernate.org>

--- hibernate-core/src/main/java/org/hibernate/query/sqm/UnaryArithmeticOperator.java
+++ hibernate-core/src/main/java/org/hibernate/query/sqm/UnaryArithmeticOperator.java
@@ -5,19 +5,23 @@
 package org.hibernate.query.sqm;
 
 /**
+ * Enumerates the unary prefix operators.
+ *
+ * @apiNote This is an SPI type allowing collaboration
+ * between {@code org.hibernate.dialect} and
+ * {@code org.hibernate.sqm}. It should never occur in
+ * APIs visible to the application program.
+ *
  * @author Steve Ebersole
  */
 public enum UnaryArithmeticOperator {
-	UNARY_PLUS( '+' ),
-	UNARY_MINUS( '-' );
-
-	private final char operatorChar;
-
-	UnaryArithmeticOperator(char operatorChar) {
-		this.operatorChar = operatorChar;
-	}
+	UNARY_PLUS,
+	UNARY_MINUS;
 
 	public char getOperatorChar() {
-		return operatorChar;
+		return switch ( this ) {
+			case UNARY_PLUS -> '+';
+			case UNARY_MINUS -> '-';
+		};
 	}
 }

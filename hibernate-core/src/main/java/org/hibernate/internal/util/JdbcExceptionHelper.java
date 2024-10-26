33e18a343daedc40ae3ae244af8694e2805ea435
Gavin King <gavin@hibernate.org>
2024-10-26 12:51:06 +0200

    HHH-18772 introduce AuthException and simplify SQLStateConversionDelegate
    Signed-off-by: Gavin King <gavin@hibernate.org>

--- hibernate-core/src/main/java/org/hibernate/internal/util/JdbcExceptionHelper.java
+++ hibernate-core/src/main/java/org/hibernate/internal/util/JdbcExceptionHelper.java
@@ -56,9 +56,6 @@ public final class JdbcExceptionHelper {
 	}
 
 	public static String determineSqlStateClassCode(String sqlState) {
-		if ( sqlState == null || sqlState.length() < 2 ) {
-			return sqlState;
-		}
-		return sqlState.substring( 0, 2 );
+		return sqlState == null || sqlState.length() < 2 ? sqlState : sqlState.substring( 0, 2 );
 	}
 }

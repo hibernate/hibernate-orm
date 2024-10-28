df4c008ce6715ccc26009e532b6fffd9a18179be
stringintech <stringintech@gmail.com>
2024-10-28 21:19:28 +0330

    HHH-18764 Adapt unit test for backport to 6.6

--- hibernate-core/src/test/java/org/hibernate/orm/test/associations/ManyToOneUniqueKeyReferenceWithCustomIdTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/associations/ManyToOneUniqueKeyReferenceWithCustomIdTest.java
@@ -4,12 +4,7 @@
  */
 package org.hibernate.orm.test.associations;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import org.hibernate.HibernateException;
 import org.hibernate.annotations.NaturalId;
 import org.hibernate.annotations.Type;
@@ -26,170 +21,187 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
+import java.util.Objects;
 
 /**
  * @author Kowsar Atazadeh
  */
 @SessionFactory
 @DomainModel(annotatedClasses =
-		{ManyToOneUniqueKeyReferenceWithCustomIdTest.Phone.class, ManyToOneUniqueKeyReferenceWithCustomIdTest.User.class})
+        {ManyToOneUniqueKeyReferenceWithCustomIdTest.Phone.class, ManyToOneUniqueKeyReferenceWithCustomIdTest.User.class})
 @JiraKey("HHH-18764")
 public class ManyToOneUniqueKeyReferenceWithCustomIdTest {
 
-	@Test
-	void test(SessionFactoryScope scope) {
-		scope.inTransaction( session -> {
-			Phone phone = new Phone();
+    @Test
+    void test(SessionFactoryScope scope) {
+        scope.inTransaction(session -> {
+            Phone phone = new Phone();
 
-			User user1 = new User( new CustomId( "u1" ), "Kowsar" );
-			session.persist( user1 );
-			phone.setUser( user1 );
-			session.persist( phone );
+            User user1 = new User(new CustomId("u1"), "Kowsar");
+            session.persist(user1);
+            phone.setUser(user1);
+            session.persist(phone);
 
-			User user2 = new User( new CustomId( "u2" ), "Someone" );
-			session.persist( user2 );
-			phone.setUser( user2 );
-			session.persist( phone );
-		} );
-	}
+            User user2 = new User(new CustomId("u2"), "Someone");
+            session.persist(user2);
+            phone.setUser(user2);
+            session.persist(phone);
+        });
+    }
 
-	@Entity(name = "Phone")
-	static class Phone {
-		@Id
-		@GeneratedValue
-		Long id;
+    @Entity(name = "Phone")
+    static class Phone {
+        @Id
+        @GeneratedValue
+        Long id;
 
-		@ManyToOne(fetch = FetchType.LAZY)
-		@JoinColumn(referencedColumnName = "name", nullable = false)
-		User user;
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(referencedColumnName = "name", nullable = false)
+        User user;
 
-		public Long getId() {
-			return id;
-		}
+        public Long getId() {
+            return id;
+        }
 
-		public void setId(Long id) {
-			this.id = id;
-		}
+        public void setId(Long id) {
+            this.id = id;
+        }
 
-		public User getUser() {
-			return user;
-		}
+        public User getUser() {
+            return user;
+        }
 
-		public void setUser(User user) {
-			this.user = user;
-		}
-	}
+        public void setUser(User user) {
+            this.user = user;
+        }
+    }
 
-	@Entity(name = "_User")
-	static class User {
-		@Id
-		@Type(CustomIdType.class)
-		CustomId id;
+    @Entity(name = "_User")
+    static class User {
+        @Id
+        @Type(CustomIdType.class)
+        CustomId id;
 
-		@NaturalId
-		String name;
+        @NaturalId
+        String name;
 
-		public User() {
-		}
+        public User() {
+        }
 
-		public User(CustomId id, String name) {
-			this.id = id;
-			this.name = name;
-		}
+        public User(CustomId id, String name) {
+            this.id = id;
+            this.name = name;
+        }
 
-		public String getName() {
-			return name;
-		}
+        public String getName() {
+            return name;
+        }
 
-		public void setName(String name) {
-			this.name = name;
-		}
+        public void setName(String name) {
+            this.name = name;
+        }
 
-		public CustomId getId() {
-			return id;
-		}
+        public CustomId getId() {
+            return id;
+        }
 
-		public void setId(CustomId id) {
-			this.id = id;
-		}
-	}
+        public void setId(CustomId id) {
+            this.id = id;
+        }
+    }
 
-	static class CustomIdType implements EnhancedUserType<CustomId> {
-		@Override
-		public String toSqlLiteral(CustomId value) {
-			return "'" + value.toString() + "'";
-		}
+    static class CustomIdType implements EnhancedUserType<CustomId> {
+        @Override
+        public String toSqlLiteral(CustomId value) {
+            return "'" + value.toString() + "'";
+        }
 
-		@Override
-		public String toString(CustomId value) throws HibernateException {
-			return value.toString();
-		}
+        @Override
+        public String toString(CustomId value) throws HibernateException {
+            return value.toString();
+        }
 
-		@Override
-		public void nullSafeSet(PreparedStatement st, CustomId value, int position,
-								SharedSessionContractImplementor session) throws SQLException {
-			st.setObject( position, value.toString(), getSqlType() );
-		}
+        @Override
+        public void nullSafeSet(PreparedStatement st, CustomId value, int index,
+                                SharedSessionContractImplementor session) throws SQLException {
+            st.setObject( index, value.toString(), getSqlType() );
+        }
 
-		@Override
-		public CustomId nullSafeGet(ResultSet rs, int position, SharedSessionContractImplementor session)
-				throws SQLException {
-			String idValue = rs.getString( position );
-			return idValue != null ? fromStringValue( idValue ) : null;
-		}
+        @Override
+        public CustomId nullSafeGet(ResultSet rs, int position, SharedSessionContractImplementor session, Object owner) throws SQLException {
+            String idValue = rs.getString(position);
+            return idValue != null ? fromStringValue( idValue ) : null;
+        }
 
-		@Override
-		public CustomId fromStringValue(CharSequence sequence) throws HibernateException {
-			return new CustomId( sequence.toString() );
-		}
+        @Override
+        public CustomId fromStringValue(CharSequence sequence) throws HibernateException {
+            return new CustomId(sequence.toString());
+        }
 
-		@Override
-		public int getSqlType() {
-			return Types.VARCHAR;
-		}
+        @Override
+        public int getSqlType() {
+            return Types.VARCHAR;
+        }
 
-		@Override
-		public Class<CustomId> returnedClass() {
-			return CustomId.class;
-		}
+        @Override
+        public Class<CustomId> returnedClass() {
+            return CustomId.class;
+        }
 
-		@Override
-		public CustomId deepCopy(CustomId value) {
-			return new CustomId( value.getId() );
-		}
+        @Override
+        public CustomId deepCopy(CustomId value) {
+            return new CustomId(value.getId());
+        }
 
-		@Override
-		public boolean isMutable() {
-			return false;
-		}
+        @Override
+        public boolean isMutable() {
+            return false;
+        }
 
-		@Override
-		public boolean equals(CustomId x, CustomId y) {
-			return EnhancedUserType.super.equals( x, y );
-		}
-	}
+        @Override
+        public Serializable disassemble(CustomId value) {
+            return value;
+        }
 
-	static class CustomId implements Serializable {
-		private String id;
+        @Override
+        public CustomId assemble(Serializable cached, Object owner) {
+            return (CustomId) cached;
+        }
 
-		public CustomId() {
-		}
+        @Override
+        public boolean equals(CustomId x, CustomId y) {
+            return Objects.equals(x, y);
+        }
 
-		public CustomId(String id) {
-			this.id = id;
-		}
+        @Override
+        public int hashCode(CustomId x) {
+            return x != null ? x.hashCode() : 0;
+        }
 
-		@Override
-		public String toString() {
-			return id;
-		}
 
-		public String getId() {
-			return id;
-		}
+    }
 
-		public void setId(String id) {
-			this.id = id;
-		}
-	}
+    static class CustomId implements Serializable {
+        private String id;
+
+        public CustomId() {
+        }
+
+        public CustomId(String id) {
+            this.id = id;
+        }
+
+        @Override
+        public String toString() {
+            return id;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+    }
 }

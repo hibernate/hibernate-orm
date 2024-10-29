db3e2ca3f81a857376616393911c0738c96e10be
Gavin King <gavin@hibernate.org>
2024-10-29 17:16:31 +0100

    move TableMigrator to the SPI package
    I must have just made a mistake when I created this interface
    
    Signed-off-by: Gavin King <gavin@hibernate.org>

--- hibernate-core/src/main/java/org/hibernate/tool/schema/spi/TableMigrator.java
+++ hibernate-core/src/main/java/org/hibernate/tool/schema/spi/TableMigrator.java
@@ -2,7 +2,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later
  * Copyright Red Hat Inc. and Hibernate Authors
  */
-package org.hibernate.tool.schema.internal;
+package org.hibernate.tool.schema.spi;
 
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.model.relational.SqlStringGenerationContext;

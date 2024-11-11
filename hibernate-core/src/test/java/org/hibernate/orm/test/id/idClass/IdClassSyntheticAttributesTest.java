7cdab319fb4db00a9fbdc527992eeb3947eab591
marko-bekhta <marko.prykladna@gmail.com>
2024-11-11 13:23:59 +0100

    HHH-18841 Create `_identifierMapper` as a synthetic attribute

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/id/idClass/IdClassSyntheticAttributesTest.java
@@ -0,0 +1,38 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.orm.test.id.idClass;
+
+import org.hibernate.mapping.PersistentClass;
+import org.hibernate.testing.orm.junit.DomainModel;
+import org.hibernate.testing.orm.junit.DomainModelScope;
+import org.hibernate.testing.orm.junit.Jira;
+import org.hibernate.testing.orm.junit.SessionFactory;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DomainModel(
+        annotatedClasses = MyEntity.class
+)
+@SessionFactory
+public class IdClassSyntheticAttributesTest {
+
+    @Jira("https://hibernate.atlassian.net/browse/HHH-18841")
+    @Test
+    public void test(DomainModelScope scope) {
+        final PersistentClass entityBinding = scope.getDomainModel().getEntityBinding(MyEntity.class.getName());
+        assertThat(entityBinding.getProperties()).hasSize(2)
+                .anySatisfy(p -> {
+                    assertThat(p.isSynthetic()).isTrue();
+                    assertThat(p.getName()).isEqualTo("_identifierMapper");
+                })
+                .anySatisfy(p -> {
+                    assertThat(p.isSynthetic()).isFalse();
+                    assertThat(p.getName()).isEqualTo("notes");
+                });
+    }
+}

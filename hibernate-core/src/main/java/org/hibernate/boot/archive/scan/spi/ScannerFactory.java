3e1f6b52c646d969b79f1c250cc4abb3ae0b6db0
Steve Ebersole <steve@hibernate.org>
2024-11-13 14:13:08 -0600

    fix hibernate-scan-jandex definitions

--- hibernate-core/src/main/java/org/hibernate/boot/archive/scan/spi/ScannerFactory.java
+++ hibernate-core/src/main/java/org/hibernate/boot/archive/scan/spi/ScannerFactory.java
@@ -4,9 +4,23 @@
  */
 package org.hibernate.boot.archive.scan.spi;
 
+import org.hibernate.Incubating;
 import org.hibernate.boot.archive.spi.ArchiveDescriptorFactory;
+import org.hibernate.service.JavaServiceLoadable;
 import org.hibernate.service.Service;
 
+/**
+ * Support for scanning various sources to detect {@code managed resources}
+ * for a {@code persistence unit}.
+ *
+ */
+@Incubating
+@JavaServiceLoadable
 public interface ScannerFactory extends Service {
+	/**
+	 * Create a scanner
+	 * @param archiveDescriptorFactory
+	 * @return
+	 */
 	Scanner getScanner(ArchiveDescriptorFactory archiveDescriptorFactory);
 }

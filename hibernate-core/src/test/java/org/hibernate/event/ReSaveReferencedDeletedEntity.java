03cf943ec115e6b733670caa9c061c257c630c9a
Gavin King <gavin@hibernate.org>
2024-11-23 10:58:16 +0100

    Update hibernate-core/src/test/java/org/hibernate/event/ReSaveReferencedDeletedEntity.java

--- hibernate-core/src/test/java/org/hibernate/event/ReSaveReferencedDeletedEntity.java
+++ hibernate-core/src/test/java/org/hibernate/event/ReSaveReferencedDeletedEntity.java
@@ -15,7 +15,7 @@ public class ReSaveReferencedDeletedEntity extends BaseCoreFunctionalTestCase {
     }
 
     @Test
-    @TestForIssue(jiraKey = "HHH-14416")
+    @JiraKey("HHH-14416")
     public void testReSaveDeletedEntityWithReferences() {
         doInHibernate( this::sessionFactory, session -> {
             Parent parent = new Parent();

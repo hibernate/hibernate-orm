182b93ad135100babf6f4a2a4bda7f944d18229b
Čedomir Igaly <cedomir.igaly@e-dama.net>
2024-12-01 07:51:30 +0100

    HHH-18868 Adapted test case from https://hibernate.atlassian.net/browse/HHH-18868

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/metamodel/hhh18868/Summary.java
@@ -0,0 +1,11 @@
+/*
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.orm.test.metamodel.hhh18868;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Summary extends BaseSummary {
+}

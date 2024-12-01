2a6fa9b735c44e9593c5f719afeb1a44cc36f9a4
Čedomir Igaly <cedomir.igaly@e-dama.net>
2024-12-01 07:51:30 +0100

    HHH-18868 Adapted test case from https://hibernate.atlassian.net/browse/HHH-18868

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/metamodel/hhh18868/HHH18868Test.java
@@ -0,0 +1,34 @@
+/*
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.orm.test.metamodel.hhh18868;
+
+import jakarta.persistence.metamodel.SingularAttribute;
+import org.hibernate.testing.orm.junit.DomainModel;
+import org.hibernate.testing.orm.junit.JiraKey;
+import org.hibernate.testing.orm.junit.SessionFactory;
+import org.hibernate.testing.orm.junit.SessionFactoryScope;
+import org.junit.jupiter.api.Test;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DomainModel(annotatedClasses = {Summary.class, BaseSummary.class})
+@SessionFactory
+@JiraKey( "HHH-18858" )
+public class HHH18868Test {
+
+	@Test
+	public void test(SessionFactoryScope scope) {
+		scope.inSession( entityManager -> {
+			final var yearAttribute = Summary_.year.getDeclaringType().getAttribute( "year" );
+			assertThat( yearAttribute ).isEqualTo( Summary_.year );
+			assertThat( ((SingularAttribute<?, ?>) yearAttribute).isId() ).isTrue();
+
+			final var monthAttribute = Summary_.month.getDeclaringType().getAttribute( "month" );
+			assertThat( monthAttribute ).isEqualTo( Summary_.month );
+			assertThat( ((SingularAttribute<?, ?>) monthAttribute).isId() ).isTrue();
+		} );
+	}
+}

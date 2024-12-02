2c84f9eb333fc9fe56f977aba625631df45f6842
Čedomir Igaly <cedomir.igaly@zg.t-com.hr>
2024-12-02 21:27:46 +0100

    HHH-18858 Test case "borrowed" from Core, slightly adapted

---
+++ tooling/metamodel-generator/src/test/java/org/hibernate/processor/test/hhh18858/Competitor.java
@@ -0,0 +1,37 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
+package org.hibernate.processor.test.hhh18858;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+/**
+ * @author Emmanuel Bernard
+ */
+@Entity
+public class Competitor {
+	private int id;
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Id
+	@GeneratedValue
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}

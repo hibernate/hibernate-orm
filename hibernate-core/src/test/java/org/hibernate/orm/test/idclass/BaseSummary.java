a68c2f86ed3eef6f7b1a5696450842c1015f2c35
Čedomir Igaly <cedomir.igaly@zg.t-com.hr>
2024-12-05 12:22:59 +0100

    HHH-18868 Renamed test classes

--- hibernate-core/src/test/java/org/hibernate/orm/test/idclass/BaseSummary.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/idclass/BaseSummary.java
@@ -2,7 +2,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later
  * Copyright Red Hat Inc. and Hibernate Authors
  */
-package org.hibernate.orm.test.metamodel.hhh18868;
+package org.hibernate.orm.test.idclass;
 
 import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;

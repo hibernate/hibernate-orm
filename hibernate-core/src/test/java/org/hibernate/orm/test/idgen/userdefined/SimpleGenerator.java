a9b8fb316e55fef0401535dd1843560be91d5ed1
Yanming Zhou <zhouyanming@gmail.com>
2024-12-17 09:41:24 +0800

    HHH-18951 Support custom annotation attributes as configuration parameters of Generator

--- hibernate-core/src/test/java/org/hibernate/orm/test/idgen/userdefined/SimpleGenerator.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/idgen/userdefined/SimpleGenerator.java
@@ -5,18 +5,25 @@
 package org.hibernate.orm.test.idgen.userdefined;
 
 import java.util.EnumSet;
+import java.util.Properties;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.hibernate.MappingException;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.generator.BeforeExecutionGenerator;
 import org.hibernate.generator.EventType;
+import org.hibernate.id.Configurable;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.type.Type;
 
 /**
  * @author Yanming Zhou
  */
-public class SimpleGenerator implements BeforeExecutionGenerator {
+public class SimpleGenerator implements BeforeExecutionGenerator, Configurable {
 
 	private final AtomicLong sequence;
+
+	private long step = 1;
 
 	public SimpleGenerator(AtomicLong sequence) {
 		this.sequence = sequence;
@@ -28,11 +35,16 @@ public class SimpleGenerator implements BeforeExecutionGenerator {
 			Object owner,
 			Object currentValue,
 			EventType eventType) {
-		return sequence.getAndIncrement();
+		return sequence.getAndAdd(step);
 	}
 
 	@Override
 	public EnumSet<EventType> getEventTypes() {
 		return EnumSet.of( EventType.INSERT );
+	}
+
+	@Override
+	public void configure(Type type, Properties parameters, ServiceRegistry serviceRegistry) throws MappingException {
+		this.step = (Long) parameters.get("step");
 	}
 }

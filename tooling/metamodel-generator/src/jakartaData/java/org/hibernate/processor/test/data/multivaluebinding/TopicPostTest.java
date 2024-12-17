3ecedc425eda18cf3776f5d491cff5f90eff970e
Čedomir Igaly <cedomir.igaly@zg.t-com.hr>
2024-12-17 15:45:53 +0100

    HHH-18960 Test case - when named query parameter allows multiple value binding, instance of java.util.Collection should be allowed as parameter

---
+++ tooling/metamodel-generator/src/jakartaData/java/org/hibernate/processor/test/data/multivaluebinding/TopicPostTest.java
@@ -0,0 +1,53 @@
+/*
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.processor.test.data.multivaluebinding;
+
+import jakarta.persistence.EntityManager;
+import org.hibernate.processor.test.util.CompilationTest;
+import org.hibernate.processor.test.util.WithClasses;
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.List;
+
+import static org.hibernate.processor.test.util.TestUtil.assertMetamodelClassGeneratedFor;
+import static org.hibernate.processor.test.util.TestUtil.assertPresenceOfMethodInMetamodelFor;
+import static org.hibernate.processor.test.util.TestUtil.getMetaModelSourceAsString;
+import static org.hibernate.processor.test.util.TestUtil.getMethodFromMetamodelFor;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TopicPostTest extends CompilationTest {
+	@Test
+	@WithClasses({Post.class, PostRepository.class})
+	public void test() {
+		assertMetamodelClassGeneratedFor( Post.class, true );
+		assertMetamodelClassGeneratedFor( Post.class );
+		assertMetamodelClassGeneratedFor( PostRepository.class );
+
+		assertPresenceOfMethodInMetamodelFor( Post.class, "getPostsByName", EntityManager.class, List.class );
+		final Method method = getMethodFromMetamodelFor( Post.class, "getPostsByName", EntityManager.class, List.class );
+		final Type methodParam = method.getGenericParameterTypes()[1];
+		if ( methodParam instanceof ParameterizedType parameterized ) {
+			assertEquals( String.class, parameterized.getActualTypeArguments()[0] );
+		}
+		else {
+			fail();
+		}
+
+		assertPresenceOfMethodInMetamodelFor( PostRepository.class, "getPostsByName", Collection.class );
+		final Method repositoryMethod = getMethodFromMetamodelFor( PostRepository.class, "getPostsByName", Collection.class );
+		final Type repositoryMethodParam = repositoryMethod.getGenericParameterTypes()[0];
+		if ( repositoryMethodParam instanceof ParameterizedType parameterized ) {
+			assertEquals( String.class, parameterized.getActualTypeArguments()[0] );
+		}
+		else {
+			fail();
+		}
+	}
+}

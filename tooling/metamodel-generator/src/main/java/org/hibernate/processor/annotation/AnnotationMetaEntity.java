050089efcc04987180b98866d128344a489345ea
Čedomir Igaly <cedomir.igaly@zg.t-com.hr>
2024-12-17 15:41:09 +0100

    HHH-18960 If query parameter allows multiple value binding, accept List, Set, or Collection with parameter type as generic type

--- tooling/metamodel-generator/src/main/java/org/hibernate/processor/annotation/AnnotationMetaEntity.java
+++ tooling/metamodel-generator/src/main/java/org/hibernate/processor/annotation/AnnotationMetaEntity.java
@@ -64,6 +64,7 @@ import jakarta.persistence.AccessType;
 import static java.beans.Introspector.decapitalize;
 import static java.lang.Boolean.FALSE;
 import static java.util.Collections.emptyList;
+import static java.util.Objects.requireNonNullElse;
 import static java.util.stream.Collectors.toList;
 import static javax.lang.model.util.ElementFilter.fieldsIn;
 import static javax.lang.model.util.ElementFilter.methodsIn;
@@ -2738,11 +2739,13 @@ public class AnnotationMetaEntity extends AnnotationMeta {
 		if ( queryParamType!=null
 				//TODO: arguments of functions get assigned "unknown" which sucks
 				&& !"unknown".equals(queryParamType) ) {
+			final String realQueryParamType =
+					requireNonNullElse( context.qualifiedNameForEntityName( queryParamType ), queryParamType );
 			if ( param.getName() != null ) {
-				checkNamedParameter(param, paramNames, paramTypes, method, mirror, value, queryParamType);
+				checkNamedParameter(param, paramNames, paramTypes, method, mirror, value, realQueryParamType);
 			}
 			else if ( param.getPosition() != null ) {
-				checkOrdinalParameter(param, paramNames, paramTypes, method, mirror, value, queryParamType);
+				checkOrdinalParameter(param, paramNames, paramTypes, method, mirror, value, realQueryParamType);
 			}
 		}
 	}

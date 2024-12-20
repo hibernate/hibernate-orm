b25ba999ec22deabba36f661805e5fcde9274884
Gavin King <gavin@hibernate.org>
2024-12-20 19:44:16 +0100

    license headers and ws

--- hibernate-core/src/test/java/org/hibernate/orm/test/annotations/usertype/MyId.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/annotations/usertype/MyId.java
@@ -1,9 +1,13 @@
+/*
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
 package org.hibernate.orm.test.annotations.usertype;
 
 class MyId {
-    final String text;
+	final String text;
 
-    MyId(String text) {
-        this.text = text;
-    }
+	MyId(String text) {
+		this.text = text;
+	}
 }

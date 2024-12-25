40e52b197795b7720de5721a3822a43ef655a98e
Gavin King <gavin@hibernate.org>
2024-12-25 10:15:04 +0100

    HHH-18979 rename to ValueList
    and fix whitespace

--- hibernate-core/src/main/java/org/hibernate/query/Restriction.java
+++ hibernate-core/src/main/java/org/hibernate/query/Restriction.java
@@ -37,7 +37,6 @@ import org.hibernate.query.range.Range;
  * @author Gavin King
  *
  * @since 7.0
- *
  */
 @Incubating
 public interface Restriction<X> {
@@ -54,77 +53,77 @@ public interface Restriction<X> {
 	@Internal
 	Predicate toPredicate(Root<? extends X> root, CriteriaBuilder builder);
 
-	static <T,U> Restriction<T> restrict(SingularAttribute<T,U> attribute, Range<U> range) {
+	static <T, U> Restriction<T> restrict(SingularAttribute<T, U> attribute, Range<U> range) {
 		return new AttributeRange<>( attribute, range );
 	}
 
-	static <T,U> Restriction<T> equal(SingularAttribute<T,U> attribute, U value) {
-		return restrict( attribute, Range.singleValue(value) );
+	static <T, U> Restriction<T> equal(SingularAttribute<T, U> attribute, U value) {
+		return restrict( attribute, Range.singleValue( value ) );
 	}
 
-	static <T,U> Restriction<T> unequal(SingularAttribute<T,U> attribute, U value) {
-		return equal(attribute, value).negated();
+	static <T, U> Restriction<T> unequal(SingularAttribute<T, U> attribute, U value) {
+		return equal( attribute, value ).negated();
 	}
 
-	static <T> Restriction<T> equalIgnoringCase(SingularAttribute<T,String> attribute, String value) {
-		return restrict( attribute, Range.singleCaseInsensitiveValue(value) );
+	static <T> Restriction<T> equalIgnoringCase(SingularAttribute<T, String> attribute, String value) {
+		return restrict( attribute, Range.singleCaseInsensitiveValue( value ) );
 	}
 
-	static <T,U> Restriction<T> in(SingularAttribute<T,U> attribute, java.util.List<U> values) {
-		return restrict( attribute, Range.valueList(values) );
+	static <T, U> Restriction<T> in(SingularAttribute<T, U> attribute, java.util.List<U> values) {
+		return restrict( attribute, Range.valueList( values ) );
 	}
 
-	static <T,U> Restriction<T> notIn(SingularAttribute<T,U> attribute, java.util.List<U> values) {
-		return in(attribute, values).negated();
+	static <T, U> Restriction<T> notIn(SingularAttribute<T, U> attribute, java.util.List<U> values) {
+		return in( attribute, values ).negated();
 	}
 
-	static <T,U extends Comparable<U>> Restriction<T> between(SingularAttribute<T,U> attribute, U lowerBound, U upperBound) {
-		return restrict( attribute, Range.closed(lowerBound, upperBound) );
+	static <T, U extends Comparable<U>> Restriction<T> between(SingularAttribute<T, U> attribute, U lowerBound, U upperBound) {
+		return restrict( attribute, Range.closed( lowerBound, upperBound ) );
 	}
 
-	static <T,U extends Comparable<U>> Restriction<T> notBetween(SingularAttribute<T,U> attribute, U lowerBound, U upperBound) {
-		return between(attribute, lowerBound, upperBound).negated();
+	static <T, U extends Comparable<U>> Restriction<T> notBetween(SingularAttribute<T, U> attribute, U lowerBound, U upperBound) {
+		return between( attribute, lowerBound, upperBound ).negated();
 	}
 
-	static <T,U extends Comparable<U>> Restriction<T> greaterThan(SingularAttribute<T,U> attribute, U lowerBound) {
-		return restrict( attribute, Range.greaterThan(lowerBound) );
+	static <T, U extends Comparable<U>> Restriction<T> greaterThan(SingularAttribute<T, U> attribute, U lowerBound) {
+		return restrict( attribute, Range.greaterThan( lowerBound ) );
 	}
 
-	static <T,U extends Comparable<U>> Restriction<T> lessThan(SingularAttribute<T,U> attribute, U upperBound) {
-		return restrict( attribute, Range.lessThan(upperBound) );
+	static <T, U extends Comparable<U>> Restriction<T> lessThan(SingularAttribute<T, U> attribute, U upperBound) {
+		return restrict( attribute, Range.lessThan( upperBound ) );
 	}
 
-	static <T,U extends Comparable<U>> Restriction<T> greaterThanOrEqual(SingularAttribute<T,U> attribute, U lowerBound) {
-		return restrict( attribute, Range.greaterThanOrEqualTo(lowerBound) );
+	static <T, U extends Comparable<U>> Restriction<T> greaterThanOrEqual(SingularAttribute<T, U> attribute, U lowerBound) {
+		return restrict( attribute, Range.greaterThanOrEqualTo( lowerBound ) );
 	}
 
-	static <T,U extends Comparable<U>> Restriction<T> lessThanOrEqual(SingularAttribute<T,U> attribute, U upperBound) {
-		return restrict( attribute, Range.lessThanOrEqualTo(upperBound) );
+	static <T, U extends Comparable<U>> Restriction<T> lessThanOrEqual(SingularAttribute<T, U> attribute, U upperBound) {
+		return restrict( attribute, Range.lessThanOrEqualTo( upperBound ) );
 	}
 
-	static <T> Restriction<T> like(SingularAttribute<T,String> attribute, String pattern, boolean caseSensitive) {
-		return restrict( attribute, Range.pattern(pattern, caseSensitive) );
+	static <T> Restriction<T> like(SingularAttribute<T, String> attribute, String pattern, boolean caseSensitive) {
+		return restrict( attribute, Range.pattern( pattern, caseSensitive ) );
 	}
 
-	static <T> Restriction<T> like(SingularAttribute<T,String> attribute, String pattern) {
-		return like(attribute, pattern, true);
+	static <T> Restriction<T> like(SingularAttribute<T, String> attribute, String pattern) {
+		return like( attribute, pattern, true );
 	}
 
-	static <T> Restriction<T> notLike(SingularAttribute<T,String> attribute, String pattern) {
-		return like(attribute, pattern, true).negated();
+	static <T> Restriction<T> notLike(SingularAttribute<T, String> attribute, String pattern) {
+		return like( attribute, pattern, true ).negated();
 	}
 
-	static <T> Restriction<T> notLike(SingularAttribute<T,String> attribute, String pattern, boolean caseSensitive) {
-		return like(attribute, pattern, caseSensitive).negated();
+	static <T> Restriction<T> notLike(SingularAttribute<T, String> attribute, String pattern, boolean caseSensitive) {
+		return like( attribute, pattern, caseSensitive ).negated();
 	}
 
 	@SafeVarargs
 	static <T> Restriction<T> and(Restriction<T>... restrictions) {
-		return new Conjunction<>( java.util.List.of(restrictions) );
+		return new Conjunction<>( java.util.List.of( restrictions ) );
 	}
 
 	@SafeVarargs
 	static <T> Restriction<T> or(Restriction<T>... restrictions) {
-		return new Disjunction<>( java.util.List.of(restrictions) );
+		return new Disjunction<>( java.util.List.of( restrictions ) );
 	}
 }

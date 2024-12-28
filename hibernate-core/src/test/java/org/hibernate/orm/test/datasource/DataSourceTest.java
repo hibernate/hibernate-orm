0ca417b064c783291eee21797b31f15db2be9b0e
Gavin King <gavin@hibernate.org>
2024-12-28 14:01:50 +0100

    HHH-18989 disable test for DB2 and for Oracle autonomous

--- hibernate-core/src/test/java/org/hibernate/orm/test/datasource/DataSourceTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/datasource/DataSourceTest.java
@@ -8,12 +8,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import org.hibernate.cfg.Environment;
 import org.hibernate.cfg.JdbcSettings;
+import org.hibernate.dialect.DB2Dialect;
+import org.hibernate.dialect.OracleDialect;
 import org.hibernate.internal.log.ConnectionInfoLogger;
 import org.hibernate.testing.logger.LogInspectionHelper;
 import org.hibernate.testing.logger.LogListener;
 import org.hibernate.testing.orm.junit.EntityManagerFactoryScope;
 import org.hibernate.testing.orm.junit.Jpa;
 import org.hibernate.testing.orm.junit.Setting;
+import org.hibernate.testing.orm.junit.SkipForDialect;
 import org.jboss.logging.Logger;
 import org.junit.jupiter.api.Test;
 
@@ -25,6 +28,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @Jpa(annotatedClasses = DataSourceTest.TestEntity.class,
 		integrationSettings = @Setting(name = JdbcSettings.CONNECTION_PROVIDER,
 				value = "org.hibernate.orm.test.datasource.TestDataSourceConnectionProvider"))
+@SkipForDialect(dialectClass = DB2Dialect.class)
 public class DataSourceTest {
 	@Test
 	void test(EntityManagerFactoryScope scope) {
@@ -32,7 +36,8 @@ public class DataSourceTest {
 		LogInspectionHelper.registerListener( listener, ConnectionInfoLogger.INSTANCE );
 		scope.getEntityManagerFactory();
 		LogInspectionHelper.clearAllListeners( ConnectionInfoLogger.INSTANCE );
-		assertTrue( listener.seen );
+		assertTrue( scope.getDialect() instanceof OracleDialect dialect && dialect.isAutonomous()
+					|| listener.seen );
 	}
 
 	@Entity(name="TestEntity")
@@ -47,9 +52,8 @@ public class DataSourceTest {
 		@Override
 		public void loggedEvent(Logger.Level level, String renderedMessage, Throwable thrown) {
 			if ( renderedMessage.contains( "Database info:" ) ) {
-				seen = true;
-				final Properties properties = Environment.getProperties();
-				assertTrue( renderedMessage.contains( split( ";", properties.getProperty(JdbcSettings.URL) )[0] ) );
+				final String url = Environment.getProperties().getProperty( JdbcSettings.URL );
+				seen = renderedMessage.contains( split( ";", url )[0] );
 			}
 		}
 	}

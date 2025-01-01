d4b7aa690c90a33259e5f508e7d287b2f1f39ea3
Čedomir Igaly <cedomir.igaly@zg.t-com.hr>
2025-01-01 17:41:42 +0100

    HHH-18988 Adapted test case from Jira issue https://hibernate.atlassian.net/browse/HHH-18988

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/columndiscriminator/Book.java
@@ -0,0 +1,32 @@
+/*
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.orm.test.columndiscriminator;
+
+public class Book {
+	private Long id;
+	private String title;
+	private BookDetails details;
+
+	public Book(String title, BookDetails details) {
+		this.title = title;
+		this.details = details;
+	}
+
+	protected Book() {
+		// default
+	}
+
+	public Long id() {
+		return id;
+	}
+
+	public String title() {
+		return title;
+	}
+
+	public BookDetails details() {
+		return details;
+	}
+}

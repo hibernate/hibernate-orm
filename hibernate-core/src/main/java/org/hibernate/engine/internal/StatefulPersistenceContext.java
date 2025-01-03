a920bf1a300b44437a7d95e1254b9e24f236ea13
Christian Beikov <christian.beikov@gmail.com>
2025-01-03 15:31:56 +0100

    HHH-2862 Throw error instead of replacing collection with same key

--- hibernate-core/src/main/java/org/hibernate/engine/internal/StatefulPersistenceContext.java
+++ hibernate-core/src/main/java/org/hibernate/engine/internal/StatefulPersistenceContext.java
@@ -1090,13 +1090,9 @@ public class StatefulPersistenceContext implements PersistenceContext {
 			if ( old == coll ) {
 				throw new AssertionFailure( "bug adding collection twice" );
 			}
-			// or should it actually throw an exception?
-			old.unsetSession( session );
-			if ( collectionEntries != null ) {
-				collectionEntries.remove( old );
-			}
-			// watch out for a case where old is still referenced
-			// somewhere in the object graph! (which is a user error)
+			throw new HibernateException(
+					"Found shared references to a collection: " + collectionKey.getRole()
+			);
 		}
 	}
 

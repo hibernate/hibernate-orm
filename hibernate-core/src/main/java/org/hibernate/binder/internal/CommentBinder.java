3344f3a1d19faf350c1596a324c0b11c3222dd38
Gavin King <gavin@hibernate.org>
2025-01-05 20:45:54 +0100

    HHH-19011 two extra fixes
    - make 'on' work properly for foreign keys
    - throw when no column name matches 'on'

--- hibernate-core/src/main/java/org/hibernate/binder/internal/CommentBinder.java
+++ hibernate-core/src/main/java/org/hibernate/binder/internal/CommentBinder.java
@@ -40,21 +40,25 @@ public class CommentBinder implements AttributeBinder<Comment>, TypeBinder<Comme
 			if ( on.isEmpty() || table.getName().equalsIgnoreCase( on ) ) {
 				table.setComment( text );
 			}
-			// but if 'on' is explicit, it can go on a column
-			Value element = collection.getElement();
-			for ( Column column : element.getColumns() ) {
-				if ( column.getName().equalsIgnoreCase( on ) ) {
-					column.setComment( text );
+			else {
+				// but if 'on' is explicit, it can go on a column
+				for ( Column column : table.getColumns() ) {
+					if ( column.getName().equalsIgnoreCase( on ) ) {
+						column.setComment( text );
+						return;
+					}
 				}
+				throw new AnnotationException( "No matching column for '@Comment(on=\"" + on + "\")'" );
 			}
-			//TODO: list index / map key columns
 		}
 		else {
 			for ( Column column : value.getColumns() ) {
 				if ( on.isEmpty() || column.getName().equalsIgnoreCase( on ) ) {
 					column.setComment( text );
+					return;
 				}
 			}
+			throw new AnnotationException( "No matching column for '@Comment(on=\"" + on + "\")'" );
 		}
 	}
 
@@ -67,12 +71,16 @@ public class CommentBinder implements AttributeBinder<Comment>, TypeBinder<Comme
 		if ( on.isEmpty() || primary.getName().equalsIgnoreCase( on ) ) {
 			primary.setComment( text );
 		}
-		// but if 'on' is explicit, it can go on a secondary table
-		for ( Join join : entity.getJoins() ) {
-			Table secondary = join.getTable();
-			if ( secondary.getName().equalsIgnoreCase( on ) ) {
-				secondary.setComment( text );
+		else {
+			// but if 'on' is explicit, it can go on a secondary table
+			for ( Join join : entity.getJoins() ) {
+				Table secondary = join.getTable();
+				if ( secondary.getName().equalsIgnoreCase( on ) ) {
+					secondary.setComment( text );
+					return;
+				}
 			}
+			throw new AnnotationException( "No matching column for '@Comment(on=\"" + on + "\")'" );
 		}
 	}
 

b2a8062ad35c83db87d7815fa51e0a8878b1fafa
Gavin King <gavin@hibernate.org>
2025-01-05 19:03:37 +0100

    javadoc for creation contexts

--- hibernate-core/src/main/java/org/hibernate/query/sqm/spi/SqmCreationContext.java
+++ hibernate-core/src/main/java/org/hibernate/query/sqm/spi/SqmCreationContext.java
@@ -13,31 +13,59 @@ import org.hibernate.query.sqm.NodeBuilder;
 
 /**
  * The context in which all SQM creations occur.
+ * <p>
+ * Since we need to be able to parse and type check queries completely
+ * outside the usual lifecycle of a Hibernate {@code SessionFactory},
+ * it's extremely important that code which builds SQM trees does not
+ * access the factory or other services or object not exposed by this
+ * context object.
  *
  * @author Steve Ebersole
  */
 @Incubating
 public interface SqmCreationContext extends BindingContext {
+
+	/**
+	 * The {@link QueryEngine}.
+	 */
 	QueryEngine getQueryEngine();
 
+	/**
+	 * The {@link NodeBuilder}.
+	 */
 	default NodeBuilder getNodeBuilder() {
 		return getQueryEngine().getCriteriaBuilder();
 	}
 
 	/**
+	 * Obtain a Java class object with the given fully-qualified
+	 * name. This method may only be used for unmanaged types,
+	 * for example, for {@code select new}, or for references to
+	 * {@code static final} constants or to {@code enum} values.
+	 *
 	 * @apiNote Avoid calling this method, since {@link Class}
 	 *          objects are not available to the query validator
-	 *          in Hibernate Processor at compilation time.
+	 *          in Hibernate Processor at compilation time. If
+	 *          you must call it, be prepared to robustly handle
+	 *          the case in which the class is not present, in
+	 *          which case this method might return something
+	 *          arbitrary like {@code Object[].class}.
 	 */
 	default Class<?> classForName(String className) {
 		return getQueryEngine().getClassLoaderService().classForName( className );
 	}
 
+	/**
+	 * The {@link MappingMetamodel}.
+	 */
 	@Override
 	default MappingMetamodel getMappingMetamodel() {
 		return getQueryEngine().getMappingMetamodel();
 	}
 
+	/**
+	 * The {@link JpaMetamodel}.
+	 */
 	@Override
 	default JpaMetamodel getJpaMetamodel() {
 		return getQueryEngine().getJpaMetamodel();

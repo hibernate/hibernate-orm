7a9d888413c1d31242b9f1873a12032bc28d0b4f
Emmanuel Jannetti <emmanuel.jannetti@oracle.com>
2025-01-07 17:52:28 +0100

    HHH-17404 : use of BigDecimal in case of DECIMAL OSON event received in document handler

--- hibernate-core/src/main/java/org/hibernate/type/format/jackson/JacksonOsonFormatMapper.java
+++ hibernate-core/src/main/java/org/hibernate/type/format/jackson/JacksonOsonFormatMapper.java
@@ -131,13 +131,8 @@ public class JacksonOsonFormatMapper extends JacksonJsonFormatMapper {
 					handler.onNullValue();
 					break;
 				case OracleJsonParser.Event.VALUE_DECIMAL:
-					if (osonParser.isIntegralNumber()) {
-						((ObjectArrayOsonDocumentHandler)handler).onOsonValue(
-								osonParser.getInt());
-					} else {
-						((ObjectArrayOsonDocumentHandler)handler).onOsonValue(
-								osonParser.getFloat());
-					}
+					((ObjectArrayOsonDocumentHandler)handler).onOsonValue(
+							osonParser.getBigDecimal());
 					break;
 				case OracleJsonParser.Event.VALUE_DOUBLE:
 					((ObjectArrayOsonDocumentHandler)handler).onOsonValue(
@@ -186,7 +181,7 @@ public class JacksonOsonFormatMapper extends JacksonJsonFormatMapper {
 
 		consumeOsonTokens(osonParser, osonParser.next(), handler);
 
-		return (T)handler.getMappedObjectArray();
+		return (T)handler.getObjectArray();
 	}
 
 	@Override

5fa352c05e3d93f55a5304c181b3b8c095f364f8
Gavin King <gavin@hibernate.org>
2025-01-10 21:28:09 +0100

    HHH-18964 remove SunOneJtaPlatform

--- hibernate-core/src/main/java/org/hibernate/boot/registry/selector/internal/DefaultJtaPlatformSelector.java
+++ hibernate-core/src/main/java/org/hibernate/boot/registry/selector/internal/DefaultJtaPlatformSelector.java
@@ -10,7 +10,6 @@ import org.hibernate.engine.transaction.jta.platform.internal.AtomikosJtaPlatfor
 import org.hibernate.engine.transaction.jta.platform.internal.JBossAppServerJtaPlatform;
 import org.hibernate.engine.transaction.jta.platform.internal.JBossStandAloneJtaPlatform;
 import org.hibernate.engine.transaction.jta.platform.internal.ResinJtaPlatform;
-import org.hibernate.engine.transaction.jta.platform.internal.SunOneJtaPlatform;
 import org.hibernate.engine.transaction.jta.platform.internal.WebSphereExtendedJtaPlatform;
 import org.hibernate.engine.transaction.jta.platform.internal.WebSphereJtaPlatform;
 import org.hibernate.engine.transaction.jta.platform.internal.WebSphereLibertyJtaPlatform;
@@ -56,7 +55,6 @@ public class DefaultJtaPlatformSelector implements LazyServiceResolver<JtaPlatfo
 		return switch ( name ) {
 			case "Atomikos" -> AtomikosJtaPlatform.class;
 			case "Resin" -> ResinJtaPlatform.class;
-			case "SunOne" -> SunOneJtaPlatform.class;
 			case null, default -> null;
 		};
 	}

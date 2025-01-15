3dc18c0b8bd1d55fad27d14baf623f2db8eca501
Bidyadhar Mohanty <bidyadhar.mohanty@oracle.com>
2025-01-15 17:35:22 +0530

    HHH-17404 : Rollback Test Entity changes and JsonMapping tests fix.

--- hibernate-core/src/test/java/org/hibernate/orm/test/mapping/embeddable/NestedJsonEmbeddableTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/mapping/embeddable/NestedJsonEmbeddableTest.java
@@ -88,7 +88,6 @@ public class NestedJsonEmbeddableTest extends BaseSessionFactoryFunctionalTest {
 
 	@Test
 	public void testFetch() {
-
 		sessionFactoryScope().inSession(
 				entityManager -> {
 					List<JsonHolder> jsonHolders = entityManager.createQuery( "from JsonHolder b where b.id = 1", JsonHolder.class ).getResultList();
@@ -454,30 +453,6 @@ public class NestedJsonEmbeddableTest extends BaseSessionFactoryFunctionalTest {
 		public TheJson() {
 		}
 
-		public String getStringField() {
-			return stringField;
-		}
-
-		public void setStringField(String stringField) {
-			this.stringField = stringField;
-		}
-
-		public SimpleEmbeddable getSimpleEmbeddable() {
-			return simpleEmbeddable;
-		}
-
-		public void setSimpleEmbeddable(SimpleEmbeddable simpleEmbeddable) {
-			this.simpleEmbeddable = simpleEmbeddable;
-		}
-
-		public EmbeddableAggregate getNested() {
-			return nested;
-		}
-
-		public void setNested(EmbeddableAggregate nested) {
-			this.nested = nested;
-		}
-
 		public TheJson(String stringField, Integer integerField, String leaf, EmbeddableAggregate nested) {
 			this.stringField = stringField;
 			this.simpleEmbeddable = new SimpleEmbeddable( integerField, leaf );
@@ -496,22 +471,6 @@ public class NestedJsonEmbeddableTest extends BaseSessionFactoryFunctionalTest {
 		private Integer integerField;
 		@JdbcTypeCode(SqlTypes.JSON)
 		private DoubleNested doubleNested;
-
-		public Integer getIntegerField() {
-			return integerField;
-		}
-
-		public void setIntegerField(Integer integerField) {
-			this.integerField = integerField;
-		}
-
-		public DoubleNested getDoubleNested() {
-			return doubleNested;
-		}
-
-		public void setDoubleNested(DoubleNested doubleNested) {
-			this.doubleNested = doubleNested;
-		}
 
 		public SimpleEmbeddable() {
 		}
@@ -558,14 +517,6 @@ public class NestedJsonEmbeddableTest extends BaseSessionFactoryFunctionalTest {
 			this.theNested = new Nested( leaf );
 		}
 
-		public Nested getTheNested() {
-			return theNested;
-		}
-
-		public void setTheNested(Nested theNested) {
-			this.theNested = theNested;
-		}
-
 		@Override
 		public boolean equals(Object o) {
 			if ( this == o ) {
@@ -598,14 +549,6 @@ public class NestedJsonEmbeddableTest extends BaseSessionFactoryFunctionalTest {
 			this.theLeaf = new Leaf( stringField );
 		}
 
-		public Leaf getTheLeaf() {
-			return theLeaf;
-		}
-
-		public void setTheLeaf(Leaf theLeaf) {
-			this.theLeaf = theLeaf;
-		}
-
 		@Override
 		public boolean equals(Object o) {
 			if ( this == o ) {
@@ -634,14 +577,6 @@ public class NestedJsonEmbeddableTest extends BaseSessionFactoryFunctionalTest {
 		}
 
 		public Leaf(String stringField) {
-			this.stringField = stringField;
-		}
-
-		public String getStringField() {
-			return stringField;
-		}
-
-		public void setStringField(String stringField) {
 			this.stringField = stringField;
 		}
 

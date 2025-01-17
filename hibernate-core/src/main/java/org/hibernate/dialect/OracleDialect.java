18dbf171cade6a77e6a9a269b591a805c3c9117f
Emmanuel Jannetti <emmanuel.jannetti@oracle.com>
2025-01-17 09:38:31 +0100

    HHH-17404 : fix aggregatesupport for "no-extension" case

--- hibernate-core/src/main/java/org/hibernate/dialect/OracleDialect.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/OracleDialect.java
@@ -1066,8 +1066,7 @@ public class OracleDialect extends Dialect {
 
 	@Override
 	public AggregateSupport getAggregateSupport() {
-		return OracleAggregateSupport.valueOf( this ,
-				!JacksonIntegration.isOracleOsonExtensionAvailable());
+		return OracleAggregateSupport.valueOf( this ,OracleOsonExtensionUsed);
 	}
 
 	@Override

d30ac942e56eb5e6b8f70b7c6c2aa39e19d104bc
Emmanuel Jannetti <emmanuel.jannetti@oracle.com>
2025-01-17 10:38:27 +0100

    HHH-17404 : fix condition on aggregate support load

--- hibernate-core/src/main/java/org/hibernate/dialect/OracleDialect.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/OracleDialect.java
@@ -1098,7 +1098,7 @@ public class OracleDialect extends Dialect {
 
 	@Override
 	public AggregateSupport getAggregateSupport() {
-		return OracleAggregateSupport.valueOf( this ,OracleOsonExtensionUsed);
+		return OracleAggregateSupport.valueOf( this ,!OracleOsonExtensionUsed);
 	}
 
 	@Override

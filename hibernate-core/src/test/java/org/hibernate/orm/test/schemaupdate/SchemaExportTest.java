13b447dbbc882ba9e1d1eab4606251d8b891f5fe
Steve Ebersole <steve@hibernate.org>
2025-02-04 17:08:56 -0600

    Various code cleanup

--- hibernate-core/src/test/java/org/hibernate/orm/test/schemaupdate/SchemaExportTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/schemaupdate/SchemaExportTest.java
@@ -105,25 +105,6 @@ public class SchemaExportTest extends BaseUnitTestCase {
 	}
 
 	@Test
-	public void testGenerateDdlToFile() {
-		final SchemaExport schemaExport = new SchemaExport();
-
-		File outFile = new File("schema.ddl");
-		schemaExport.setOutputFile( outFile.getPath() );
-
-		// do not script to console or export to database
-		schemaExport.execute( EnumSet.of( TargetType.SCRIPT ), SchemaExport.Action.DROP, metadata );
-		if ( doesDialectSupportDropTableIfExist() && schemaExport.getExceptions().size() > 0 ) {
-			assertEquals( 2, schemaExport.getExceptions().size() );
-		}
-		assertTrue( outFile.exists() );
-
-		//check file is not empty
-		assertTrue( outFile.length() > 0 );
-		outFile.delete();
-	}
-
-	@Test
 	public void testCreateAndDrop() {
 		final SchemaExport schemaExport = new SchemaExport();
 

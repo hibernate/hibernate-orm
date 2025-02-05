26eed585c4df7119a74ed39838314c64a35f4d88
Steve Ebersole <steve@hibernate.org>
2025-02-05 12:42:07 -0600

    Various code cleanup

---
+++ hibernate-core/src/test/java/org/hibernate/bytecode/internal/bytebuddy/BaseMappedSuperclass.java
@@ -0,0 +1,31 @@
+/*
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.bytecode.internal.bytebuddy;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class BaseMappedSuperclass {
+	@Id
+	protected Long id;
+	protected String value;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+}

cc76f759528344513e5fbedb74ba9428963e7e4e
Marco Belladelli <marcobladel@gmail.com>
2025-02-06 13:42:12 +0100

    HHH-19070 HHH-18326 Reset entity instance ids, use `setPersistenceInfo`

--- hibernate-core/src/main/java/org/hibernate/internal/util/collections/InstanceIdentityStore.java
+++ hibernate-core/src/main/java/org/hibernate/internal/util/collections/InstanceIdentityStore.java
@@ -7,10 +7,15 @@ package org.hibernate.internal.util.collections;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.hibernate.engine.spi.InstanceIdentity;
 
+import java.util.ConcurrentModificationException;
+
 /**
  * Utility collection that takes advantage of {@link InstanceIdentity}'s identifier to store objects.
  * The store is based on {@link AbstractPagedArray} and it stores element using their instance-id
  * as index.
+ * <p>
+ * Instance ids are considered to start from 1, and if two instances are found to have the
+ * same identifier a {@link java.util.ConcurrentModificationException will be thrown}.
  * <p>
  * Both keys and values are stored in this array, requiring very few allocations to keep track of the pair.
  * The downside to this is we cannot easily access the key, especially if asking for a specific type, since
@@ -26,7 +31,7 @@ public class InstanceIdentityStore<V> extends AbstractPagedArray<Object> {
 	 * @return the index of the corresponding key instance in the array
 	 */
 	private static int toKeyIndex(int instanceId) {
-		return instanceId * 2;
+		return (instanceId - 1) * 2;
 	}
 
 	/**
@@ -41,6 +46,10 @@ public class InstanceIdentityStore<V> extends AbstractPagedArray<Object> {
 	 * equality check ({@code ==}) with the provided key to ensure it corresponds to the mapped one
 	 */
 	public @Nullable V get(int instanceId, Object key) {
+		if ( instanceId <= 0 ) {
+			return null;
+		}
+
 		final int keyIndex = toKeyIndex( instanceId );
 		final Page<Object> page = getPage( keyIndex );
 		if ( page != null ) {
@@ -49,6 +58,12 @@ public class InstanceIdentityStore<V> extends AbstractPagedArray<Object> {
 			if ( k == key ) {
 				//noinspection unchecked
 				return (V) page.get( offset + 1 );
+			}
+			else {
+				throw new ConcurrentModificationException(
+						"Found a different instance corresponding to instanceId [" + instanceId +
+						"], this might indicate a concurrent access to this persistence context."
+				);
 			}
 		}
 		return null;
@@ -61,12 +76,14 @@ public class InstanceIdentityStore<V> extends AbstractPagedArray<Object> {
 	 * @param key key with which the specified value is to be associated
 	 * @param value value to be associated with the specified key
 	 */
-	public <K extends InstanceIdentity> void put(K key, V value) {
+	public void put(Object key, int instanceId, V value) {
 		if ( key == null ) {
 			throw new NullPointerException( "This store does not support null keys" );
 		}
+		else if ( instanceId <= 0 ) {
+			throw new IllegalArgumentException( "Instance ID must be a positive value" );
+		}
 
-		final int instanceId = key.$$_hibernate_getInstanceId();
 		final int keyIndex = toKeyIndex( instanceId );
 		final Page<Object> page = getOrCreateEntryPage( keyIndex );
 		final int pageOffset = toPageOffset( keyIndex );
@@ -83,6 +100,10 @@ public class InstanceIdentityStore<V> extends AbstractPagedArray<Object> {
 	 * equality check ({@code ==}) with the provided key to ensure it corresponds to the mapped one
 	 */
 	public void remove(int instanceId, Object key) {
+		if ( instanceId <= 0 ) {
+			return;
+		}
+
 		final int keyIndex = toKeyIndex( instanceId );
 		final Page<Object> page = getPage( keyIndex );
 		if ( page != null ) {
@@ -93,8 +114,10 @@ public class InstanceIdentityStore<V> extends AbstractPagedArray<Object> {
 				page.set( pageOffset + 1, null );
 			}
 			else {
-				// If it doesn't, reset the array value to the old key
-				page.set( pageOffset, k );
+				throw new ConcurrentModificationException(
+						"Found a different instance corresponding to instanceId [" + instanceId +
+						"], this might indicate a concurrent access to this persistence context."
+				);
 			}
 		}
 	}

76f2232eb5790a1705518c00772f7563e536766b
marko-bekhta <marko.prykladna@gmail.com>
2025-02-07 16:08:34 +0100

    HBX-2840 Prevent Hibernate ORM from trying to load component classes

--- tooling/hibernate-reveng/src/main/java/org/hibernate/tool/internal/reveng/util/EnhancedComponent.java
+++ tooling/hibernate-reveng/src/main/java/org/hibernate/tool/internal/reveng/util/EnhancedComponent.java
@@ -38,4 +38,11 @@ public class EnhancedComponent extends Component implements EnhancedValue {
 		return genStrategy;
 	}
 
+	@Override
+	public Class<?> getComponentClass() throws MappingException {
+		// we prevent ORM from trying to load a component class by name,
+		// since at the point when we are building these, a corresponding class is not yet created
+		// (so can't even think about it being compiled and able to load via any classloader) ...
+		return Object.class;
+	}
 }

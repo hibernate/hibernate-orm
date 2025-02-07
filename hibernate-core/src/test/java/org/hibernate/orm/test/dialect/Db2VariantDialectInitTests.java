cf41e60d067e78f6152a19041dcdfe2c2151ffc2
Steve Ebersole <steve@hibernate.org>
2025-02-07 12:39:14 -0600

    Various code cleanup

--- hibernate-core/src/test/java/org/hibernate/orm/test/dialect/Db2VariantDialectInitTests.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/dialect/Db2VariantDialectInitTests.java
@@ -5,23 +5,26 @@
 package org.hibernate.orm.test.dialect;
 
 import org.hibernate.dialect.DB2iDialect;
-import org.hibernate.testing.RequiresDialect;
+import org.hibernate.dialect.DB2zDialect;
 import org.hibernate.testing.orm.junit.JiraKey;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
 
 /**
- * @author Onno Goczol
+ * @author Steve Ebersole
  */
 @JiraKey(value = "HHH-15046")
-@RequiresDialect(DB2iDialect.class)
-public class DB2iDialectInitTestCase {
-
+public class Db2VariantDialectInitTests {
 	@Test
-	public void testInitUniqueDelegate() {
-		final var db2iDialect = new DB2iDialect();
-		assertNotNull(db2iDialect);
+	public void testDB2zDialectInit() {
+		final var db2zDialect = new DB2zDialect();
+		assertNotNull( db2zDialect );
 	}
 
+	@Test
+	public void testDB2iDialectInit() {
+		final var db2iDialect = new DB2iDialect();
+		assertNotNull( db2iDialect );
+	}
 }

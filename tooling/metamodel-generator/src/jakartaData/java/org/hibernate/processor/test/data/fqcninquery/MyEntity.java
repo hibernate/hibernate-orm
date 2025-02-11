8a4acbdf6edf5d172f835479ea9c5815321c09a4
Čedomir Igaly <cedomir.igaly@zg.t-com.hr>
2025-02-11 16:33:37 +0100

    HHH-19130 Test case - query using FQCN in JPA @NamedQuery and Jakarta Data @Query

---
+++ tooling/metamodel-generator/src/jakartaData/java/org/hibernate/processor/test/data/fqcninquery/MyEntity.java
@@ -0,0 +1,24 @@
+/*
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.processor.test.data.fqcninquery;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+
+@Entity
+@NamedQuery(name = "#getName",
+		query = "select name from org.hibernate.processor.test.data.fqcninquery.MyEntity where id=:id")
+@NamedQuery(name = "#getUniqueId",
+		query = "select uniqueId from MyEntity where name=:name")
+public class MyEntity {
+
+	@Id
+	Integer id;
+
+	String name;
+
+	String uniqueId;
+}

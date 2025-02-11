4592c417eba1ab4f46a36b4ed56a63102cc7400c
Bidyadhar Mohanty <bidyadhar.mohanty@oracle.com>
2025-02-11 15:55:52 +0530

    HHH-17404- Remove DURATION mapping to INTERVALDS

--- hibernate-core/src/main/java/org/hibernate/dialect/OracleDialect.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/OracleDialect.java
@@ -1068,9 +1068,6 @@ public class OracleDialect extends Dialect {
 		// Oracle requires a custom binder for binding untyped nulls with the NULL type
 		typeContributions.contributeJdbcType( NullJdbcType.INSTANCE );
 		typeContributions.contributeJdbcType( ObjectNullAsNullTypeJdbcType.INSTANCE );
-		// Oracle Stores the duration is ISO-8601 format.
-		typeContributions.contributeJdbcType( OracleDurationJdbcType.INSTANCE );
-		typeContributions.contributeJavaType( OracleDurationJavaType.INSTANCE );
 
 		// Until we remove StandardBasicTypes, we have to keep this
 		typeContributions.contributeType(

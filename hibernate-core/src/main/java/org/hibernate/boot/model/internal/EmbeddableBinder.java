11ef49037eeb17821a06be529bf145327fe2e130
Čedomir Igaly <cedomir.igaly@zg.t-com.hr>
2025-02-22 07:46:45 +0100

    HHH-19195 Discriminator values should be hierarchically sorted.           While collecting they are inserted in proper order, but TreeMap will           not preserver ordering, but sort them alphabetically by names.           To keep values order LinkedHashMap should be used instead of TreeMap.

--- hibernate-core/src/main/java/org/hibernate/boot/model/internal/EmbeddableBinder.java
+++ hibernate-core/src/main/java/org/hibernate/boot/model/internal/EmbeddableBinder.java
@@ -59,10 +59,10 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.TreeMap;
 
 import static org.hibernate.boot.model.internal.AnnotatedDiscriminatorColumn.DEFAULT_DISCRIMINATOR_COLUMN_NAME;
 import static org.hibernate.boot.model.internal.AnnotatedDiscriminatorColumn.buildDiscriminatorColumn;
@@ -516,7 +516,7 @@ public class EmbeddableBinder {
 			final BasicType<?> discriminatorType = (BasicType<?>) component.getDiscriminator().getType();
 			// Discriminator values are used to construct the embeddable domain
 			// type hierarchy so order of processing is important
-			final Map<Object, String> discriminatorValues = new TreeMap<>();
+			final Map<Object, String> discriminatorValues = new LinkedHashMap<>();
 			collectDiscriminatorValue( returnedClassOrElement, discriminatorType, discriminatorValues );
 			collectSubclassElements(
 					propertyAccessor,

4f2e2f4e774b9300a2757dd792ef032d4cfb39ae
Marco Belladelli <marcobladel@gmail.com>
2025-02-24 10:45:09 +0100

    HHH-19182 Remove LGPL-only contributions
    8e30a2b86d692ce135d4947edf2358deb69e1e38 rewrite test
    4477baf53bb8592800115e0ce1d1c4cd48434338 rewrite test
    1fc74feadab895d634cc9004d8044f9ec5cd2d5f remove test, rewrite mappings
    cd2b031b6b166d67977e5ee91c5958a3639dd962 remove test

--- hibernate-core/src/test/java/org/hibernate/orm/test/cut/Transaction.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/cut/Transaction.java
@@ -13,7 +13,6 @@ public class Transaction {
 	private Long id;
 	private String description;
 	private MonetoryAmount value;
-	private CompositeDateTime timestamp;
 
 	public String getDescription() {
 		return description;
@@ -37,14 +36,6 @@ public class Transaction {
 
 	public void setValue(MonetoryAmount value) {
 		this.value = value;
-	}
-
-	public CompositeDateTime getTimestamp() {
-		return timestamp;
-	}
-
-	public void setTimestamp(CompositeDateTime timestamp) {
-		this.timestamp = timestamp;
 	}
 
 }

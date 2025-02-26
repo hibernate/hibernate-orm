b46d2f018afdf2d97686a74f18e11d3545e1c53c
Christian Beikov <christian.beikov@gmail.com>
2025-02-26 17:34:52 +0100

    Try increase Sybase heap memory to avoid transient errors

--- docker_db.sh
+++ docker_db.sh
@@ -442,6 +442,8 @@ create login $SYBASE_USER with password $SYBASE_PASSWORD
 go
 exec sp_configure 'enable xml', 1
 go
+exec sp_configure 'heap memory per user', 0, '16K'
+go
 exec sp_dboption $SYBASE_DB, 'abort tran on log full', true
 go
 exec sp_dboption $SYBASE_DB, 'allow nulls by default', true

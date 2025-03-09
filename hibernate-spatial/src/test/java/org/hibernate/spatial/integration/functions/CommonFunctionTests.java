413c3cdf548142846a2b4ba4b17c6d4599026aba
Karel Maesen <karel@geovise.com>
2025-03-09 16:07:11 +0100

    Fix Spatial functions tests
    The TestFactory in CommonFunctionTests no longer created
    any tests because the `isSupported` function always
    returned false due to case-mismatch.
    
    The case-mismatch is likely due to a change in how the
    function keys are returned by the SQMFunctionRegistry.

--- hibernate-spatial/src/test/java/org/hibernate/spatial/integration/functions/CommonFunctionTests.java
+++ hibernate-spatial/src/test/java/org/hibernate/spatial/integration/functions/CommonFunctionTests.java
@@ -41,7 +41,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SuppressWarnings("rawtypes")
 @RequiresDialectFeature(feature = IsSupportedBySpatial.class)
 @SessionFactory
-@SkipForDialect(dialectClass = OracleDialect.class, majorVersion = 11, reason = "See https://hibernate.atlassian.net/browse/HHH-15669")
+@SkipForDialect(dialectClass = OracleDialect.class, reason = "See https://hibernate.atlassian.net/browse/HHH-15669")
 public class CommonFunctionTests extends SpatialTestBase {
 
 	public final static TestSupport.TestDataPurpose PURPOSE = TestSupport.TestDataPurpose.SpatialFunctionsData;

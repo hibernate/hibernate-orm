25929f7a5fc2a48a6d857987703265c8a28347c5
Gavin King <gavin@hibernate.org>
2025-03-21 19:21:28 +0100

    work on tests for batch fetching

--- hibernate-core/src/test/java/org/hibernate/orm/test/bytecode/enhancement/lazy/proxy/BatchFetchProxyTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/bytecode/enhancement/lazy/proxy/BatchFetchProxyTest.java
@@ -197,6 +197,40 @@ public class BatchFetchProxyTest {
 		);
 	}
 
+	@Test
+	@JiraKey("HHH-11147")
+	public void testBatchEntityRemoval(SessionFactoryScope scope) {
+		scope.inTransaction(
+				session -> {
+					final Statistics statistics = scope.getSessionFactory().getStatistics();
+					statistics.clear();
+
+					List<Employer> employers = new ArrayList<>();
+					for ( int i = 0 ; i < 5 ; i++ ) {
+						employers.add( session.getReference( Employer.class, i + 1) );
+					}
+
+					assertEquals( 0, statistics.getPrepareStatementCount() );
+
+					session.find( Employer.class, 0 );
+					session.find( Employer.class, 1 );
+					session.find( Employer.class, 2 );
+					session.find( Employer.class, 5 );
+
+					assertEquals( 1, statistics.getPrepareStatementCount() );
+
+					session.find( Employer.class, 6 );
+					session.find( Employer.class, 7 );
+
+					assertEquals( 3, statistics.getPrepareStatementCount() );
+
+					for ( Employer employer : employers ) {
+						assertTrue( Hibernate.isInitialized( employer ) );
+					}
+				}
+		);
+	}
+
 	@BeforeEach
 	public void setUpData(SessionFactoryScope scope) {
 		scope.inTransaction(

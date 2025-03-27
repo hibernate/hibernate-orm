96c0203d0159f53287702681dedeefda39febc47
Emmanuel JANNETTI <emmanuel.jannetti@gmail.com>
2025-03-27 19:33:29 +0100

    HHH-17404 add flag to disable OSON extension

--- hibernate-core/src/main/java/org/hibernate/type/format/jackson/JacksonJsonFormatMapper.java
+++ hibernate-core/src/main/java/org/hibernate/type/format/jackson/JacksonJsonFormatMapper.java
@@ -4,14 +4,11 @@
  */
 package org.hibernate.type.format.jackson;
 
-import org.hibernate.type.descriptor.WrapperOptions;
-import org.hibernate.type.descriptor.java.JavaType;
 import org.hibernate.type.format.AbstractJsonFormatMapper;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.IOException;
 import java.lang.reflect.Type;
 
 /**
@@ -50,30 +47,5 @@ public class JacksonJsonFormatMapper extends AbstractJsonFormatMapper {
 		catch (JsonProcessingException e) {
 			throw new IllegalArgumentException( "Could not serialize object of java type: " + type, e );
 		}
-	}
-
-	@Override
-	public <T> void writeToTarget(T value, JavaType<T> javaType, Object target, WrapperOptions options)
-			throws IOException {
-
-		try {
-			objectMapper.writerFor(
-					objectMapper.constructType( javaType.getJavaType() ) ).writeValueAsString( value );
-		}
-		catch (JsonProcessingException e) {
-			throw new IllegalArgumentException( "Could not serialize object of java type: " + javaType.getJavaType(), e );
-		}
-
-	}
-
-	@Override
-	public <T> T readFromSource(JavaType<T> javaType, Object source, WrapperOptions options) throws IOException {
-		try {
-			return objectMapper.readValue( ((CharSequence)source).toString(), objectMapper.constructType( javaType.getJavaType() ) );
-		}
-		catch (JsonProcessingException e) {
-			throw new IllegalArgumentException( "Could not deserialize string to java type: " + javaType.getJavaType(), e );
-		}
-
 	}
 }

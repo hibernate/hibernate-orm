37ee87a28e44ec9a47844de7dcf30a371709bb9e
marko-bekhta <marko.prykladna@gmail.com>
2025-03-28 18:53:34 +0100

    HHH-19036 Process all filters before going through classes

--- hibernate-core/src/main/java/org/hibernate/boot/model/source/internal/annotations/AnnotationMetadataSourceProcessorImpl.java
+++ hibernate-core/src/main/java/org/hibernate/boot/model/source/internal/annotations/AnnotationMetadataSourceProcessorImpl.java
@@ -37,6 +37,8 @@ import org.hibernate.internal.util.collections.CollectionHelper;
 import org.jboss.jandex.IndexView;
 import org.jboss.logging.Logger;
 
+import static org.hibernate.boot.model.internal.AnnotationBinder.bindFilterDefs;
+
 /**
  * @author Steve Ebersole
  */
@@ -248,6 +250,13 @@ public class AnnotationMetadataSourceProcessorImpl implements MetadataSourceProc
 				orderedClasses,
 				rootMetadataBuildingContext
 		);
+		// we want to go through all classes and collect the filter definitions first,
+		//  so that when we bind the classes we have the complete list of filters to search from:
+		for ( XClass clazz : orderedClasses ) {
+			if ( !processedEntityNames.contains( clazz.getName() ) ) {
+				bindFilterDefs( clazz, rootMetadataBuildingContext );
+			}
+		}
 
 		for ( XClass clazz : orderedClasses ) {
 			if ( processedEntityNames.contains( clazz.getName() ) ) {

733e3d352c8694d3f68dcb26fc8d5a462c6c0332
Andrea Boriero <andrea@hibernate.org>
2025-03-28 10:59:39 +0100

    HHH-19280 Add test for issue

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/resource/ResultSetReleaseWithStatementDelegationTest.java
@@ -0,0 +1,88 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.orm.test.resource;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.internal.CoreMessageLogger;
+import org.hibernate.resource.jdbc.internal.ResourceRegistryStandardImpl;
+import org.hibernate.testing.DialectChecks;
+import org.hibernate.testing.RequiresDialectFeature;
+import org.hibernate.testing.logger.Triggerable;
+import org.hibernate.testing.orm.junit.DomainModel;
+import org.hibernate.testing.orm.junit.JiraKey;
+import org.hibernate.testing.orm.junit.ServiceRegistry;
+import org.hibernate.testing.orm.junit.SessionFactory;
+import org.hibernate.testing.orm.junit.SessionFactoryScope;
+import org.hibernate.testing.orm.junit.SettingProvider;
+import org.hibernate.testing.orm.logger.LoggerInspectionExtension;
+import org.jboss.logging.Logger;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import java.lang.invoke.MethodHandles;
+
+@DomainModel(
+		annotatedClasses = {
+				ResultSetReleaseWithStatementDelegationTest.Foo.class,
+		}
+)
+@ServiceRegistry(
+		settingProviders = @SettingProvider(settingName = AvailableSettings.CONNECTION_PROVIDER,
+				provider = ResultSetReleaseWithStatementDelegationTest.ConnectionProviderDelegateProvider.class)
+)
+@SessionFactory
+@RequiresDialectFeature(DialectChecks.SupportsIdentityColumns.class)
+@JiraKey( "HHH-19280" )
+class ResultSetReleaseWithStatementDelegationTest {
+
+	public static class ConnectionProviderDelegateProvider implements SettingProvider.Provider<String> {
+		@Override
+		public String getSetting() {
+			return ConnectionProviderDelegate.class.getName();
+		}
+	}
+
+	private Triggerable triggerable;
+
+	@RegisterExtension
+	public LoggerInspectionExtension logger = LoggerInspectionExtension.builder().setLogger(
+			Logger.getMessageLogger(
+					MethodHandles.lookup(),
+					CoreMessageLogger.class,
+					ResourceRegistryStandardImpl.class.getName() )
+	).build();
+
+	@BeforeEach
+	public void setUp() {
+		triggerable = logger.watchForLogMessages( "Exception clearing maxRows/queryTimeout" );
+		triggerable.reset();
+	}
+
+	@Test
+	void testStatementReleae(SessionFactoryScope scope) {
+		scope.inTransaction( session ->
+				session.persist( new Foo() )
+		);
+		Assertions.assertFalse(
+				triggerable.wasTriggered(),
+				"Warning message `Exception clearing maxRows/queryTimeout` has been raised releasing a ResultSet "
+		);
+	}
+
+	@Entity(name = "Foo")
+	static class Foo {
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private long id;
+
+		private String name;
+	}
+}

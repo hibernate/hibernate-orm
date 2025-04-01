3cb9867493967f83fe7c8f1fe6c205f934e2af13
Gavin King <gavin@hibernate.org>
2025-04-01 14:17:46 +0200

    add an @apiNote to LockAcquisitionException and LockTimeoutException

--- hibernate-core/src/main/java/org/hibernate/exception/LockTimeoutException.java
+++ hibernate-core/src/main/java/org/hibernate/exception/LockTimeoutException.java
@@ -12,6 +12,13 @@ import java.sql.SQLException;
  * A {@link JDBCException} indicating that a {@linkplain org.hibernate.LockMode lock}
  * request timed out on the database.
  *
+ * @apiNote Some databases make it quite hard to for a client
+ * to distinguish a lock timeout from other sorts of rejected
+ * lock acquisitions, and so application programs should avoid
+ * over-interpreting the distinction made between
+ * {@code LockTimeoutException} and its superclass
+ * {@link LockAcquisitionException} on such platforms.
+ *
  * @author Steve Ebersole
  *
  * @see org.hibernate.LockOptions#getTimeOut

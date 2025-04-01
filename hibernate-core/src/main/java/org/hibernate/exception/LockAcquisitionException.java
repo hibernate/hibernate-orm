5a80a54dbd9c2e2890d08c7685bcf32605a6c51a
Gavin King <gavin@hibernate.org>
2025-04-01 14:17:46 +0200

    add an @apiNote to LockAcquisitionException and LockTimeoutException

--- hibernate-core/src/main/java/org/hibernate/exception/LockAcquisitionException.java
+++ hibernate-core/src/main/java/org/hibernate/exception/LockAcquisitionException.java
@@ -13,6 +13,13 @@ import org.hibernate.JDBCException;
  * A {@link JDBCException} indicating a problem acquiring a lock
  * on the database.
  *
+ * @apiNote Some databases make it quite hard to for a client to
+ * distinguish a {@linkplain LockTimeoutException lock timeout}
+ * from other sorts of rejected lock acquisitions, and so
+ * application programs should not over-interpret the distinction
+ * made between {@code LockAcquisitionException} and its subclass
+ * {@link LockTimeoutException} on such platforms.
+ *
  * @author Steve Ebersole
  */
 public class LockAcquisitionException extends PessimisticLockException {

bcf3488afcdfa9d4b465142831d2d75148afb4f0
BidyadharM <bidyadhar.mohanty@oracle.com>
2025-04-10 01:32:55 +0530

    HHH-17404 : Removed Duration mapping to intervalDS misssed during previous uploads.

--- hibernate-core/src/main/java/org/hibernate/dialect/OracleDialect.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/OracleDialect.java
@@ -819,9 +819,6 @@ public class OracleDialect extends Dialect {
 			case VARBINARY:
 				return "raw($l)";
 
-			case DURATION:
-				return "interval day to second";
-
 			default:
 				return super.columnType( sqlTypeCode );
 		}

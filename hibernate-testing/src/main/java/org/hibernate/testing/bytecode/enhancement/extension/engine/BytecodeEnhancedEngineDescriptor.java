1866e3de9b7c38914d4ca37ce0901ff020e67b3a
Christian Beikov <christian.beikov@gmail.com>
2025-04-09 14:35:07 +0200

    Make bytecode test engine backwards compatible with older JUnit versions

--- hibernate-testing/src/main/java/org/hibernate/testing/bytecode/enhancement/extension/engine/BytecodeEnhancedEngineDescriptor.java
+++ hibernate-testing/src/main/java/org/hibernate/testing/bytecode/enhancement/extension/engine/BytecodeEnhancedEngineDescriptor.java
@@ -8,10 +8,18 @@ package org.hibernate.testing.bytecode.enhancement.extension.engine;
 
 import org.junit.jupiter.engine.config.JupiterConfiguration;
 import org.junit.jupiter.engine.descriptor.JupiterEngineDescriptor;
+import org.junit.platform.engine.TestDescriptor;
 import org.junit.platform.engine.UniqueId;
 
 public class BytecodeEnhancedEngineDescriptor extends JupiterEngineDescriptor {
 	public BytecodeEnhancedEngineDescriptor(UniqueId uniqueId, JupiterConfiguration configuration) {
 		super( uniqueId, configuration );
+	}
+
+	public BytecodeEnhancedEngineDescriptor(JupiterEngineDescriptor engineDescriptor) {
+		super( engineDescriptor.getUniqueId(), engineDescriptor.getConfiguration() );
+		for ( TestDescriptor child : engineDescriptor.getChildren() ) {
+			addChild( child );
+		}
 	}
 }

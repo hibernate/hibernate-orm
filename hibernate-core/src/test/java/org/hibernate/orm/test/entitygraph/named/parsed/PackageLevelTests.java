daf5279511fb6fb67e52a16c29f57cc2e18b03dd
Christian Beikov <christian.beikov@gmail.com>
2025-04-09 13:11:04 +0200

    Ensure EntityGraph#getName returns the name of a named entity graph

--- hibernate-core/src/test/java/org/hibernate/orm/test/entitygraph/named/parsed/PackageLevelTests.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/entitygraph/named/parsed/PackageLevelTests.java
@@ -9,6 +9,7 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.graph.InvalidGraphException;
+import org.hibernate.graph.spi.RootGraphImplementor;
 import org.hibernate.orm.test.entitygraph.named.parsed.pkg.Book;
 import org.hibernate.orm.test.entitygraph.named.parsed.pkg.Duplicator;
 import org.hibernate.orm.test.entitygraph.named.parsed.pkg.Isbn;
@@ -22,6 +23,7 @@ import org.hibernate.testing.orm.junit.SessionFactoryScope;
 import org.junit.jupiter.api.Test;
 
 import static org.hibernate.orm.test.entitygraph.parser.AssertionHelper.assertBasicAttributes;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 /**
@@ -38,9 +40,15 @@ public class PackageLevelTests {
 	void testDiscovery(SessionFactoryScope factoryScope) {
 		final SessionFactoryImplementor sessionFactory = factoryScope.getSessionFactory();
 
-		assertBasicAttributes( sessionFactory.findEntityGraphByName( "book-title-isbn" ), "title", "isbn" );
-		assertBasicAttributes( sessionFactory.findEntityGraphByName( "book-title-isbn-author" ), "title", "isbn", "author" );
-		assertBasicAttributes( sessionFactory.findEntityGraphByName( "book-title-isbn-editor" ), "title", "isbn", "editor" );
+		assertBasicGraph( sessionFactory, "book-title-isbn", "title", "isbn" );
+		assertBasicGraph( sessionFactory, "book-title-isbn-author", "title", "isbn", "author" );
+		assertBasicGraph( sessionFactory, "book-title-isbn-editor", "title", "isbn", "editor" );
+	}
+
+	private static void assertBasicGraph(SessionFactoryImplementor sessionFactory, String name, String... names) {
+		RootGraphImplementor<?> graph = sessionFactory.findEntityGraphByName( name );
+		assertEquals( name, graph.getName() );
+		assertBasicAttributes( graph, names );
 	}
 
 	@Test

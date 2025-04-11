34fc204559616c12b3720b1629ed5e7beceb9956
oyeliseiev-ua <oyeliseiev-ua@singlestore.com>
2025-04-11 20:18:42 +0300

    HHH-19297 Register SingleStore json functions to SingleStoreDialect

---
+++ hibernate-community-dialects/src/main/java/org/hibernate/community/dialect/function/json/SingleStoreJsonExistsFunction.java
@@ -0,0 +1,63 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.community.dialect.function.json;
+
+import java.util.List;
+
+import org.hibernate.QueryException;
+import org.hibernate.dialect.function.json.JsonExistsFunction;
+import org.hibernate.dialect.function.json.JsonPathHelper;
+import org.hibernate.metamodel.model.domain.ReturnableType;
+import org.hibernate.sql.ast.SqlAstTranslator;
+import org.hibernate.sql.ast.spi.SqlAppender;
+import org.hibernate.sql.ast.tree.expression.JsonExistsErrorBehavior;
+import org.hibernate.type.spi.TypeConfiguration;
+
+/**
+ * SingleStore json_exists function.
+ */
+public class SingleStoreJsonExistsFunction extends JsonExistsFunction {
+
+	public SingleStoreJsonExistsFunction(TypeConfiguration typeConfiguration) {
+		super( typeConfiguration, true, false );
+	}
+
+	@Override
+	protected void render(
+			SqlAppender sqlAppender,
+			JsonExistsArguments arguments,
+			ReturnableType<?> returnType,
+			SqlAstTranslator<?> walker) {
+		if ( arguments.errorBehavior() != null && arguments.errorBehavior() != JsonExistsErrorBehavior.ERROR ) {
+			throw new QueryException( "Can't emulate on error clause on SingleStore" );
+		}
+		final String jsonPath;
+		try {
+			jsonPath = walker.getLiteralValue( arguments.jsonPath() );
+		}
+		catch (Exception ex) {
+			throw new QueryException( "SingleStore json_exists only support literal json paths, but got " + arguments.jsonPath() );
+		}
+		final List<JsonPathHelper.JsonPathElement> jsonPathElements = JsonPathHelper.parseJsonPathElements( jsonPath );
+		sqlAppender.appendSql( "json_match_any_exists(" );
+		arguments.jsonDocument().accept( walker );
+		for ( JsonPathHelper.JsonPathElement pathElement : jsonPathElements ) {
+			sqlAppender.appendSql( ',' );
+			if ( pathElement instanceof JsonPathHelper.JsonAttribute attribute ) {
+				sqlAppender.appendSingleQuoteEscapedString( attribute.attribute() );
+			}
+			else if ( pathElement instanceof JsonPathHelper.JsonParameterIndexAccess ) {
+				final String parameterName = ( (JsonPathHelper.JsonParameterIndexAccess) pathElement ).parameterName();
+				throw new QueryException( "JSON path [" + jsonPath + "] uses parameter [" + parameterName + "] that is not passed" );
+			}
+			else {
+				sqlAppender.appendSql( '\'' );
+				sqlAppender.appendSql( ( (JsonPathHelper.JsonIndexAccess) pathElement ).index() );
+				sqlAppender.appendSql( '\'' );
+			}
+		}
+		sqlAppender.appendSql( ')' );
+	}
+}

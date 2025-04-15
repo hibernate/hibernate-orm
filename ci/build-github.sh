e94c36648b10664b839f270ac3fe8274795300c4
SweetWuXiaoMei <sweetwuxiaomei@qq.com>
2025-04-15 17:35:45 +0800

    HHH-19365 - ci: Remove special build conditions for GaussDB
    - Removed code blocks that handled GaussDB specially based on RDBMS environment variables
    - Removed the EXTRA_GRADLE_ARGS variable set during the build process
    - Now execute the build.sh script directly without passing extra parameters

--- ci/build-github.sh
+++ ci/build-github.sh
@@ -3,7 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 java -version
-if [ "$RDBMS" == "gaussdb" ]; then
-  export EXTRA_GRADLE_ARGS="-x :hibernate-agroal:test"
-fi
-exec bash $DIR/build.sh $EXTRA_GRADLE_ARGS
+exec bash $DIR/build.sh

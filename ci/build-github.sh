5de37575eafb2ecabfd6c360f247ebf88de071b8
SweetWuXiaoMei <sweetwuxiaomei@qq.com>
2025-04-15 16:13:41 +0800

    HHH-19365 - ci: Configure Hibernate build for GaussDB - Add support for GaussDB database in build scripts - Skip hibernate-agroal tests when RDBMS environment variable is gaussdb

--- ci/build-github.sh
+++ ci/build-github.sh
@@ -3,5 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 java -version
-
+if [ "$RDBMS" == "gaussdb" ]; then
+  export EXTRA_GRADLE_ARGS="-x :hibernate-agroal:test"
+fi
 exec bash $DIR/build.sh

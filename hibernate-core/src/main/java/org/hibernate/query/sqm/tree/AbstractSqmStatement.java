6b33971249d5fc5e217482c9689662cb26e95c61
Gavin King <gavin@hibernate.org>
2025-04-29 07:40:04 +0200

    HHH-17002, HHH-18820, HHH-19391, HHH-18514 fix generated aliases
    Oracle disapproves of aliases of form "_1"

--- hibernate-core/src/main/java/org/hibernate/query/sqm/tree/AbstractSqmStatement.java
+++ hibernate-core/src/main/java/org/hibernate/query/sqm/tree/AbstractSqmStatement.java
@@ -103,6 +103,6 @@ public abstract class AbstractSqmStatement<T> extends AbstractSqmNode implements
 
 	@Override
 	public String generateAlias() {
-		return "_" + (++aliasCounter);
+		return "t_" + (++aliasCounter);
 	}
 }

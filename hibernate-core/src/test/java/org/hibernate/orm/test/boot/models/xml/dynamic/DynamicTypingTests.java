ed5220133a15f7d7ca1035015a98f79cc0ff8e5e
Marco Belladelli <marcobladel@gmail.com>
2025-05-06 12:33:32 +0200

    HHH-19324 Fix `DynamicTypingTests` on hana_cloud and cockroachdb

--- hibernate-core/src/test/java/org/hibernate/orm/test/boot/models/xml/dynamic/DynamicTypingTests.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/boot/models/xml/dynamic/DynamicTypingTests.java
@@ -57,11 +57,11 @@ public class DynamicTypingTests {
 		assertThat( entityBinding ).isNotNull();
 
 		verifyBasicAttribute( entityBinding, "theBoolean", BooleanJavaType.class, booleanJdbcType.getJdbcTypeCode() );
-		verifyBasicAttribute( entityBinding, "theString", StringJavaType.class, SqlTypes.VARCHAR );
+		verifyBasicAttribute( entityBinding, "theString", StringJavaType.class, SqlTypes.VARCHAR, /*HANA Cloud uses UTF8 by default*/ SqlTypes.NVARCHAR );
 		verifyBasicAttribute( entityBinding, "theInt", IntegerJavaType.class, SqlTypes.INTEGER );
 		verifyBasicAttribute( entityBinding, "theInteger", IntegerJavaType.class, SqlTypes.INTEGER );
 		verifyBasicAttribute( entityBinding, "theUrl", UrlJavaType.class, SqlTypes.VARCHAR );
-		verifyBasicAttribute( entityBinding, "theClob", ClobJavaType.class, SqlTypes.CLOB );
+		verifyBasicAttribute( entityBinding, "theClob", ClobJavaType.class, SqlTypes.CLOB, /*CockroachDB doesn't support CLOBs*/ SqlTypes.VARCHAR );
 		verifyBasicAttribute( entityBinding, "theInstant", InstantJavaType.class, SqlTypes.INSTANT );
 		verifyBasicAttribute( entityBinding, "theDate", JdbcDateJavaType.class, SqlTypes.DATE );
 		verifyBasicAttribute( entityBinding, "theTime", JdbcTimeJavaType.class, SqlTypes.TIME );
@@ -72,15 +72,15 @@ public class DynamicTypingTests {
 	}
 
 
-	private static void verifyBasicAttribute(RootClass rootClass, String attributeName, Class<? extends BasicJavaType<?>> expectedJavaType, int expectedJdbcTypeCode) {
+	private static void verifyBasicAttribute(RootClass rootClass, String attributeName, Class<? extends BasicJavaType<?>> expectedJavaType, int... expectedJdbcTypeCodes) {
 		final Property attribute = rootClass.getProperty( attributeName );
 		assertThat( attribute.getType() ).isInstanceOf( BasicType.class );
-		verifyBasicMapping( (BasicType<?>) attribute.getType(), expectedJavaType, expectedJdbcTypeCode );
+		verifyBasicMapping( (BasicType<?>) attribute.getType(), expectedJavaType, expectedJdbcTypeCodes );
 	}
 
-	private static void verifyBasicMapping(BasicType<?> type, Class<? extends BasicJavaType<?>> expectedJavaType, int expectedJdbcTypeCode) {
+	private static void verifyBasicMapping(BasicType<?> type, Class<? extends BasicJavaType<?>> expectedJavaType, int... expectedJdbcTypeCodes) {
 		assertThat( type.getJavaTypeDescriptor().getClass() ).isEqualTo( expectedJavaType );
-		assertThat( type.getJdbcType().getJdbcTypeCode() ).isEqualTo( expectedJdbcTypeCode );
+		assertThat( expectedJdbcTypeCodes ).contains( type.getJdbcType().getJdbcTypeCode() );
 	}
 
 	private static void verifyElementCollection(RootClass rootClass, String name, Class<? extends BasicJavaType<?>> expectedJavaType, int expectedJdbcTypeCode) {

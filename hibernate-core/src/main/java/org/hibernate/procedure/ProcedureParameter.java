1b74f539ee347c1f16654f56b008aae7e73f548e
Gavin King <gavin@hibernate.org>
2025-05-10 18:29:49 +0200

    move stuff out of org.hibernate.query.procedure
    to realize consistent SPI/API split

--- hibernate-core/src/main/java/org/hibernate/procedure/ProcedureParameter.java
+++ hibernate-core/src/main/java/org/hibernate/procedure/ProcedureParameter.java
@@ -2,7 +2,7 @@
  * SPDX-License-Identifier: Apache-2.0
  * Copyright Red Hat Inc. and Hibernate Authors
  */
-package org.hibernate.query.procedure;
+package org.hibernate.procedure;
 
 import org.hibernate.Incubating;
 import org.hibernate.query.QueryParameter;

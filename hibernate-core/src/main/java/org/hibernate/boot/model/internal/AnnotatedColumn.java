fd0f2ae5213f7a2dbf7d6e5bb6504816faec5428
Gavin King <gavin@hibernate.org>
2025-05-17 09:56:40 +0200

    squash! code cleanups in AnnotatedColumn

--- hibernate-core/src/main/java/org/hibernate/boot/model/internal/AnnotatedColumn.java
+++ hibernate-core/src/main/java/org/hibernate/boot/model/internal/AnnotatedColumn.java
@@ -23,7 +23,6 @@ import org.hibernate.boot.model.naming.ObjectNameNormalizer;
 import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
 import org.hibernate.boot.model.relational.Database;
 import org.hibernate.boot.model.source.spi.AttributePath;
-import org.hibernate.boot.spi.InFlightMetadataCollector;
 import org.hibernate.boot.spi.MetadataBuildingContext;
 import org.hibernate.boot.spi.PropertyData;
 import org.hibernate.internal.CoreMessageLogger;

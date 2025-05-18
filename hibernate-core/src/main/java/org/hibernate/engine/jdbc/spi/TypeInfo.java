053d3ec6ba4bda1e4fe04e7166977135d9c0ca0f
mehmetali2003 <mehmetaliemektar@gmail.com>
2025-05-18 20:07:47 +0300

    HHH-15993: NullPointerException from DatabaseMetaData validation
    Check for possible null values from metaData.getTypeInfo()

--- hibernate-core/src/main/java/org/hibernate/engine/jdbc/spi/TypeInfo.java
+++ hibernate-core/src/main/java/org/hibernate/engine/jdbc/spi/TypeInfo.java
@@ -86,36 +86,38 @@ public class TypeInfo {
 		final LinkedHashSet<TypeInfo> typeInfoSet = new LinkedHashSet<>();
 		try {
 			final ResultSet resultSet = metaData.getTypeInfo();
-			try {
-				while ( resultSet.next() ) {
-					typeInfoSet.add(
-							new TypeInfo(
-									resultSet.getString( "TYPE_NAME" ),
-									resultSet.getInt( "DATA_TYPE" ),
-									interpretCreateParams( resultSet.getString( "CREATE_PARAMS" ) ),
-									resultSet.getBoolean( "UNSIGNED_ATTRIBUTE" ),
-									resultSet.getInt( "PRECISION" ),
-									resultSet.getShort( "MINIMUM_SCALE" ),
-									resultSet.getShort( "MAXIMUM_SCALE" ),
-									resultSet.getBoolean( "FIXED_PREC_SCALE" ),
-									resultSet.getString( "LITERAL_PREFIX" ),
-									resultSet.getString( "LITERAL_SUFFIX" ),
-									resultSet.getBoolean( "CASE_SENSITIVE" ),
-									TypeSearchability.interpret( resultSet.getShort( "SEARCHABLE" ) ),
-									TypeNullability.interpret( resultSet.getShort( "NULLABLE" ) )
-							)
-					);
-				}
-			}
-			catch ( SQLException e ) {
-				LOG.unableToAccessTypeInfoResultSet( e.toString() );
-			}
-			finally {
+			if (resultSet != null) {
 				try {
-					resultSet.close();
+					while ( resultSet.next() ) {
+						typeInfoSet.add(
+								new TypeInfo(
+										resultSet.getString( "TYPE_NAME" ),
+										resultSet.getInt( "DATA_TYPE" ),
+										interpretCreateParams( resultSet.getString( "CREATE_PARAMS" ) ),
+										resultSet.getBoolean( "UNSIGNED_ATTRIBUTE" ),
+										resultSet.getInt( "PRECISION" ),
+										resultSet.getShort( "MINIMUM_SCALE" ),
+										resultSet.getShort( "MAXIMUM_SCALE" ),
+										resultSet.getBoolean( "FIXED_PREC_SCALE" ),
+										resultSet.getString( "LITERAL_PREFIX" ),
+										resultSet.getString( "LITERAL_SUFFIX" ),
+										resultSet.getBoolean( "CASE_SENSITIVE" ),
+										TypeSearchability.interpret( resultSet.getShort( "SEARCHABLE" ) ),
+										TypeNullability.interpret( resultSet.getShort( "NULLABLE" ) )
+								)
+						);
+					}
 				}
 				catch ( SQLException e ) {
-					LOG.unableToReleaseTypeInfoResultSet();
+					LOG.unableToAccessTypeInfoResultSet( e.toString() );
+				}
+				finally {
+					try {
+						resultSet.close();
+					}
+					catch ( SQLException e ) {
+						LOG.unableToReleaseTypeInfoResultSet();
+					}
 				}
 			}
 		}

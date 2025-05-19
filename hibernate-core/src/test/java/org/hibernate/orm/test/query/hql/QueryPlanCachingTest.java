9ab496fcdb8fcc2556abde5d84c309bf2961c8d2
Victor NOËL <victor.noel@pricefx.com>
2025-05-19 11:54:57 +0200

    HHH-19331: query plan cache should take parameter type into accound for cast

--- hibernate-core/src/test/java/org/hibernate/orm/test/query/hql/QueryPlanCachingTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/query/hql/QueryPlanCachingTest.java
@@ -5,30 +5,47 @@
 package org.hibernate.orm.test.query.hql;
 
 import org.hibernate.engine.spi.SessionImplementor;
-import org.hibernate.orm.test.mapping.SmokeTests;
-
 import org.hibernate.query.criteria.HibernateCriteriaBuilder;
 import org.hibernate.query.criteria.JpaCriteriaQuery;
 import org.hibernate.query.criteria.JpaRoot;
+import org.hibernate.testing.orm.domain.gambit.BasicEntity;
 import org.hibernate.testing.orm.junit.DomainModel;
+import org.hibernate.testing.orm.junit.Jira;
 import org.hibernate.testing.orm.junit.ServiceRegistry;
 import org.hibernate.testing.orm.junit.SessionFactory;
 import org.hibernate.testing.orm.junit.SessionFactoryScope;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Steve Ebersole
  */
-@DomainModel( annotatedClasses = SmokeTests.SimpleEntity.class )
+@DomainModel(annotatedClasses = BasicEntity.class)
 @ServiceRegistry
-@SessionFactory( exportSchema = true )
+@SessionFactory(exportSchema = true)
 public class QueryPlanCachingTest {
+
+	@BeforeAll
+	public void setUp(SessionFactoryScope scope) {
+		scope.inTransaction( session -> session.persist( new BasicEntity( 1, "entity_1" ) ) );
+	}
+
+	@AfterAll
+	public void tearDown(SessionFactoryScope scope) {
+		scope.getSessionFactory().getSchemaManager().truncateMappedObjects();
+	}
+
 	@Test
 	public void testHqlTranslationCaching(SessionFactoryScope scope) {
 		scope.inTransaction(
 				session -> {
-					session.createQuery( "select e from SimpleEntity e" ).list();
-					session.createQuery( "select e from SimpleEntity e" ).list();
+					session.createQuery( "select e from BasicEntity e" ).list();
+					session.createQuery( "select e from BasicEntity e" ).list();
 				}
 		);
 	}
@@ -44,11 +61,45 @@ public class QueryPlanCachingTest {
 		);
 	}
 
-	private static JpaCriteriaQuery<SmokeTests.SimpleEntity> constructCriteriaQuery(SessionImplementor session) {
+	private static JpaCriteriaQuery<BasicEntity> constructCriteriaQuery(SessionImplementor session) {
 		final HibernateCriteriaBuilder cb = session.getCriteriaBuilder();
-		final JpaCriteriaQuery<SmokeTests.SimpleEntity> query = cb.createQuery( SmokeTests.SimpleEntity.class );
-		final JpaRoot<SmokeTests.SimpleEntity> root = query.from( SmokeTests.SimpleEntity.class );
+		final JpaCriteriaQuery<BasicEntity> query = cb.createQuery( BasicEntity.class );
+		final JpaRoot<BasicEntity> root = query.from( BasicEntity.class );
 		query.select( root );
 		return query;
+	}
+
+	@Test
+	@Jira("https://hibernate.atlassian.net/browse/HHH-19331")
+	public void hhh19331_selectionquery(SessionFactoryScope scope) {
+		scope.inTransaction( session -> {
+			assertThat(
+					session.createSelectionQuery( "select :p0 from BasicEntity", Object[].class )
+							.setParameter( "p0", 1 )
+							.getSingleResult()
+			).containsExactly( 1 );
+			assertThat(
+					session.createSelectionQuery( "select :p0 from BasicEntity", Object[].class )
+							.setParameter( "p0", BigDecimal.valueOf( 3.14 ) )
+							.getSingleResult()
+			).containsExactly( BigDecimal.valueOf( 3.14 ) );
+		} );
+	}
+
+	@Test
+	@Jira("https://hibernate.atlassian.net/browse/HHH-19331")
+	public void hhh19331_query(SessionFactoryScope scope) {
+		scope.inTransaction( session -> {
+			assertThat(
+					session.createQuery( "select :p0 from BasicEntity", Object[].class )
+							.setParameter( "p0", 1 )
+							.getSingleResult()
+			).containsExactly( 1 );
+			assertThat(
+					session.createQuery( "select :p0 from BasicEntity", Object[].class )
+							.setParameter( "p0", BigDecimal.valueOf( 3.14 ) )
+							.getSingleResult()
+			).containsExactly( BigDecimal.valueOf( 3.14 ) );
+		} );
 	}
 }

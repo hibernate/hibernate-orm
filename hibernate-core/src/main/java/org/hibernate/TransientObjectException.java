b73d381ef88c1b59124bb3b3dd69d8fbf14c9374
Gavin King <gavin@hibernate.org>
2025-05-20 19:31:05 +0200

    improve javadoc of TransientObjectException

--- hibernate-core/src/main/java/org/hibernate/TransientObjectException.java
+++ hibernate-core/src/main/java/org/hibernate/TransientObjectException.java
@@ -5,8 +5,20 @@
 package org.hibernate;
 
 /**
- * Thrown when the user passes a transient instance to a {@link Session}
- * method that expects a persistent instance.
+ * Thrown if a transient instance of an entity class is passed to
+ * a {@link Session} method that expects a persistent instance,
+ * or if the state of an entity instance cannot be made persistent
+ * because the instance holds a reference to a transient entity.
+ * <p>
+ * An entity is considered <em>transient</em> if it is:
+ * <ul>
+ * <li>a newly-instantiated instance of an entity class which has
+ *    never been {@linkplain Session#persist made persistent} in
+ *    the database, or
+ * <li>an entity instance previously associated with a persistence
+ *     context which has been {@linkplain Session#remove removed}
+ *     from the database.
+ * </ul>
  *
  * @author Gavin King
  */

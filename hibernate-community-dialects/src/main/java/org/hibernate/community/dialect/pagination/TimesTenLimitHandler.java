e6f5c1ef5503de668ab76c756d658f81083fd8de
carlblan <carlos.a.blanco@oracle.com>
2025-05-22 22:41:03 +0000

    Adding updates to TimesTenDialect, TimesTenSqlAstTranslator, TimesTenLimitHandler and TimesTenSequenceSupport

--- hibernate-community-dialects/src/main/java/org/hibernate/community/dialect/pagination/TimesTenLimitHandler.java
+++ hibernate-community-dialects/src/main/java/org/hibernate/community/dialect/pagination/TimesTenLimitHandler.java
@@ -7,22 +7,76 @@
 package org.hibernate.community.dialect.pagination;
 
 import org.hibernate.dialect.pagination.LimitHandler;
+import org.hibernate.dialect.pagination.LimitHandler;
+import org.hibernate.dialect.pagination.AbstractLimitHandler;
 
 /**
  * A {@link LimitHandler} for TimesTen, which uses {@code ROWS n},
  * but at the start of the query instead of at the end.
  */
-public class TimesTenLimitHandler extends RowsLimitHandler {
+public class TimesTenLimitHandler extends AbstractLimitHandler {
 
 	public static final TimesTenLimitHandler INSTANCE = new TimesTenLimitHandler();
 
-	@Override
-	protected String insert(String rows, String sql) {
-		return insertAfterSelect( rows, sql );
-	}
+  public TimesTenLimitHandler(){
+  }
 
-	@Override
-	public boolean bindLimitParametersFirst() {
-		return true;
-	}
+  /*
+   * Copyright (c) 2025, Oracle and/or its affiliates.
+   * Licensed under the Universal Permissive License v 1.0 as shown
+   * at http://oss.oracle.com/licenses/upl
+   *
+   *   - The class now extends 'AbstractLimitHandler'
+   *   - Removed a custom definition for 'insert()'
+   *   - Added a custom definition for 'supportsLimit()'
+   *   - Added a custom definition for 'supportsOffset()'
+   *   - Added a custom definition for 'supportsLimitOffset()'
+   *   - Added a custom definition for 'supportsVariableLimit()'
+   *   - Added a custom definition for 'convertToFirstRowValue(int zeroBasedFirstResult)'
+   *   - Added a custom definition for 'useMaxForLimit()'
+   *   - Added a custom definition for 'limitClause(boolean hasFirstRow)'
+   *
+  */
+  @Override
+  public boolean supportsLimit() {
+    return true;
+  }
+
+  @Override
+  public boolean supportsOffset() {
+    return false;
+  }
+  
+  @Override
+  public boolean supportsLimitOffset() { 
+    return true;
+  }
+
+  @Override
+  public boolean supportsVariableLimit() {
+    // a limit string using literals instead of parameters is
+    // required to translate from Hibernate's 0 based row numbers
+    // to TimesTen 1 based row numbers
+    return false;
+  }
+
+  @Override
+  // TimesTen is 1 based
+  public int convertToFirstRowValue(int zeroBasedFirstResult) {
+    return zeroBasedFirstResult + 1;
+  }
+
+  @Override
+  public boolean useMaxForLimit() {
+    return true;
+  }
+
+  @Override
+  public boolean bindLimitParametersFirst() {
+    return true;
+  }
+
+  protected String limitClause(boolean hasFirstRow) {
+    return hasFirstRow ? " rows ? to ?" : " first ?";
+  }
 }

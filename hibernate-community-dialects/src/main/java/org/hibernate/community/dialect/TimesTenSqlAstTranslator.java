c0d86b2bb92423b1feb51ee2cc31ced98c6ff14d
carlblan <carlos.a.blanco@oracle.com>
2025-05-22 22:41:03 +0000

    Adding updates to TimesTenDialect, TimesTenSqlAstTranslator, TimesTenLimitHandler and TimesTenSequenceSupport

--- hibernate-community-dialects/src/main/java/org/hibernate/community/dialect/TimesTenSqlAstTranslator.java
+++ hibernate-community-dialects/src/main/java/org/hibernate/community/dialect/TimesTenSqlAstTranslator.java
@@ -28,6 +28,8 @@ import org.hibernate.sql.ast.tree.select.QueryPart;
 import org.hibernate.sql.ast.tree.select.QuerySpec;
 import org.hibernate.sql.ast.tree.select.SelectClause;
 import org.hibernate.sql.exec.spi.JdbcOperation;
+import org.hibernate.internal.util.collections.Stack;
+import org.hibernate.sql.ast.Clause;
 
 /**
  * A SQL AST translator for TimesTen.
@@ -143,4 +145,64 @@ public class TimesTenSqlAstTranslator<T extends JdbcOperation> extends AbstractS
 	protected boolean supportsRowValueConstructorSyntaxInQuantifiedPredicates() {
 		return false;
 	}
+
+  /*
+   * Copyright (c) 2025, Oracle and/or its affiliates.
+   * Licensed under the Universal Permissive License v 1.0 as shown
+   * at http://oss.oracle.com/licenses/upl
+   *
+   * - Added a custom definition for 'renderRowsToClause()'.
+   *
+  */
+  @Override
+  protected void renderRowsToClause(Expression offsetClauseExpression, Expression fetchClauseExpression) {
+    // offsetClauseExpression -> firstRow
+    // fetchClauseExpression  -> maxRows
+    final Stack<Clause> clauseStack = getClauseStack();
+
+    if ( offsetClauseExpression == null && fetchClauseExpression != null ) {
+      // We only have a maxRows/limit. We use 'SELECT FIRST n' syntax
+      appendSql("first ");
+      clauseStack.push( Clause.FETCH );
+      try {
+        renderFetchExpression( fetchClauseExpression );
+      }
+      finally {
+        clauseStack.pop();
+      }
+    }
+    else if ( offsetClauseExpression != null && fetchClauseExpression == null ) {
+      throw new UnsupportedOperationException( 
+        "Only passing setFirstResult(m) and not setMaxResults(n) to 'ROWS m TO n' clause not supported." 
+      );
+    }
+    else if ( offsetClauseExpression != null && fetchClauseExpression != null ) {
+      // We have offset and maxRows/limit. We use 'SELECT ROWS offset TO limit' syntax
+      appendSql( "rows " );
+      
+      // Render offset parameter
+      clauseStack.push( Clause.OFFSET );
+      try {
+        renderOffsetExpression( offsetClauseExpression );
+      }
+      finally {
+        clauseStack.pop();
+      }
+
+      appendSql( " to " );
+
+      // Render maxRows/limit parameter
+      clauseStack.push( Clause.FETCH );
+      try {
+        // TimesTen includes both m and n rows of 'ROWS m to n'; 
+        // We need to substract 1 row to fit maxRows
+        renderFetchPlusOffsetExpressionAsLiteral( fetchClauseExpression, offsetClauseExpression, -1 );
+      }
+      finally {
+        clauseStack.pop();
+      }
+    }
+
+    appendSql( WHITESPACE );
+  }
 }

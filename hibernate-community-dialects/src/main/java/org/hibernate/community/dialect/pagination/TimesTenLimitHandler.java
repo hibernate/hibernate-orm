27e72ac5dda21b44f5dfdec0935d6b1283a10781
carlblan <carlos.a.blanco@oracle.com>
2025-05-23 19:56:22 +0000

    Fix indentation

--- hibernate-community-dialects/src/main/java/org/hibernate/community/dialect/pagination/TimesTenLimitHandler.java
+++ hibernate-community-dialects/src/main/java/org/hibernate/community/dialect/pagination/TimesTenLimitHandler.java
@@ -18,65 +18,65 @@ public class TimesTenLimitHandler extends AbstractLimitHandler {
 
 	public static final TimesTenLimitHandler INSTANCE = new TimesTenLimitHandler();
 
-  public TimesTenLimitHandler(){
-  }
+	public TimesTenLimitHandler(){
+	}
 
-  /*
-   * Copyright (c) 2025, Oracle and/or its affiliates.
-   * Licensed under the Universal Permissive License v 1.0 as shown
-   * at http://oss.oracle.com/licenses/upl
-   *
-   *   - The class now extends 'AbstractLimitHandler'
-   *   - Removed a custom definition for 'insert()'
-   *   - Added a custom definition for 'supportsLimit()'
-   *   - Added a custom definition for 'supportsOffset()'
-   *   - Added a custom definition for 'supportsLimitOffset()'
-   *   - Added a custom definition for 'supportsVariableLimit()'
-   *   - Added a custom definition for 'convertToFirstRowValue(int zeroBasedFirstResult)'
-   *   - Added a custom definition for 'useMaxForLimit()'
-   *   - Added a custom definition for 'limitClause(boolean hasFirstRow)'
-   *
+	/*
+	 * Copyright (c) 2025, Oracle and/or its affiliates.
+	 * Licensed under the Universal Permissive License v 1.0 as shown
+	 * at http://oss.oracle.com/licenses/upl
+	 *
+	 *   - The class now extends 'AbstractLimitHandler'
+	 *   - Removed a custom definition for 'insert()'
+	 *   - Added a custom definition for 'supportsLimit()'
+	 *   - Added a custom definition for 'supportsOffset()'
+	 *   - Added a custom definition for 'supportsLimitOffset()'
+	 *   - Added a custom definition for 'supportsVariableLimit()'
+	 *   - Added a custom definition for 'convertToFirstRowValue(int zeroBasedFirstResult)'
+	 *   - Added a custom definition for 'useMaxForLimit()'
+	 *   - Added a custom definition for 'limitClause(boolean hasFirstRow)'
+	 *
   */
-  @Override
-  public boolean supportsLimit() {
-    return true;
-  }
+	@Override
+	public boolean supportsLimit() {
+		return true;
+	}
 
-  @Override
-  public boolean supportsOffset() {
-    return false;
-  }
+	@Override
+	public boolean supportsOffset() {
+		return false;
+	}
   
-  @Override
-  public boolean supportsLimitOffset() { 
-    return true;
-  }
+	@Override
+	public boolean supportsLimitOffset() { 
+		return true;
+	}
 
-  @Override
-  public boolean supportsVariableLimit() {
-    // a limit string using literals instead of parameters is
-    // required to translate from Hibernate's 0 based row numbers
-    // to TimesTen 1 based row numbers
-    return false;
-  }
+	@Override
+	public boolean supportsVariableLimit() {
+ 		// a limit string using literals instead of parameters is
+		// required to translate from Hibernate's 0 based row numbers
+		// to TimesTen 1 based row numbers
+ 		return false;
+	}
 
-  @Override
-  // TimesTen is 1 based
-  public int convertToFirstRowValue(int zeroBasedFirstResult) {
-    return zeroBasedFirstResult + 1;
-  }
+	@Override
+	// TimesTen is 1 based
+	public int convertToFirstRowValue(int zeroBasedFirstResult) {
+		return zeroBasedFirstResult + 1;
+	}
 
-  @Override
-  public boolean useMaxForLimit() {
-    return true;
-  }
+	@Override
+	public boolean useMaxForLimit() {
+		return true;
+	}
 
-  @Override
-  public boolean bindLimitParametersFirst() {
-    return true;
-  }
+	@Override
+	public boolean bindLimitParametersFirst() {
+		return true;
+	}
 
-  protected String limitClause(boolean hasFirstRow) {
-    return hasFirstRow ? " rows ? to ?" : " first ?";
-  }
+	protected String limitClause(boolean hasFirstRow) {
+		return hasFirstRow ? " rows ? to ?" : " first ?";
+	}
 }
--- hibernate-community-dialects/src/main/java/org/hibernate/community/dialect/sequence/TimesTenSequenceSupport.java
+++ hibernate-community-dialects/src/main/java/org/hibernate/community/dialect/sequence/TimesTenSequenceSupport.java
@@ -17,53 +17,53 @@ public final class TimesTenSequenceSupport implements SequenceSupport {
 
 	public static final SequenceSupport INSTANCE = new TimesTenSequenceSupport();
 
-  /*
-   * Copyright (c) 2025, Oracle and/or its affiliates.
-   * Licensed under the Universal Permissive License v 1.0 as shown
-   * at http://oss.oracle.com/licenses/upl
-   *
-   *  - The Class now implements 'SequenceSupport'
-   *  - Added a custom definition for 'supportsSequences()'
-   *  - Added a custom definition for 'supportsPooledSequences()'
-   *  - Added a custom definition for 'getSelectSequenceNextValString(String sequenceName)'
-   *  - Added a custom definition for 'getSequenceNextValString(String sequenceName)'
-   *  - Added a custom definition for 'getCreateSequenceString(String sequenceName)'
-   *  - Added a custom definition for 'getDropSequenceString(String sequenceName)'
-   *
-  */
+	/*
+	 * Copyright (c) 2025, Oracle and/or its affiliates.
+	 * Licensed under the Universal Permissive License v 1.0 as shown
+	 * at http://oss.oracle.com/licenses/upl
+	 *
+	 *  - The Class now implements 'SequenceSupport'
+	 *  - Added a custom definition for 'supportsSequences()'
+	 *  - Added a custom definition for 'supportsPooledSequences()'
+	 *  - Added a custom definition for 'getSelectSequenceNextValString(String sequenceName)'
+	 *  - Added a custom definition for 'getSequenceNextValString(String sequenceName)'
+	 *  - Added a custom definition for 'getCreateSequenceString(String sequenceName)'
+	 *  - Added a custom definition for 'getDropSequenceString(String sequenceName)'
+	 *
+	*/
 
-  @Override
-  public boolean supportsSequences() {
-    return true;
-  }
+	@Override
+	public boolean supportsSequences() {
+		return true;
+	}
 
-  @Override
-  public boolean supportsPooledSequences() {
-    return true;
-  }
+	@Override
+	public boolean supportsPooledSequences() {
+		return true;
+	}
 
-  @Override
-  public String getSelectSequenceNextValString(String sequenceName) {
-    return sequenceName + ".nextval";
-  }
+	@Override
+	public String getSelectSequenceNextValString(String sequenceName) {
+		return sequenceName + ".nextval";
+	}
 
-  @Override
-  public String getSequenceNextValString(String sequenceName) {
-    return "select " + sequenceName + ".nextval from sys.dual";
-  }
+	@Override
+	public String getSequenceNextValString(String sequenceName) {
+		return "select " + sequenceName + ".nextval from sys.dual";
+	}
   
-  @Override
-  public String getFromDual() {
-    return " from sys.dual";
-  }
+	@Override
+	public String getFromDual() {
+		return " from sys.dual";
+	}
 
-  @Override
-  public String getCreateSequenceString(String sequenceName) {
-    return "create sequence " + sequenceName;
-  }
+	@Override
+	public String getCreateSequenceString(String sequenceName) {
+		return "create sequence " + sequenceName;
+	}
 
-  @Override
-  public String getDropSequenceString(String sequenceName) {
-    return "drop sequence " + sequenceName;
-  }
+	@Override
+	public String getDropSequenceString(String sequenceName) {
+		return "drop sequence " + sequenceName;
+	}
 }

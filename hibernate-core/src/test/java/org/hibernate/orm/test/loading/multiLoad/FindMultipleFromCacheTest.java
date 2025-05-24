37c1f8829d36b57308b4e08bf42cdd34656518ff
Gavin King <gavin@hibernate.org>
2025-05-24 13:07:19 +0200

    add additional test for findMultiple() with second-level cache

--- hibernate-core/src/test/java/org/hibernate/orm/test/loading/multiLoad/FindMultipleFromCacheTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/loading/multiLoad/FindMultipleFromCacheTest.java
@@ -2,11 +2,12 @@
  * SPDX-License-Identifier: Apache-2.0
  * Copyright Red Hat Inc. and Hibernate Authors
  */
-package org.hibernate.orm.test.stateless;
+package org.hibernate.orm.test.loading.multiLoad;
 
 import jakarta.persistence.Cacheable;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import org.hibernate.LockMode;
 import org.hibernate.testing.orm.junit.DomainModel;
 import org.hibernate.testing.orm.junit.SessionFactory;
 import org.hibernate.testing.orm.junit.SessionFactoryScope;
@@ -18,26 +19,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SessionFactory(generateStatistics = true)
-@DomainModel(annotatedClasses = GetMultipleFromCacheTest.Record.class)
-public class GetMultipleFromCacheTest {
+@DomainModel(annotatedClasses = FindMultipleFromCacheTest.Record.class)
+public class FindMultipleFromCacheTest {
 	@Test void test(SessionFactoryScope scope) {
 		scope.inStatelessTransaction(s-> {
 			s.insert(new Record(123L,"hello earth"));
 			s.insert(new Record(456L,"hello mars"));
 		});
-		scope.inStatelessTransaction(s-> {
-			List<Record> all = s.getMultiple(Record.class, List.of(456L, 123L, 2L));
-			assertEquals("hello mars",all.get(0).message);
-			assertEquals("hello earth",all.get(1).message);
+		scope.inTransaction(s-> {
+			List<Record> all = s.findMultiple(Record.class, List.of(456L, 123L, 2L));
+			Record mars = all.get( 0 );
+			Record earth = all.get( 1 );
+			assertEquals( LockMode.READ, s.getCurrentLockMode( mars ) );
+			assertEquals( LockMode.READ, s.getCurrentLockMode( earth ) );
+			assertEquals("hello mars", mars.message);
+			assertEquals("hello earth", earth.message);
 			assertNull(all.get(2));
 		});
 		assertEquals( 0,
 				scope.getSessionFactory().getStatistics().getSecondLevelCacheHitCount() );
 		scope.getSessionFactory().getStatistics().clear();
-		scope.inStatelessTransaction(s-> {
-			List<Record> all = s.getMultiple(Record.class, List.of(123L, 2L, 456L));
-			assertEquals("hello earth",all.get(0).message);
-			assertEquals("hello mars",all.get(2).message);
+		scope.inTransaction(s-> {
+			List<Record> all = s.findMultiple(Record.class, List.of(123L, 2L, 456L));
+			Record earth = all.get( 0 );
+			Record mars = all.get( 2 );
+			assertEquals( LockMode.NONE, s.getCurrentLockMode( mars ) );
+			assertEquals( LockMode.NONE, s.getCurrentLockMode( earth ) );
+			assertEquals("hello earth", earth.message);
+			assertEquals("hello mars", mars.message);
 			assertNull(all.get(1));
 		});
 		assertEquals( 2,

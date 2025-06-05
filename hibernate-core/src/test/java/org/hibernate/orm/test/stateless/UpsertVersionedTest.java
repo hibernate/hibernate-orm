06963e693797f82cbc575e04436bd5ad6f0bf5f7
Gavin King <gavin@hibernate.org>
2025-06-05 22:56:09 +0200

    HHH-19522 report failed optimistic lock checking for upsert()
    upsert() should throw StaleObjectStateException if the version
    verification fails

--- hibernate-core/src/test/java/org/hibernate/orm/test/stateless/UpsertVersionedTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/stateless/UpsertVersionedTest.java
@@ -7,17 +7,21 @@ package org.hibernate.orm.test.stateless;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Version;
+import org.hibernate.StaleObjectStateException;
 import org.hibernate.testing.orm.junit.DomainModel;
 import org.hibernate.testing.orm.junit.SessionFactory;
 import org.hibernate.testing.orm.junit.SessionFactoryScope;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @SessionFactory
 @DomainModel(annotatedClasses = UpsertVersionedTest.Record.class)
 public class UpsertVersionedTest {
+
 	@Test void test(SessionFactoryScope scope) {
+		scope.getSessionFactory().getSchemaManager().truncate();
 		scope.inStatelessTransaction(s-> {
 			s.upsert(new Record(123L,null,"hello earth"));
 			s.upsert(new Record(456L,2L,"hello mars"));
@@ -41,6 +45,29 @@ public class UpsertVersionedTest {
 			assertEquals("goodbye mars",s.get(Record.class,456L).message);
 		});
 	}
+
+	@Test void testStaleUpsert(SessionFactoryScope scope) {
+		scope.getSessionFactory().getSchemaManager().truncate();
+		scope.inStatelessTransaction( s -> {
+			s.insert(new Record(789L, 1L, "hello world"));
+		} );
+		scope.inStatelessTransaction( s -> {
+			s.upsert(new Record(789L, 1L, "hello mars"));
+		} );
+		try {
+			scope.inStatelessTransaction( s -> {
+				s.upsert(new Record( 789L, 1L, "hello venus"));
+			} );
+			fail();
+		}
+		catch (StaleObjectStateException sose) {
+			//expected
+		}
+		scope.inStatelessTransaction( s-> {
+			assertEquals( "hello mars", s.get(Record.class,789L).message );
+		} );
+	}
+
 	@Entity(name = "Record")
 	static class Record {
 		@Id Long id;

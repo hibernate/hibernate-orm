7678603a35e14aa86babe78499e3535702e71a9e
Peter Bambazek <peter.bambazek@gmail.com>
2025-06-11 17:40:38 +0200

    HHH-18837 Oracle epoch extraction doesn't work with dates

---
+++ hibernate-core/src/main/java/org/hibernate/dialect/function/OracleExtractFunction.java
@@ -0,0 +1,55 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.dialect.function;
+
+import jakarta.persistence.TemporalType;
+import org.hibernate.dialect.Dialect;
+import org.hibernate.metamodel.mapping.JdbcMappingContainer;
+import org.hibernate.query.ReturnableType;
+import org.hibernate.query.sqm.TemporalUnit;
+import org.hibernate.query.sqm.produce.function.internal.PatternRenderer;
+import org.hibernate.sql.ast.SqlAstTranslator;
+import org.hibernate.sql.ast.spi.SqlAppender;
+import org.hibernate.sql.ast.tree.SqlAstNode;
+import org.hibernate.sql.ast.tree.expression.Expression;
+import org.hibernate.sql.ast.tree.expression.ExtractUnit;
+import org.hibernate.type.spi.TypeConfiguration;
+
+import java.util.List;
+
+import static org.hibernate.query.sqm.TemporalUnit.EPOCH;
+import static org.hibernate.type.spi.TypeConfiguration.getSqlTemporalType;
+
+public class OracleExtractFunction extends ExtractFunction {
+	public OracleExtractFunction(Dialect dialect, TypeConfiguration typeConfiguration) {
+		super( dialect, typeConfiguration );
+	}
+
+	@Override
+	public void render(
+			SqlAppender sqlAppender,
+			List<? extends SqlAstNode> sqlAstArguments,
+			ReturnableType<?> returnType,
+			SqlAstTranslator<?> walker) {
+		new PatternRenderer( extractPattern( sqlAstArguments ) ).render( sqlAppender, sqlAstArguments, walker );
+	}
+
+	@SuppressWarnings("deprecation")
+	private String extractPattern(List<? extends SqlAstNode> sqlAstArguments) {
+		final ExtractUnit field = (ExtractUnit) sqlAstArguments.get( 0 );
+		final TemporalUnit unit = field.getUnit();
+		if ( unit == EPOCH ) {
+			final Expression expression = (Expression) sqlAstArguments.get( 1 );
+			final JdbcMappingContainer type = expression.getExpressionType();
+			final TemporalType temporalType = type != null ? getSqlTemporalType( type ) : null;
+			if ( temporalType == TemporalType.DATE ) {
+				return "trunc((cast(from_tz(cast(?2 as timestamp),'UTC') as date) - date '1970-1-1')*86400)";
+			}
+		}
+		return dialect.extractPattern( unit );
+	}
+}

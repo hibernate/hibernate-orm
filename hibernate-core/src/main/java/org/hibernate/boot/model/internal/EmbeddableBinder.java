162b9354a4a098c30e5de2f39bb3f0f6be2dd868
Sylvain Lecoy <sylvain.lecoy@gmail.com>
2025-06-17 03:01:24 +0200

    HHH-19542: Embeddable property order for SecondaryTable is no longer causing issue

--- hibernate-core/src/main/java/org/hibernate/boot/model/internal/EmbeddableBinder.java
+++ hibernate-core/src/main/java/org/hibernate/boot/model/internal/EmbeddableBinder.java
@@ -352,15 +352,15 @@ public class EmbeddableBinder {
 		final PropertyBinder binder = new PropertyBinder();
 		binder.setDeclaringClass( inferredData.getDeclaringClass() );
 		binder.setName( inferredData.getPropertyName() );
-		binder.setValue(component);
+		binder.setValue( component );
 		binder.setMemberDetails( inferredData.getAttributeMember() );
 		binder.setAccessType( inferredData.getDefaultAccess() );
-		binder.setEmbedded(isComponentEmbedded);
-		binder.setHolder(propertyHolder);
-		binder.setId(isId);
-		binder.setEntityBinder(entityBinder);
-		binder.setInheritanceStatePerClass(inheritanceStatePerClass);
-		binder.setBuildingContext(context);
+		binder.setEmbedded( isComponentEmbedded );
+		binder.setHolder( propertyHolder );
+		binder.setId( isId );
+		binder.setEntityBinder( entityBinder );
+		binder.setInheritanceStatePerClass( inheritanceStatePerClass );
+		binder.setBuildingContext( context );
 		binder.makePropertyAndBind();
 		return binder;
 	}
@@ -455,7 +455,7 @@ public class EmbeddableBinder {
 		if ( LOG.isDebugEnabled() ) {
 			LOG.debug( "Binding component with path: " + subpath );
 		}
-		final PropertyHolder subholder = buildPropertyHolder(
+		final ComponentPropertyHolder subholder = buildPropertyHolder(
 				component,
 				subpath,
 				inferredData,
@@ -578,6 +578,8 @@ public class EmbeddableBinder {
 								+ "' is annotated '@GeneratedValue' but is not part of an identifier" );
 			}
 		}
+
+		subholder.checkPropertyConsistency();
 
 		if ( compositeUserType != null ) {
 			processCompositeUserType( component, compositeUserType );

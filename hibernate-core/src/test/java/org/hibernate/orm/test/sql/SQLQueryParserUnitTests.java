9f432e81ef7c6dae19be30282e9a6de9216e69b7
Stefan Trienen <stefan.dev.public@gmail.com>
2025-06-18 16:48:23 +0200

    HHH-19558: Improved Test

--- hibernate-core/src/test/java/org/hibernate/orm/test/sql/SQLQueryParserUnitTests.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/sql/SQLQueryParserUnitTests.java
@@ -13,6 +13,8 @@ import org.hibernate.testing.orm.junit.RequiresDialect;
 import org.hibernate.testing.orm.junit.SessionFactory;
 import org.hibernate.testing.orm.junit.SessionFactoryScope;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -24,25 +26,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SuppressWarnings("JUnitMalformedDeclaration")
 public class SQLQueryParserUnitTests {
 
-	@Test
+	@ParameterizedTest
 	@DomainModel
 	@SessionFactory
 	@RequiresDialect(H2Dialect.class)
-	void testJDBCEscapeSyntaxParsing(SessionFactoryScope scope) {
+	@ValueSource(strings = {
+			"{d '2025-06-18'}",
+			"{t '14:00'}",
+			"{t '14:00:00'}",
+			"{ts '2025-06-18T14:00'}",
+			"{ts '2025-06-18T14:00:00'}",
+			"{ts '2025-06-18T14:00:00.123'}",
+			"{ts '2025-06-18T14:00:00+01:00'}"})
+	void testJDBCEscapeSyntaxParsing(String variant, SessionFactoryScope scope) {
 		final SessionFactoryImplementor sessionFactory = scope.getSessionFactory();
-		final String sqlQuery = "select id, name from {h-domain}the_table where date = {d '2025-06-18'}";
+		final String sqlQuery = "select id, name from {h-domain}the_table where date = " + variant;
 
 		final String full = processSqlString( sqlQuery, "my_catalog", "my_schema", sessionFactory );
-		assertThat( full ).contains( "{d '2025-06-18'}" );
+		assertThat( full ).contains( variant );
 
 		final String catalogOnly = processSqlString( sqlQuery, "my_catalog", null, sessionFactory );
-		assertThat( catalogOnly ).contains( "{d '2025-06-18'}" );
+		assertThat( catalogOnly ).contains( variant );
 
 		final String schemaOnly = processSqlString( sqlQuery, null, "my_schema", sessionFactory );
-		assertThat( schemaOnly ).contains( "{d '2025-06-18'}" );
+		assertThat( schemaOnly ).contains( variant );
 
 		final String none = processSqlString( sqlQuery, null, null, sessionFactory );
-		assertThat( none ).contains( "{d '2025-06-18'}" );
+		assertThat( none ).contains( variant );
 	}
 
 	private static String processSqlString(

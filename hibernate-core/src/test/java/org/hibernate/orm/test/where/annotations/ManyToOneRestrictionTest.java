8892e3879e6ab8fb177b354b3c34bfc0a69b5ef2
Gavin King <gavin@hibernate.org>
2025-06-21 13:44:01 +0200

    HHH-19565 stricter test for fix to bug

--- hibernate-core/src/test/java/org/hibernate/orm/test/where/annotations/ManyToOneRestrictionTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/where/annotations/ManyToOneRestrictionTest.java
@@ -11,14 +11,16 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import org.hibernate.annotations.SQLRestriction;
 import org.hibernate.testing.orm.junit.EntityManagerFactoryScope;
+import org.hibernate.testing.orm.junit.JiraKey;
 import org.hibernate.testing.orm.junit.Jpa;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.AssertionsKt.assertNull;
 
 @Jpa(annotatedClasses = {ManyToOneRestrictionTest.X.class, ManyToOneRestrictionTest.Y.class})
 class ManyToOneRestrictionTest {
+	@JiraKey("HHH-19565")
 	@Test void test(EntityManagerFactoryScope scope) {
 		scope.inTransaction(em -> {
 			Y y = new Y();
@@ -28,24 +30,22 @@ class ManyToOneRestrictionTest {
 			em.persist(x);
 			em.persist(y);
 		});
+		// @SQLRestrictions should not be applied to
+		// foreign key associations, or the FK will
+		// be set to null when the entity is updated,
+		// leading to data loss
 		scope.inTransaction(em -> {
 			Y y = em.find(Y.class, 0L);
-			assertNull(y.x);
-			var fk =
-					em.createNativeQuery( "select xx from YY", long.class )
-							.getSingleResultOrNull();
-			assertNotNull(fk);
+			assertNotNull(y.x);
+			assertEquals(-1, y.x.id);
 			y.name = "hello";
 		});
 		scope.inTransaction(em -> {
 			Y y = em.find(Y.class, 0L);
-			assertNull(y.x);
-			var fk =
-					em.createNativeQuery( "select xx from YY", long.class )
-							.getSingleResultOrNull();
-			assertNotNull(fk);
+			assertNotNull(y.x);
+			assertEquals(-1, y.x.id);
+			assertEquals("hello", y.name);
 		});
-
 	}
 
 	@Entity

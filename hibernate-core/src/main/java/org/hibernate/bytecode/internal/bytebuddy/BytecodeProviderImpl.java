cc7133992daa8b403bf0bf6d31de4278de2ef5d0
Christian Beikov <christian.beikov@gmail.com>
2025-06-23 19:30:46 +0200

    HHH-19571 Make AccessOptimizer bytecode generation deterministic

--- hibernate-core/src/main/java/org/hibernate/bytecode/internal/bytebuddy/BytecodeProviderImpl.java
+++ hibernate-core/src/main/java/org/hibernate/bytecode/internal/bytebuddy/BytecodeProviderImpl.java
@@ -18,7 +18,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Callable;
 
 import org.hibernate.HibernateException;
 import org.hibernate.bytecode.enhance.internal.bytebuddy.EnhancerClassLocator;
@@ -184,7 +183,7 @@ public class BytecodeProviderImpl implements BytecodeProvider {
 				.method( setPropertyValuesMethodName )
 				.intercept( new Implementation.Simple( new SetPropertyValues( clazz, getterNames, setters ) ) )
 				.method( getPropertyNamesMethodName )
-				.intercept( MethodCall.call( new CloningPropertyCall( getterNames ) ) )
+				.intercept( new Implementation.Simple( new GetPropertyNames( getterNames ) ) )
 		);
 
 		try {
@@ -253,7 +252,7 @@ public class BytecodeProviderImpl implements BytecodeProvider {
 					.method( setPropertyValuesMethodName )
 					.intercept( new Implementation.Simple( new SetPropertyValues( clazz, propertyNames, setters ) ) )
 					.method( getPropertyNamesMethodName )
-					.intercept( MethodCall.call( new CloningPropertyCall( propertyNames ) ) )
+					.intercept( new Implementation.Simple( new GetPropertyNames( propertyNames ) ) )
 			);
 		}
 		else {
@@ -266,7 +265,7 @@ public class BytecodeProviderImpl implements BytecodeProvider {
 					.method( setPropertyValuesMethodName )
 					.intercept( new Implementation.Simple( new SetPropertyValues( clazz, propertyNames, setters ) ) )
 					.method( getPropertyNamesMethodName )
-					.intercept( MethodCall.call( new CloningPropertyCall( propertyNames ) ) )
+					.intercept( new Implementation.Simple( new GetPropertyNames( propertyNames ) ) )
 			);
 		}
 
@@ -1346,17 +1345,29 @@ public class BytecodeProviderImpl implements BytecodeProvider {
 		}
 	}
 
-	public static class CloningPropertyCall implements Callable<String[]> {
+	public static class GetPropertyNames implements ByteCodeAppender {
 
 		private final String[] propertyNames;
 
-		private CloningPropertyCall(String[] propertyNames) {
+		private GetPropertyNames(String[] propertyNames) {
 			this.propertyNames = propertyNames;
 		}
 
 		@Override
-		public String[] call() {
-			return propertyNames.clone();
+		public Size apply(
+				MethodVisitor methodVisitor,
+				Implementation.Context implementationContext,
+				MethodDescription instrumentedMethod) {
+			methodVisitor.visitLdcInsn( propertyNames.length );
+			methodVisitor.visitTypeInsn( Opcodes.ANEWARRAY, Type.getInternalName( String.class ) );
+			for ( int i = 0; i < propertyNames.length; i++ ) {
+				methodVisitor.visitInsn( Opcodes.DUP );
+				methodVisitor.visitLdcInsn( i );
+				methodVisitor.visitLdcInsn( propertyNames[i] );
+				methodVisitor.visitInsn( Opcodes.AASTORE );
+			}
+			methodVisitor.visitInsn( Opcodes.ARETURN );
+			return new Size( 4, instrumentedMethod.getStackSize() + 1 );
 		}
 	}
 

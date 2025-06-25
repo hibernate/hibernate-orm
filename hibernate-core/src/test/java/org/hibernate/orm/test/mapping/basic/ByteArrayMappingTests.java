9e590cd708d295adb7763e59de417017d628d072
Christian Beikov <christian.beikov@gmail.com>
2025-06-25 09:50:04 +0200

    HHH-19573 Special case primitive type names for plural basic types

--- hibernate-core/src/test/java/org/hibernate/orm/test/mapping/basic/ByteArrayMappingTests.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/mapping/basic/ByteArrayMappingTests.java
@@ -65,7 +65,7 @@ public class ByteArrayMappingTests {
 		}
 
 		{
-			final BasicAttributeMapping primitive = (BasicAttributeMapping) entityDescriptor.findAttributeMapping("wrapper");
+			final BasicAttributeMapping primitive = (BasicAttributeMapping) entityDescriptor.findAttributeMapping("boxed");
 			final JdbcMapping jdbcMapping = primitive.getJdbcMapping();
 			assertThat(jdbcMapping.getJavaTypeDescriptor().getJavaTypeClass(), equalTo(Byte[].class));
 			if ( dialect.supportsStandardArrays() ) {
@@ -134,7 +134,7 @@ public class ByteArrayMappingTests {
 		//tag::basic-bytearray-example[]
 		// mapped as VARBINARY
 		private byte[] primitive;
-		private Byte[] wrapper;
+		private Byte[] boxed;
 		@JavaType( ByteArrayJavaType.class )
 		private Byte[] wrapperOld;
 
@@ -151,7 +151,7 @@ public class ByteArrayMappingTests {
 		public EntityOfByteArrays(Integer id, byte[] primitive, Byte[] wrapper) {
 			this.id = id;
 			this.primitive = primitive;
-			this.wrapper = wrapper;
+			this.boxed = wrapper;
 			this.primitiveLob = primitive;
 			this.wrapperLob = wrapper;
 		}
@@ -159,7 +159,7 @@ public class ByteArrayMappingTests {
 		public EntityOfByteArrays(Integer id, byte[] primitive, Byte[] wrapper, byte[] primitiveLob, Byte[] wrapperLob) {
 			this.id = id;
 			this.primitive = primitive;
-			this.wrapper = wrapper;
+			this.boxed = wrapper;
 			this.primitiveLob = primitiveLob;
 			this.wrapperLob = wrapperLob;
 		}

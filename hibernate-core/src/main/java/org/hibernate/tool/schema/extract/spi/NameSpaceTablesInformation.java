6590bd5852d0ece03715a8df8c3aee8751e775ad
mehmetali2003 <mehmetaliemektar@gmail.com>
2025-06-29 17:01:43 +0300

    HHH-14270 GroupedSchemaValidatorImpl / NameSpaceTablesInformation does not find different-case table information in case insensitive environment

--- hibernate-core/src/main/java/org/hibernate/tool/schema/extract/spi/NameSpaceTablesInformation.java
+++ hibernate-core/src/main/java/org/hibernate/tool/schema/extract/spi/NameSpaceTablesInformation.java
@@ -22,7 +22,7 @@ public class NameSpaceTablesInformation {
 	}
 
 	public void addTableInformation(TableInformation tableInformation) {
-		tables.put( tableInformation.getName().getTableName().getText(), tableInformation );
+		tables.put( identifierHelper.toMetaDataObjectName( tableInformation.getName().getTableName() ), tableInformation );
 	}
 
 	public TableInformation getTableInformation(Table table) {

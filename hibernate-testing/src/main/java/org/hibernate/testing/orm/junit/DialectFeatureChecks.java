48f20d207a7e0f6cc7209d8addee8cf390d78084
Gavin King <gavin@hibernate.org>
2025-06-30 12:36:30 +0200

    get 'extract(epoch)' working Informix
    yay!

--- hibernate-testing/src/main/java/org/hibernate/testing/orm/junit/DialectFeatureChecks.java
+++ hibernate-testing/src/main/java/org/hibernate/testing/orm/junit/DialectFeatureChecks.java
@@ -557,13 +557,6 @@ abstract public class DialectFeatureChecks {
 		}
 	}
 
-	public static class SupportsExtractEpoch implements DialectFeatureCheck {
-		public boolean apply(Dialect dialect) {
-			// I could not find any reasonable way to implement this on Informix
-			return !( dialect instanceof InformixDialect );
-		}
-	}
-
 	public static class SupportsExtractDayOfWeekYearMonth implements DialectFeatureCheck {
 		public boolean apply(Dialect dialect) {
 			return !( dialect instanceof InformixDialect );

0ac033350b7e37b4d9eb59023b3965a616566140
Christian Beikov <christian.beikov@gmail.com>
2025-07-04 16:42:36 +0200

    HHH-19542 Ensure columns in embeddables default to correct table

--- hibernate-core/src/main/java/org/hibernate/boot/model/internal/PropertyHolderBuilder.java
+++ hibernate-core/src/main/java/org/hibernate/boot/model/internal/PropertyHolderBuilder.java
@@ -47,7 +47,7 @@ public final class PropertyHolderBuilder {
 	 *
 	 * @return PropertyHolder
 	 */
-	public static ComponentPropertyHolder buildPropertyHolder(
+	public static PropertyHolder buildPropertyHolder(
 			Component component,
 			String path,
 			PropertyData inferredData,

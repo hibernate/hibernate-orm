05881f112e6d11ed62c11eb22c1bda94e980094c
Gavin King <gavin@hibernate.org>
2025-07-12 11:13:46 +0200

    HHH-19608 add tests for table partitioning with 'options' and @PartitionKey

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/sql/partition/OraclePartitionedTableTest.java
@@ -0,0 +1,49 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.orm.test.sql.partition;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.PartitionKey;
+import org.hibernate.dialect.OracleDialect;
+import org.hibernate.testing.orm.junit.EntityManagerFactoryScope;
+import org.hibernate.testing.orm.junit.Jpa;
+import org.hibernate.testing.orm.junit.RequiresDialect;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@RequiresDialect(OracleDialect.class)
+@Jpa(annotatedClasses = OraclePartitionedTableTest.Partitioned.class)
+class OraclePartitionedTableTest {
+	@Test void test(EntityManagerFactoryScope scope) {
+		scope.inTransaction( session -> {
+			Partitioned partitioned = new Partitioned();
+			partitioned.id = 1L;
+			partitioned.pid = 500L;
+			session.persist( partitioned );
+		} );
+		scope.inTransaction( session -> {
+			Partitioned partitioned = session.find( Partitioned.class, 1L );
+			assertNotNull( partitioned );
+			partitioned.text = "updated";
+		} );
+	}
+	@Entity
+	@Table(name = "oraparts",
+		options =
+			"""
+			PARTITION BY RANGE (pid) (
+				PARTITION p1 VALUES LESS THAN (1000),
+				PARTITION p2 VALUES LESS THAN (2000)
+			)
+			""")
+	static class Partitioned {
+		@Id Long id;
+		@PartitionKey Long pid;
+		String text = "";
+	}
+}

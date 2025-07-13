9f6c6d6e5e5b03f59cd65f19ec3496b4e30369b8
Gavin King <gavin@hibernate.org>
2025-07-13 03:37:25 +0200

    downgrade two more DEBUG-level messages to TRACE

--- hibernate-core/src/main/java/org/hibernate/engine/transaction/jta/platform/internal/JtaPlatformResolverInitiator.java
+++ hibernate-core/src/main/java/org/hibernate/engine/transaction/jta/platform/internal/JtaPlatformResolverInitiator.java
@@ -29,7 +29,7 @@ public class JtaPlatformResolverInitiator implements StandardServiceInitiator<Jt
 				registry.requireService( StrategySelector.class )
 						.resolveStrategy( JtaPlatformResolver.class, setting );
 		if ( resolver == null ) {
-			log.debugf( "No JtaPlatformResolver was specified, using default [%s]", StandardJtaPlatformResolver.class.getName() );
+			log.tracef( "No JtaPlatformResolver was specified, using default [%s]", StandardJtaPlatformResolver.class.getName() );
 			return StandardJtaPlatformResolver.INSTANCE;
 		}
 		return resolver;

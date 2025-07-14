d1862220afadadeb76b15f658df83b993e4eefce
Andrea Boriero <andrea@hibernate.org>
2025-07-14 15:57:59 +0200

    HHH-19579 Criteria update join - Column 'code' in SET is ambiguous

--- hibernate-core/src/main/java/org/hibernate/sql/ast/spi/AbstractSqlAstTranslator.java
+++ hibernate-core/src/main/java/org/hibernate/sql/ast/spi/AbstractSqlAstTranslator.java
@@ -1143,7 +1143,7 @@ public abstract class AbstractSqlAstTranslator<T extends JdbcOperation> implemen
 		final List<ColumnReference> columnReferences = assignable.getColumnReferences();
 		final Expression assignedValue = assignment.getAssignedValue();
 		if ( columnReferences.size() == 1 ) {
-			columnReferences.get( 0 ).appendColumnForWrite( this, null );
+			appendAssignmentColumn( columnReferences.get( 0 ) );
 			appendSql( '=' );
 			final SqlTuple sqlTuple = getSqlTuple( assignedValue );
 			if ( sqlTuple != null ) {
@@ -1158,7 +1158,7 @@ public abstract class AbstractSqlAstTranslator<T extends JdbcOperation> implemen
 			char separator = OPEN_PARENTHESIS;
 			for ( ColumnReference columnReference : columnReferences ) {
 				appendSql( separator );
-				columnReference.appendColumnForWrite( this, null );
+				appendAssignmentColumn( columnReference );
 				separator = COMMA_SEPARATOR_CHAR;
 			}
 			appendSql( ")=" );
@@ -1167,7 +1167,7 @@ public abstract class AbstractSqlAstTranslator<T extends JdbcOperation> implemen
 		else {
 			assert assignedValue instanceof SqlTupleContainer;
 			final List<? extends Expression> expressions = ( (SqlTupleContainer) assignedValue ).getSqlTuple().getExpressions();
-			columnReferences.get( 0 ).appendColumnForWrite( this, null );
+			appendAssignmentColumn( columnReferences.get( 0 ) );
 			appendSql( '=' );
 			expressions.get( 0 ).accept( this );
 			for ( int i = 1; i < columnReferences.size(); i++ ) {
@@ -1177,6 +1177,10 @@ public abstract class AbstractSqlAstTranslator<T extends JdbcOperation> implemen
 				expressions.get( i ).accept( this );
 			}
 		}
+	}
+
+	protected void appendAssignmentColumn(ColumnReference column) {
+		column.appendColumnForWrite( this, null );
 	}
 
 	protected void visitSetAssignmentEmulateJoin(Assignment assignment, UpdateStatement statement) {

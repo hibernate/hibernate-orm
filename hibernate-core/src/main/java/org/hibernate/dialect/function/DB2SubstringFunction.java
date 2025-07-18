30c9faf945edf8a1ae85025705464d91c6c3667d
Christian Beikov <christian.beikov@gmail.com>
2025-07-18 09:24:25 +0200

    HHH-19621 Don't render code units for DB2 for i

--- hibernate-core/src/main/java/org/hibernate/dialect/function/DB2SubstringFunction.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/function/DB2SubstringFunction.java
@@ -29,7 +29,13 @@ import static org.hibernate.query.sqm.produce.function.FunctionParameterType.STR
  */
 public class DB2SubstringFunction extends AbstractSqmSelfRenderingFunctionDescriptor {
 
+	private final boolean needsCodeUnit;
+
 	public DB2SubstringFunction(TypeConfiguration typeConfiguration) {
+		this( true, typeConfiguration );
+	}
+
+	public DB2SubstringFunction(boolean needsCodeUnit, TypeConfiguration typeConfiguration) {
 		super(
 				"substring",
 				new ArgumentTypesValidator( StandardArgumentsValidators.between( 2, 4 ), STRING, INTEGER, INTEGER, FunctionParameterType.ANY ),
@@ -37,6 +43,7 @@ public class DB2SubstringFunction extends AbstractSqmSelfRenderingFunctionDescri
 						StandardBasicTypes.STRING ) ),
 				StandardFunctionArgumentTypeResolvers.invariant( typeConfiguration, STRING, INTEGER, INTEGER )
 		);
+		this.needsCodeUnit = needsCodeUnit;
 	}
 
 	@Override
@@ -51,7 +58,7 @@ public class DB2SubstringFunction extends AbstractSqmSelfRenderingFunctionDescri
 			sqlAppender.appendSql( ',' );
 			arguments.get( i ).accept( walker );
 		}
-		if ( argumentCount != 4 ) {
+		if ( argumentCount != 4 && needsCodeUnit ) {
 			sqlAppender.appendSql( ",codeunits32" );
 		}
 		sqlAppender.appendSql( ')' );

2e9eac2320dadb0d498ab4d708fc567447fc6c4f
Gavin King <gavin@hibernate.org>
2025-07-25 05:03:20 +0800

    HHH-19640 tests for update-generated fields and forced version increments

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/mapping/generated/GeneratedByDbOnForcedIncrementTest.java
@@ -0,0 +1,47 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.orm.test.mapping.generated;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.testing.orm.junit.EntityManagerFactoryScope;
+import org.hibernate.testing.orm.junit.Jpa;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import static org.hibernate.annotations.SourceType.DB;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Jpa(annotatedClasses = GeneratedByDbOnForcedIncrementTest.WithUpdateTimestamp.class)
+class GeneratedByDbOnForcedIncrementTest {
+	@Test void test(EntityManagerFactoryScope scope) throws InterruptedException {
+		var persisted = scope.fromTransaction( em -> {
+			var entity = new WithUpdateTimestamp();
+			em.persist( entity );
+			return entity;
+		} );
+		Thread.sleep( 100 );
+		var updated = scope.fromTransaction( em -> {
+			var entity = em.find( WithUpdateTimestamp.class, 0L );
+			entity.names.add( "Gavin" );
+			return entity;
+		} );
+		assertTrue( persisted.updated.isBefore( updated.updated ) );
+	}
+	@Entity
+	static class WithUpdateTimestamp {
+		@Id long id;
+		@Version long version;
+		@UpdateTimestamp(source = DB)
+		LocalDateTime updated;
+		@ElementCollection
+		Set<String> names;
+	}
+}

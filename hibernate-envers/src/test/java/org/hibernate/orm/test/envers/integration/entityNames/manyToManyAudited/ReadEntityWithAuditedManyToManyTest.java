50177c5b2efd0bb84a2fc59bfa27a7e885274a89
loic_lefevre <loic.lefevre@oracle.com>
2025-07-24 09:56:37 +0200

    HHH-19643 - Redirect some hibernate-envers tests to H2 as they use an H2 configured hibernate configuration file

--- hibernate-envers/src/test/java/org/hibernate/orm/test/envers/integration/entityNames/manyToManyAudited/ReadEntityWithAuditedManyToManyTest.java
+++ hibernate-envers/src/test/java/org/hibernate/orm/test/envers/integration/entityNames/manyToManyAudited/ReadEntityWithAuditedManyToManyTest.java
@@ -11,14 +11,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.MappingException;
+import org.hibernate.dialect.H2Dialect;
 import org.hibernate.orm.test.envers.AbstractOneSessionTest;
 import org.hibernate.orm.test.envers.Priority;
 
+import org.hibernate.testing.orm.junit.RequiresDialect;
 import org.junit.Test;
 
 /**
  * @author Hern&aacute;n Chanfreau
  */
+@RequiresDialect(H2Dialect.class)
 public class ReadEntityWithAuditedManyToManyTest extends AbstractOneSessionTest {
 
 	private long id_car1;

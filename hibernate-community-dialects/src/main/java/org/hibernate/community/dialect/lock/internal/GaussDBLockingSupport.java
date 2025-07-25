b505d6cae943a83919af27f6732abecfe3c68ede
SweetWuXiaoMei <sweetwuxiaomei@qq.com>
2025-07-25 17:25:49 +0800

    HHH-19652 - GaussDB locking support

--- hibernate-community-dialects/src/main/java/org/hibernate/community/dialect/lock/internal/GaussDBLockingSupport.java
+++ hibernate-community-dialects/src/main/java/org/hibernate/community/dialect/lock/internal/GaussDBLockingSupport.java
@@ -4,18 +4,12 @@
  */
 package org.hibernate.community.dialect.lock.internal;
 
-import jakarta.persistence.Timeout;
 import org.hibernate.dialect.RowLockStrategy;
 import org.hibernate.dialect.lock.spi.ConnectionLockTimeoutStrategy;
-import org.hibernate.dialect.lock.spi.LockTimeoutType;
 import org.hibernate.dialect.lock.spi.LockingSupport;
 import org.hibernate.dialect.lock.spi.OuterJoinLockingType;
 
 
-import static org.hibernate.Timeouts.NO_WAIT_MILLI;
-import static org.hibernate.Timeouts.SKIP_LOCKED_MILLI;
-import static org.hibernate.Timeouts.WAIT_FOREVER_MILLI;
-import static org.hibernate.dialect.lock.spi.LockTimeoutType.QUERY;
 
 /**
  * @author chen zhida
@@ -24,17 +18,7 @@ import static org.hibernate.dialect.lock.spi.LockTimeoutType.QUERY;
  */
 public class GaussDBLockingSupport implements LockingSupport, LockingSupport.Metadata, ConnectionLockTimeoutStrategy {
 	public static final LockingSupport LOCKING_SUPPORT = new GaussDBLockingSupport();
-	private final boolean supportsNoWait;
-	private final boolean supportsSkipLocked;
 
-	public GaussDBLockingSupport() {
-		this( true, true );
-	}
-
-	public GaussDBLockingSupport(boolean supportsNoWait, boolean supportsSkipLocked) {
-		this.supportsNoWait = supportsNoWait;
-		this.supportsSkipLocked = supportsSkipLocked;
-	}
 
 	@Override
 	public Metadata getMetadata() {
@@ -44,17 +28,6 @@ public class GaussDBLockingSupport implements LockingSupport, LockingSupport.Met
 	@Override
 	public RowLockStrategy getWriteRowLockStrategy() {
 		return RowLockStrategy.TABLE;
-	}
-
-	@Override
-	public LockTimeoutType getLockTimeoutType(Timeout timeout) {
-		return switch ( timeout.milliseconds() ) {
-			case NO_WAIT_MILLI -> supportsNoWait ? QUERY : LockTimeoutType.NONE;
-			case SKIP_LOCKED_MILLI -> supportsSkipLocked ? QUERY : LockTimeoutType.NONE;
-			case WAIT_FOREVER_MILLI -> LockTimeoutType.NONE;
-			// we can apply a timeout via the connection
-			default -> LockTimeoutType.CONNECTION;
-		};
 	}
 
 	@Override

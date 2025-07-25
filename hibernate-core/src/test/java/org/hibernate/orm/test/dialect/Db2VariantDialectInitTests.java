d12439b5a115a3ae5fd535898413d6e16bb885d9
loic_lefevre <loic.lefevre@oracle.com>
2025-07-25 15:52:48 +0200

    HHH-19645 - Some database specific tests are missing RequiresDialect annotation

--- hibernate-core/src/test/java/org/hibernate/orm/test/dialect/Db2VariantDialectInitTests.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/dialect/Db2VariantDialectInitTests.java
@@ -7,6 +7,7 @@ package org.hibernate.orm.test.dialect;
 import org.hibernate.dialect.DB2iDialect;
 import org.hibernate.dialect.DB2zDialect;
 import org.hibernate.testing.orm.junit.JiraKey;
+import org.hibernate.testing.orm.junit.RequiresDialect;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
@@ -15,6 +16,8 @@ import static org.junit.Assert.assertNotNull;
  * @author Steve Ebersole
  */
 @JiraKey(value = "HHH-15046")
+@RequiresDialect(DB2iDialect.class)
+@RequiresDialect(DB2zDialect.class)
 public class Db2VariantDialectInitTests {
 	@Test
 	public void testDB2zDialectInit() {
--- hibernate-core/src/test/java/org/hibernate/orm/test/dialect/unit/sequence/DB2zSequenceInformationExtractorTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/dialect/unit/sequence/DB2zSequenceInformationExtractorTest.java
@@ -6,6 +6,7 @@ package org.hibernate.orm.test.dialect.unit.sequence;
 
 import org.hibernate.dialect.DB2zDialect;
 import org.hibernate.dialect.Dialect;
+import org.hibernate.testing.orm.junit.RequiresDialect;
 import org.hibernate.tool.schema.extract.internal.SequenceInformationExtractorDB2DatabaseImpl;
 import org.hibernate.tool.schema.extract.spi.SequenceInformationExtractor;
 
@@ -15,6 +16,7 @@ import org.hibernate.testing.orm.junit.JiraKey;
  * @author Andrea Boriero
  */
 @JiraKey(value = "HHH-11470")
+@RequiresDialect(DB2zDialect.class)
 public class DB2zSequenceInformationExtractorTest extends AbstractSequenceInformationExtractorTest {
 
 	@Override

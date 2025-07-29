43638b37fdc8635b3b347bec11885e150d96db0e
Christian Beikov <christian.beikov@gmail.com>
2025-07-29 16:30:49 +0200

    HHH-18981 Handle aggregate function arguments to array_to_string in H2 and HSQL specially

--- hibernate-core/src/main/java/org/hibernate/dialect/function/array/H2ArrayToStringFunction.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/function/array/H2ArrayToStringFunction.java
@@ -9,10 +9,18 @@ package org.hibernate.dialect.function.array;
 import java.util.List;
 
 import org.hibernate.query.ReturnableType;
+import org.hibernate.query.sqm.function.SelfRenderingOrderedSetAggregateFunctionSqlAstExpression;
+import org.hibernate.sql.ast.Clause;
+import org.hibernate.sql.ast.SqlAstNodeRenderingMode;
 import org.hibernate.sql.ast.SqlAstTranslator;
 import org.hibernate.sql.ast.spi.SqlAppender;
 import org.hibernate.sql.ast.tree.SqlAstNode;
 import org.hibernate.sql.ast.tree.expression.Expression;
+import org.hibernate.sql.ast.tree.expression.FunctionExpression;
+import org.hibernate.sql.ast.tree.predicate.Predicate;
+import org.hibernate.sql.ast.tree.select.SortSpecification;
+import org.hibernate.type.BasicPluralType;
+import org.hibernate.type.SqlTypes;
 import org.hibernate.type.spi.TypeConfiguration;
 
 /**
@@ -38,26 +46,99 @@ public class H2ArrayToStringFunction extends ArrayToStringFunction {
 		final Expression arrayExpression = (Expression) sqlAstArguments.get( 0 );
 		final Expression separatorExpression = (Expression) sqlAstArguments.get( 1 );
 		final Expression defaultExpression = sqlAstArguments.size() > 2 ? (Expression) sqlAstArguments.get( 2 ) : null;
-		sqlAppender.append( "case when " );
-		arrayExpression.accept( walker );
-		sqlAppender.append( " is not null then coalesce((select listagg(" );
-		if ( defaultExpression != null ) {
-			sqlAppender.append( "coalesce(" );
-		}
-		sqlAppender.append( "array_get(" );
-		arrayExpression.accept( walker );
-		sqlAppender.append(",i.idx)" );
-		if ( defaultExpression != null ) {
+		final BasicPluralType<?, ?> pluralType = (BasicPluralType<?, ?>) arrayExpression.getExpressionType().getSingleJdbcMapping();
+		final int ddlTypeCode = pluralType.getElementType().getJdbcType().getDdlTypeCode();
+		final boolean needsCast = !SqlTypes.isStringType( ddlTypeCode );
+		if ( arrayExpression instanceof SelfRenderingOrderedSetAggregateFunctionSqlAstExpression
+				&& ArrayAggFunction.FUNCTION_NAME.equals( ( (FunctionExpression) arrayExpression ).getFunctionName() ) ) {
+			final SelfRenderingOrderedSetAggregateFunctionSqlAstExpression functionExpression
+					= (SelfRenderingOrderedSetAggregateFunctionSqlAstExpression) arrayExpression;
+			// When the array argument is an aggregate expression, we access its contents directly
+			final Expression arrayElementExpression = (Expression) functionExpression.getArguments().get( 0 );
+			final List<SortSpecification> withinGroup = functionExpression.getWithinGroup();
+			final Predicate filter = functionExpression.getFilter();
+
+			sqlAppender.append( "listagg(" );
+			if ( defaultExpression != null ) {
+				sqlAppender.append( "coalesce(" );
+			}
+			if ( needsCast ) {
+				if ( ddlTypeCode == SqlTypes.BOOLEAN ) {
+					// By default, H2 uses upper case, so lower it for a consistent experience
+					sqlAppender.append( "lower(" );
+				}
+				sqlAppender.append( "cast(" );
+			}
+			arrayElementExpression.accept( walker );
+			if ( needsCast ) {
+				sqlAppender.append( " as varchar)" );
+				if ( ddlTypeCode == SqlTypes.BOOLEAN ) {
+					sqlAppender.append( ')' );
+				}
+			}
+			if ( defaultExpression != null ) {
+				sqlAppender.append( ',' );
+				defaultExpression.accept( walker );
+				sqlAppender.append( ')' );
+			}
 			sqlAppender.append( "," );
-			defaultExpression.accept( walker );
-			sqlAppender.append( ")" );
+			walker.render( separatorExpression, SqlAstNodeRenderingMode.DEFAULT );
+			sqlAppender.appendSql( ')' );
+
+			if ( withinGroup != null && !withinGroup.isEmpty() ) {
+				walker.getCurrentClauseStack().push( Clause.WITHIN_GROUP );
+				sqlAppender.appendSql( " within group (order by " );
+				withinGroup.get( 0 ).accept( walker );
+				for ( int i = 1; i < withinGroup.size(); i++ ) {
+					sqlAppender.appendSql( ',' );
+					withinGroup.get( i ).accept( walker );
+				}
+				sqlAppender.appendSql( ')' );
+				walker.getCurrentClauseStack().pop();
+			}
+			if ( filter != null ) {
+				walker.getCurrentClauseStack().push( Clause.WHERE );
+				sqlAppender.appendSql( " filter (where " );
+				filter.accept( walker );
+				sqlAppender.appendSql( ')' );
+				walker.getCurrentClauseStack().pop();
+			}
 		}
-		sqlAppender.append("," );
-		separatorExpression.accept( walker );
-		sqlAppender.append( ") within group (order by i.idx) from system_range(1,");
-		sqlAppender.append( Integer.toString( maximumArraySize ) );
-		sqlAppender.append( ") i(idx) where i.idx<=coalesce(cardinality(");
-		arrayExpression.accept( walker );
-		sqlAppender.append("),0)),'') end" );
+		else {
+			sqlAppender.append( "case when " );
+			arrayExpression.accept( walker );
+			sqlAppender.append( " is not null then coalesce((select listagg(" );
+			if ( defaultExpression != null ) {
+				sqlAppender.append( "coalesce(" );
+			}
+			if ( needsCast ) {
+				if ( ddlTypeCode == SqlTypes.BOOLEAN ) {
+					// By default, H2 uses upper case, so lower it for a consistent experience
+					sqlAppender.append( "lower(" );
+				}
+				sqlAppender.append( "cast(" );
+			}
+			sqlAppender.append( "array_get(" );
+			arrayExpression.accept( walker );
+			sqlAppender.append( ",i.idx)" );
+			if ( needsCast ) {
+				sqlAppender.append( " as varchar)" );
+				if ( ddlTypeCode == SqlTypes.BOOLEAN ) {
+					sqlAppender.append( ')' );
+				}
+			}
+			if ( defaultExpression != null ) {
+				sqlAppender.append( ',' );
+				defaultExpression.accept( walker );
+				sqlAppender.append( ')' );
+			}
+			sqlAppender.append( "," );
+			walker.render( separatorExpression, SqlAstNodeRenderingMode.DEFAULT );
+			sqlAppender.append( ") within group (order by i.idx) from system_range(1," );
+			sqlAppender.append( Integer.toString( maximumArraySize ) );
+			sqlAppender.append( ") i(idx) where i.idx<=coalesce(cardinality(" );
+			arrayExpression.accept( walker );
+			sqlAppender.append( "),0)),'') end" );
+		}
 	}
 }
--- hibernate-core/src/main/java/org/hibernate/dialect/function/array/HSQLArrayToStringFunction.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/function/array/HSQLArrayToStringFunction.java
@@ -9,11 +9,18 @@ package org.hibernate.dialect.function.array;
 import java.util.List;
 
 import org.hibernate.query.ReturnableType;
+import org.hibernate.query.sqm.function.SelfRenderingOrderedSetAggregateFunctionSqlAstExpression;
+import org.hibernate.sql.ast.Clause;
 import org.hibernate.sql.ast.SqlAstNodeRenderingMode;
 import org.hibernate.sql.ast.SqlAstTranslator;
 import org.hibernate.sql.ast.spi.SqlAppender;
 import org.hibernate.sql.ast.tree.SqlAstNode;
 import org.hibernate.sql.ast.tree.expression.Expression;
+import org.hibernate.sql.ast.tree.expression.FunctionExpression;
+import org.hibernate.sql.ast.tree.predicate.Predicate;
+import org.hibernate.sql.ast.tree.select.SortSpecification;
+import org.hibernate.type.BasicPluralType;
+import org.hibernate.type.SqlTypes;
 import org.hibernate.type.spi.TypeConfiguration;
 
 /**
@@ -34,23 +41,96 @@ public class HSQLArrayToStringFunction extends ArrayToStringFunction {
 		final Expression arrayExpression = (Expression) sqlAstArguments.get( 0 );
 		final Expression separatorExpression = (Expression) sqlAstArguments.get( 1 );
 		final Expression defaultExpression = sqlAstArguments.size() > 2 ? (Expression) sqlAstArguments.get( 2 ) : null;
-		sqlAppender.append( "case when " );
-		arrayExpression.accept( walker );
-		sqlAppender.append( " is not null then coalesce((select group_concat(" );
-		if ( defaultExpression != null ) {
-			sqlAppender.append( "coalesce(" );
+		final BasicPluralType<?, ?> pluralType = (BasicPluralType<?, ?>) arrayExpression.getExpressionType().getSingleJdbcMapping();
+		final int ddlTypeCode = pluralType.getElementType().getJdbcType().getDdlTypeCode();
+		final boolean needsCast = !SqlTypes.isStringType( ddlTypeCode );
+		if ( arrayExpression instanceof SelfRenderingOrderedSetAggregateFunctionSqlAstExpression
+				&& ArrayAggFunction.FUNCTION_NAME.equals( ( (FunctionExpression) arrayExpression ).getFunctionName() ) ) {
+			final SelfRenderingOrderedSetAggregateFunctionSqlAstExpression functionExpression
+					= (SelfRenderingOrderedSetAggregateFunctionSqlAstExpression) arrayExpression;
+			// When the array argument is an aggregate expression, we access its contents directly
+			final Expression arrayElementExpression = (Expression) functionExpression.getArguments().get( 0 );
+			final List<SortSpecification> withinGroup = functionExpression.getWithinGroup();
+			final Predicate filter = functionExpression.getFilter();
+
+			sqlAppender.append( "group_concat(" );
+			if ( defaultExpression != null ) {
+				sqlAppender.append( "coalesce(" );
+			}
+			if ( needsCast ) {
+				if ( ddlTypeCode == SqlTypes.BOOLEAN ) {
+					// By default, HSQLDB uses upper case, so lower it for a consistent experience
+					sqlAppender.append( "lower(" );
+				}
+				sqlAppender.append( "cast(" );
+			}
+			arrayElementExpression.accept( walker );
+			if ( needsCast ) {
+				sqlAppender.append( " as longvarchar)" );
+				if ( ddlTypeCode == SqlTypes.BOOLEAN ) {
+					sqlAppender.append( ')' );
+				}
+			}
+			if ( defaultExpression != null ) {
+				sqlAppender.append( "," );
+				defaultExpression.accept( walker );
+				sqlAppender.append( ")" );
+			}
+
+			if ( withinGroup != null && !withinGroup.isEmpty() ) {
+				walker.getCurrentClauseStack().push( Clause.WITHIN_GROUP );
+				sqlAppender.appendSql( " order by " );
+				withinGroup.get( 0 ).accept( walker );
+				for ( int i = 1; i < withinGroup.size(); i++ ) {
+					sqlAppender.appendSql( ',' );
+					withinGroup.get( i ).accept( walker );
+				}
+				walker.getCurrentClauseStack().pop();
+			}
+			sqlAppender.append( " separator " );
+			// HSQLDB doesn't like non-literals as separator
+			walker.render( separatorExpression, SqlAstNodeRenderingMode.INLINE_PARAMETERS );
+			sqlAppender.appendSql( ')' );
+			if ( filter != null ) {
+				walker.getCurrentClauseStack().push( Clause.WHERE );
+				sqlAppender.appendSql( " filter (where " );
+				filter.accept( walker );
+				sqlAppender.appendSql( ')' );
+				walker.getCurrentClauseStack().pop();
+			}
 		}
-		sqlAppender.append( "t.val" );
-		if ( defaultExpression != null ) {
-			sqlAppender.append( "," );
-			defaultExpression.accept( walker );
-			sqlAppender.append( ")" );
+		else {
+			sqlAppender.append( "case when " );
+			arrayExpression.accept( walker );
+			sqlAppender.append( " is not null then coalesce((select group_concat(" );
+			if ( defaultExpression != null ) {
+				sqlAppender.append( "coalesce(" );
+			}
+			if ( needsCast ) {
+				if ( ddlTypeCode == SqlTypes.BOOLEAN ) {
+					// By default, HSQLDB uses upper case, so lower it for a consistent experience
+					sqlAppender.append( "lower(" );
+				}
+				sqlAppender.append( "cast(" );
+			}
+			sqlAppender.append( "t.val" );
+			if ( needsCast ) {
+				sqlAppender.append( " as longvarchar)" );
+				if ( ddlTypeCode == SqlTypes.BOOLEAN ) {
+					sqlAppender.append( ')' );
+				}
+			}
+			if ( defaultExpression != null ) {
+				sqlAppender.append( "," );
+				defaultExpression.accept( walker );
+				sqlAppender.append( ")" );
+			}
+			sqlAppender.append( " order by t.idx separator " );
+			// HSQLDB doesn't like non-literals as separator
+			walker.render( separatorExpression, SqlAstNodeRenderingMode.INLINE_PARAMETERS );
+			sqlAppender.append( ") from unnest(" );
+			arrayExpression.accept( walker );
+			sqlAppender.append( ") with ordinality t(val,idx)),'') end" );
 		}
-		sqlAppender.append( " order by t.idx separator " );
-		// HSQLDB doesn't like non-literals as separator
-		walker.render( separatorExpression, SqlAstNodeRenderingMode.INLINE_PARAMETERS );
-		sqlAppender.append( ") from unnest(");
-		arrayExpression.accept( walker );
-		sqlAppender.append(") with ordinality t(val,idx)),'') end" );
 	}
 }

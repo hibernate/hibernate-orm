88f0d8b53a60ef099ea0e194ba8709b05b4731e7
Steve Ebersole <steve@hibernate.org>
2025-08-07 10:31:43 -0500

    HHH-19692 - Drop org.hibernate.boot.jaxb.spi.XmlSource

--- hibernate-core/src/main/java/org/hibernate/boot/jaxb/internal/InputStreamXmlSource.java
+++ hibernate-core/src/main/java/org/hibernate/boot/jaxb/internal/InputStreamXmlSource.java
@@ -4,41 +4,54 @@
  */
 package org.hibernate.boot.jaxb.internal;
 
+import org.hibernate.boot.InvalidMappingException;
+import org.hibernate.boot.jaxb.Origin;
+import org.hibernate.boot.jaxb.SourceType;
+import org.hibernate.boot.jaxb.spi.Binding;
+import org.hibernate.boot.jaxb.spi.JaxbBindableMappingDescriptor;
+
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.hibernate.boot.InvalidMappingException;
-import org.hibernate.boot.jaxb.Origin;
-import org.hibernate.boot.jaxb.spi.Binder;
-import org.hibernate.boot.jaxb.spi.Binding;
-import org.hibernate.boot.jaxb.spi.XmlSource;
-import org.hibernate.internal.CoreLogging;
-
-import org.jboss.logging.Logger;
+import static org.hibernate.boot.jaxb.JaxbLogger.JAXB_LOGGER;
 
 /**
+ * Support for processing mapping XML from a {@linkplain InputStream} reference.
+ *
+ * @see MappingBinder
+ *
  * @author Steve Ebersole
  */
-public class InputStreamXmlSource extends XmlSource {
-	private static final Logger log = CoreLogging.logger( InputStreamXmlSource.class );
+public class InputStreamXmlSource {
+	/**
+	 * Create a mapping {@linkplain Binding binding} from an input stream.
+	 *
+	 * @apiNote This method does not close the given {@code inputStream}.
+	 */
+	public static Binding<? extends JaxbBindableMappingDescriptor> fromStream(
+			InputStream inputStream,
+			MappingBinder mappingBinder) {
+		JAXB_LOGGER.trace( "reading mappings from InputStream" );
 
-	private final InputStream inputStream;
-	private final boolean autoClose;
-
-	public InputStreamXmlSource(Origin origin, InputStream inputStream, boolean autoClose) {
-		super( origin );
-		this.inputStream = inputStream;
-		this.autoClose = autoClose;
+		final Origin origin = new Origin( SourceType.INPUT_STREAM, null );
+		return fromStream( inputStream, origin, false, mappingBinder );
 	}
 
-	@Override
-	public <T> Binding<T> doBind(Binder<T> binder) {
-		return doBind( binder, inputStream, getOrigin(), autoClose );
-	}
-
-	public static <T> Binding<T> doBind(Binder<T> binder, InputStream inputStream, Origin origin, boolean autoClose) {
+	/**
+	 * Utility form to create a {@linkplain Binding binding} from an input source.
+	 *
+	 * @param stream The stream from which to read the mappings
+	 * @param origin Description of the source from which the stream came
+	 * @param autoClose Whether to {@linkplain InputStream#close() close} the stream after we have processed it
+	 * @param binder The JAXB binder to use
+	 */
+	public static Binding<? extends JaxbBindableMappingDescriptor> fromStream(
+			InputStream stream,
+			Origin origin,
+			boolean autoClose,
+			MappingBinder binder) {
 		try {
-			return binder.bind( inputStream, origin );
+			return binder.bind( stream, origin );
 		}
 		catch ( Exception e ) {
 			throw new InvalidMappingException( origin, e );
@@ -46,10 +59,10 @@ public class InputStreamXmlSource extends XmlSource {
 		finally {
 			if ( autoClose ) {
 				try {
-					inputStream.close();
+					stream.close();
 				}
 				catch ( IOException ignore ) {
-					log.trace( "Was unable to close input stream" );
+					JAXB_LOGGER.trace( "Was unable to close input stream" );
 				}
 			}
 		}

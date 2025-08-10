a8cc9e19c6d196edbff024649be613027529cc65
Gavin King <gavin@hibernate.org>
2025-08-10 18:48:56 +1000

    HHH-19702 leave stub implementations behind
    Not really necessary, since these classes were internal

---
+++ hibernate-core/src/main/java/org/hibernate/engine/jdbc/connections/internal/DriverManagerConnectionProviderImpl.java
@@ -0,0 +1,12 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.engine.jdbc.connections.internal;
+
+/**
+ * @deprecated Use {@link DriverManagerConnectionProvider}
+ */
+@Deprecated(since = "7.1", forRemoval = true)
+public class DriverManagerConnectionProviderImpl extends DriverManagerConnectionProvider {
+}

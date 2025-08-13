40598f5e8a72c40161696d5f8e95c59f4686ad1a
Steve Ebersole <steve@hibernate.org>
2025-08-13 15:00:19 -0500

    HHH-19602 - Adjust JdbcOperation to allow more-than-one statement

---
+++ hibernate-core/src/main/java/org/hibernate/sql/exec/spi/SecondaryAction.java
@@ -0,0 +1,17 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.sql.exec.spi;
+
+import org.hibernate.Incubating;
+
+/**
+ * Common marker interface for {@linkplain PreAction} and {@linkplain PostAction},
+ * which are split to allow implementing both simultaneously.
+ *
+ * @author Steve Ebersole
+ */
+@Incubating
+public interface SecondaryAction {
+}

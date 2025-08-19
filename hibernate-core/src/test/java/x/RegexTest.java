1d86d62afa2de6f7a6ddd128f1f6d7f9ca35337c
Gavin King <gavin@hibernate.org>
2025-08-20 09:03:29 +1000

    HHH-3404 more tests

--- hibernate-core/src/test/java/x/RegexTest.java
+++ hibernate-core/src/test/java/x/RegexTest.java
@@ -4,6 +4,7 @@
  */
 package x;
 
+import org.hibernate.dialect.MariaDBDialect;
 import org.hibernate.dialect.OracleDialect;
 import org.hibernate.dialect.SQLServerDialect;
 import org.hibernate.dialect.SybaseASEDialect;
@@ -12,6 +13,7 @@ import org.hibernate.testing.orm.junit.Jpa;
 import org.hibernate.testing.orm.junit.SkipForDialect;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Jpa
@@ -19,11 +21,24 @@ class RegexTest {
 	@Test
 	@SkipForDialect(dialectClass = OracleDialect.class, majorVersion = 19)
 	@SkipForDialect(dialectClass = OracleDialect.class, majorVersion = 21)
-	@SkipForDialect(dialectClass = SybaseASEDialect.class)
 	@SkipForDialect(dialectClass = SQLServerDialect.class)
-	void test(EntityManagerFactoryScope scope) {
+	void testInSelect(EntityManagerFactoryScope scope) {
 		scope.inEntityManager( em -> {
 			assertTrue( em.createQuery( "select regexp_like('abcdef', 'ab.*')", Boolean.class ).getSingleResult() );
+		} );
+	}
+	@Test
+	@SkipForDialect(dialectClass = SybaseASEDialect.class)
+	@SkipForDialect(dialectClass = MariaDBDialect.class)
+	void testInSelectCaseInsensitive(EntityManagerFactoryScope scope) {
+		scope.inEntityManager( em -> {
+			assertTrue( em.createQuery( "select regexp_like('ABCDEF', 'ab.*', 'i')", Boolean.class ).getSingleResult() );
+		} );
+	}
+	@Test
+	void testInWhere(EntityManagerFactoryScope scope) {
+		scope.inEntityManager( em -> {
+			assertEquals( 1, em.createQuery( "select 1 where regexp_like('abcdef', 'ab.*')", Integer.class ).getSingleResult() );
 		} );
 	}
 }

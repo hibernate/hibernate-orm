c9e07255775b36d97b0a20646b34fb0200cc5edb
Gavin King <gavin@hibernate.org>
2025-08-19 23:58:16 +1000

    HHH-3404 skip tests on platforms with no regex support

--- hibernate-core/src/test/java/x/RegexTest.java
+++ hibernate-core/src/test/java/x/RegexTest.java
@@ -4,8 +4,12 @@
  */
 package x;
 
+import org.hibernate.dialect.OracleDialect;
+import org.hibernate.dialect.SQLServerDialect;
+import org.hibernate.dialect.SybaseASEDialect;
 import org.hibernate.testing.orm.junit.EntityManagerFactoryScope;
 import org.hibernate.testing.orm.junit.Jpa;
+import org.hibernate.testing.orm.junit.SkipForDialect;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -13,6 +17,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @Jpa
 class RegexTest {
 	@Test
+	@SkipForDialect(dialectClass = OracleDialect.class, majorVersion = 19)
+	@SkipForDialect(dialectClass = OracleDialect.class, majorVersion = 21)
+	@SkipForDialect(dialectClass = SybaseASEDialect.class)
+	@SkipForDialect(dialectClass = SQLServerDialect.class)
 	void test(EntityManagerFactoryScope scope) {
 		scope.inEntityManager( em -> {
 			assertTrue( em.createQuery( "select regexp_like('abcdef', 'ab.*')", Boolean.class ).getSingleResult() );

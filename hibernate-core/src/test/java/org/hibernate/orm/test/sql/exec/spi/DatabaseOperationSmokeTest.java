d0c3521d93a1abca189a28da5a2c263a9c6b42d1
Steve Ebersole <steve@hibernate.org>
2025-08-22 10:43:48 -0500

    HHH-19602 - Adjust JdbcOperation to allow more-than-one statement

--- hibernate-core/src/test/java/org/hibernate/orm/test/sql/exec/spi/DatabaseOperationSmokeTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/sql/exec/spi/DatabaseOperationSmokeTest.java
@@ -23,6 +23,7 @@ import org.hibernate.query.spi.QueryOptions;
 import org.hibernate.sql.ast.tree.expression.JdbcParameter;
 import org.hibernate.sql.ast.tree.select.SelectStatement;
 import org.hibernate.sql.exec.internal.BaseExecutionContext;
+import org.hibernate.sql.exec.internal.CallbackImpl;
 import org.hibernate.sql.exec.internal.JdbcParameterBindingImpl;
 import org.hibernate.sql.exec.internal.JdbcParameterBindingsImpl;
 import org.hibernate.sql.exec.internal.StandardStatementCreator;
@@ -35,7 +36,9 @@ import org.hibernate.sql.exec.spi.PostAction;
 import org.hibernate.sql.exec.spi.PreAction;
 import org.hibernate.sql.exec.spi.StatementAccess;
 import org.hibernate.sql.results.spi.SingleResultConsumer;
+import org.hibernate.testing.orm.junit.DialectFeatureChecks;
 import org.hibernate.testing.orm.junit.DomainModel;
+import org.hibernate.testing.orm.junit.RequiresDialectFeature;
 import org.hibernate.testing.orm.junit.SessionFactory;
 import org.hibernate.testing.orm.junit.SessionFactoryScope;
 import org.junit.jupiter.api.AfterEach;
@@ -43,6 +46,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 /**
@@ -96,14 +103,13 @@ public class DatabaseOperationSmokeTest {
 	}
 
 	@Test
+	@RequiresDialectFeature( feature = DialectFeatureChecks.SupportsConnectionLockTimeouts.class)
 	void testConnectionLockTimeout(SessionFactoryScope factoryScope) {
 		final SessionFactoryImplementor sessionFactory = factoryScope.getSessionFactory();
 
 		final LockingSupport lockingSupport = sessionFactory.getJdbcServices().getDialect().getLockingSupport();
 		final ConnectionLockTimeoutStrategy lockTimeoutStrategy = lockingSupport.getConnectionLockTimeoutStrategy();
-		if ( lockTimeoutStrategy.getSupportedLevel() == ConnectionLockTimeoutStrategy.Level.NONE ) {
-			return;
-		}
+		assert lockTimeoutStrategy.getSupportedLevel() != ConnectionLockTimeoutStrategy.Level.NONE;
 
 		final EntityPersister entityDescriptor = sessionFactory.getMappingMetamodel().findEntityDescriptor( Person.class );
 
@@ -138,6 +144,66 @@ public class DatabaseOperationSmokeTest {
 		} );
 	}
 
+	@Test
+	void testFollowOnLockingParadigm(SessionFactoryScope factoryScope) {
+		// NOTE: this just tests the principle -
+		// for now, just collect the values loaded.
+		// ultimately, this can be used to apply smarter follow-on locking.
+
+		final SessionFactoryImplementor sessionFactory = factoryScope.getSessionFactory();
+		final EntityPersister entityDescriptor = sessionFactory.getMappingMetamodel().findEntityDescriptor( Person.class );
+
+		final PersonQuery personQuery = createPersonQuery( entityDescriptor, sessionFactory );
+		final JdbcOperationQuerySelect jdbcOperation = personQuery.jdbcOperation();
+		final JdbcParameterBindings jdbcParameterBindings = personQuery.jdbcParameterBindings();
+
+		factoryScope.inTransaction( (session) -> {
+			final LoadedValueCollector loadedValueCollector = new LoadedValueCollector();
+
+			final Callback callback = new CallbackImpl();
+			callback.registerAfterLoadAction( (entity, entityMappingType, session1) ->  {
+				loadedValueCollector.loadedValues.add( entity );
+			} );
+
+			final SingleIdExecutionContext executionContext = new SingleIdExecutionContext(
+					session,
+					null,
+					1,
+					entityDescriptor,
+					QueryOptions.NONE,
+					callback
+			);
+
+
+			final DatabaseOperationSelectImpl.Builder operationBuilder = DatabaseOperationSelectImpl
+					.builder( jdbcOperation )
+					.appendPostAction( loadedValueCollector );
+
+			final ConnectionLockTimeoutStrategy lockTimeoutStrategy = session
+					.getDialect()
+					.getLockingSupport()
+					.getConnectionLockTimeoutStrategy();
+			if ( lockTimeoutStrategy.getSupportedLevel() != ConnectionLockTimeoutStrategy.Level.NONE ) {
+				final LockTimeoutHandler lockTimeoutHandler = new LockTimeoutHandler( Timeout.seconds( 2 ), lockTimeoutStrategy );
+				operationBuilder.addSecondaryActionPair( lockTimeoutHandler, lockTimeoutHandler );
+			}
+
+			final DatabaseOperationSelectImpl databaseOperation = operationBuilder.build();
+			final Person person = databaseOperation.execute(
+					Person.class,
+					1,
+					StandardStatementCreator.getStatementCreator( ScrollMode.FORWARD_ONLY ),
+					jdbcParameterBindings,
+					row -> (Person) row[0],
+					SingleResultConsumer.instance(),
+					executionContext
+			);
+
+			assertThat( loadedValueCollector.loadedValues ).hasSize( 1 );
+		} );
+
+	}
+
 	private static class LockTimeoutHandler implements PreAction, PostAction {
 		private final ConnectionLockTimeoutStrategy lockTimeoutStrategy;
 		private final Timeout timeout;
@@ -169,6 +235,17 @@ public class DatabaseOperationSmokeTest {
 
 			// reset the timeout
 			lockTimeoutStrategy.setLockTimeout( baseline, jdbcConnection, factory );
+		}
+	}
+
+	private static class LoadedValueCollector implements PostAction {
+		private final List<Object> loadedValues = new ArrayList<>();
+
+		@Override
+		public void performPostAction(StatementAccess jdbcStatementAccess, Connection jdbcConnection, ExecutionContext executionContext) {
+			loadedValues.forEach( (value) -> {
+				System.out.printf( "Loaded value: %s\n", value );
+			} );
 		}
 	}
 

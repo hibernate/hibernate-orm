016e26953309a260696640a4762eb18dead43d2a
Gavin King <gavin@hibernate.org>
2025-08-26 18:31:58 +1000

    HHH-19738 mask out 'jakarta.persistence.jdbc.password'

--- hibernate-core/src/main/java/org/hibernate/internal/util/config/ConfigurationHelper.java
+++ hibernate-core/src/main/java/org/hibernate/internal/util/config/ConfigurationHelper.java
@@ -270,7 +270,7 @@ public final class ConfigurationHelper {
 	}
 
 	/**
-	 * replace a property by a starred version
+	 * Replace a property by a starred version
 	 *
 	 * @param props properties to check
 	 * @param key property to mask
@@ -278,9 +278,27 @@ public final class ConfigurationHelper {
 	 * @return cloned and masked properties
 	 */
 	public static Properties maskOut(Properties props, String key) {
-		final Properties clone = (Properties) props.clone();
+		final var clone = (Properties) props.clone();
 		if ( clone.get( key ) != null ) {
 			clone.setProperty( key, "****" );
+		}
+		return clone;
+	}
+
+	/**
+	 * Replace properties by starred version
+	 *
+	 * @param props properties to check
+	 * @param keys properties to mask
+	 *
+	 * @return cloned and masked properties
+	 */
+	public static Properties maskOut(Properties props, String... keys) {
+		final var clone = (Properties) props.clone();
+		for ( String key : keys ) {
+			if ( clone.get( key ) != null ) {
+				clone.setProperty( key, "****" );
+			}
 		}
 		return clone;
 	}

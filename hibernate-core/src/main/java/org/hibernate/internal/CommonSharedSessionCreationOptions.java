6be678d667ba56858ce9479c95cb08a5e42be4e5
Gavin King <gavin@hibernate.org>
2025-09-12 06:36:34 +0200

    add missing @since annotations

--- hibernate-core/src/main/java/org/hibernate/internal/CommonSharedSessionCreationOptions.java
+++ hibernate-core/src/main/java/org/hibernate/internal/CommonSharedSessionCreationOptions.java
@@ -14,9 +14,12 @@ import org.hibernate.resource.transaction.spi.TransactionCoordinator;
  * Creation options for shared {@linkplain org.hibernate.engine.spi.SessionImplementor stateful}
  * and {@linkplain org.hibernate.engine.spi.StatelessSessionImplementor stateless} sessions.
  *
- * @implNote At the moment this is only used in the creation of {@linkplain org.hibernate.engine.spi.StatelessSessionImplementor stateless} sessions.
+ * @implNote At the moment this is only used in the creation of
+ *           {@linkplain org.hibernate.engine.spi.StatelessSessionImplementor stateless sessions}.
  *
  * @author Steve Ebersole
+ *
+ * @since 7.2
  */
 public interface CommonSharedSessionCreationOptions {
 

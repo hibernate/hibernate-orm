d349a6fd2086b8718b724f80b98f30a347dacfa2
Marco Belladelli <marcobladel@gmail.com>
2025-09-25 11:48:10 +0200

    HHH-19781 Add test for issue

--- hibernate-core/src/test/java/org/hibernate/orm/test/query/dynamic/BasicEntity.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/query/dynamic/BasicEntity.java
@@ -22,4 +22,36 @@ public class BasicEntity {
 
 	@ManyToOne
 	OtherEntity other;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public void setPosition(int position) {
+		this.position = position;
+	}
+
+	public OtherEntity getOther() {
+		return other;
+	}
+
+	public void setOther(OtherEntity other) {
+		this.other = other;
+	}
 }

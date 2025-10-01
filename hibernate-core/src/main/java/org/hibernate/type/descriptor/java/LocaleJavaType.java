21903c93a0bd3be18f768a36149b256d42a86b19
Christian Beikov <christian.beikov@gmail.com>
2025-10-01 19:19:11 +0200

    HHH-14032 Support parsing script and extensions in LocaleJavaType, as well as BCP 47 language tag format

--- hibernate-core/src/main/java/org/hibernate/type/descriptor/java/LocaleJavaType.java
+++ hibernate-core/src/main/java/org/hibernate/type/descriptor/java/LocaleJavaType.java
@@ -44,53 +44,137 @@ public class LocaleJavaType extends AbstractClassJavaType<Locale> {
 	}
 
 	public Locale fromString(CharSequence sequence) {
-		// TODO : Ultimately switch to Locale.Builder for this. However, Locale.Builder is Java 7
-
 		if ( sequence == null ) {
 			return null;
 		}
 
 		String string = sequence.toString();
-		if( string.isEmpty() ) {
+		if ( string.isEmpty() ) {
 			return Locale.ROOT;
 		}
 
-		boolean separatorFound = false;
+		final char[] chars = string.toCharArray();
+		final Locale.Builder builder = new Locale.Builder();
+		State state = State.LANGUAGE;
 		int position = 0;
-		char[] chars = string.toCharArray();
 
 		for ( int i = 0; i < chars.length; i++ ) {
 			// We just look for separators
 			if ( chars[i] == '_' ) {
-				if ( !separatorFound ) {
-					// On the first separator we know that we have at least a language
-					string = new String( chars, position, i - position );
-					position = i + 1;
-				}
-				else {
-					// On the second separator we have to check whether there are more chars available for variant
-					if ( chars.length > i + 1 ) {
-						// There is a variant so add it to the constructor
-						return new Locale( string, new String( chars, position, i - position ), new String( chars,
-								i + 1, chars.length - i - 1 ) );
-					}
-					else {
-						// No variant given, we just have language and country
-						return new Locale( string, new String( chars, position, i - position ), "" );
-					}
-				}
-
-				separatorFound = true;
+				state = state.parseNext( chars, position, i - position, builder );
+				position = i + 1;
+			}
+			// If we find a dash instead of an underscore, assume this is a BCP 47 language tag
+			else if ( state == State.LANGUAGE && chars[i] == '-' ) {
+				builder.setLanguageTag( string );
+				position = chars.length;
+				break;
 			}
 		}
 
-		if ( !separatorFound ) {
-			// No separator found, there is only a language
-			return new Locale( string );
+		if ( position != chars.length ) {
+			if ( state == State.LANGUAGE ) {
+				// This is special, we have no separators and at least 1 character, so assume it is a language tag.
+				// To retain backwards compatibility, convert old locale languages to the proper language tags.
+				// All the other locale languages are equivalent to the language tag languages.
+				builder.setLanguageTag( switch ( string ) {
+					case "iw", "Iw", "iW", "IW" -> "he";
+					case "ji", "Ji", "jI", "JI" -> "yi";
+					case "in", "In", "iN", "IN" -> "id";
+					default -> string;
+				} );
+			}
+			else {
+				state.parseNext( chars, position, chars.length - position, builder );
+			}
 		}
-		else {
-			// Only one separator found, there is a language and a country
-			return new Locale( string, new String( chars, position, chars.length - position ) );
+		return builder.build();
+	}
+
+	enum State {
+		LANGUAGE,
+		REGION,
+		VARIANT,
+		SCRIPT,
+		EXTENSION,
+		END;
+
+		State parseNext(char[] chars, int start, int length, Locale.Builder builder) {
+			return switch ( this ) {
+				case LANGUAGE -> {
+					builder.setLanguage( new String( chars, start, length ) );
+					yield REGION;
+				}
+				case REGION -> {
+					builder.setRegion( new String( chars, start, length ) );
+					yield VARIANT;
+				}
+				case VARIANT -> {
+					if ( chars[start] == '#' ) {
+						if ( isScript( chars, start + 1, length - 1 ) ) {
+							builder.setScript( new String( chars, start + 1, length - 1 ) );
+							yield EXTENSION;
+						}
+						else {
+							handleExtension( chars, start + 1, length - 1, builder );
+							yield END;
+						}
+					}
+					else {
+						builder.setVariant( new String( chars, start, length ) );
+						yield SCRIPT;
+					}
+				}
+				case SCRIPT -> {
+					if ( length < 5 || chars[start] != '#' ) {
+						throw new IllegalArgumentException( "Invalid script: " + new String( chars, start, length ) );
+					}
+					if ( isScript( chars, start + 1, length - 1 ) ) {
+						builder.setScript( new String( chars, start + 1, length - 1 ) );
+						yield EXTENSION;
+					}
+					else {
+						handleExtension( chars, start + 1, length - 1, builder );
+						yield END;
+					}
+				}
+				case EXTENSION -> {
+					handleExtension(  chars, start, length, builder );
+					yield END;
+				}
+				case END -> throw new IllegalStateException( "Unexpected continuation of locale value after extension: " + new String( chars, start, length ) );
+			};
+		}
+
+		private boolean isScript(char[] chars, int start, int length) {
+			return length == 4
+				&& Character.isLetter( chars[start] )
+				&& Character.isLetter( chars[start + 1] )
+				&& Character.isLetter( chars[start + 2] )
+				&& Character.isLetter( chars[start + 3] );
+		}
+
+		private void handleExtension(char[] chars, int start, int length, Locale.Builder builder) {
+			if ( length < 3 || chars[start + 1] != '-' ) {
+				throw new IllegalArgumentException( "Invalid extension: " + new String( chars, start, length ) );
+			}
+			if ( Character.toLowerCase( chars[start] ) == 'u' ) {
+				// After a Unicode extension, there could come a private use extension which we need to detect
+				int unicodeStart = start + 2;
+				int unicodeLength = length - 2;
+				final int end = start + length;
+				for ( int i = start + 2; i < end; i++ ) {
+					if ( chars[i] == '-' && i + 3 < end && chars[i + 1] == 'x' && chars[i + 2] == '-' ) {
+						builder.setExtension( 'x', new String( chars, i + 3, end - i - 3 ) );
+						unicodeLength = i - unicodeStart;
+						break;
+					}
+				}
+				builder.setExtension( chars[start], new String( chars, unicodeStart, unicodeLength ) );
+			}
+			else {
+				builder.setExtension( chars[start], new String( chars, start + 2, length - 2 ) );
+			}
 		}
 	}
 

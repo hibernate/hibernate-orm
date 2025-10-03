adc1885ea33092d66bedc17ee09ce7e838965ad5
Yoann Rodière <yoann@hibernate.org>
2025-10-03 19:39:52 +0200

    Fix license headers

--- hibernate-core/src/test/java/org/hibernate/orm/test/hql/customFunctions/CountItemsGreaterValSqmFunction.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/hql/customFunctions/CountItemsGreaterValSqmFunction.java
@@ -1,8 +1,6 @@
 /*
- * Hibernate, Relational Persistence for Idiomatic Java
- *
- * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
- * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
  */
 package org.hibernate.orm.test.hql.customFunctions;
 
@@ -31,111 +29,111 @@ import static org.hibernate.query.sqm.produce.function.FunctionParameterType.NUM
 
 //tag::hql-user-defined-dialect-function-sqm-renderer[]
 public class CountItemsGreaterValSqmFunction extends AbstractSqmSelfRenderingFunctionDescriptor {
-    private final CastFunction castFunction;
-    private final BasicType<BigDecimal> bigDecimalType;
+	private final CastFunction castFunction;
+	private final BasicType<BigDecimal> bigDecimalType;
 
-    public CountItemsGreaterValSqmFunction(String name, Dialect dialect, TypeConfiguration typeConfiguration) {
-        super(
-            name,
-            FunctionKind.AGGREGATE,
-            /* Function consumes 2 numeric typed args:
-            - the aggregation argument
-            - the bottom edge for the count predicate*/
-            new ArgumentTypesValidator(StandardArgumentsValidators.exactly(2),
-                    FunctionParameterType.NUMERIC,
-                    FunctionParameterType.NUMERIC
-            ),
-            // Function returns one value - the number of items
-            StandardFunctionReturnTypeResolvers.invariant(
-                    typeConfiguration.getBasicTypeRegistry()
-                            .resolve(StandardBasicTypes.BIG_INTEGER)
-            ),
-            StandardFunctionArgumentTypeResolvers.invariant(
-                    typeConfiguration, NUMERIC, NUMERIC
-            )
-        );
-        // Extracting cast function for setting input arguments to correct the type
-        castFunction = new CastFunction(
-                dialect,
-                dialect.getPreferredSqlTypeCodeForBoolean()
-        );
-        bigDecimalType = typeConfiguration.getBasicTypeRegistry()
-                .resolve(StandardBasicTypes.BIG_DECIMAL);
-    }
+	public CountItemsGreaterValSqmFunction(String name, Dialect dialect, TypeConfiguration typeConfiguration) {
+		super(
+			name,
+			FunctionKind.AGGREGATE,
+			/* Function consumes 2 numeric typed args:
+			- the aggregation argument
+			- the bottom edge for the count predicate*/
+			new ArgumentTypesValidator(StandardArgumentsValidators.exactly(2),
+					FunctionParameterType.NUMERIC,
+					FunctionParameterType.NUMERIC
+			),
+			// Function returns one value - the number of items
+			StandardFunctionReturnTypeResolvers.invariant(
+					typeConfiguration.getBasicTypeRegistry()
+							.resolve(StandardBasicTypes.BIG_INTEGER)
+			),
+			StandardFunctionArgumentTypeResolvers.invariant(
+					typeConfiguration, NUMERIC, NUMERIC
+			)
+		);
+		// Extracting cast function for setting input arguments to correct the type
+		castFunction = new CastFunction(
+				dialect,
+				dialect.getPreferredSqlTypeCodeForBoolean()
+		);
+		bigDecimalType = typeConfiguration.getBasicTypeRegistry()
+				.resolve(StandardBasicTypes.BIG_DECIMAL);
+	}
 
-    @Override
-    public void render(
-            SqlAppender sqlAppender,
-            List<? extends SqlAstNode> sqlAstArguments,
-            SqlAstTranslator<?> walker) {
-        render(sqlAppender, sqlAstArguments, null, walker);
-    }
+	@Override
+	public void render(
+			SqlAppender sqlAppender,
+			List<? extends SqlAstNode> sqlAstArguments,
+			SqlAstTranslator<?> walker) {
+		render(sqlAppender, sqlAstArguments, null, walker);
+	}
 
-    //tag::hql-user-defined-dialect-function-sqm-renderer-definition[]
-    @Override
-    public void render(
-            SqlAppender sqlAppender,
-            List<? extends SqlAstNode> sqlAstArguments,
-            Predicate filter,
-            SqlAstTranslator<?> translator) {
-        // Renderer definition
-        //end::hql-user-defined-dialect-function-sqm-renderer[]
+	//tag::hql-user-defined-dialect-function-sqm-renderer-definition[]
+	@Override
+	public void render(
+			SqlAppender sqlAppender,
+			List<? extends SqlAstNode> sqlAstArguments,
+			Predicate filter,
+			SqlAstTranslator<?> translator) {
+		// Renderer definition
+		//end::hql-user-defined-dialect-function-sqm-renderer[]
 
-        // Appending name of SQL function to result query
-        sqlAppender.appendSql(getName());
-        sqlAppender.appendSql('(');
+		// Appending name of SQL function to result query
+		sqlAppender.appendSql(getName());
+		sqlAppender.appendSql('(');
 
-        // Extracting 2 arguments
-        final Expression first_arg = (Expression) sqlAstArguments.get(0);
-        final Expression second_arg = (Expression) sqlAstArguments.get(1);
+		// Extracting 2 arguments
+		final Expression first_arg = (Expression) sqlAstArguments.get(0);
+		final Expression second_arg = (Expression) sqlAstArguments.get(1);
 
-        // If JPQL contains "filter" expression, but database doesn't support it
-        // then append: function_name(case when (filter_expr) then (argument) else null end)
-        final boolean caseWrapper = filter != null && !translator.supportsFilterClause();
-        if (caseWrapper) {
-            translator.getCurrentClauseStack().push(Clause.WHERE);
-            sqlAppender.appendSql("case when ");
+		// If JPQL contains "filter" expression, but database doesn't support it
+		// then append: function_name(case when (filter_expr) then (argument) else null end)
+		final boolean caseWrapper = filter != null && !translator.supportsFilterClause();
+		if (caseWrapper) {
+			translator.getCurrentClauseStack().push(Clause.WHERE);
+			sqlAppender.appendSql("case when ");
 
-            filter.accept(translator);
-            translator.getCurrentClauseStack().pop();
+			filter.accept(translator);
+			translator.getCurrentClauseStack().pop();
 
-            sqlAppender.appendSql(" then ");
-            renderArgument(sqlAppender, translator, first_arg);
-            sqlAppender.appendSql(" else null end)");
-        } else {
-            renderArgument(sqlAppender, translator, first_arg);
-            sqlAppender.appendSql(", ");
-            renderArgument(sqlAppender, translator, second_arg);
-            sqlAppender.appendSql(')');
-            if (filter != null) {
-                translator.getCurrentClauseStack().push(Clause.WHERE);
-                sqlAppender.appendSql(" filter (where ");
+			sqlAppender.appendSql(" then ");
+			renderArgument(sqlAppender, translator, first_arg);
+			sqlAppender.appendSql(" else null end)");
+		} else {
+			renderArgument(sqlAppender, translator, first_arg);
+			sqlAppender.appendSql(", ");
+			renderArgument(sqlAppender, translator, second_arg);
+			sqlAppender.appendSql(')');
+			if (filter != null) {
+				translator.getCurrentClauseStack().push(Clause.WHERE);
+				sqlAppender.appendSql(" filter (where ");
 
-                filter.accept(translator);
-                sqlAppender.appendSql(')');
-                translator.getCurrentClauseStack().pop();
-            }
-        }
-        //tag::hql-user-defined-dialect-function-sqm-renderer[]
-    }
+				filter.accept(translator);
+				sqlAppender.appendSql(')');
+				translator.getCurrentClauseStack().pop();
+			}
+		}
+		//tag::hql-user-defined-dialect-function-sqm-renderer[]
+	}
 
-    //end::hql-user-defined-dialect-function-sqm-renderer[]
-    private void renderArgument(
-            SqlAppender sqlAppender,
-            SqlAstTranslator<?> translator,
-            Expression arg) {
-        // Extracting the type of argument
-        final JdbcMapping sourceMapping = arg.getExpressionType().getJdbcMappings().get(0);
-        if (sourceMapping.getJdbcType().isNumber()) {
-            castFunction.render(sqlAppender,
-                    Arrays.asList(arg, new CastTarget(bigDecimalType)),
-                    translator
-            );
-        } else {
-            arg.accept(translator);
-        }
-    }
-    //tag::hql-user-defined-dialect-function-sqm-renderer[]
-    //end::hql-user-defined-dialect-function-sqm-renderer-definition[]
+	//end::hql-user-defined-dialect-function-sqm-renderer[]
+	private void renderArgument(
+			SqlAppender sqlAppender,
+			SqlAstTranslator<?> translator,
+			Expression arg) {
+		// Extracting the type of argument
+		final JdbcMapping sourceMapping = arg.getExpressionType().getJdbcMappings().get(0);
+		if (sourceMapping.getJdbcType().isNumber()) {
+			castFunction.render(sqlAppender,
+					Arrays.asList(arg, new CastTarget(bigDecimalType)),
+					translator
+			);
+		} else {
+			arg.accept(translator);
+		}
+	}
+	//tag::hql-user-defined-dialect-function-sqm-renderer[]
+	//end::hql-user-defined-dialect-function-sqm-renderer-definition[]
 }
 //end::hql-user-defined-dialect-function-sqm-renderer[]
--- hibernate-core/src/test/java/org/hibernate/orm/test/hql/customFunctions/Employee.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/hql/customFunctions/Employee.java
@@ -1,8 +1,6 @@
 /*
- * Hibernate, Relational Persistence for Idiomatic Java
- *
- * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
- * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
  */
 package org.hibernate.orm.test.hql.customFunctions;
 
@@ -13,57 +11,57 @@ import jakarta.persistence.Id;
 //tag::hql-examples-domain-model-example[]
 @Entity
 public class Employee {
-    @Id
-    private Long id;
-    private Long salary;
-    private String name;
-    private String surname;
+	@Id
+	private Long id;
+	private Long salary;
+	private String name;
+	private String surname;
 
-    public Employee(Long id, Long salary, String name, String surname) {
-        this.id = id;
-        this.salary = salary;
-        this.name = name;
-        this.surname = surname;
-    }
+	public Employee(Long id, Long salary, String name, String surname) {
+		this.id = id;
+		this.salary = salary;
+		this.name = name;
+		this.surname = surname;
+	}
 
-    //Getters and setters are omitted for brevity
+	//Getters and setters are omitted for brevity
 
 //end::hql-examples-domain-model-example[]
 
-    public Employee() {
-    }
+	public Employee() {
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public Long getSalary() {
-        return salary;
-    }
+	public Long getSalary() {
+		return salary;
+	}
 
-    public void setSalary(Long salary) {
-        this.salary = salary;
-    }
+	public void setSalary(Long salary) {
+		this.salary = salary;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getSurname() {
-        return surname;
-    }
+	public String getSurname() {
+		return surname;
+	}
 
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-    //tag::hql-examples-domain-model-example[]
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	//tag::hql-examples-domain-model-example[]
 }
 //end::hql-examples-domain-model-example[]

19a9600318b6276858b48ebb0eaf50eb11075af7
Andrea Boriero <andrea@hibernate.org>
2025-10-10 16:33:25 +0200

    HHH-19846 Drop JUnit 4 usage

--- hibernate-core/src/test/java/org/hibernate/orm/test/annotations/entity/HibernateAnnotationMappingTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/annotations/entity/HibernateAnnotationMappingTest.java
@@ -4,37 +4,38 @@
  */
 package org.hibernate.orm.test.annotations.entity;
 
-import static org.junit.Assert.fail;
-
-import java.util.ConcurrentModificationException;
-
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
+import org.hibernate.testing.orm.junit.BaseUnitTest;
 import org.hibernate.testing.orm.junit.JiraKey;
-import org.hibernate.testing.junit4.BaseUnitTestCase;
 import org.hibernate.testing.util.ServiceRegistryUtil;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import java.util.ConcurrentModificationException;
+
+import static org.assertj.core.api.Fail.fail;
 
 /**
  * @author Guenther Demetz
  */
-public class HibernateAnnotationMappingTest extends BaseUnitTestCase {
+@BaseUnitTest
+public class HibernateAnnotationMappingTest {
 
 	@Test
-	@JiraKey( value = "HHH-7446" )
-	public void testUniqueConstraintAnnotationOnNaturalIds() throws Exception {
+	@JiraKey(value = "HHH-7446")
+	public void testUniqueConstraintAnnotationOnNaturalIds() {
 		Configuration configuration = new Configuration();
 		ServiceRegistryUtil.applySettings( configuration.getStandardServiceRegistryBuilder() );
 		configuration.setProperty( Environment.HBM2DDL_AUTO, "create-drop" );
-		configuration.addAnnotatedClass( Month.class);
-		SessionFactory sf = null;
+		configuration.addAnnotatedClass( Month.class );
+		SessionFactory sf;
 		try {
 			sf = configuration.buildSessionFactory();
 			sf.close();
 		}
 		catch (ConcurrentModificationException e) {
-			fail(e.toString());
+			fail( e.toString() );
 		}
 	}
 }

0af6fa596578d2816d1ee78dddecd7d28c4f02d2
Andrea Boriero <andrea@hibernate.org>
2025-10-13 14:29:40 +0200

    HHH-19846 Drop JUnit 4 usage

--- hibernate-core/src/test/java/org/hibernate/orm/test/annotations/index/jpa/OrmXmlIndexTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/annotations/index/jpa/OrmXmlIndexTest.java
@@ -5,12 +5,13 @@
 package org.hibernate.orm.test.annotations.index.jpa;
 
 
+import org.hibernate.testing.orm.junit.DomainModel;
+
 /**
  * @author Strong Liu
  */
+@DomainModel(
+		xmlMappings = "org/hibernate/orm/test/annotations/index/jpa/orm-index.xml"
+)
 public class OrmXmlIndexTest extends AbstractJPAIndexTest {
-	@Override
-	protected String[] getXmlFiles() {
-		return new String[] { "org/hibernate/orm/test/annotations/index/jpa/orm-index.xml" };
-	}
 }

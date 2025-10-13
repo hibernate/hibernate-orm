c96092098dc2b6ca165ee3e7d894a4219925565a
Andrea Boriero <andrea@hibernate.org>
2025-10-13 21:29:25 +0200

    HHH-19846 Drop JUnit 4 usage

--- hibernate-core/src/test/java/org/hibernate/orm/test/annotations/lob/locator/LobLocatorTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/annotations/lob/locator/LobLocatorTest.java
@@ -4,28 +4,29 @@
  */
 package org.hibernate.orm.test.annotations.lob.locator;
 
-import java.sql.SQLException;
-
-import org.junit.Assert;
-import org.junit.Test;
-
-import org.hibernate.Session;
-import org.hibernate.testing.DialectChecks;
-import org.hibernate.testing.RequiresDialectFeature;
+import org.hibernate.testing.orm.junit.DialectFeatureChecks;
+import org.hibernate.testing.orm.junit.DomainModel;
 import org.hibernate.testing.orm.junit.JiraKey;
-import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
+import org.hibernate.testing.orm.junit.RequiresDialectFeature;
+import org.hibernate.testing.orm.junit.SessionFactory;
+import org.hibernate.testing.orm.junit.SessionFactoryScope;
 import org.hibernate.type.descriptor.java.DataHelper;
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.BDDAssertions.fail;
 import static org.hibernate.Hibernate.getLobHelper;
 
 /**
  * @author Lukasz Antoniak
  */
-public class LobLocatorTest extends BaseCoreFunctionalTestCase {
-	@Override
-	protected Class<?>[] getAnnotatedClasses() {
-		return new Class<?>[] { LobHolder.class };
-	}
+@DomainModel(
+		annotatedClasses = {
+				LobHolder.class
+		}
+)
+@SessionFactory
+public class LobLocatorTest {
 
 	/**
 	 * Specific JDBC drivers (e.g. SQL Server) may not automatically rewind bound input stream
@@ -34,47 +35,56 @@ public class LobLocatorTest extends BaseCoreFunctionalTestCase {
 	 */
 	@Test
 	@JiraKey(value = "HHH-8193")
-	@RequiresDialectFeature(DialectChecks.UsesInputStreamToInsertBlob.class)
-	public void testStreamResetBeforeParameterBinding() throws SQLException {
-		final Session session = openSession();
+	@RequiresDialectFeature(feature = DialectFeatureChecks.UsesInputStreamToInsertBlob.class)
+	public void testStreamResetBeforeParameterBinding(SessionFactoryScope scope) {
 
-		session.getTransaction().begin();
-		LobHolder entity = new LobHolder(
-				getLobHelper().createBlob( "blob".getBytes() ),
-				getLobHelper().createClob( "clob" ), 0
+		scope.inTransaction(
+				session -> {
+					LobHolder entity = new LobHolder(
+							getLobHelper().createBlob( "blob".getBytes() ),
+							getLobHelper().createClob( "clob" ), 0
+					);
+					session.persist( entity );
+					session.getTransaction().commit();
+
+					final int updatesLimit = 3;
+
+					for ( int i = 1; i <= updatesLimit; ++i ) {
+						session.getTransaction().begin();
+						entity = session.find( LobHolder.class, entity.getId() );
+						entity.setCounter( i );
+						entity = session.merge( entity );
+						session.getTransaction().commit();
+					}
+
+					session.getTransaction().begin();
+					entity = session.find( LobHolder.class, entity.getId() );
+					entity.setBlobLocator( getLobHelper().createBlob( "updated blob".getBytes() ) );
+					entity.setClobLocator( getLobHelper().createClob( "updated clob" ) );
+					entity = session.merge( entity );
+					session.getTransaction().commit();
+
+					session.clear();
+
+					session.getTransaction().begin();
+					try {
+						checkState(
+								"updated blob".getBytes(),
+								"updated clob",
+								updatesLimit,
+								session.find( LobHolder.class, entity.getId() )
+						);
+					}
+					catch (Exception e) {
+						fail( e );
+					}
+				}
 		);
-		session.persist( entity );
-		session.getTransaction().commit();
-
-		final Integer updatesLimit = 3;
-
-		for ( int i = 1; i <= updatesLimit; ++i ) {
-			session.getTransaction().begin();
-			entity = (LobHolder) session.get( LobHolder.class, entity.getId() );
-			entity.setCounter( i );
-			entity = (LobHolder) session.merge( entity );
-			session.getTransaction().commit();
-		}
-
-		session.getTransaction().begin();
-		entity = (LobHolder) session.get( LobHolder.class, entity.getId() );
-		entity.setBlobLocator( getLobHelper().createBlob( "updated blob".getBytes() ) );
-		entity.setClobLocator( getLobHelper().createClob( "updated clob" ) );
-		entity = (LobHolder) session.merge( entity );
-		session.getTransaction().commit();
-
-		session.clear();
-
-		session.getTransaction().begin();
-		checkState( "updated blob".getBytes(), "updated clob", updatesLimit, (LobHolder) session.get( LobHolder.class, entity.getId() ) );
-		session.getTransaction().commit();
-
-		session.close();
 	}
 
-	private void checkState(byte[] blob, String clob, Integer counter, LobHolder entity) throws SQLException {
-		Assert.assertEquals( counter, entity.getCounter() );
-		Assert.assertArrayEquals( blob, DataHelper.extractBytes( entity.getBlobLocator().getBinaryStream() ) );
-		Assert.assertEquals( clob, DataHelper.extractString( entity.getClobLocator() ) );
+	private void checkState(byte[] blob, String clob, Integer counter, LobHolder entity) throws Exception {
+		assertThat( entity.getCounter() ).isEqualTo( counter );
+		assertThat( DataHelper.extractBytes( entity.getBlobLocator().getBinaryStream() ) ).isEqualTo( blob );
+		assertThat( DataHelper.extractString( entity.getClobLocator() ) ).isEqualTo( clob );
 	}
 }

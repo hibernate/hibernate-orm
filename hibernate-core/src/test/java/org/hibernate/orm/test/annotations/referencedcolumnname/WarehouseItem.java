9a1fca8b52885f6c5661978ae3cd3f03b6f3fd4a
Andrea Boriero <andrea@hibernate.org>
2025-10-16 09:34:39 +0200

    HHH-19846 Drop JUnit 4 usage

--- hibernate-core/src/test/java/org/hibernate/orm/test/annotations/referencedcolumnname/WarehouseItem.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/annotations/referencedcolumnname/WarehouseItem.java
@@ -3,6 +3,7 @@
  * Copyright Red Hat Inc. and Hibernate Authors
  */
 package org.hibernate.orm.test.annotations.referencedcolumnname;
+
 import java.math.BigDecimal;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;

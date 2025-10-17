2cc9d41983b21432425f1ea15c0a54bf20f8e8cb
Andrea Boriero <andrea@hibernate.org>
2025-10-17 08:57:32 +0200

    HHH-19846 Drop JUnit 4 usage

--- hibernate-core/src/test/java/org/hibernate/orm/test/annotations/uniqueconstraint/UniqueConstraintThrowsConstraintViolationExceptionTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/annotations/uniqueconstraint/UniqueConstraintThrowsConstraintViolationExceptionTest.java
@@ -10,56 +10,43 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.PersistenceException;
 import jakarta.persistence.Table;
-
 import org.hibernate.exception.ConstraintViolationException;
-
+import org.hibernate.testing.orm.junit.DomainModel;
 import org.hibernate.testing.orm.junit.JiraKey;
-import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
-import org.junit.Test;
+import org.hibernate.testing.orm.junit.SessionFactory;
+import org.hibernate.testing.orm.junit.SessionFactoryScope;
+import org.junit.jupiter.api.Test;
 
-import static org.hibernate.testing.transaction.TransactionUtil.doInHibernate;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 /**
  * @author Vlad Mihalcea
  */
 @JiraKey(value = "HHH-11236")
-public class UniqueConstraintThrowsConstraintViolationExceptionTest extends BaseCoreFunctionalTestCase {
+@DomainModel(
+		annotatedClasses = {
+				UniqueConstraintThrowsConstraintViolationExceptionTest.Customer.class
+		}
+)
+@SessionFactory
+public class UniqueConstraintThrowsConstraintViolationExceptionTest {
 
-	@Override
-	protected Class<?>[] getAnnotatedClasses() {
-		return new Class[] { Customer.class };
-	}
 
 	@Test
-	public void testUniqueConstraintWithEmptyColumnName() {
-		doInHibernate( this::sessionFactory, session -> {
+	public void testUniqueConstraintWithEmptyColumnName(SessionFactoryScope scope) {
+		scope.inTransaction( session -> {
 			Customer customer1 = new Customer();
 			customer1.customerId = "123";
 			session.persist( customer1 );
 		} );
-		try {
-			doInHibernate( this::sessionFactory, session -> {
-				Customer customer1 = new Customer();
-				customer1.customerId = "123";
-				session.persist( customer1 );
-			} );
-			fail( "Should throw" );
-		}
-		catch ( PersistenceException e ) {
-			assertEquals(
-					ConstraintViolationException.class,
-					e.getClass()
-			);
-		}
-	}
 
-	@Override
-	protected boolean isCleanupTestDataRequired() {
-		return true;
+		assertThrows( ConstraintViolationException.class, () -> scope.inTransaction( session -> {
+			Customer customer1 = new Customer();
+			customer1.customerId = "123";
+			session.persist( customer1 );
+		} ) );
 	}
 
 	@Entity(name = "Customer")

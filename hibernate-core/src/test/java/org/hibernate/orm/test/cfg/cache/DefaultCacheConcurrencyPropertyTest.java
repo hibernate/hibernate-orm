f39f97847292e7ed275b789d18941e402d14103f
Andrea Boriero <andrea@hibernate.org>
2025-10-20 14:34:17 +0200

    HHH-19846 Drop JUnit 4 usage

--- hibernate-core/src/test/java/org/hibernate/orm/test/cfg/cache/DefaultCacheConcurrencyPropertyTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/cfg/cache/DefaultCacheConcurrencyPropertyTest.java
@@ -8,7 +8,6 @@ import jakarta.persistence.Cacheable;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
 import org.hibernate.annotations.Immutable;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
@@ -19,53 +18,44 @@ import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.engine.config.spi.ConfigurationService;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.persister.entity.EntityPersister;
-
+import org.hibernate.testing.orm.junit.BaseUnitTest;
 import org.hibernate.testing.orm.junit.JiraKey;
-import org.hibernate.testing.junit4.BaseUnitTestCase;
 import org.hibernate.testing.util.ServiceRegistryUtil;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 /**
  * @author Gail Badner
  */
-public class DefaultCacheConcurrencyPropertyTest extends BaseUnitTestCase {
+@BaseUnitTest
+public class DefaultCacheConcurrencyPropertyTest {
 
 	@Test
-	@JiraKey( value = "HHH-9763" )
+	@JiraKey(value = "HHH-9763")
 	public void testExplicitDefault() {
 
 		final StandardServiceRegistry ssr = ServiceRegistryUtil.serviceRegistryBuilder()
 				.applySetting( AvailableSettings.DEFAULT_CACHE_CONCURRENCY_STRATEGY, "read-only" )
 				.build();
 		try {
-			assertEquals(
-					"read-only",
-					ssr.getService( ConfigurationService.class ).getSettings().get(
-							AvailableSettings.DEFAULT_CACHE_CONCURRENCY_STRATEGY
-					)
-			);
+			assertThat( ssr.getService( ConfigurationService.class ).getSettings()
+					.get( AvailableSettings.DEFAULT_CACHE_CONCURRENCY_STRATEGY ) )
+					.isEqualTo( "read-only" );
 			final MetadataImplementor metadata = (MetadataImplementor) new MetadataSources( ssr )
 					.addAnnotatedClass( TheEntity.class )
 					.buildMetadata();
-			assertEquals(
-					AccessType.READ_ONLY,
-					metadata.getMetadataBuildingOptions().getMappingDefaults().getImplicitCacheAccessType()
-			);
-			final SessionFactoryImplementor sf = (SessionFactoryImplementor) metadata.buildSessionFactory();
-			try {
+			assertThat( metadata.getMetadataBuildingOptions().getMappingDefaults().getImplicitCacheAccessType() )
+					.isEqualTo( AccessType.READ_ONLY );
+
+			try (SessionFactoryImplementor sf = metadata.buildSessionFactory()) {
 				final EntityPersister persister = sf.getRuntimeMetamodels()
 						.getMappingMetamodel()
 						.getEntityDescriptor( TheEntity.class.getName() );
-				assertTrue( persister.canReadFromCache() );
-				assertTrue( persister.canWriteToCache() );
-				assertNotNull( persister.getCacheAccessStrategy() );
-			}
-			finally {
-				sf.close();
+				assertThat( persister.canReadFromCache() ).isTrue();
+				assertThat( persister.canWriteToCache() ).isTrue();
+				assertThat( persister.getCacheAccessStrategy() ).isNotNull();
 			}
 		}
 		finally {

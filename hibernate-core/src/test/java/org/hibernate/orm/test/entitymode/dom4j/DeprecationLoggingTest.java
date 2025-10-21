8d2ef5e7e603757ced6091de0b7d732e4d52d4e3
Andrea Boriero <andrea@hibernate.org>
2025-10-21 13:28:31 +0200

    HHH-19846 Drop JUnit 4 usage
    # Conflicts:
    #	hibernate-core/src/test/java/org/hibernate/orm/test/entitymode/map/basic/DynamicClassTest.java

--- hibernate-core/src/test/java/org/hibernate/orm/test/entitymode/dom4j/DeprecationLoggingTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/entitymode/dom4j/DeprecationLoggingTest.java
@@ -9,12 +9,11 @@ import org.hibernate.boot.registry.BootstrapServiceRegistry;
 import org.hibernate.boot.registry.BootstrapServiceRegistryBuilder;
 import org.hibernate.internal.log.DeprecationLogger;
 import org.hibernate.service.ServiceRegistry;
-
-import org.hibernate.testing.junit4.BaseUnitTestCase;
-import org.hibernate.testing.logger.LoggerInspectionRule;
+import org.hibernate.testing.orm.junit.BaseUnitTest;
+import org.hibernate.testing.orm.logger.LoggerInspectionExtension;
 import org.hibernate.testing.util.ServiceRegistryUtil;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 /**
  * Tests that {@code hbm.xml} mappings that do not specify dom4j entity-mode information
@@ -22,23 +21,24 @@ import org.junit.Test;
  *
  * @author Steve Ebersole
  */
-public class DeprecationLoggingTest extends BaseUnitTestCase {
-
-	@Rule
-	public LoggerInspectionRule logInspection = new LoggerInspectionRule( DeprecationLogger.DEPRECATION_LOGGER );
+@BaseUnitTest
+public class DeprecationLoggingTest {
+	@RegisterExtension
+	public LoggerInspectionExtension logInspection = LoggerInspectionExtension.builder()
+			.setLogger( DeprecationLogger.DEPRECATION_LOGGER ).build();
 
 	@Test
 	public void basicTest() {
 		logInspection.registerListener( LogListenerImpl.INSTANCE );
 
 		MetadataSources metadataSources = new MetadataSources( ServiceRegistryUtil.serviceRegistry() )
-			.addResource( "org/hibernate/orm/test/entitymode/dom4j/Car.hbm.xml" );
+				.addResource( "org/hibernate/orm/test/entitymode/dom4j/Car.hbm.xml" );
 		try {
 			metadataSources.buildMetadata();
 		}
 		finally {
 			ServiceRegistry metaServiceRegistry = metadataSources.getServiceRegistry();
-			if(metaServiceRegistry instanceof BootstrapServiceRegistry ) {
+			if ( metaServiceRegistry instanceof BootstrapServiceRegistry ) {
 				BootstrapServiceRegistryBuilder.destroy( metaServiceRegistry );
 			}
 		}

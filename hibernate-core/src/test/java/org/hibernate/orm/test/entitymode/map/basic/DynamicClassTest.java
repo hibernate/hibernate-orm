e0c88c372eb57c1973cb5464b9a67a7ad572b9a8
Andrea Boriero <andrea@hibernate.org>
2025-10-21 13:28:31 +0200

    HHH-19846 Drop JUnit 4 usage
    # Conflicts:
    #	hibernate-core/src/test/java/org/hibernate/orm/test/entitymode/map/basic/DynamicClassTest.java

--- hibernate-core/src/test/java/org/hibernate/orm/test/entitymode/map/basic/DynamicClassTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/entitymode/map/basic/DynamicClassTest.java
@@ -12,71 +12,76 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 
 /**
  * @author Gavin King
  */
-@SessionFactory
 @DomainModel(
 		xmlMappings = "org/hibernate/orm/test/entitymode/map/basic/ProductLine.hbm.xml"
 )
-class DynamicClassTest {
+@SessionFactory
+public class DynamicClassTest {
 
 	@Test
-	void testLazyDynamicClass(SessionFactoryScope scope) {
-		scope.inTransaction( s -> {
-			Map<String, Object> cars = new HashMap<>();
-			cars.put( "description", "Cars" );
-			Map<String, Object> monaro = new HashMap<>();
-			monaro.put( "productLine", cars );
-			monaro.put( "name", "monaro" );
-			monaro.put( "description", "Holden Monaro" );
-			Map<String, Object> hsv = new HashMap<>();
-			hsv.put( "productLine", cars );
-			hsv.put( "name", "hsv" );
-			hsv.put( "description", "Holden Commodore HSV" );
-			List<Map<String, Object>> models = new ArrayList<>();
-			cars.put( "models", models );
-			models.add( hsv );
-			models.add( monaro );
-			s.persist( "ProductLine", cars );
-		} );
+	public void testLazyDynamicClass(SessionFactoryScope scope) {
 
-		scope.inTransaction( s -> {
-			Map<String, Object> cars = (Map<String, Object>) s.createQuery(
-					"from ProductLine pl order by pl.description" ).uniqueResult();
-			List<Map<String, Object>> models = (List<Map<String, Object>>) cars.get( "models" );
-			assertFalse( Hibernate.isInitialized( models ) );
-			assertEquals( 2, models.size() );
-			assertTrue( Hibernate.isInitialized( models ) );
+		scope.inTransaction(
+				session -> {
+					Map<String, Object> cars = new HashMap<>();
+					cars.put( "description", "Cars" );
 
-			s.clear();
+					Map<String, Object> monaro = new HashMap<>();
+					monaro.put( "productLine", cars );
+					monaro.put( "name", "monaro" );
+					monaro.put( "description", "Holden Monaro" );
 
-			List<?> list = s.createQuery( "from Model m" ).list();
-			for ( Iterator<?> i = list.iterator(); i.hasNext(); ) {
-				assertFalse( Hibernate.isInitialized( ((Map<String, Object>) i.next()).get( "productLine" ) ) );
-			}
-			Map<String, Object> model = (Map<String, Object>) list.get( 0 );
-			assertTrue( ((List<Map<String, Object>>) ((Map<String, Object>) model.get( "productLine" )).get(
-					"models" )).contains( model ) );
-			s.clear();
+					Map<String, Object> hsv = new HashMap<>();
+					hsv.put( "productLine", cars );
+					hsv.put( "name", "hsv" );
+					hsv.put( "description", "Holden Commodore HSV" );
 
-		} );
+					List<Map<String, Object>> models = new ArrayList<>();
+					cars.put( "models", models );
+					models.add( hsv );
+					models.add( monaro );
 
-		scope.inTransaction( s -> {
-			Map<String, Object> cars = (Map<String, Object>) s.createQuery(
-					"from ProductLine pl order by pl.description" ).uniqueResult();
-			s.remove( cars );
-		} );
+					session.persist( "ProductLine", cars );
+				}
+		);
+
+		scope.inTransaction(
+				session -> {
+					Map<String, Object> cars = (Map<String, Object>) session
+							.createQuery( "from ProductLine pl order by pl.description" )
+							.uniqueResult();
+					List<Map<String, Object>> models = (List<Map<String, Object>>) cars.get( "models" );
+					assertThat( Hibernate.isInitialized( models ) ).isFalse();
+					assertThat( models ).hasSize( 2 );
+					assertThat( Hibernate.isInitialized( models ) ).isTrue();
+
+					session.clear();
+
+					List<Map<String, Map<String, Object>>> list = session.createQuery( "from Model m" ).list();
+					for ( Map<String, Map<String, Object>> stringObjectMap : list ) {
+						assertThat( Hibernate.isInitialized( stringObjectMap.get( "productLine" ) ) ).isFalse();
+					}
+					Map<String, Map<String, Object>> model = list.get( 0 );
+					assertThat( ((((List<Map<String, Object>>) (model.get( "productLine" )).get( "models" )) ).contains( model );
+					session.clear();
+				}
+		);
+
+
+		scope.inTransaction(
+				session -> {
+					session.remove(
+							session.createQuery( "from ProductLine pl order by pl.description" ).uniqueResult() );
+				}
+		);
 	}
 
 	@Test

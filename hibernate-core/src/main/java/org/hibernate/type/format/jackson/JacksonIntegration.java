18e79fcacaddb4abd1eec8075fec65e2d3065efd
Christian Beikov <christian.beikov@gmail.com>
2025-10-22 12:38:03 +0200

    HHH-19887 Construct FormatMapper with context for AggregatedClassLoader access

--- hibernate-core/src/main/java/org/hibernate/type/format/jackson/JacksonIntegration.java
+++ hibernate-core/src/main/java/org/hibernate/type/format/jackson/JacksonIntegration.java
@@ -4,7 +4,9 @@
  */
 package org.hibernate.type.format.jackson;
 
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.hibernate.type.format.FormatMapper;
+import org.hibernate.type.format.FormatMapperCreationContext;
 
 public final class JacksonIntegration {
 
@@ -13,10 +15,6 @@ public final class JacksonIntegration {
 	private static final boolean JACKSON_XML_AVAILABLE = ableToLoadJacksonXMLMapper();
 	private static final boolean JACKSON_JSON_AVAILABLE = ableToLoadJacksonJSONMapper();
 	private static final boolean JACKSON_OSON_AVAILABLE = ableToLoadJacksonOSONFactory();
-	private static final FormatMapper XML_FORMAT_MAPPER = JACKSON_XML_AVAILABLE ? new JacksonXmlFormatMapper() : null;
-	private static final FormatMapper XML_FORMAT_MAPPER_PORTABLE = JACKSON_XML_AVAILABLE ? new JacksonXmlFormatMapper( false ) : null;
-	private static final FormatMapper JSON_FORMAT_MAPPER = JACKSON_JSON_AVAILABLE ? new JacksonJsonFormatMapper() : null;
-	private static final FormatMapper OSON_FORMAT_MAPPER = JACKSON_OSON_AVAILABLE ? new JacksonOsonFormatMapper() : null;
 
 
 	private JacksonIntegration() {
@@ -41,15 +39,67 @@ public final class JacksonIntegration {
 				canLoad( "oracle.jdbc.provider.oson.OsonFactory" );
 	}
 
-	public static FormatMapper getXMLJacksonFormatMapperOrNull(boolean legacyFormat) {
-		return legacyFormat ? XML_FORMAT_MAPPER : XML_FORMAT_MAPPER_PORTABLE;
+	public static @Nullable FormatMapper getXMLJacksonFormatMapperOrNull(FormatMapperCreationContext creationContext) {
+		return JACKSON_XML_AVAILABLE
+				? createFormatMapper( "org.hibernate.type.format.jackson.JacksonXmlFormatMapper", creationContext )
+				: null;
 	}
 
-	public static FormatMapper getJsonJacksonFormatMapperOrNull() {
-		return JSON_FORMAT_MAPPER;
+	public static @Nullable FormatMapper getJsonJacksonFormatMapperOrNull(FormatMapperCreationContext creationContext) {
+		return JACKSON_JSON_AVAILABLE
+				? createFormatMapper( "org.hibernate.type.format.jackson.JacksonJsonFormatMapper", creationContext )
+				: null;
 	}
-	public static FormatMapper getOsonJacksonFormatMapperOrNull() {
-		return OSON_FORMAT_MAPPER;
+	public static @Nullable FormatMapper getOsonJacksonFormatMapperOrNull(FormatMapperCreationContext creationContext) {
+		return JACKSON_OSON_AVAILABLE
+				? createFormatMapper( "org.hibernate.type.format.jackson.JacksonOsonFormatMapper", creationContext )
+				: null;
+	}
+
+	public static @Nullable FormatMapper getXMLJacksonFormatMapperOrNull(boolean legacyFormat) {
+		if ( JACKSON_XML_AVAILABLE ) {
+			try {
+				final Class<?> formatMapperClass = JacksonIntegration.class.getClassLoader()
+						.loadClass( "org.hibernate.type.format.jackson.JacksonXmlFormatMapper" );
+				return (FormatMapper) formatMapperClass.getDeclaredConstructor( boolean.class )
+						.newInstance( legacyFormat );
+			}
+			catch (Exception e) {
+				throw new RuntimeException( "Couldn't instantiate Jackson XML FormatMapper", e );
+			}
+		}
+		return null;
+	}
+
+	public static @Nullable FormatMapper getJsonJacksonFormatMapperOrNull() {
+		return JACKSON_JSON_AVAILABLE
+				? createFormatMapper( "org.hibernate.type.format.jackson.JacksonJsonFormatMapper", null )
+				: null;
+	}
+	public static @Nullable FormatMapper getOsonJacksonFormatMapperOrNull() {
+		return JACKSON_OSON_AVAILABLE
+				? createFormatMapper( "org.hibernate.type.format.jackson.JacksonOsonFormatMapper", null )
+				: null;
+	}
+
+	private static FormatMapper createFormatMapper(String className, @Nullable FormatMapperCreationContext creationContext) {
+		try {
+			if ( creationContext == null ) {
+				final Class<?> formatMapperClass = JacksonIntegration.class.getClassLoader()
+						.loadClass( className );
+				return (FormatMapper) formatMapperClass.getDeclaredConstructor().newInstance();
+			}
+			else {
+				return (FormatMapper) creationContext.getBootstrapContext()
+						.getClassLoaderAccess()
+						.classForName( className )
+						.getDeclaredConstructor( FormatMapperCreationContext.class )
+						.newInstance( creationContext );
+			}
+		}
+		catch (Exception e) {
+			throw new RuntimeException( "Couldn't instantiate Jackson FormatMapper", e );
+		}
 	}
 
 	/**

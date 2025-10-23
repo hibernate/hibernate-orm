7ea6fbc5698992f393eaff68885717e030fb24dd
Andrea Boriero <andrea@hibernate.org>
2025-10-23 19:26:55 +0200

    HHH-19846 Drop JUnit 4 usage

--- hibernate-core/src/test/java/org/hibernate/orm/test/hbm/query/QueryReturnTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/hbm/query/QueryReturnTest.java
@@ -4,49 +4,50 @@
  */
 package org.hibernate.orm.test.hbm.query;
 
-import java.io.StringReader;
-
 import org.hibernate.cfg.Configuration;
 import org.hibernate.dialect.H2Dialect;
-import org.hibernate.internal.util.ReaderInputStream;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.internal.util.ReaderInputStream;
 import org.hibernate.query.internal.ResultSetMappingResolutionContext;
 import org.hibernate.query.named.NamedResultSetMappingMemento;
 import org.hibernate.query.results.ResultSetMapping;
 import org.hibernate.query.results.internal.ResultSetMappingImpl;
 import org.hibernate.query.results.internal.complete.CompleteResultBuilderEntityValued;
-
-import org.hibernate.testing.RequiresDialect;
+import org.hibernate.testing.orm.junit.BaseUnitTest;
 import org.hibernate.testing.orm.junit.JiraKey;
-import org.hibernate.testing.junit4.BaseUnitTestCase;
+import org.hibernate.testing.orm.junit.RequiresDialect;
 import org.hibernate.testing.util.ServiceRegistryUtil;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import java.io.StringReader;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 /**
  * @author Koen Aers
  */
-@JiraKey( value = "HHH-10405" )
-@RequiresDialect( H2Dialect.class )
-public class QueryReturnTest extends BaseUnitTestCase {
+@JiraKey(value = "HHH-10405")
+@RequiresDialect(H2Dialect.class)
+@BaseUnitTest
+public class QueryReturnTest {
 
-	private static String QUERY_RETURN_HBM_XML =
-			"<hibernate-mapping package='org.hibernate.orm.test.hbm.query'>          "+
-			"    <class name='QueryReturnTest$Bar'>                                  "+
-			"        <id name='id'>                                                  "+
-			"            <generator class='sequence'/>                               "+
-			"        </id>                                                           "+
-			"        <property name='foo' type='string'/>                            "+
-			"    </class>                                                            "+
-			"    <sql-query name='myQuery'>                                          "+
-			"        <synchronize table='myTable'/>                                  "+
-			"        <return                                                         "+
-			"                alias='e'                                               "+
-			"                class='org.hibernate.orm.test.hbm.query.QueryReturnTest$Bar'"+
-			"        />                                                              "+
-			"        <![CDATA[from elephant as {e} where {e.age} > 50]]>             "+
-			"    </sql-query>                                                        "+
+	private static final String QUERY_RETURN_HBM_XML =
+			"<hibernate-mapping package='org.hibernate.orm.test.hbm.query'>          " +
+			"    <class name='QueryReturnTest$Bar'>                                  " +
+			"        <id name='id'>                                                  " +
+			"            <generator class='sequence'/>                               " +
+			"        </id>                                                           " +
+			"        <property name='foo' type='string'/>                            " +
+			"    </class>                                                            " +
+			"    <sql-query name='myQuery'>                                          " +
+			"        <synchronize table='myTable'/>                                  " +
+			"        <return                                                         " +
+			"                alias='e'                                               " +
+			"                class='org.hibernate.orm.test.hbm.query.QueryReturnTest$Bar'" +
+			"        />                                                              " +
+			"        <![CDATA[from elephant as {e} where {e.age} > 50]]>             " +
+			"    </sql-query>                                                        " +
 			"</hibernate-mapping>                                                    ";
 
 	@Test
@@ -60,7 +61,7 @@ public class QueryReturnTest extends BaseUnitTestCase {
 			NamedResultSetMappingMemento mappingMemento = sessionFactory.getQueryEngine()
 					.getNamedObjectRepository()
 					.getResultSetMappingMemento( "myQuery" );
-			Assert.assertNotNull( mappingMemento );
+			assertThat( mappingMemento ).isNotNull();
 
 			// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 			// NYI
@@ -73,17 +74,16 @@ public class QueryReturnTest extends BaseUnitTestCase {
 				}
 			};
 
-			mappingMemento.resolve( mapping, querySpace -> {}, resolutionContext );
-			Assert.assertEquals( 1, mapping.getNumberOfResultBuilders() );
+			mappingMemento.resolve( mapping, querySpace -> {
+			}, resolutionContext );
+			assertThat( mapping.getNumberOfResultBuilders() ).isEqualTo( 1 );
 			mapping.visitResultBuilders(
 					(i, resultBuilder) -> {
-						Assert.assertTrue( resultBuilder instanceof CompleteResultBuilderEntityValued );
+						assertThat( resultBuilder ).isInstanceOf( CompleteResultBuilderEntityValued.class );
 						CompleteResultBuilderEntityValued myQueryRootReturn = (CompleteResultBuilderEntityValued) resultBuilder;
 //						Assert.assertEquals( "e", myQueryRootReturn.getTableAlias() );
-						Assert.assertEquals(
-								"org.hibernate.orm.test.hbm.query.QueryReturnTest$Bar",
-								myQueryRootReturn.getReferencedPart().getEntityName()
-						);
+						assertThat( myQueryRootReturn.getReferencedPart().getEntityName() )
+								.isEqualTo( "org.hibernate.orm.test.hbm.query.QueryReturnTest$Bar" );
 					}
 			);
 		}
@@ -95,10 +95,22 @@ public class QueryReturnTest extends BaseUnitTestCase {
 	public static class Bar {
 		public Integer id;
 		public String foo;
-		public Integer getId() { return id; }
-		public void setId(Integer id) { this.id = id; }
-		public String getFoo() { return foo; }
-		public void setFoo(String foo) { this.foo = foo; }
+
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public String getFoo() {
+			return foo;
+		}
+
+		public void setFoo(String foo) {
+			this.foo = foo;
+		}
 	}
 
 }

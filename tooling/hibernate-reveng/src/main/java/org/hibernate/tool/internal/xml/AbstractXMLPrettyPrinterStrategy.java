b5dbe19f67044d0cd51ee076ed8f30e9b5f338c2
Koen Aers <koen.aers@gmail.com>
2025-10-23 16:06:35 +0200

    HBX-3173: Explicitly use the Xerces DocumentBuilderFactory implementation to avoid possible ParserConfigurationException
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- tooling/hibernate-reveng/src/main/java/org/hibernate/tool/internal/xml/AbstractXMLPrettyPrinterStrategy.java
+++ tooling/hibernate-reveng/src/main/java/org/hibernate/tool/internal/xml/AbstractXMLPrettyPrinterStrategy.java
@@ -32,13 +32,15 @@ import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 public abstract class AbstractXMLPrettyPrinterStrategy implements XMLPrettyPrinterStrategy {
 
-    protected Document newDocument(String xml, String encoding) throws SAXException, IOException, ParserConfigurationException {
-    	final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-    	dbf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
-        final Document document = dbf
+    private static final Logger LOGGER = Logger.getLogger( AbstractXMLPrettyPrinterStrategy.class.getName() );
+    private static final DocumentBuilderFactory DOCUMENT_BUILDER_FACTORY = createDocumentBuilderFactory();
+
+    public Document newDocument(String xml, String encoding) throws SAXException, IOException, ParserConfigurationException {
+        final Document document = DOCUMENT_BUILDER_FACTORY
                 .newDocumentBuilder()
                 .parse(new InputSource(new ByteArrayInputStream(xml.getBytes(encoding))));
         document.normalize();
@@ -56,4 +58,23 @@ public abstract class AbstractXMLPrettyPrinterStrategy implements XMLPrettyPrint
             node.getParentNode().removeChild(node);
         }
     }
+
+    private static DocumentBuilderFactory createDocumentBuilderFactory() {
+        DocumentBuilderFactory result = null;
+        try {
+            result = DocumentBuilderFactory.newInstance(
+                    "com.sun.org.apache.xerces.internal.jaxp.DocumentBuilderFactoryImpl",
+                    null);
+            result.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+        }
+        catch (ParserConfigurationException e) {
+            LOGGER.severe(
+                    "A ParserConfigurationException happened while setting the " +
+                            "'http://apache.org/xml/features/nonvalidating/load-external-dtd' feature" +
+                            "to false." );
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
 }

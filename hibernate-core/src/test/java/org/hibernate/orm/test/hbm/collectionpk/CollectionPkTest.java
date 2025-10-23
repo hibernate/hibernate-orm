f18f6e1554e10b6e958adc93730fa062bb076948
Andrea Boriero <andrea@hibernate.org>
2025-10-23 19:26:55 +0200

    HHH-19846 Drop JUnit 4 usage

--- hibernate-core/src/test/java/org/hibernate/orm/test/hbm/collectionpk/CollectionPkTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/hbm/collectionpk/CollectionPkTest.java
@@ -4,38 +4,38 @@
  */
 package org.hibernate.orm.test.hbm.collectionpk;
 
-import java.util.Arrays;
-
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.tool.schema.internal.SchemaCreatorImpl;
-
-import org.hibernate.testing.orm.junit.JiraKey;
-import org.hibernate.testing.junit4.BaseUnitTestCase;
-import org.hibernate.testing.util.ServiceRegistryUtil;
-
 import org.hibernate.orm.test.hbm.index.JournalingSchemaToolingTarget;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.hibernate.testing.orm.junit.BaseUnitTest;
+import org.hibernate.testing.orm.junit.JiraKey;
+import org.hibernate.testing.util.ServiceRegistryUtil;
+import org.hibernate.tool.schema.internal.SchemaCreatorImpl;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 /**
  * @author Steve Ebersole
  */
-@JiraKey( value = "HHH-10206" )
-public class CollectionPkTest extends BaseUnitTestCase {
+@JiraKey(value = "HHH-10206")
+@BaseUnitTest
+public class CollectionPkTest {
 	private StandardServiceRegistry ssr;
 
-	@Before
+	@BeforeEach
 	public void before() {
 		ssr = ServiceRegistryUtil.serviceRegistry();
 	}
 
-	@After
+	@AfterEach
 	public void after() {
 		if ( ssr != null ) {
 			StandardServiceRegistryBuilder.destroy( ssr );
@@ -59,10 +59,10 @@ public class CollectionPkTest extends BaseUnitTestCase {
 		final JournalingSchemaToolingTarget target = new JournalingSchemaToolingTarget();
 		new SchemaCreatorImpl( ssr ).doCreation( metadata, false, target );
 
-		assertTrue(
-				"Expected foreign-key name [" + Arrays.toString(expectedName) + "] not seen in schema creation output",
-				Arrays.stream( expectedName ).anyMatch( target::containedText )
-		);
+		assertThat( Arrays.stream( expectedName ).anyMatch( target::containedText ) )
+				.describedAs( "Expected foreign-key name [" + Arrays.toString(
+						expectedName ) + "] not seen in schema creation output" )
+				.isTrue();
 	}
 
 	@Test
--- hibernate-core/src/test/java/org/hibernate/orm/test/hbm/version/GeneratedVersionBindingTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/hbm/version/GeneratedVersionBindingTest.java
@@ -8,26 +8,27 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.BootstrapServiceRegistry;
 import org.hibernate.boot.registry.BootstrapServiceRegistryBuilder;
 import org.hibernate.service.ServiceRegistry;
-
-import org.hibernate.testing.junit4.BaseUnitTestCase;
+import org.hibernate.testing.orm.junit.BaseUnitTest;
 import org.hibernate.testing.util.ServiceRegistryUtil;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * @author Steve Ebersole
  */
-public class GeneratedVersionBindingTest extends BaseUnitTestCase {
+@BaseUnitTest
+public class GeneratedVersionBindingTest {
+
 	@Test
 	public void testIt() {
 		MetadataSources metadataSources = new MetadataSources( ServiceRegistryUtil.serviceRegistry() )
-			.addResource( "org/hibernate/orm/test/hbm/version/Mappings.hbm.xml" );
+				.addResource( "org/hibernate/orm/test/hbm/version/Mappings.hbm.xml" );
 
 		try {
 			metadataSources.buildMetadata();
 		}
 		finally {
 			ServiceRegistry metaServiceRegistry = metadataSources.getServiceRegistry();
-			if(metaServiceRegistry instanceof BootstrapServiceRegistry ) {
+			if ( metaServiceRegistry instanceof BootstrapServiceRegistry ) {
 				BootstrapServiceRegistryBuilder.destroy( metaServiceRegistry );
 			}
 		}

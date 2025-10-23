4eac25fee95acadfdf8b2137660f8c9cb865fe5b
Koen Aers <koen.aers@gmail.com>
2025-10-23 19:05:16 +0200

    HBX-3178: Improve the implementation of TableIdentifier
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- tooling/hibernate-reveng/src/main/java/org/hibernate/tool/api/reveng/TableIdentifier.java
+++ tooling/hibernate-reveng/src/main/java/org/hibernate/tool/api/reveng/TableIdentifier.java
@@ -19,31 +19,33 @@ package org.hibernate.tool.api.reveng;
 
 import org.hibernate.mapping.Table;
 
+import java.util.Objects;
+
 /**
  * Identifier used for the full name of tables.
  * @author max
  *
  */
 public class TableIdentifier {
-	
+
 	public static TableIdentifier create(Table table) {
 		return new TableIdentifier(table.getCatalog(), table.getSchema(), table.getName() );
 	}
-	
+
 	public static TableIdentifier create(String catalog, String schema, String name) {
 		return new TableIdentifier(catalog, schema, name);
 	}
-	
+
 	private final String catalog;
 	private final String schema;
 	private final String name;
-	
+
 	private TableIdentifier(String catalog, String schema, String name) {
 		this.catalog = (catalog==null?null:catalog.intern() );
 		this.schema = (schema==null?null:schema.intern() );
-		this.name = (name==null?null:name.intern() );		
+		this.name = (name==null?null:name.intern() );
 	}
-	
+
 	public String getCatalog() {
 		return catalog;
 	}
@@ -53,47 +55,41 @@ public class TableIdentifier {
 	public String getSchema() {
 		return schema;
 	}
-	
+
 	public boolean equals(Object obj) {
-		return obj instanceof TableIdentifier && equals( (TableIdentifier)obj);
+		return obj instanceof TableIdentifier
+				&& isEqualIdentifier( (TableIdentifier)obj);
 	}
-	
-	public boolean equals(TableIdentifier otherIdentifier) {
+
+	private boolean isEqualIdentifier(TableIdentifier otherIdentifier) {
 		if (otherIdentifier==null) return false;
 		if (this==otherIdentifier) return true;
-		
-		if (equals(this.name, otherIdentifier.name) ) {
-			if(equals(this.schema, otherIdentifier.schema) ) {
-				return equals(this.catalog, otherIdentifier.catalog);
+
+		if (Objects.equals(this.name, otherIdentifier.name) ) {
+			if(Objects.equals(this.schema, otherIdentifier.schema) ) {
+				return Objects.equals(this.catalog, otherIdentifier.catalog);
 			}
 		}
-		
 		return false;
 	}
-	
+
 	public int hashCode() {
 		int result = 13;
 		result = 37 * result + ( name==null ? 0 : name.hashCode() );
 		result = 37 * result + ( schema==null ? 0 : schema.hashCode() );
 		result = 37 * result + ( catalog==null ? 0 : catalog.hashCode() );
-		
+
 		return result;
 	}
-	
-	private boolean equals(String left, String right) {
-		if (left==right) return true;
-		if (left==null || right==null) return false;
-		return left.equals(right);
-	}
-	
+
 	public String toString() {
-		StringBuffer buf = new StringBuffer()
-					.append( "TableIdentifier" )
-					.append('(');
-		if ( getCatalog()!=null ) buf.append( getCatalog() + "." );
-		if ( getSchema()!=null ) buf.append( getSchema()+ ".");
+		StringBuilder buf = new StringBuilder()
+				.append( "TableIdentifier" )
+				.append('(');
+		if ( getCatalog()!=null ) buf.append( getCatalog() ).append( "." );
+		if ( getSchema()!=null ) buf.append( getSchema() ).append( "." );
 		buf.append( getName() ).append(')');
 		return buf.toString();
 	}
-	
+
 }

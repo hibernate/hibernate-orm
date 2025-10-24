47a1393614bb8669ba998923271f786d69d62aec
Koen Aers <koen.aers@gmail.com>
2025-10-24 14:39:50 +0200

    HBX-3183: Improve the implementation of AbstractMetaDataDialect
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- tooling/hibernate-reveng/src/main/java/org/hibernate/tool/internal/reveng/dialect/AbstractMetaDataDialect.java
+++ tooling/hibernate-reveng/src/main/java/org/hibernate/tool/internal/reveng/dialect/AbstractMetaDataDialect.java
@@ -42,22 +42,22 @@ import org.jboss.logging.Logger;
 public abstract class AbstractMetaDataDialect implements RevengDialect {
 
 	protected final Logger log = Logger.getLogger(this.getClass());
-	
+
 	private Connection connection;
 	private DatabaseMetaData metaData;
-	
+
 	private ConnectionProvider connectionProvider = null;
 
 	public void configure(
 			ConnectionProvider connectionProvider) {
-		this.connectionProvider = connectionProvider;	
+		this.connectionProvider = connectionProvider;
 	}
-	
+
 	public void close() {
 		metaData = null;
 		if(connection != null) {
 			try {
-				connectionProvider.closeConnection(connection);				
+				connectionProvider.closeConnection(connection);
 			}
 			catch (SQLException e) {
 				throw new RuntimeException("Problem while closing connection", e);
@@ -65,70 +65,74 @@ public abstract class AbstractMetaDataDialect implements RevengDialect {
 				connection = null;
 			}
 		}
-		connectionProvider = null;		
+		connectionProvider = null;
 	}
-	
+
 	protected DatabaseMetaData getMetaData() {
 		if (metaData == null) {
 			try {
-				metaData = getConnection().getMetaData();				
-			} 
+				metaData = getConnection().getMetaData();
+			}
 			catch (SQLException e) {
 				throw new RuntimeException("Getting database metadata", e);
 			}
 		}
 		return metaData;
 	}
-	
+
 	protected String getDatabaseStructure(String catalog, String schema) {
-	      ResultSet schemaRs = null;
-	      ResultSet catalogRs = null;
-	      String nl = System.getProperty("line.separator");
-	      StringBuffer sb = new StringBuffer(nl);
-	      // Let's give the user some feedback. The exception
-	      // is probably related to incorrect schema configuration.
-	      sb.append("Configured schema:").append(schema).append(nl);
-	      sb.append("Configured catalog:").append(catalog ).append(nl);
+		ResultSet schemaRs = null;
+		ResultSet catalogRs = null;
+		String nl = System.lineSeparator();
+		StringBuilder sb = new StringBuilder(nl);
+		// Let's give the user some feedback. The exception
+		// is probably related to incorrect schema configuration.
+		sb.append("Configured schema:").append(schema).append(nl);
+		sb.append("Configured catalog:").append(catalog ).append(nl);
 
-	      try {
-	         schemaRs = getMetaData().getSchemas();
-	         sb.append("Available schemas:").append(nl);
-	         while (schemaRs.next() ) {
-	            sb.append("  ").append(schemaRs.getString("TABLE_SCHEM") ).append(nl);
-	         }
-	      } 
-	      catch (SQLException e2) {
-	         log.warn("Could not get schemas", e2);
-	         sb.append("  <SQLException while getting schemas>").append(nl);
-	      } 
-	      finally {
-	         try {
-	            schemaRs.close();
-	         } 
-	         catch (Exception ignore) {
-	         }
-	      }
+		try {
+			schemaRs = getMetaData().getSchemas();
+			sb.append("Available schemas:").append(nl);
+			while (schemaRs.next() ) {
+				sb.append("  ").append(schemaRs.getString("TABLE_SCHEM") ).append(nl);
+			}
+		}
+		catch (SQLException e2) {
+			log.warn("Could not get schemas", e2);
+			sb.append("  <SQLException while getting schemas>").append(nl);
+		}
+		finally {
+			try {
+				if (schemaRs != null) {
+					schemaRs.close();
+				}
+			}
+			catch (Exception ignore) {
+			}
+		}
 
-	      try {
-	         catalogRs = getMetaData().getCatalogs();
-	         sb.append("Available catalogs:").append(nl);
-	         while (catalogRs.next() ) {
-	            sb.append("  ").append(catalogRs.getString("TABLE_CAT") ).append(nl);
-	         }
-	      } 
-	      catch (SQLException e2) {
-	         log.warn("Could not get catalogs", e2);
-	         sb.append("  <SQLException while getting catalogs>").append(nl);
-	      } 
-	      finally {
-	         try {
-	            catalogRs.close();
-	         } 
-	         catch (Exception ignore) {
-	         }
-	      }
-	      return sb.toString();
-	   }
+		try {
+			catalogRs = getMetaData().getCatalogs();
+			sb.append("Available catalogs:").append(nl);
+			while (catalogRs.next() ) {
+				sb.append("  ").append(catalogRs.getString("TABLE_CAT") ).append(nl);
+			}
+		}
+		catch (SQLException e2) {
+			log.warn("Could not get catalogs", e2);
+			sb.append("  <SQLException while getting catalogs>").append(nl);
+		}
+		finally {
+			try {
+				if (catalogRs != null) {
+					catalogRs.close();
+				}
+			}
+			catch (Exception ignore) {
+			}
+		}
+		return sb.toString();
+	}
 
 	protected Connection getConnection() throws SQLException {
 		if(connection==null) {
@@ -136,31 +140,30 @@ public abstract class AbstractMetaDataDialect implements RevengDialect {
 		}
 		return connection;
 	}
-	
+
 	public void close(Iterator<?> iterator) {
 		if(iterator instanceof ResultSetIterator) {
 			((ResultSetIterator)iterator).close();
 		}
 	}
-	
+
 	public boolean needQuote(String name) {
-		
+
 		if(name==null) return false;
-	
-		// TODO: use jdbc metadata to decide on this. but for now we just handle the most typical cases.		
+
+		// TODO: use jdbc metadata to decide on this. but for now we just handle the most typical cases.
 		if(name.indexOf('-')>0) return true;
 		if(name.indexOf(' ')>0) return true;
-		if(name.indexOf('.')>0) return true;
-		return false;
+		return name.indexOf( '.' ) > 0;
 	}
-	
+
 	protected String caseForSearch(String value) throws SQLException  {
 		// TODO: handle quoted requests (just strip it ?)
 		if(needQuote(value)) {
 			if ( getMetaData().storesMixedCaseQuotedIdentifiers() ) {
 				return value;
-			} else if ( getMetaData().storesUpperCaseQuotedIdentifiers() ) { 
-				return toUpperCase( value ); 
+			} else if ( getMetaData().storesUpperCaseQuotedIdentifiers() ) {
+				return toUpperCase( value );
 			} else if( getMetaData().storesLowerCaseQuotedIdentifiers() ) {
 				return toLowerCase( value );
 			} else {
@@ -168,23 +171,23 @@ public abstract class AbstractMetaDataDialect implements RevengDialect {
 			}
 		} else if ( getMetaData().storesMixedCaseQuotedIdentifiers() ) {
 			return value;
-		} else if ( getMetaData().storesUpperCaseIdentifiers() ) { 
-			return toUpperCase( value ); 
+		} else if ( getMetaData().storesUpperCaseIdentifiers() ) {
+			return toUpperCase( value );
 		} else if( getMetaData().storesLowerCaseIdentifiers() ) {
 			return toLowerCase( value );
 		} else {
 			return value;
-		}		
+		}
 	}
 
 	private String toUpperCase(String str) {
 		return str==null ? null : str.toUpperCase();
 	}
-	
+
 	private String toLowerCase(String str) {
 		return str == null ? null : str.toLowerCase(Locale.ENGLISH);
 	}
-	
+
 	public Iterator<Map<String, Object>> getSuggestedPrimaryKeyStrategyName(String catalog, String schema, String table) {
 		Map<String, Object> m = new HashMap<String, Object>();
 		m.put( "TABLE_CAT", catalog );

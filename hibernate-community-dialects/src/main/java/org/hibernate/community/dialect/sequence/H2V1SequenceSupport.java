2f2c2230229f0043e1f6a14d5429598480af8cfb
Moad Elfatihi <elfatihi.moad@gmail.com>
2025-11-17 02:47:13 +0100

    HHH-19934 simplify H2Dialect and move H2V1SequenceSupport to community dialect

--- hibernate-community-dialects/src/main/java/org/hibernate/community/dialect/sequence/H2V1SequenceSupport.java
+++ hibernate-community-dialects/src/main/java/org/hibernate/community/dialect/sequence/H2V1SequenceSupport.java
@@ -2,9 +2,10 @@
  * SPDX-License-Identifier: Apache-2.0
  * Copyright Red Hat Inc. and Hibernate Authors
  */
-package org.hibernate.dialect.sequence;
+package org.hibernate.community.dialect.sequence;
 
 import org.hibernate.MappingException;
+import org.hibernate.dialect.sequence.SequenceSupport;
 
 /**
  * Sequence support for {@link org.hibernate.dialect.H2Dialect}.

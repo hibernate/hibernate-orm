d3d143e077dd4108b21b2482dbf7554e6d25daf8
Čedomir Igaly <cedomir.igaly@zg.t-com.hr>
2025-11-17 15:01:04 +0100

    HHH-19935 Make sure that increment to current sequence can not be zero

--- hibernate-core/src/main/java/org/hibernate/id/uuid/UuidVersion7Strategy.java
+++ hibernate-core/src/main/java/org/hibernate/id/uuid/UuidVersion7Strategy.java
@@ -54,6 +54,13 @@ public class UuidVersion7Strategy implements UUIDGenerationStrategy, UuidValueGe
 			return lastTimestamp.toEpochMilli();
 		}
 
+		/**
+		 * Sub-miliseconds part of timestamp (micro- and nanoseconds) mapped to 12 bit integral value.
+		 * Calculated as nanos / 1000000 * 4096
+		 *
+		 * @param timestamp
+		 * @return
+		 */
 		private static long nanos(Instant timestamp) {
 			return (long) ((timestamp.getNano() % 1_000_000L) * 0.004096);
 		}
@@ -64,9 +71,17 @@ public class UuidVersion7Strategy implements UUIDGenerationStrategy, UuidValueGe
 				return new State( now, randomSequence() );
 			}
 			else {
-				final long nextSequence = lastSequence + Holder.numberGenerator.nextLong( 0xFFFF_FFFFL );
-				return nextSequence > MAX_RANDOM_SEQUENCE
-						? new State( lastTimestamp.plusNanos( 250 ), randomSequence() )
+				final long nextSequence = randomSequence();
+				/*
+				 *	If next random sequence is less or equal to last one sub-millisecond part
+				 * 	should be incremented to preserve monotonicity of generated UUIDs.
+				 * 	To do this smallest number of nanoseconds that will always increase
+				 * 	sub-millisecond part mapped to 12 bits is
+				 * 		1_000_000 (nanons per milli) / 4096 (12 bits) = 244.14...
+				 * 	So 245 is used as smallest integer larger than this value.
+				 */
+				return lastSequence >= nextSequence
+						? new State( lastTimestamp.plusNanos( 245 ), nextSequence )
 						: new State( lastTimestamp, nextSequence );
 			}
 		}
@@ -77,7 +92,7 @@ public class UuidVersion7Strategy implements UUIDGenerationStrategy, UuidValueGe
 		}
 
 		private static long randomSequence() {
-			return Holder.numberGenerator.nextLong( MAX_RANDOM_SEQUENCE );
+			return Holder.numberGenerator.nextLong( MAX_RANDOM_SEQUENCE + 1 );
 		}
 	}
 
@@ -118,7 +133,7 @@ public class UuidVersion7Strategy implements UUIDGenerationStrategy, UuidValueGe
 				| state.nanos() & 0xFFFL,
 				// LSB bits 0-1 - variant = 4
 				0x8000_0000_0000_0000L
-				// LSB bits 2-15 - pseudorandom counter
+				// LSB bits 2-63 - pseudorandom counter
 				| state.lastSequence
 		);
 	}

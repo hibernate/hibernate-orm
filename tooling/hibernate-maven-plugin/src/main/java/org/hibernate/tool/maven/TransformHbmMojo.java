312c9dc061a64b25e01f8a9df9c37c8671a88ecc
Koen Aers <koen.aers@gmail.com>
2025-12-08 15:00:10 +0100

    HHH-19879: Move Hibernate Tools' reveng module to Hibernate ORM and merge the relevant ant/gradle/maven plugins   - Add exporter to generate mapping.xml files   - Make hbm2orm mojo use the above exporter   - Add example/integration test for the hbm2orm mojo   - Some improvements to the Maven (integration) tests
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- tooling/hibernate-maven-plugin/src/main/java/org/hibernate/tool/maven/TransformHbmMojo.java
+++ tooling/hibernate-maven-plugin/src/main/java/org/hibernate/tool/maven/TransformHbmMojo.java
@@ -4,133 +4,54 @@
  */
 package org.hibernate.tool.maven;
 
-import jakarta.xml.bind.JAXBException;
-import jakarta.xml.bind.Marshaller;
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.plugins.annotations.ResolutionScope;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.jaxb.Origin;
-import org.hibernate.boot.jaxb.SourceType;
-import org.hibernate.boot.jaxb.hbm.spi.JaxbHbmHibernateMapping;
-import org.hibernate.boot.jaxb.hbm.transform.HbmXmlTransformer;
-import org.hibernate.boot.jaxb.hbm.transform.UnsupportedFeatureHandling;
-import org.hibernate.boot.jaxb.internal.MappingBinder;
-import org.hibernate.boot.jaxb.mapping.spi.JaxbEntityMappingsImpl;
-import org.hibernate.boot.jaxb.spi.Binding;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.boot.spi.MetadataImplementor;
-import org.hibernate.cfg.JdbcSettings;
-import org.hibernate.dialect.H2Dialect;
-import org.hibernate.service.ServiceRegistry;
+import static org.apache.maven.plugins.annotations.LifecyclePhase.GENERATE_RESOURCES;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.Serial;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-import static org.apache.maven.plugins.annotations.LifecyclePhase.GENERATE_RESOURCES;
+import org.apache.maven.artifact.DependencyResolutionRequiredException;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+
+import org.hibernate.tool.reveng.internal.export.mapping.MappingExporter;
 
 @Mojo(
-	name = "hbm2orm",
-	defaultPhase = GENERATE_RESOURCES,
-	requiresDependencyResolution = ResolutionScope.RUNTIME)
+		name = "hbm2orm",
+		defaultPhase = GENERATE_RESOURCES,
+		requiresDependencyResolution = ResolutionScope.RUNTIME)
 public class TransformHbmMojo extends AbstractMojo {
 
 	@Parameter(defaultValue = "${project.basedir}/src/main/resources")
 	private File inputFolder;
 
+	@Parameter(defaultValue = "true")
+	private boolean format;
+
+	@Parameter(defaultValue = "${project}", readonly = true, required = true)
+	private MavenProject project;
+
 	@Override
 	public void execute() {
-		MappingBinder mappingBinder = new MappingBinder(
-				MappingBinder.class.getClassLoader()::getResourceAsStream,
-				UnsupportedFeatureHandling.ERROR);
-		List<File> hbmFiles = getHbmFiles(inputFolder);
-		List<Binding<JaxbHbmHibernateMapping>> hbmMappings = getHbmMappings(hbmFiles, mappingBinder);
-		performTransformation(hbmMappings, mappingBinder, createServiceRegistry());
-	}
-
-	private ServiceRegistry createServiceRegistry() {
-		StandardServiceRegistryBuilder ssrb = new StandardServiceRegistryBuilder();
-		ssrb.clearSettings();
-		ssrb.applySetting(JdbcSettings.ALLOW_METADATA_ON_BOOT, false);
-		// Choose the H2 dialect by default, make this configurable
-		ssrb.applySetting(JdbcSettings.DIALECT, H2Dialect.class.getName());
-		return ssrb.build();
-	}
-
-	private void performTransformation(
-			List<Binding<JaxbHbmHibernateMapping>> hbmBindings,
-			MappingBinder mappingBinder,
-			ServiceRegistry serviceRegistry) {
-		Marshaller marshaller = createMarshaller(mappingBinder);
-		MetadataSources metadataSources = new MetadataSources( serviceRegistry );
-		hbmBindings.forEach( metadataSources::addHbmXmlBinding );
-		List<Binding<JaxbEntityMappingsImpl>> transformedBindings = HbmXmlTransformer.transform(
-				hbmBindings,
-				(MetadataImplementor) metadataSources.buildMetadata(),
-				UnsupportedFeatureHandling.ERROR
-		);
-		for (int i = 0; i < hbmBindings.size(); i++) {
-			Binding<JaxbHbmHibernateMapping> hbmBinding = hbmBindings.get( i );
-			Binding<JaxbEntityMappingsImpl> transformedBinding = transformedBindings.get( i );
-
-			HbmXmlOrigin origin = (HbmXmlOrigin)hbmBinding.getOrigin();
-			File hbmXmlFile = origin.getHbmXmlFile();
-
-			marshall(marshaller, transformedBinding.getRoot(), hbmXmlFile);
-		}
-	}
-
-	private List<Binding<JaxbHbmHibernateMapping>> getHbmMappings(List<File> hbmXmlFiles, MappingBinder mappingBinder) {
-		List<Binding<JaxbHbmHibernateMapping>> result = new ArrayList<>();
-		hbmXmlFiles.forEach((hbmXmlFile) -> {
-			final String fullPath = hbmXmlFile.getAbsolutePath();
-			getLog().info("Adding file: '" + fullPath + "' to the list to be transformed.");
-			Origin origin = new HbmXmlOrigin( hbmXmlFile );
-			Binding<JaxbHbmHibernateMapping> binding = bindMapping( mappingBinder, hbmXmlFile, origin );
-			result.add(binding);
-		});
-		return result;
-	}
-
-	private void marshall(Marshaller marshaller, JaxbEntityMappingsImpl mappings, File hbmXmlFile) {
-		File mappingXmlFile =  new File(
-				hbmXmlFile.getParentFile(),
-				hbmXmlFile.getName().replace(".hbm.xml", ".mapping.xml"));
-		getLog().info("Marshalling file: " + hbmXmlFile.getAbsolutePath() + " into " + mappingXmlFile.getAbsolutePath());
+		ClassLoader original = Thread.currentThread().getContextClassLoader();
 		try {
-			marshaller.marshal( mappings, mappingXmlFile );
+			Thread.currentThread().setContextClassLoader(createClassLoader(original));
+			getLog().info("Starting " + this.getClass().getSimpleName() + "...");
+			MappingExporter mappingExporter = new MappingExporter();
+			mappingExporter.setHbmFiles(getHbmFiles(inputFolder));
+			mappingExporter.setFormatResult(format);
+			mappingExporter.start();
+			getLog().info("Finished " + this.getClass().getSimpleName() + "!");
 		}
-		catch (JAXBException e) {
-			throw new RuntimeException(
-					"Unable to marshall mapping JAXB representation to file `" + mappingXmlFile.getAbsolutePath() + "`",
-					e
-			);
-		}
-	}
-
-	private Binding<JaxbHbmHibernateMapping> bindMapping(
-			MappingBinder mappingBinder, File hbmXmlFile, Origin origin) {
-		try ( final FileInputStream fileStream = new FileInputStream(hbmXmlFile) ) {
-			return mappingBinder.bind( fileStream, origin );
-		}
-		catch (IOException e) {
-			getLog().warn( "Unable to open hbm.xml file `" + hbmXmlFile.getAbsolutePath() + "` for transformation", e );
-			return null;
-		}
-	}
-
-	private Marshaller createMarshaller(MappingBinder mappingBinder) {
-		try {
-			return mappingBinder.mappingJaxbContext().createMarshaller();
-		}
-		catch (JAXBException e) {
-			throw new RuntimeException("Unable to create JAXB Marshaller", e);
+		finally {
+			Thread.currentThread().setContextClassLoader(original);
 		}
 	}
 
@@ -149,22 +70,17 @@ public class TransformHbmMojo extends AbstractMojo {
 		return result;
 	}
 
-	private static class HbmXmlOrigin extends Origin {
-
-		@Serial
-		private static final long serialVersionUID = 1L;
-
-		private final File hbmXmlFile;
-
-		public HbmXmlOrigin(File hbmXmlFile) {
-			super( SourceType.FILE, hbmXmlFile.getAbsolutePath() );
-			this.hbmXmlFile = hbmXmlFile;
+	private ClassLoader createClassLoader(ClassLoader parent) {
+		ArrayList<URL> urls = new ArrayList<>();
+		try {
+			for (String cpe : project.getRuntimeClasspathElements()) {
+				urls.add(new File(cpe).toURI().toURL());
+			}
 		}
-
-		public File getHbmXmlFile() {
-			return hbmXmlFile;
+		catch (DependencyResolutionRequiredException | MalformedURLException e) {
+			throw new RuntimeException("Problem while constructing project classloader", e);
 		}
-
+		return new URLClassLoader(urls.toArray(new URL[0]), parent);
 	}
 
 }

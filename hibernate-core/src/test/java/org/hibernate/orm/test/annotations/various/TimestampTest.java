734d974d3f032ac5f73dcf329c4ab8c929b2c98e
Gavin King <gavin@hibernate.org>
2025-12-12 22:31:28 +0100

    overwrite the previous useless test for @Source
    with one that actually tests something meaningful

--- hibernate-core/src/test/java/org/hibernate/orm/test/annotations/various/TimestampTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/annotations/various/TimestampTest.java
@@ -4,70 +4,45 @@
  */
 package org.hibernate.orm.test.annotations.various;
 
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.boot.spi.MetadataImplementor;
-import org.hibernate.testing.orm.junit.BaseUnitTest;
-import org.hibernate.testing.util.ServiceRegistryUtil;
-import org.hibernate.type.BasicType;
-import org.hibernate.type.StandardBasicTypes;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.hibernate.testing.orm.junit.DomainModel;
+import org.hibernate.testing.orm.junit.SessionFactory;
+import org.hibernate.testing.orm.junit.SessionFactoryScope;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.sql.Timestamp;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * Test for the @Timestamp annotation.
  *
  * @author Hardy Ferentschik
  */
-@BaseUnitTest
+@SessionFactory
+@DomainModel(annotatedClasses = {VMTimestamped.class, DBTimestamped.class})
 public class TimestampTest {
-	private StandardServiceRegistry ssr;
-	private MetadataImplementor metadata;
+	@Test void test(SessionFactoryScope scope) {
+		scope.inTransaction( session -> {
+			var vmTimestamped = new VMTimestamped();
+			session.persist( vmTimestamped );
+			var dbTimestamped = new DBTimestamped();
+			session.persist( dbTimestamped );
+			session.flush();
+			assertNotNull( vmTimestamped.getLastUpdate() );
+			assertNotNull( dbTimestamped.getLastUpdate() );
+			assertInstanceOf( Date.class, vmTimestamped.getLastUpdate() );
+			assertInstanceOf( Timestamp.class, dbTimestamped.getLastUpdate() );
+		} );
+		scope.inTransaction( session -> {
+			var vmTimestamped = session.find(VMTimestamped.class, 1);
+			var dbTimestamped = session.find(DBTimestamped.class, 1);
+			assertNotNull( vmTimestamped.getLastUpdate() );
+			assertNotNull( dbTimestamped.getLastUpdate() );
+			assertInstanceOf( Timestamp.class, vmTimestamped.getLastUpdate() );
+			assertInstanceOf( Timestamp.class, dbTimestamped.getLastUpdate() );
+		} );
 
-	@BeforeAll
-	public void setUp() {
-		ssr = ServiceRegistryUtil.serviceRegistry();
-		metadata = (MetadataImplementor) new MetadataSources( ssr )
-				.addAnnotatedClass( VMTimestamped.class )
-				.addAnnotatedClass( DBTimestamped.class )
-				.getMetadataBuilder()
-				.build();
-	}
-
-	@AfterAll
-	public void tearDown() {
-		if ( ssr != null ) {
-			StandardServiceRegistryBuilder.destroy( ssr );
-		}
-	}
-
-	@Test
-	public void testTimestampSourceIsVM() {
-		assertTimestampSource( VMTimestamped.class );
-	}
-
-	@Test
-	public void testTimestampSourceIsDB() {
-		assertTimestampSource( DBTimestamped.class );
-	}
-
-	private void assertTimestampSource(Class<?> clazz ) {
-		assertTimestampSource( clazz,
-				metadata.getTypeConfiguration().getBasicTypeRegistry()
-						.resolve( StandardBasicTypes.TIMESTAMP ) );
-	}
-
-	private void assertTimestampSource(Class<?> clazz, BasicType<?> basicType) {
-		var persistentClass = metadata.getEntityBinding( clazz.getName() );
-		assertThat( persistentClass ).isNotNull();
-		var versionProperty = persistentClass.getVersion();
-		assertThat( versionProperty ).isNotNull();
-		assertThat( versionProperty.getType().getName() )
-				.describedAs( "Wrong timestamp type" )
-				.isEqualTo( basicType.getName() );
 	}
 }

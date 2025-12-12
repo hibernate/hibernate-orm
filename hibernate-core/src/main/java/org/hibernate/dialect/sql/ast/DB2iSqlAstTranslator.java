ebdb0950773f3c785f8d468740299563014c1caa
Christian Beikov <christian.beikov@gmail.com>
2025-12-12 16:15:36 +0100

    HHH-19929 Render special DB2 for i VALUES expression to avoid parameter type inference issues

--- hibernate-core/src/main/java/org/hibernate/dialect/sql/ast/DB2iSqlAstTranslator.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/sql/ast/DB2iSqlAstTranslator.java
@@ -9,7 +9,10 @@ import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.query.sqm.ComparisonOperator;
 import org.hibernate.sql.ast.tree.Statement;
 import org.hibernate.sql.ast.tree.expression.Expression;
+import org.hibernate.sql.ast.tree.expression.SqlTupleContainer;
 import org.hibernate.sql.exec.spi.JdbcOperation;
+
+import java.util.List;
 
 import static org.hibernate.dialect.DB2iDialect.DB2_LUW_VERSION;
 
@@ -30,6 +33,20 @@ public class DB2iSqlAstTranslator<T extends JdbcOperation> extends DB2SqlAstTran
 	@Override
 	protected void renderComparison(Expression lhs, ComparisonOperator operator, Expression rhs) {
 		renderComparisonStandard( lhs, operator, rhs );
+	}
+
+	@Override
+	protected void renderExpressionsAsValuesSubquery(int tupleSize, List<Expression> listExpressions) {
+		// DB2 for i supports type-inference in this special VALUES expression, but not if it's wrapped as SELECT
+		appendSql( "values" );
+		char separator = ' ';
+		for ( Expression expression : listExpressions ) {
+			appendSql( separator );
+			appendSql( OPEN_PARENTHESIS );
+			renderCommaSeparated( SqlTupleContainer.getSqlTuple( expression ).getExpressions() );
+			appendSql( CLOSE_PARENTHESIS );
+			separator = ',';
+		}
 	}
 
 	@Override

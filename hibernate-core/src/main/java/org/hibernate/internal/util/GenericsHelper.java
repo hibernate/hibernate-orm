6b9ceae63ae27879b53ad0c00fe112a3c8e72136
Gavin King <gavin@hibernate.org>
2025-12-23 22:24:37 +0100

    use more efficient (if correct) implementation of type arg inference
    rename methods for clarity

--- hibernate-core/src/main/java/org/hibernate/internal/util/GenericsHelper.java
+++ hibernate-core/src/main/java/org/hibernate/internal/util/GenericsHelper.java
@@ -4,11 +4,18 @@
  */
 package org.hibernate.internal.util;
 
-import org.checkerframework.checker.nullness.qual.NonNull;
 import org.hibernate.models.spi.MemberDetails;
 
-import java.lang.reflect.*;
-import java.util.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.GenericArrayType;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
 
 /**
  * @author Gavin King
@@ -37,10 +44,23 @@ public final class GenericsHelper {
 	 */
 	public static Type actualInheritedMemberType(Class<?> subclass, Member superMember) {
 		return substituteTypeVariables( getMemberType( superMember ),
-				extractTypeArguments( subclass, superMember.getDeclaringClass() ) );
+				collectTypeArguments( subclass, superMember ) );
 	}
 
-	private static @NonNull Type getMemberType(Member superMember) {
+	private static Map<TypeVariable<?>, Type> collectTypeArguments(
+			Class<?> subclass, Member superMember) {
+		final var superclass = superMember.getDeclaringClass();
+		final var typeArguments = typeArguments( superclass, subclass );
+		final var typeParameters = superclass.getTypeParameters();
+		final Map<TypeVariable<?>, Type> typeMap =
+				new HashMap<>( typeParameters.length );
+		for ( int i = 0; i < typeParameters.length; i++ ) {
+			typeMap.put( typeParameters[i], typeArguments[i] );
+		}
+		return typeMap;
+	}
+
+	private static Type getMemberType(Member superMember) {
 		if ( superMember instanceof Field field ) {
 			return field.getGenericType();
 		}
@@ -52,96 +72,96 @@ public final class GenericsHelper {
 		}
 	}
 
-	private static Map<TypeVariable<?>, Type> extractTypeArguments(
-			Class<?> subclass,
-			Class<?> targetSuperclass) {
-		final Map<TypeVariable<?>, Type> typeMap = new HashMap<>();
-		extractTypeArguments( subclass, targetSuperclass, typeMap );
-		return typeMap;
-	}
+// let's not delete this alternative implementation for now,
+// just in case we run into problems with the implementation above
 
-	private static boolean extractTypeArguments(
-			Class<?> current,
-			Class<?> target,
-			Map<TypeVariable<?>, Type> typeMap) {
+//	private static Map<TypeVariable<?>, Type> extractTypeArguments(
+//			Class<?> subclass,
+//			Class<?> targetSuperclass) {
+//		final Map<TypeVariable<?>, Type> typeMap = new HashMap<>();
+//		extractTypeArguments( subclass, targetSuperclass, typeMap );
+//		return typeMap;
+//	}
 
-		if ( current == null || current == Object.class ) {
-			return false;
-		}
+//	private static boolean extractTypeArguments(
+//			Class<?> current,
+//			Class<?> target,
+//			Map<TypeVariable<?>, Type> typeMap) {
+//
+//		if ( current == null || current == Object.class ) {
+//			return false;
+//		}
+//
+//		final var genericSuperclass = current.getGenericSuperclass();
+//		if ( genericSuperclass != null ) {
+//			final var raw = current.getSuperclass();
+//			if ( raw != null ) {
+//				if ( raw == target ) {
+//					collectTypeArguments( genericSuperclass, raw, typeMap );
+//					return true;
+//				}
+//				if ( extractTypeArguments( raw, target, typeMap ) ) {
+//					collectTypeArguments( genericSuperclass, raw, typeMap );
+//					return true;
+//				}
+//			}
+//		}
+//
+//		for ( var type : current.getGenericInterfaces() ) {
+//			final var raw = erasedType( type );
+//			if ( raw != null ) {
+//				if ( raw == target ) {
+//					collectTypeArguments( type, raw, typeMap );
+//					return true;
+//				}
+//				if ( extractTypeArguments( raw, target, typeMap ) ) {
+//					collectTypeArguments( type, raw, typeMap );
+//					return true;
+//				}
+//			}
+//		}
+//
+//		return false;
+//	}
 
-		final var genericSuperclass = current.getGenericSuperclass();
-		if ( genericSuperclass != null ) {
-			final var raw = current.getSuperclass();
-			if ( raw != null ) {
-				if ( raw == target ) {
-					collectTypeArguments( genericSuperclass, raw, typeMap );
-					return true;
-				}
-				if ( extractTypeArguments( raw, target, typeMap ) ) {
-					collectTypeArguments( genericSuperclass, raw, typeMap );
-					return true;
-				}
-			}
-		}
+//	private static void collectTypeArguments(
+//			Type type,
+//			Class<?> raw,
+//			Map<TypeVariable<?>, Type> typeMap) {
+//		if ( type instanceof ParameterizedType pt ) {
+//			final var vars = raw.getTypeParameters();
+//			final var args = pt.getActualTypeArguments();
+//			for ( int i = 0; i < vars.length; i++ ) {
+//				typeMap.put( vars[i], args[i] );
+//			}
+//		}
+//	}
 
-		for ( var type : current.getGenericInterfaces() ) {
-			final var raw = erasedType( type );
-			if ( raw != null ) {
-				if ( raw == target ) {
-					collectTypeArguments( type, raw, typeMap );
-					return true;
-				}
-				if ( extractTypeArguments( raw, target, typeMap ) ) {
-					collectTypeArguments( type, raw, typeMap );
-					return true;
-				}
-			}
-		}
+	private static Type substituteTypeVariables(Type type, Map<TypeVariable<?>, Type> typeMap) {
 
-		return false;
-	}
-
-	private static void collectTypeArguments(
-			Type type,
-			Class<?> raw,
-			Map<TypeVariable<?>, Type> typeMap) {
-		if ( type instanceof ParameterizedType pt ) {
-			final var vars = raw.getTypeParameters();
-			final var args = pt.getActualTypeArguments();
-			for ( int i = 0; i < vars.length; i++ ) {
-				typeMap.put( vars[i], args[i] );
-			}
-		}
-	}
-
-	private static Type substituteTypeVariables(
-			Type type,
-			Map<TypeVariable<?>, Type> typeMap) {
-
-		if ( type instanceof TypeVariable<?> tv ) {
-			final var substituted = typeMap.get( tv );
+		if ( type instanceof TypeVariable<?> typeVariable ) {
+			final var substituted = typeMap.get( typeVariable );
 			return substituted == null
 					? Object.class
 					: substituteTypeVariables( substituted, typeMap );
 		}
-		else if ( type instanceof ParameterizedType pt ) {
-			final var args = pt.getActualTypeArguments();
+		else if ( type instanceof ParameterizedType parameterizedType ) {
+			final var args = parameterizedType.getActualTypeArguments();
 			final var resolved = new Type[args.length];
 			for ( int i = 0; i < args.length; i++ ) {
 				resolved[i] = substituteTypeVariables( args[i], typeMap );
 			}
 			return new SimpleParameterizedType(
-					(Class<?>) pt.getRawType(),
+					(Class<?>) parameterizedType.getRawType(),
 					resolved,
-					pt.getOwnerType()
+					parameterizedType.getOwnerType()
 			);
 		}
-		else if ( type instanceof GenericArrayType gat ) {
+		else if ( type instanceof GenericArrayType genericArrayType ) {
 			final var elementType =
-					substituteTypeVariables( gat.getGenericComponentType(), typeMap );
+					substituteTypeVariables( genericArrayType.getGenericComponentType(), typeMap );
 			return new GenericArrayType() {
 				@Override
-				@NonNull
 				public Type getGenericComponentType() {
 					return elementType;
 				}
@@ -163,17 +183,17 @@ public final class GenericsHelper {
 	 * @return The erased type
 	 */
 	public static Class<?> erasedType(Type type) {
-		if ( type instanceof Class<?> c ) {
-			return c;
+		if ( type instanceof Class<?> clazz ) {
+			return clazz;
 		}
-		else if ( type instanceof ParameterizedType pt ) {
-			return erasedType( pt.getRawType() );
+		else if ( type instanceof ParameterizedType parameterizedType ) {
+			return erasedType( parameterizedType.getRawType() );
 		}
-		else if ( type instanceof TypeVariable<?> tv ) {
-			return erasedType( tv.getBounds()[0] );
+		else if ( type instanceof TypeVariable<?> typeVariable ) {
+			return erasedType( typeVariable.getBounds()[0] );
 		}
-		else if ( type instanceof GenericArrayType gat ) {
-			return gat.getGenericComponentType() instanceof Class<?> elementClass
+		else if ( type instanceof GenericArrayType genericArrayType ) {
+			return genericArrayType.getGenericComponentType() instanceof Class<?> elementClass
 					? elementClass.arrayType()
 					: Object[].class;
 		}
@@ -190,80 +210,102 @@ public final class GenericsHelper {
 	 * @return The type arguments assigned to parameters of the generic type constructor
 	 */
 	public static Type[] typeArguments(Class<?> genericType, Type implementingType) {
-
-		final Map<TypeVariable<?>, Type> typeMap = new HashMap<>();
-		if ( !collectSupertypeTypeArguments( implementingType, genericType, typeMap ) ) {
-			throw new IllegalArgumentException(
-					implementingType + " does not implement " + genericType );
+		if ( genericType.getTypeParameters().length == 0 ) {
+			return EMPTY_TYPE_ARRAY;
 		}
-
-		final var vars = genericType.getTypeParameters();
-		final var result = new Type[vars.length];
-		for ( int i = 0; i < vars.length; i++ ) {
-			result[i] = substituteTypeVariables( vars[i], typeMap );
+		else {
+			final var instantiation =
+					supertypeInstantiation( genericType, implementingType );
+			if ( instantiation == null ) {
+				throw new IllegalArgumentException(
+						implementingType.getTypeName()
+						+ " is is not a subtype of "
+						+ genericType.getName() );
+			}
+			return instantiation.getActualTypeArguments();
 		}
-		return result;
 	}
 
-	private static boolean collectSupertypeTypeArguments(
-			Type current,
-			Class<?> targetInterface,
-			Map<TypeVariable<?>, Type> typeMap) {
+	private static final Type[] EMPTY_TYPE_ARRAY = new Type[0];
 
-		if ( current == null ) {
-			return false;
-		}
+// let's not delete this alternative implementation for now,
+// just in case we run into problems with the implementation above
 
-		final var raw = erasedType( current );
-		if ( raw == null || raw == Object.class ) {
-			return false;
-		}
-
-		// If current is parameterized, record its bindings
-		collectTypeArguments( current, raw, typeMap );
-
-		if ( raw == targetInterface ) {
-			return true;
-		}
-
-		// Check directly implemented interfaces
-		for ( var iface : raw.getGenericInterfaces() ) {
-			final var ifaceRaw = erasedType( iface );
-			if ( ifaceRaw != null ) {
-				if ( ifaceRaw == targetInterface ) {
-					collectTypeArguments( iface, ifaceRaw, typeMap );
-					return true;
-				}
-
-				if ( targetInterface.isAssignableFrom( ifaceRaw ) ) {
-					if ( collectSupertypeTypeArguments( iface, targetInterface, typeMap ) ) {
-						collectTypeArguments( iface, ifaceRaw, typeMap );
-						return true;
-					}
-				}
-			}
-
-		}
-
-		// Check superclass
-		final var superclass = raw.getGenericSuperclass();
-		if ( superclass != null ) {
-			final var classRaw = erasedType( superclass );
-			if ( classRaw != null ) {
-				if ( classRaw == targetInterface ) {
-					collectTypeArguments( superclass, classRaw, typeMap );
-					return true;
-				}
-
-				if ( collectSupertypeTypeArguments( superclass, targetInterface, typeMap ) ) {
-					collectTypeArguments( superclass, erasedType( superclass ), typeMap );
-					return true;
-				}
-			}
-		}
-
-		return false;
-	}
+//	public static Type[] typeArguments(Class<?> genericType, Type implementingType) {
+//
+//		final Map<TypeVariable<?>, Type> typeMap = new HashMap<>();
+//		if ( !collectSupertypeTypeArguments( implementingType, genericType, typeMap ) ) {
+//			throw new IllegalArgumentException(
+//					implementingType + " does not implement " + genericType );
+//		}
+//
+//		final var vars = genericType.getTypeParameters();
+//		final var result = new Type[vars.length];
+//		for ( int i = 0; i < vars.length; i++ ) {
+//			result[i] = substituteTypeVariables( vars[i], typeMap );
+//		}
+//		return result;
+//	}
+//
+//	private static boolean collectSupertypeTypeArguments(
+//			Type current,
+//			Class<?> targetInterface,
+//			Map<TypeVariable<?>, Type> typeMap) {
+//
+//		if ( current == null ) {
+//			return false;
+//		}
+//
+//		final var raw = erasedType( current );
+//		if ( raw == null || raw == Object.class ) {
+//			return false;
+//		}
+//
+//		// If current is parameterized, record its bindings
+//		collectTypeArguments( current, raw, typeMap );
+//
+//		if ( raw == targetInterface ) {
+//			return true;
+//		}
+//
+//		// Check directly implemented interfaces
+//		for ( var iface : raw.getGenericInterfaces() ) {
+//			final var ifaceRaw = erasedType( iface );
+//			if ( ifaceRaw != null ) {
+//				if ( ifaceRaw == targetInterface ) {
+//					collectTypeArguments( iface, ifaceRaw, typeMap );
+//					return true;
+//				}
+//
+//				if ( targetInterface.isAssignableFrom( ifaceRaw ) ) {
+//					if ( collectSupertypeTypeArguments( iface, targetInterface, typeMap ) ) {
+//						collectTypeArguments( iface, ifaceRaw, typeMap );
+//						return true;
+//					}
+//				}
+//			}
+//
+//		}
+//
+//		// Check superclass
+//		final var superclass = raw.getGenericSuperclass();
+//		if ( superclass != null ) {
+//			final var classRaw = erasedType( superclass );
+//			if ( classRaw != null ) {
+//				if ( classRaw == targetInterface ) {
+//					collectTypeArguments( superclass, classRaw, typeMap );
+//					return true;
+//				}
+//
+//				if ( collectSupertypeTypeArguments( superclass, targetInterface, typeMap ) ) {
+//					collectTypeArguments( superclass, erasedType( superclass ), typeMap );
+//					return true;
+//				}
+//			}
+//		}
+//
+//		return false;
+//	}
 
 	/**
 	 * A supertype of the given type which is an instantiation of the
@@ -274,9 +316,6 @@ public final class GenericsHelper {
 	 * @return An instantiation of the generic type constructor which
 	 *         is a supertype of the given type, or null if none exists
 	 */
-	// currently unused, but let's not delete this one
-	// this might be more efficient than typeArguments(),
-	// assuming that it's actually correct
 	public static ParameterizedType supertypeInstantiation(Class<?> genericType, Type base) {
 		if ( base == null ) {
 			return null;
@@ -287,10 +326,14 @@ public final class GenericsHelper {
 			return null;
 		}
 
+		if ( clazz == genericType
+				&& base instanceof ParameterizedType result ) {
+			return result;
+		}
+
 		final var superclass = clazz.getGenericSuperclass();
 		if ( superclass != null ) {
-			var type = superclass;
-			type = resolveType( type, base );
+			final var type = substituteTypeArguments( superclass, base );
 			if ( type instanceof ParameterizedType parameterizedType
 					&& genericType.equals( parameterizedType.getRawType() ) ) {
 				return parameterizedType;
@@ -303,8 +346,8 @@ public final class GenericsHelper {
 			}
 		}
 
-		for ( var type : clazz.getGenericInterfaces() ) {
-			type = resolveType( type, base );
+		for ( var iface : clazz.getGenericInterfaces() ) {
+			final var type = substituteTypeArguments( iface, base );
 			if ( type instanceof ParameterizedType parameterizedType
 					&& genericType.equals( parameterizedType.getRawType() ) ) {
 				return parameterizedType;
@@ -320,7 +363,8 @@ public final class GenericsHelper {
 		return null;
 	}
 
-	private static Type resolveTypeVariable(TypeVariable<?> typeVariable, ParameterizedType context) {
+	private static Type replaceTypeVariableWithArgument(
+			TypeVariable<?> typeVariable, ParameterizedType context) {
 		final var clazz = erasedType( context.getRawType() );
 		if ( clazz == null ) {
 			return null;
@@ -330,30 +374,32 @@ public final class GenericsHelper {
 		final var typeParameters = clazz.getTypeParameters();
 		for ( int idx = 0; idx < typeParameters.length; idx++ ) {
 			if ( typeVariable.getName().equals( typeParameters[idx].getName() ) ) {
-				return resolveType( typeArguments[idx], context );
+				return substituteTypeArguments( typeArguments[idx], context );
 			}
 		}
 
 		return typeVariable;
 	}
 
-	private static Type resolveType(Type target, Type context) {
+	private static Type substituteTypeArguments(Type target, Type context) {
 		if ( target instanceof ParameterizedType parameterizedType ) {
-			return resolveParameterizedType( parameterizedType, context );
+			return replaceTypeVariablesWithArguments( parameterizedType, context );
 		}
-		else if ( target instanceof TypeVariable<?> typeVariable ) {
-			return resolveTypeVariable( typeVariable, (ParameterizedType) context );
+		else if ( target instanceof TypeVariable<?> typeVariable
+					&& context instanceof ParameterizedType parameterizedContext ) {
+			return replaceTypeVariableWithArgument( typeVariable, parameterizedContext );
 		}
 		else {
 			return target;
 		}
 	}
 
-	private static ParameterizedType resolveParameterizedType(ParameterizedType parameterizedType, Type context) {
+	private static ParameterizedType replaceTypeVariablesWithArguments(
+			ParameterizedType parameterizedType, Type context) {
 		final var typeArguments = parameterizedType.getActualTypeArguments();
 		final var resolvedTypeArguments = new Type[typeArguments.length];
 		for ( int idx = 0; idx < typeArguments.length; idx++ ) {
-			resolvedTypeArguments[idx] = resolveType( typeArguments[idx], context );
+			resolvedTypeArguments[idx] = substituteTypeArguments( typeArguments[idx], context );
 		}
 		return new SimpleParameterizedType(
 				erasedType( parameterizedType ),
@@ -362,29 +408,24 @@ public final class GenericsHelper {
 		);
 	}
 
-	private record SimpleParameterizedType
-			(@NonNull Class<?> raw, @NonNull Type[] args, @NonNull Type owner)
+	private record SimpleParameterizedType(Class<?> raw, Type[] args, Type owner)
 			implements ParameterizedType {
 		@Override
-		@NonNull
 		public Type[] getActualTypeArguments() {
 			return args;
 		}
 
 		@Override
-		@NonNull
 		public Type getRawType() {
 			return raw;
 		}
 
 		@Override
-		@NonNull
 		public Type getOwnerType() {
 			return owner;
 		}
 
 		@Override
-		@NonNull
 		public String toString() {
 			final var joiner = new StringJoiner( ", ", "<", ">" );
 			for ( var type : args ) {

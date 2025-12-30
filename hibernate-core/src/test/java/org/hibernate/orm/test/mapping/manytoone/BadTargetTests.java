18cc198ce5f08369e6f9184e250d7b4003436652
Steve Ebersole <steve@hibernate.org>
2025-12-30 08:50:31 -0700

    HHH-7202 - Early detection of bad targetEntity

--- hibernate-core/src/test/java/org/hibernate/orm/test/mapping/manytoone/BadTargetTests.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/mapping/manytoone/BadTargetTests.java
@@ -8,28 +8,30 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.OneToMany;
 import org.hibernate.MappingException;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.spi.MetadataImplementor;
 import org.hibernate.testing.orm.junit.Jira;
 import org.hibernate.testing.orm.junit.ServiceRegistry;
 import org.hibernate.testing.orm.junit.ServiceRegistryScope;
-import org.hibernate.testing.orm.junit.Setting;
 import org.junit.jupiter.api.Test;
 
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
-import static org.hibernate.cfg.SchemaToolingSettings.HBM2DDL_AUTO;
 
 /**
  * @author Steve Ebersole
  */
 @SuppressWarnings("JUnitMalformedDeclaration")
 @Jira( "https://hibernate.atlassian.net/browse/HHH-7202" )
-@ServiceRegistry(settings = @Setting( name = HBM2DDL_AUTO, value = "create-drop" ))
+@ServiceRegistry
 public class BadTargetTests {
 	@Test
-	void testIt(ServiceRegistryScope registryScope) {
+	void testToOne(ServiceRegistryScope registryScope) {
 		var model = (MetadataImplementor) new MetadataSources( registryScope.getRegistry() )
 				.addAnnotatedClasses( Foo.class, Bar.class, Baz.class )
 				.buildMetadata();
@@ -40,8 +42,35 @@ public class BadTargetTests {
 		}
 	}
 
-	@Entity(name="Foo")
-	@Table(name="foos")
+	@Test
+	void testToMany(ServiceRegistryScope registryScope) {
+		var model = (MetadataImplementor) new MetadataSources( registryScope.getRegistry() )
+				.addAnnotatedClasses( Brazos.class, Bar.class, Baz.class )
+				.buildMetadata();
+		try (var sf = model.buildSessionFactory()) {
+			fail( "Expecting a failure" );
+		}
+		catch (MappingException expected) {
+			assertThat( expected.getMessage() ).startsWith( "Plural attribute [" );
+			assertThat( expected.getMessage() ).contains( "Brazos.bars] was mapped with targetEntity=`" );
+		}
+	}
+
+	@Test
+	void testToManyGetter(ServiceRegistryScope registryScope) {
+		var model = (MetadataImplementor) new MetadataSources( registryScope.getRegistry() )
+				.addAnnotatedClasses( Brazos2.class, Bar.class, Baz.class )
+				.buildMetadata();
+		try (var sf = model.buildSessionFactory()) {
+			fail( "Expecting a failure" );
+		}
+		catch (MappingException expected) {
+			assertThat( expected.getMessage() ).startsWith( "Plural attribute [" );
+			assertThat( expected.getMessage() ).contains( "Brazos2.bars] was mapped with targetEntity=`" );
+		}
+	}
+
+	@Entity
 	public static class Foo {
 		@Id
 		private Integer id;
@@ -60,8 +89,7 @@ public class BadTargetTests {
 		}
 	}
 
-	@Entity(name="Bar")
-	@Table(name="bars")
+	@Entity
 	public static class Bar {
 		@Id
 		private Integer id;
@@ -76,11 +104,61 @@ public class BadTargetTests {
 		}
 	}
 
-	@Entity(name="Baz")
-	@Table(name="Baz")
+	@Entity
 	public static class Baz {
 		@Id
 		private Integer id;
 		private String name;
 	}
+
+	@Entity
+	public static class Brazos {
+		@Id
+		private Integer id;
+		private String name;
+		@OneToMany(targetEntity = Baz.class)
+		@JoinColumn(name = "bar_fk")
+		private Set<Bar> bars;
+
+		public Brazos() {
+		}
+	}
+
+	@Entity
+	public static class Brazos2 {
+		private Integer id;
+		private String name;
+		private Set<Bar> bars;
+
+		public Brazos2() {
+		}
+
+		@Id
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		@OneToMany(targetEntity = Baz.class)
+		@JoinColumn(name = "bar_fk")
+		public Set<Bar> getBars() {
+			return bars;
+		}
+
+		public void setBars(Set<Bar> bars) {
+			this.bars = bars;
+		}
+	}
+
 }

085cf0d5a492a8e1aaa1a53799da6ebea6fd37f5
marko-bekhta <marko.prykladna@gmail.com>
2026-01-05 22:20:09 +0100

    Adjust the "reclaim disk space" step in the GH workflow builds

--- .github/ci-prerequisites.sh
+++ .github/ci-prerequisites.sh
@@ -1,8 +1,14 @@
 # Reclaim disk space, otherwise we only have 13 GB free at the start of a job
 
+echo 'Before the cleanup:'
+df -h
 # Remove the container images for node:
-docker rmi node:10 node:12 mcr.microsoft.com/azure-pipelines/node8-typescript:latest
+echo 'Docker images (available for possible removal):'
+docker images
 # That is 18 GB
 sudo rm -rf /usr/share/dotnet
 # That is 1.2 GB
 sudo rm -rf /usr/share/swift
+
+echo 'After the cleanup:'
+df -h

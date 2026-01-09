916e852a27d7af6c90ebe02690ecff35ab6c44fa
Steve Ebersole <steve@hibernate.org>
2026-01-09 10:39:24 -0700

    HHH-20028 - Update to Jakarta Persistence 4.0

--- hibernate-core/src/main/java/org/hibernate/query/sqm/internal/SqmQueryImpl.java
+++ hibernate-core/src/main/java/org/hibernate/query/sqm/internal/SqmQueryImpl.java
@@ -23,7 +23,6 @@ import org.hibernate.LockMode;
 import org.hibernate.LockOptions;
 import org.hibernate.Locking;
 import org.hibernate.ScrollMode;
-import org.hibernate.Timeouts;
 import org.hibernate.engine.spi.LoadQueryInfluencers;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.graph.GraphSemantic;
@@ -769,7 +768,7 @@ public class SqmQueryImpl<R>
 	public SqmQueryImplementor<R> setTimeout(Timeout timeout) {
 		assert timeout != null;
 		getSession().checkOpen( false );
-		getQueryOptions().setTimeout( Timeouts.getTimeoutInSeconds( timeout ) );
+		getQueryOptions().setTimeout( timeout );
 		return this;
 	}
 

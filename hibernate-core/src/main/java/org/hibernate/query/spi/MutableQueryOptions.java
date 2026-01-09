5cd849acc3a016723f858d82958fd7de245e403a
Steve Ebersole <steve@hibernate.org>
2026-01-09 10:39:24 -0700

    HHH-20028 - Update to Jakarta Persistence 4.0

--- hibernate-core/src/main/java/org/hibernate/query/spi/MutableQueryOptions.java
+++ hibernate-core/src/main/java/org/hibernate/query/spi/MutableQueryOptions.java
@@ -7,6 +7,7 @@ package org.hibernate.query.spi;
 import jakarta.persistence.CacheRetrieveMode;
 import jakarta.persistence.CacheStoreMode;
 
+import jakarta.persistence.Timeout;
 import org.hibernate.CacheMode;
 import org.hibernate.FlushMode;
 import org.hibernate.graph.GraphSemantic;
@@ -67,6 +68,11 @@ public interface MutableQueryOptions extends QueryOptions {
 	 * Corollary to {@link #isLimitInMemoryEnabled()}
 	 */
 	void setLimitInMemory(boolean limitInMemory);
+
+	/**
+	 * Corollary to {@link #getTimeout()}
+	 */
+	void setTimeout(Timeout timeout);
 
 	/**
 	 * Corollary to {@link #getTimeout()}

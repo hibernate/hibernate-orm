aa7b69f8fd96413df714807b00c567ce4e232fd6
isc-service <RE-External@intersystems.com>
2026-01-14 09:30:34 -0500

    HHH-19956: Add InterSystemsIRISLimitHandler.java

---
+++ hibernate-community-dialects/src/main/java/org/hibernate/community/dialect/pagination/InterSystemsIRISLimitHandler.java
@@ -0,0 +1,87 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.community.dialect.pagination;
+
+import org.hibernate.dialect.pagination.AbstractLimitHandler;
+import org.hibernate.query.spi.Limit;
+import java.util.Locale;
+
+public class InterSystemsIRISLimitHandler extends AbstractLimitHandler {
+	public static final InterSystemsIRISLimitHandler INSTANCE = new InterSystemsIRISLimitHandler(true);
+
+	private final boolean variableLimit;
+
+	public InterSystemsIRISLimitHandler(boolean variableLimit) {
+		this.variableLimit = variableLimit;
+	}
+
+	@Override
+	public String processSql(String sql, Limit limit) {
+
+		boolean hasFirstRow = hasFirstRow(limit);
+		boolean hasMaxRows = hasMaxRows(limit);
+
+		if (!hasFirstRow && !hasMaxRows) {
+			return sql;
+		}
+
+		String lowersql = sql.toLowerCase(Locale.ROOT);
+		int selectIndex = lowersql.indexOf("select");
+		if (hasFirstRow && hasMaxRows) {
+			return new StringBuilder(sql.length() + 27)
+					.append(sql)
+					.insert(selectIndex + 6, " %ROWOFFSET ? %ROWLIMIT ? ")
+					.toString();
+
+		}
+		else if (hasFirstRow) {
+			return new StringBuilder(sql.length() + 15)
+					.append(sql)
+					.insert(selectIndex + 6, " %ROWOFFSET ? ")
+					.toString();
+		}
+		else {
+			final int selectDistinctIndex = lowersql.indexOf("select distinct");
+			final int insertionPoint = selectIndex + (selectDistinctIndex == selectIndex ? 15 : 6);
+
+			return new StringBuilder(sql.length() + 8)
+					.append(sql)
+					.insert(insertionPoint, " TOP ? ")
+					.toString();
+		}
+	}
+
+
+	@Override
+	public final boolean supportsLimit() {
+		return true;
+	}
+
+	@Override
+	public final boolean supportsOffset() {
+		return true;
+	}
+
+	@Override
+	public boolean supportsLimitOffset() {
+		return true;
+	}
+
+	@Override
+	public final boolean supportsVariableLimit() {
+		return true;
+	}
+
+	@Override
+	public boolean useMaxForLimit() {
+		return false;
+	}
+
+	@Override
+	public boolean bindLimitParametersFirst() {
+		return true;
+	}
+
+}

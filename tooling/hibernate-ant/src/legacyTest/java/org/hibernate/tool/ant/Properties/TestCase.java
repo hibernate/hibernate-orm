9bb0e4a5f056b0ff372d1db8a178c35639b3fe3f
Koen Aers <koen.aers@gmail.com>
2026-01-14 17:18:35 +0100

    HBX-3276: Move the 'Apache Ant' tests in the 'test/common' module to the 'legacyTest' source tree in the 'ant' module   - Move the 'ant/Properties' test
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- tooling/hibernate-ant/src/legacyTest/java/org/hibernate/tool/ant/Properties/TestCase.java
+++ tooling/hibernate-ant/src/legacyTest/java/org/hibernate/tool/ant/Properties/TestCase.java
@@ -1,13 +1,26 @@
 /*
- * SPDX-License-Identifier: Apache-2.0
- * Copyright Red Hat Inc. and Hibernate Authors
+ * Hibernate Tools, Tooling for your Hibernate Projects
+ *
+ * Copyright 2004-2025 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.hibernate.tool.ant.Properties;
 
-import org.hibernate.tool.ant.test.utils.AntUtil;
-import org.hibernate.tool.ant.test.utils.FileUtil;
-import org.hibernate.tool.ant.test.utils.JdbcUtil;
-import org.hibernate.tool.ant.test.utils.ResourceUtil;
+import org.hibernate.tools.test.util.AntUtil;
+import org.hibernate.tools.test.util.FileUtil;
+import org.hibernate.tools.test.util.JdbcUtil;
+import org.hibernate.tools.test.util.ResourceUtil;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,17 +28,16 @@ import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestCase {
-
+	
 	@TempDir
 	public File outputFolder = new File("output");
-
+	
 	private File destinationDir = null;
 	private File resourcesDir = null;
-
+	
 	@BeforeEach
 	public void setUp() {
 		destinationDir = new File(outputFolder, "destination");
@@ -34,40 +46,40 @@ public class TestCase {
 		assertTrue(resourcesDir.mkdir());
 		JdbcUtil.createDatabase(this);
 	}
-
+	
 	@AfterEach
 	public void tearDown() {
 		JdbcUtil.dropDatabase(this);
 	}
-
+	
 	@Test
 	public void testProperties() {
 
 		String[] resources = new String[] {"build.xml", "SomeClass.hbm.xml"};
 		ResourceUtil.createResources(this, resources, resourcesDir);
-		File buildFile = new File(resourcesDir, "build.xml");
+		File buildFile = new File(resourcesDir, "build.xml");	
 		ResourceUtil.createResources(this, new String[] { "/hibernate.properties" }, resourcesDir);
 		File templatesDir = new File(resourcesDir, "templates");
 		assertTrue(templatesDir.mkdir());
 		File pojoTemplateDir = new File(templatesDir, "pojo");
 		assertTrue(pojoTemplateDir.mkdir());
 		ResourceUtil.createResources(this, new String[] { "Pojo.ftl" }, pojoTemplateDir);
-
+		
 		AntUtil.Project project = AntUtil.createProject(buildFile);
 		project.setProperty("destinationDir", destinationDir.getAbsolutePath());
 		project.setProperty("resourcesDir", resourcesDir.getAbsolutePath());
-
+		
 		File file = new File(destinationDir, "SomeClass.java");
 		assertFalse(file.exists());
-
+		
 		project.executeTarget("testProperties");
 
 		String log = AntUtil.getLog(project);
-		assertFalse(log.contains("Exception"), log);
-
+        assertFalse(log.contains("Exception"), log);
+		
 		assertTrue(file.exists());
-
-		assertTrue( FileUtil
+		
+		assertTrue(FileUtil
 				.findFirstString("hbm2java.weirdAl", file)
 				.contains("foo3"));
 		assertTrue(FileUtil
@@ -85,6 +97,7 @@ public class TestCase {
 		assertTrue(FileUtil
 				.findFirstString("value", file)
 				.contains("some value"));
-	}
 
+	}
+	
 }

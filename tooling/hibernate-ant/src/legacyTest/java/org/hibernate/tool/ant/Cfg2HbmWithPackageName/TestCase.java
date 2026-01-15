a04313f03a2579749a77545c5387b701c62cc67d
Koen Aers <koen.aers@gmail.com>
2026-01-15 14:16:44 +0100

    HBX-3276: Move the 'Apache Ant' tests in the 'test/common' module to the 'legacyTest' source tree in the 'ant' module   - Move the 'ant/Cfg2HbmWithPackageName' test
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- tooling/hibernate-ant/src/legacyTest/java/org/hibernate/tool/ant/Cfg2HbmWithPackageName/TestCase.java
+++ tooling/hibernate-ant/src/legacyTest/java/org/hibernate/tool/ant/Cfg2HbmWithPackageName/TestCase.java
@@ -1,13 +1,26 @@
 /*
- * SPDX-License-Identifier: Apache-2.0
- * Copyright Red Hat Inc. and Hibernate Authors
+ * Hibernate Tools, Tooling for your Hibernate Projects
+ *
+ * Copyright 2004-2025 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.hibernate.tool.ant.Cfg2HbmWithPackageName;
 
-import org.hibernate.tool.ant.test.utils.AntUtil;
-import org.hibernate.tool.ant.test.utils.FileUtil;
-import org.hibernate.tool.ant.test.utils.JdbcUtil;
-import org.hibernate.tool.ant.test.utils.ResourceUtil;
+import org.hibernate.tools.test.util.AntUtil;
+import org.hibernate.tools.test.util.FileUtil;
+import org.hibernate.tools.test.util.JdbcUtil;
+import org.hibernate.tools.test.util.ResourceUtil;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,39 +28,38 @@ import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestCase {
-
+	
 	@TempDir
 	public File outputFolder = new File("output");
-
+	
 	private File destinationDir = null;
 	private File resourcesDir = null;
-
+	
 	@BeforeEach
 	public void setUp() {
 		destinationDir = new File(outputFolder, "destination");
-		assertTrue( destinationDir.mkdir());
+		destinationDir.mkdir();
 		resourcesDir = new File(outputFolder, "resources");
-		assertTrue(resourcesDir.mkdir());
+		resourcesDir.mkdir();
 		JdbcUtil.createDatabase(this);
 	}
-
+	
 	@AfterEach
 	public void tearDown() {
 		JdbcUtil.dropDatabase(this);
 	}
-
+	
 	@Test
 	public void testCfg2HbmWithPackageName() {
 
 		String[] resources = new String[] {"build.xml"};
 		ResourceUtil.createResources(this, resources, resourcesDir);
-		File buildFile = new File(resourcesDir, "build.xml");
+		File buildFile = new File(resourcesDir, "build.xml");	
 		ResourceUtil.createResources(this, new String[] { "/hibernate.properties" }, resourcesDir);
-
+		
 		AntUtil.Project project = AntUtil.createProject(buildFile);
 		project.setProperty("destinationDir", destinationDir.getAbsolutePath());
 		project.setProperty("resourcesDir", resourcesDir.getAbsolutePath());
@@ -56,11 +68,11 @@ public class TestCase {
 		assertFalse(hbmxml.exists());
 
 		project.executeTarget("testCfg2HbmWithPackageName");
-
+		
 		assertTrue(hbmxml.exists());
-		assertTrue( FileUtil
+		assertTrue(FileUtil
 				.findFirstString("class", hbmxml)
 				.contains("foo.bar.VerySimpleTable"));
 	}
-
+	
 }

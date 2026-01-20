6dad631795129d650cf13c54bc93ce966d882fcb
Koen Aers <koen.aers@gmail.com>
2026-01-20 14:42:34 +0100

    HBX-3276: Move the 'Apache Ant' tests in the 'test/common' module to the 'legacyTest' source tree in the 'ant' module       - Move the 'ant/EJB3Configuration' test
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- tooling/hibernate-ant/src/legacyTest/java/org/hibernate/tool/ant/EJB3Configuration/TestCase.java
+++ tooling/hibernate-ant/src/legacyTest/java/org/hibernate/tool/ant/EJB3Configuration/TestCase.java
@@ -1,13 +1,26 @@
 /*
- * SPDX-License-Identifier: Apache-2.0
- * Copyright Red Hat Inc. and Hibernate Authors
+ * Hibernate Tools, Tooling for your Hibernate Projects
+ *
+ * Copyright 2004-2025 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.hibernate.tool.ant.EJB3Configuration;
 
-import org.hibernate.tool.ant.test.utils.AntUtil;
-import org.hibernate.tool.ant.test.utils.FileUtil;
-import org.hibernate.tool.ant.test.utils.JdbcUtil;
-import org.hibernate.tool.ant.test.utils.ResourceUtil;
+import org.hibernate.tools.test.util.AntUtil;
+import org.hibernate.tools.test.util.FileUtil;
+import org.hibernate.tools.test.util.JdbcUtil;
+import org.hibernate.tools.test.util.ResourceUtil;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,18 +28,16 @@ import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestCase {
-
+	
 	@TempDir
 	public File outputFolder = new File("output");
-
+	
 	private File destinationDir = null;
 	private File resourcesDir = null;
-
+	
 	@BeforeEach
 	public void setUp() {
 		destinationDir = new File(outputFolder, "destination");
@@ -35,32 +46,32 @@ public class TestCase {
 		assertTrue(resourcesDir.mkdir());
 		JdbcUtil.createDatabase(this);
 	}
-
+	
 	@AfterEach
 	public void tearDown() {
 		JdbcUtil.dropDatabase(this);
 	}
-
+	
 	@Test
 	public void testEJB3ConfigurationFailureExpected() {
 
 		String[] resources = new String[] {"build.xml", "hibernate.cfg.xml", "persistence.xml"};
 		ResourceUtil.createResources(this, resources, resourcesDir);
-		File buildFile = new File(resourcesDir, "build.xml");
+		File buildFile = new File(resourcesDir, "build.xml");	
 		ResourceUtil.createResources(this, new String[] { "/hibernate.properties" }, resourcesDir);
-
+		
 		AntUtil.Project project = AntUtil.createProject(buildFile);
 		project.setProperty("destinationDir", destinationDir.getAbsolutePath());
 		project.setProperty("resourcesDir", resourcesDir.getAbsolutePath());
 
 		File ejb3 = new File(destinationDir, "ejb3.sql");
 		assertFalse(ejb3.exists());
-
+				
 		project.executeTarget("testEJB3Configuration");
 
 		assertTrue(ejb3.exists());
-		assertNotNull( FileUtil.findFirstString("create", ejb3));
+		assertNotNull(FileUtil.findFirstString("create", ejb3));
 
 	}
-
+	
 }

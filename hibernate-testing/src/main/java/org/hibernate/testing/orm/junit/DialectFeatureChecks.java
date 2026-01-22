1d9d0b164d8ef72d3a348ce89885d2273641edfe
Rayudu A L P <abbireddynani@gmail.com>
2026-01-22 15:53:23 +0530

    HHH-20099: Handle on conflict clause for Spanner

--- hibernate-testing/src/main/java/org/hibernate/testing/orm/junit/DialectFeatureChecks.java
+++ hibernate-testing/src/main/java/org/hibernate/testing/orm/junit/DialectFeatureChecks.java
@@ -1317,8 +1317,9 @@ abstract public class DialectFeatureChecks {
 	}
 
 	public static class SupportsUpsertOrMerge implements DialectFeatureCheck {
+		@Override
 		public boolean apply(Dialect dialect) {
-			return !( dialect instanceof DerbyDialect );
+			return !(dialect instanceof DerbyDialect || dialect instanceof SpannerDialect);
 		}
 	}
 

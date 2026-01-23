d0bfb76d86a549d405a540e144209a8852519652
Wander Winkelhorst <w.winkelhorst@meineman.nl>
2026-01-23 13:18:34 +0100

    HHH-20253 Fix ClassCastException when using hibernate-enhance-maven-plugin plugin

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/bytecode/enhancement/lazy/proxy/HHH20253Test.java
@@ -0,0 +1,206 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.orm.test.bytecode.enhancement.lazy.proxy;
+
+import jakarta.persistence.*;
+import org.hibernate.testing.bytecode.enhancement.EnhancementOptions;
+import org.hibernate.testing.bytecode.enhancement.extension.BytecodeEnhanced;
+import org.hibernate.testing.orm.junit.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static jakarta.persistence.CascadeType.ALL;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hibernate.graph.GraphSemantic.FETCH;
+
+/**
+ * @author Wander Winkelhorst
+ */
+
+@JiraKey( "HHH-20253" )
+@DomainModel(
+		annotatedClasses = {
+				HHH20253Test.CompanyEntity.class,
+				HHH20253Test.CompanyEmails.class,
+				HHH20253Test.Item.class
+		}
+)
+@ServiceRegistry
+@SessionFactory
+@BytecodeEnhanced
+@EnhancementOptions( lazyLoading = true )
+class HHH20253Test {
+
+	@Test
+	void testWithFetchGraph(SessionFactoryScope scope) {
+		scope.inTransaction( session -> {
+			var sqlStatementInspector = scope.getCollectingStatementInspector();
+			sqlStatementInspector.clear();
+			var query = session.createQuery( "select e from Item e", Item.class )
+					.setFetchSize( 10 );
+
+			var graph = session.createEntityGraph( Item.class );
+			graph.addAttributeNode("company");
+			query.applyGraph( graph, FETCH );
+
+			var resultList = query.list();
+			assertThat( resultList ).isNotEmpty();
+		} );
+	}
+
+	@BeforeEach
+	void prepareTestData(SessionFactoryScope scope) {
+		scope.inTransaction(
+				session -> {
+
+					CompanyEntity company1 = new CompanyEntity();
+					company1.setCompanyEmails( new CompanyEmails( company1 ) );
+					session.persist( company1 );
+
+					CompanyEntity company2 = new CompanyEntity();
+					company2.setCompanyEmails( new CompanyEmails( company2 ) );
+					company2.setParent( company1 );
+					session.persist( company2 );
+
+					Item item1 = new Item();
+					item1.setCompany( company2 );
+					session.persist( item1 );
+
+					Item item2 = new Item();
+					item2.setCompany( company1 );
+					session.persist( item2 );
+
+					session.flush();
+					session.clear();
+				}
+		);
+	}
+
+	@AfterEach
+	public void tearDown(SessionFactoryScope scope) {
+		scope.inTransaction(
+				session -> {
+					session.createQuery( "delete from Item" ).executeUpdate();
+				}
+		);
+	}
+
+	@Entity
+	static class CompanyEntity {
+
+		@Id
+		@GeneratedValue
+		@Column(name = "id", nullable = false)
+		private Long id;
+
+		@OneToOne(mappedBy = "company", cascade = ALL, orphanRemoval = true)
+		private CompanyEmails companyEmails;
+
+		@ManyToOne(fetch = FetchType.LAZY)
+    	@JoinColumn(name = "parent_company_id")
+		private CompanyEntity parent;
+
+		public CompanyEntity getParent() {
+			return parent;
+		}
+
+		public void setParent(CompanyEntity parent) {
+			this.parent = parent;
+		}
+
+		@Override
+		public String toString() {
+			return "RootEntity#" + id;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public void setCompanyEmails(CompanyEmails companyEmails) {
+			this.companyEmails = companyEmails;
+		}
+	}
+
+	@Entity
+	static class CompanyEmails {
+
+		@Id
+		@GeneratedValue
+		@Column(name = "id", nullable = false)
+		private Long id;
+
+		@OneToOne
+		@JoinColumn(name = "company_id")
+		private CompanyEntity company;
+
+		public CompanyEmails() {
+		}
+
+		public CompanyEmails(CompanyEntity company) {
+			this.company = company;
+		}
+
+		public CompanyEntity getCompany() {
+			return company;
+		}
+
+		public void setCompany(CompanyEntity company) {
+			this.company = company;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+	}
+
+	@Entity(name = "Item")
+	static class Item {
+
+		@Id
+		@GeneratedValue
+		private Long id;
+
+		private String name;
+
+		@ManyToOne(fetch = FetchType.LAZY)
+		private CompanyEntity company;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public CompanyEntity getCompany() {
+			return company;
+		}
+
+		public void setCompany(CompanyEntity company) {
+			this.company = company;
+		}
+	}
+}

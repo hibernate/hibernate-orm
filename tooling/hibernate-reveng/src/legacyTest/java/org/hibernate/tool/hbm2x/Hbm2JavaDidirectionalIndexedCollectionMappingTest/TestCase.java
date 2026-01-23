b653dcc327ab2c3dfaa21c121e4e791417a3c0e5
Koen Aers <koen.aers@gmail.com>
2026-01-23 11:39:45 +0100

    HBX-3282: Move the relevant tests in module 'test/nodb' to the 'legacyTest' source tree in the 'orm' module   - Move 'org.hibernate.tool.hbm2x.JavaDidirectionalIndexedCollectionMappingTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ tooling/hibernate-reveng/src/legacyTest/java/org/hibernate/tool/hbm2x/Hbm2JavaDidirectionalIndexedCollectionMappingTest/TestCase.java
@@ -0,0 +1,72 @@
+/*
+ * Hibernate Tools, Tooling for your Hibernate Projects
+ *
+ * Copyright 2004-2025 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.hibernate.tool.hbm2x.Hbm2JavaDidirectionalIndexedCollectionMappingTest;
+
+import org.hibernate.tool.api.export.Exporter;
+import org.hibernate.tool.api.export.ExporterConstants;
+import org.hibernate.tool.api.export.ExporterFactory;
+import org.hibernate.tool.api.export.ExporterType;
+import org.hibernate.tool.api.metadata.MetadataDescriptor;
+import org.hibernate.tool.test.utils.HibernateUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+/**
+ * @author max
+ * @author koen
+ */
+public class TestCase {
+
+	private static final String[] HBM_XML_FILES = new String[] {
+			"GenericModel.hbm.xml"
+	};
+	
+	@TempDir
+	public File outputFolder = new File("output");
+	
+	private File srcDir = null;
+	private File resourcesDir = null;
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		srcDir = new File(outputFolder, "src");
+		srcDir.mkdir();
+		resourcesDir = new File(outputFolder, "resources");
+		resourcesDir.mkdir();
+		MetadataDescriptor metadataDescriptor = HibernateUtil
+				.initializeMetadataDescriptor(this, HBM_XML_FILES, resourcesDir);
+		Exporter exporter = ExporterFactory.createExporter(ExporterType.JAVA);
+		exporter.getProperties().put(ExporterConstants.METADATA_DESCRIPTOR, metadataDescriptor);
+		exporter.getProperties().put(ExporterConstants.DESTINATION_FOLDER, srcDir);
+		exporter.start();
+	}
+	
+	@Test
+	public void testReflection() throws Exception {
+		assertTrue(new File(srcDir, "GenericObject.java").exists());
+		assertTrue(new File(srcDir, "GenericValue.java").exists());
+	}
+	
+}

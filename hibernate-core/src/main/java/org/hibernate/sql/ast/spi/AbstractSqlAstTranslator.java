71135cc6b4e2cd59b08e273608a8b5ba411b3897
Gavin King <gavin@hibernate.org>
2026-01-26 19:29:43 +0100

    HHH-20077 attempt to support flashback archive on Oracle

--- hibernate-core/src/main/java/org/hibernate/sql/ast/spi/AbstractSqlAstTranslator.java
+++ hibernate-core/src/main/java/org/hibernate/sql/ast/spi/AbstractSqlAstTranslator.java
@@ -6200,7 +6200,7 @@ public abstract class AbstractSqlAstTranslator<T extends JdbcOperation> implemen
 		final boolean historical = tableReference.getTemporalInstant() != null;
 		if ( shouldRenderSystemTimeClause( tableReference, historical ) ) {
 			appendSql( WHITESPACE );
-			appendSql( dialect.getAsOfOperator() );
+			appendSql( dialect.getAsOfOperator( getTemporalTableStrategy() ) );
 			appendSql( WHITESPACE );
 			if ( historical ) {
 				visitParameterAsParameter(

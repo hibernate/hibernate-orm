52a90499f07c974cf0d9c2e6f2a4dd2a4a4e0514
Koen Aers <koen.aers@gmail.com>
2026-01-26 12:26:07 +0100

    HBX-3282: Move the relevant tests in module 'test/nodb' to the 'legacyTest' source tree in the 'orm' module   - Move 'org.hibernate.tool.hbm2x.hbm2hbmxml.IdBagTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ tooling/hibernate-reveng/src/legacyTest/java/org/hibernate/tool/hbm2x/hbm2hbmxml/IdBagTest/Group.java
@@ -0,0 +1,37 @@
+/*
+ * Hibernate Tools, Tooling for your Hibernate Projects
+ *
+ * Copyright 2004-2025 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hibernate.tool.hbm2x.hbm2hbmxml.IdBagTest;
+
+public class Group {
+	private String name;
+	
+	public Group() {}
+	
+	public Group(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}

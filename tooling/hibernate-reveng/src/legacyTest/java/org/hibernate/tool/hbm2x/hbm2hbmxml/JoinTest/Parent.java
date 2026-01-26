d56752e10134690fb20f4a4072f92eba7da6216f
Koen Aers <koen.aers@gmail.com>
2026-01-26 10:43:19 +0100

    HBX-3282: Move the relevant tests in module 'test/nodb' to the 'legacyTest' source tree in the 'orm' module   - Move 'org.hibernate.tool.hbm2x.hbm2hbmxml.JoinTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ tooling/hibernate-reveng/src/legacyTest/java/org/hibernate/tool/hbm2x/hbm2hbmxml/JoinTest/Parent.java
@@ -0,0 +1,67 @@
+/*
+ * Hibernate Tools, Tooling for your Hibernate Projects
+ *
+ * Copyright 2004-2025 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.hibernate.tool.hbm2x.hbm2hbmxml.JoinTest;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+/**
+ * @author gavin
+ */
+public class Parent {
+	private Long id;
+	private String name;
+	private Collection<Child> children = new HashSet<>();
+	/**
+	 * @return Returns the children.
+	 */
+	public Collection<Child> getChildren() {
+		return children;
+	}
+	/**
+	 * @param children The children to set.
+	 */
+	public void setChildren(Collection<Child> children) {
+		this.children = children;
+	}
+	/**
+	 * @return Returns the id.
+	 */
+	public Long getId() {
+		return id;
+	}
+	/**
+	 * @param id The id to set.
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+	/**
+	 * @return Returns the name.
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name The name to set.
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+}

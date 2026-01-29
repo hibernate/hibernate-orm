fa9a96dec485d2df6a1803296947c7bbe4103faa
Marco Belladelli <marcobladel@gmail.com>
2026-01-29 15:47:35 +0100

    HHH-20113 Use `power()` instead of exponential in round emulation

--- hibernate-core/src/main/java/org/hibernate/dialect/function/PostgreSQLTruncRoundFunction.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/function/PostgreSQLTruncRoundFunction.java
@@ -79,23 +79,26 @@ public class PostgreSQLTruncRoundFunction extends AbstractSqmFunctionDescriptor 
 		}
 		else {
 			// workaround using floor
+			final SqlAstNode secondArg = arguments.get( 1 );
 			if ( getName().equals( "trunc" ) ) {
 				sqlAppender.appendSql( "sign(" );
 				firstArg.accept( walker );
 				sqlAppender.appendSql( ")*floor(abs(" );
 				firstArg.accept( walker );
-				sqlAppender.appendSql( ")*1e" );
-				arguments.get( 1 ).accept( walker );
+				sqlAppender.appendSql( ")*power(10," );
+				secondArg.accept( walker );
+				sqlAppender.appendSql( "))" );
 			}
 			else {
 				sqlAppender.appendSql( "floor(" );
 				firstArg.accept( walker );
-				sqlAppender.appendSql( "*1e" );
-				arguments.get( 1 ).accept( walker );
-				sqlAppender.appendSql( "+0.5" );
+				sqlAppender.appendSql( "*power(10," );
+				secondArg.accept( walker );
+				sqlAppender.appendSql( ")+0.5)" );
 			}
-			sqlAppender.appendSql( ")/1e" );
-			arguments.get( 1 ).accept( walker );
+			sqlAppender.appendSql( "/power(10," );
+			secondArg.accept( walker );
+			sqlAppender.appendSql( ")" );
 		}
 	}
 

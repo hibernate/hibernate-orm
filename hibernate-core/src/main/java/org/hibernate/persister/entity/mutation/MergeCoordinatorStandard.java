10a999798f124d1767cf7196a0c322189031d51e
Gavin King <gavin@hibernate.org>
2026-01-31 10:13:14 +0100

    HHH-20077 Coordinator code cleanups

--- hibernate-core/src/main/java/org/hibernate/persister/entity/mutation/MergeCoordinatorStandard.java
+++ hibernate-core/src/main/java/org/hibernate/persister/entity/mutation/MergeCoordinatorStandard.java
@@ -16,9 +16,9 @@ import org.hibernate.sql.model.ast.builder.TableMergeBuilder;
  *
  * @author Gavin King
  */
-public class MergeCoordinator extends UpdateCoordinatorStandard {
+public class MergeCoordinatorStandard extends UpdateCoordinatorStandard {
 
-	public MergeCoordinator(EntityPersister entityPersister, SessionFactoryImplementor factory) {
+	public MergeCoordinatorStandard(EntityPersister entityPersister, SessionFactoryImplementor factory) {
 		super( entityPersister, factory );
 	}
 

3f1234fde072e5a6c1dfe70c910f50629c7f3945
Gavin King <gavin@hibernate.org>
2026-02-01 19:06:44 +0100

    HHH-20077 improve jdoc

--- hibernate-core/src/main/java/org/hibernate/service/TransactionIdentifierService.java
+++ hibernate-core/src/main/java/org/hibernate/service/TransactionIdentifierService.java
@@ -11,10 +11,23 @@ import java.util.function.Supplier;
  * A source of transaction identifiers or timestamps for use
  * with {@linkplain org.hibernate.annotations.Temporal temporal}
  * or {@linkplain org.hibernate.annotations.Audited audited} data.
+ * <p>
+ * Transaction ids produced by the {@linkplain #getIdentifierSupplier
+ * supplier} must be distinct and monotonically increasing. Note,
+ * however, that unless the database is in serializable isolation
+ * mode, transactions themselves do not have a well-defined total
+ * order. Therefore, the transaction ids are ordered by the moment
+ * at which they are obtained; approximately, by the instant at
+ * which the transaction <em>started</em>. This must be taken into
+ * account when interpreting the results of historical queries
+ * against a temporal table or audit log. The historical changes
+ * cannot be linearized by transaction completion time, since the
+ * completion time of a transaction is never known when the records
+ * are written to the history table or audit log.
  *
  * @apiNote A transaction id or timestamp is assumed to be constant
  * during a transaction. This service is not aware of transaction
- * scopes, and so it is the responsibility of the client to ensure
+ * contexts, and so it is the responsibility of the client to ensure
  * that the {@linkplain #getIdentifierSupplier supplier} is called
  * no more than once in a transaction.
  *

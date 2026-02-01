1df11307335e0b54281e059b2a877117724ef30a
Gavin King <gavin@hibernate.org>
2026-02-02 00:42:16 +0100

    HHH-20077 fix NATIVE temporal table queries

--- hibernate-core/src/main/java/org/hibernate/persister/state/internal/StandardStateManagement.java
+++ hibernate-core/src/main/java/org/hibernate/persister/state/internal/StandardStateManagement.java
@@ -5,6 +5,12 @@
 package org.hibernate.persister.state.internal;
 
 import org.hibernate.Internal;
+import org.hibernate.mapping.Collection;
+import org.hibernate.mapping.RootClass;
+import org.hibernate.metamodel.mapping.AuxiliaryMapping;
+import org.hibernate.metamodel.mapping.PluralAttributeMapping;
+import org.hibernate.metamodel.mapping.internal.MappingModelCreationProcess;
+import org.hibernate.persister.entity.EntityPersister;
 
 /**
  * @author Gavin King
@@ -14,5 +20,23 @@ public final class StandardStateManagement extends AbstractStateManagement {
 	public static final StandardStateManagement INSTANCE = new StandardStateManagement();
 
 	private StandardStateManagement() {
+	}
+
+	@Override
+	public AuxiliaryMapping createAuxiliaryMapping(
+			EntityPersister persister,
+			RootClass rootClass,
+			MappingModelCreationProcess creationProcess) {
+		// for NATIVE temporal tables
+		return TemporalStateManagement.INSTANCE.createAuxiliaryMapping( persister, rootClass, creationProcess);
+	}
+
+	@Override
+	public AuxiliaryMapping createAuxiliaryMapping(
+			PluralAttributeMapping pluralAttributeMapping,
+			Collection bootDescriptor,
+			MappingModelCreationProcess creationProcess) {
+		// for NATIVE temporal tables
+		return TemporalStateManagement.INSTANCE.createAuxiliaryMapping( pluralAttributeMapping, bootDescriptor, creationProcess);
 	}
 }

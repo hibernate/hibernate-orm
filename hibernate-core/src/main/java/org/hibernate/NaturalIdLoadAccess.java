171a9bf21da2c57e09fb87c62a328a9bdfe3087c
Steve Ebersole <steve@hibernate.org>
2026-02-02 13:18:42 -0700

    HHH-20028 - Update to Jakarta Persistence 4.0 HHH-20074 - Adapt Hibernate Query contracts to new JPA 4.0 structure
    - clean up some Javadoc errors

--- hibernate-core/src/main/java/org/hibernate/NaturalIdLoadAccess.java
+++ hibernate-core/src/main/java/org/hibernate/NaturalIdLoadAccess.java
@@ -34,7 +34,7 @@ import java.util.Optional;
  * @see org.hibernate.annotations.NaturalId
  * @see SimpleNaturalIdLoadAccess
  *
- * @deprecated (since 7.3) Use {@linkplain Session#find} with {@link KeyType#NATURAL} instead.
+ * @deprecated (since 7.3) Use {@linkplain Session#find} with {@linkplain KeyType#NATURAL} instead.
  */
 @Deprecated
 public interface NaturalIdLoadAccess<T> {

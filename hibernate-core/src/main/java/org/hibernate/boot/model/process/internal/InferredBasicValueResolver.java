7abea507a7650e5fb7116be8d6c3ccd513be0c50
qlfyd123 <hajewoong@gmail.com>
2026-02-03 15:14:20 +0900

    HHH-20092 handle AdjustableJdbcType during JDBC mapping resolution

--- hibernate-core/src/main/java/org/hibernate/boot/model/process/internal/InferredBasicValueResolver.java
+++ hibernate-core/src/main/java/org/hibernate/boot/model/process/internal/InferredBasicValueResolver.java
@@ -35,6 +35,7 @@ import org.hibernate.type.descriptor.java.MutabilityPlan;
 import org.hibernate.type.descriptor.java.SerializableJavaType;
 import org.hibernate.type.descriptor.java.TemporalJavaType;
 import org.hibernate.type.descriptor.java.spi.JdbcTypeRecommendationException;
+import org.hibernate.type.descriptor.jdbc.AdjustableJdbcType;
 import org.hibernate.type.descriptor.jdbc.JdbcType;
 import org.hibernate.type.descriptor.jdbc.JdbcTypeIndicators;
 import org.hibernate.type.descriptor.jdbc.ObjectJdbcType;
@@ -134,7 +135,16 @@ public class InferredBasicValueResolver {
 			}
 			else if ( explicitJdbcType != null ) {
 				// we also have an explicit JdbcType
-				jdbcMapping = basicTypeRegistry.resolve( reflectedJtd, explicitJdbcType );
+				if ( explicitJdbcType instanceof AdjustableJdbcType ) {
+					jdbcMapping = resolveSqlTypeIndicators(
+							stdIndicators,
+							basicTypeRegistry.resolve( reflectedJtd, explicitJdbcType ),
+							reflectedJtd
+					);
+				}
+				else {
+					jdbcMapping = basicTypeRegistry.resolve( reflectedJtd, explicitJdbcType );
+				}
 			}
 			else {
 				// see if there is a registered BasicType for this JavaType and,

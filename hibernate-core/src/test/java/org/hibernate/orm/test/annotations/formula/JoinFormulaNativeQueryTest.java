baf53d4ee6e00776a8b4cab7a2939de27b8a7edd
StratosGiak <efstratios.giak@gmail.com>
2026-02-03 03:43:33 +0200

    HHH-20133 Add test case

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/annotations/formula/JoinFormulaNativeQueryTest.java
@@ -0,0 +1,153 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.orm.test.annotations.formula;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.Formula;
+import org.hibernate.annotations.JoinFormula;
+import org.hibernate.testing.orm.junit.EntityManagerFactoryScope;
+import org.hibernate.testing.orm.junit.FailureExpected;
+import org.hibernate.testing.orm.junit.Jira;
+import org.hibernate.testing.orm.junit.Jpa;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@Jpa(
+		annotatedClasses = {
+				JoinFormulaNativeQueryTest.Post.class,
+				JoinFormulaNativeQueryTest.Comment.class,
+		}
+)
+@Jira("https://hibernate.atlassian.net/browse/HHH-20133")
+public class JoinFormulaNativeQueryTest {
+
+	@BeforeAll
+	protected void setUp(EntityManagerFactoryScope scope) {
+		scope.inTransaction( entityManager -> {
+			Post post = new Post();
+			post.setId( 1L );
+			entityManager.persist( post );
+
+			Comment comment1 = new Comment();
+			comment1.setId( 1L );
+			comment1.setPost( post );
+			entityManager.persist( comment1 );
+
+			Comment comment2 = new Comment();
+			comment2.setId( 2L );
+			comment2.setPost( post );
+			entityManager.persist( comment2 );
+		} );
+	}
+
+	@Test
+	public void testHqlQueryFormula(EntityManagerFactoryScope scope) {
+		List<Comment> resultList = scope.fromTransaction( entityManager ->
+				entityManager.createQuery( "SELECT c FROM Comment c", Comment.class ).getResultList()
+		);
+		assertThat( resultList ).hasSize( 2 );
+		assertThat( resultList.get( 0 ).getAce() ).isEqualTo( 1L );
+	}
+
+	@Test
+	public void testHqlQueryJoinFormula(EntityManagerFactoryScope scope) {
+		List<Post> resultList = scope.fromTransaction( entityManager ->
+				entityManager.createQuery( "SELECT p FROM Post p", Post.class ).getResultList()
+		);
+		assertThat( resultList ).hasSize( 1 );
+		assertThat( resultList.get( 0 ).getLatestComment() ).isNotNull();
+		assertThat( resultList.get( 0 ).getLatestComment().getId() ).isEqualTo( 2L );
+	}
+
+	@Test
+	@FailureExpected
+	public void testNativeQueryFormula(EntityManagerFactoryScope scope) {
+		//noinspection unchecked
+		List<Comment> resultList = scope.fromTransaction( entityManager ->
+				entityManager.createNativeQuery( "SELECT c.* FROM comment c", Comment.class ).getResultList()
+		);
+		assertThat( resultList ).hasSize( 2 );
+		assertThat( resultList.get( 0 ).getAce() ).isEqualTo( 1L );
+	}
+
+	@Test
+	@FailureExpected
+	public void testNativeQueryJoinFormula(EntityManagerFactoryScope scope) {
+		//noinspection unchecked
+		List<Post> resultList = scope.fromTransaction( entityManager ->
+				entityManager.createNativeQuery( "SELECT p.* FROM post p", Post.class ).getResultList()
+		);
+		assertThat( resultList ).hasSize( 1 );
+		assertThat( resultList.get( 0 ).getLatestComment() ).isNotNull();
+		assertThat( resultList.get( 0 ).getLatestComment().getId() ).isEqualTo( 2L );
+	}
+
+	@Entity(name = "Post")
+	@Table(name = "post")
+	public static class Post {
+
+		@Id
+		private Long id;
+
+		@ManyToOne(fetch = FetchType.LAZY)
+		@JoinFormula("(SELECT c.id FROM comment c WHERE c.post_id = id ORDER BY c.id DESC LIMIT 1)")
+		private Comment latestComment;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public Comment getLatestComment() {
+			return latestComment;
+		}
+	}
+
+	@Entity(name = "Comment")
+	@Table(name = "comment")
+	public static class Comment {
+
+		@Id
+		private Long id;
+
+		@ManyToOne(fetch = FetchType.LAZY)
+		private Post post;
+
+		@Formula("(SELECT 1 FROM dual)")
+		private Long ace;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public Long getAce() {
+			return ace;
+		}
+
+		public Post getPost() {
+			return post;
+		}
+
+		public void setPost(Post post) {
+			this.post = post;
+		}
+	}
+
+}

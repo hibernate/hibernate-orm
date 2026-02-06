d97f999a03842a6bf7bf7ec0dac44b1049498c57
Gavin King <gavin@hibernate.org>
2026-02-06 12:44:35 +0100

    HHH-12997 further fixes

--- hibernate-core/src/test/java/org/hibernate/orm/test/mapping/joinformula/JoinFormulaUnidiTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/mapping/joinformula/JoinFormulaUnidiTest.java
@@ -7,6 +7,7 @@ package org.hibernate.orm.test.mapping.joinformula;
 import jakarta.persistence.*;
 import org.hibernate.annotations.JoinFormula;
 import org.hibernate.testing.orm.junit.EntityManagerFactoryScope;
+import org.hibernate.testing.orm.junit.JiraKey;
 import org.hibernate.testing.orm.junit.Jpa;
 import org.junit.jupiter.api.Test;
 
@@ -16,49 +17,50 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Jpa(annotatedClasses = JoinFormulaUnidiTest.Thing.class)
+@JiraKey("HHH-12997")
 class JoinFormulaUnidiTest {
 
 	@Test void test(EntityManagerFactoryScope scope) {
 		scope.inTransaction( entityManager -> {
 			Thing rootThing = new Thing();
-			rootThing.name = "0";
+			rootThing.name = "a";
 			entityManager.persist( rootThing );
 
 			Thing childThing1 = new Thing();
-			childThing1.name = "1";
-			childThing1.parentName = "0";
+			childThing1.name = "b";
+			childThing1.parentName = "A";
 			entityManager.persist( childThing1 );
 
 			Thing childThing2 = new Thing();
-			childThing2.name = "2";
-			childThing2.parentName = "0";
+			childThing2.name = "c";
+			childThing2.parentName = "A";
 			entityManager.persist( childThing2 );
 		} );
 		scope.inTransaction( entityManager -> {
-			Thing rootThing = entityManager.find( Thing.class, "0");
+			Thing rootThing = entityManager.find( Thing.class, "a");
 			assertEquals(2, rootThing.childThings.size());
 		} );
 		scope.inTransaction( entityManager -> {
 			Thing rootThing =
-					entityManager.createQuery("from Thing t join fetch childThings where t.name = '0'", Thing.class)
+					entityManager.createQuery("from Thing t join fetch childThings where t.name = 'a'", Thing.class)
 							.getSingleResult();
 			assertEquals(2, rootThing.childThings.size());
 		} );
 		scope.inTransaction( entityManager -> {
-			Thing rootThing = entityManager.find( Thing.class, "0");
-			Thing childThing = entityManager.find( Thing.class, "1");
+			Thing rootThing = entityManager.find( Thing.class, "a");
+			Thing childThing = entityManager.find( Thing.class, "b");
 			rootThing.childThings.remove( childThing );
 		} );
 		scope.inTransaction( entityManager -> {
-			Thing rootThing = entityManager.find( Thing.class, "0");
+			Thing rootThing = entityManager.find( Thing.class, "a");
 			assertEquals(2, rootThing.childThings.size());
 		} );
 		scope.inTransaction( entityManager -> {
-			Thing childThing = entityManager.find( Thing.class, "1");
+			Thing childThing = entityManager.find( Thing.class, "b");
 			childThing.parentName = null;
 		} );
 		scope.inTransaction( entityManager -> {
-			Thing rootThing = entityManager.find( Thing.class, "0");
+			Thing rootThing = entityManager.find( Thing.class, "a");
 			assertEquals(1, rootThing.childThings.size());
 		} );
 	}

a65b8c3d31478ae2b9c7d3a8a525aa2f497c7219
lvydra <lvydra@proton.me>
2026-02-11 07:26:15 +0100

    [HHH-20161] Hibernate ArrayToStringWithArrayAggregateTest tests fail with Oracle 19c

--- hibernate-core/src/test/java/org/hibernate/orm/test/function/array/ArrayToStringWithArrayAggregateTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/function/array/ArrayToStringWithArrayAggregateTest.java
@@ -74,6 +74,7 @@ public class ArrayToStringWithArrayAggregateTest {
 	}
 
 	@Test
+	@SkipForDialect(dialectClass = OracleDialect.class, majorVersion = 19, reason = "Oracle bug in version 19")
 	@SkipForDialect(dialectClass = OracleDialect.class, majorVersion = 21, reason = "Oracle bug in version 21")
 	public void testWithCte(SessionFactoryScope scope) {
 		scope.inSession( em -> {

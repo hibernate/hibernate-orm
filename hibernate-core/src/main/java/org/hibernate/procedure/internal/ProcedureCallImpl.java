30a4244ceab8de0632bd339239fe9f92087e176e
marko-bekhta <marko.prykladna@gmail.com>
2026-02-19 19:23:10 +0100

    HHH-20487 Throw IllegalStateException to match the spec requirements

--- hibernate-core/src/main/java/org/hibernate/procedure/internal/ProcedureCallImpl.java
+++ hibernate-core/src/main/java/org/hibernate/procedure/internal/ProcedureCallImpl.java
@@ -310,12 +310,12 @@ public class ProcedureCallImpl<R>
 
 	@Override
 	public ProcedureCallImplementor<R> setLockMode(LockModeType lockMode) {
-		throw new UnsupportedOperationException( "Locking not supported for ProcedureCall" );
+		throw new IllegalStateException( "Locking not supported for ProcedureCall" );
 	}
 
 	@Override
 	public ProcedureCallImplementor<R> setLockScope(PessimisticLockScope lockScope) {
-		throw new UnsupportedOperationException( "Locking not supported for ProcedureCall" );
+		throw new IllegalStateException( "Locking not supported for ProcedureCall" );
 	}
 
 	@Override

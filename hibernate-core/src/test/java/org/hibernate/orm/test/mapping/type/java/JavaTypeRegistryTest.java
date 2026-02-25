4e88bed0d98800c6fadb4f0c263c4f90762f83bd
Bartosz Herczyk <bherczyk@trans.eu>
2026-02-25 12:31:16 +0100

    HHH-20209 fix race condition in JavaTypeRegistry

--- hibernate-core/src/test/java/org/hibernate/orm/test/mapping/type/java/JavaTypeRegistryTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/mapping/type/java/JavaTypeRegistryTest.java
@@ -15,6 +15,13 @@ import org.hibernate.type.spi.TypeConfiguration;
 import org.junit.jupiter.api.Test;
 
 import java.util.Comparator;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.assertj.core.api.Assertions;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -40,7 +47,31 @@ public class JavaTypeRegistryTest {
 		assertThat( descriptor ).isInstanceOf( CustomJavaType.class );
 	}
 
-	public static class CustomType {}
+	@Test
+	void shouldReturnSameObjectWhenParallelResolveDescriptor() {
+		//given
+		final var latch = new CountDownLatch( 1 );
+		final var executorService = Executors.newFixedThreadPool( 2 );
+		final var typeConfiguration = new TypeConfiguration();
+		final var registry = new JavaTypeRegistry( typeConfiguration );
+		Callable<JavaType<CustomType>> task = () -> {
+			latch.await();
+			return registry.resolveDescriptor( CustomType.class );
+		};
+
+		//when
+		final var future1 = CompletableFuture.supplyAsync( () -> uncheck( task ), executorService );
+		final var future2 = CompletableFuture.supplyAsync( () -> uncheck( task ), executorService );
+		latch.countDown();
+
+		//then
+		final var combinedFuture = CompletableFuture.allOf( future1, future2 );
+		combinedFuture.join();
+		Assertions.assertThat( uncheck( future1 ) ).isSameAs( uncheck( future2 ) );
+	}
+
+	public static class CustomType {
+	}
 
 	public static class CustomJavaType implements JavaType<CustomType> {
 		@Override
@@ -96,6 +127,24 @@ public class JavaTypeRegistryTest {
 		@Override
 		public <X> X unwrap(CustomType value, Class<X> type, WrapperOptions options) {
 			return null;
+		}
+	}
+
+	private <T> T uncheck(Callable<T> callable) {
+		try {
+			return callable.call();
+		}
+		catch (Exception e) {
+			throw new RuntimeException( e );
+		}
+	}
+
+	private <T> T uncheck(Future<T> future) {
+		try {
+			return future.get();
+		}
+		catch (Exception e) {
+			throw new RuntimeException( e );
 		}
 	}
 }

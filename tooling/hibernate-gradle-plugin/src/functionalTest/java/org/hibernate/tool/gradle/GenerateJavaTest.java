88f3eb4f562be7728d113bd5852795cdfa2a0e19
Koen Aers <koen.aers@gmail.com>
2026-02-26 14:35:44 +0100

    HHH-19879: Move Hibernate Tools' reveng module to Hibernate ORM and merge the relevant ant/gradle/maven plugins   - Move the Gradle plugin functional tests
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ tooling/hibernate-gradle-plugin/src/functionalTest/java/org/hibernate/tool/gradle/GenerateJavaTest.java
@@ -0,0 +1,142 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.tool.gradle;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.nio.file.Files;
+
+import org.hibernate.tool.it.gradle.TestTemplate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GenerateJavaTest extends TestTemplate {
+
+	@BeforeEach
+	public void beforeEach() {
+		setGradleTaskToPerform("generateJava");
+		setDatabaseCreationScript(new String[] {
+				"create table PERSON (ID int not null, NAME varchar(20), primary key (ID))"
+		});
+	}
+
+	@Test
+	public void testJpaDefault() throws Exception {
+		createProjectAndExecuteGradleCommand();
+		File generatedOutputFolder = new File(getProjectDir(), "app/generated-sources");
+		assertTrue(generatedOutputFolder.exists());
+		assertTrue(generatedOutputFolder.isDirectory());
+		assertEquals(1, generatedOutputFolder.list().length);
+		File generatedPersonJavaFile = new File(generatedOutputFolder, "Person.java");
+		assertTrue(generatedPersonJavaFile.exists());
+		assertTrue(generatedPersonJavaFile.isFile());
+		String generatedPersonJavaFileContents = new String(
+				Files.readAllBytes(generatedPersonJavaFile.toPath()));
+		assertTrue(generatedPersonJavaFileContents.contains("import jakarta.persistence.Entity;"));
+		assertTrue(generatedPersonJavaFileContents.contains("public class Person "));
+	}
+
+	@Test
+	public void testNoAnnotations() throws Exception {
+		setHibernateToolsExtensionSection(
+				"hibernateTools { \n" +
+						"  generateAnnotations=false \n" +
+						"}"
+		);
+		createProjectAndExecuteGradleCommand();
+		File generatedOutputFolder = new File(getProjectDir(), "app/generated-sources");
+		assertTrue(generatedOutputFolder.exists());
+		assertTrue(generatedOutputFolder.isDirectory());
+		assertEquals(1, generatedOutputFolder.list().length);
+		File generatedPersonJavaFile = new File(generatedOutputFolder, "Person.java");
+		assertTrue(generatedPersonJavaFile.exists());
+		assertTrue(generatedPersonJavaFile.isFile());
+		String generatedPersonJavaFileContents = new String(
+				Files.readAllBytes(generatedPersonJavaFile.toPath()));
+		assertFalse(generatedPersonJavaFileContents.contains("import jakarta.persistence.Entity;"));
+		assertTrue(generatedPersonJavaFileContents.contains("public class Person "));
+	}
+
+	@Test
+	public void testNoGenerics() throws Exception {
+		setDatabaseCreationScript(new String[] {
+				"create table PERSON (ID int not null,  NAME varchar(20), primary key (ID))",
+				"create table ITEM (ID int not null,  NAME varchar(20), OWNER_ID int not null, " +
+						"   primary key (ID), foreign key (OWNER_ID) references PERSON(ID))"
+		});
+		setHibernateToolsExtensionSection(
+				"hibernateTools { \n" +
+						"  useGenerics=false \n" +
+						"}"
+		);
+		createProjectAndExecuteGradleCommand();
+		executeGradleCommand("generateJava");
+		File generatedOutputFolder = new File(getProjectDir(), "app/generated-sources");
+		assertTrue(generatedOutputFolder.exists());
+		assertTrue(generatedOutputFolder.isDirectory());
+		assertEquals(2, generatedOutputFolder.list().length);
+		File generatedPersonJavaFile = new File(generatedOutputFolder, "Person.java");
+		assertTrue(generatedPersonJavaFile.exists());
+		assertTrue(generatedPersonJavaFile.isFile());
+		String generatedPersonJavaFileContents = new String(
+				Files.readAllBytes(generatedPersonJavaFile.toPath()));
+		assertTrue(generatedPersonJavaFileContents.contains("public class Person "));
+		assertFalse(generatedPersonJavaFileContents.contains("Set<Item>"));
+		File generatedItemJavaFile = new File(generatedOutputFolder, "Item.java");
+		assertTrue(generatedItemJavaFile.exists());
+		assertTrue(generatedItemJavaFile.isFile());
+		String generatedItemJavaFileContents = new String(
+				Files.readAllBytes(generatedItemJavaFile.toPath()));
+		assertTrue(generatedItemJavaFileContents.contains("public class Item "));
+	}
+
+	@Test
+	public void testUseGenerics() throws Exception {
+		setDatabaseCreationScript(new String[] {
+				"create table PERSON (ID int not null,  NAME varchar(20), primary key (ID))",
+				"create table ITEM (ID int not null,  NAME varchar(20), OWNER_ID int not null, " +
+						"   primary key (ID), foreign key (OWNER_ID) references PERSON(ID))"
+		});
+		createProjectAndExecuteGradleCommand();
+		executeGradleCommand("generateJava");
+		File generatedOutputFolder = new File(getProjectDir(), "app/generated-sources");
+		assertTrue(generatedOutputFolder.exists());
+		assertTrue(generatedOutputFolder.isDirectory());
+		assertEquals(2, generatedOutputFolder.list().length);
+		File generatedPersonJavaFile = new File(generatedOutputFolder, "Person.java");
+		assertTrue(generatedPersonJavaFile.exists());
+		assertTrue(generatedPersonJavaFile.isFile());
+		String generatedPersonJavaFileContents = new String(
+				Files.readAllBytes(generatedPersonJavaFile.toPath()));
+		assertTrue(generatedPersonJavaFileContents.contains("public class Person "));
+		assertTrue(generatedPersonJavaFileContents.contains("Set<Item>"));
+		File generatedItemJavaFile = new File(generatedOutputFolder, "Item.java");
+		assertTrue(generatedItemJavaFile.exists());
+		assertTrue(generatedItemJavaFile.isFile());
+		String generatedItemJavaFileContents = new String(
+				Files.readAllBytes(generatedItemJavaFile.toPath()));
+		assertTrue(generatedItemJavaFileContents.contains("public class Item "));
+	}
+
+	@Test
+	public void testPackageName() throws Exception {
+		setHibernateToolsExtensionSection(
+				"hibernateTools { \n" +
+				"  packageName = 'foo.model' \n" +
+				"}"
+		);
+		createProjectAndExecuteGradleCommand();
+		File generatedSourcesFolder = new File(getProjectDir(), "app/generated-sources");
+		assertTrue(generatedSourcesFolder.exists());
+		assertTrue(generatedSourcesFolder.isDirectory());
+		File fooFile = new File(generatedSourcesFolder, "foo/model/Person.java");
+		assertTrue(fooFile.exists());
+		assertTrue(fooFile.isFile());
+	}
+
+}

1824b5c40814b29cd698338f69447867dea9d396
Gavin King <gavin@hibernate.org>
2026-02-26 11:33:15 +0100

    HHH-20212 add test

---
+++ tooling/metamodel-generator/src/jakartaData/java/org/hibernate/processor/test/data/hhh20212/HHH20212Entity.java
@@ -0,0 +1,14 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.processor.test.data.hhh20212;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class HHH20212Entity {
+	@Id
+	Long id;
+}

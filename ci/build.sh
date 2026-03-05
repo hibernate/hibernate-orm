943c64bf385523c63b3ec364723acbfb065081f1
marko-bekhta <marko.prykladna@gmail.com>
2026-03-06 00:47:59 +0100

    HHH-20216 Switch to `releasePrepare` from `preVerifyRelease` to check docs rendering during CI builds

--- ci/build.sh
+++ ci/build.sh
@@ -3,10 +3,10 @@
 goal=
 if [ "$RDBMS" == "h2" ] || [ "$RDBMS" == "" ]; then
   # This is the default.
-  #   - special check for Jenkins CI jobs where we don't want to run preVerifyRelease
+  #   - special check for Jenkins CI jobs where we don't want to run releasePrepare
   if [[ "$CI_SYSTEM" != "jenkins" ]]; then
-    goal="preVerifyRelease"
-    # Settings needed for `preVerifyRelease` execution - for asciidoctor doc rendering
+    goal="releasePrepare"
+    # Settings needed for `releasePrepare` execution - for asciidoctor doc rendering
     export GRADLE_OPTS=-Dorg.gradle.jvmargs='-Dlog4j2.disableJmx -Xmx4g -XX:MaxMetaspaceSize=768m -XX:+HeapDumpOnOutOfMemoryError -Duser.language=en -Duser.country=US -Duser.timezone=UTC -Dfile.encoding=UTF-8'
   fi
 elif [ "$RDBMS" == "hsqldb" ] || [ "$RDBMS" == "hsqldb_2_6" ]; then

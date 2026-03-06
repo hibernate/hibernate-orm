e3408a17cf910aa4ec32022e22b4411b9662d1a6
Gavin King <gavin@hibernate.org>
2026-03-06 18:10:22 +0100

    HHH-20242 tiny fix

--- hibernate-core/src/main/java/org/hibernate/query/hql/internal/StandardHqlTranslator.java
+++ hibernate-core/src/main/java/org/hibernate/query/hql/internal/StandardHqlTranslator.java
@@ -158,7 +158,7 @@ public class StandardHqlTranslator implements HqlTranslator {
 			}
 			else {
 				// don't use String.lines() because it strips trailing bank lines
-				final String[] lines = hql.split( "\r?\n|\r" );
+				final String[] lines = hql.split( "\r?\n|\r", -1 );
 				final String lineText = lines[lines.length - 1];
 				final String text =
 						lineText.substring( 0, charPositionInLine )

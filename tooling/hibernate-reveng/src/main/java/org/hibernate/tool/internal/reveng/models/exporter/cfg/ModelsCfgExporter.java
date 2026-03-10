005d0898c6ecced2720c2ab2211f7d45f7553389
Koen Aers <koen.aers@gmail.com>
2026-03-10 11:21:51 +0100

    HHH-20376: Replace the use of the hibernate-orm mapping API with hibernate-models   - Add ModelsCfgExporter to generate hibernate.cfg.xml from     List<TableMetadata>, mirroring the existing CfgExporter behavior     (property filtering, ejb3/resource mapping, inheritance ordering,     XML escaping)   - Move ModelsCfgExporter to the exporter.cfg subpackage   - Add tests for all new functionality
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ tooling/hibernate-reveng/src/main/java/org/hibernate/tool/internal/reveng/models/exporter/cfg/ModelsCfgExporter.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2010 - 2025 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hibernate.tool.internal.reveng.models.exporter.cfg;
+
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+import java.util.TreeMap;
+
+import org.hibernate.cfg.Environment;
+import org.hibernate.tool.internal.reveng.models.metadata.TableMetadata;
+
+/**
+ * Generates {@code hibernate.cfg.xml} from a {@code List<TableMetadata>}.
+ *
+ * @author Koen Aers
+ */
+public class ModelsCfgExporter {
+
+	private final List<TableMetadata> tables;
+
+	private ModelsCfgExporter(List<TableMetadata> tables) {
+		this.tables = tables;
+	}
+
+	public static ModelsCfgExporter create(List<TableMetadata> tables) {
+		return new ModelsCfgExporter(tables);
+	}
+
+	public void export(Writer output, Properties properties) {
+		PrintWriter pw = new PrintWriter(output);
+		boolean ejb3 = Boolean.parseBoolean((String) properties.get("ejb3"));
+		Map<Object, Object> props = new TreeMap<>(properties);
+		String sfname = (String) props.get(Environment.SESSION_FACTORY_NAME);
+		pw.println("""
+				<?xml version="1.0" encoding="UTF-8"?>
+				<!DOCTYPE hibernate-configuration PUBLIC\r
+						"-//Hibernate/Hibernate Configuration DTD 3.0//EN"\r
+						"https://www.hibernate.org/dtd/hibernate-configuration-3.0.dtd">\r
+				<hibernate-configuration>""");
+		pw.println("    <session-factory" + (sfname == null ? "" : " name=\"" + sfname + "\"") + ">");
+		Map<Object, Object> ignoredProperties = new HashMap<>();
+		ignoredProperties.put(Environment.SESSION_FACTORY_NAME, null);
+		ignoredProperties.put(Environment.HBM2DDL_AUTO, "false");
+		ignoredProperties.put("hibernate.temp.use_jdbc_metadata_defaults", null);
+		ignoredProperties.put(
+				Environment.TRANSACTION_COORDINATOR_STRATEGY,
+				"org.hibernate.console.FakeTransactionManagerLookup");
+		Set<Map.Entry<Object, Object>> set = props.entrySet();
+		for (Map.Entry<Object, Object> element : set) {
+			String key = (String) element.getKey();
+			if (ignoredProperties.containsKey(key)) {
+				Object ignoredValue = ignoredProperties.get(key);
+				if (ignoredValue == null || element.getValue().equals(ignoredValue)) {
+					continue;
+				}
+			}
+			if (key.startsWith("hibernate.")) {
+				pw.println("        <property name=\"" + key + "\">" + forXML(
+						element.getValue().toString()) + "</property>");
+			}
+		}
+		for (TableMetadata table : tables) {
+			if (table.getParentEntityClassName() == null) {
+				dump(pw, ejb3, table);
+			}
+		}
+		pw.println("    </session-factory>\r\n" +
+				"</hibernate-configuration>");
+		pw.flush();
+	}
+
+	private void dump(PrintWriter pw, boolean useClass, TableMetadata table) {
+		String qualifiedName = table.getEntityPackage() + "." + table.getEntityClassName();
+		if (useClass) {
+			pw.println("<mapping class=\"" + qualifiedName + "\"/>");
+		} else {
+			pw.println("<mapping resource=\"" + qualifiedName.replace('.', '/') + ".hbm.xml\"/>");
+		}
+		for (TableMetadata child : tables) {
+			if (table.getEntityClassName().equals(child.getParentEntityClassName())) {
+				dump(pw, useClass, child);
+			}
+		}
+	}
+
+	static String forXML(String text) {
+		if (text == null) return null;
+		final StringBuilder result = new StringBuilder();
+		char[] chars = text.toCharArray();
+		for (char character : chars) {
+			if (character == '<') {
+				result.append("&lt;");
+			} else if (character == '>') {
+				result.append("&gt;");
+			} else {
+				result.append(character);
+			}
+		}
+		return result.toString();
+	}
+
+}
---
+++ tooling/hibernate-reveng/src/test/java/org/hibernate/tool/internal/reveng/models/exporter/cfg/ModelsCfgExporterTest.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright 2010 - 2025 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hibernate.tool.internal.reveng.models.exporter.cfg;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.hibernate.cfg.Environment;
+import org.hibernate.tool.internal.reveng.models.metadata.TableMetadata;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for {@link ModelsCfgExporter}.
+ *
+ * @author Koen Aers
+ */
+public class ModelsCfgExporterTest {
+
+	@Test
+	public void testEmptySchema() {
+		ModelsCfgExporter exporter = ModelsCfgExporter.create(List.of());
+		StringWriter writer = new StringWriter();
+		exporter.export(writer, new Properties());
+		String xml = writer.toString();
+		assertTrue(xml.contains("<hibernate-configuration>"));
+		assertTrue(xml.contains("<session-factory>"));
+		assertTrue(xml.contains("</session-factory>"));
+		assertTrue(xml.contains("</hibernate-configuration>"));
+		assertFalse(xml.contains("<mapping"));
+	}
+
+	@Test
+	public void testSingleEntity() {
+		TableMetadata table = new TableMetadata("EMPLOYEE", "Employee", "com.example");
+		ModelsCfgExporter exporter = ModelsCfgExporter.create(List.of(table));
+		StringWriter writer = new StringWriter();
+		Properties props = new Properties();
+		props.put("ejb3", "true");
+		exporter.export(writer, props);
+		String xml = writer.toString();
+		assertTrue(xml.contains("<mapping class=\"com.example.Employee\"/>"));
+	}
+
+	@Test
+	public void testSingleEntityResourceMapping() {
+		TableMetadata table = new TableMetadata("EMPLOYEE", "Employee", "com.example");
+		ModelsCfgExporter exporter = ModelsCfgExporter.create(List.of(table));
+		StringWriter writer = new StringWriter();
+		exporter.export(writer, new Properties());
+		String xml = writer.toString();
+		assertTrue(xml.contains("<mapping resource=\"com/example/Employee.hbm.xml\"/>"));
+	}
+
+	@Test
+	public void testMultipleEntities() {
+		List<TableMetadata> tables = List.of(
+				new TableMetadata("EMPLOYEE", "Employee", "com.example"),
+				new TableMetadata("DEPARTMENT", "Department", "com.example"));
+		ModelsCfgExporter exporter = ModelsCfgExporter.create(tables);
+		StringWriter writer = new StringWriter();
+		Properties props = new Properties();
+		props.put("ejb3", "true");
+		exporter.export(writer, props);
+		String xml = writer.toString();
+		assertTrue(xml.contains("<mapping class=\"com.example.Employee\"/>"));
+		assertTrue(xml.contains("<mapping class=\"com.example.Department\"/>"));
+	}
+
+	@Test
+	public void testHibernateProperties() {
+		ModelsCfgExporter exporter = ModelsCfgExporter.create(List.of());
+		StringWriter writer = new StringWriter();
+		Properties props = new Properties();
+		props.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+		props.put("hibernate.show_sql", "true");
+		exporter.export(writer, props);
+		String xml = writer.toString();
+		assertTrue(xml.contains("<property name=\"hibernate.dialect\">org.hibernate.dialect.H2Dialect</property>"));
+		assertTrue(xml.contains("<property name=\"hibernate.show_sql\">true</property>"));
+	}
+
+	@Test
+	public void testIgnoredProperties() {
+		ModelsCfgExporter exporter = ModelsCfgExporter.create(List.of());
+		StringWriter writer = new StringWriter();
+		Properties props = new Properties();
+		props.put(Environment.SESSION_FACTORY_NAME, "myFactory");
+		props.put(Environment.HBM2DDL_AUTO, "false");
+		props.put("hibernate.temp.use_jdbc_metadata_defaults", "true");
+		props.put(Environment.TRANSACTION_COORDINATOR_STRATEGY,
+				"org.hibernate.console.FakeTransactionManagerLookup");
+		exporter.export(writer, props);
+		String xml = writer.toString();
+		assertFalse(xml.contains("<property name=\"" + Environment.SESSION_FACTORY_NAME + "\">"));
+		assertFalse(xml.contains("<property name=\"" + Environment.HBM2DDL_AUTO + "\">"));
+		assertFalse(xml.contains("<property name=\"hibernate.temp.use_jdbc_metadata_defaults\">"));
+		assertFalse(xml.contains("<property name=\"" + Environment.TRANSACTION_COORDINATOR_STRATEGY + "\">"));
+	}
+
+	@Test
+	public void testSessionFactoryName() {
+		ModelsCfgExporter exporter = ModelsCfgExporter.create(List.of());
+		StringWriter writer = new StringWriter();
+		Properties props = new Properties();
+		props.put(Environment.SESSION_FACTORY_NAME, "mySessionFactory");
+		exporter.export(writer, props);
+		String xml = writer.toString();
+		assertTrue(xml.contains("<session-factory name=\"mySessionFactory\">"));
+	}
+
+	@Test
+	public void testInheritanceHierarchy() {
+		TableMetadata root = new TableMetadata("VEHICLE", "Vehicle", "com.example");
+		TableMetadata child = new TableMetadata("CAR", "Car", "com.example")
+				.parent("Vehicle", "com.example");
+		List<TableMetadata> tables = new ArrayList<>();
+		tables.add(child);
+		tables.add(root);
+		ModelsCfgExporter exporter = ModelsCfgExporter.create(tables);
+		StringWriter writer = new StringWriter();
+		Properties props = new Properties();
+		props.put("ejb3", "true");
+		exporter.export(writer, props);
+		String xml = writer.toString();
+		int rootIdx = xml.indexOf("<mapping class=\"com.example.Vehicle\"/>");
+		int childIdx = xml.indexOf("<mapping class=\"com.example.Car\"/>");
+		assertTrue(rootIdx >= 0, "Root entity should be present");
+		assertTrue(childIdx >= 0, "Child entity should be present");
+		assertTrue(rootIdx < childIdx, "Root entity should appear before child entity");
+	}
+
+	@Test
+	public void testXmlEscaping() {
+		ModelsCfgExporter exporter = ModelsCfgExporter.create(List.of());
+		StringWriter writer = new StringWriter();
+		Properties props = new Properties();
+		props.put("hibernate.custom", "value <with> special chars");
+		exporter.export(writer, props);
+		String xml = writer.toString();
+		assertTrue(xml.contains("value &lt;with&gt; special chars"));
+		assertFalse(xml.contains("value <with> special chars"));
+	}
+
+}

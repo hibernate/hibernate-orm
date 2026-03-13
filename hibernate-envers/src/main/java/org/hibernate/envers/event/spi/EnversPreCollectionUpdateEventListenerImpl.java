95fb8a5806fa3ae0a8dff7e75cbb71ac057787d1
Christian Beikov <christian.beikov@gmail.com>
2026-03-13 19:34:25 +0100

    HHH-19818 Check whether to generate revision early and without CollectionEntry

--- hibernate-envers/src/main/java/org/hibernate/envers/event/spi/EnversPreCollectionUpdateEventListenerImpl.java
+++ hibernate-envers/src/main/java/org/hibernate/envers/event/spi/EnversPreCollectionUpdateEventListenerImpl.java
@@ -27,12 +27,15 @@ public class EnversPreCollectionUpdateEventListenerImpl
 
 	@Override
 	public void onPreUpdateCollection(PreCollectionUpdateEvent event) {
-		final CollectionEntry collectionEntry = getCollectionEntry( event );
-		if ( !collectionEntry.getLoadedPersister().isInverse() ) {
-			onCollectionAction( event, event.getCollection(), collectionEntry.getSnapshot(), collectionEntry );
-		}
-		else {
-			onCollectionActionInversed( event, event.getCollection(), collectionEntry.getSnapshot(), collectionEntry );
+		if ( shouldGenerateRevision( event ) ) {
+			final CollectionEntry collectionEntry = getCollectionEntry( event );
+			if ( !collectionEntry.getLoadedPersister().isInverse() ) {
+				onCollectionAction( event, event.getCollection(), collectionEntry.getSnapshot(), collectionEntry );
+			}
+			else {
+				onCollectionActionInversed( event, event.getCollection(), collectionEntry.getSnapshot(),
+						collectionEntry );
+			}
 		}
 	}
 }

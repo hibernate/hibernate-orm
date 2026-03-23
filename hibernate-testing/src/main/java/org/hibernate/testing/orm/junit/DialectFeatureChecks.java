7784979812d96768b45524bf24b7369314b23a53
Christian Beikov <christian.beikov@gmail.com>
2026-03-23 12:27:57 +0100

    HHH-20272 Make sure JDBC pre-actions actually run before the select statement

--- hibernate-testing/src/main/java/org/hibernate/testing/orm/junit/DialectFeatureChecks.java
+++ hibernate-testing/src/main/java/org/hibernate/testing/orm/junit/DialectFeatureChecks.java
@@ -298,7 +298,15 @@ abstract public class DialectFeatureChecks {
 
 	public static class SupportsLockTimeouts implements DialectFeatureCheck {
 		public boolean apply(Dialect dialect) {
-			return dialect.supportsLockTimeouts();
+			return dialect.getLockingSupport().getMetadata().getLockTimeoutType( Timeouts.ONE_SECOND ) != LockTimeoutType.NONE;
+		}
+	}
+
+	public static class SupportsNoWait implements DialectFeatureCheck {
+		public boolean apply(Dialect dialect) {
+			return dialect.getLockingSupport()
+					.getMetadata()
+					.getLockTimeoutType( Timeouts.NO_WAIT ) != LockTimeoutType.NONE;
 		}
 	}
 
@@ -332,19 +340,19 @@ abstract public class DialectFeatureChecks {
 
 	public static class SupportsSkipLocked implements DialectFeatureCheck {
 		public boolean apply(Dialect dialect) {
-			return dialect.supportsSkipLocked();
+			return dialect.getLockingSupport().getMetadata().getLockTimeoutType( Timeouts.SKIP_LOCKED ) != LockTimeoutType.NONE;
 		}
 	}
 
 	public static class SupportNoWait implements DialectFeatureCheck {
 		public boolean apply(Dialect dialect) {
-			return dialect.supportsNoWait();
+			return dialect.getLockingSupport().getMetadata().getLockTimeoutType( Timeouts.NO_WAIT ) != LockTimeoutType.NONE;
 		}
 	}
 
 	public static class SupportsWait implements DialectFeatureCheck {
 		public boolean apply(Dialect dialect) {
-			return dialect.supportsWait();
+			return dialect.getLockingSupport().getMetadata().getLockTimeoutType( Timeouts.ONE_SECOND ) != LockTimeoutType.NONE;
 		}
 	}
 

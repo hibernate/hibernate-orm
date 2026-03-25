b841c9759b9fbfe1195dd088dd9a65fa45c6b1b3
jenniferjoby <jennifer.joby@ucdconnect.ie>
2026-03-25 15:13:24 +0000

    HHH-20261 Better error message for invalid native query constructor
    Signed-off-by: jenniferjoby <jennifer.joby@ucdconnect.ie>

--- hibernate-core/src/main/java/org/hibernate/jpa/spi/NativeQueryConstructorTransformer.java
+++ hibernate-core/src/main/java/org/hibernate/jpa/spi/NativeQueryConstructorTransformer.java
@@ -4,10 +4,10 @@
  */
 package org.hibernate.jpa.spi;
 
+import java.lang.reflect.Constructor;
+
 import org.hibernate.InstantiationException;
 import org.hibernate.query.TupleTransformer;
-
-import java.lang.reflect.Constructor;
 
 /**
  * A {@link TupleTransformer} which packages each native query result in
@@ -25,6 +25,24 @@ public class NativeQueryConstructorTransformer<T> implements TupleTransformer<T>
 
 	private final Class<T> resultClass;
 	private transient Constructor<T> constructor;
+
+	public NativeQueryConstructorTransformer(Class<T> resultClass) {
+		this.resultClass = resultClass;
+	}
+
+	private static String constructorSignature(Constructor<?> ctor) {
+		StringBuilder sb = new StringBuilder();
+		sb.append( ctor.getDeclaringClass().getSimpleName() ).append( "(" );
+		Class<?>[] params = ctor.getParameterTypes();
+		for ( int i = 0; i < params.length; i++ ) {
+			if ( i > 0 ) {
+				sb.append( ", " );
+			}
+			sb.append( params[i].getSimpleName() );
+		}
+		sb.append( ")" );
+		return sb.toString();
+	}
 
 	private Constructor<T> constructor(Object[] elements) {
 		if ( constructor == null ) {
@@ -55,24 +73,50 @@ public class NativeQueryConstructorTransformer<T> implements TupleTransformer<T>
 				throw new InstantiationException( "Cannot instantiate query result type", resultClass, e );
 			}
 			if ( constructor == null ) {
-				throw new InstantiationException( "Result class must have a single constructor with exactly "
-						+ elements.length + " parameters", resultClass );
+				StringBuilder sb = new StringBuilder();
+				sb.append( "Result class" )
+						.append( " must have exactly one constructor with " )
+						.append( elements.length )
+						.append( " parameters, found: [" );
+				boolean first = true;
+				for ( Constructor<?> c : resultClass.getDeclaredConstructors() ) {
+					if ( c.getParameterCount() == elements.length ) {
+						if ( !first ) {
+							sb.append( ", " );
+						}
+						sb.append( constructorSignature( c ) );
+						first = false;
+					}
+				}
+
+				sb.append( "]" );
+
+				throw new InstantiationException( sb.toString(), resultClass );
 			}
 		}
 		return constructor;
 	}
 
-	public NativeQueryConstructorTransformer(Class<T> resultClass) {
-		this.resultClass = resultClass;
-	}
-
 	@Override
 	public T transformTuple(Object[] tuple, String[] aliases) {
+		Constructor<T> ctor = constructor( tuple );
 		try {
-			return constructor( tuple ).newInstance( tuple );
+			return ctor.newInstance( tuple );
 		}
 		catch (Exception e) {
-			throw new InstantiationException( "Cannot instantiate query result type", resultClass, e );
+			final StringBuilder sb = new StringBuilder();
+			sb.append( "Cannot instantiate query result type, expected: " ).append( constructorSignature( ctor ) )
+					.append( " but found (" );
+
+			for ( int i = 0; i < tuple.length; i++ ) {
+				final Object value = tuple[i];
+				if ( i > 0 ) {
+					sb.append( ", " );
+				}
+				sb.append( value == null ? "null" : value.getClass().getSimpleName() );
+			}
+			sb.append( ")" );
+			throw new InstantiationException( sb.toString(), resultClass, e );
 		}
 	}
 

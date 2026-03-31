b7ac3b4ee6a6f744f212870626ce7a7dc2bae8fe
Koen Aers <koen.aers@gmail.com>
2026-03-31 14:19:41 +0200

    HHH-19879: Move Hibernate Tools' reveng module to Hibernate ORM and merge the relevant ant/gradle/maven plugins   - Deprecate HBM XML generation tasks in Ant, Gradle, and Maven plugins
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- tooling/hibernate-ant/src/main/java/org/hibernate/tool/ant/Hbm2HbmXmlExporterTask.java
+++ tooling/hibernate-ant/src/main/java/org/hibernate/tool/ant/Hbm2HbmXmlExporterTask.java
@@ -8,6 +8,7 @@ import org.hibernate.tool.reveng.api.export.Exporter;
 import org.hibernate.tool.reveng.api.export.ExporterFactory;
 import org.hibernate.tool.reveng.api.export.ExporterType;
 
+@Deprecated(forRemoval = true)
 public class Hbm2HbmXmlExporterTask extends ExporterTask {
 
 	public Hbm2HbmXmlExporterTask(HibernateToolTask parent) {
@@ -15,6 +16,9 @@ public class Hbm2HbmXmlExporterTask extends ExporterTask {
 	}
 
 	protected Exporter createExporter() {
+		parent.log( "The hbm2hbmxml exporter task is deprecated and will be removed in a future version. "
+				+ "Use the hbm2java exporter task to generate annotated Java entities instead.",
+				org.apache.tools.ant.Project.MSG_WARN );
 		return ExporterFactory.createExporter(ExporterType.HBM);
 	}
 

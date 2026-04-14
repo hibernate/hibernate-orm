d19ab48cb2e95a11c9702b09de5242ef6cb8d4cf
Koen Aers <koen.aers@gmail.com>
2026-04-14 16:34:28 +0200

    HHH-20376: Replace the use of the hibernate-orm mapping API with hibernate-models   - Rewrite HbmLintTest to use new lint exporter and detectors from     models.exporter.lint package instead of old internal.export.lint   - Update SchemaAnalyzer test to import Issue, IssueCollector, and     SchemaByMetaDataDetector from new models.exporter.lint package   - Remove unused HbmExporter import from PropertiesTest
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- tooling/hibernate-reveng/src/legacyTest/java/org/hibernate/tool/hbm2x/PropertiesTest/TestCase.java
+++ tooling/hibernate-reveng/src/legacyTest/java/org/hibernate/tool/hbm2x/PropertiesTest/TestCase.java
@@ -24,7 +24,6 @@ import org.hibernate.tool.api.export.ExporterFactory;
 import org.hibernate.tool.api.export.ExporterType;
 import org.hibernate.tool.api.metadata.MetadataDescriptor;
 import org.hibernate.tool.api.export.DefaultArtifactCollector;
-import org.hibernate.tool.internal.export.hbm.HbmExporter;
 import org.hibernate.tool.test.utils.FileUtil;
 import org.hibernate.tool.test.utils.HibernateUtil;
 import org.hibernate.tool.test.utils.JUnitUtil;
--- tooling/hibernate-reveng/src/legacyTest/java/org/hibernate/tool/hbmlint/HbmLintTest/TestCase.java
+++ tooling/hibernate-reveng/src/legacyTest/java/org/hibernate/tool/hbmlint/HbmLintTest/TestCase.java
@@ -17,15 +17,24 @@
  */
 package org.hibernate.tool.hbmlint.HbmLintTest;
 
+import org.hibernate.tool.api.export.Exporter;
 import org.hibernate.tool.api.export.ExporterConstants;
+import org.hibernate.tool.api.export.ExporterFactory;
+import org.hibernate.tool.api.export.ExporterType;
 import org.hibernate.tool.api.metadata.MetadataDescriptor;
-import org.hibernate.tool.internal.export.lint.*;
+import org.hibernate.tool.internal.reveng.models.exporter.MetadataHelper;
+import org.hibernate.tool.internal.reveng.models.exporter.lint.BadCachingDetector;
+import org.hibernate.tool.internal.reveng.models.exporter.lint.HbmLintExporter;
+import org.hibernate.tool.internal.reveng.models.exporter.lint.Issue;
+import org.hibernate.tool.internal.reveng.models.exporter.lint.LintDetector;
+import org.hibernate.tool.internal.reveng.models.exporter.lint.ShadowedIdentifierDetector;
 import org.hibernate.tool.test.utils.HibernateUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -36,46 +45,43 @@ public class TestCase {
 			"IdentifierIssues.hbm.xml",
 			"BrokenLazy.hbm.xml"
 	};
-	
+
 	@TempDir
 	public File outputDir = new File("output");
 
     private MetadataDescriptor metadataDescriptor = null;
-	
+
 	@BeforeEach
 	public void setUp() {
         File resourcesDir = new File(outputDir, "resources");
 		assertTrue(resourcesDir.mkdir());
 		metadataDescriptor = HibernateUtil.initializeMetadataDescriptor(this, HBM_XML_FILES, resourcesDir);
 	}
-	
+
 	@Test
-	public void testExporter() {	
-		HbmLintExporter exporter = new HbmLintExporter();
+	public void testExporter() {
+		Exporter exporter = ExporterFactory.createExporter(ExporterType.HBM_LINT);
 		exporter.getProperties().put(ExporterConstants.METADATA_DESCRIPTOR, metadataDescriptor);
 		exporter.getProperties().put(ExporterConstants.DESTINATION_FOLDER, outputDir);
 		exporter.start();
 	}
-	
+
 	@Test
-	public void testValidateCache() {	
-		HbmLint analyzer = new HbmLint(new Detector[] { new BadCachingDetector() });		
-		analyzer.analyze(metadataDescriptor.createMetadata());
-		assertEquals(1,analyzer.getResults().size());		
+	public void testValidateCache() {
+		var entities = MetadataHelper.from(metadataDescriptor).getEntityClassDetails();
+		HbmLintExporter linter = HbmLintExporter.create(
+				entities, new LintDetector[] { new BadCachingDetector() });
+		List<Issue> results = linter.analyze();
+		assertEquals(1, results.size());
 	}
 
 	@Test
-	public void testValidateIdentifier() {		
-		HbmLint analyzer = new HbmLint(new Detector[] { new ShadowedIdentifierDetector() });		
-		analyzer.analyze(metadataDescriptor.createMetadata());
-		assertEquals(1,analyzer.getResults().size());
+	public void testValidateIdentifier() {
+		var entities = MetadataHelper.from(metadataDescriptor).getEntityClassDetails();
+		HbmLintExporter linter = HbmLintExporter.create(
+				entities, new LintDetector[] { new ShadowedIdentifierDetector() });
+		List<Issue> results = linter.analyze();
+		assertEquals(1, results.size());
 	}
-	
-	@Test
-	public void testBytecodeRestrictions() {		
-		HbmLint analyzer = new HbmLint(new Detector[] { new InstrumentationDetector() });		
-		analyzer.analyze(metadataDescriptor.createMetadata());
-		assertEquals(2,analyzer.getResults().size(), analyzer.getResults().toString());
-	}
-	
+
 }

6ddf436b3823e878af7d54ac5cc8c0f43c4c0aeb
Steve Ebersole <steve@hibernate.org>
2026-04-24 08:12:06 -0600

    HHH-20375 - Prepare for JPA 4.0 M2 development
    Address compilation errors with CriteriaBuilder, mostly regarding -
    
    * and() / or() method signatures
    * creating criteria from JPQL
    
    Signed-off-by: Steve Ebersole <steve@hibernate.org>

--- hibernate-core/src/main/java/org/hibernate/query/sqm/tree/delete/SqmDeleteStatement.java
+++ hibernate-core/src/main/java/org/hibernate/query/sqm/tree/delete/SqmDeleteStatement.java
@@ -7,11 +7,14 @@ package org.hibernate.query.sqm.tree.delete;
 import java.util.Map;
 import java.util.Set;
 
+import jakarta.persistence.criteria.BooleanExpression;
+import jakarta.persistence.criteria.CriteriaDelete;
 import org.hibernate.query.criteria.JpaCriteriaDelete;
 import org.hibernate.query.sqm.NodeBuilder;
 import org.hibernate.query.sqm.SemanticQueryWalker;
 import org.hibernate.query.sqm.SqmQuerySource;
 import org.hibernate.query.sqm.internal.SqmUtil;
+import org.hibernate.query.sqm.internal.SimpleSqmCopyContext;
 import org.hibernate.query.sqm.tree.AbstractSqmRestrictedDmlStatement;
 import org.hibernate.query.sqm.tree.SqmCopyContext;
 import org.hibernate.query.sqm.tree.SqmDeleteOrUpdateStatement;
@@ -26,6 +29,8 @@ import jakarta.persistence.criteria.Predicate;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import jakarta.persistence.criteria.Subquery;
 import jakarta.persistence.metamodel.EntityType;
+
+import static org.hibernate.query.sqm.SqmQuerySource.CRITERIA;
 
 /**
  * @author Steve Ebersole
@@ -60,6 +65,22 @@ public class SqmDeleteStatement<T>
 		super( builder, querySource, parameters, cteStatements, target );
 	}
 
+	/**
+	 * @implNote This form is used when transforming HQL to criteria.
+	 *           All it does is change the SqmQuerySource to CRITERIA
+	 *           in order to allow correct parameter handing.
+	 */
+	public SqmDeleteStatement(SqmDeleteStatement<?> original) {
+		super(
+				original.nodeBuilder(),
+				CRITERIA,
+				original.getSqmParameters(),
+				original.copyCteStatements( new SimpleSqmCopyContext() ),
+				(SqmRoot<T>) original.getTarget()
+		);
+		whereClause = original.copyWhereClause( new SimpleSqmCopyContext() );
+	}
+
 	@Override
 	public SqmDeleteStatement<T> copy(SqmCopyContext context) {
 		final var existing = context.getCopy( this );
@@ -91,6 +112,12 @@ public class SqmDeleteStatement<T>
 	@Override
 	public SqmDeleteStatement<T> where(@Nullable Expression<Boolean> restriction) {
 		setWhere( restriction );
+		return this;
+	}
+
+	@Override
+	public CriteriaDelete<T> where(BooleanExpression... restrictions) {
+		setWhere( restrictions );
 		return this;
 	}
 

7389a99de9ab3d5470292093f15bd6e85cdf452c
Gavin King <gavin@hibernate.org>
2026-05-01 15:46:09 +0200

    HHH-18210 fix broken very ancient test

--- hibernate-core/src/test/java/org/hibernate/orm/test/mapping/usertypes/TestEntity.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/mapping/usertypes/TestEntity.java
@@ -5,13 +5,13 @@
 package org.hibernate.orm.test.mapping.usertypes;
 
 public class TestEntity {
-	private int id;
+	private String id;
 	private TestEnum testEnum;
 
-	public void setId(int id) {
+	public void setId(String id) {
 		this.id = id;
 	}
-	public int getId() {
+	public String getId() {
 		return id;
 	}
 	public void setTestEnum(TestEnum testEnum) {

f7ef74a67331452efd8fcef10f38a79703e65b1f
Gavin King <gavin@hibernate.org>
2026-05-07 23:06:16 +0200

    HHH-20413 clean up FetchOptionsHelper

--- hibernate-core/src/main/java/org/hibernate/metamodel/mapping/internal/FetchOptionsHelper.java
+++ hibernate-core/src/main/java/org/hibernate/metamodel/mapping/internal/FetchOptionsHelper.java
@@ -9,9 +9,6 @@ import org.hibernate.engine.FetchStyle;
 import org.hibernate.engine.FetchTiming;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.persister.collection.AbstractCollectionPersister;
-import org.hibernate.persister.collection.CollectionPersister;
-import org.hibernate.persister.entity.EntityPersister;
-import org.hibernate.sql.results.graph.FetchOptions;
 import org.hibernate.type.AnyType;
 import org.hibernate.type.AssociationType;
 import org.hibernate.type.CollectionType;
@@ -32,24 +29,19 @@ public final class FetchOptionsHelper {
 	 *
 	 * @param mappingFetchMode The mapping defined fetch mode
 	 * @param type The association type
-	 * @param sessionFactory The session factory
+	 * @param factory The session factory
 	 *
 	 * @return the fetch style
 	 */
 	public static FetchStyle determineFetchStyleByMetadata(
 			FetchMode mappingFetchMode,
 			AssociationType type,
-			SessionFactoryImplementor sessionFactory) {
-		if ( !( type instanceof EntityType ) && !( type instanceof CollectionType ) ) {
-			return FetchStyle.SELECT;
-		}
-
+			SessionFactoryImplementor factory) {
 		if ( mappingFetchMode == FetchMode.JOIN ) {
 			return FetchStyle.JOIN;
 		}
-
-		if ( type instanceof EntityType ) {
-			final EntityPersister persister = (EntityPersister) type.getAssociatedJoinable( sessionFactory );
+		else if ( type instanceof EntityType entityType ) {
+			final var persister = entityType.getAssociatedEntityPersister( factory );
 			if ( persister.isBatchLoadable() ) {
 				return FetchStyle.BATCH;
 			}
@@ -59,9 +51,12 @@ public final class FetchOptionsHelper {
 			else if ( !persister.hasProxy() ) {
 				return FetchStyle.JOIN;
 			}
+			else {
+				return FetchStyle.SELECT;
+			}
 		}
-		else {
-			final CollectionPersister persister = (CollectionPersister) type.getAssociatedJoinable( sessionFactory );
+		else if ( type instanceof CollectionType collectionType ) {
+			final var persister = collectionType.getPersister( factory );
 			if ( persister instanceof AbstractCollectionPersister
 					&& persister.isSubselectLoadable() ) {
 				return FetchStyle.SUBSELECT;
@@ -69,25 +64,26 @@ public final class FetchOptionsHelper {
 			else if ( persister.getBatchSize() > 0 ) {
 				return FetchStyle.BATCH;
 			}
+			else {
+				return FetchStyle.SELECT;
+			}
 		}
-		return FetchStyle.SELECT;
+		else {
+			return FetchStyle.SELECT;
+		}
 	}
 
 	public static FetchTiming determineFetchTiming(
 			FetchStyle style,
 			AssociationType type,
-			SessionFactoryImplementor sessionFactory) {
-		switch ( style ) {
-			case JOIN: {
-				return FetchTiming.IMMEDIATE;
-			}
-			case BATCH:
-			case SUBSELECT:
-			default: {
-				return isSubsequentSelectDelayed( type, sessionFactory )
-						? FetchTiming.DELAYED
-						: FetchTiming.IMMEDIATE;
-			}
+			SessionFactoryImplementor factory) {
+		if ( style == FetchStyle.JOIN ) {
+			return FetchTiming.IMMEDIATE;
+		}
+		else {
+			return isSubsequentSelectDelayed( type, factory )
+					? FetchTiming.DELAYED
+					: FetchTiming.IMMEDIATE;
 		}
 	}
 
@@ -96,42 +92,40 @@ public final class FetchOptionsHelper {
 			AssociationType type,
 			boolean lazy,
 			String role,
-			SessionFactoryImplementor sessionFactory) {
-		switch ( style ) {
-			case JOIN: {
-				if ( lazy ) {
-					CORE_LOGGER.fetchModeJoinWithLazyWarning( role );
-					return FetchTiming.DELAYED;
-				}
+			SessionFactoryImplementor factory) {
+		if ( style == FetchStyle.JOIN ) {
+			if ( lazy ) {
+				CORE_LOGGER.fetchModeJoinWithLazyWarning( role );
+				return FetchTiming.DELAYED;
+			}
+			else {
 				return FetchTiming.IMMEDIATE;
 			}
-			case BATCH:
-			case SUBSELECT:
-			default: {
-				return isSubsequentSelectDelayed( type, sessionFactory )
-						? FetchTiming.DELAYED
-						: FetchTiming.IMMEDIATE;
-			}
-		}
-	}
-
-	private static boolean isSubsequentSelectDelayed(AssociationType type, SessionFactoryImplementor sessionFactory) {
-		if ( type instanceof AnyType ) {
-			// we'd need more context here.  this is only kept as part of the property state on the owning entity
-			return false;
-		}
-		else if ( type instanceof EntityType ) {
-			final var entityPersister = (EntityPersister) type.getAssociatedJoinable( sessionFactory );
-			return entityPersister.isLazy();
 		}
 		else {
-			final var collectionPersister = (CollectionPersister) type.getAssociatedJoinable( sessionFactory );
-			return collectionPersister.isLazy() || collectionPersister.isExtraLazy();
+			return isSubsequentSelectDelayed( type, factory )
+					? FetchTiming.DELAYED
+					: FetchTiming.IMMEDIATE;
 		}
 	}
 
-	public static boolean isJoinFetched(FetchOptions fetchOptions) {
-		return fetchOptions.getTiming() == FetchTiming.IMMEDIATE
-				&& fetchOptions.getStyle() == FetchStyle.JOIN;
+	private static boolean isSubsequentSelectDelayed(AssociationType type, SessionFactoryImplementor factory) {
+		if ( type instanceof AnyType ) {
+			// We'd need more context here.
+			// This is only kept as part of the
+			// property state on the owning entity
+			return false;
+		}
+		else if ( type instanceof EntityType entityType ) {
+			return entityType.getAssociatedEntityPersister( factory ).isLazy();
+		}
+		else if ( type instanceof CollectionType collectionType ) {
+			final var collectionPersister = collectionType.getPersister( factory );
+			return collectionPersister.isLazy()
+				|| collectionPersister.isExtraLazy();
+		}
+		else {
+			return false;
+		}
 	}
 }

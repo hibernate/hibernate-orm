bd91ec16d2c0a5b18d5afab5533bade5394ff7e5
Gavin King <gavin@hibernate.org>
2026-05-08 15:52:35 +0200

    add package descriptor

---
+++ hibernate-core/src/main/java/org/hibernate/annotations/schema/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Annotations allowing JPA column and table mapping annotations
+ * to be applied as meta-annotations in generated code.
+ *
+ * @since 8.0
+ */
+@Incubating
+package org.hibernate.annotations.schema;
+
+import org.hibernate.Incubating;

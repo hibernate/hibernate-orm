3aa1973fdaa53d3e2f3cd9f550221e11fbcbce59
marko-bekhta <marko.prykladna@gmail.com>
2026-05-13 19:27:09 +0200

    Include additional job to run the TCK as part of GitHub workflow

--- ci/build-tck.sh
+++ ci/build-tck.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source "$DIR/db-params.sh"
 
-logAndExec ./gradlew ciCheck ${goal} "${@}" -Plog-test-progress=true --stacktrace
+logAndExec ./gradlew :hibernate-tck-runner:test ${goal} -Pruntck=true "${@}" -Plog-test-progress=true --stacktrace

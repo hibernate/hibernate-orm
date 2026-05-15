b758767950e23f0c24d36924c7372fb46033a373
Christian Beikov <christian.beikov@gmail.com>
2026-05-15 11:19:37 +0200

    Fix upsert problems with older PostgreSQL and SpannerPG
    (cherry picked from commit e1a89bca6f7d94be088d5b06f489baefe1738c44)

--- hibernate-core/src/main/java/org/hibernate/sql/model/jdbc/OptionalTableUpdateOperation.java
+++ hibernate-core/src/main/java/org/hibernate/sql/model/jdbc/OptionalTableUpdateOperation.java
@@ -72,12 +72,20 @@ public class OptionalTableUpdateOperation implements SelfExecutingUpdateOperatio
 	private final List<JdbcValueDescriptor> jdbcValueDescriptors;
 
 	public OptionalTableUpdateOperation(
-			MutationTarget<?> mutationTarget,
+			EntityMutationTarget mutationTarget,
 			OptionalTableUpdate upsert,
 			@SuppressWarnings("unused") SessionFactoryImplementor factory) {
-		this.mutationTarget = (EntityMutationTarget) mutationTarget;
+		this( mutationTarget, upsert, upsert.getExpectation(), factory );
+	}
+
+	protected OptionalTableUpdateOperation(
+			EntityMutationTarget mutationTarget,
+			OptionalTableUpdate upsert,
+			Expectation expectation,
+			@SuppressWarnings("unused") SessionFactoryImplementor factory) {
+		this.mutationTarget = mutationTarget;
 		this.tableMapping = (EntityTableMapping) upsert.getMutatingTable().getTableMapping();
-		this.expectation = upsert.getExpectation();
+		this.expectation = expectation;
 		this.valueBindings = upsert.getValueBindings();
 		this.keyBindings = upsert.getKeyBindings();
 		this.optimisticLockBindings = upsert.getOptimisticLockBindings();

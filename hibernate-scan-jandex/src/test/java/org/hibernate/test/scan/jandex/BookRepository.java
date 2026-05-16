52c33fe9c85b1b2b90415762a8832621d29e060d
Gavin King <gavin@hibernate.org>
2026-05-16 16:54:11 +0200

    treat Jakarta Data @Repository annotation as if it were @Discoverable

---
+++ hibernate-scan-jandex/src/test/java/org/hibernate/test/scan/jandex/BookRepository.java
@@ -0,0 +1,9 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.test.scan.jandex;
+
+/// @author Steve Ebersole
+public interface BookRepository {
+}

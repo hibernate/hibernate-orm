2ebeea6114fc8db9e638505c1b44b9a2c7d12f8d
Čedomir Igaly <cedomir.igaly@zg.t-com.hr>
2026-05-17 14:22:34 +0200

    HHH-19214 Replaced array binary search with loop

--- hibernate-core/src/main/java/org/hibernate/metamodel/internal/EmbeddableHelper.java
+++ hibernate-core/src/main/java/org/hibernate/metamodel/internal/EmbeddableHelper.java
@@ -4,14 +4,19 @@
  */
 package org.hibernate.metamodel.internal;
 
-import java.util.Arrays;
 
 public class EmbeddableHelper {
 	public static int[] determineMappingIndex(String[] sortedNames, String[] names) {
 		final int[] index = new int[sortedNames.length];
 		int i = 0;
 		for ( String name : names ) {
-			final int mappingIndex = Arrays.binarySearch( sortedNames, name );
+			int mappingIndex = -1;
+			for ( int j = 0; j < sortedNames.length; j++ ) {
+				if ( name.equals( sortedNames[j] ) ) {
+					mappingIndex = j;
+					break;
+				}
+			}
 			if ( mappingIndex != -1 ) {
 				index[i++] = mappingIndex;
 			}
@@ -22,7 +27,13 @@ public class EmbeddableHelper {
 	public static boolean resolveIndex(String[] sortedComponentNames, String[] componentNames, int[] index) {
 		boolean hasGaps = false;
 		for ( int i = 0; i < componentNames.length; i++ ) {
-			final int newIndex = Arrays.binarySearch( sortedComponentNames, componentNames[i] );
+			int newIndex = -1;
+			for ( int j = 0; j < sortedComponentNames.length; j++ ) {
+				if ( componentNames[i].equals( sortedComponentNames[j] ) ) {
+					newIndex = j;
+					break;
+				}
+			}
 			index[i] = newIndex;
 			hasGaps = hasGaps || newIndex < 0;
 		}

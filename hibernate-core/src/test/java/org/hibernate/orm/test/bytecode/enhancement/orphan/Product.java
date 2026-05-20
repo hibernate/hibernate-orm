b191b24ef33fe9ca330d71bc460ed774ea06c90b
Andrea Boriero <andrea@hibernate.org>
2026-05-20 17:42:13 +0200

    HHH-19424 Continue switching tests using hbm.xml to use mapping.xml

--- hibernate-core/src/test/java/org/hibernate/orm/test/bytecode/enhancement/orphan/Product.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/bytecode/enhancement/orphan/Product.java
@@ -2,7 +2,7 @@
  * SPDX-License-Identifier: Apache-2.0
  * Copyright Red Hat Inc. and Hibernate Authors
  */
-package org.hibernate.orm.test.orphan;
+package org.hibernate.orm.test.bytecode.enhancement.orphan;
 
 import java.io.Serializable;
 import java.util.HashSet;
@@ -13,17 +13,21 @@ import java.util.Set;
  */
 public class Product implements Serializable {
 	private String name;
-	private Set parts = new HashSet();
+	private Set<Part> parts = new HashSet<>();
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Set getParts() {
+
+	public Set<Part> getParts() {
 		return parts;
 	}
-	public void setParts(Set parts) {
+
+	public void setParts(Set<Part> parts) {
 		this.parts = parts;
 	}
 }

a22d5c21ca5ff0bbea13ecca3d701930cbb45854
Christian Beikov <christian.beikov@gmail.com>
2026-05-28 14:18:21 +0200

    HHH-20499 Ensure LONG32 JdbcTypes with LOB DDL type are treated as LOBs

--- hibernate-community-dialects/src/main/java/org/hibernate/community/dialect/PostgreSQLLegacyDialect.java
+++ hibernate-community-dialects/src/main/java/org/hibernate/community/dialect/PostgreSQLLegacyDialect.java
@@ -259,6 +259,14 @@ public class PostgreSQLLegacyDialect extends Dialect {
 	}
 
 	@Override
+	protected boolean isLob(int sqlTypeCode) {
+		return switch ( sqlTypeCode ) {
+			case LONG32VARCHAR, LONG32NVARCHAR, LONG32VARBINARY -> false;
+			default -> super.isLob( sqlTypeCode );
+		};
+	}
+
+	@Override
 	protected String castType(int sqlTypeCode) {
 		return switch ( sqlTypeCode ) {
 			case CHAR, NCHAR, VARCHAR, NVARCHAR -> "varchar";

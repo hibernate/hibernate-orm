657af6d0fb4fec1f4dd007f55df360a5ca44359b
Gavin King <gavin@hibernate.org>
2026-06-05 13:39:36 +0200

    jdoc improvements to new enums

--- hibernate-core/src/main/java/org/hibernate/KeyType.java
+++ hibernate-core/src/main/java/org/hibernate/KeyType.java
@@ -6,10 +6,16 @@ package org.hibernate;
 
 import jakarta.persistence.FindOption;
 
-/// FindOption allowing to load based on either id (default) or natural-id.
+/// Specifies whether an identifier value passed to
+/// {@link jakarta.persistence.EntityHandler#get get()} or
+/// {@link jakarta.persistence.EntityHandler#find find()} should be
+/// interpreted as a [regular id][jakarta.persistence.Id] (the default)
+/// or as a [natural id][org.hibernate.annotations.NaturalId].
 ///
-/// @see jakarta.persistence.EntityManager#find
-/// @see Session#findMultiple
+/// @see jakarta.persistence.EntityHandler#find
+/// @see jakarta.persistence.EntityHandler#get
+/// @see jakarta.persistence.EntityHandler#findMultiple
+/// @see jakarta.persistence.EntityHandler#getMultiple
 /// @see Session#getReference(Class, Object, KeyType)
 ///
 /// @since 7.3
@@ -17,19 +23,20 @@ import jakarta.persistence.FindOption;
 /// @author Steve Ebersole
 /// @author Gavin King
 public enum KeyType implements FindOption {
-	/// Indicates to find by the entity's identifier.  The default.
+	/// Indicates to find by the entity's identifier. The default.
 	///
 	/// @see jakarta.persistence.Id
 	/// @see jakarta.persistence.EmbeddedId
 	/// @see jakarta.persistence.IdClass
 	IDENTIFIER,
 
-	/// Indicates to find based on the entity's natural-id, if one.
+	/// Indicates to find based on the entity's natural id, if it has one.
 	///
 	/// @see org.hibernate.annotations.NaturalId
 	/// @see org.hibernate.annotations.NaturalIdClass
+	/// @see NaturalIdSynchronization
 	///
-	/// @implSpec Will trigger an [IllegalArgumentException] if the entity does
-	/// not define a natural-id.
+	/// @implSpec Triggers an [IllegalArgumentException] if the entity
+	///           does not define a natural id.
 	NATURAL
 }

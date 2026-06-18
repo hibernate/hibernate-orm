6c7bda5361c4aeca455a2c8fe6e44e40fe0c0050
Mark Brodziak <mark.brodziak@onyxtech.com.au>
2026-06-18 08:44:41 +0800

    HHH-20586 Add XML query hints to global named query registrations

--- hibernate-core/src/main/java/org/hibernate/boot/models/internal/GlobalRegistrationsImpl.java
+++ hibernate-core/src/main/java/org/hibernate/boot/models/internal/GlobalRegistrationsImpl.java
@@ -1311,6 +1311,7 @@ public class GlobalRegistrationsImpl implements GlobalRegistrations, GlobalRegis
 			final QueryHintJpaAnnotation hint = JpaAnnotations.QUERY_HINT.createUsage( sourceModelContext );
 			hint.name( jaxbHint.getName() );
 			hint.value( jaxbHint.getValue() );
+			hints.add( hint );
 		}
 		return hints;
 	}

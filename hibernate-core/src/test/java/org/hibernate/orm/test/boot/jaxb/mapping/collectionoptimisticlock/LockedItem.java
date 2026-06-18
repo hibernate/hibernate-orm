d6d8568fbba5f62440d98586671fbe112be776cb
Andrea Boriero <andrea@hibernate.org>
2026-06-18 12:51:32 +0200

    HHH-20590 Add test for issue

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/boot/jaxb/mapping/collectionoptimisticlock/LockedItem.java
@@ -0,0 +1,35 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.orm.test.boot.jaxb.mapping.collectionoptimisticlock;
+
+public class LockedItem {
+	private Long id;
+	private String name;
+	private Owner owner;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Owner getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Owner owner) {
+		this.owner = owner;
+	}
+}

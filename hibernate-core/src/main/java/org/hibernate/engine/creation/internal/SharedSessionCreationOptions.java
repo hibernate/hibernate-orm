023f94b24e6324408f82e381d4e29729bbb75ab3
Gavin King <gavin@hibernate.org>
2026-06-23 10:13:53 +0200

    add nullability annotations to SessionCreationOptions

--- hibernate-core/src/main/java/org/hibernate/engine/creation/internal/SharedSessionCreationOptions.java
+++ hibernate-core/src/main/java/org/hibernate/engine/creation/internal/SharedSessionCreationOptions.java
@@ -4,6 +4,9 @@
  */
 package org.hibernate.engine.creation.internal;
 
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
+
 import org.hibernate.SessionEventListener;
 import org.hibernate.Transaction;
 import org.hibernate.engine.jdbc.spi.JdbcCoordinator;
@@ -12,9 +15,9 @@ import org.hibernate.engine.spi.TransactionCompletionCallbacksImplementor;
 import org.hibernate.resource.transaction.spi.TransactionCoordinator;
 
 /**
- * An extension of {@link SessionCreationOptions} for cases where the
- * session to be created shares some part of the "transaction context"
- * of another session.
+ * An extension of {@link SessionCreationOptions} for cases
+ * where the session to be created shares some part of the
+ * "transaction context" of another session.
  *
  * @author Steve Ebersole
  *
@@ -23,21 +26,55 @@ import org.hibernate.resource.transaction.spi.TransactionCoordinator;
  * @since 7.2
  */
 public interface SharedSessionCreationOptions extends SessionCreationOptions {
+	/**
+	 * Is the transaction coordinator shared with another
+	 * session?
+	 * @return {@code true} if it is shared
+	 */
 	boolean isTransactionCoordinatorShared();
+
+	/**
+	 * If the transaction coordinator is shared, return it,
+	 * otherwise, return {@code null}.
+	 */
+	@Nullable
 	TransactionCoordinator getTransactionCoordinator();
+
+	/**
+	 * If the transaction coordinator is shared, return the
+	 * {@link JdbcCoordinator}, otherwise, return {@code null}.
+	 */
+	@Nullable
 	JdbcCoordinator getJdbcCoordinator();
+
+	/**
+	 * If the transaction coordinator is shared, return the
+	 * {@link Transaction}, otherwise, return {@code null}.
+	 */
+	@Nullable
 	Transaction getTransaction();
+
+	/**
+	 * If the transaction coordinator is shared, return the
+	 * {@link TransactionCompletionCallbacksImplementor},
+	 * otherwise, return {@code null}.
+	 */
+	@Nullable
 	TransactionCompletionCallbacksImplementor getTransactionCompletionCallbacks();
 
 	/**
-	 * Registers callbacks for the child session to integrate with events of the parent session.
+	 * Registers callbacks for the child session to integrate
+	 * with events of the parent session.
 	 */
-	void registerParentSessionObserver(ParentSessionObserver observer);
+	void registerParentSessionObserver(@Nonnull ParentSessionObserver observer);
 
 	/**
-	 * Consolidated implementation of adding the parent session observer.
+	 * Consolidated implementation of adding the parent
+	 * session observer.
 	 */
-	default void registerParentSessionObserver(ParentSessionObserver observer, SharedSessionContractImplementor original) {
+	default void registerParentSessionObserver(
+			@Nonnull ParentSessionObserver observer,
+			@Nonnull SharedSessionContractImplementor original) {
 		original.getEventListenerManager().addListener( new SessionEventListener() {
 			@Override
 			public void flushEnd(int numberOfEntities, int numberOfCollections) {

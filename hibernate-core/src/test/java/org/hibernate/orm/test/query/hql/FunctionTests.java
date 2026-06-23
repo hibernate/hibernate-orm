b4f73d9ee332dcf493f0ba3c7a28c7c6f88c88a4
Christian Beikov <christian.beikov@gmail.com>
2026-06-23 14:25:09 +0200

    Fix CockroachDB nightly CI errors
    (cherry picked from commit faf7e03663134e5fb6ecec499feacd4c448ed174)

--- hibernate-core/src/test/java/org/hibernate/orm/test/query/hql/FunctionTests.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/query/hql/FunctionTests.java
@@ -643,6 +643,8 @@ public class FunctionTests {
 
 	@Test
 	@SkipForDialect(dialectClass = SpannerDialect.class, reason = "date and timestamp are not compatible")
+	@SkipForDialect(dialectClass = InformixDialect.class, reason = "Datetime truncation is not supported")
+	@SkipForDialect(dialectClass = CockroachDialect.class, reason = "unsupported binary operator: <timestamptz> - <date>")
 	public void testDateTruncWithLocalDatetimeMinusLocalDate(SessionFactoryScope scope) {
 		scope.inTransaction( session ->
 				assertThat( session.createQuery( "select trunc(local datetime, day) - local date", Duration.class )

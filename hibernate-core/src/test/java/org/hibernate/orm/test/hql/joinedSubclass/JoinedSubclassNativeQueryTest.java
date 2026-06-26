597de23abf4630aa417d5e405300bb2c87b38880
Christian Beikov <christian.beikov@gmail.com>
2026-06-26 15:09:42 +0200

    HHH-19569 Test native query alias expansion with JOINED inheritance

--- hibernate-core/src/test/java/org/hibernate/orm/test/hql/joinedSubclass/JoinedSubclassNativeQueryTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/hql/joinedSubclass/JoinedSubclassNativeQueryTest.java
@@ -7,15 +7,15 @@ package org.hibernate.orm.test.hql.joinedSubclass;
 import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.metamodel.mapping.internal.SqlTypedMappingImpl;
+
 import org.hibernate.testing.orm.junit.DomainModel;
-import org.hibernate.testing.orm.junit.JiraKey;
+import org.hibernate.testing.orm.junit.Jira;
 import org.hibernate.testing.orm.junit.SessionFactory;
 import org.hibernate.testing.orm.junit.SessionFactoryScope;
 import org.hibernate.type.spi.TypeConfiguration;
@@ -26,6 +26,8 @@ import org.junit.jupiter.api.Test;
 import java.util.Locale;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Jan Schatteman
@@ -39,7 +41,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class JoinedSubclassNativeQueryTest {
 
 	@Test
-	@JiraKey( value = "HHH-16180")
+	@Jira("https://hibernate.atlassian.net/browse/HHH-19569")
+	public void testJoinedInheritanceAliasInjection(SessionFactoryScope scope) {
+		scope.inTransaction(
+				session -> {
+					Person p = session.createNativeQuery( "select {p.*} from Person p left join Employee p_1_ on p.id=p_1_.id where p.id = 2", Person.class )
+							.addEntity( "p", Person.class )
+							.getSingleResult();
+					assertNotNull( p );
+					assertInstanceOf( Employee.class, p );
+					assertEquals( "Christian", p.getFirstName() );
+					assertEquals( "CommonHaus", ((Employee) p).getCompanyName() );
+				}
+		);
+	}
+
+	@Test
+	@Jira("https://hibernate.atlassian.net/browse/HHH-16180")
 	public void testJoinedInheritanceNativeQuery(SessionFactoryScope scope) {
 		final SessionFactoryImplementor sessionFactory = scope.getSessionFactory();
 		final TypeConfiguration typeConfiguration = sessionFactory.getTypeConfiguration();
@@ -53,7 +71,7 @@ public class JoinedSubclassNativeQueryTest {
 
 		final String qry = String.format(
 				Locale.ROOT,
-				"select p.*, %s as company_name, 0 as clazz_  from Person p",
+				"select p.*, %s as company_name, 0 as clazz_ from Person p where p.id = 1",
 				nullColumnString
 		);
 
@@ -63,18 +81,26 @@ public class JoinedSubclassNativeQueryTest {
 			session.setProperty( AvailableSettings.NATIVE_IGNORE_JDBC_PARAMETERS, true );
 
 			Person p = session.createNativeQuery( qry, Person.class ).getSingleResult();
-			assertThat( p ).isNotNull();
-			assertThat( p.getFirstName() ).isEqualTo( "Jan" );
+					assertThat( p ).isNotNull();
+					assertThat( p.getFirstName() ).isEqualTo( "Jan" );
 		} );
 	}
 
 	@BeforeEach
 	public void setup(SessionFactoryScope scope) {
-		scope.inTransaction( (session) -> {
-			Person p = new Person();
-			p.setFirstName( "Jan" );
-			session.persist( p );
-		} );
+		scope.inTransaction(
+				session -> {
+					Person p = new Person();
+					p.setId( 1L );
+					p.setFirstName( "Jan" );
+					session.persist( p );
+					Employee p2 = new Employee();
+					p2.setId( 2L );
+					p2.setFirstName( "Christian" );
+					p2.setCompanyName( "CommonHaus" );
+					session.persist( p2 );
+				}
+		);
 	}
 
 	@AfterEach
@@ -86,12 +112,19 @@ public class JoinedSubclassNativeQueryTest {
 	@Inheritance(strategy = InheritanceType.JOINED)
 	public static class Person {
 		@Id
-		@GeneratedValue
 		private Long id;
 
 		@Basic(optional = false)
 		@Column(name = "first_name")
 		private String firstName;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
 
 		public String getFirstName() {
 			return firstName;

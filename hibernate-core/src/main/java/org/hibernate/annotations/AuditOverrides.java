684c3dbb620009472158e4d2b025be473f93c8f0
Niklas Enns <niklas.enns@openvalue.de>
2026-07-06 13:21:44 +0200

    HHH-20641 - implemented @AuditOverrides and @AuditOverride

---
+++ hibernate-core/src/main/java/org/hibernate/annotations/AuditOverrides.java
@@ -0,0 +1,37 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * The {@code AuditingOverrides} annotation is used to override the auditing
+ * behavior for one or more fields (or properties) inside an embedded
+ * component.
+ *
+ * @author Erik-Berndt Scheper
+ * @author Niklas Enns
+ * @see jakarta.persistence.Embedded
+ * @see jakarta.persistence.Embeddable
+ * @see jakarta.persistence.MappedSuperclass
+ * @see jakarta.persistence.AssociationOverride
+ * @see jakarta.persistence.AssociationOverrides
+ * @see AuditOverride
+ */
+@Target({TYPE, METHOD, FIELD})
+@Retention(RUNTIME)
+public @interface AuditOverrides {
+	/**
+	 * An array of {@link AuditOverride} values, to define the new auditing
+	 * behavior.
+	 */
+	AuditOverride[] value();
+}

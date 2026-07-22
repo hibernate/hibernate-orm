5f812385a669b6bdd918f4baebc2b1068d45fe08
marko-bekhta <marko.prykladna@gmail.com>
2026-07-22 15:22:09 +0200

    Do not run release prepare for the TCK runs (even on h2)

--- ci/db-params.sh
+++ ci/db-params.sh
@@ -4,7 +4,7 @@ goal=
 if [ "$RDBMS" == "h2" ] || [ "$RDBMS" == "" ]; then
   # This is the default.
   #   - special check for Jenkins CI jobs where we don't want to run releasePrepare
-  if [[ "$CI_SYSTEM" != "jenkins" ]]; then
+  if [[ "$CI_SYSTEM" != "jenkins" ]] && [[ "$TCK_RUN" != "true" ]]; then
     goal="releasePrepare"
     # Settings needed for `releasePrepare` execution - for asciidoctor doc rendering
     export GRADLE_OPTS=-Dorg.gradle.jvmargs='-Dlog4j2.disableJmx -Xmx4g -XX:MaxMetaspaceSize=768m -XX:+HeapDumpOnOutOfMemoryError -Duser.language=en -Duser.country=US -Duser.timezone=UTC -Dfile.encoding=UTF-8'

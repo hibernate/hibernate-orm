9d90a99a33a1c03f75b16ba61f70a38a2fff997e
marko-bekhta <marko.prykladna@gmail.com>
2026-07-29 18:05:20 +0200

    HHH-20815 Wire the PropertyAccessorService through the PropertyAccessStrategy#buildPropertyAccess
    Co-authored-by: Andrea Boriero <andrea@hibernate.org>
    
    # Conflicts:
    #	hibernate-core/src/main/java/org/hibernate/metamodel/mapping/internal/VirtualEmbeddedAttributeMapping.java
    
    # Conflicts:
    #	hibernate-core/src/main/java/org/hibernate/property/access/spi/PropertyAccessStrategy.java

--- hibernate-core/src/test/java/org/hibernate/orm/test/bytecode/BeanReflectionHelper.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/bytecode/BeanReflectionHelper.java
@@ -5,9 +5,11 @@
 package org.hibernate.orm.test.bytecode;
 import java.util.Date;
 
+import org.hibernate.models.accessor.HibernateAccessorFactory;
 import org.hibernate.property.access.internal.PropertyAccessStrategyBasicImpl;
 import org.hibernate.property.access.spi.Getter;
 import org.hibernate.property.access.spi.PropertyAccess;
+import org.hibernate.property.access.spi.PropertyAccessorService;
 import org.hibernate.property.access.spi.Setter;
 
 /**
@@ -26,49 +28,49 @@ public class BeanReflectionHelper {
 	static {
 		final PropertyAccessStrategyBasicImpl propertyAccessStrategy = new PropertyAccessStrategyBasicImpl();
 
-		PropertyAccess propertyAccess = propertyAccessStrategy.buildPropertyAccess( Bean.class, "someString", true );
+		PropertyAccess propertyAccess = propertyAccessStrategy.buildPropertyAccess( TestPropertyAccessorService.INSTANCE, Bean.class, "someString", true );
 		Getter getter = propertyAccess.getGetter();
 		Setter setter = propertyAccess.getSetter();
 		getterNames[0] = getter.getMethodName();
 		types[0] = getter.getReturnTypeClass();
 		setterNames[0] = setter.getMethodName();
 
-		propertyAccess = propertyAccessStrategy.buildPropertyAccess( Bean.class, "someLong", true );
+		propertyAccess = propertyAccessStrategy.buildPropertyAccess( TestPropertyAccessorService.INSTANCE, Bean.class, "someLong", true );
 		getter = propertyAccess.getGetter();
 		setter = propertyAccess.getSetter();
 		getterNames[1] = getter.getMethodName();
 		types[1] = getter.getReturnTypeClass();
 		setterNames[1] = setter.getMethodName();
 
-		propertyAccess = propertyAccessStrategy.buildPropertyAccess( Bean.class, "someInteger", true );
+		propertyAccess = propertyAccessStrategy.buildPropertyAccess( TestPropertyAccessorService.INSTANCE, Bean.class, "someInteger", true );
 		getter = propertyAccess.getGetter();
 		setter = propertyAccess.getSetter();
 		getterNames[2] = getter.getMethodName();
 		types[2] = getter.getReturnTypeClass();
 		setterNames[2] = setter.getMethodName();
 
-		propertyAccess = propertyAccessStrategy.buildPropertyAccess( Bean.class, "someDate", true );
+		propertyAccess = propertyAccessStrategy.buildPropertyAccess( TestPropertyAccessorService.INSTANCE, Bean.class, "someDate", true );
 		getter = propertyAccess.getGetter();
 		setter = propertyAccess.getSetter();
 		getterNames[3] = getter.getMethodName();
 		types[3] = getter.getReturnTypeClass();
 		setterNames[3] = setter.getMethodName();
 
-		propertyAccess = propertyAccessStrategy.buildPropertyAccess( Bean.class, "somelong", true );
+		propertyAccess = propertyAccessStrategy.buildPropertyAccess( TestPropertyAccessorService.INSTANCE, Bean.class, "somelong", true );
 		getter = propertyAccess.getGetter();
 		setter = propertyAccess.getSetter();
 		getterNames[4] = getter.getMethodName();
 		types[4] = getter.getReturnTypeClass();
 		setterNames[4] = setter.getMethodName();
 
-		propertyAccess = propertyAccessStrategy.buildPropertyAccess( Bean.class, "someint", true );
+		propertyAccess = propertyAccessStrategy.buildPropertyAccess( TestPropertyAccessorService.INSTANCE, Bean.class, "someint", true );
 		getter = propertyAccess.getGetter();
 		setter = propertyAccess.getSetter();
 		getterNames[5] = getter.getMethodName();
 		types[5] = getter.getReturnTypeClass();
 		setterNames[5] = setter.getMethodName();
 
-		propertyAccess = propertyAccessStrategy.buildPropertyAccess( Bean.class, "someObject", true );
+		propertyAccess = propertyAccessStrategy.buildPropertyAccess( TestPropertyAccessorService.INSTANCE, Bean.class, "someObject", true );
 		getter = propertyAccess.getGetter();
 		setter = propertyAccess.getSetter();
 		getterNames[6] = getter.getMethodName();
@@ -86,5 +88,14 @@ public class BeanReflectionHelper {
 
 	public static Class[] getTypes() {
 		return types;
+	}
+
+	private static class TestPropertyAccessorService implements PropertyAccessorService {
+		static final PropertyAccessorService INSTANCE = new TestPropertyAccessorService();
+
+		@Override
+		public HibernateAccessorFactory hibernateAccessorFactory() {
+			return HibernateAccessorFactory.reflection();
+		}
 	}
 }

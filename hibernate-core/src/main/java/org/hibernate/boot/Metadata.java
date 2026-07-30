d02fac6b4436351ce66dc152efdfb28564d6feed
Steve Ebersole <steve@hibernate.org>
2026-07-30 12:59:59 -0600

    Deprecations for 8.x identified by 9.0 development
    HHH-20739 - Deprecate IdentifierGeneratorDefinition and friends
    
    Signed-off-by: Steve Ebersole <steve@hibernate.org>

--- hibernate-core/src/main/java/org/hibernate/boot/Metadata.java
+++ hibernate-core/src/main/java/org/hibernate/boot/Metadata.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.function.Consumer;
 
+import org.hibernate.Remove;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.model.IdentifierGeneratorDefinition;
 import org.hibernate.boot.model.NamedEntityGraphDefinition;
@@ -191,6 +192,7 @@ public interface Metadata extends MappingContext {
 
 	Map<String, NamedEntityGraphDefinition> getNamedEntityGraphs();
 
+	@Remove
 	IdentifierGeneratorDefinition getIdentifierGenerator(String name);
 
 	java.util.Collection<Table> collectTableMappings();

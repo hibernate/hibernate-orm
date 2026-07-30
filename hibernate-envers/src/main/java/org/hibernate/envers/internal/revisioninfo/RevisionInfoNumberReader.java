c21d15be81e12577502dd48f97799438db191797
marko-bekhta <marko.prykladna@gmail.com>
2026-07-30 17:26:25 +0200

    HHH-20815 Move Envers to use PropertyValueAccessor instead of Getter/Setter
    Co-authored-by: Andrea Boriero <andrea@hibernate.org>
    
    # Conflicts:
    #	hibernate-envers/src/main/java/org/hibernate/envers/internal/entities/mapper/AbstractMapper.java
    #	hibernate-envers/src/main/java/org/hibernate/envers/internal/entities/mapper/MultiPropertyMapper.java

--- hibernate-envers/src/main/java/org/hibernate/envers/internal/revisioninfo/RevisionInfoNumberReader.java
+++ hibernate-envers/src/main/java/org/hibernate/envers/internal/revisioninfo/RevisionInfoNumberReader.java
@@ -6,7 +6,7 @@ package org.hibernate.envers.internal.revisioninfo;
 
 import org.hibernate.envers.internal.entities.PropertyData;
 import org.hibernate.envers.internal.tools.ReflectionTools;
-import org.hibernate.property.access.spi.Getter;
+import org.hibernate.property.access.spi.PropertyValueAccessor;
 import org.hibernate.service.ServiceRegistry;
 
 /**
@@ -15,10 +15,10 @@ import org.hibernate.service.ServiceRegistry;
  * @author Adam Warski (adam at warski dot org)
  */
 public class RevisionInfoNumberReader {
-	private final Getter revisionIdGetter;
+	private final PropertyValueAccessor revisionIdGetter;
 
 	public RevisionInfoNumberReader(Class<?> revisionInfoClass, PropertyData revisionInfoIdData, ServiceRegistry serviceRegistry, RevisionInfoGenerator generator) {
-		revisionIdGetter = ReflectionTools.getGetter( revisionInfoClass, revisionInfoIdData, serviceRegistry );
+		revisionIdGetter = ReflectionTools.getPropertyValueAccessor( revisionInfoClass, revisionInfoIdData, serviceRegistry );
 		generator.setRevisionInfoNumberReader( this );
 	}
 

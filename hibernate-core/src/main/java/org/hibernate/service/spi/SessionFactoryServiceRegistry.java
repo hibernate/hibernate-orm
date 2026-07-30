b1e06aeab4de3b7633439b5fb833bd30f5fda8cb
Steve Ebersole <steve@hibernate.org>
2026-07-30 12:42:16 -0600

    Deprecations for 8.x identified by 9.0 development
    HHH-20734 - Deprecate SessionFactoryServiceRegistry, SessionFactoryServiceContributor and friends
    
    Signed-off-by: Steve Ebersole <steve@hibernate.org>

--- hibernate-core/src/main/java/org/hibernate/service/spi/SessionFactoryServiceRegistry.java
+++ hibernate-core/src/main/java/org/hibernate/service/spi/SessionFactoryServiceRegistry.java
@@ -4,12 +4,17 @@
  */
 package org.hibernate.service.spi;
 
+import org.hibernate.Remove;
+
 /**
  * Specialized {@link org.hibernate.service.ServiceRegistry} implementation that
  * holds services which need access to the {@link org.hibernate.SessionFactory}
  * during initialization.
  *
  * @author Steve Ebersole
+ *
+ * @apiNote This contract will be removed in 9.0.
  */
+@Remove
 public interface SessionFactoryServiceRegistry extends ServiceRegistryImplementor {
 }

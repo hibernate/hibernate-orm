18da8e0b1770eff9ccf7b6f15ef63075b8ea4a1d
marko-bekhta <marko.prykladna@gmail.com>
2026-07-30 17:26:25 +0200

    HHH-20815 Move Envers to use PropertyValueAccessor instead of Getter/Setter
    Co-authored-by: Andrea Boriero <andrea@hibernate.org>
    
    # Conflicts:
    #	hibernate-envers/src/main/java/org/hibernate/envers/internal/entities/mapper/AbstractMapper.java
    #	hibernate-envers/src/main/java/org/hibernate/envers/internal/entities/mapper/MultiPropertyMapper.java

--- hibernate-envers/src/main/java/org/hibernate/envers/strategy/spi/AuditStrategyContext.java
+++ hibernate-envers/src/main/java/org/hibernate/envers/strategy/spi/AuditStrategyContext.java
@@ -5,7 +5,7 @@
 package org.hibernate.envers.strategy.spi;
 
 import org.hibernate.Incubating;
-import org.hibernate.property.access.spi.Getter;
+import org.hibernate.property.access.spi.PropertyValueAccessor;
 
 /**
  * A contract to initialize an {@link AuditStrategy}
@@ -23,9 +23,6 @@ public interface AuditStrategyContext {
 	/**
 	 * Get the revision info timestamp accessor
 	 * @return the getter for the timestamp attribute on the revision entity
-	 *
-	 * @deprecated no longer used, it will be replaced by a method that will not return the deprecated {@link Getter} interface
 	 */
-	@Deprecated(since = "7.4", forRemoval = true)
-	Getter getRevisionInfoTimestampAccessor();
+	PropertyValueAccessor getRevisionInfoTimestampAccessor();
 }

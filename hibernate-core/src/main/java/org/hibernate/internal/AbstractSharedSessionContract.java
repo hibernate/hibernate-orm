c46867c31d0af71cace51a03b4086bd228a1d4fe
Gavin King <gavin@hibernate.org>
2026-09-01 11:08:04 +0200

    HHH-20829 JPA 4 requires an IAE instead of QueryTypeMismatchException

--- hibernate-core/src/main/java/org/hibernate/internal/AbstractSharedSessionContract.java
+++ hibernate-core/src/main/java/org/hibernate/internal/AbstractSharedSessionContract.java
@@ -1710,7 +1710,7 @@ abstract class AbstractSharedSessionContract
 		}
 		catch (IllegalSelectQueryException e) {
 			// JPA requires IllegalArgumentException
-			var iae = new IllegalArgumentException( e.getMessage() );
+			final var iae = new IllegalArgumentException( e.getMessage() );
 			iae.addSuppressed( e );
 			throw iae;
 		}
@@ -1730,7 +1730,7 @@ abstract class AbstractSharedSessionContract
 		}
 		catch ( RuntimeException e ) {
 			markForRollbackOnly();
-			throw e;
+			throw getExceptionConverter().convert( e );
 		}
 	}
 
@@ -1747,7 +1747,6 @@ abstract class AbstractSharedSessionContract
 		}
 		selectionQuery.setComment( hql );
 		applyQuerySettingsAndHints( selectionQuery );
-
 		return selectionQuery;
 	}
 

0f80ebf12b88d3d405f9aacbc3840ae992094748
Niklas Enns <niklas.enns@openvalue.de>
2026-09-03 12:21:00 +0200

    HHH-20641 - making also the persister and loader respect the audit overrides

--- hibernate-core/src/main/java/org/hibernate/persister/entity/AbstractEntityPersister.java
+++ hibernate-core/src/main/java/org/hibernate/persister/entity/AbstractEntityPersister.java
@@ -256,6 +256,7 @@ import static java.util.Collections.emptyMap;
 import static java.util.Collections.emptySet;
 import static java.util.Collections.unmodifiableList;
 import static java.util.function.Function.identity;
+import static org.hibernate.boot.model.internal.AuditHelper.getOverridesMap;
 import static org.hibernate.engine.internal.CacheHelper.fromSharedCache;
 import static org.hibernate.engine.internal.CacheHelper.readingFromCache;
 import static org.hibernate.engine.internal.ManagedTypeHelper.asPersistentAttributeInterceptable;
@@ -616,6 +617,8 @@ public abstract class AbstractEntityPersister
 
 		final var propertyClosure = persistentClass.getPropertyClosure();
 		boolean foundFormula = false;
+		var auditOverrides = getOverridesMap( persistentClass,
+				creationContext.getBootstrapContext().getModelsContext() );
 		for ( int i = 0; i < propertyClosure.size(); i++ ) {
 			final var property = propertyClosure.get(i);
 			thisClassProperties.add( property );
@@ -624,7 +627,15 @@ public abstract class AbstractEntityPersister
 			final boolean temporalExcluded = property.isTemporalExcluded();
 			propertyTemporalExcluded[i] = temporalExcluded;
 			foundTemporalExcluded = foundTemporalExcluded || temporalExcluded;
-			propertyAuditedExcluded[i] = property.isAuditedExcluded();
+
+			var overrideForProperty = auditOverrides.get( property.getName() );
+			if ( overrideForProperty != null ) {
+				propertyAuditedExcluded[i] = !overrideForProperty.isAudited();
+			}
+			else {
+				propertyAuditedExcluded[i] = property.isAuditedExcluded();
+			}
+
 			foundNonExcludedCollection = foundNonExcludedCollection
 					|| propertyValue instanceof org.hibernate.mapping.Collection
 							&& !temporalExcluded;

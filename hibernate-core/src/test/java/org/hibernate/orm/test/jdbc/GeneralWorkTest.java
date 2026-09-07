430ff87e266707139f73a92355783c2651619030
Andrea Boriero <andrea@hibernate.org>
2026-09-07 11:33:30 +0200

    HHH-20589 Continue switching tests using hbm.xml to use mapping.xml

--- hibernate-core/src/test/java/org/hibernate/orm/test/jdbc/GeneralWorkTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/jdbc/GeneralWorkTest.java
@@ -24,7 +24,7 @@ import static org.junit.jupiter.api.Assertions.fail;
  * @author Steve Ebersole
  */
 @SuppressWarnings("JUnitMalformedDeclaration")
-@DomainModel(annotatedClasses = {Person.class, Boat.class})
+@DomainModel(xmlMappings = "org/hibernate/orm/test/jdbc/Mappings.orm.xml")
 @SessionFactory
 public class GeneralWorkTest {
 	@AfterEach

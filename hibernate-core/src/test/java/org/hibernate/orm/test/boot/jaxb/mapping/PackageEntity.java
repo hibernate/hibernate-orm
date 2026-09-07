4e0f2fa9ae6a6d68fdbc5580acaeb0b85bb8e002
Andrea Boriero <andrea@hibernate.org>
2026-09-07 12:45:04 +0200

    HHH-20845 Add test for issue

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/boot/jaxb/mapping/PackageEntity.java
@@ -0,0 +1,29 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.orm.test.boot.jaxb.mapping;
+
+/**
+ * Test entity for named query transformation tests
+ */
+public class PackageEntity {
+	private Integer id;
+	private String name;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}

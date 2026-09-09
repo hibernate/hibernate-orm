c6bd309cd9bfd284ca874625cfb2def16a392ee5
Srltas <kdm97912@gmail.com>
2026-09-09 21:52:25 +0900

    HHH-20784 Improve CUBRIDDialect SQL rendering and type mappings

--- hibernate-community-dialects/src/main/java/org/hibernate/community/dialect/CUBRIDSqlAstTranslator.java
+++ hibernate-community-dialects/src/main/java/org/hibernate/community/dialect/CUBRIDSqlAstTranslator.java
@@ -6,17 +6,28 @@ package org.hibernate.community.dialect;
 
 import java.util.List;
 
-import org.hibernate.query.sqm.ComparisonOperator;
-import org.hibernate.dialect.sql.ast.spi.PaginationRenderingSupport;
-import org.hibernate.dialect.sql.ast.spi.StandardPaginationRenderingSupport;
 import org.hibernate.dialect.sql.ast.spi.AbstractSqlAstTranslator;
-import org.hibernate.sql.ast.spi.query.select.SqlSelection;
-import org.hibernate.sql.ast.spi.Statement;
+import org.hibernate.dialect.sql.ast.spi.DerivedTableRenderingSupport;
+import org.hibernate.dialect.sql.ast.spi.PaginationRenderingSupport;
 import org.hibernate.dialect.sql.ast.spi.SqlAstTranslationRequest;
+import org.hibernate.dialect.sql.ast.spi.StandardDerivedTableRenderingSupport;
+import org.hibernate.dialect.sql.ast.spi.StandardPaginationRenderingSupport;
+import org.hibernate.query.sqm.ComparisonOperator;
+import org.hibernate.sql.ast.spi.Statement;
+import org.hibernate.sql.ast.spi.query.delete.DeleteStatement;
+import org.hibernate.sql.ast.spi.query.expression.ColumnReference;
 import org.hibernate.sql.ast.spi.query.expression.Expression;
 import org.hibernate.sql.ast.spi.query.expression.Literal;
 import org.hibernate.sql.ast.spi.query.expression.SqlTuple;
 import org.hibernate.sql.ast.spi.query.expression.Summarization;
+import org.hibernate.sql.ast.spi.query.from.NamedTableReference;
+import org.hibernate.sql.ast.spi.query.insert.InsertSelectStatement;
+import org.hibernate.sql.ast.spi.query.predicate.InListPredicate;
+import org.hibernate.sql.ast.spi.query.predicate.InSubQueryPredicate;
+import org.hibernate.sql.ast.spi.query.select.SelectStatement;
+import org.hibernate.sql.ast.spi.query.select.SqlSelection;
+import org.hibernate.sql.ast.spi.query.update.UpdateStatement;
+import org.hibernate.sql.ast.spi.translation.Clause;
 import org.hibernate.sql.exec.spi.JdbcOperation;
 
 /**
@@ -36,8 +47,16 @@ public class CUBRIDSqlAstTranslator<T extends JdbcOperation> extends AbstractSql
 	}
 
 	@Override
+	protected DerivedTableRenderingSupport getDerivedTableRenderingSupport() {
+		//CUBRID accepts a correlated derived table but not the 'lateral' keyword, and this is the
+		//only standard profile that renders every lateral reference implicitly
+		return StandardDerivedTableRenderingSupport.SQL_SERVER;
+	}
+
+	@Override
 	protected void renderComparison(Expression lhs, ComparisonOperator operator, Expression rhs) {
-		renderComparisonEmulateIntersect( lhs, operator, rhs );
+		//CUBRID has the null-safe '<=>' operator, so distinct-from needs no intersect emulation
+		renderComparisonDistinctOperator( lhs, operator, rhs );
 	}
 
 	@Override
@@ -61,6 +80,73 @@ public class CUBRIDSqlAstTranslator<T extends JdbcOperation> extends AbstractSql
 		}
 		else {
 			expression.accept( this );
+		}
+	}
+
+	@Override
+	protected void renderDeleteClause(DeleteStatement statement) {
+		appendSql( "delete" );
+		final var clauseStack = getClauseStack();
+		try {
+			clauseStack.push( Clause.DELETE );
+			renderTableReferenceIdentificationVariable( statement.getTargetTable() );
+			if ( statement.getFromClause().getRoots().isEmpty() ) {
+				appendSql( " from " );
+				renderDmlTargetTableExpression( statement.getTargetTable() );
+			}
+			else {
+				visitFromClause( statement.getFromClause() );
+			}
+		}
+		finally {
+			clauseStack.pop();
+		}
+	}
+
+	@Override
+	protected void renderUpdateClause(UpdateStatement updateStatement) {
+		if ( updateStatement.getFromClause().getRoots().isEmpty() ) {
+			super.renderUpdateClause( updateStatement );
+		}
+		else {
+			appendSql( "update " );
+			renderFromClauseSpaces( updateStatement.getFromClause() );
+		}
+	}
+
+	@Override
+	protected void renderAssignmentColumn(ColumnReference column) {
+		column.appendColumnForWrite(
+				this,
+				getAffectedTableNames().size() > 1 && !( getStatement() instanceof InsertSelectStatement )
+						? determineColumnReferenceQualifier( column )
+						: null
+		);
+	}
+
+	@Override
+	protected void renderDmlTargetTableExpression(NamedTableReference tableReference) {
+		super.renderDmlTargetTableExpression( tableReference );
+		if ( getClauseStack().getCurrent() != Clause.INSERT ) {
+			renderTableReferenceIdentificationVariable( tableReference );
+		}
+	}
+
+	@Override
+	public void visitInListPredicate(InListPredicate inListPredicate) {
+		final List<Expression> listExpressions = inListPredicate.getListExpressions();
+		//CUBRID reads 'x in ((select ...))' as a list holding one scalar subquery and fails as soon as
+		//the subquery returns more than one row, so render it as a plain in-subquery
+		if ( listExpressions.size() == 1 && listExpressions.get( 0 ) instanceof SelectStatement subQuery ) {
+			visitInSubQueryPredicate( new InSubQueryPredicate(
+					inListPredicate.getTestExpression(),
+					subQuery,
+					inListPredicate.isNegated(),
+					inListPredicate.getExpressionType()
+			) );
+		}
+		else {
+			super.visitInListPredicate( inListPredicate );
 		}
 	}
 }

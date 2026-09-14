b3ff209dc22f542edc4844d22c4724cd78ab3e22
Gavin King <gavin@hibernate.org>
2026-09-14 20:20:35 +0200

    HHH-20828 Use ordinary versioning for non-excluded update-generated fields
    (cherry picked from commit 8e009fb3aafc98fe45c66339691db5efa9f78730)

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/locking/ExcludedGeneratedVersioningSelectingTests.java
@@ -0,0 +1,26 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.orm.test.locking;
+
+import org.hibernate.dialect.generated.spi.GeneratedValuesSupport;
+import org.hibernate.testing.orm.junit.ServiceRegistry;
+import org.hibernate.testing.orm.junit.Setting;
+
+import static org.hibernate.cfg.JdbcSettings.DIALECT;
+import static org.hibernate.dialect.generated.spi.GeneratedValuesSupport.Capability.ARBITRARY_GENERATED_KEYS;
+
+@ServiceRegistry(settings = @Setting(name = DIALECT,
+		value = "org.hibernate.orm.test.locking.ExcludedGeneratedVersioningSelectingTests$SelectingDialect"))
+public class ExcludedGeneratedVersioningSelectingTests extends ExcludedGeneratedVersioningNoReturningTests {
+	public static class SelectingDialect extends ExcludedFromVersioningNoReturningTests.NoReturningDialect {
+		@Override
+		public GeneratedValuesSupport getGeneratedValuesSupport() {
+			return GeneratedValuesSupport.builder( super.getGeneratedValuesSupport() )
+					.disable( ARBITRARY_GENERATED_KEYS )
+					.unquoteGeneratedKeyColumnNames( false )
+					.build();
+		}
+	}
+}

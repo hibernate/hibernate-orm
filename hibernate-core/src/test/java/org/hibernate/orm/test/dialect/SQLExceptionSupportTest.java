82d58e8b952beda113f4468e51323d975313f312
Gavin King <gavin@hibernate.org>
2026-09-14 03:28:03 +0200

    HHH-20879 fix SybseDialect.causesRollback()
    (cherry picked from commit 557bb8c1adab1a0c3728fd536e069781a7b285a9)

--- hibernate-core/src/test/java/org/hibernate/orm/test/dialect/SQLExceptionSupportTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/dialect/SQLExceptionSupportTest.java
@@ -5,15 +5,20 @@
 package org.hibernate.orm.test.dialect;
 
 import java.sql.SQLException;
+import java.util.stream.Stream;
 
+import org.hibernate.community.dialect.SybaseASELegacyDialect;
 import org.hibernate.dialect.DatabaseVersion;
 import org.hibernate.dialect.Dialect;
 import org.hibernate.dialect.H2Dialect;
 import org.hibernate.dialect.PostgreSQLDialect;
+import org.hibernate.dialect.SybaseASEDialect;
 import org.hibernate.exception.ConstraintViolationException;
 import org.hibernate.exception.spi.SQLExceptionConversionDelegate;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -54,5 +59,22 @@ public class SQLExceptionSupportTest {
 	@Test
 	void postgreSqlRecognizesRollbackCausingExceptions() {
 		assertThat( new PostgreSQLDialect().causesRollback( new SQLException() ) ).isTrue();
+	}
+
+	static Stream<Dialect> sybaseAseDialects() {
+		return Stream.of( new SybaseASEDialect(), new SybaseASELegacyDialect() );
+	}
+
+	@ParameterizedTest
+	@MethodSource("sybaseAseDialects")
+	void sybaseAseRecognizesRollbackOnLockTimeout(Dialect dialect) {
+		final var timeout = new SQLException( "Aborting the transaction", "S1000", 12205 );
+		assertThat( dialect.causesRollback( timeout ) ).isTrue();
+		final var wrapper = new SQLException( "Statement failed" );
+		wrapper.setNextException( timeout );
+		assertThat( dialect.causesRollback( wrapper ) ).isTrue();
+		// An explicit LOCK TABLE timeout reports 12207 and leaves the transaction intact.
+		assertThat( dialect.causesRollback( new SQLException( "Table lock unavailable", "S1000", 12207 ) ) )
+				.isFalse();
 	}
 }

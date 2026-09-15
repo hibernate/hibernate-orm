953c274c92189e78d3e51439dd10281b52fa2d4b
Gavin King <gavin@hibernate.org>
2026-09-15 11:58:39 +0200

    HHH-20878 fix temporal updates and deletes

--- hibernate-core/src/main/java/org/hibernate/persister/entity/mutation/AbstractTemporalUpdateCoordinator.java
+++ hibernate-core/src/main/java/org/hibernate/persister/entity/mutation/AbstractTemporalUpdateCoordinator.java
@@ -46,6 +46,7 @@ abstract class AbstractTemporalUpdateCoordinator extends AbstractMutationCoordin
 		applyTemporalEnding( tableUpdateBuilder, temporalMapping );
 		applyPartitionKeyRestriction( tableUpdateBuilder );
 		applyOptimisticLocking( tableUpdateBuilder );
+		applyTenantRestriction( tableUpdateBuilder );
 
 		return createMutationOperationGroup( tableUpdateBuilder );
 	}
@@ -75,6 +76,7 @@ abstract class AbstractTemporalUpdateCoordinator extends AbstractMutationCoordin
 						endUpdateGroup, session );
 		try {
 			final var jdbcValueBindings = mutationExecutor.getJdbcValueBindings();
+			bindTenantRestriction( session, jdbcValueBindings, endUpdateGroup );
 			for ( int i = 0; i < endUpdateGroup.getNumberOfOperations(); i++ ) {
 				breakDownKeyJdbcValues( id, rowId, session, jdbcValueBindings,
 						(EntityTableMapping) endUpdateGroup.getOperation( i ).getTableDetails() );

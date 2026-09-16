067aef70064fcf074d434e5bd17e66b75e1b6f71
Christian Beikov <christian.beikov@gmail.com>
2026-09-16 19:03:16 +0200

    HHH-20421 Skip native generator test when pooled sequences are not supported

--- hibernate-core/src/test/java/org/hibernate/orm/test/idgen/n_ative/NativeGeneratorTypeSequenceTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/idgen/n_ative/NativeGeneratorTypeSequenceTest.java
@@ -13,8 +13,10 @@ import jakarta.persistence.Table;
 import org.hibernate.annotations.NativeGenerator;
 import org.hibernate.id.enhanced.SequenceStyleGenerator;
 import org.hibernate.persister.entity.EntityPersister;
+import org.hibernate.testing.orm.junit.DialectFeatureChecks;
 import org.hibernate.testing.orm.junit.DomainModel;
 import org.hibernate.testing.orm.junit.JiraKey;
+import org.hibernate.testing.orm.junit.RequiresDialectFeature;
 import org.hibernate.testing.orm.junit.SessionFactory;
 import org.hibernate.testing.orm.junit.SessionFactoryScope;
 import org.junit.jupiter.api.AfterEach;
@@ -41,6 +43,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 				NativeGeneratorTypeSequenceTest.IncrSizeFiveStartTen.class
 		}
 )
+@RequiresDialectFeature(feature = DialectFeatureChecks.SupportPooledSequences.class)
 public class NativeGeneratorTypeSequenceTest {
 
 	@AfterEach

22f16cdfa81d0469bc858f0b55122fdc15837a8d
Gavin King <gavin@hibernate.org>
2026-09-17 21:33:06 +0200

    HHH-20883 Stabilize temporal tenant tests on Sybase
    (cherry picked from commit 49a27e698be5c100313b9d467d314c75cd61d79c)

--- hibernate-core/src/test/java/org/hibernate/orm/test/tenantid/TenantIdTemporalMutationTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/tenantid/TenantIdTemporalMutationTest.java
@@ -108,6 +108,7 @@ class TenantIdTemporalMutationTest {
 	@ValueSource(booleans = { false, true })
 	void managedMutationRejectsReusedIdentifier(boolean remove, SessionFactoryScope scope) {
 		inTenant( scope, "mine", session -> session.persist( new PlainItem() ) );
+		awaitTimestampTick();
 		try ( var session = scope.getSessionFactory().withOptions().tenantIdentifier( "mine" ).openSession() ) {
 			final var transaction = session.beginTransaction();
 			try {
@@ -152,6 +153,7 @@ class TenantIdTemporalMutationTest {
 		awaitTimestampTick();
 		inTenant( scope, tenant, session -> session.find( Item.class, 1L ).name = "changed" );
 		assertRows( scope, new Item(), "mine", "changed", 1, 2 );
+		awaitTimestampTick();
 		inTenant( scope, tenant, session -> session.remove( session.find( Item.class, 1L ) ) );
 		assertRows( scope, new Item(), "mine", "changed", 0, 2 );
 	}
@@ -161,6 +163,7 @@ class TenantIdTemporalMutationTest {
 	void upsertInsertsWhenOnlyHistoryOrNoRowExists(boolean history, SessionFactoryScope scope) {
 		if ( history ) {
 			inTenant( scope, "yours", session -> session.persist( new Item() ) );
+			awaitTimestampTick();
 			inTenant( scope, "yours", session -> session.remove( session.find( Item.class, 1L ) ) );
 			awaitTimestampTick();
 		}
